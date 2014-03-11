0dab6ead1a94154c63ee67d4192df4d8658f1890
sanikoyes <sanikoyes@163.com>
2014-03-11 13:50:17 +0800

    export lua sciprt is compiled to bytecode now

--- modules/luascript/register_types.cpp
+++ modules/luascript/register_types.cpp
@@ -15,6 +15,7 @@
 #include "io/resource_loader.h"
 #include "os/file_access.h"
 
+#include <memory.h>
 
 LuaScriptLanguage *script_language_lua=NULL;
 ResourceFormatLoaderLuaScript *resource_loader_lua=NULL;
@@ -35,19 +36,35 @@ public:
 	virtual Vector<uint8_t> custom_export(String& p_path,const Ref<EditorExportPlatform> &p_platform) {
 		//compile lua script to bytecode
 		if (p_path.ends_with(".lua")) {
-//			Vector<uint8_t> file = FileAccess::get_file_as_array(p_path);
-//			if (file.empty())
-//				return file;
-//			String txt;
-//			txt.parse_utf8((const char*)file.ptr(),file.size());
-//			file = GDTokenizerBuffer::parse_code_string(txt);
-//			if (!file.empty()) {
-//				print_line("PREV: "+p_path);
-//				p_path=p_path.basename()+".gdc";
-//				print_line("NOW: "+p_path);
-//				return file;
-//			}
-//
+			Vector<uint8_t> file = FileAccess::get_file_as_array(p_path);
+			if (file.empty())
+				return file;
+
+            lua_State *L = luaL_newstate();
+            luaL_openlibs(L);
+            if(!luaL_loadbuffer(L, (const char *) file.ptr(), file.size(), p_path.utf8().get_data()))
+            {
+                lua_getglobal(L, "string");
+                lua_getfield(L, -1, "dump");
+                lua_remove(L, -2);
+                lua_insert(L, -2);
+                lua_pushboolean(L, false); // true = strip debug info
+                if(!lua_pcall(L, 2, 1, 0))
+                {
+                    size_t sz;
+                    const char *bc = luaL_checklstring(L, -1, &sz);
+                    if(file.resize(sz + 1) == OK)
+                    {
+                        memcpy(&file[0], bc, sz);
+    				    print_line("PREV: "+p_path);
+	    			    p_path=p_path.basename()+".luac";
+		    		    print_line("NOW: "+p_path);
+                        lua_close(L);
+                        return file;
+                    }
+                }
+            }
+            lua_close(L);
 		}
 
 		return Vector<uint8_t>();
