e4a0e26cb1af28680796eae876335523568a2d75
sanikoyes <sanikoyes@163.com>
2014-04-24 10:16:19 +0800

    Update string format argument index Add string % operator( format )

--- core/variant_op.cpp
+++ core/variant_op.cpp
@@ -738,6 +738,18 @@ void Variant::evaluate(const Operator& p_op, const Variant& p_a, const Variant& 
 				_RETURN( p_a._data._int % p_b._data._int );
 			}
 
+			if (p_a.type==STRING && p_b.type==ARRAY) {
+
+				const Array& arr = p_b;
+				String str=p_a;
+				for(int i=0;i<arr.size();i++) {
+
+					str=str.replace(String("{") + String::num(i) + String("}"),arr[i].operator String());
+				}
+
+				_RETURN(str);
+			}
+
 			r_valid=false;
 			return;
 
