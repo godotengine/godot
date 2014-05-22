11ba9a30510f77f2e839eaa464cc1ca72462aa55
marynate <mary.w.nate@gmail.com>
2014-05-22 16:00:54 +0800

    Fix value evaluator virtual function signature

--- core/helper/value_evaluator.h
+++ core/helper/value_evaluator.h
@@ -35,7 +35,7 @@ class ValueEvaluator : public Object {
 
 	OBJ_TYPE(ValueEvaluator, Object);
 public:
-	virtual double eval(String& p_text) {
+	virtual double eval(const String& p_text) {
 		return p_text.to_double();
 	}
 };
