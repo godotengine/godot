9897d1304470fe9c6f80746e03ba6ad4e12df43b
sanikoyes <sanikoyes@163.com>
2014-07-07 17:13:37 +0800

    1、RichTextLabel::add_text - replace '\\n' to '\n'(newline) 2、add more html color tag support for bbcode parse

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -1020,9 +1020,11 @@ void RichTextLabel::_invalidate_current_line() {
 	}
 }
 
-void RichTextLabel::add_text(const String& p_text) {
+void RichTextLabel::add_text(const String& p_in_text) {
 
 	int pos=0;
+
+	String p_text=p_in_text.replace("\\n", "\n");
 
 	while (pos<p_text.length()) {
 
@@ -1282,6 +1284,158 @@ Error RichTextLabel::parse_bbcode(const String& p_bbcode) {
 	return append_bbcode(p_bbcode);
 }
 
+typedef struct {
+	const char *name;
+	const char *color;
+} html_color_map;
+
+static html_color_map color_map[]={
+	{ "aliceblue", "#f0f8ff" },
+	{ "antiquewhite", "#faebd7" },
+	{ "aqua", "#00ffff" },
+	{ "aquamarine", "#7fffd4" },
+	{ "azure", "#f0ffff" },
+	{ "beige", "#f5f5dc" },
+	{ "bisque", "#ffe4c4" },
+	{ "black", "#000000" },
+	{ "blanchedalmond", "#ffebcd" },
+	{ "blue", "#0000ff" },
+	{ "blueviolet", "#8a2be2" },
+	{ "brown", "#a52a2a" },
+	{ "burlywood", "#deb887" },
+	{ "cadetblue", "#5f9ea0" },
+	{ "chartreuse", "#7fff00" },
+	{ "chocolate", "#d2691e" },
+	{ "coral", "#ff7f50" },
+	{ "cornflowerblue", "#6495ed" },
+	{ "cornsilk", "#fff8dc" },
+	{ "crimson", "#dc143c" },
+	{ "cyan", "#00ffff" },
+	{ "darkblue", "#00008b" },
+	{ "darkcyan", "#008b8b" },
+	{ "darkgoldenrod", "#b8860b" },
+	{ "darkgray", "#a9a9a9" },
+	{ "darkgreen", "#006400" },
+	{ "darkkhaki", "#bdb76b" },
+	{ "darkmagenta", "#8b008b" },
+	{ "darkolivegreen", "#556b2f" },
+	{ "darkorange", "#ff8c00" },
+	{ "darkorchid", "#9932cc" },
+	{ "darkred", "#8b0000" },
+	{ "darksalmon", "#e9967a" },
+	{ "darkseagreen", "#8fbc8f" },
+	{ "darkslateblue", "#483d8b" },
+	{ "darkslategray", "#2f4f4f" },
+	{ "darkturquoise", "#00ced1" },
+	{ "darkviolet", "#9400d3" },
+	{ "deeppink", "#ff1493" },
+	{ "deepskyblue", "#00bfff" },
+	{ "dimgray", "#696969" },
+	{ "dodgerblue", "#1e90ff" },
+	{ "feldspar", "#d19275" },
+	{ "firebrick", "#b22222" },
+	{ "floralwhite", "#fffaf0" },
+	{ "forestgreen", "#228b22" },
+	{ "fuchsia", "#ff00ff" },
+	{ "gainsboro", "#dcdcdc" },
+	{ "ghostwhite", "#f8f8ff" },
+	{ "gold", "#ffd700" },
+	{ "goldenrod", "#daa520" },
+	{ "gray", "#808080" },
+	{ "green", "#008000" },
+	{ "greenyellow", "#adff2f" },
+	{ "honeydew", "#f0fff0" },
+	{ "hotpink", "#ff69b4" },
+	{ "indianred ", "#cd5c5c" },
+	{ "indigo ", "#4b0082" },
+	{ "ivory", "#fffff0" },
+	{ "khaki", "#f0e68c" },
+	{ "lavender", "#e6e6fa" },
+	{ "lavenderblush", "#fff0f5" },
+	{ "lawngreen", "#7cfc00" },
+	{ "lemonchiffon", "#fffacd" },
+	{ "lightblue", "#add8e6" },
+	{ "lightcoral", "#f08080" },
+	{ "lightcyan", "#e0ffff" },
+	{ "lightgoldenrodyellow", "#fafad2" },
+	{ "lightgrey", "#d3d3d3" },
+	{ "lightgreen", "#90ee90" },
+	{ "lightpink", "#ffb6c1" },
+	{ "lightsalmon", "#ffa07a" },
+	{ "lightseagreen", "#20b2aa" },
+	{ "lightskyblue", "#87cefa" },
+	{ "lightslateblue", "#8470ff" },
+	{ "lightslategray", "#778899" },
+	{ "lightsteelblue", "#b0c4de" },
+	{ "lightyellow", "#ffffe0" },
+	{ "lime", "#00ff00" },
+	{ "limegreen", "#32cd32" },
+	{ "linen", "#faf0e6" },
+	{ "magenta", "#ff00ff" },
+	{ "maroon", "#800000" },
+	{ "mediumaquamarine", "#66cdaa" },
+	{ "mediumblue", "#0000cd" },
+	{ "mediumorchid", "#ba55d3" },
+	{ "mediumpurple", "#9370d8" },
+	{ "mediumseagreen", "#3cb371" },
+	{ "mediumslateblue", "#7b68ee" },
+	{ "mediumspringgreen", "#00fa9a" },
+	{ "mediumturquoise", "#48d1cc" },
+	{ "mediumvioletred", "#c71585" },
+	{ "midnightblue", "#191970" },
+	{ "mintcream", "#f5fffa" },
+	{ "mistyrose", "#ffe4e1" },
+	{ "moccasin", "#ffe4b5" },
+	{ "navajowhite", "#ffdead" },
+	{ "navy", "#000080" },
+	{ "oldlace", "#fdf5e6" },
+	{ "olive", "#808000" },
+	{ "olivedrab", "#6b8e23" },
+	{ "orange", "#ffa500" },
+	{ "orangered", "#ff4500" },
+	{ "orchid", "#da70d6" },
+	{ "palegoldenrod", "#eee8aa" },
+	{ "palegreen", "#98fb98" },
+	{ "paleturquoise", "#afeeee" },
+	{ "palevioletred", "#d87093" },
+	{ "papayawhip", "#ffefd5" },
+	{ "peachpuff", "#ffdab9" },
+	{ "peru", "#cd853f" },
+	{ "pink", "#ffc0cb" },
+	{ "plum", "#dda0dd" },
+	{ "powderblue", "#b0e0e6" },
+	{ "purple", "#800080" },
+	{ "red", "#ff0000" },
+	{ "rosybrown", "#bc8f8f" },
+	{ "royalblue", "#4169e1" },
+	{ "saddlebrown", "#8b4513" },
+	{ "salmon", "#fa8072" },
+	{ "sandybrown", "#f4a460" },
+	{ "seagreen", "#2e8b57" },
+	{ "seashell", "#fff5ee" },
+	{ "sienna", "#a0522d" },
+	{ "silver", "#c0c0c0" },
+	{ "skyblue", "#87ceeb" },
+	{ "slateblue", "#6a5acd" },
+	{ "slategray", "#708090" },
+	{ "snow", "#fffafa" },
+	{ "springgreen", "#00ff7f" },
+	{ "steelblue", "#4682b4" },
+	{ "tan", "#d2b48c" },
+	{ "teal", "#008080" },
+	{ "thistle", "#d8bfd8" },
+	{ "tomato", "#ff6347" },
+	{ "turquoise", "#40e0d0" },
+	{ "violet", "#ee82ee" },
+	{ "violetred", "#d02090" },
+	{ "wheat", "#f5deb3" },
+	{ "white", "#ffffff" },
+	{ "whitesmoke", "#f5f5f5" },
+	{ "yellow", "#ffff00" },
+	{ "yellowgreen", "#9acd32" },
+	{ NULL, NULL },
+};
+
 Error RichTextLabel::append_bbcode(const String& p_bbcode) {
 
 	int pos = 0;
@@ -1406,40 +1560,53 @@ Error RichTextLabel::append_bbcode(const String& p_bbcode) {
 
 			if (col.begins_with("#"))
 				color=Color::html(col);
-			else if (col=="aqua")
-				color=Color::html("#00FFFF");
-			else if (col=="black")
-				color=Color::html("#000000");
-			else if (col=="blue")
-				color=Color::html("#0000FF");
-			else if (col=="fuchsia")
-				color=Color::html("#FF00FF");
-			else if (col=="gray" || col=="grey")
-				color=Color::html("#808080");
-			else if (col=="green")
-				color=Color::html("#008000");
-			else if (col=="lime")
-				color=Color::html("#00FF00");
-			else if (col=="maroon")
-				color=Color::html("#800000");
-			else if (col=="navy")
-				color=Color::html("#000080");
-			else if (col=="olive")
-				color=Color::html("#808000");
-			else if (col=="purple")
-				color=Color::html("#800080");
-			else if (col=="red")
-				color=Color::html("#FF0000");
-			else if (col=="silver")
-				color=Color::html("#C0C0C0");
-			else if (col=="teal")
-				color=Color::html("#008008");
-			else if (col=="white")
-				color=Color::html("#FFFFFF");
-			else if (col=="yellow")
-				color=Color::html("#FFFF00");
 			else
-				color=base_color;
+			{
+				String col_lower=col.to_lower();
+				for(size_t idx=0; idx < sizeof(color_map) / sizeof(color_map[0]); idx++) {
+					html_color_map& cm=color_map[idx];
+					if(cm.name==NULL)
+						color=base_color;
+					else if(col_lower==cm.name) {
+						color=Color::html(cm.color);
+						break;
+					}
+				}
+			}
+			//else if (col=="aqua")
+			//	color=Color::html("#00FFFF");
+			//else if (col=="black")
+			//	color=Color::html("#000000");
+			//else if (col=="blue")
+			//	color=Color::html("#0000FF");
+			//else if (col=="fuchsia")
+			//	color=Color::html("#FF00FF");
+			//else if (col=="gray" || col=="grey")
+			//	color=Color::html("#808080");
+			//else if (col=="green")
+			//	color=Color::html("#008000");
+			//else if (col=="lime")
+			//	color=Color::html("#00FF00");
+			//else if (col=="maroon")
+			//	color=Color::html("#800000");
+			//else if (col=="navy")
+			//	color=Color::html("#000080");
+			//else if (col=="olive")
+			//	color=Color::html("#808000");
+			//else if (col=="purple")
+			//	color=Color::html("#800080");
+			//else if (col=="red")
+			//	color=Color::html("#FF0000");
+			//else if (col=="silver")
+			//	color=Color::html("#C0C0C0");
+			//else if (col=="teal")
+			//	color=Color::html("#008008");
+			//else if (col=="white")
+			//	color=Color::html("#FFFFFF");
+			//else if (col=="yellow")
+			//	color=Color::html("#FFFF00");
+			//else
+			//	color=base_color;
 
 
 
