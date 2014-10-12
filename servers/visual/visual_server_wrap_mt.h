b0e95ffac0c8d0fdf2e5def7c94a69a378e0b352
Juan Linietsky <reduzio@gmail.com>
2014-10-12 02:13:22 -0300

    Little Bits -=-=-=-=-=-
    -fix duplicate function bug when creating script callback in editor
    -fix bug where hiding lights does not work
    -fix 2D audio listener bug (romulox_x reported)
    -fix exported properties with inheritance bug
    -fix timer autostart (make it not work on editor)
    -reactivate first camara found if viewport runs out of active camera
    -option to hide gizmos in viewport
    -changed skeleton gizmo because it sucks
    -Make convex shapes using CollisionShape visible (use quickhull class)
    -fix up menu when editing a mesh, to export collision, navmesh, convex, etc. from it.
    -make a menu option to show SRGB in 3D editor views by default
    -make option to edit default light direction in viewport settings
    -make option to edit default ambient light in viewport settings
    -make software conversion of linear->RGB if hardware support not found

--- servers/visual/visual_server_wrap_mt.h
+++ servers/visual/visual_server_wrap_mt.h
@@ -929,6 +929,7 @@ public:
 	FUNC2(camera_set_environment,RID,RID);
 	FUNC1RC(RID,camera_get_environment,RID);
 
+
 	FUNC2(camera_set_use_vertical_aspect,RID,bool);
 	FUNC2RC(bool,camera_is_using_vertical_aspect,RID,bool);
 
@@ -998,6 +999,7 @@ public:
 	FUNC2(scenario_set_debug,RID,ScenarioDebugMode);
 	FUNC2(scenario_set_environment,RID, RID);
 	FUNC2RC(RID,scenario_get_environment,RID, RID);
+	FUNC2(scenario_set_fallback_environment,RID, RID);
 
 
 	/* INSTANCING API */
