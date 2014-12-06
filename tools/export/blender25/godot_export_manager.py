cfd040255796a243394fffb3b3c18cb0400bf933
ndee85 <andreasesau@gmail.com>
2014-12-06 14:28:16 +0100

    Added a GPL Licence Block to the addon.

--- tools/export/blender25/godot_export_manager.py
+++ tools/export/blender25/godot_export_manager.py
@@ -1,3 +1,23 @@
+# ##### BEGIN GPL LICENSE BLOCK #####
+#
+# This program is free software; you can redistribute it and/or
+# modify it under the terms of the GNU General Public License
+# as published by the Free Software Foundation; either version 2
+# of the License, or (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software Foundation,
+# Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA.
+#
+# ##### END GPL LICENSE BLOCK #####
+
+# Script copyright (c) Andreas Esau
+
 bl_info = {
     "name": "Godot Export Manager",
     "author": "Andreas Esau",
