dfc59d1e006a0c7c90d48d2781b48000cbd7c88c
Gen <z.rz2323721@gmail.com>
2015-04-28 23:07:41 +0800

    Update label.h

--- scene/gui/label.h
+++ scene/gui/label.h
@@ -75,8 +75,9 @@ private:
 		int char_pos; // if -1, then newline
 		int word_len;
 		int pixel_width;
+		bool space_insert;
 		WordCache *next;
-		WordCache() { char_pos=0; word_len=0; pixel_width=0; next=0; }
+		WordCache() { char_pos=0; word_len=0; pixel_width=0; next=0; space_insert=true;}
 	};	
 	
 	bool word_cache_dirty;
