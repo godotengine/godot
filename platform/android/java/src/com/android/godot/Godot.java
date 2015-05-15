bc3afc8ed8659c1aea2fa349a7d826f7f01e39ef
vipsbpig <vipsbpig@gmail.com>
2015-05-15 12:40:34 +0800

    fix multitouch release problem

--- platform/android/java/src/com/android/godot/Godot.java
+++ platform/android/java/src/com/android/godot/Godot.java
@@ -747,7 +747,8 @@ public class Godot extends Activity implements SensorEventListener, IDownloaderC
 				//}
 			} break;
 			case MotionEvent.ACTION_POINTER_UP: {
-				int pointer_idx = event.getActionIndex();
+				final int indexPointUp = event.getAction() >> MotionEvent.ACTION_POINTER_INDEX_SHIFT;
+				final int pointer_idx = event.getPointerId(indexPointUp); 
 				GodotLib.touch(4,pointer_idx,evcount,arr);
 				//System.out.printf("%d - s.up at: %f,%f\n",pointer_idx, event.getX(pointer_idx),event.getY(pointer_idx));
 			} break;
