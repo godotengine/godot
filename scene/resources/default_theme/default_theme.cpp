899953397e2c08b9a779c2073d1d5c6a1b5ac772
sanikoyes <sanikoyes@163.com>
2015-06-13 16:57:30 +0800

    Fix theme

--- scene/resources/default_theme/default_theme.cpp
+++ scene/resources/default_theme/default_theme.cpp
@@ -922,7 +922,6 @@ void make_default_theme() {
 	t->set_constant("margin","MarginContainer",8);
 	t->set_constant("hseparation","GridContainer",4);
 	t->set_constant("vseparation","GridContainer",4);
-	t->set_constant("center_separation","GridContainer", false);
 
 	t->set_constant("separation","HSplitContainer",8);
 	t->set_constant("separation","VSplitContainer",8);
