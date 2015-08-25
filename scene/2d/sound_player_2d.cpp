21dd41b34d792f69a199ba0ef7b6847d14277786
Hearto Lazor <heartolazor@gmail.com>
2015-08-25 04:00:32 -0300

    Ongoing sound stop playing on change parent, I found exact lines which provokes the behavior on sound_player_2d:                  case NOTIFICATION_EXIT_TREE: {                         if (source_rid.is_valid())                                 SpatialSound2DServer::get_singleton()->free(source_rid);                 } break; Moved free to NOTIFICATION_PREDELETE and only create source_rid if is not created on ENTER_TREE, this is for preserve reference after change parent so PREDELETE correctly free the sound and stop it

--- scene/2d/sound_player_2d.cpp
+++ scene/2d/sound_player_2d.cpp
@@ -42,12 +42,14 @@ void SoundPlayer2D::_notification(int p_what) {
 		case NOTIFICATION_ENTER_TREE: {
 			//find the sound space
 
-			source_rid = SpatialSound2DServer::get_singleton()->source_create(get_world_2d()->get_sound_space());
+			if (!source_rid.is_valid()) {
+				source_rid = SpatialSound2DServer::get_singleton()->source_create(get_world_2d()->get_sound_space());
 
-			for(int i=0;i<PARAM_MAX;i++)
-				set_param(Param(i),params[i]);
+				for (int i = 0; i < PARAM_MAX; i++)
+					set_param(Param(i), params[i]);
 
-			SpatialSound2DServer::get_singleton()->source_set_transform(source_rid,get_global_transform());
+				SpatialSound2DServer::get_singleton()->source_set_transform(source_rid, get_global_transform());
+			}
 
 
 		} break;
@@ -58,9 +60,13 @@ void SoundPlayer2D::_notification(int p_what) {
 		} break;
 		case NOTIFICATION_EXIT_TREE: {
 
+			//if (source_rid.is_valid())
+			//	SpatialSound2DServer::get_singleton()->free(source_rid);
+
+		} break;
+		case NOTIFICATION_PREDELETE: {
 			if (source_rid.is_valid())
 				SpatialSound2DServer::get_singleton()->free(source_rid);
-
 		} break;
 	}
 
