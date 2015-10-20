bb3724a4cc737599d5b12abeb8b1590781bb5ddb
Zher Huei Lee <lee.zh.92@gmail.com>
2015-10-20 09:51:00 +0100

    Updated RegEx demos

--- demos/misc/regex/regex.gd
+++ demos/misc/regex/regex.gd
@@ -2,21 +2,23 @@ extends VBoxContainer
 
 var regex = RegEx.new()
 
-func update_expression():
-	regex.compile(get_node("Expression").get_text())
+func update_expression(text):
+	regex.compile(text)
 	update_text()
 
 func update_text():
 	var text = get_node("Text").get_text()
-	regex.find(text)
 	var list = get_node("List")
 	for child in list.get_children():
 		child.queue_free()
-	for res in regex.get_captures():
-		var label = Label.new()
-		label.set_text(res)
-		list.add_child(label)
+	if regex.is_valid():
+		regex.find(text)
+		for res in regex.get_captures():
+			var label = Label.new()
+			label.set_text(res)
+			list.add_child(label)
 
 func _ready():
 	get_node("Text").set_text("They asked me \"What's going on \\\"in the manor\\\"?\"")
-	update_expression()
+	update_expression(get_node("Expression").get_text())
+
