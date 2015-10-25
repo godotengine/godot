2ba5931dd30b9322113986f7c1abf58407d861e6
Juan Linietsky <reduzio@gmail.com>
2015-10-25 20:08:18 -0300

    -Fixed a bug in scrollcontainer not respecting expand flag If you relied on this, make sure the children of your scrollcontainer is set to expand with the expand flag in either vertical or horizontal axis

--- scene/gui/scroll_container.cpp
+++ scene/gui/scroll_container.cpp
@@ -195,11 +195,19 @@ void ScrollContainer::_notification(int p_what) {
 			Rect2 r = Rect2(-scroll,minsize);
 			if (!scroll_h) {
 				r.pos.x=0;
-				r.size.width=size.width;
+				if (c->get_h_size_flags()&SIZE_EXPAND)
+					r.size.width=MAX(size.width,minsize.width);
+				else
+					r.size.width=minsize.width;
 			}
 			if (!scroll_v) {
 				r.pos.y=0;
 				r.size.height=size.height;
+				if (c->get_v_size_flags()&SIZE_EXPAND)
+					r.size.height=MAX(size.height,minsize.height);
+				else
+					r.size.height=minsize.height;
+
 			}
 			fit_child_in_rect(c,r);
 		}
