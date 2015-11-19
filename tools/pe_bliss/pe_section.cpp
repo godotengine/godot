2b36da0f79dd4b279a28fe2ff850fb4664e1a79f
ZuBsPaCe <kurt.rudin@gmx.net>
2015-11-19 11:07:46 +0100

    Fixes Visual Studio compile error due to pe_bliss
    Error: C2039: 'min': is not a member of 'std'
    Reason: #2518 introduced pe_bliss
    
    Fix: Visual Studio 2015 & 2013 need to include algorithm header before string, otherwise std:min cannot be used.
    See: http://stackoverflow.com/questions/17409956

--- tools/pe_bliss/pe_section.cpp
+++ tools/pe_bliss/pe_section.cpp
@@ -19,6 +19,7 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
+#include <algorithm>
 #include <string.h>
 #include "utils.h"
 #include "pe_section.h"
