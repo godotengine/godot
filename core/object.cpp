8122ea466cd4b6104f863f9a28c5bd80453b885c
admix <eugene.andreeshchev@gmail.com>
2015-11-28 16:47:09 +0100

    fixed crash when creating new object with wrong number of arguments

--- core/object.cpp
+++ core/object.cpp
@@ -970,7 +970,10 @@ void Object::set_script_instance(ScriptInstance *p_instance) {
 
 	script_instance=p_instance;
 
-	script=p_instance->get_script().get_ref_ptr();
+	if (p_instance)
+		script=p_instance->get_script().get_ref_ptr();
+	else
+		script=RefPtr();
 }
 
 RefPtr Object::get_script() const {
