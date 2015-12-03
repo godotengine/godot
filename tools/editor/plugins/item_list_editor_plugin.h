9f07f58911e636ce301e52f72855c75c54cb4c87
Franklin Sobrinho <franklin_gs@hotmail.com>
2015-12-03 14:59:59 -0300

    Make ItemListEditor plugin functional

--- tools/editor/plugins/item_list_editor_plugin.h
+++ tools/editor/plugins/item_list_editor_plugin.h
@@ -31,10 +31,11 @@
 
 #include "tools/editor/editor_plugin.h"
 #include "tools/editor/editor_node.h"
+#include "canvas_item_editor_plugin.h"
+
 #include "scene/gui/option_button.h"
 #include "scene/gui/menu_button.h"
 #include "scene/gui/popup_menu.h"
-#include "scene/gui/spin_box.h"
 
 /**
 	@author Juan Linietsky <reduzio@gmail.com>
@@ -51,43 +52,42 @@ protected:
 	bool _get(const StringName& p_name,Variant &r_ret) const;
 	void _get_property_list( List<PropertyInfo> *p_list) const;
 
-
 public:
 
 	enum Flags {
 
 		FLAG_ICON=1,
 		FLAG_CHECKABLE=2,
-		FLAG_ACCEL=4,
-		FLAG_ID=8,
-		FLAG_ENABLE=16,
-		FLAG_SEPARATOR=32
+		FLAG_ID=4,
+		FLAG_ENABLE=8,
+		FLAG_SEPARATOR=16
 	};
 
 	virtual void set_object(Object *p_object)=0;
-
 	virtual bool handles(Object *p_object) const=0;
 
 	virtual int get_flags() const=0;
 
-	virtual void set_item_text(int p_idx,const String& p_text){}
-	virtual void set_item_icon(int p_idx,const Ref<Texture>& p_tex){}
-	virtual void set_item_checkable(int p_idx,bool p_check){}
-	virtual void set_item_checked(int p_idx,bool p_checked){}
-	virtual void set_item_accel(int p_idx,int p_accel){}
-	virtual void set_item_enabled(int p_idx,int p_enabled){}
-	virtual void set_item_id(int p_idx,int p_id){}
-	virtual void set_item_separator(int p_idx,bool p_separator){}
-
-
+	virtual void set_item_text(int p_idx, const String& p_text) {}
 	virtual String get_item_text(int p_idx) const{ return ""; };
+
+	virtual void set_item_icon(int p_idx, const Ref<Texture>& p_tex) {}
 	virtual Ref<Texture> get_item_icon(int p_idx) const{ return Ref<Texture>(); };
+
+	virtual void set_item_checkable(int p_idx, bool p_check) {}
 	virtual bool is_item_checkable(int p_idx) const{ return false; };
+
+	virtual void set_item_checked(int p_idx, bool p_checked) {}
 	virtual bool is_item_checked(int p_idx) const{ return false; };
-	virtual int get_item_accel(int p_idx) const{ return 0; };
+
+	virtual void set_item_enabled(int p_idx, int p_enabled) {}
 	virtual bool is_item_enabled(int p_idx) const{ return false; };
+
+	virtual void set_item_id(int p_idx, int p_id) {}
 	virtual int get_item_id(int p_idx) const{ return -1; };
-	virtual bool is_item_separator(int p_idx) const{ return false; };
+
+	virtual void set_item_separator(int p_idx, bool p_separator) {}
+	virtual bool is_item_separator(int p_idx) const { return false; };
 
 	virtual void add_item()=0;
 	virtual int get_item_count() const=0;
@@ -96,41 +96,107 @@ public:
 	ItemListPlugin() {}
 };
 
-class ItemListEditor : public Control {
+///////////////////////////////////////////////////////////////
 
-	OBJ_TYPE(ItemListEditor, Control );
+class ItemListOptionButtonPlugin : public ItemListPlugin {
+
+	OBJ_TYPE(ItemListOptionButtonPlugin,ItemListPlugin);
+
+	OptionButton *ob;
+public:
+
+	virtual void set_object(Object *p_object);
+	virtual bool handles(Object *p_object) const;
+	virtual int get_flags() const;
+
+	virtual void set_item_text(int p_idx, const String& p_text) { ob->set_item_text(p_idx,p_text); }
+	virtual String get_item_text(int p_idx) const { return ob->get_item_text(p_idx); }
+
+	virtual void set_item_icon(int p_idx, const Ref<Texture>& p_tex) { ob->set_item_icon(p_idx, p_tex); }
+	virtual Ref<Texture> get_item_icon(int p_idx) const { return ob->get_item_icon(p_idx); }
+
+	virtual void set_item_enabled(int p_idx, int p_enabled) { ob->set_item_disabled(p_idx, !p_enabled); }
+	virtual bool is_item_enabled(int p_idx) const { return !ob->is_item_disabled(p_idx); }
+
+	virtual void set_item_id(int p_idx, int p_id) { ob->set_item_ID(p_idx,p_id); }
+	virtual int get_item_id(int p_idx) const { return ob->get_item_ID(p_idx); }
+
+	virtual void add_item();
+	virtual int get_item_count() const;
+	virtual void erase(int p_idx);
+
+	ItemListOptionButtonPlugin();
+};
+
+class ItemListPopupMenuPlugin : public ItemListPlugin {
+
+	OBJ_TYPE(ItemListPopupMenuPlugin,ItemListPlugin);
+
+	PopupMenu *pp;
+public:
+
+	virtual void set_object(Object *p_object);
+	virtual bool handles(Object *p_object) const;
+	virtual int get_flags() const;
+
+	virtual void set_item_text(int p_idx, const String& p_text) { pp->set_item_text(p_idx,p_text); }
+	virtual String get_item_text(int p_idx) const { return pp->get_item_text(p_idx); }
+
+	virtual void set_item_icon(int p_idx, const Ref<Texture>& p_tex) { pp->set_item_icon(p_idx,p_tex); }
+	virtual Ref<Texture> get_item_icon(int p_idx) const { return pp->get_item_icon(p_idx); }
+
+	virtual void set_item_checkable(int p_idx, bool p_check) { pp->set_item_as_checkable(p_idx,p_check); }
+	virtual bool is_item_checkable(int p_idx) const { return pp->is_item_checkable(p_idx); }
+
+	virtual void set_item_checked(int p_idx, bool p_checked) { pp->set_item_checked(p_idx,p_checked); }
+	virtual bool is_item_checked(int p_idx) const { return pp->is_item_checked(p_idx); }
+
+	virtual void set_item_enabled(int p_idx, int p_enabled) { pp->set_item_disabled(p_idx,!p_enabled); }
+	virtual bool is_item_enabled(int p_idx) const { return !pp->is_item_disabled(p_idx); }
+
+	virtual void set_item_id(int p_idx, int p_id) { pp->set_item_ID(p_idx,p_idx); }
+	virtual int get_item_id(int p_idx) const { return pp->get_item_ID(p_idx); }
+
+	virtual void set_item_separator(int p_idx, bool p_separator) { pp->set_item_as_separator(p_idx,p_separator); }
+	virtual bool is_item_separator(int p_idx) const { return pp->is_item_separator(p_idx); }
+
+	virtual void add_item();
+	virtual int get_item_count() const;
+	virtual void erase(int p_idx);
+
+	ItemListPopupMenuPlugin();
+};
+
+///////////////////////////////////////////////////////////////
+
+class ItemListEditor : public HBoxContainer {
+
+	OBJ_TYPE(ItemListEditor,HBoxContainer);
 
 	Node *item_list;
 
-	enum {
-
-		MENU_EDIT_ITEMS,
-		MENU_CLEAR
-	};
+	ToolButton *toolbar_button;
 
 	AcceptDialog *dialog;
-
-	PropertyEditor *prop_editor;
-
-	MenuButton * options;
-	int selected_idx;
-
+	PropertyEditor *property_editor;
+	Tree *tree;
 	Button *add_button;
 	Button *del_button;
 
-
-//	FileDialog *emission_file_dialog;
-	void _menu_option(int);
+	int selected_idx;
 
 	Vector<ItemListPlugin*> item_plugins;
 
-	void _node_removed(Node *p_node);
+	void _edit_items();
+
 	void _add_pressed();
 	void _delete_pressed();
+
+	void _node_removed(Node *p_node);
+
 protected:
 
 	void _notification(int p_notification);
-
 	static void _bind_methods();
 public:
 
@@ -143,7 +209,7 @@ public:
 
 class ItemListEditorPlugin : public EditorPlugin {
 
-	OBJ_TYPE( ItemListEditorPlugin, EditorPlugin );
+	OBJ_TYPE(ItemListEditorPlugin,EditorPlugin);
 
 	ItemListEditor *item_list_editor;
 	EditorNode *editor;
