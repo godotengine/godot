974e5362bcb9bf32ee5fdb13936a1231fec93567
reduz <reduzio@gmail.com>
2015-12-29 12:14:02 -0300

    -forgot a check for tools enabled, closes #3166

--- scene/gui/popup.cpp
+++ scene/gui/popup.cpp
@@ -48,9 +48,11 @@ void Popup::_notification(int p_what) {
 
 	if (p_what==NOTIFICATION_ENTER_TREE) {
 		//small helper to make editing of these easier in editor
+#ifdef TOOLS_ENABLED
 		if (get_tree()->is_editor_hint() && get_tree()->get_edited_scene_root() && get_tree()->get_edited_scene_root()->is_a_parent_of(this)) {
 			set_as_toplevel(false);
 		}
+#endif
 	}
 
 }
