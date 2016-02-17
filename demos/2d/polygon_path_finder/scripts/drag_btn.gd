cc53b3b29ddffd0405b8f09e4d5cb4e4c798a248
Adham Zahran <adham.zahran@hotmail.com>
2016-02-17 13:23:28 +0200

    polygon path finder demo is now interactive and dynamic

---
+++ demos/2d/polygon_path_finder/scripts/drag_btn.gd
@@ -0,0 +1,27 @@
+extends Sprite
+
+var mouse_inside
+var mouse_pressed
+
+func _ready():
+	set_process_input(true)
+	set_process(true)
+	self.connect("mouse_enter", self, "_on_mouse_enter")
+	self.connect("mouse_exit", self, "_on_mouse_exit")
+
+func _on_mouse_enter():
+	mouse_inside = true
+
+func _on_mouse_exit():
+	mouse_inside = false
+
+func _input(event):
+	if event.is_action_pressed("click") and mouse_inside:
+		mouse_pressed = true
+	elif event.is_action_released("click") and mouse_inside:
+		mouse_pressed = false
+
+func _process(delta):
+	if mouse_pressed:
+		var mouse_position = get_viewport().get_mouse_pos()
+		set_pos(mouse_position)
