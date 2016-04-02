1d771a8fe0a101014de7bb6f2ad351cf57f64387
Luiz Paulo <luizpvasc@gmail.com>
2016-04-02 17:53:03 -0300

    Change toggle comment behaviour. Fixes #4198

--- tools/editor/plugins/script_editor_plugin.cpp
+++ tools/editor/plugins/script_editor_plugin.cpp
@@ -1256,6 +1256,12 @@ void ScriptEditor::_menu_option(int p_option) {
 				{
 					int begin = tx->get_selection_from_line();
 					int end = tx->get_selection_to_line();
+					int column_end = tx->get_selection_to_column();
+
+					// End of selection ends on the first column of the last line, ignore it.
+					if(column_end == 0)
+						end -= 1;
+					
 					for (int i = begin; i <= end; i++)
 					{
 						String line_text = tx->get_line(i);
