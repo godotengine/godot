43c74056b1764f07acc252e78e1cfa29a49a51c3
MSC <m.schlack@protonmail.com>
2016-04-03 19:24:14 +0200

    free Material 'shadow_material_double_sided' on ::finish() (cherry picked from commit 7a9998370f0a805a75634f31558abba9c4d74a48)

--- drivers/gles2/rasterizer_gles2.cpp
+++ drivers/gles2/rasterizer_gles2.cpp
@@ -10998,6 +10998,7 @@ void RasterizerGLES2::finish() {
 
 	free(default_material);
 	free(shadow_material);
+	free(shadow_material_double_sided);
 	free(canvas_shadow_blur);
 	free( overdraw_material );
 }
