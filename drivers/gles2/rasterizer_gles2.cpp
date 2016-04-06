00566a859268f541d807e26c0d12fb9ced202af6
Rémi Verschelde <rverschelde@gmail.com>
2016-04-06 19:37:16 +0200

    Revert "free Material 'shadow_material_double_sided' on ::finish()"
    This reverts commit 43c74056b1764f07acc252e78e1cfa29a49a51c3.

--- drivers/gles2/rasterizer_gles2.cpp
+++ drivers/gles2/rasterizer_gles2.cpp
@@ -10998,7 +10998,6 @@ void RasterizerGLES2::finish() {
 
 	free(default_material);
 	free(shadow_material);
-	free(shadow_material_double_sided);
 	free(canvas_shadow_blur);
 	free( overdraw_material );
 }
