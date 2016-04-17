0ca7e192423e743db3ded3822ae70888a523a35e
Mounir Ybanez <rinuom91@gmail.com>
2016-04-17 12:23:34 +0800

    Added getter method for the connection property in HTTPClient. (#4336)
    - Exposed a getter method for the private property named `connection` in HTTPClient class.

--- core/io/http_client.h
+++ core/io/http_client.h
@@ -168,6 +168,7 @@ public:
 	Error connect(const String &p_host,int p_port,bool p_ssl=false,bool p_verify_host=true);
 
 	void set_connection(const Ref<StreamPeer>& p_connection);
+	Ref<StreamPeer> get_connection() const;
 
 	Error request( Method p_method, const String& p_url, const Vector<String>& p_headers,const String& p_body=String());
 	Error send_body_text(const String& p_body);
