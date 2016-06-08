36fcf4714ab4484265cab0389a532b90b5c86445
J08nY <johny@neuromancer.sk>
2016-06-08 11:26:54 +0200

    RtAudio: proper under/overflow warning
    fix #5102

--- drivers/rtaudio/audio_driver_rtaudio.cpp
+++ drivers/rtaudio/audio_driver_rtaudio.cpp
@@ -32,8 +32,14 @@ const char* AudioDriverRtAudio::get_name() const {
 int AudioDriverRtAudio::callback( void *outputBuffer, void *inputBuffer, unsigned int nBufferFrames,
 	double streamTime, RtAudioStreamStatus status, void *userData ) {
 
-	if (status)
-		print_line("lost?");
+	if (status) {
+		if (status & RTAUDIO_INPUT_OVERFLOW) {
+			WARN_PRINT("RtAudio input overflow!");
+		}
+		if (status & RTAUDIO_OUTPUT_UNDERFLOW) {
+			WARN_PRINT("RtAudio output underflow!");
+		}
+	}
 	int32_t *buffer = (int32_t *) outputBuffer;
 
 	AudioDriverRtAudio *self = (AudioDriverRtAudio*)userData;
