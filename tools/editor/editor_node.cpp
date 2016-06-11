e8209b9c5cd40649e0ab87515d20b4aab6c9feb0
Juan Linietsky <reduzio@gmail.com>
2016-06-11 20:51:44 -0300

    Prevent crash in specific situation of removing a plugin, fixes #5019

--- tools/editor/editor_node.cpp
+++ tools/editor/editor_node.cpp
@@ -3089,6 +3089,7 @@ void EditorNode::set_addon_plugin_enabled(const String& p_addon,bool p_enabled) 
 	if (!p_enabled) {
 
 		EditorPlugin *addon = plugin_addons[p_addon];
+		editor_data.remove_editor_plugin( addon );
 		memdelete(addon); //bye
 		plugin_addons.erase(p_addon);
 		_update_addon_config();
