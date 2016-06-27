081c400ac6481192d0df214fac9da0f3206c1db7
sanikoyes <sanikoyes@163.com>
2016-06-27 19:47:40 +0800

    Add placeholder property for line_edit this useful for user input tip

--- scene/gui/line_edit.h
+++ scene/gui/line_edit.h
@@ -67,6 +67,7 @@ private:
 
 	String undo_text;
 	String text;
+	String placeholder;
 
 	PopupMenu *menu;
 
@@ -135,6 +136,8 @@ public:
 	void delete_text(int p_from_column, int p_to_column);
 	void set_text(String p_text);
 	String get_text() const;
+	void set_placeholder(String p_text);
+	String get_placeholder() const;
 	void set_cursor_pos(int p_pos);
 	int get_cursor_pos() const;
 	void set_max_length(int p_max_length);
