dffa1da01211b3270f7328cbab9fdffe53f68ac1
Juan Linietsky <reduzio@gmail.com>
2016-06-28 10:44:38 -0300

    Fix extends issue, closes #4026
    (cherry picked from commit 565bb3afcc207aea6608bb119e21c6833f1b87b3)

--- modules/gdscript/gd_compiler.cpp
+++ modules/gdscript/gd_compiler.cpp
@@ -1470,7 +1470,8 @@ Error GDCompiler::_parse_class(GDScript *p_script,GDScript *p_owner,const GDPars
 					String sub = p_class->extends_class[i];
 					if (script->subclasses.has(sub)) {
 
-						script=script->subclasses[sub];
+						Ref<Script> subclass = script->subclasses[sub]; //avoid reference from dissapearing
+						script=subclass;
 					} else {
 
 						_set_error("Could not find subclass: "+sub,p_class);
@@ -1646,6 +1647,7 @@ Error GDCompiler::_parse_class(GDScript *p_script,GDScript *p_owner,const GDPars
 		Error err = _parse_class(subclass.ptr(),p_script,p_class->subclasses[i]);
 		if (err)
 			return err;
+
 
 		p_script->constants.insert(name,subclass); //once parsed, goes to the list of constants
 		p_script->subclasses.insert(name,subclass);
