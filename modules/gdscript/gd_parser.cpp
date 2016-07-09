4bf31b3f3d00bd2a27d8414e8e5bd254b4d4a29b
George Marques <george@gmarqu.es>
2016-07-08 22:12:50 -0300

    Revert removing of function call in gd_parser
    The function call was removed in #5538 because of the unused return value,
    but the function itself has side effects and the absence of the call was
    causing crashes.

--- modules/gdscript/gd_parser.cpp
+++ modules/gdscript/gd_parser.cpp
@@ -514,6 +514,7 @@ GDParser::Node* GDParser::_parse_expression(Node *p_parent,bool p_static,bool p_
 		} else if (/*tokenizer->get_token()==GDTokenizer::TK_OP_ADD ||*/ tokenizer->get_token()==GDTokenizer::TK_OP_SUB || tokenizer->get_token()==GDTokenizer::TK_OP_NOT || tokenizer->get_token()==GDTokenizer::TK_OP_BIT_INVERT) {
 
 			//single prefix operators like !expr -expr ++expr --expr
+			alloc_node<OperatorNode>();
 			Expression e;
 			e.is_op=true;
 
