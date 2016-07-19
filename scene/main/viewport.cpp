be560a750c363355ad077331bf6bdbc62c6b2762
Jaroslaw Filiochowski <jarfil@gmail.com>
2016-07-19 06:27:12 +0200

    Hide tooltip if new mouse position's tooltip is empty

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -2009,11 +2009,17 @@ void Viewport::_gui_input_event(InputEvent p_event) {
 
 				bool is_tooltip_shown = false;
 
-				if (can_tooltip && gui.tooltip_popup) {
-					String tooltip = over->get_tooltip(gui.tooltip->get_global_transform().xform_inv(mpos));
+				if (gui.tooltip_popup) {
+					if (can_tooltip) {
+						String tooltip = over->get_tooltip(gui.tooltip->get_global_transform().xform_inv(mpos));
 
-					if (gui.tooltip_popup && tooltip == gui.tooltip_label->get_text())
-						is_tooltip_shown = true;
+						if (tooltip.length() == 0)
+							_gui_cancel_tooltip();
+						else if (tooltip == gui.tooltip_label->get_text())
+							is_tooltip_shown = true;
+					}
+					else
+						_gui_cancel_tooltip();
 				}
 
 				if (can_tooltip && !is_tooltip_shown) {
