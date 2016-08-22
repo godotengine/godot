2e5a4cb5ca33576ea81601f949b2e633ff92213c
volzhs <volzhs@gmail.com>
2016-08-23 08:14:04 +0900

    Show last added action on Input Map and implement InputEvent "=="

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -3128,7 +3128,7 @@ void Tree::ensure_cursor_is_visible() {
 	int screenh=get_size().height-h_scroll->get_combined_minimum_size().height;
 
 	if (ofs+h>v_scroll->get_val()+screenh)
-		v_scroll->set_val(ofs-screenh+h);
+		v_scroll->call_deferred("set_val", ofs-screenh+h);
 	else if (ofs < v_scroll->get_val())
 		v_scroll->set_val(ofs);
 }
