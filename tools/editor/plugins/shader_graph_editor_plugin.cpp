c44757c2b7344f9f30c445245fa490082b06c5a9
Pedro J. Estébanez <pedrojrulez@gmail.com>
2016-09-07 01:52:54 +0200

    Expose light shadow color to canvas item shaders
    (cherry picked from commit 09608876258ffa7d19be1fb887541f3798fa945e)

--- tools/editor/plugins/shader_graph_editor_plugin.cpp
+++ tools/editor/plugins/shader_graph_editor_plugin.cpp
@@ -2417,6 +2417,7 @@ void ShaderGraphView::_create_node(int p_id) {
 		colors.push_back("Color");
 		colors.push_back("LightColor");
 		colors.push_back("Light");
+		colors.push_back("ShadowColor");
 		colors.push_back("Diffuse");
 		colors.push_back("Specular");
 		colors.push_back("Emmision");
@@ -2429,6 +2430,7 @@ void ShaderGraphView::_create_node(int p_id) {
 		reals.push_back("ShadeParam");
 		reals.push_back("SpecularExp");
 		reals.push_back("LightAlpha");
+		reals.push_back("ShadowAlpha");
 		reals.push_back("PointSize");
 		reals.push_back("Discard");
 
