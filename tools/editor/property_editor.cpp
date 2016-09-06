0094c30938c870cdc0dd546be792c7f1951b5e08
Juan Linietsky <reduzio@gmail.com>
2016-09-06 19:38:31 -0300

    Do not expose resource/ properties in sectioned property editor, closes #6396

--- tools/editor/property_editor.cpp
+++ tools/editor/property_editor.cpp
@@ -4557,7 +4557,7 @@ void SectionedPropertyEditor::update_category_list() {
 		else if ( !(pi.usage&PROPERTY_USAGE_EDITOR) )
 			continue;
 
-		if (pi.name.find(":")!=-1 || pi.name=="script/script")
+		if (pi.name.find(":")!=-1 || pi.name=="script/script" || pi.name.begins_with("resource/"))
 			continue;
 		int sp = pi.name.find("/");
 		if (sp!=-1) {
