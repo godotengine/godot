ff3891f88e86000424f767d5cef817fd5fca1f76
Patrick Reh <musicmylove@gmx.net>
2016-09-16 18:00:51 +0200

    small improvement to y_sort: make clear which item has to be drawn first when two have the same y-coordinate
    (cherry picked from commit 4118b21e43c59e6abfe4f45ccf236ee529626f4e)

--- servers/visual/visual_server_raster.h
+++ servers/visual/visual_server_raster.h
@@ -407,7 +407,10 @@ class VisualServerRaster : public VisualServer {
 
 		_FORCE_INLINE_ bool operator()(const CanvasItem* p_left,const CanvasItem* p_right) const {
 
-			return p_left->xform.elements[2].y < p_right->xform.elements[2].y;
+			if(Math::abs(p_left->xform.elements[2].y - p_right->xform.elements[2].y) < CMP_EPSILON )
+				return p_left->xform.elements[2].x < p_right->xform.elements[2].x;
+			else
+				return p_left->xform.elements[2].y < p_right->xform.elements[2].y;
 		}
 	};
 
