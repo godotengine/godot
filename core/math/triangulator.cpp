654d69d4e42d2cf016a2ad53a98ae842dd20835d
D <dntk.daisei@gmail.com>
2016-10-09 16:34:40 +0900

    convert charset to UTF-8 with BOM.
    fix warning C4819

--- core/math/triangulator.cpp
+++ core/math/triangulator.cpp
@@ -1,4 +1,4 @@
-//Copyright (C) 2011 by Ivan Fratric
+﻿//Copyright (C) 2011 by Ivan Fratric
 //
 //Permission is hereby granted, free of charge, to any person obtaining a copy
 //of this software and associated documentation files (the "Software"), to deal
--- drivers/mpc/synth_filter.c
+++ drivers/mpc/synth_filter.c
@@ -1,4 +1,4 @@
-/*
+﻿/*
   Copyright (c) 2005-2009, The Musepack Development Team
   All rights reserved.
 
