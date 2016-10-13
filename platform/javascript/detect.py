c8a97c36781d633b20e52579993aaadb9122a94b
Rémi Verschelde <rverschelde@gmail.com>
2016-10-13 20:46:42 +0200

    mpc: Move to a module and split thirdparty libmpcdec
    (cherry picked from commit 5c12c9e69b85023934dc85f3aada03da150556be)

--- platform/javascript/detect.py
+++ platform/javascript/detect.py
@@ -25,10 +25,10 @@ def get_flags():
 
 	return [
 		('tools', 'no'),
-		('musepack', 'no'),
 		('squish', 'no'),
 		('module_speex_enabled', 'no'),
 		('etc1', 'no'),
+		('module_mpc_enabled', 'no'),
 		('module_theora_enabled', 'no'),
 	]
 
