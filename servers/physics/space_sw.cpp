7da5cfcb351c5b88efc99d73216ec7a8286322e2
Ben Hickling <ben.hickling@aria-networks.com>
2016-12-06 16:30:53 +0000

    The 3D physics engine was only checking against layer masks so I've added a check against the collision mask as well

--- servers/physics/space_sw.cpp
+++ servers/physics/space_sw.cpp
@@ -37,7 +37,7 @@ _FORCE_INLINE_ static bool _match_object_type_query(CollisionObjectSW *p_object,
 	if (p_object->get_type()==CollisionObjectSW::TYPE_AREA)
 		return p_type_mask&PhysicsDirectSpaceState::TYPE_MASK_AREA;
 
-	if ((p_object->get_layer_mask()&p_layer_mask)==0)
+	if (((p_object->get_layer_mask()&p_layer_mask)==0) && ((p_object->get_collision_mask()&p_layer_mask)==0))
 		return false;
 
 	BodySW *body = static_cast<BodySW*>(p_object);
