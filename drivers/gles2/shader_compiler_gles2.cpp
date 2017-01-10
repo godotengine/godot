8589a1b117458fbf834015044bf01ff326897cc6
Pedro J. Estébanez <pedrojrulez@gmail.com>
2017-01-10 17:05:09 +0100

    Fix uniforms with the same name shadowing others Follow-up of #7344

--- drivers/gles2/shader_compiler_gles2.cpp
+++ drivers/gles2/shader_compiler_gles2.cpp
@@ -571,15 +571,12 @@ Error ShaderCompilerGLES2::compile_node(SL::ProgramNode *p_program) {
 
 		global_code+=uline;
 		if (uniforms) {
-			//if (uniforms->has(E->key())) {
-			//	//repeated uniform, error
-		//		ERR_EXPLAIN("Uniform already exists from other shader: "+String(E->key()));
-		//		ERR_FAIL_COND_V(uniforms->has(E->key()),ERR_ALREADY_EXISTS);
-//
-//			}
-			SL::Uniform u = E->get();
-			u.order+=ubase;
-			uniforms->insert(E->key(),u);
+			// Check to avoid uniforms with the same name being re-added to avoid overwriting entries
+			if (!uniforms->has(E->key())) {
+				SL::Uniform u = E->get();
+				u.order+=ubase;
+				uniforms->insert(E->key(),u);
+			}
 		}
 	}
 
