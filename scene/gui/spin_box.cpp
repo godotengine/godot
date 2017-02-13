674a090e5917eab5b590563fb156ff90ea53fe31
Andreas Haas <liu.gam3@gmail.com>
2017-02-14 00:54:59 +0100

    Spinbox: don't ignore double clicks.
    Fixes the problem with spinboxes not updating when clicking too fast.
    
    (cherry picked from commit dd4c2709e4a4f1abef558c5b3a4e4454db06410d)

--- scene/gui/spin_box.cpp
+++ scene/gui/spin_box.cpp
@@ -98,8 +98,6 @@ void SpinBox::_input_event(const InputEvent& p_event) {
 	if (p_event.type==InputEvent::MOUSE_BUTTON && p_event.mouse_button.pressed) {
 		const InputEventMouseButton &mb=p_event.mouse_button;
 
-		if (mb.doubleclick)
-			return; //ignore doubleclick
 
 		bool up = mb.y < (get_size().height/2);
 
