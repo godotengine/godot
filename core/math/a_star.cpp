4b0a1637ace1dbf693672e568fd68d9e77be5fe6
Ferenc Arn <tagcup@yahoo.com>
2017-03-02 16:00:06 -0600

    Improved A-star (adapted from #7823)

--- core/math/a_star.cpp
+++ core/math/a_star.cpp
@@ -30,6 +30,17 @@
 #include "geometry.h"
 
 
+
+void AStar::set_delegate(Object *p_to_object) {
+
+	delegate = p_to_object;
+}
+
+Object *AStar::get_delegate() const {
+
+	return delegate;
+}
+
 int AStar::get_available_point_id() const {
 
 	if (points.empty()) {
@@ -39,7 +50,8 @@ int AStar::get_available_point_id() const {
 	return points.back()->key()+1;
 }
 
-void AStar::add_point(int p_id, const Vector3 &p_pos, real_t p_weight_scale) {
+void AStar::add_point(int p_id, const Vector3 &p_pos, real_t p_weight_scale,const Vector<Variant>& p_binds,uint32_t p_flags) {
+
 	ERR_FAIL_COND(p_id<0);
 	if (!points.has(p_id)) {
 		Point *pt = memnew( Point );
@@ -48,10 +60,14 @@ void AStar::add_point(int p_id, const Vector3 &p_pos, real_t p_weight_scale) {
 		pt->weight_scale=p_weight_scale;
 		pt->prev_point=NULL;
 		pt->last_pass=0;
+		pt->binds = p_binds;
+		pt->flags = p_flags;
 		points[p_id]=pt;
 	} else {
 		points[p_id]->pos=p_pos;
 		points[p_id]->weight_scale=p_weight_scale;
+		points[p_id]->binds = p_binds;
+		points[p_id]->flags = p_flags;
 	}
 }
 
@@ -62,6 +78,13 @@ Vector3 AStar::get_point_pos(int p_id) const{
 	return points[p_id]->pos;
 
 }
+
+void AStar::set_point_pos(int p_id, const Vector3 &p_pos){
+
+	points[p_id]->pos = p_pos;
+
+}
+
 real_t AStar::get_point_weight_scale(int p_id) const{
 
 	ERR_FAIL_COND_V(!points.has(p_id),0);
@@ -69,6 +92,40 @@ real_t AStar::get_point_weight_scale(int p_id) const{
 	return points[p_id]->weight_scale;
 
 }
+
+void AStar::set_point_weight_scale(int p_id, real_t p_weight_scale) {
+
+	points[p_id]->weight_scale=p_weight_scale;
+
+}
+
+Vector<Variant> AStar::get_point_binds(int p_id) const{
+
+	ERR_FAIL_COND_V(!points.has(p_id),Vector<Variant>());
+
+	return points[p_id]->binds;
+
+}
+
+void AStar::set_point_binds(int p_id,const Vector<Variant>& p_binds) {
+
+	points[p_id]->binds=p_binds;
+
+}
+
+uint32_t AStar::get_point_flags(int p_id) const {
+
+	ERR_FAIL_COND_V(!points.has(p_id),0);
+
+	return points[p_id]->flags;
+}
+
+void AStar::set_point_flags(int p_id, uint32_t p_flags) {
+
+	points[p_id]->flags = p_flags;
+}
+
+
 void AStar::remove_point(int p_id){
 
 	ERR_FAIL_COND(!points.has(p_id));
@@ -86,7 +143,7 @@ void AStar::remove_point(int p_id){
 	points.erase(p_id);
 }
 
-void AStar::connect_points(int p_id,int p_with_id){
+void AStar::connect_points(int p_id,int p_with_id,bool bidirectional){
 
 	ERR_FAIL_COND(!points.has(p_id));
 	ERR_FAIL_COND(!points.has(p_with_id));
@@ -96,7 +153,8 @@ void AStar::connect_points(int p_id,int p_with_id){
 	Point* a = points[p_id];
 	Point* b = points[p_with_id];
 	a->neighbours.push_back(b);
-	b->neighbours.push_back(a);
+	if (bidirectional)
+		b->neighbours.push_back(a);
 
 	Segment s(p_id,p_with_id);
 	if (s.from==p_id) {
@@ -187,7 +245,7 @@ Vector3 AStar::get_closest_pos_in_segment(const Vector3& p_point) const {
 	return closest_point;
 }
 
-bool AStar::_solve(Point* begin_point, Point* end_point) {
+bool AStar::_solve(Point* begin_point, Point* end_point, bool exhaustive) {
 
 	pass++;
 
@@ -195,46 +253,53 @@ bool AStar::_solve(Point* begin_point, Point* end_point) {
 
 	bool found_route=false;
 
-
 	for(int i=0;i<begin_point->neighbours.size();i++) {
 
 		Point *n = begin_point->neighbours[i];
 		n->prev_point=begin_point;
-		n->distance=n->pos.distance_to(begin_point->pos);
-		n->distance*=n->weight_scale;
+		n->distance = _compute_cost(n, begin_point, "compute_cost");
+		n->distance *= n->weight_scale;
 		n->last_pass=pass;
 		open_list.add(&n->list);
 
 		if (end_point==n) {
 			found_route=true;
-			break;
+			if (!exhaustive)
+				break;
 		}
 	}
 
-	while(!found_route) {
+	while(!found_route || exhaustive) {
 
 		if (open_list.first()==NULL) {
-			//could not find path sadly
+			//found_route indicates if a route has been found
 			break;
 		}
 		//check open list
 
 		SelfList<Point> *least_cost_point=NULL;
-		real_t least_cost=1e30;
 
-		//this could be faster (cache previous results)
-		for (SelfList<Point> *E=open_list.first();E;E=E->next()) {
+		if (exhaustive) {
+			// exhaustive search doesnt care about looking down the least cost path first
+			// as it will search all paths for the best route
+			least_cost_point = open_list.first();
+		}
+		else {
+			//this could be faster (cache previous results)
+			real_t least_cost = 1e30;
+			for (SelfList<Point> *E=open_list.first();E;E=E->next()) {
 
-			Point *p=E->self();
+				Point *p=E->self();
 
-			real_t cost=p->distance;
-			cost+=p->pos.distance_to(end_point->pos);
-			cost*=p->weight_scale;
+				real_t cost = p->distance;
+				cost += _compute_cost(p, end_point, "estimate_cost"); // estimate as code might need to know the difference
+				cost *= p->weight_scale;
 
-			if (cost<least_cost) {
+				if (cost<least_cost) {
 
-				least_cost_point=E;
-				least_cost=cost;
+					least_cost_point=E;
+					least_cost=cost;
+				}
 			}
 		}
 
@@ -249,8 +314,8 @@ bool AStar::_solve(Point* begin_point, Point* end_point) {
 			Point* e=p->neighbours[i];
 
 
-			real_t distance = p->pos.distance_to(e->pos) + p->distance;
-			distance*=e->weight_scale;
+			real_t distance = _compute_cost(p, e, "compute_cost") + p->distance;
+			distance *= e->weight_scale;
 
 
 
@@ -268,20 +333,19 @@ bool AStar::_solve(Point* begin_point, Point* end_point) {
 				e->prev_point=p;
 				e->distance=distance;
 				e->last_pass=pass; //mark as used
-				open_list.add(&e->list);
 
 				if (e==end_point) {
-					//oh my reached end! stop algorithm
+					//oh my reached end! stop algorithm if not exhaustive
 					found_route=true;
-					break;
-
+					if (!exhaustive)
+						break;
 				}
-
+				else {
+					// dont put end point into the open list
+					open_list.add(&e->list);
+				}
 			}
 		}
-
-		if (found_route)
-			break;
 
 		open_list.remove(least_cost_point);
 	}
@@ -295,11 +359,41 @@ bool AStar::_solve(Point* begin_point, Point* end_point) {
 
 }
 
-PoolVector<Vector3> AStar::get_point_path(int p_from_id, int p_to_id) {
+real_t AStar::_compute_cost(Point *from, Point *to, const StringName& p_method) {
+	if (delegate && delegate->has_method(p_method)) {
+
+		Dictionary f;
+		f["id"]=from->id;
+		f["pos"]=from->pos;
+		f["weight_scale"]=from->weight_scale;
+		f["binds"]=from->binds;
+		f["flags"]=from->flags;
+		Variant fv(f);
+
+		Dictionary t;
+		t["id"]=to->id;
+		t["pos"]=to->pos;
+		t["weight_scale"]=to->weight_scale;
+		t["binds"]=to->binds;
+		t["flags"]=to->flags;
+		Variant tv(t);
+
+		Variant self(this);
+
+		const Variant* args[] = { &self, &fv, &tv };
+		Variant::CallError ce;
+		real_t cost=delegate->call(p_method, args, 3, ce);
+
+		return cost;
+	}
+
+	return from->pos.distance_to(to->pos);
+}
+
+PoolVector<Vector3> AStar::get_point_path(int p_from_id, int p_to_id, bool exhaustive) {
 
 	ERR_FAIL_COND_V(!points.has(p_from_id),PoolVector<Vector3>());
 	ERR_FAIL_COND_V(!points.has(p_to_id),PoolVector<Vector3>());
-
 
 	pass++;
 
@@ -316,7 +410,7 @@ PoolVector<Vector3> AStar::get_point_path(int p_from_id, int p_to_id) {
 	Point *begin_point=a;
 	Point *end_point=b;
 
-	bool found_route=_solve(begin_point,end_point);
+	bool found_route=_solve(begin_point,end_point, exhaustive);
 
 	if (!found_route)
 		return PoolVector<Vector3>();
@@ -325,6 +419,11 @@ PoolVector<Vector3> AStar::get_point_path(int p_from_id, int p_to_id) {
 	Point *p=end_point;
 	int pc=1; //begin point
 	while(p!=begin_point) {
+		if (pc > points.size()) {
+			ERR_PRINT("Infinite loop detected in AStar");
+			return PoolVector<Vector3>();
+		}
+
 		pc++;
 		p=p->prev_point;
 	}
@@ -351,11 +450,10 @@ PoolVector<Vector3> AStar::get_point_path(int p_from_id, int p_to_id) {
 }
 
 
-PoolVector<int> AStar::get_id_path(int p_from_id, int p_to_id) {
+PoolVector<int> AStar::get_id_path(int p_from_id, int p_to_id, bool exhaustive) {
 
 	ERR_FAIL_COND_V(!points.has(p_from_id),PoolVector<int>());
 	ERR_FAIL_COND_V(!points.has(p_to_id),PoolVector<int>());
-
 
 	pass++;
 
@@ -372,7 +470,7 @@ PoolVector<int> AStar::get_id_path(int p_from_id, int p_to_id) {
 	Point *begin_point=a;
 	Point *end_point=b;
 
-	bool found_route=_solve(begin_point,end_point);
+	bool found_route=_solve(begin_point,end_point, exhaustive);
 
 	if (!found_route)
 		return PoolVector<int>();
@@ -381,6 +479,11 @@ PoolVector<int> AStar::get_id_path(int p_from_id, int p_to_id) {
 	Point *p=end_point;
 	int pc=1; //begin point
 	while(p!=begin_point) {
+		if (pc > points.size()) {
+			ERR_PRINT("Infinite loop detected in AStar");
+			return PoolVector<int>();
+		}
+
 		pc++;
 		p=p->prev_point;
 	}
@@ -407,13 +510,22 @@ PoolVector<int> AStar::get_id_path(int p_from_id, int p_to_id) {
 
 void AStar::_bind_methods() {
 
+	ClassDB::bind_method(D_METHOD("set_delegate"),&AStar::set_delegate);
+	ClassDB::bind_method(D_METHOD("get_delegate"),&AStar::get_delegate);
+
 	ClassDB::bind_method(D_METHOD("get_available_point_id"),&AStar::get_available_point_id);
-	ClassDB::bind_method(D_METHOD("add_point","id","pos","weight_scale"),&AStar::add_point,DEFVAL(1.0));
+	ClassDB::bind_method(D_METHOD("add_point","id","pos","weight_scale", "binds", "flags"),&AStar::add_point,DEFVAL(1.0), DEFVAL(Vector<Variant>()), DEFVAL(0));
 	ClassDB::bind_method(D_METHOD("get_point_pos","id"),&AStar::get_point_pos);
+	ClassDB::bind_method(D_METHOD("set_point_pos","id","pos"),&AStar::set_point_pos);
 	ClassDB::bind_method(D_METHOD("get_point_weight_scale","id"),&AStar::get_point_weight_scale);
+	ClassDB::bind_method(D_METHOD("set_point_weight_scale","id","weight_scale"),&AStar::set_point_weight_scale);
+	ClassDB::bind_method(D_METHOD("set_point_binds","id","binds"),&AStar::set_point_binds);
+	ClassDB::bind_method(D_METHOD("get_point_binds","id"),&AStar::get_point_binds);
+	ClassDB::bind_method(D_METHOD("set_point_flags","id","flags"),&AStar::set_point_flags);
+	ClassDB::bind_method(D_METHOD("get_point_flags","id"),&AStar::get_point_flags);
 	ClassDB::bind_method(D_METHOD("remove_point","id"),&AStar::remove_point);
 
-	ClassDB::bind_method(D_METHOD("connect_points","id","to_id"),&AStar::connect_points);
+	ClassDB::bind_method(D_METHOD("connect_points","id","to_id"),&AStar::connect_points,DEFVAL(true));
 	ClassDB::bind_method(D_METHOD("disconnect_points","id","to_id"),&AStar::disconnect_points);
 	ClassDB::bind_method(D_METHOD("are_points_connected","id","to_id"),&AStar::are_points_connected);
 
@@ -422,8 +534,8 @@ void AStar::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_closest_point","to_pos"),&AStar::get_closest_point);
 	ClassDB::bind_method(D_METHOD("get_closest_pos_in_segment","to_pos"),&AStar::get_closest_pos_in_segment);
 
-	ClassDB::bind_method(D_METHOD("get_point_path","from_id","to_id"),&AStar::get_point_path);
-	ClassDB::bind_method(D_METHOD("get_id_path","from_id","to_id"),&AStar::get_id_path);
+	ClassDB::bind_method(D_METHOD("get_point_path","from_id","to_id", "exhaustive"),&AStar::get_point_path,DEFVAL(false));
+	ClassDB::bind_method(D_METHOD("get_id_path","from_id","to_id", "exhaustive"),&AStar::get_id_path,DEFVAL(false));
 
 }
 
@@ -431,6 +543,7 @@ void AStar::_bind_methods() {
 AStar::AStar() {
 
 	pass=1;
+	delegate = NULL;
 }
 
 
