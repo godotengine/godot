826f8942ef1d83dfe37b1cfd66d35db2da2b4760
allkhor <say2word@gmail.com>
2017-03-02 23:34:17 +0600

    Revert "Merge pull request #7873 from volzhs/issue-7820"
    This reverts commit f67881bada003e4cd6341809d101642e2886c1e6, reversing
    changes made to 6490e9ae5b73903fe91f4ec3c2043683a079bd74.

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -833,7 +833,6 @@ void Tree::update_cache() {
 	cache.title_button_color = get_color("title_button_color");
 
 	v_scroll->set_custom_step(cache.font->get_height());
-	cache.click_item=get_selected();
 
 }
 
