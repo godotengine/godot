bd26eacc59cdcd1cb408c736292e0c61b8226010
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2017-03-08 17:14:01 +0100

    Allow non blocking UDP put_packet in C++.
    - Add blocking mode option to PacketPeerUDP.
    - put_packet returns ERR_UNAVAILABLE when operation would block.
    - ENet module uses non-blocking UDP.
    
    (cherry picked from commit 5f681d0b0f28cd39bc033c0cdf8eb3cb3a4acbe6)

--- drivers/unix/packet_peer_udp_posix.h
+++ drivers/unix/packet_peer_udp_posix.h
@@ -48,6 +48,7 @@ class PacketPeerUDPPosix : public PacketPeerUDP {
 	mutable int packet_port;
 	mutable int queue_count;
 	int sockfd;
+	bool sock_blocking;
 	IP::Type sock_type;
 
 	IP_Address peer_addr;
@@ -56,6 +57,7 @@ class PacketPeerUDPPosix : public PacketPeerUDP {
 	_FORCE_INLINE_ int _get_socket();
 
 	static PacketPeerUDP *_create();
+	void _set_sock_blocking(bool p_blocking);
 	virtual Error _poll(bool p_block);
 
 public:
