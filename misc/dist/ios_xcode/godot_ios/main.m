1a0d1bb18dc392ffdeef43451b8190f79b02d424
Rémi Verschelde <rverschelde@gmail.com>
2017-03-26 22:03:58 +0200

    Remove redundant subfolder in iOS template

--- misc/dist/ios_xcode/godot_ios/main.m
+++ misc/dist/ios_xcode/godot_ios/main.m
@@ -31,9 +31,8 @@
 
 #import "AppDelegate.h"
 
-int main(int argc, char * argv[])
-{
-    @autoreleasepool {
-        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
-    }
+int main(int argc, char *argv[]) {
+	@autoreleasepool {
+		return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+	}
 }
