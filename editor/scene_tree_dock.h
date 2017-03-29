1307ff7eb16ecef8683ad8ebda8bfb0338e5204c
William Tumeo <tumeowilliam@gmail.com>
2017-03-29 17:23:49 -0300

    Re-add clear script button and context menu from ce5200b30e6d262905912c6571d51ba6f5979bd7

--- editor/scene_tree_dock.h
+++ editor/scene_tree_dock.h
@@ -60,6 +60,7 @@ class SceneTreeDock : public VBoxContainer {
 		TOOL_GROUP,
 		TOOL_CREATE_SCRIPT,
 		TOOL_LOAD_SCRIPT,
+		TOOL_CLEAR_SCRIPT,
 		TOOL_MOVE_UP,
 		TOOL_MOVE_DOWN,
 		TOOL_DUPLICATE,
@@ -80,6 +81,7 @@ class SceneTreeDock : public VBoxContainer {
 	ToolButton *button_instance;
 	ToolButton *button_create_script;
 	ToolButton *button_load_script;
+	ToolButton *button_clear_script;
 
 	SceneTreeEditor *scene_tree;
 
