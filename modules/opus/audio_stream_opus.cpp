df61dc4b2bd54a5a40c515493c76f5a458e5b541
Rémi Verschelde <rverschelde@gmail.com>
2017-04-08 00:11:42 +0200

    Add "Godot Engine contributors" copyright line

--- modules/opus/audio_stream_opus.cpp
+++ modules/opus/audio_stream_opus.cpp
@@ -6,6 +6,7 @@
 /*                    http://www.godotengine.org                         */
 /*************************************************************************/
 /* Copyright (c) 2007-2017 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2017 Godot Engine contributors (cf. AUTHORS.md)    */
 /*                                                                       */
 /* Author: George Marques <george@gmarqu.es>                             */
 /*                                                                       */
