18f2b961868021ba6afcc5c88ac2c26b018f57c3
Andreas Haas <liu.gam3@gmail.com>
2017-04-09 10:32:38 +0200

    Fix warning message when EditorPlugin script is not in tool mode.
    (cherry picked from commit 0029440955f6d500a0c97bf7348a7b5353db0cea)

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -2985,7 +2985,7 @@ void EditorNode::set_addon_plugin_enabled(const String &p_addon, bool p_enabled)
 	}
 
 	if (!script->is_tool()) {
-		show_warning("Unable to load addon script from path: '" + path + "' Script is does not support tool mode.");
+		show_warning("Unable to load addon script from path: '" + path + "' Script is not in tool mode.");
 		return;
 	}
 
