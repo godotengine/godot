a2ad2eedbb0517faab7d672d41cafe85b3bdab39
volzhs <volzhs@gmail.com>
2017-05-22 20:25:19 +0900

    Fit grid with label and component of editor

--- scene/gui/box_container.cpp
+++ scene/gui/box_container.cpp
@@ -308,6 +308,7 @@ MarginContainer *VBoxContainer::add_margin_child(const String &p_label, Control 
 	l->set_text(p_label);
 	add_child(l);
 	MarginContainer *mc = memnew(MarginContainer);
+	mc->add_constant_override("margin_left", 0);
 	mc->add_child(p_control);
 	add_child(mc);
 	if (p_expand)
