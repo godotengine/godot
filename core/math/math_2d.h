e1f4abe062d983c0c4af0acce41770cb40adcb1f
toger5 <toger5@hotmail.de>
2017-05-29 04:36:14 -0700

    added grow functions to Rect2  - grow_individual  - grow_margin
    (cherry picked from commit 66b308925c3174901fc59a23245c8a1d19143420)

--- core/math/math_2d.h
+++ core/math/math_2d.h
@@ -327,6 +327,25 @@ struct Rect2 {
 		g.size.height += p_by * 2;
 		return g;
 	}
+	inline Rect2 grow_margin(Margin p_margin, real_t p_amount) const {
+		Rect2 g = *this;
+		g.grow_individual((MARGIN_LEFT == p_margin) ? p_amount : 0,
+				(MARGIN_TOP == p_margin) ? p_amount : 0,
+				(MARGIN_RIGHT == p_margin) ? p_amount : 0,
+				(MARGIN_BOTTOM == p_margin) ? p_amount : 0);
+		return g;
+	}
+
+	inline Rect2 grow_individual(real_t p_left, real_t p_top, real_t p_right, real_t p_bottom) const {
+
+		Rect2 g = *this;
+		g.pos.x -= p_left;
+		g.pos.y -= p_top;
+		g.size.width += p_left + p_right;
+		g.size.height += p_top + p_bottom;
+
+		return g;
+	}
 
 	inline Rect2 expand(const Vector2 &p_vector) const {
 
