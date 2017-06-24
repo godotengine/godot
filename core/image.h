e9e39dd6dc988960c5ccf57e7b28ccb4fdaf43e5
dumitru-stama <dumitru.stama@gmail.com>
2017-06-24 08:16:35 -0700

    Added 'blit_rect_mask' method

--- core/image.h
+++ core/image.h
@@ -351,6 +351,7 @@ public:
 	void normalmap_to_xy();
 
 	void blit_rect(const Image &p_src, const Rect2 &p_src_rect, const Point2 &p_dest);
+	void blit_rect_mask(const Image &p_src, const Image &p_mask, const Rect2 &p_src_rect, const Point2 &p_dest);
 	void blend_rect(const Image &p_src, const Rect2 &p_src_rect, const Point2 &p_dest);
 	void blend_rect_mask(const Image &p_src, const Image &p_mask, const Rect2 &p_src_rect, const Point2 &p_dest);
 
