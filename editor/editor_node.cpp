003a49eb66a3af2a677587b37206828aad5bcfbc
Poommetee Ketson <poommetee@protonmail.com>
2017-07-01 23:23:16 +0700

    SceneTab: MMB or double LMB to create new scene

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -4392,8 +4392,14 @@ void EditorNode::_scene_tab_input(const Ref<InputEvent> &p_input) {
 	Ref<InputEventMouseButton> mb = p_input;
 
 	if (mb.is_valid()) {
-		if (mb->get_button_index() == BUTTON_MIDDLE && mb->is_pressed() && scene_tabs->get_hovered_tab() >= 0) {
-			_scene_tab_closed(scene_tabs->get_hovered_tab());
+		if (scene_tabs->get_hovered_tab() >= 0) {
+			if (mb->get_button_index() == BUTTON_MIDDLE && mb->is_pressed()) {
+				_scene_tab_closed(scene_tabs->get_hovered_tab());
+			}
+		} else {
+			if ((mb->get_button_index() == BUTTON_LEFT && mb->is_doubleclick()) || (mb->get_button_index() == BUTTON_MIDDLE && mb->is_pressed())) {
+				_menu_option_confirm(FILE_NEW_SCENE, true);
+			}
 		}
 	}
 }
