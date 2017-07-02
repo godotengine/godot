7ca40c3eb91e0c0388016983599e4334447c6c97
volzhs <volzhs@gmail.com>
2017-07-03 00:02:36 +0900

    Fix visibility changes for Spatial

--- scene/3d/spatial.cpp
+++ scene/3d/spatial.cpp
@@ -547,10 +547,7 @@ void Spatial::show() {
 	if (!is_inside_tree())
 		return;
 
-	if (!data.parent || is_visible()) {
-
-		_propagate_visibility_changed();
-	}
+	_propagate_visibility_changed();
 }
 
 void Spatial::hide() {
@@ -558,14 +555,14 @@ void Spatial::hide() {
 	if (!data.visible)
 		return;
 
-	bool was_visible = is_visible();
 	data.visible = false;
 
-	if (!data.parent || was_visible) {
+	if (!is_inside_tree())
+		return;
 
-		_propagate_visibility_changed();
-	}
+	_propagate_visibility_changed();
 }
+
 bool Spatial::is_visible() const {
 
 	const Spatial *s = this;
