37eaa9d7923e69d7aa31926b868a31b3b6157e46
Rémi Verschelde <rverschelde@gmail.com>
2017-07-02 13:16:39 +0200

    EditorNameDialog: Fix margins
    Somehow the previous code is what works in *master*, though it looks buggy.
    For 2.1, the margins should be set on the container itself.

--- editor/editor_name_dialog.cpp
+++ editor/editor_name_dialog.cpp
@@ -80,11 +80,12 @@ void EditorNameDialog::_bind_methods() {
 
 EditorNameDialog::EditorNameDialog() {
 	makevb = memnew(VBoxContainer);
+	makevb->set_margin(MARGIN_TOP, 5);
+	makevb->set_anchor_and_margin(MARGIN_LEFT, ANCHOR_BEGIN, 5);
+	makevb->set_anchor_and_margin(MARGIN_RIGHT, ANCHOR_END, 5);
 	add_child(makevb);
+
 	name = memnew(LineEdit);
 	makevb->add_child(name);
-	name->set_margin(MARGIN_TOP, 5);
-	name->set_anchor_and_margin(MARGIN_LEFT, ANCHOR_BEGIN, 5);
-	name->set_anchor_and_margin(MARGIN_RIGHT, ANCHOR_END, 5);
 	name->connect("input_event", this, "_line_input_event");
 }
