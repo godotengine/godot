6a5d4125258f17e5164feb82cd73087f2698d3a8
redtoorange <redtoorange@gmail.com>
2017-07-17 07:03:31 -0400

    Revert "Revert "Modules""
    This reverts commit 6142de5a8db7fa750318aedd7be5dff630b0416d.

---
+++ modules/priority_queue/register_types.cpp
@@ -0,0 +1,13 @@
+﻿/* register_types.cpp */
+
+#include "register_types.h"
+#include "class_db.h"
+#include "priority_queue.h"
+
+void register_priority_queue_types() {
+	ClassDB::register_class<PriorityQueue>();
+}
+
+void unregister_priority_queue_types() {
+	//nothing to do here
+}
---
+++ modules/test_node/register_types.h
@@ -0,0 +1,5 @@
+﻿/* register_types.h */
+
+void register_test_node_types();
+void unregister_test_node_types();
+/* yes, the word in the middle must be the same as the module folder name */
