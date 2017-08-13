V-Sekai-fire/multiplayer-fabric-godot

TileMap: Fix infinite loop when trying to bucket-delete empty tiles.
Right-clicking when the bucket tool is enabled means "erase all instances of this tile".
So it doesn't make sense to try doing this for empty tiles :P

Fixes #10065

editor/plugins/tile_map_editor_plugin.cpp
@@ -330,6 +330,8 @@ PoolVector<Vector2> TileMapEditor::_bucket_fill(const Point2i &p_start, bool era
 
 		if (id == TileMap::INVALID_CELL)
 			return PoolVector<Vector2>();
+	} else if (prev_id == TileMap::INVALID_CELL) {
+		return PoolVector<Vector2>();
 	}
 
 	Rect2i r = node->get_item_rect();

godotengine/godot

(cherry picked from commit 95da12212beb6ab947de5ebbb0d12ffba3d2006c)

editor/plugins/tile_map_editor_plugin.cpp
@@ -330,6 +330,8 @@ DVector<Vector2> TileMapEditor::_bucket_fill(const Point2i &p_start, bool erase,
 
 		if (id == TileMap::INVALID_CELL)
 			return DVector<Vector2>();
+	} else if (prev_id == TileMap::INVALID_CELL) {
+		return DVector<Vector2>();
 	}
 
 	Rect2i r = node->get_item_rect();