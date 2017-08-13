V-Sekai-fire/multiplayer-fabric-godot

keep default exported script values unless overriden, closes #8127

scene/resources/texture.cpp
@@ -194,6 +194,7 @@ void ImageTexture::create(int p_width, int p_height, Image::Format p_format, uin
 }
 void ImageTexture::create_from_image(const Ref<Image> &p_image, uint32_t p_flags) {
 
+	ERR_FAIL_COND(p_image.is_null());
 	flags = p_flags;
 	w = p_image->get_width();
 	h = p_image->get_height();

godotengine/godot

(cherry picked from commit 475e8b28b28962a24b783597f9a3cabf4a370dee)

scene/resources/texture.cpp
@@ -188,6 +188,7 @@ void ImageTexture::create(int p_width, int p_height, Image::Format p_format, uin
 }
 void ImageTexture::create_from_image(const Image &p_image, uint32_t p_flags) {
 
+	ERR_FAIL_COND(p_image.empty());
 	flags = p_flags;
 	w = p_image.get_width();
 	h = p_image.get_height();