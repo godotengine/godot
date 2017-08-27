V-Sekai-fire/multiplayer-fabric-godot

Exposed create_from_mesh in Navmesh

scene/3d/navigation_mesh.cpp
@@ -208,6 +208,8 @@ void NavigationMesh::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_polygon", "idx"), &NavigationMesh::get_polygon);
 	ClassDB::bind_method(D_METHOD("clear_polygons"), &NavigationMesh::clear_polygons);
 
+	ClassDB::bind_method(D_METHOD("create_from_mesh", "mesh"), &NavigationMesh::create_from_mesh);
+
 	ClassDB::bind_method(D_METHOD("_set_polygons", "polygons"), &NavigationMesh::_set_polygons);
 	ClassDB::bind_method(D_METHOD("_get_polygons"), &NavigationMesh::_get_polygons);
 

godotengine/godot

(cherry picked from commit deaf095dcfe405d142b876b359df512e9e1ba2cc)

scene/3d/navigation_mesh.cpp
@@ -208,6 +208,8 @@ void NavigationMesh::_bind_methods() {
 	ObjectTypeDB::bind_method(_MD("get_polygon", "idx"), &NavigationMesh::get_polygon);
 	ObjectTypeDB::bind_method(_MD("clear_polygons"), &NavigationMesh::clear_polygons);
 
+	ObjectTypeDB::bind_method(_MD("create_from_mesh", "mesh"), &NavigationMesh::create_from_mesh);
+
 	ObjectTypeDB::bind_method(_MD("_set_polygons", "polygons"), &NavigationMesh::_set_polygons);
 	ObjectTypeDB::bind_method(_MD("_get_polygons"), &NavigationMesh::_get_polygons);
 