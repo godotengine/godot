bd282ff43f23fe845f29a3e25c8efc01bd65ffb0
Rémi Verschelde <rverschelde@gmail.com>
2017-08-27 14:16:55 +0200

    Use HTTPS URL for Godot's website in the headers

--- servers/audio/effects/audio_effect_stereo_enhance.h
+++ servers/audio/effects/audio_effect_stereo_enhance.h
@@ -3,7 +3,7 @@
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
-/*                    http://www.godotengine.org                         */
+/*                      https://godotengine.org                          */
 /*************************************************************************/
 /* Copyright (c) 2007-2017 Juan Linietsky, Ariel Manzur.                 */
 /* Copyright (c) 2014-2017 Godot Engine contributors (cf. AUTHORS.md)    */
--- servers/visual/visual_server_wrap_mt.cpp
+++ servers/visual/visual_server_wrap_mt.cpp
@@ -3,7 +3,7 @@
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
-/*                    http://www.godotengine.org                         */
+/*                      https://godotengine.org                          */
 /*************************************************************************/
 /* Copyright (c) 2007-2017 Juan Linietsky, Ariel Manzur.                 */
 /*                                                                       */
