0ecdd9653fbee296666ab80b9cdb11149505ebe1
Jakub Grzesik <kubecz3k@gmail.com>
2017-09-02 16:17:59 +0200

    Improve 'quick open' search time

--- editor/quick_open.h
+++ editor/quick_open.h
@@ -49,6 +49,7 @@ class EditorQuickOpen : public ConfirmationDialog {
 
 	void _sbox_input(const Ref<InputEvent> &p_ie);
 	void _parse_fs(EditorFileSystemDirectory *efsd, Vector<Pair<String, Ref<Texture> > > &list);
+	Vector<Pair<String, Ref<Texture> > > _sort_fs(Vector<Pair<String, Ref<Texture> > > &list);
 	float _path_cmp(String search, String path) const;
 
 	void _confirmed();
