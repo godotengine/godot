db3776e1fdb6ee3789eb556db5d60d496ab30d96
Nathan Warden <nathan@nathanwarden.com>
2017-09-09 22:00:57 -0500

    Changed the text from "Cancel" to "Done" in the "Fix Dependencies" dialog.

--- editor/dependency_editor.cpp
+++ editor/dependency_editor.cpp
@@ -489,6 +489,7 @@ DependencyErrorDialog::DependencyErrorDialog() {
 	vb->add_margin_child(TTR("Scene failed to load due to missing dependencies:"), files, true);
 	files->set_v_size_flags(SIZE_EXPAND_FILL);
 	get_ok()->set_text(TTR("Open Anyway"));
+	get_cancel()->set_text(TTR("Done"));
 
 	text = memnew(Label);
 	vb->add_child(text);
