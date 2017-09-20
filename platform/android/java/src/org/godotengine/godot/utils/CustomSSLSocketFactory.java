24f2864e78370f0a75cd85a334946889ea06f34a
volzhs <volzhs@gmail.com>
2017-09-21 06:23:40 +0900

    Fix TrustManager for Android

--- platform/android/java/src/org/godotengine/godot/utils/CustomSSLSocketFactory.java
+++ platform/android/java/src/org/godotengine/godot/utils/CustomSSLSocketFactory.java
@@ -36,12 +36,9 @@ import java.security.KeyStore;
 import java.security.KeyStoreException;
 import java.security.NoSuchAlgorithmException;
 import java.security.UnrecoverableKeyException;
-import java.security.cert.CertificateException;
-import java.security.cert.X509Certificate;
 
 import javax.net.ssl.SSLContext;
-import javax.net.ssl.TrustManager;
-import javax.net.ssl.X509TrustManager;
+import javax.net.ssl.TrustManagerFactory;
 
 import org.apache.http.conn.ssl.SSLSocketFactory;
 
@@ -56,19 +53,10 @@ public class CustomSSLSocketFactory extends SSLSocketFactory {
     public CustomSSLSocketFactory(KeyStore truststore) throws NoSuchAlgorithmException, KeyManagementException, KeyStoreException, UnrecoverableKeyException {
         super(truststore);
 
-        TrustManager tm = new X509TrustManager() {
-            public void checkClientTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
+        TrustManagerFactory tmf = TrustManagerFactory.getInstance("X509");
+        tmf.init(truststore);
 
-            public void checkServerTrusted(X509Certificate[] chain, String authType) throws CertificateException {
-            }
-
-            public X509Certificate[] getAcceptedIssuers() {
-                return null;
-            }
-        };
-
-        sslContext.init(null, new TrustManager[] { tm }, null);
+        sslContext.init(null, tmf.getTrustManagers(), null);
     }
 
     @Override
