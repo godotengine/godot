d595b79c62a2db691f5b7314d365d3a57b601c18
George Marques <george@gmarqu.es>
2017-09-21 22:47:52 -0300

    UWP: Fix build issue about Object ambiguity

--- platform/uwp/gl_context_egl.cpp
+++ platform/uwp/gl_context_egl.cpp
@@ -31,7 +31,7 @@
 
 #include "EGL/eglext.h"
 
-using namespace Platform;
+using Platform::Exception;
 
 void ContextEGL::release_current() {
 
@@ -103,23 +103,23 @@ Error ContextEGL::initialize() {
 
 		const EGLint displayAttributes[] =
 				{
-				  /*EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
-			EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
-			EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
-			EGL_NONE,*/
-				  // These are the default display attributes, used to request ANGLE's D3D11 renderer.
-				  // eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
-				  EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+					/*EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
+					EGL_PLATFORM_ANGLE_MAX_VERSION_MAJOR_ANGLE, 9,
+					EGL_PLATFORM_ANGLE_MAX_VERSION_MINOR_ANGLE, 3,
+					EGL_NONE,*/
+					// These are the default display attributes, used to request ANGLE's D3D11 renderer.
+					// eglInitialize will only succeed with these attributes if the hardware supports D3D11 Feature Level 10_0+.
+					EGL_PLATFORM_ANGLE_TYPE_ANGLE, EGL_PLATFORM_ANGLE_TYPE_D3D11_ANGLE,
 
-				  // EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
-				  // Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
-				  //EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
+					// EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER is an optimization that can have large performance benefits on mobile devices.
+					// Its syntax is subject to change, though. Please update your Visual Studio templates if you experience compilation issues with it.
+					//EGL_ANGLE_DISPLAY_ALLOW_RENDER_TO_BACK_BUFFER, EGL_TRUE,
 
-				  // EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE is an option that enables ANGLE to automatically call
-				  // the IDXGIDevice3::Trim method on behalf of the application when it gets suspended.
-				  // Calling IDXGIDevice3::Trim when an application is suspended is a Windows Store application certification requirement.
-				  EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
-				  EGL_NONE,
+					// EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE is an option that enables ANGLE to automatically call
+					// the IDXGIDevice3::Trim method on behalf of the application when it gets suspended.
+					// Calling IDXGIDevice3::Trim when an application is suspended is a Windows Store application certification requirement.
+					EGL_PLATFORM_ANGLE_ENABLE_AUTOMATIC_TRIM_ANGLE, EGL_TRUE,
+					EGL_NONE,
 				};
 
 		PFNEGLGETPLATFORMDISPLAYEXTPROC eglGetPlatformDisplayEXT = reinterpret_cast<PFNEGLGETPLATFORMDISPLAYEXTPROC>(eglGetProcAddress("eglGetPlatformDisplayEXT"));
