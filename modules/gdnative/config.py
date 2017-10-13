41e70d4192281f87962c60942d11c351e78c4257
BastiaanOlij <mux213@gmail.com>
2017-10-13 21:49:01 +1100

    Move GDNative docs into gdnative folder

--- modules/gdnative/config.py
+++ modules/gdnative/config.py
@@ -1,8 +1,12 @@
 
-
 def can_build(platform):
     return True
 
-
 def configure(env):
     env.use_ptrcall = True
+
+def get_doc_classes():
+  return ["GDNative", "GDNativeClass", "GDNativeLibrary", "NativeScript"]
+
+def get_doc_path():
+  return "doc_classes"
