2449d21671bbafa4deea3d91619b7588322ca140
Poommetee Ketson <poommetee@protonmail.com>
2017-10-22 11:36:43 +0700

    AudioEffectFilter: lowercase dB property to db

--- servers/audio/effects/audio_effect_filter.cpp
+++ servers/audio/effects/audio_effect_filter.cpp
@@ -158,7 +158,7 @@ void AudioEffectFilter::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "cutoff_hz", PROPERTY_HINT_RANGE, "1,40000,0.1"), "set_cutoff", "get_cutoff");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "resonance", PROPERTY_HINT_RANGE, "0,1,0.01"), "set_resonance", "get_resonance");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "gain", PROPERTY_HINT_RANGE, "0,4,0.01"), "set_gain", "get_gain");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "dB", PROPERTY_HINT_ENUM, "6db,12db,18db,24db"), "set_db", "get_db");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "db", PROPERTY_HINT_ENUM, "6 dB,12 dB,18 dB,24 dB"), "set_db", "get_db");
 
 	BIND_ENUM_CONSTANT(FILTER_6DB);
 	BIND_ENUM_CONSTANT(FILTER_12DB);
