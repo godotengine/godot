d1102cb37d4c299958c9aed0a5db5d62754753b2
Leon Krause <lk@leonkrause.com>
2017-10-26 22:06:47 +0200

    Remove HTTPClient::send_body_text and ::send_body_data
    These were never implemented, the request* methods provide parameters to
    send body data as part of the client's requests.

--- core/io/http_client.h
+++ core/io/http_client.h
@@ -169,8 +169,6 @@ public:
 
 	Error request_raw(Method p_method, const String &p_url, const Vector<String> &p_headers, const PoolVector<uint8_t> &p_body);
 	Error request(Method p_method, const String &p_url, const Vector<String> &p_headers, const String &p_body = String());
-	Error send_body_text(const String &p_body);
-	Error send_body_data(const PoolByteArray &p_body);
 
 	void close();
 
