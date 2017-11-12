cd70f675764b3d99293e74d09d73bd66f1a60da0
William McKIE <mckie.william@hotmail.co.uk>
2017-11-12 15:58:10 +0100

    Fix shape collision when tile map's origin is centered
    The behaviour should be act like it was in 2.X, but improvement should
    be made in order to the shape follow its parent when it is mirrored or
    transposed.
    
    Closes #11876.

--- scene/2d/tile_map.cpp
+++ scene/2d/tile_map.cpp
@@ -459,9 +459,9 @@ void TileMap::_update_dirty_quadrants() {
 					Transform2D xform;
 					xform.set_origin(offset.floor());
 
-					_fix_cell_transform(xform, c, center_ofs, s);
+					_fix_cell_transform(xform, c, shapes[i].shape_transform.get_origin() + center_ofs, s);
 
-					xform *= shapes[i].shape_transform;
+					//xform *= shapes[i].shape_transform;
 
 					if (debug_canvas_item.is_valid()) {
 						vs->canvas_item_add_set_transform(debug_canvas_item, xform);
