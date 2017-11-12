58fafea485bc7223ff4cffe9b9eef8ce9cda0c4c
Andreas Haas <liu.gam3@gmail.com>
2017-11-12 10:50:06 +0100

    Remove preview when closing "change default type" dialog.

--- editor/plugins/canvas_item_editor_plugin.h
+++ editor/plugins/canvas_item_editor_plugin.h
@@ -568,7 +568,8 @@ class CanvasItemEditorViewport : public Control {
 
 	void _on_mouse_exit();
 	void _on_select_type(Object *selected);
-	void _on_change_type();
+	void _on_change_type_confirmed();
+	void _on_change_type_closed();
 
 	void _create_preview(const Vector<String> &files) const;
 	void _remove_preview();
