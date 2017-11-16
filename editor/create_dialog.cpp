496e4e0d2f310fe603cd683c197e247431a48b4a
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2017-11-16 20:24:33 -0200

    Enabling relationship lines in the Scene Tree enables them in the Create New Node dialog as well.

--- editor/create_dialog.cpp
+++ editor/create_dialog.cpp
@@ -97,6 +97,15 @@ void CreateDialog::popup_create(bool p_dontclear) {
 	search_box->grab_focus();
 
 	_update_search();
+
+	bool enable_rl = EditorSettings::get_singleton()->get("docks/scene_tree/draw_relationship_lines");
+	Color rl_color = EditorSettings::get_singleton()->get("docks/scene_tree/relationship_line_color");
+
+	if (enable_rl) {
+		search_options->add_constant_override("draw_relationship_lines", 1);
+		search_options->add_color_override("relationship_line_color", rl_color);
+	} else
+		search_options->add_constant_override("draw_relationship_lines", 0);
 }
 
 void CreateDialog::_text_changed(const String &p_newtext) {
