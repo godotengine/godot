17a3e2b370c19a2741b0fe01272bf7180c3a1780
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2017-12-01 12:37:25 -0200

    Changed EditorAbout's TextEdits to RichTextLabels.

--- editor/editor_about.cpp
+++ editor/editor_about.cpp
@@ -44,8 +44,8 @@ void EditorAbout::_notification(int p_what) {
 		case NOTIFICATION_THEME_CHANGED: {
 
 			Ref<Font> font = EditorNode::get_singleton()->get_gui_base()->get_font("source", "EditorFonts");
-			_tpl_text->add_font_override("font", font);
-			_license_text->add_font_override("font", font);
+			_tpl_text->add_font_override("normal_font", font);
+			_license_text->add_font_override("normal_font", font);
 		} break;
 	}
 }
@@ -53,7 +53,6 @@ void EditorAbout::_notification(int p_what) {
 void EditorAbout::_license_tree_selected() {
 
 	TreeItem *selected = _tpl_tree->get_selected();
-	_tpl_text->select(0, 0, 0, 0);
 	_tpl_text->set_text(selected->get_metadata(0));
 }
 
@@ -165,12 +164,10 @@ EditorAbout::EditorAbout() {
 
 	// License
 
-	_license_text = memnew(TextEdit);
+	_license_text = memnew(RichTextLabel);
 	_license_text->set_name(TTR("License"));
 	_license_text->set_h_size_flags(Control::SIZE_EXPAND_FILL);
 	_license_text->set_v_size_flags(Control::SIZE_EXPAND_FILL);
-	_license_text->set_wrap(true);
-	_license_text->set_readonly(true);
 	_license_text->set_text(String::utf8(about_license));
 	tc->add_child(_license_text);
 
@@ -239,11 +236,9 @@ EditorAbout::EditorAbout() {
 	tpl_ti_all->set_metadata(0, long_text);
 	tpl_hbc->add_child(_tpl_tree);
 
-	_tpl_text = memnew(TextEdit);
+	_tpl_text = memnew(RichTextLabel);
 	_tpl_text->set_h_size_flags(Control::SIZE_EXPAND_FILL);
 	_tpl_text->set_v_size_flags(Control::SIZE_EXPAND_FILL);
-	_tpl_text->set_wrap(true);
-	_tpl_text->set_readonly(true);
 	tpl_hbc->add_child(_tpl_text);
 
 	_tpl_tree->connect("item_selected", this, "_license_tree_selected");
