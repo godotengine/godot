48cefc9c969719d6fab8d3237c6d9e02bd34b64f
Rémi Verschelde <rverschelde@gmail.com>
2017-12-10 16:40:31 +0100

    TileMap: Drop unused center_x/center_y booleans
    Two years later they are still unused and we do not know
    their intend use case, so tschüss.
    Closes #2513.

--- scene/2d/tile_map.h
+++ scene/2d/tile_map.h
@@ -68,7 +68,6 @@ private:
 	Ref<TileSet> tile_set;
 	Size2i cell_size;
 	int quadrant_size;
-	bool center_x, center_y;
 	Mode mode;
 	Transform2D custom_transform;
 	HalfOffset half_offset;
@@ -230,11 +229,6 @@ public:
 
 	void set_quadrant_size(int p_size);
 	int get_quadrant_size() const;
-
-	void set_center_x(bool p_enable);
-	bool get_center_x() const;
-	void set_center_y(bool p_enable);
-	bool get_center_y() const;
 
 	void set_cell(int p_x, int p_y, int p_tile, bool p_flip_x = false, bool p_flip_y = false, bool p_transpose = false, Vector2 p_autotile_coord = Vector2());
 	int get_cell(int p_x, int p_y) const;
