e1da261844419ebb31a1d0a82f0853acc42f6427
Juan Linietsky <juan@godotengine.org>
2017-12-24 09:37:51 -0300

    Restored opus build since WebM needs it, but disabled it from code.

--- modules/opus/config.py
+++ modules/opus/config.py
@@ -1,10 +1,5 @@
 def can_build(platform):
-    # Sorry guys, do not enable this unless you can figure out a way
-    # to get Opus to not do any memory allocation or system calls
-    # in the audio thread.
-    # Currently the implementation even reads files from the audio thread,
-    # and this is not how audio programming works.
-    return False
+    return True
 
 def configure(env):
     pass
