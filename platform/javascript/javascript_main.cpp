cf5b074a95c806c9a49cec98a4e0708dc8fb426f
Leon Krause <lk@leonkrause.com>
2018-01-06 15:53:04 +0100

    Fix internal Emscripten JS API calls
    Emscripten 1.37.24 no longer exports these by default

--- platform/javascript/javascript_main.cpp
+++ platform/javascript/javascript_main.cpp
@@ -65,7 +65,7 @@ int main(int argc, char *argv[]) {
 		FS.mkdir('/userfs');
 		FS.mount(IDBFS, {}, '/userfs');
 		FS.syncfs(true, function(err) {
-			Module['ccall']('main_after_fs_sync', null, ['string'], [err ? err.message : ""])
+			ccall('main_after_fs_sync', null, ['string'], [err ? err.message : ""])
 		});
 	);
 	/* clang-format on */
