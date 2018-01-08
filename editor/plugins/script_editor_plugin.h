fdedc442437b61caed1beb8ebad16c5057431589
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2018-01-07 22:31:36 -0200

    Changes for the "Recent Scripts" menu.

--- editor/plugins/script_editor_plugin.h
+++ editor/plugins/script_editor_plugin.h
@@ -199,6 +199,7 @@ class ScriptEditor : public PanelContainer {
 	VSplitContainer *list_split;
 	TabContainer *tab_container;
 	EditorFileDialog *file_dialog;
+	AcceptDialog *error_dialog;
 	ConfirmationDialog *erase_tab_confirm;
 	ScriptCreateDialog *script_create_dialog;
 	ScriptEditorDebugger *debugger;
@@ -228,8 +229,6 @@ class ScriptEditor : public PanelContainer {
 	Vector<ScriptHistory> history;
 	int history_pos;
 
-	Vector<String> previous_scripts;
-
 	EditorHelpIndex *help_index;
 
 	void _tab_changed(int p_which);
@@ -253,7 +252,7 @@ class ScriptEditor : public PanelContainer {
 
 	void _show_error_dialog(String p_path);
 
-	void _close_tab(int p_idx, bool p_save = true, bool p_history_back = true);
+	void _close_tab(int p_idx, bool p_save = true);
 
 	void _close_current_tab();
 	void _close_discard_current_tab(const String &p_str);
