9d27bd3c3b39e3a1cd7d42872cddb55e6368da70
poke1024 <poke1024@gmx.de>
2018-01-09 19:03:51 +0100

    Fix SortArray crashing with bad comparison functions

--- core/array.cpp
+++ core/array.cpp
@@ -259,7 +259,7 @@ Array &Array::sort_custom(Object *p_obj, const StringName &p_function) {
 
 	ERR_FAIL_NULL_V(p_obj, *this);
 
-	SortArray<Variant, _ArrayVariantSortCustom> avs;
+	SortArray<Variant, _ArrayVariantSortCustom, true> avs;
 	avs.compare.obj = p_obj;
 	avs.compare.func = p_function;
 	avs.sort(_p->array.ptrw(), _p->array.size());
