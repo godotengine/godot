70e7a386a8a59b3d7c04fc26813d38eae2dbe917
Marcin Zawiejski <dragmz@gmail.com>
2018-02-04 13:23:23 +0100

    fix buffer write performance on Windows and Unix

--- drivers/unix/file_access_unix.h
+++ drivers/unix/file_access_unix.h
@@ -73,6 +73,7 @@ public:
 	virtual Error get_error() const; ///< get last error
 
 	virtual void store_8(uint8_t p_dest); ///< store a byte
+	virtual void store_buffer(const uint8_t *p_src, int p_length); ///< store an array of bytes
 
 	virtual bool file_exists(const String &p_path); ///< return true if a file exists
 
