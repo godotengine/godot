b3a45df54b21248e3959e41f5938119ec61e53ae
Ruslan Mustakov <r.mustakov@gmail.com>
2018-02-27 22:37:20 +0700

    Make BaseButton not emit press when container is scrolled
    This fixes the problem described in #13996 in a proper way.
    
    This also adds "deadzone" property to ScrollContainer. It can be used
    on mobile, where taps are not as precise as mouse clicks. Player could
    slightly move their finger when tapping, in which case we still want
    the button to be pressed rather than the container to be scrolled.

--- scene/gui/base_button.cpp
+++ scene/gui/base_button.cpp
@@ -252,7 +252,7 @@ void BaseButton::_notification(int p_what) {
 		status.hovering = false;
 		update();
 	}
-	if (p_what == NOTIFICATION_DRAG_BEGIN) {
+	if (p_what == NOTIFICATION_DRAG_BEGIN || p_what == NOTIFICATION_SCROLL_BEGIN) {
 
 		if (status.press_attempt) {
 			status.press_attempt = false;
