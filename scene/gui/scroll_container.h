ea5c89596bb851e18171fd058095fbddfcc636f1
Ruslan Mustakov <r.mustakov@gmail.com>
2018-02-27 22:37:20 +0700

    Make BaseButton not emit press when container is scrolled
    This fixes the problem described in #13996 in a proper way.
    
    This also adds "deadzone" property to ScrollContainer. It can be used
    on mobile, where taps are not as precise as mouse clicks. Player could
    slightly move their finger when tapping, in which case we still want
    the button to be pressed rather than the container to be scrolled.
    
    (cherry picked from commit dcf5be92a3aede861909c08d06b114669bbe75e8)

--- scene/gui/scroll_container.h
+++ scene/gui/scroll_container.h
@@ -92,9 +92,6 @@ public:
 	int get_deadzone() const;
 	void set_deadzone(int p_deadzone);
 
-	HScrollBar *get_h_scrollbar();
-	VScrollBar *get_v_scrollbar();
-
 	virtual bool clips_input() const;
 
 	virtual String get_configuration_warning() const;
