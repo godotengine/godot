b0e325d5dd98fde707bc22fc21ca386e6beca037
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2018-03-02 19:19:46 +0100

    Fix more regressions in RichTextLabel from PR 15711

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -266,10 +266,6 @@ int RichTextLabel::_process_line(ItemFrame *p_frame, const Vector2 &p_ofs, int &
 				int ascent = font->get_ascent();
 				int descent = font->get_descent();
 
-				line_ascent = MAX(line_ascent, ascent);
-				line_descent = MAX(line_descent, descent);
-				fh = MAX(fh, line_ascent + line_descent); // various fonts!
-
 				Color color;
 				bool underline = false;
 
@@ -320,10 +316,9 @@ int RichTextLabel::_process_line(ItemFrame *p_frame, const Vector2 &p_ofs, int &
 					CHECK_HEIGHT(fh);
 					ENSURE_WIDTH(w);
 
-					if (p_mode == PROCESS_CACHE) {
-						line_ascent = ascent;
-						line_descent = descent;
-					}
+					line_ascent = MAX(line_ascent, ascent);
+					line_descent = MAX(line_descent, descent);
+					fh = line_ascent + line_descent;
 
 					if (end && c[end - 1] == ' ') {
 						if (p_mode == PROCESS_CACHE) {
