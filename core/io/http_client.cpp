602da6fa4fbc13255e1b0f6c8748ff0035e96325
Pedro J. Estébanez <pedrojrulez@gmail.com>
2018-03-04 01:22:59 +0100

    Fix wrong SSL handshake
    The name of the remote host is passed to mbed TLS in all cases so the client hello message is correctly formed.

--- core/io/http_client.cpp
+++ core/io/http_client.cpp
@@ -298,7 +298,7 @@ Error HTTPClient::poll() {
 				case StreamPeerTCP::STATUS_CONNECTED: {
 					if (ssl) {
 						Ref<StreamPeerSSL> ssl = StreamPeerSSL::create();
-						Error err = ssl->connect_to_stream(tcp_connection, ssl_verify_host, ssl_verify_host ? conn_host : String());
+						Error err = ssl->connect_to_stream(tcp_connection, ssl_verify_host, conn_host);
 						if (err != OK) {
 							close();
 							status = STATUS_SSL_HANDSHAKE_ERROR;
