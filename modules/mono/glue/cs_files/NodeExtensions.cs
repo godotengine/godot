877ea882ce3a4938c3b73b903e5d6e1e8f970eef
Paul Joannon <hello@pauljoannon.com>
2018-03-06 15:39:01 +0100

    Add missing methods to Node class in the mono glue
    * GetNodeOrNull<T>
    * GetChild<T>
    * GetChildOrNull<T>
    * GetOwner<T>
    * GetOwnerOrNull<T>
    * GetParent<T>
    * GetParentOrNull<T>

--- modules/mono/glue/cs_files/NodeExtensions.cs
+++ modules/mono/glue/cs_files/NodeExtensions.cs
@@ -6,5 +6,40 @@ namespace Godot
         {
             return (T)GetNode(path);
         }
+
+        public T GetNodeOrNull<T>(NodePath path) where T : Godot.Node
+        {
+            return GetNode(path) as T;
+        }
+
+        public T GetChild<T>(int idx) where T : Godot.Node
+        {
+            return (T)GetChild(idx);
+        }
+
+        public T GetChildOrNull<T>(int idx) where T : Godot.Node
+        {
+            return GetChild(idx) as T;
+        }
+
+        public T GetOwner<T>() where T : Godot.Node
+        {
+            return (T)GetOwner();
+        }
+
+        public T GetOwnerOrNull<T>() where T : Godot.Node
+        {
+            return GetOwner() as T;
+        }
+
+        public T GetParent<T>() where T : Godot.Node
+        {
+            return (T)GetParent();
+        }
+
+        public T GetParentOrNull<T>() where T : Godot.Node
+        {
+            return GetParent() as T;
+        }
     }
 }
