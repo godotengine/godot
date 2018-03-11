d0f6d92b9f054a0145564b37b560be4f3c82a8a1
Bernhard Liebl <Bernhard.Liebl@gmx.org>
2018-03-11 15:28:23 +0100

    Fix column width on AutoLoad table on hidpi displays
    (cherry picked from commit f494d5ac5cb849f6fbe80e0b84449e9d8f361e32)

--- editor/editor_autoload_settings.cpp
+++ editor/editor_autoload_settings.cpp
@@ -830,7 +830,7 @@ EditorAutoloadSettings::EditorAutoloadSettings() {
 	tree->set_column_min_width(2, 80 * EDSCALE);
 
 	tree->set_column_expand(3, false);
-	tree->set_column_min_width(3, 120 * EDSCALE);
+	tree->set_column_min_width(3, 80 * EDSCALE);
 
 	tree->connect("cell_selected", this, "_autoload_selected");
 	tree->connect("item_edited", this, "_autoload_edited");
