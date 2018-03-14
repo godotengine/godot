9baef79ea586bd0bda4b9a9732d085d958aaa93e
Ruslan Mustakov <r.mustakov@gmail.com>
2018-03-14 19:38:34 +0700

    Ceil dynamic font glyph size
    Fixes #15459. When oversampling is enabled, glyphs may have fractional
    size, but they are still rendered into integral pixels, which results in
    them taking more space than was anticiped by autowrapping algorithm. The
    solution here is to return ceiled width, which makes autowrapper
    consider characters a bit larger than they are, but it doesn't hurt the
    actual rendering and ensures there is enough space for the characters.

--- scene/resources/dynamic_font.cpp
+++ scene/resources/dynamic_font.cpp
@@ -308,6 +308,9 @@ Size2 DynamicFontAtSize::get_char_size(CharType p_char, CharType p_next, const V
 			ret.x += (delta.x >> 6) / oversampling;
 		}
 	}
+
+	// ensures oversampled glyphs will have enough space when this value is used by clipping/wrapping algorithms
+	ret.x = Math::ceil(ret.x);
 	return ret;
 }
 
