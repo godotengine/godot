80cd6c0ef3acf1afac85c946204785769b90d4d1
Bastiaan Olij <mux213@gmail.com>
2018-03-15 22:24:49 +1100

    Added missing import BoolVariable
    Didn't like the missing BoolVariable :)

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -65,7 +65,7 @@ def get_opts():
         EnumVariable('debug_symbols', 'Add debug symbols to release version', 'yes', ('yes', 'no', 'full')),
         BoolVariable('separate_debug_symbols', 'Create a separate file with the debug symbols', False),
         ('msvc_version', 'MSVC version to use. Ignored if VCINSTALLDIR is set in shell env.', None),
-        (BoolVariable('use_mingw', 'Use the Mingw compiler, even if MSVC is installed. Only used on Windows.', False)),
+        BoolVariable('use_mingw', 'Use the Mingw compiler, even if MSVC is installed. Only used on Windows.', False),
     ]
 
 
