5cadde8b422633c794add31354cdbda73467dbee
Wilson E. Alvarez <wilson.e.alvarez1@gmail.com>
2018-03-26 13:50:48 -0400

    Added shortcut to rename files

--- editor/filesystem_dock.cpp
+++ editor/filesystem_dock.cpp
@@ -1690,6 +1690,8 @@ void FileSystemDock::_files_gui_input(Ref<InputEvent> p_event) {
 			_file_option(FILE_COPY_PATH);
 		} else if (ED_IS_SHORTCUT("filesystem_dock/delete", p_event)) {
 			_file_option(FILE_REMOVE);
+		} else if (ED_IS_SHORTCUT("filesystem_dock/rename", p_event)) {
+			_file_option(FILE_RENAME);
 		}
 	}
 }
@@ -1800,6 +1802,7 @@ FileSystemDock::FileSystemDock(EditorNode *p_editor) {
 	ED_SHORTCUT("filesystem_dock/copy_path", TTR("Copy Path"), KEY_MASK_CMD | KEY_C);
 	ED_SHORTCUT("filesystem_dock/duplicate", TTR("Duplicate..."), KEY_MASK_CMD | KEY_D);
 	ED_SHORTCUT("filesystem_dock/delete", TTR("Delete"), KEY_DELETE);
+	ED_SHORTCUT("filesystem_dock/rename", TTR("Rename"));
 
 	HBoxContainer *toolbar_hbc = memnew(HBoxContainer);
 	add_child(toolbar_hbc);
