ef845da2641e385c4ab19a8a9d2c1712ea4b103c
Pedro J. Estébanez <pedrojrulez@gmail.com>
2018-03-27 23:41:27 +0200

    Improve popup menus usability
    It seems that popups were intended to "grab" the mouse click that triggered them, but their intent was being lost. This commit does the necessary changes to let it happen and updates items that were trying to get advantage of it, because the semantics of `Control::grab_click_focus()` have changed a bit. Namely, it must be called **before** showing the modal.
    
    This allows to popup a menu and activate an item in it in a single click-point-release cycle, instead of having to click once to open the menu and once more to pick an item.
    
    This ability is extended even to context menus activated with the RMB (or any other mouse button, for that matter). The editor benefits from this in the context menu of the tree dock, which has been patched to opt-in for this feature.
    
    This improves UX a bit by saving unnecessary clicks.
    
    From now on, `PopupMenu` always grabs the click and also invalidates the first button release unless the mouse has moved (that's what `set_invalidate_click_until_motion()` was doing and now it's removed), so there is no longer the need of doing both things at every point a pop-up menu is shown.

--- scene/gui/popup_menu.h
+++ scene/gui/popup_menu.h
@@ -201,13 +201,7 @@ public:
 	void set_hide_on_multistate_item_selection(bool p_enabled);
 	bool is_hide_on_multistate_item_selection() const;
 
-	void set_submenu_popup_delay(float p_time);
-	float get_submenu_popup_delay() const;
-
 	virtual void popup(const Rect2 &p_bounds = Rect2());
-
-	void set_hide_on_window_lose_focus(bool p_enabled);
-	bool is_hide_on_window_lose_focus() const;
 
 	PopupMenu();
 	~PopupMenu();
