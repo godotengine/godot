4e56da28322a96de6803772c2a4b35fa10a16d6f
Marc Gilleron <marc.gilleron@gmail.com>
2018-03-27 21:45:27 +0200

    Make heightmap shape usable from PhysicsServer
    - Fixed bad size check
    - Fixed bad member initialization
    - Removed unused cell_size (Bullet expects us to use localScaling)
    - Accept precomputed min/max height, will be calculated if not provided

--- modules/bullet/shape_bullet.h
+++ modules/bullet/shape_bullet.h
@@ -85,7 +85,7 @@ public:
 	/// IMPORTANT: Remember to delete the shape interface by calling: delete my_shape->getMeshInterface();
 	static class btConvexPointCloudShape *create_shape_convex(btAlignedObjectArray<btVector3> &p_vertices, const btVector3 &p_local_scaling = btVector3(1, 1, 1));
 	static class btScaledBvhTriangleMeshShape *create_shape_concave(btBvhTriangleMeshShape *p_mesh_shape, const btVector3 &p_local_scaling = btVector3(1, 1, 1));
-	static class btHeightfieldTerrainShape *create_shape_height_field(PoolVector<real_t> &p_heights, int p_width, int p_depth, real_t p_cell_size);
+	static class btHeightfieldTerrainShape *create_shape_height_field(PoolVector<real_t> &p_heights, int p_width, int p_depth, real_t p_min_height, real_t p_max_height);
 	static class btRayShape *create_shape_ray(real_t p_length, bool p_slips_on_slope);
 };
 
