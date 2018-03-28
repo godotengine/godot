c117736f004fab14aebee2d49a6fe9891d8386e3
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-03-28 15:36:44 +0200

    Always verify peer certificate in lws.
    Behavior is defined by SSL flags passed via the context.
    
    Simplified port of the patch submitted and accepted in lws master branch
    (PR 1215)

--- thirdparty/lws/client/ssl-client.c
+++ thirdparty/lws/client/ssl-client.c
@@ -176,11 +176,7 @@ lws_ssl_client_bio_create(struct lws *wsi)
 #endif
 #else
 #if defined(LWS_WITH_MBEDTLS)
-	if (wsi->vhost->x509_client_CA)
-		SSL_set_verify(wsi->ssl, SSL_VERIFY_PEER, OpenSSL_client_verify_callback);
-	else
-		SSL_set_verify(wsi->ssl, SSL_VERIFY_NONE, OpenSSL_client_verify_callback);
-
+	SSL_set_verify(wsi->ssl, SSL_VERIFY_PEER, OpenSSL_client_verify_callback);
 #else
 #ifdef SSL_CTRL_SET_TLSEXT_HOSTNAME
 	SSL_set_tlsext_host_name(wsi->ssl, hostname);
