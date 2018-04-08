5f5ec7e1629b29a542e977766c86e93a16c3b7be
Marcelo Fernandez <marcelofg55@gmail.com>
2018-04-07 21:02:47 -0300

    WASAPI audio driver compile fix on mingw
    (cherry picked from commit 3316a64e12cb38d499e39b53d9905ce222a0b82e)

--- drivers/wasapi/audio_driver_wasapi.cpp
+++ drivers/wasapi/audio_driver_wasapi.cpp
@@ -37,6 +37,17 @@
 
 #include <functiondiscoverykeys.h>
 
+#ifndef PKEY_Device_FriendlyName
+
+#undef DEFINE_PROPERTYKEY
+/* clang-format off */
+#define DEFINE_PROPERTYKEY(id, a, b, c, d, e, f, g, h, i, j, k, l) \
+	const PROPERTYKEY id = { { a, b, c, { d, e, f, g, h, i, j, k, } }, l };
+/* clang-format on */
+
+DEFINE_PROPERTYKEY(PKEY_Device_FriendlyName, 0xa45c254e, 0xdf1c, 0x4efd, 0x80, 0x20, 0x67, 0xd1, 0x46, 0xa8, 0x50, 0xe0, 14);
+#endif
+
 const CLSID CLSID_MMDeviceEnumerator = __uuidof(MMDeviceEnumerator);
 const IID IID_IMMDeviceEnumerator = __uuidof(IMMDeviceEnumerator);
 const IID IID_IAudioClient = __uuidof(IAudioClient);
