bb2bcfe56a57173f786a35e24081a94c6842f525
Xavier Cho <mysticfallband@gmail.com>
2018-04-09 14:30:03 +0900

    #18051: Use common name for Color type argument
    (cherry picked from commit 94edd92248b4742e7d7a8e283da44d505957c50e)

--- modules/mono/glue/cs_files/Color.cs
+++ modules/mono/glue/cs_files/Color.cs
@@ -272,14 +272,14 @@ namespace Godot
             );
         }
 
-        public Color Lightened(float amount)
+        public Color LinearInterpolate(Color c, float t)
         {
             var res = this;
 
-            res.r += t * (b.r - r);
-            res.g += t * (b.g - g);
-            res.b += t * (b.b - this.b);
-            res.a += t * (b.a - a);
+            res.r += t * (c.r - r);
+            res.g += t * (c.g - g);
+            res.b += t * (c.b - b);
+            res.a += t * (c.a - a);
 
             return res;
         }
