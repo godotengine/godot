e686c2e2208eb3491afde6ad38bdbf5cff307125
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-04-10 17:52:10 +0200

    Implement get_peer_[address|port] in ENet/WSServer
    Also implement get_connected_host and get_connected_port in WebSocketPeer
    (not supported in HTML5 due to browser limitation).
    Add shorthand disconnect_peer(id) for get_peer(id)->close() like in ENet to
    WebSocketServer.

--- modules/enet/networked_multiplayer_enet.h
+++ modules/enet/networked_multiplayer_enet.h
@@ -115,6 +115,9 @@ public:
 
 	virtual int get_packet_peer() const;
 
+	virtual IP_Address get_peer_address(int p_peer_id) const;
+	virtual int get_peer_port(int p_peer_id) const;
+
 	Error create_server(int p_port, int p_max_clients = 32, int p_in_bandwidth = 0, int p_out_bandwidth = 0);
 	Error create_client(const IP_Address &p_ip, int p_port, int p_in_bandwidth = 0, int p_out_bandwidth = 0);
 
