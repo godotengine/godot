8f6bebe7c53decdccd84d3b5f3cea1bb6ca48dc9
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2018-04-24 22:14:43 -0300

    Changed periods in the script templates.

--- modules/mono/csharp_script.cpp
+++ modules/mono/csharp_script.cpp
@@ -305,7 +305,7 @@ Ref<Script> CSharpLanguage::get_template(const String &p_class_name, const Strin
 							 "    public override void _Ready()\n"
 							 "    {\n"
 							 "        // Called every time the node is added to the scene.\n"
-							 "        // Initialization here\n"
+							 "        // Initialization here.\n"
 							 "        \n"
 							 "    }\n"
 							 "\n"
