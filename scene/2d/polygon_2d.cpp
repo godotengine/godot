4c3c510a807ee57c564e7b8b7885cf4fa113bbd5
Juan Linietsky <juan@godotengine.org>
2018-05-02 11:00:35 -0300

    WIP Polygon2D weight editing

--- scene/2d/polygon_2d.cpp
+++ scene/2d/polygon_2d.cpp
@@ -430,6 +430,71 @@ Vector2 Polygon2D::get_offset() const {
 	return offset;
 }
 
+void Polygon2D::add_bone(const NodePath &p_path, const PoolVector<float> &p_weights) {
+
+	Bone bone;
+	bone.path = p_path;
+	bone.weights = p_weights;
+	bone_weights.push_back(bone);
+}
+int Polygon2D::get_bone_count() const {
+	return bone_weights.size();
+}
+NodePath Polygon2D::get_bone_path(int p_index) const {
+	ERR_FAIL_INDEX_V(p_index, bone_weights.size(), NodePath());
+	return bone_weights[p_index].path;
+}
+PoolVector<float> Polygon2D::get_bone_weights(int p_index) const {
+
+	ERR_FAIL_INDEX_V(p_index, bone_weights.size(), PoolVector<float>());
+	return bone_weights[p_index].weights;
+}
+void Polygon2D::erase_bone(int p_idx) {
+
+	ERR_FAIL_INDEX(p_idx, bone_weights.size());
+	bone_weights.remove(p_idx);
+}
+
+void Polygon2D::clear_bones() {
+	bone_weights.clear();
+}
+
+void Polygon2D::set_bone_weights(int p_index, const PoolVector<float> &p_weights) {
+	ERR_FAIL_INDEX(p_index, bone_weights.size());
+	bone_weights[p_index].weights = p_weights;
+	update();
+}
+void Polygon2D::set_bone_path(int p_index, const NodePath &p_path) {
+	ERR_FAIL_INDEX(p_index, bone_weights.size());
+	bone_weights[p_index].path = p_path;
+	update();
+}
+
+Array Polygon2D::_get_bones() const {
+	Array bones;
+	for (int i = 0; i < get_bone_count(); i++) {
+		bones.push_back(get_bone_path(i));
+		bones.push_back(get_bone_weights(i));
+	}
+	return bones;
+}
+void Polygon2D::_set_bones(const Array &p_bones) {
+
+	ERR_FAIL_COND(p_bones.size() & 1);
+	clear_bones();
+	for (int i = 0; i < p_bones.size(); i += 2) {
+		add_bone(p_bones[i], p_bones[i + 1]);
+	}
+}
+
+void Polygon2D::set_skeleton(const NodePath &p_skeleton) {
+	skeleton = p_skeleton;
+	update();
+}
+NodePath Polygon2D::get_skeleton() const {
+	return skeleton;
+}
+
 void Polygon2D::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("set_polygon", "polygon"), &Polygon2D::set_polygon);
@@ -474,6 +539,21 @@ void Polygon2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_offset", "offset"), &Polygon2D::set_offset);
 	ClassDB::bind_method(D_METHOD("get_offset"), &Polygon2D::get_offset);
 
+	ClassDB::bind_method(D_METHOD("add_bone", "path", "weights"), &Polygon2D::add_bone);
+	ClassDB::bind_method(D_METHOD("get_bone_count"), &Polygon2D::get_bone_count);
+	ClassDB::bind_method(D_METHOD("get_bone_path", "index"), &Polygon2D::get_bone_path);
+	ClassDB::bind_method(D_METHOD("get_bone_weights", "index"), &Polygon2D::get_bone_weights);
+	ClassDB::bind_method(D_METHOD("erase_bone", "index"), &Polygon2D::erase_bone);
+	ClassDB::bind_method(D_METHOD("clear_bones"), &Polygon2D::clear_bones);
+	ClassDB::bind_method(D_METHOD("set_bone_path", "index", "path"), &Polygon2D::set_bone_path);
+	ClassDB::bind_method(D_METHOD("set_bone_weights", "index", "weights"), &Polygon2D::set_bone_weights);
+
+	ClassDB::bind_method(D_METHOD("set_skeleton", "skeleton"), &Polygon2D::set_skeleton);
+	ClassDB::bind_method(D_METHOD("get_skeleton"), &Polygon2D::get_skeleton);
+
+	ClassDB::bind_method(D_METHOD("_set_bones", "bones"), &Polygon2D::_set_bones);
+	ClassDB::bind_method(D_METHOD("_get_bones"), &Polygon2D::_get_bones);
+
 	ADD_PROPERTY(PropertyInfo(Variant::POOL_VECTOR2_ARRAY, "polygon"), "set_polygon", "get_polygon");
 	ADD_PROPERTY(PropertyInfo(Variant::POOL_VECTOR2_ARRAY, "uv"), "set_uv", "get_uv");
 	ADD_PROPERTY(PropertyInfo(Variant::POOL_INT_ARRAY, "splits"), "set_splits", "get_splits");
@@ -488,10 +568,14 @@ void Polygon2D::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "texture_scale"), "set_texture_scale", "get_texture_scale");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "texture_rotation_degrees", PROPERTY_HINT_RANGE, "-1440,1440,0.1"), "set_texture_rotation_degrees", "get_texture_rotation_degrees");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "texture_rotation", PROPERTY_HINT_NONE, "", 0), "set_texture_rotation", "get_texture_rotation");
+	ADD_GROUP("Skeleton", "");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "skeleton"), "set_skeleton", "get_skeleton");
 
 	ADD_GROUP("Invert", "invert_");
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "invert_enable"), "set_invert", "get_invert");
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "invert_border", PROPERTY_HINT_RANGE, "0.1,16384,0.1"), "set_invert_border", "get_invert_border");
+
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "bones", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_NOEDITOR), "_set_bones", "_get_bones");
 }
 
 Polygon2D::Polygon2D() {
