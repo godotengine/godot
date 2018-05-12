e6d34a1f179709fd08b0e44c7bf88398e5e8d8d6
Rémi Verschelde <rverschelde@gmail.com>
2018-05-12 09:38:00 +0200

    Sync classref with current source

--- modules/csg/config.py
+++ modules/csg/config.py
@@ -3,3 +3,19 @@ def can_build(platform):
 
 def configure(env):
     pass
+
+def get_doc_classes():
+    return [
+        "CSGBox",
+        "CSGCombiner",
+        "CSGCylinder",
+        "CSGMesh",
+        "CSGPolygon",
+        "CSGPrimitive",
+        "CSGShape",
+        "CSGSphere",
+        "CSGTorus",
+    ]
+
+def get_doc_path():
+    return "doc_classes"
