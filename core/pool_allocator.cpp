13a90fdd89db07447a527a92ce752c6c23f44ab1
nemerle <nemerle5@gmail.com>
2018-05-17 00:35:47 +0200

    Fix PoolAllocator::resize for too large p_new_size
    The code had a subtle signed/unsigned bug -
    ```cpp
    if( signed - unsigned < 0)
    // signed - unsigned is unsigned in c++, so
    if( unsigned < 0)
    // and thus the if block will never be executed
    ```
    
    Thus all the following code would be ran, including unnecessary retries
    of compacting the pool.
    
    (cherry picked from commit 2bbe6144ff4eb6b68b8b06144bca21bffdf1e8b5)

--- core/pool_allocator.cpp
+++ core/pool_allocator.cpp
@@ -359,7 +359,7 @@ Error PoolAllocator::resize(ID p_mem, int p_new_size) {
 	//p_new_size = align(p_new_size)
 	int _free = free_mem; // - static_area_size;
 
-	if ((_free + aligned(e->len)) - alloc_size < 0) {
+	if (uint32_t(_free + aligned(e->len)) < alloc_size) {
 		mt_unlock();
 		ERR_FAIL_V(ERR_OUT_OF_MEMORY);
 	};
