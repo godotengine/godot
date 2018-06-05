5748fcd4bdcfd17e022da3de730578e3fb63f9df
volzhs <volzhs@gmail.com>
2018-06-06 06:18:11 +0900

    Show opened script or help name

--- editor/plugins/script_editor_plugin.cpp
+++ editor/plugins/script_editor_plugin.cpp
@@ -1542,18 +1542,18 @@ void ScriptEditor::_update_members_overview_visibility() {
 
 	ScriptEditorBase *se = _get_current_editor();
 	if (!se) {
-		members_overview_buttons_hbox->set_visible(false);
+		members_overview_alphabeta_sort_button->set_visible(false);
 		members_overview->set_visible(false);
 		overview_vbox->set_visible(false);
 		return;
 	}
 
 	if (members_overview_enabled && se->show_members_overview()) {
-		members_overview_buttons_hbox->set_visible(true);
+		members_overview_alphabeta_sort_button->set_visible(true);
 		members_overview->set_visible(true);
 		overview_vbox->set_visible(true);
 	} else {
-		members_overview_buttons_hbox->set_visible(false);
+		members_overview_alphabeta_sort_button->set_visible(false);
 		members_overview->set_visible(false);
 		overview_vbox->set_visible(false);
 	}
@@ -1582,7 +1582,7 @@ void ScriptEditor::_update_members_overview() {
 		members_overview->set_item_metadata(i, functions[i].get_slice(":", 1).to_int() - 1);
 	}
 
-	String path = se->get_edited_resource()->get_path();
+	String path = se->get_edited_script()->get_path();
 	bool built_in = !path.is_resource_file();
 	String name = built_in ? path.get_file() : se->get_name();
 	filename->set_text(name);
@@ -1604,9 +1604,8 @@ void ScriptEditor::_update_help_overview_visibility() {
 	}
 
 	if (help_overview_enabled) {
-		members_overview_buttons_hbox->set_visible(false);
+		members_overview_alphabeta_sort_button->set_visible(false);
 		help_overview->set_visible(true);
-		overview_vbox->set_visible(true);
 		filename->set_text(se->get_name());
 	} else {
 		help_overview->set_visible(false);
@@ -2945,6 +2944,12 @@ ScriptEditor::ScriptEditor(EditorNode *p_editor) {
 	list_split->add_child(members_overview_vbox);
 	members_overview_buttons_hbox = memnew(HBoxContainer);
 	members_overview_vbox->add_child(members_overview_buttons_hbox);
+
+	filename = memnew(Label);
+	filename->set_clip_text(true);
+	filename->set_h_size_flags(SIZE_EXPAND_FILL);
+	filename->add_style_override("normal", EditorNode::get_singleton()->get_gui_base()->get_stylebox("normal", "LineEdit"));
+	members_overview_buttons_hbox->add_child(filename);
 
 	members_overview_alphabeta_sort_button = memnew(ToolButton);
 	members_overview_alphabeta_sort_button->set_tooltip(TTR("Toggle alphabetical sorting of the method list."));
