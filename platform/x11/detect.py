ec293e4f55e13727245de2ca092decb6a203a548
Rémi Verschelde <rverschelde@gmail.com>
2018-06-07 10:41:11 +0200

    SCons: Allow unbundling libwebsockets and miniupnpc

--- platform/x11/detect.py
+++ platform/x11/detect.py
@@ -167,14 +167,6 @@ def configure(env):
 
     # FIXME: Check for existence of the libs before parsing their flags with pkg-config
 
-    if not env['builtin_mbedtls']:
-        # mbedTLS does not provide a pkgconfig config yet. See https://github.com/ARMmbed/mbedtls/issues/228
-        env.Append(LIBS=['mbedtls', 'mbedcrypto', 'mbedx509'])
-
-    if not env['builtin_libwebp']:
-        env.ParseConfig('pkg-config libwebp --cflags --libs')
-
-
     # freetype depends on libpng and zlib, so bundling one of them while keeping others
     # as shared libraries leads to weird issues
     if env['builtin_freetype'] or env['builtin_libpng'] or env['builtin_zlib']:
