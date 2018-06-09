dd81ee9855d12e586bcf12efd97386ceeb0d720a
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2018-06-09 11:45:34 +0200

    Correct test list returned by test_get_names
    (cherry picked from commit 7e3b605ff43c309e146225c6bdee669e724e620c)

--- main/tests/test_main.cpp
+++ main/tests/test_main.cpp
@@ -50,15 +50,20 @@ const char **tests_get_names() {
 
 	static const char *test_names[] = {
 		"string",
-		"containers",
 		"math",
+		"physics",
+		"physics_2d",
 		"render",
-		"multimesh",
+		"oa_hash_map",
 		"gui",
 		"io",
 		"shaderlang",
-		"physics",
-		"oa_hash_map",
+		"gd_tokenizer",
+		"gd_parser",
+		"gd_compiler",
+		"gd_bytecode",
+		"image",
+		"ordered_hash_map",
 		NULL
 	};
 
