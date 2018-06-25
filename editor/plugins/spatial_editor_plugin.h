46ec1deba579c7b3c073201324d241b498eacb15
Kyle Dayton <kyle@grol.ly>
2018-06-24 20:24:31 -0500

    Add lock rotation feature to spatial editor viewport

--- editor/plugins/spatial_editor_plugin.h
+++ editor/plugins/spatial_editor_plugin.h
@@ -93,7 +93,8 @@ class SpatialEditorViewport : public Control {
 		VIEW_DISPLAY_NORMAL,
 		VIEW_DISPLAY_WIREFRAME,
 		VIEW_DISPLAY_OVERDRAW,
-		VIEW_DISPLAY_SHADELESS
+		VIEW_DISPLAY_SHADELESS,
+		VIEW_LOCK_ROTATION
 	};
 
 public:
@@ -131,6 +132,7 @@ private:
 	Camera *camera;
 	bool transforming;
 	bool orthogonal;
+	bool lock_rotation;
 	float gizmo_scale;
 
 	bool freelook_active;
