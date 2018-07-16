08481b99a9087374f9cf75e54e34741f0dd85dfb
Juan Linietsky <reduzio@gmail.com>
2018-07-15 22:38:14 -0300

    Added small icon to expand the bottom panel (and shortcut too)

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -377,7 +377,11 @@ private:
 
 	PanelContainer *bottom_panel;
 	HBoxContainer *bottom_panel_hb;
+	HBoxContainer *bottom_panel_hb_editors;
 	VBoxContainer *bottom_panel_vb;
+	ToolButton *bottom_panel_raise;
+
+	void _bottom_panel_raise_toggled(bool);
 
 	EditorInterface *editor_interface;
 
