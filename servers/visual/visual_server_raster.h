e3d140924c0b8be6b71004afbd2371eed8b3cd05
Max Hilbrunner <mhilbrunner@users.noreply.github.com>
2018-07-17 16:52:13 +0200

    Revert "Fix #19507 Not emitted particles affects performance"

--- servers/visual/visual_server_raster.h
+++ servers/visual/visual_server_raster.h
@@ -396,7 +396,6 @@ public:
 
 	BIND2(particles_set_emitting, RID, bool)
 	BIND1R(bool, particles_get_emitting, RID)
-	BIND1R(bool, particles_get_inactive, RID)
 	BIND2(particles_set_amount, RID, int)
 	BIND2(particles_set_lifetime, RID, float)
 	BIND2(particles_set_one_shot, RID, bool)
