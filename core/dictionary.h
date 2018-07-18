e7110984f3baad4670eac5bc1fbabb8b1ca7c26f
Ignacio Etcheverry <ignalfonsore@gmail.com>
2018-07-18 23:07:31 +0200

    Add Dictionary::erase_checked(key) method Same as erase, but it returns a boolean value indicating whether the pair was erased or not. This method should be removed during the next compatibility breakage, and 'Dictionary::erase(key)' should be changed to return a boolean.
    (cherry picked from commit 2f69e36cef8acca00ec5445f4aa8ec538bb38e3e)

--- core/dictionary.h
+++ core/dictionary.h
@@ -64,6 +64,7 @@ public:
 	bool has_all(const Array &p_keys) const;
 
 	void erase(const Variant &p_key);
+	bool erase_checked(const Variant &p_key);
 
 	bool operator==(const Dictionary &p_dictionary) const;
 
