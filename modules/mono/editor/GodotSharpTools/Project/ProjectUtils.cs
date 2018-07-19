604e7507f37328cdd08d4aa1bead58052101e423
Nufflee <zifcani@gmail.com>
2018-07-19 23:19:25 +0200

    Revert "Detect and update .csproj when C# files are added, moved or removed."
    This reverts commit 8fbce27ea8d699f99c234aad1e1ac29c7abfb5bb.

--- modules/mono/editor/GodotSharpTools/Project/ProjectUtils.cs
+++ modules/mono/editor/GodotSharpTools/Project/ProjectUtils.cs
@@ -10,7 +10,6 @@ namespace GodotSharpTools.Project
         {
             var dir = Directory.GetParent(projectPath).FullName;
             var root = ProjectRootElement.Open(projectPath);
-
             if (root.AddItemChecked(itemType, include.RelativeToPath(dir).Replace("/", "\\")))
                 root.Save();
         }
