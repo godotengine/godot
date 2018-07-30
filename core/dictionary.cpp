ec3fe5820bbe2f02f950a0f10e46fab700b25ef3
m <malcolm.g.howard@gmail.com>
2018-07-30 00:04:32 -0400

    Added Python-like .get() method to Dictionary in GDScript #20488
    Added .get() method to Dictionary class in GDScript to return the value if the key exists, or return Null if the key does not exist.

--- core/dictionary.cpp
+++ core/dictionary.cpp
@@ -114,7 +114,11 @@ Variant Dictionary::get_valid(const Variant &p_key) const {
 
 Variant Dictionary::get(const Variant &p_key) const {
 
-	return get_valid(p_key);
+	OrderedHashMap<Variant, Variant, VariantHasher, VariantComparator>::ConstElement E = ((const OrderedHashMap<Variant, Variant, VariantHasher, VariantComparator> *)&_p->variant_map)->find(p_key);
+
+	if (!E)
+		return Variant();
+	return E.get();
 }
 
 int Dictionary::size() const {
