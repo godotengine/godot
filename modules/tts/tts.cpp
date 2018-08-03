84fc523b6a7ad67c4192ca986c93b28e558e8d81
m <malcolm.g.howard@gmail.com>
2018-08-02 22:21:32 -0400

    Added Festival module to support speech synthesis (text-to-speech) within Godot to enhance accessibility. #14011

---
+++ modules/tts/tts.cpp
@@ -0,0 +1,47 @@
+/*************************************************************************/
+/*  tts.cpp                                                              */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2018 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2018 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#include "tts.h"
+#include "festival/src/include/festival.h"
+
+bool TTS::say_text(String txt) {
+
+    //convert Godot String to Godot CharString to C string
+    return festival_say_text(txt.ascii().get_data());
+}
+
+void TTS::_bind_methods() {
+
+    ClassDB::bind_method(D_METHOD("say_text", "txt"), &TTS::say_text);
+}
+
+TTS::TTS() {
+    festival_initialize(true, 210000); //not the best way to do it as this should only ever be called once.
+}
