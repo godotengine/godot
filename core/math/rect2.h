2eb8a9749ea005325e6918288a1b6f1be311eebf
Aaron Franke <arnfranke@yahoo.com>
2018-08-10 23:40:44 -0500

    [Core] Move Rect2 and Transform2D to their own files
    Math2D includes Transform2D, which includes Rect2, which includes Vector2.

--- core/math/rect2.h
+++ core/math/rect2.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  math_2d.h                                                            */
+/*  rect2.h                                                              */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,102 +28,75 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#ifndef MATH_2D_H
-#define MATH_2D_H
+#ifndef RECT2_H
+#define RECT2_H
 
-#include "transform_2d.h"
-/**
-	@author Juan Linietsky <reduzio@gmail.com>
-*/
-/* INTEGER STUFF */
+#include "vector2.h" // also includes math_funcs and ustring
 
-struct Point2i {
+struct Transform2D;
 
-	union {
-		int x;
-		int width;
-	};
-	union {
-		int y;
-		int height;
-	};
+struct Rect2 {
 
-	_FORCE_INLINE_ int &operator[](int p_idx) {
-		return p_idx ? y : x;
-	}
-	_FORCE_INLINE_ const int &operator[](int p_idx) const {
-		return p_idx ? y : x;
-	}
+	Point2 position;
+	Size2 size;
 
-	Point2i operator+(const Point2i &p_v) const;
-	void operator+=(const Point2i &p_v);
-	Point2i operator-(const Point2i &p_v) const;
-	void operator-=(const Point2i &p_v);
-	Point2i operator*(const Point2i &p_v1) const;
+	const Vector2 &get_position() const { return position; }
+	void set_position(const Vector2 &p_pos) { position = p_pos; }
+	const Vector2 &get_size() const { return size; }
+	void set_size(const Vector2 &p_size) { size = p_size; }
 
-	Point2i operator*(const int &rvalue) const;
-	void operator*=(const int &rvalue);
+	real_t get_area() const { return size.width * size.height; }
 
-	Point2i operator/(const Point2i &p_v1) const;
-
-	Point2i operator/(const int &rvalue) const;
-
-	void operator/=(const int &rvalue);
-
-	Point2i operator-() const;
-	bool operator<(const Point2i &p_vec2) const { return (x == p_vec2.x) ? (y < p_vec2.y) : (x < p_vec2.x); }
-	bool operator>(const Point2i &p_vec2) const { return (x == p_vec2.x) ? (y > p_vec2.y) : (x > p_vec2.x); }
-
-	bool operator==(const Point2i &p_vec2) const;
-	bool operator!=(const Point2i &p_vec2) const;
-
-	real_t get_aspect() const { return width / (real_t)height; }
-
-	operator String() const { return String::num(x) + ", " + String::num(y); }
-
-	operator Vector2() const { return Vector2(x, y); }
-	inline Point2i(const Vector2 &p_vec2) {
-		x = (int)p_vec2.x;
-		y = (int)p_vec2.y;
-	}
-	inline Point2i(int p_x, int p_y) {
-		x = p_x;
-		y = p_y;
-	}
-	inline Point2i() {
-		x = 0;
-		y = 0;
-	}
-};
-
-typedef Point2i Size2i;
-
-struct Rect2i {
-
-	Point2i position;
-	Size2i size;
-
-	const Point2i &get_position() const { return position; }
-	void set_position(const Point2i &p_pos) { position = p_pos; }
-	const Point2i &get_size() const { return size; }
-	void set_size(const Point2i &p_size) { size = p_size; }
-
-	int get_area() const { return size.width * size.height; }
-
-	inline bool intersects(const Rect2i &p_rect) const {
-		if (position.x > (p_rect.position.x + p_rect.size.width))
+	inline bool intersects(const Rect2 &p_rect) const {
+		if (position.x >= (p_rect.position.x + p_rect.size.width))
 			return false;
-		if ((position.x + size.width) < p_rect.position.x)
+		if ((position.x + size.width) <= p_rect.position.x)
 			return false;
-		if (position.y > (p_rect.position.y + p_rect.size.height))
+		if (position.y >= (p_rect.position.y + p_rect.size.height))
 			return false;
-		if ((position.y + size.height) < p_rect.position.y)
+		if ((position.y + size.height) <= p_rect.position.y)
 			return false;
 
 		return true;
 	}
 
-	inline bool encloses(const Rect2i &p_rect) const {
+	inline real_t distance_to(const Vector2 &p_point) const {
+
+		real_t dist = 0.0;
+		bool inside = true;
+
+		if (p_point.x < position.x) {
+			real_t d = position.x - p_point.x;
+			dist = inside ? d : MIN(dist, d);
+			inside = false;
+		}
+		if (p_point.y < position.y) {
+			real_t d = position.y - p_point.y;
+			dist = inside ? d : MIN(dist, d);
+			inside = false;
+		}
+		if (p_point.x >= (position.x + size.x)) {
+			real_t d = p_point.x - (position.x + size.x);
+			dist = inside ? d : MIN(dist, d);
+			inside = false;
+		}
+		if (p_point.y >= (position.y + size.y)) {
+			real_t d = p_point.y - (position.y + size.y);
+			dist = inside ? d : MIN(dist, d);
+			inside = false;
+		}
+
+		if (inside)
+			return 0;
+		else
+			return dist;
+	}
+
+	bool intersects_transformed(const Transform2D &p_xform, const Rect2 &p_rect) const;
+
+	bool intersects_segment(const Point2 &p_from, const Point2 &p_to, Point2 *r_pos = NULL, Point2 *r_normal = NULL) const;
+
+	inline bool encloses(const Rect2 &p_rect) const {
 
 		return (p_rect.position.x >= position.x) && (p_rect.position.y >= position.y) &&
 			   ((p_rect.position.x + p_rect.size.x) < (position.x + size.x)) &&
@@ -134,12 +107,12 @@ struct Rect2i {
 
 		return (size.x <= 0 || size.y <= 0);
 	}
-	inline Rect2i clip(const Rect2i &p_rect) const { /// return a clipped rect
+	inline Rect2 clip(const Rect2 &p_rect) const { /// return a clipped rect
 
-		Rect2i new_rect = p_rect;
+		Rect2 new_rect = p_rect;
 
 		if (!intersects(new_rect))
-			return Rect2i();
+			return Rect2();
 
 		new_rect.position.x = MAX(p_rect.position.x, position.x);
 		new_rect.position.y = MAX(p_rect.position.y, position.y);
@@ -147,15 +120,15 @@ struct Rect2i {
 		Point2 p_rect_end = p_rect.position + p_rect.size;
 		Point2 end = position + size;
 
-		new_rect.size.x = (int)(MIN(p_rect_end.x, end.x) - new_rect.position.x);
-		new_rect.size.y = (int)(MIN(p_rect_end.y, end.y) - new_rect.position.y);
+		new_rect.size.x = MIN(p_rect_end.x, end.x) - new_rect.position.x;
+		new_rect.size.y = MIN(p_rect_end.y, end.y) - new_rect.position.y;
 
 		return new_rect;
 	}
 
-	inline Rect2i merge(const Rect2i &p_rect) const { ///< return a merged rect
+	inline Rect2 merge(const Rect2 &p_rect) const { ///< return a merged rect
 
-		Rect2i new_rect;
+		Rect2 new_rect;
 
 		new_rect.position.x = MIN(p_rect.position.x, position.x);
 		new_rect.position.y = MIN(p_rect.position.y, position.y);
@@ -167,7 +140,7 @@ struct Rect2i {
 
 		return new_rect;
 	};
-	bool has_point(const Point2 &p_point) const {
+	inline bool has_point(const Point2 &p_point) const {
 		if (p_point.x < position.x)
 			return false;
 		if (p_point.y < position.y)
@@ -181,14 +154,14 @@ struct Rect2i {
 		return true;
 	}
 
-	bool no_area() { return (size.width <= 0 || size.height <= 0); }
+	inline bool no_area() const { return (size.width <= 0 || size.height <= 0); }
 
-	bool operator==(const Rect2i &p_rect) const { return position == p_rect.position && size == p_rect.size; }
-	bool operator!=(const Rect2i &p_rect) const { return position != p_rect.position || size != p_rect.size; }
+	bool operator==(const Rect2 &p_rect) const { return position == p_rect.position && size == p_rect.size; }
+	bool operator!=(const Rect2 &p_rect) const { return position != p_rect.position || size != p_rect.size; }
 
-	Rect2i grow(int p_by) const {
+	inline Rect2 grow(real_t p_by) const {
 
-		Rect2i g = *this;
+		Rect2 g = *this;
 		g.position.x -= p_by;
 		g.position.y -= p_by;
 		g.size.width += p_by * 2;
@@ -196,10 +169,37 @@ struct Rect2i {
 		return g;
 	}
 
-	inline void expand_to(const Point2i &p_vector) {
+	inline Rect2 grow_margin(Margin p_margin, real_t p_amount) const {
+		Rect2 g = *this;
+		g = g.grow_individual((MARGIN_LEFT == p_margin) ? p_amount : 0,
+				(MARGIN_TOP == p_margin) ? p_amount : 0,
+				(MARGIN_RIGHT == p_margin) ? p_amount : 0,
+				(MARGIN_BOTTOM == p_margin) ? p_amount : 0);
+		return g;
+	}
 
-		Point2i begin = position;
-		Point2i end = position + size;
+	inline Rect2 grow_individual(real_t p_left, real_t p_top, real_t p_right, real_t p_bottom) const {
+
+		Rect2 g = *this;
+		g.position.x -= p_left;
+		g.position.y -= p_top;
+		g.size.width += p_left + p_right;
+		g.size.height += p_top + p_bottom;
+
+		return g;
+	}
+
+	inline Rect2 expand(const Vector2 &p_vector) const {
+
+		Rect2 r = *this;
+		r.expand_to(p_vector);
+		return r;
+	}
+
+	inline void expand_to(const Vector2 &p_vector) { //in place function for speed
+
+		Vector2 begin = position;
+		Vector2 end = position + size;
 
 		if (p_vector.x < begin.x)
 			begin.x = p_vector.x;
@@ -215,22 +215,22 @@ struct Rect2i {
 		size = end - begin;
 	}
 
+	inline Rect2 abs() const {
+
+		return Rect2(Point2(position.x + MIN(size.x, 0), position.y + MIN(size.y, 0)), size.abs());
+	}
+
 	operator String() const { return String(position) + ", " + String(size); }
 
-	operator Rect2() const { return Rect2(position, size); }
-	Rect2i(const Rect2 &p_r2) :
-			position(p_r2.position),
-			size(p_r2.size) {
-	}
-	Rect2i() {}
-	Rect2i(int p_x, int p_y, int p_width, int p_height) :
+	Rect2() {}
+	Rect2(real_t p_x, real_t p_y, real_t p_width, real_t p_height) :
 			position(Point2(p_x, p_y)),
 			size(Size2(p_width, p_height)) {
 	}
-	Rect2i(const Point2 &p_pos, const Size2 &p_size) :
+	Rect2(const Point2 &p_pos, const Size2 &p_size) :
 			position(p_pos),
 			size(p_size) {
 	}
 };
 
-#endif
+#endif // RECT2_H
