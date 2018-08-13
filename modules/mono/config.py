f7aaf161fb392077b4ca8a48dd4b19d9d39ae5f4
Teashrock <kajitsu22@gmail.com>
2018-08-13 16:31:41 +0300

    Fixed absent variable exception while trying to raise another exception.
    (cherry picked from commits 51a0a3813577f20dd76a297e51d2b4445546a7d9
    and 695b8d254e508bb2023e21d7ffa99871c4095e94)

--- modules/mono/config.py
+++ modules/mono/config.py
@@ -82,6 +82,7 @@ def configure(env):
     mono_lib_names = ['mono-2.0-sgen', 'monosgen-2.0']
 
     if env['platform'] == 'windows':
+        mono_root = ''
         if bits == '32':
             if os.getenv('MONO32_PREFIX'):
                 mono_root = os.getenv('MONO32_PREFIX')
