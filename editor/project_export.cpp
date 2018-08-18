b3246f13d18cdd0a416d499963408736e1a37c41
Rémi Verschelde <rverschelde@gmail.com>
2018-08-18 15:24:31 +0200

    Revert "Port ImportDock and ProjectExport to new property editor"

--- editor/project_export.cpp
+++ editor/project_export.cpp
@@ -29,7 +29,6 @@
 /*************************************************************************/
 
 #include "project_export.h"
-
 #include "compressed_translation.h"
 #include "editor_data.h"
 #include "editor_node.h"
@@ -843,9 +842,10 @@ ProjectExportDialog::ProjectExportDialog() {
 	settings_vb->add_child(sections);
 	sections->set_v_size_flags(SIZE_EXPAND_FILL);
 
-	parameters = memnew(EditorInspector);
+	parameters = memnew(PropertyEditor);
 	sections->add_child(parameters);
 	parameters->set_name(TTR("Options"));
+	parameters->hide_top_label();
 	parameters->set_v_size_flags(SIZE_EXPAND_FILL);
 
 	parameters->connect("property_edited", this, "_update_parameters");
