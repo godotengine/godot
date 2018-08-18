475a46c59cdc1d5abcd3a9f7441a0689847df186
Hugo Locurcio <hugo.locurcio@hugo.pro>
2018-08-18 15:30:00 +0200

    Add more project window placement options
    It is now possible to use the previous or next monitor (relative to
    the editor) to display running projects. If either end is reached,
    it will wrap around to the last or first monitor (respectively).
    
    This closes #20283.

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -492,7 +492,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 
 	_initial_set("run/window_placement/rect", 1);
 	hints["run/window_placement/rect"] = PropertyInfo(Variant::INT, "run/window_placement/rect", PROPERTY_HINT_ENUM, "Top Left,Centered,Custom Position,Force Maximized,Force Fullscreen");
-	String screen_hints = TTR("Default (Same as Editor)");
+	String screen_hints = "Same as Editor,Previous Monitor,Next Monitor";
 	for (int i = 0; i < OS::get_singleton()->get_screen_count(); i++) {
 		screen_hints += ",Monitor " + itos(i + 1);
 	}
