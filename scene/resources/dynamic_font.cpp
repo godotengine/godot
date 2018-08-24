c399c41dfb065efab8d3b168ef0a1c5a5cf609d5
Juan Linietsky <reduzio@gmail.com>
2018-08-24 11:20:50 -0300

    Initalize font size to 16, closes #21165

--- scene/resources/dynamic_font.cpp
+++ scene/resources/dynamic_font.cpp
@@ -1034,6 +1034,7 @@ SelfList<DynamicFont>::List DynamicFont::dynamic_fonts;
 DynamicFont::DynamicFont() :
 		font_list(this) {
 
+	cache_id.size = 16;
 	spacing_top = 0;
 	spacing_bottom = 0;
 	spacing_char = 0;
