f6fbf841d50d85f49fa127afdb29ebf23fe5974d
Juan Linietsky <reduzio@gmail.com>
2018-09-06 13:32:34 -0300

    Show a warning if you try to grab focus when not allowed, closes #15388

--- scene/gui/control.cpp
+++ scene/gui/control.cpp
@@ -2062,8 +2062,11 @@ void Control::grab_focus() {
 	if (!is_inside_tree()) {
 		ERR_FAIL_COND(!is_inside_tree());
 	}
-	if (data.focus_mode == FOCUS_NONE)
+
+	if (data.focus_mode == FOCUS_NONE) {
+		WARN_PRINT("This control can't grab focus. Use set_focus_mode() to allow a control to get focus.");
 		return;
+	}
 
 	get_viewport()->_gui_control_grab_focus(this);
 }
