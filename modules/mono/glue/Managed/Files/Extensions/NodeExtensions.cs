b84992e37454594a01fd2deec807edcb40d70679
zochris <20927697+zochris@users.noreply.github.com>
2018-10-05 15:06:05 +0200

    loosen generic guard on Node extension methods
    For more details see #22658

--- modules/mono/glue/Managed/Files/Extensions/NodeExtensions.cs
+++ modules/mono/glue/Managed/Files/Extensions/NodeExtensions.cs
@@ -2,42 +2,42 @@ namespace Godot
 {
     public partial class Node
     {
-        public T GetNode<T>(NodePath path) where T : Godot.Node
+        public T GetNode<T>(NodePath path) where T : class
         {
             return (T)GetNode(path);
         }
 
-        public T GetNodeOrNull<T>(NodePath path) where T : Godot.Node
+        public T GetNodeOrNull<T>(NodePath path) where T : class
         {
             return GetNode(path) as T;
         }
 
-        public T GetChild<T>(int idx) where T : Godot.Node
+        public T GetChild<T>(int idx) where T : class
         {
             return (T)GetChild(idx);
         }
 
-        public T GetChildOrNull<T>(int idx) where T : Godot.Node
+        public T GetChildOrNull<T>(int idx) where T : class
         {
             return GetChild(idx) as T;
         }
 
-        public T GetOwner<T>() where T : Godot.Node
+        public T GetOwner<T>() where T : class
         {
             return (T)GetOwner();
         }
 
-        public T GetOwnerOrNull<T>() where T : Godot.Node
+        public T GetOwnerOrNull<T>() where T : class
         {
             return GetOwner() as T;
         }
 
-        public T GetParent<T>() where T : Godot.Node
+        public T GetParent<T>() where T : class
         {
             return (T)GetParent();
         }
 
-        public T GetParentOrNull<T>() where T : Godot.Node
+        public T GetParentOrNull<T>() where T : class
         {
             return GetParent() as T;
         }
