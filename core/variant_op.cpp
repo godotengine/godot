bbfc6f698511dfdfa44e61aa88bd5678dda06bd8
qonnop <qonnop@users.noreply.github.com>
2018-10-06 07:05:17 +0200

    Fixed int interpolation issue, closes #22763
    When interpolating between two equal int values a and b, floating point
    calculation imprecisions can result in different values depending on
    the interpolation factor.

--- core/variant_op.cpp
+++ core/variant_op.cpp
@@ -3542,7 +3542,10 @@ void Variant::interpolate(const Variant &a, const Variant &b, float c, Variant &
 		case INT: {
 			int64_t va = a._data._int;
 			int64_t vb = b._data._int;
-			r_dst = int((1.0 - c) * va + vb * c);
+			if (va != vb)
+				r_dst = int((1.0 - c) * va + vb * c);
+			else //avoid int casting issues
+				r_dst = a;
 		}
 			return;
 		case REAL: {
