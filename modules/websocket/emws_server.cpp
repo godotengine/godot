e6a069119b2a4721808c7ad39c58a85ea094dfd3
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2018-10-23 12:49:24 +0200

    Add missing EMWSServer::poll function.
    Empty of course as server is not supported in HTML5, but the symbol must
    be defined.

--- modules/websocket/emws_server.cpp
+++ modules/websocket/emws_server.cpp
@@ -71,6 +71,9 @@ int EMWSServer::get_peer_port(int p_peer_id) const {
 void EMWSServer::disconnect_peer(int p_peer_id, int p_code, String p_reason) {
 }
 
+void EMWSServer::poll() {
+}
+
 EMWSServer::EMWSServer() {
 }
 
