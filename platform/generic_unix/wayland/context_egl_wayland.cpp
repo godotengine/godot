a7f144b97dcef185f4917a0c005580c99df938a1
toger5 <toger5@hotmail.de>
2018-10-31 20:41:56 +0100

    add wayland context

---
+++ platform/generic_unix/wayland/context_egl_wayland.cpp
@@ -0,0 +1,212 @@
+/*************************************************************************/
+/*  context_egl_wayland.cpp                                              */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2018 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2018 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+//#if defined(OPENGL_ENABLED)
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include <wayland-client-protocol.h>
+#include <wayland-client.h>
+#include <wayland-egl.h> // Wayland EGL MUST be included before EGL headers
+#include <wayland-server.h>
+
+#include "context_egl_wayland.h" // includes the egl headers
+
+void ContextGL_EGL::release_current() {
+
+	eglMakeCurrent(egl_display, EGL_NO_SURFACE, EGL_NO_SURFACE, egl_context);
+}
+
+void ContextGL_EGL::make_current() {
+
+	eglMakeCurrent(egl_display, egl_surface, egl_surface, egl_context);
+}
+
+void ContextGL_EGL::swap_buffers() {
+	if (eglSwapBuffers(egl_display, egl_surface) != EGL_TRUE) {
+		cleanup();
+
+		initialize();
+
+		// tell rasterizer to reload textures and stuff?
+	}
+}
+
+Error ContextGL_EGL::initialize() {
+	print_verbose("start create egl context!");
+	eglBindAPI(EGL_OPENGL_API);
+	EGLint configAttribList[] = {
+		EGL_RED_SIZE, 8,
+		EGL_GREEN_SIZE, 8,
+		EGL_BLUE_SIZE, 8,
+		// EGL_DEPTH_SIZE, 8,
+		// EGL_STENCIL_SIZE, 8,
+		// EGL_SAMPLE_BUFFERS, 0,
+		EGL_NONE
+	};
+	EGLint surfaceAttribList[] = {
+		EGL_NONE, EGL_NONE
+	};
+
+	EGLint numConfigs = 8;
+	EGLint majorVersion = 1;
+	EGLint minorVersion;
+	if (context_type == GLES_2_0) {
+		minorVersion = 0;
+	} else {
+		minorVersion = 5;
+	}
+	egl_display = EGL_NO_DISPLAY;
+	egl_context = EGL_NO_CONTEXT;
+	egl_surface = EGL_NO_SURFACE;
+	EGLConfig config = nullptr;
+
+	EGLint contextAttribs[3];
+	// if (context_type == GLES_2_0) {
+	// 	contextAttribs[0] = EGL_CONTEXT_MAJOR_VERSION;
+	// 	contextAttribs[1] = 3;
+	// 	contextAttribs[0] = EGL_CONTEXT_MINOR_VERSION;
+	// 	contextAttribs[1] = 3;
+	// 	contextAttribs[4] = EGL_NONE;
+	// } else {
+	// 	contextAttribs[0] = EGL_CONTEXT_MAJOR_VERSION;
+	// 	contextAttribs[1] = 3;
+	// 	contextAttribs[0] = EGL_CONTEXT_MINOR_VERSION;
+	// 	contextAttribs[1] = 3;
+	// 	contextAttribs[4] = EGL_NONE;
+	// }
+	contextAttribs[0] = EGL_CONTEXT_CLIENT_VERSION;
+	contextAttribs[1] = 4;
+	contextAttribs[2] = EGL_NONE;
+
+	EGLDisplay display = eglGetDisplay(native_display);
+	EGLSurface surface;
+	EGLContext context;
+
+	if (display == EGL_NO_DISPLAY) {
+		print_verbose("No EGL Display...\n");
+		return FAILED;
+	}
+
+	// Initialize EGL
+	if (!eglInitialize(display, &majorVersion, &minorVersion)) {
+		print_verbose("No Initialisation...\n");
+		return FAILED;
+	}
+
+	// Choose config
+	if ((eglChooseConfig(display, configAttribList, &config, 1, &numConfigs) != EGL_TRUE) || (numConfigs != 1)) {
+		print_verbose("No configuration...\n");
+		return FAILED;
+	}
+
+	// Create a surface
+	surface = eglCreateWindowSurface(display, config, native_window, surfaceAttribList);
+	if (surface == EGL_NO_SURFACE) {
+		print_verbose("No surface...\n");
+		return FAILED;
+	}
+	print_verbose("egl surface created!");
+	// Create a GL context
+
+	context = eglCreateContext(display, config, EGL_NO_CONTEXT, contextAttribs);
+	if (context == EGL_NO_CONTEXT) {
+		print_verbose("No context...\n");
+		return FAILED;
+	}
+	print_verbose("egl context !!!!");
+	// Make the context current
+	if (!eglMakeCurrent(display, surface, surface, context)) {
+		print_verbose("Could not make the current window current !\n");
+		return FAILED;
+	}
+	print_verbose("current context exists !");
+	egl_display = display;
+	egl_surface = surface;
+	egl_context = context;
+
+	eglQuerySurface(display, surface, EGL_WIDTH, &width);
+	eglQuerySurface(display, surface, EGL_HEIGHT, &height);
+
+	return OK;
+}
+
+int ContextGL_EGL::get_window_width() {
+
+	return width;
+}
+
+int ContextGL_EGL::get_window_height() {
+
+	return height;
+}
+
+void ContextGL_EGL::set_use_vsync(bool p_use) {
+	use_vsync = p_use;
+}
+bool ContextGL_EGL::is_using_vsync() const {
+
+	return use_vsync;
+}
+
+void ContextGL_EGL::cleanup() {
+
+	if (egl_display != EGL_NO_DISPLAY && egl_surface != EGL_NO_SURFACE) {
+		eglDestroySurface(egl_display, egl_surface);
+		egl_surface = EGL_NO_SURFACE;
+	}
+
+	if (egl_display != EGL_NO_DISPLAY && egl_context != EGL_NO_CONTEXT) {
+		eglDestroyContext(egl_display, egl_context);
+		egl_context = EGL_NO_CONTEXT;
+	}
+
+	if (egl_display != EGL_NO_DISPLAY) {
+		eglTerminate(egl_display);
+		egl_display = EGL_NO_DISPLAY;
+	}
+};
+ContextGL_EGL::ContextGL_EGL(EGLNativeDisplayType p_egl_display, EGLNativeWindowType &p_egl_window, const OS::VideoMode &p_default_video_mode, Driver p_context_type) {
+
+	default_video_mode = p_default_video_mode;
+
+	context_type = p_context_type;
+
+	double_buffer = false;
+	direct_render = false;
+	native_display = p_egl_display;
+	native_window = p_egl_window;
+	use_vsync = false;
+}
+
+ContextGL_EGL::~ContextGL_EGL() {
+	cleanup();
+}
---
+++ platform/generic_unix/wayland/context_egl_wayland.h
@@ -0,0 +1,96 @@
+/*************************************************************************/
+/*  context_egl_wayland.h                                                */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2018 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2018 Godot Engine contributors (cf. AUTHORS.md)    */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+/**
+	@author Timo Kandra <toger5@hotmail.de>
+*/
+#ifndef CONTEXT_EGL_H
+#define CONTEXT_EGL_H
+
+//#ifdef X11_ENABLED
+
+//#if defined(OPENGL_ENABLED)
+
+#include "core/os/os.h"
+#include "drivers/gl_context/context_gl.h"
+
+// #include <GLES/egl.h>
+#include <EGL/egl.h>
+#include <EGL/eglplatform.h>
+
+class ContextGL_EGL : public ContextGL {
+
+public:
+	enum Driver {
+		GLES_2_0,
+		GLES_3_0
+	};
+
+private:
+	OS::VideoMode default_video_mode;
+	Driver context_type;
+
+	EGLint width;
+	EGLint height;
+
+	/// Native System informations
+	EGLNativeDisplayType native_display;
+	EGLNativeWindowType native_window;
+
+	/// EGL surface
+	EGLSurface egl_surface;
+	/// EGL display
+	EGLDisplay egl_display;
+	/// EGL context
+	EGLContext egl_context;
+
+	//uint16_t window_width, window_height;
+	bool double_buffer;
+	bool direct_render;
+	// int egl_minor, egl_major;
+	bool use_vsync;
+
+public:
+	virtual void release_current();
+	virtual void make_current();
+	virtual void swap_buffers();
+	virtual int get_window_width();
+	virtual int get_window_height();
+
+	virtual Error initialize();
+
+	virtual void set_use_vsync(bool p_use);
+	virtual bool is_using_vsync() const;
+	void cleanup();
+	ContextGL_EGL(EGLNativeDisplayType p_egl_display, EGLNativeWindowType &p_egl_window, const OS::VideoMode &p_default_video_mode, Driver p_context_type);
+	virtual ~ContextGL_EGL();
+};
+
+#endif
