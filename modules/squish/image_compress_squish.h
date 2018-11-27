32751e09ac1313baa4d22a4098c8dd03ba6a1741
Leon Krause <lk@leonkrause.com>
2018-11-27 18:14:54 +0100

    Build squish decompression in runtime

--- modules/squish/image_compress_squish.h
+++ modules/squish/image_compress_squish.h
@@ -33,7 +33,9 @@
 
 #include "core/image.h"
 
+#ifdef TOOLS_ENABLED
 void image_compress_squish(Image *p_image, float p_lossy_quality, Image::CompressSource p_source);
+#endif
 void image_decompress_squish(Image *p_image);
 
 #endif // IMAGE_COMPRESS_SQUISH_H
