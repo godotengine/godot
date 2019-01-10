godotengine/godot

commit cd0b82fd56bdba5a4f1a057fd2d50854c1f503ef

Mark AnimatedTexture frame_* properties as internal
This way they no longer appear in the documentation, and the related
setters and getters do.

scene/resources/texture.cpp
@@ -30,11 +30,11 @@
 
 #include "texture.h"
 
-#include "bit_mask.h"
 #include "core/core_string_names.h"
 #include "core/io/image_loader.h"
 #include "core/method_bind_ext.gen.inc"
 #include "core/os/os.h"
+#include "scene/resources/bit_mask.h"
 
 Size2 Texture::get_size() const {
 
@@ -1945,8 +1945,8 @@ void AnimatedTexture::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "fps", PROPERTY_HINT_RANGE, "0,1024,0.1"), "set_fps", "get_fps");
 
 	for (int i = 0; i < MAX_FRAMES; i++) {
-		ADD_PROPERTYI(PropertyInfo(Variant::OBJECT, "frame_" + itos(i) + "/texture", PROPERTY_HINT_RESOURCE_TYPE, "Texture"), "set_frame_texture", "get_frame_texture", i);
-		ADD_PROPERTYI(PropertyInfo(Variant::REAL, "frame_" + itos(i) + "/delay_sec", PROPERTY_HINT_RANGE, "0.0,16.0,0.01"), "set_frame_delay", "get_frame_delay", i);
+		ADD_PROPERTYI(PropertyInfo(Variant::OBJECT, "frame_" + itos(i) + "/texture", PROPERTY_HINT_RESOURCE_TYPE, "Texture", PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL), "set_frame_texture", "get_frame_texture", i);
+		ADD_PROPERTYI(PropertyInfo(Variant::REAL, "frame_" + itos(i) + "/delay_sec", PROPERTY_HINT_RANGE, "0.0,16.0,0.01", PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL), "set_frame_delay", "get_frame_delay", i);
 	}
 }
 

doc/classes/StreamPeerTCP.xml
@@ -43,7 +43,7 @@
 				Return the port of this peer.
 			</description>
 		</method>
-		<method name="get_status" qualifiers="const">
+		<method name="get_status">
 			<return type="int" enum="StreamPeerTCP.Status">
 			</return>
 			<description>

scene/resources/texture.h
@@ -37,9 +37,10 @@
 #include "core/os/rw_lock.h"
 #include "core/os/thread_safe.h"
 #include "core/resource.h"
-#include "curve.h"
 #include "scene/resources/color_ramp.h"
+#include "scene/resources/curve.h"
 #include "servers/visual_server.h"
+
 /**
 	@author Juan Linietsky <reduzio@gmail.com>
 */

commit 11c09a17c24f8495be25967c57c908d66c1bb562

Fix showing AnimatedTexture frame_* properties in editor
Regression from cd0b82fd56bdba5a4f1a057fd2d50854c1f503ef, it must be hidden from docs
(PROPERTY_USAGE_INTERNAL) but not from the editor.

Supersedes and fixes #24880.

Co-authored-by: QbieShay <cislaghi.ilaria@gmail.com>

scene/resources/texture.cpp
@@ -1975,8 +1975,8 @@ void AnimatedTexture::_bind_methods() {
 	ADD_PROPERTY(PropertyInfo(Variant::REAL, "fps", PROPERTY_HINT_RANGE, "0,1024,0.1"), "set_fps", "get_fps");
 
 	for (int i = 0; i < MAX_FRAMES; i++) {
-		ADD_PROPERTYI(PropertyInfo(Variant::OBJECT, "frame_" + itos(i) + "/texture", PROPERTY_HINT_RESOURCE_TYPE, "Texture", PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL), "set_frame_texture", "get_frame_texture", i);
-		ADD_PROPERTYI(PropertyInfo(Variant::REAL, "frame_" + itos(i) + "/delay_sec", PROPERTY_HINT_RANGE, "0.0,16.0,0.01", PROPERTY_USAGE_NOEDITOR | PROPERTY_USAGE_INTERNAL), "set_frame_delay", "get_frame_delay", i);
+		ADD_PROPERTYI(PropertyInfo(Variant::OBJECT, "frame_" + itos(i) + "/texture", PROPERTY_HINT_RESOURCE_TYPE, "Texture", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_INTERNAL), "set_frame_texture", "get_frame_texture", i);
+		ADD_PROPERTYI(PropertyInfo(Variant::REAL, "frame_" + itos(i) + "/delay_sec", PROPERTY_HINT_RANGE, "0.0,16.0,0.01", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_INTERNAL), "set_frame_delay", "get_frame_delay", i);
 	}
 }
 