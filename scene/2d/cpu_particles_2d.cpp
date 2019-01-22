b482c031dd641cea13b91816d6f37cea52e0f703
clayjohn <claynjohn@gmail.com>
2019-01-21 21:00:00 -0800

    properly initialize angular_velocity in cpuparticles2d

--- scene/2d/cpu_particles_2d.cpp
+++ scene/2d/cpu_particles_2d.cpp
@@ -1378,6 +1378,7 @@ CPUParticles2D::CPUParticles2D() {
 	set_param(PARAM_INITIAL_LINEAR_VELOCITY, 1);
 	//set_param(PARAM_ORBIT_VELOCITY, 0);
 	set_param(PARAM_LINEAR_ACCEL, 0);
+	set_param(PARAM_ANGULAR_VELOCITY, 0);
 	set_param(PARAM_RADIAL_ACCEL, 0);
 	set_param(PARAM_TANGENTIAL_ACCEL, 0);
 	set_param(PARAM_DAMPING, 0);
