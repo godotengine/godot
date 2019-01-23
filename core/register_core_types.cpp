a729dd2197883675b4cf051009f3536eac9b90f3
Marcelo Fernandez <marcelofg55@gmail.com>
2019-01-23 09:09:03 -0300

    Expose InputEventMIDI to scripts

--- core/register_core_types.cpp
+++ core/register_core_types.cpp
@@ -142,6 +142,7 @@ void register_core_types() {
 	ClassDB::register_virtual_class<InputEventGesture>();
 	ClassDB::register_class<InputEventMagnifyGesture>();
 	ClassDB::register_class<InputEventPanGesture>();
+	ClassDB::register_class<InputEventMIDI>();
 
 	ClassDB::register_class<FuncRef>();
 	ClassDB::register_virtual_class<StreamPeer>();
