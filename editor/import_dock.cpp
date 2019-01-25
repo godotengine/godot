065907b732e3b60714df01012d2c50be1320295c
Juan Linietsky <reduzio@gmail.com>
2019-01-25 17:27:50 -0300

    Stop preview generator before reimporting resources with different type.

--- editor/import_dock.cpp
+++ editor/import_dock.cpp
@@ -424,6 +424,7 @@ void ImportDock::_reimport_attempt() {
 void ImportDock::_reimport_and_restart() {
 
 	EditorNode::get_singleton()->save_all_scenes();
+	EditorResourcePreview::get_singleton()->stop(); //dont try to re-create previews
 	_reimport();
 	EditorNode::get_singleton()->restart_editor();
 }
