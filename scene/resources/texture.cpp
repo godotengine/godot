c2541188f21926d8cc4163d1996d3cb9fab1aa0b
Rémi Verschelde <rverschelde@gmail.com>
2019-01-28 14:56:06 +0100

    Fix recursive assignment of Textures and BitMapFont
    Fixes #24213.
    
    (cherry picked from commit 402cfa983d1773b72cc69c63a9e3bccff285d0a4)

--- scene/resources/texture.cpp
+++ scene/resources/texture.cpp
@@ -836,6 +836,7 @@ uint32_t AtlasTexture::get_flags() const {
 
 void AtlasTexture::set_atlas(const Ref<Texture> &p_atlas) {
 
+	ERR_FAIL_COND(p_atlas == this);
 	if (atlas == p_atlas)
 		return;
 	atlas = p_atlas;
@@ -1074,6 +1075,7 @@ void LargeTexture::set_piece_offset(int p_idx, const Point2 &p_offset) {
 
 void LargeTexture::set_piece_texture(int p_idx, const Ref<Texture> &p_texture) {
 
+	ERR_FAIL_COND(p_texture == this);
 	ERR_FAIL_INDEX(p_idx, pieces.size());
 	pieces[p_idx].texture = p_texture;
 };
@@ -1619,6 +1621,7 @@ void ProxyTexture::_bind_methods() {
 
 void ProxyTexture::set_base(const Ref<Texture> &p_texture) {
 
+	ERR_FAIL_COND(p_texture == this);
 	base = p_texture;
 	if (base.is_valid()) {
 		VS::get_singleton()->texture_set_proxy(proxy, base->get_rid());
