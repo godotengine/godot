db6d4352ea528e925c69ee94f6fb9bc0942719ec
bruvzg <7645683+bruvzg@users.noreply.github.com>
2019-02-06 15:57:06 +0200

    [macOS] Add methods to modify global and dock menus. Add ability to open multiple editor/project manager instances, recent/favourite project list to project manager dock menu and opened scene list to editor dock menu.

--- core/os/main_loop.h
+++ core/os/main_loop.h
@@ -71,6 +71,7 @@ public:
 	virtual void finish();
 
 	virtual void drop_files(const Vector<String> &p_files, int p_from_screen = 0);
+	virtual void global_menu_action(const Variant &p_id, const Variant &p_meta);
 
 	void set_init_script(const Ref<Script> &p_init_script);
 
