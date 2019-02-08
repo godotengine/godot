cf236cbd4e9a02fedc5110302cec150a9434ad62
qarmin <mikrutrafal54@gmail.com>
2019-02-08 23:59:51 +0100

    Clearch SearchBox when unclick SearchButton

--- editor/project_settings_editor.cpp
+++ editor/project_settings_editor.cpp
@@ -1592,6 +1592,7 @@ void ProjectSettingsEditor::_toggle_search_bar(bool p_pressed) {
 		search_box->select_all();
 	} else {
 
+		search_box->clear();
 		search_bar->hide();
 		add_prop_bar->show();
 	}
