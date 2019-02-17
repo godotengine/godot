fb4d1fb95edd6b9db333a9a52c3b893d2214607b
Marcelo Fernandez <marcelofg55@gmail.com>
2019-02-17 11:20:10 -0300

    Fix AudioStreams::stop possibly causing a small noise

--- scene/3d/audio_stream_player_3d.h
+++ scene/3d/audio_stream_player_3d.h
@@ -109,8 +109,9 @@ private:
 	float pitch_scale;
 	bool autoplay;
 	bool stream_paused;
-	bool stream_paused_fade_in;
-	bool stream_paused_fade_out;
+	bool stream_fade_in;
+	bool stream_fade_out;
+	bool stream_stop;
 	StringName bus;
 
 	void _mix_audio();
