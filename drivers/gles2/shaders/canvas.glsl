133a7ea1eb3acbb1c201ef070d13dfa587f2ab05
JFonS <joan.fonssanchez@gmail.com>
2019-02-21 12:16:10 +0100

    Revert back to ignoring LIGHT_VEC for 2D shadows

--- drivers/gles2/shaders/canvas.glsl
+++ drivers/gles2/shaders/canvas.glsl
@@ -440,6 +440,10 @@ FRAGMENT_SHADER_CODE
 		color *= light;
 
 #ifdef USE_SHADOWS
+		// Reset light_vec to compute shadows, the shadow map is created from the light origin, so it only
+		// makes sense to compute shadows from there.
+		light_vec = light_uv_interp.zw;
+
 		float angle_to_light = -atan(light_vec.x, light_vec.y);
 		float PI = 3.14159265358979323846264;
 		/*int i = int(mod(floor((angle_to_light+7.0*PI/6.0)/(4.0*PI/6.0))+1.0, 3.0)); // +1 pq os indices estao em ordem 2,0,1 nos arrays
