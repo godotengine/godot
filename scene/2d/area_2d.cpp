8c2801d1257783d4c9587cbfe16d0252840d3e75
Juan Linietsky <juan@godotengine.org>
2019-02-22 09:04:59 -0300

    Check that area is inside tree besides checking that queries are flushed, fixes #26148

--- scene/2d/area_2d.cpp
+++ scene/2d/area_2d.cpp
@@ -426,7 +426,7 @@ bool Area2D::is_monitoring() const {
 
 void Area2D::set_monitorable(bool p_enable) {
 
-	if (locked || Physics2DServer::get_singleton()->is_flushing_queries()) {
+	if (locked || (is_inside_tree() && Physics2DServer::get_singleton()->is_flushing_queries())) {
 		ERR_EXPLAIN("Function blocked during in/out signal. Use set_deferred(\"monitorable\",true/false)");
 	}
 	ERR_FAIL_COND(locked || Physics2DServer::get_singleton()->is_flushing_queries());
