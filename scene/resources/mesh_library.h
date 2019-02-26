8c412a1a62dd972bc3ea9a821f04aae32ba6e0a9
Bojidar Marinov <bojidar.marinov.bg@gmail.com>
2019-02-26 15:38:23 +0200

    Consider StaticBody and NavMesh position when converting to a MeshLibrary
    Would have added mesh transform, but realized that will cause problems when moving the mesh around the scene for visualization purposes.
    
    Closes #11722

--- scene/resources/mesh_library.h
+++ scene/resources/mesh_library.h
@@ -52,6 +52,7 @@ public:
 		Ref<Mesh> mesh;
 		Vector<ShapeData> shapes;
 		Ref<Texture> preview;
+		Transform navmesh_transform;
 		Ref<NavigationMesh> navmesh;
 	};
 
@@ -72,11 +73,13 @@ public:
 	void set_item_name(int p_item, const String &p_name);
 	void set_item_mesh(int p_item, const Ref<Mesh> &p_mesh);
 	void set_item_navmesh(int p_item, const Ref<NavigationMesh> &p_navmesh);
+	void set_item_navmesh_transform(int p_item, const Transform &p_transform);
 	void set_item_shapes(int p_item, const Vector<ShapeData> &p_shapes);
 	void set_item_preview(int p_item, const Ref<Texture> &p_preview);
 	String get_item_name(int p_item) const;
 	Ref<Mesh> get_item_mesh(int p_item) const;
 	Ref<NavigationMesh> get_item_navmesh(int p_item) const;
+	Transform get_item_navmesh_transform(int p_item) const;
 	Vector<ShapeData> get_item_shapes(int p_item) const;
 	Ref<Texture> get_item_preview(int p_item) const;
 
