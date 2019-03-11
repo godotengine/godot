21e4d02b9346723b0bff2eca92c1bbb1a3b03085
Rémi Verschelde <rverschelde@gmail.com>
2019-03-11 23:17:54 +0100

    GLES2: Add comments around EXT_shader_texture_lod check
    To avoid reintroducing bugs as I did in #26928 and #26932.
    
    texture2DLodEXT and textureCubeLodEXT are only for the fragment shader with
    https://www.khronos.org/registry/OpenGL/extensions/EXT/EXT_shader_texture_lod.txt
    In the vertex shader, texture2DLod and textureCubeLod are built-in.

--- drivers/gles2/shaders/cubemap_filter.glsl
+++ drivers/gles2/shaders/cubemap_filter.glsl
@@ -25,15 +25,15 @@ void main() {
 /* clang-format off */
 [fragment]
 
+// texture2DLodEXT and textureCubeLodEXT are fragment shader specific.
+// Do not copy these defines in the [vertex] section.
 #ifndef USE_GLES_OVER_GL
-
 #ifdef GL_EXT_shader_texture_lod
 #extension GL_EXT_shader_texture_lod : enable
 #define texture2DLod(img, coord, lod) texture2DLodEXT(img, coord, lod)
 #define textureCubeLod(img, coord, lod) textureCubeLodEXT(img, coord, lod)
 #endif
-
-#endif
+#endif // !USE_GLES_OVER_GL
 
 #ifdef GL_ARB_shader_texture_lod
 #extension GL_ARB_shader_texture_lod : enable
@@ -43,8 +43,6 @@ void main() {
 #define texture2DLod(img, coord, lod) texture2D(img, coord, lod)
 #define textureCubeLod(img, coord, lod) textureCube(img, coord, lod)
 #endif
-
-
 
 #ifdef USE_GLES_OVER_GL
 #define lowp
