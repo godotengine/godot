780e441a405455e9a272f37c089965abefa00beb
Yeryomin1 <48363433+Yeryomin1@users.noreply.github.com>
2019-03-17 13:55:24 +0300

    Improving of the code readability
    Revision of the pull request #27144 for compliance with the style rules. Comment: https://github.com/godotengine/godot/pull/27144#issuecomment-473606776
    I'm using clang-format for the first time. I hope the new version meets the requirements.

--- core/math/rect2.cpp
+++ core/math/rect2.cpp
@@ -103,29 +103,28 @@ bool Rect2::intersects_transformed(const Transform2D &p_xform, const Rect2 &p_re
 
 	//base rect2 first (faster)
 
-	if ( (xf_points[0].y <= position.y) && (xf_points[1].y <= position.y) 
-            && (xf_points[2].y <= position.y) && (xf_points[3].y <= position.y) )
-            return false;	    
-        
-        low_limit = position.y + size.y;  
-  
-	if ( (xf_points[0].y >= low_limit) && (xf_points[1].y >= low_limit)
-	    && (xf_points[2].y >= low_limit) && (xf_points[3].y >= low_limit) )
-            return false;
-	
-	if ( (xf_points[0].x <= position.x) && (xf_points[1].x <= position.x) 
-            && (xf_points[2].x <= position.x) && (xf_points[3].x <= position.x) )
-            return false;
+	if ((xf_points[0].y <= position.y) && (xf_points[1].y <= position.y) &&
+	(xf_points[2].y <= position.y) && (xf_points[3].y <= position.y))
+	return false;
+
+	low_limit = position.y + size.y;
+
+	if ((xf_points[0].y >= low_limit) && (xf_points[1].y >= low_limit) &&
+	(xf_points[2].y >= low_limit) && (xf_points[3].y >= low_limit))
+	return false;
+
+	if ((xf_points[0].x <= position.x) && (xf_points[1].x <= position.x) &&
+	(xf_points[2].x <= position.x) && (xf_points[3].x <= position.x))
+	return false;
 
 	low_limit = position.x + size.x;
-	
-	if ( (xf_points[0].x >= low_limit) && (xf_points[1].x >= low_limit)
-            && (xf_points[2].x >= low_limit) && (xf_points[3].x >= low_limit) )
-        	return false;
+
+	if ((xf_points[0].x >= low_limit) && (xf_points[1].x >= low_limit) &&
+	(xf_points[2].x >= low_limit) && (xf_points[3].x >= low_limit))
+	return false;
 
 	Vector2 xf_points2[4] = {
-		position,
-		Vector2(position.x + size.x, position.y),
+		position, Vector2(position.x + size.x, position.y),
 		Vector2(position.x, position.y + size.y),
 		Vector2(position.x + size.x, position.y + size.y),
 	};
