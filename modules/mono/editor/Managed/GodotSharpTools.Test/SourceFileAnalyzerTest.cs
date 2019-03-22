9346b5325b4e3e813d41cda8efd7b81ac199359d
Sebastian Hartte <sebastian@hartte.de>
2019-03-22 01:40:19 +0100

    Moved GodotSharpTools managed project and added tests. Migrated projects to new .NET SDK style and added code scanning based on the C# Roslyn compiler.

---
+++ modules/mono/editor/Managed/GodotSharpTools.Test/SourceFileAnalyzerTest.cs
@@ -0,0 +1,63 @@
+using System;
+using GodotSharpTools.Build;
+using NUnit.Framework;
+
+namespace GodotSharpToolsTest
+{
+    public class SourceFileAnalyzerTest
+    {
+
+        [Test]
+        public void TestPlainClasses()
+        {
+
+            var code = @"class MyClass : System.Object {}";
+            Assert.AreEqual("MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "IgnoreDir/MyClass.cs"));
+            
+            code = @"namespace ABC { class MyClass : System.Object {} }";
+            Assert.AreEqual("ABC.MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+
+            code = @"namespace ABC.QWD { class MyClass : System.Object {} }";
+            Assert.AreEqual("ABC.QWD.MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+
+            code = @"namespace ABC.QWD { namespace XXX { class MyClass : System.Object {} } }";
+            Assert.AreEqual("ABC.QWD.XXX.MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+
+        }
+
+        [Test]
+        public void TestSkipGenericClasses()
+        {
+            var code = @"class MyClass<T> : System.Object {}";
+            Assert.IsNull(SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+        }
+
+        [Test]
+        public void TestIncludeOnlyClassesMatchingFilename()
+        {
+            var code = @"class OtherClass : System.Object {} class MyClass : System.Object {}";
+            Assert.AreEqual("MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+        }
+
+        [Test]
+        public void TestFailOnAmbiguity()
+        {
+            var e = Assert.Throws<ArgumentException>(() =>
+            {
+                var code = @"class MyClass : System.Object {} namespace X { class MyClass : System.Object {} }";
+                SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs");
+            });
+
+            Assert.AreEqual(e.Message, "Source file 'MyClass.cs' contains multiple top level classes: MyClass, X.MyClass");
+
+        }
+
+        [Test]
+        public void TestSyntaxErrors()
+        {
+            var code = "class MyClass : System.Object { { xxx\n some junk";
+            Assert.AreEqual("MyClass", SourceFileAnalyzer.FindTopLevelClass(code, "MyClass.cs"));
+        }
+
+    }
+}
