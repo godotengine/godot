4aecbbea4c7c34cc31e058fdcdf7683a8228e26c
Rémi Verschelde <rverschelde@gmail.com>
2019-04-03 11:25:01 +0200

    SCons: Fix python3 compat for builtin_bullet=no

--- platform/x11/detect.py
+++ platform/x11/detect.py
@@ -197,7 +197,7 @@ def configure(env):
         # We need at least version 2.88
         import subprocess
         bullet_version = subprocess.check_output(['pkg-config', 'bullet', '--modversion']).strip()
-        if bullet_version < "2.88":
+        if str(bullet_version) < "2.88":
             # Abort as system bullet was requested but too old
             print("Bullet: System version {0} does not match minimal requirements ({1}). Aborting.".format(bullet_version, "2.88"))
             sys.exit(255)
