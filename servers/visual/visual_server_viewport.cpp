e0b703e3fe9da396f9ad0c8bd29bb61ab1ab0fdd
Bastiaan Olij <mux213@gmail.com>
2019-04-10 00:14:10 +1000

    Added eye_height, changed description and fixed size of viewport issue

--- servers/visual/visual_server_viewport.cpp
+++ servers/visual/visual_server_viewport.cpp
@@ -295,8 +295,8 @@ void VisualServerViewport::draw_viewports() {
 
 		if (vp->use_arvr && arvr_interface.is_valid()) {
 			// override our size, make sure it matches our required size
-			Size2 size = arvr_interface->get_render_targetsize();
-			VSG::storage->render_target_set_size(vp->render_target, size.x, size.y);
+			vp->size = arvr_interface->get_render_targetsize();
+			VSG::storage->render_target_set_size(vp->render_target, vp->size.x, vp->size.y);
 
 			// render mono or left eye first
 			ARVRInterface::Eyes leftOrMono = arvr_interface->is_stereo() ? ARVRInterface::EYE_LEFT : ARVRInterface::EYE_MONO;
