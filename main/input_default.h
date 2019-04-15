1bae73d7d060eeb9531fc5ba46de5d31dc7073c7
Guilherme Felipe <guilhermefelipecgs@gmail.com>
2019-04-15 12:30:20 -0300

    Add Input::get_current_cursor_shape
    [Clean up] Removed unused/unnecessary methods.

--- main/input_default.h
+++ main/input_default.h
@@ -242,10 +242,10 @@ public:
 	void set_emulate_mouse_from_touch(bool p_emulate);
 	virtual bool is_emulating_mouse_from_touch() const;
 
-	virtual CursorShape get_default_cursor_shape();
+	virtual CursorShape get_default_cursor_shape() const;
 	virtual void set_default_cursor_shape(CursorShape p_shape);
+	virtual CursorShape get_current_cursor_shape() const;
 	virtual void set_custom_mouse_cursor(const RES &p_cursor, CursorShape p_shape = Input::CURSOR_ARROW, const Vector2 &p_hotspot = Vector2());
-	virtual void set_mouse_in_window(bool p_in_window);
 
 	void parse_mapping(String p_mapping);
 	void joy_button(int p_device, int p_button, bool p_pressed);
