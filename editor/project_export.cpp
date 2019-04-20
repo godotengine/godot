4e11ecbb854dadd1c48677a05f2bf1eaf2ca07f5
sparkart <emmanuelbarroga@gmail.com>
2019-04-19 22:07:47 -0700

    Fixed naming issue when duplicating an export.

--- editor/project_export.cpp
+++ editor/project_export.cpp
@@ -571,9 +571,9 @@ void ProjectExportDialog::_duplicate_preset() {
 	Ref<EditorExportPreset> preset = current->get_platform()->create_preset();
 	ERR_FAIL_COND(!preset.is_valid());
 
-	String name = current->get_name() + "" + itos(1);
+	String name = current->get_name();
 	bool make_runnable = true;
-	int attempt = 2;
+	int attempt = 1;
 	while (true) {
 
 		bool valid = true;
@@ -593,7 +593,11 @@ void ProjectExportDialog::_duplicate_preset() {
 			break;
 
 		attempt++;
-		name = current->get_name() + " " + itos(attempt);
+		while (name.length() && name[name.length() - 1] >= '0' && name[name.length() - 1] <= '9') {
+			name = name.substr(0, name.length() - 1);
+		}
+
+		name += itos(attempt);
 	}
 
 	preset->set_name(name);
