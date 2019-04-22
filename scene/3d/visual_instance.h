2b8dbd0ff75ff1bce6fbfd89600858fd13a22b1a
Juan Linietsky <reduzio@gmail.com>
2019-04-22 19:27:30 -0300

    Added bindings for methods related to obtaining base an instance from VisualInstance

--- scene/3d/visual_instance.h
+++ scene/3d/visual_instance.h
@@ -43,6 +43,7 @@ class VisualInstance : public Spatial {
 	GDCLASS(VisualInstance, Spatial);
 	OBJ_CATEGORY("3D Visual Nodes");
 
+	RID base;
 	RID instance;
 	uint32_t layers;
 
@@ -69,6 +70,7 @@ public:
 	virtual AABB get_transformed_aabb() const; // helper
 
 	void set_base(const RID &p_base);
+	RID get_base() const;
 
 	void set_layer_mask(uint32_t p_mask);
 	uint32_t get_layer_mask() const;
