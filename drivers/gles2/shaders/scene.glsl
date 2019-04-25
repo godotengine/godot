3c351f5953f1965289c0804d42f650c3ce0ca721
Guilherme Souza <gdsdsilva@inf.ufpel.edu.br>
2019-04-25 18:29:39 -0300

    Adds flag AMBIENT_LIGHT_DISABLED to GLES2
    Signed-off-by: Guilherme Souza <gdsdsilva@inf.ufpel.edu.br>

--- drivers/gles2/shaders/scene.glsl
+++ drivers/gles2/shaders/scene.glsl
@@ -1545,6 +1545,11 @@ FRAGMENT_SHADER_CODE
 
 #ifdef BASE_PASS
 	//none
+
+#ifdef AMBIENT_LIGHT_DISABLED
+	ambient_light = vec3(0.0, 0.0, 0.0);
+#else
+
 #ifdef USE_RADIANCE_MAP
 
 	vec3 ref_vec = reflect(-eye_position, N);
@@ -1567,6 +1572,7 @@ FRAGMENT_SHADER_CODE
 
 #endif
 
+#endif // AMBIENT_LIGHT_DISABLED
 	ambient_light *= ambient_energy;
 
 #if defined(USE_REFLECTION_PROBE1) || defined(USE_REFLECTION_PROBE2)
