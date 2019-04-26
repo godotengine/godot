e73671968fe361fbc1769685bd67f7aea28f7863
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-04-26 18:49:37 +0200

    Limit the size of the Manage Editor Feature Profiles dialog
    This makes it more readable on large monitors.

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -2451,7 +2451,13 @@ void EditorNode::_menu_option_confirm(int p_option, bool p_confirmed) {
 		} break;
 		case SETTINGS_MANAGE_FEATURE_PROFILES: {
 
-			feature_profile_manager->popup_centered_ratio();
+			Size2 popup_size = Size2(900, 800) * editor_get_scale();
+			Size2 window_size = get_viewport()->get_size();
+
+			popup_size.x = MIN(window_size.x * 0.8, popup_size.x);
+			popup_size.y = MIN(window_size.y * 0.8, popup_size.y);
+
+			feature_profile_manager->popup_centered(popup_size);
 
 		} break;
 		case SETTINGS_TOGGLE_FULLSCREEN: {
