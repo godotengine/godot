759b488b226593d3787b275fbc3e30930c2ac76f
Hugo Locurcio <hugo.locurcio@hugo.pro>
2019-05-02 00:45:34 +0200

    Add a property hint for DynamicFont size
    This caps its size to reasonable values in the Inspector.
    
    This closes #22581.
    
    (cherry picked from commit 818f756d9034abe8b1ab4ecc014666893fcb00b6)

--- scene/resources/dynamic_font.cpp
+++ scene/resources/dynamic_font.cpp
@@ -1008,7 +1008,7 @@ void DynamicFont::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_fallback_count"), &DynamicFont::get_fallback_count);
 
 	ADD_GROUP("Settings", "");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "size"), "set_size", "get_size");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "size", PROPERTY_HINT_RANGE, "1,255,1"), "set_size", "get_size");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "outline_size", PROPERTY_HINT_RANGE, "0,255,1"), "set_outline_size", "get_outline_size");
 	ADD_PROPERTY(PropertyInfo(Variant::COLOR, "outline_color"), "set_outline_color", "get_outline_color");
 	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "use_mipmaps"), "set_use_mipmaps", "get_use_mipmaps");
