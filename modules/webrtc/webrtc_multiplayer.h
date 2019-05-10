0bccf96c732b043e4ba1e06aac14b354ddecd6fc
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2019-05-11 01:53:49 +0200

    Add WebRTCMultiplayer

--- modules/webrtc/webrtc_multiplayer.h
+++ modules/webrtc/webrtc_multiplayer.h
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  register_types.cpp                                                   */
+/*  webrtc_multiplayer.h                                                 */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,34 +28,89 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-#include "register_types.h"
-#include "webrtc_data_channel.h"
+#ifndef WEBRTC_MULTIPLAYER_H
+#define WEBRTC_MULTIPLAYER_H
+
+#include "core/io/networked_multiplayer_peer.h"
 #include "webrtc_peer_connection.h"
 
-#ifdef JAVASCRIPT_ENABLED
-#include "emscripten.h"
-#include "webrtc_peer_connection_js.h"
-#endif
-#ifdef WEBRTC_GDNATIVE_ENABLED
-#include "webrtc_data_channel_gdnative.h"
-#include "webrtc_peer_connection_gdnative.h"
-#endif
-#include "webrtc_multiplayer.h"
+class WebRTCMultiplayer : public NetworkedMultiplayerPeer {
 
-void register_webrtc_types() {
-#ifdef JAVASCRIPT_ENABLED
-	WebRTCPeerConnectionJS::make_default();
-#elif defined(WEBRTC_GDNATIVE_ENABLED)
-	WebRTCPeerConnectionGDNative::make_default();
-#endif
+	GDCLASS(WebRTCMultiplayer, NetworkedMultiplayerPeer);
 
-	ClassDB::register_custom_instance_class<WebRTCPeerConnection>();
-#ifdef WEBRTC_GDNATIVE_ENABLED
-	ClassDB::register_class<WebRTCPeerConnectionGDNative>();
-	ClassDB::register_class<WebRTCDataChannelGDNative>();
-#endif
-	ClassDB::register_virtual_class<WebRTCDataChannel>();
-	ClassDB::register_class<WebRTCMultiplayer>();
-}
+protected:
+	static void _bind_methods();
 
-void unregister_webrtc_types() {}
+private:
+	enum {
+		CH_RELIABLE = 0,
+		CH_ORDERED = 1,
+		CH_UNRELIABLE = 2,
+		CH_RESERVED_MAX = 3
+	};
+
+	class ConnectedPeer : public Reference {
+
+	public:
+		Ref<WebRTCPeerConnection> connection;
+		List<Ref<WebRTCDataChannel> > channels;
+		bool connected;
+
+		ConnectedPeer() {
+			connected = false;
+			for (int i = 0; i < CH_RESERVED_MAX; i++)
+				channels.push_front(Ref<WebRTCDataChannel>());
+		}
+	};
+
+	uint32_t unique_id;
+	int target_peer;
+	int client_count;
+	bool refuse_connections;
+	ConnectionStatus connection_status;
+	TransferMode transfer_mode;
+	int next_packet_peer;
+	bool server_compat;
+
+	Map<int, Ref<ConnectedPeer> > peer_map;
+
+	void _peer_to_dict(Ref<ConnectedPeer> p_connected_peer, Dictionary &r_dict);
+	void _find_next_peer();
+
+public:
+	WebRTCMultiplayer();
+	~WebRTCMultiplayer();
+
+	Error initialize(int p_self_id, bool p_server_compat = false);
+	Error add_peer(Ref<WebRTCPeerConnection> p_peer, int p_peer_id, int p_unreliable_lifetime = 1);
+	void remove_peer(int p_peer_id);
+	bool has_peer(int p_peer_id);
+	Dictionary get_peer(int p_peer_id);
+	Dictionary get_peers();
+	void close();
+
+	// PacketPeer
+	Error get_packet(const uint8_t **r_buffer, int &r_buffer_size); ///< buffer is GONE after next get_packet
+	Error put_packet(const uint8_t *p_buffer, int p_buffer_size);
+	int get_available_packet_count() const;
+	int get_max_packet_size() const;
+
+	// NetworkedMultiplayerPeer
+	void set_transfer_mode(TransferMode p_mode);
+	TransferMode get_transfer_mode() const;
+	void set_target_peer(int p_peer_id);
+
+	int get_unique_id() const;
+	int get_packet_peer() const;
+
+	bool is_server() const;
+
+	void poll();
+
+	void set_refuse_new_connections(bool p_enable);
+	bool is_refusing_new_connections() const;
+
+	ConnectionStatus get_connection_status() const;
+};
+
+#endif
