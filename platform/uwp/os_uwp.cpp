58cec3efc4c7b8d2a60102ce5b3bc28e7899f9e5
Rémi Verschelde <rverschelde@gmail.com>
2019-05-16 08:47:27 +0200

    Fix OS_UWP::execute's signature after cd4449e
    Same as #28919.

--- platform/uwp/os_uwp.cpp
+++ platform/uwp/os_uwp.cpp
@@ -713,7 +713,7 @@ void OS_UWP::set_custom_mouse_cursor(const RES &p_cursor, CursorShape p_shape, c
 	// TODO
 }
 
-Error OS_UWP::execute(const String &p_path, const List<String> &p_arguments, bool p_blocking, ProcessID *r_child_id, String *r_pipe, int *r_exitcode, bool read_stderr) {
+Error OS_UWP::execute(const String &p_path, const List<String> &p_arguments, bool p_blocking, ProcessID *r_child_id, String *r_pipe, int *r_exitcode, bool read_stderr, Mutex *p_pipe_mutex) {
 
 	return FAILED;
 };
