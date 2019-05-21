b5c8205312c04205db5fe7ebf87ddbaa9dbfc950
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2019-05-21 13:14:45 -0300

    Fix bottom panel visibility behaviour in the theme editor
    Fixes #29034, fixes #29056

--- editor/plugins/theme_editor_plugin.cpp
+++ editor/plugins/theme_editor_plugin.cpp
@@ -879,11 +879,9 @@ ThemeEditor::ThemeEditor() {
 void ThemeEditorPlugin::edit(Object *p_node) {
 
 	if (Object::cast_to<Theme>(p_node)) {
-		theme_editor->show();
 		theme_editor->edit(Object::cast_to<Theme>(p_node));
 	} else {
 		theme_editor->edit(Ref<Theme>());
-		theme_editor->hide();
 	}
 }
 
@@ -898,11 +896,11 @@ void ThemeEditorPlugin::make_visible(bool p_visible) {
 		theme_editor->set_process(true);
 		button->show();
 		editor->make_bottom_panel_item_visible(theme_editor);
-
 	} else {
 		theme_editor->set_process(false);
 		if (theme_editor->is_visible_in_tree())
 			editor->hide_bottom_panel();
+
 		button->hide();
 	}
 }
