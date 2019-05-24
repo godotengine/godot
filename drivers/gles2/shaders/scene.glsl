e3da1ad2332a259201d669e8dd3d2698dbd2d603
clayjohn <claynjohn@gmail.com>
2019-05-23 17:17:27 -0700

    do not compute fog when using unshaded in GLES2

--- drivers/gles2/shaders/scene.glsl
+++ drivers/gles2/shaders/scene.glsl
@@ -2133,8 +2133,6 @@ FRAGMENT_SHADER_CODE
 #endif
 	// gl_FragColor = vec4(normal, 1.0);
 
-#endif //unshaded
-
 //apply fog
 #if defined(FOG_DEPTH_ENABLED) || defined(FOG_HEIGHT_ENABLED)
 
@@ -2188,6 +2186,8 @@ FRAGMENT_SHADER_CODE
 #endif //use vertex lit
 
 #endif // defined(FOG_DEPTH_ENABLED) || defined(FOG_HEIGHT_ENABLED)
+
+#endif //unshaded
 
 #else // not RENDER_DEPTH
 //depth render
