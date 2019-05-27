d479b72cb1a2c202e85cea6b6287e3f0950e348b
Daniel Matarov <DMatarov@outlook.com>
2019-05-27 16:02:52 +0100

    27.05 trying to make playlist work

--- modules/InteractiveMusic/register_types.cpp
+++ modules/InteractiveMusic/register_types.cpp
@@ -1,13 +1,15 @@
 #include "register_types.h"
 
 #include "core/class_db.h"
-#include "audio_stream_transitioner.h"
-#include "audio_stream_transitioner_playback.h"
+#include "audio_stream_playlist.h"
+#include "audio_stream_playback_playlist.h"
 
 void register_InteractiveMusic_types() {
 
-        ClassDB::register_class<AudioStreamTransitioner>();
-        ClassDB::register_class<AudioStreamTransitionerPlayback>();
+       
+		ClassDB::register_class<AudioStreamPlaylist>();
+       
+		ClassDB::register_class<AudioStreamPlaylistPlayback>();
 }
 
 void unregister_InteractiveMusic_types() {
