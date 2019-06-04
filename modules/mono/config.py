5d7de876922f1c33ce6a0c106917fc0e2d218bf5
Ignacio Etcheverry <ignalfonsore@gmail.com>
2019-06-04 16:15:00 +0200

    Mono: Fix SCons options added to the wrong environment

--- modules/mono/config.py
+++ modules/mono/config.py
@@ -8,6 +8,16 @@ def configure(env):
     env.use_ptrcall = True
     env.add_module_version_string('mono')
 
+    from SCons.Script import BoolVariable, PathVariable, Variables
+
+    envvars = Variables()
+    envvars.Add(PathVariable('mono_prefix', 'Path to the mono installation directory for the target platform and architecture', '', PathVariable.PathAccept))
+    envvars.Add(BoolVariable('mono_static', 'Statically link mono', False))
+    envvars.Add(BoolVariable('mono_glue', 'Build with the mono glue sources', True))
+    envvars.Add(BoolVariable('copy_mono_root', 'Make a copy of the mono installation directory to bundle with the editor', False))
+    envvars.Add(BoolVariable('xbuild_fallback', 'If MSBuild is not found, fallback to xbuild', False))
+    envvars.Update(env)
+
 
 def get_doc_classes():
     return [
