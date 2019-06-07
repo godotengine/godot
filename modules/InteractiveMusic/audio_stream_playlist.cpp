ed00fc85862470f77f1c9425c29b5666dfa737ec
Daniel Matarov <DMatarov@outlook.com>
2019-06-07 17:17:23 +0100

    Mix logic and properties added, crashes when audiostreamplaylist is selected in audiostreamplayer

--- modules/InteractiveMusic/audio_stream_playlist.cpp
+++ modules/InteractiveMusic/audio_stream_playlist.cpp
@@ -2,8 +2,9 @@
 #include "core/math/math_funcs.h"
 #include "core/print_string.h"
 
-
 AudioStreamPlaylist::AudioStreamPlaylist() {
+	bpm = 120;
+	stream_count = 1;
 
 }
 
@@ -24,30 +25,51 @@ void AudioStreamPlaylist::set_position(uint64_t p) {
 	pos = p;
 }
 
+void AudioStreamPlaylist::set_stream_beats(int p_stream, int beats) {
+	audio_streams[p_stream].beat_count = beats;
+}
+
+int AudioStreamPlaylist::get_stream_beats(int p_stream) {
+	return audio_streams[p_stream].beat_count;
+}
+
+void AudioStreamPlaylist::set_list_stream(int stream_number, const Ref<AudioStream> &p_stream) {
+	ERR_FAIL_COND(p_stream == this);
+	ERR_FAIL_INDEX(stream_number, MAX_STREAMS);
+
+
+	audio_streams[stream_number].stream = p_stream;
+}
+
+Ref<AudioStream> AudioStreamPlaylist::get_list_stream(int stream_number) {
+	ERR_FAIL_INDEX_V(stream_number, MAX_STREAMS, Ref<AudioStream>());
+
+	return audio_streams[stream_number].stream;
+}
+
 void AudioStreamPlaylist::set_stream_count(int count) {
 	stream_count = count;
 }
 
-void AudioStreamPlaylist::set_bpm(int beats) {
-	bpm = beats;
+int AudioStreamPlaylist::get_stream_count() {
+	return stream_count;
+}
+
+void AudioStreamPlaylist::set_bpm(int beats_per_minute) {
+	bpm = beats_per_minute;
+}
+
+int AudioStreamPlaylist::get_bpm() {
+	return bpm;
 }
 
 void AudioStreamPlaylist::set_order(OrderMode p_order) {
 	p_order = order_mode;
 }
 
-void AudioStreamPlaylist::play(Vector<Ref<AudioStream> > audio_streams, int stream_count) {
-	for (int i = 0; i < stream_count; i++) {
-		//do something with audio_streams, looping through them and writing to a buffer?
-		//
-
-
-	}
-}
-
 void AudioStreamPlaylist::_validate_property(PropertyInfo &property) const {
 	String prop = property.name;
-	if (prop.begins_with("audio_")) {
+	if (prop.begins_with("stream_")) {
 		int stream = prop.get_slicec('/', 0).get_slicec('_', 1).to_int();
 		if (stream >= stream_count) {
 			property.usage = 0;
@@ -55,24 +77,38 @@ void AudioStreamPlaylist::_validate_property(PropertyInfo &property) const {
 	}
 }
 
-
 void AudioStreamPlaylist::_bind_methods() {
-	
+	ClassDB::bind_method(D_METHOD("set_stream_count", "stream_count"), &AudioStreamPlaylist::set_stream_count);
+	ClassDB::bind_method(D_METHOD("get_stream_count"), &AudioStreamPlaylist::get_stream_count);
+
+	ClassDB::bind_method(D_METHOD("set_bpm", "bpm"), &AudioStreamPlaylist::set_bpm);
+	ClassDB::bind_method(D_METHOD("get_bpm"), &AudioStreamPlaylist::get_bpm);
+
+	ClassDB::bind_method(D_METHOD("set_list_stream", "stream_number", "audio_stream"), &AudioStreamPlaylist::set_list_stream);
+	ClassDB::bind_method(D_METHOD("get_list_stream", "stream_number"), &AudioStreamPlaylist::get_list_stream);
+
+	ClassDB::bind_method(D_METHOD("set_stream_beats", "stream_number", "beat_count"), &AudioStreamPlaylist::set_stream_beats);
+	ClassDB::bind_method(D_METHOD("get_stream_beats", "stream_number"), &AudioStreamPlaylist::get_stream_beats);
+
+
+
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "stream_count", PROPERTY_HINT_RANGE, "1," + itos(MAX_STREAMS), PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_UPDATE_ALL_IF_MODIFIED), "set_stream_count", "get_stream_count");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "bpm", PROPERTY_HINT_RANGE, "0,400"), "set_bpm", "get_bpm");
+
+	for (int i = 0; i < MAX_STREAMS; i++) {
+		ADD_PROPERTYI(PropertyInfo(Variant::OBJECT, "stream_" + itos(i), PROPERTY_HINT_RESOURCE_TYPE, "AudioStream", PROPERTY_USAGE_DEFAULT | PROPERTY_USAGE_INTERNAL), "set_list_stream", "get_list_stream", i);
+
+	}
+
+	BIND_CONSTANT(MAX_STREAMS);
 }
 
-
-
-
-
-
 //////////////////////
 //////////////////////
-
 
 AudioStreamPlaybackPlaylist::AudioStreamPlaybackPlaylist() :
 		active(false) {
 	AudioServer::get_singleton()->lock();
-	
 	zeromem(pcm_buffer, buffer_size);
 	AudioServer::get_singleton()->unlock();
 }
@@ -86,11 +122,21 @@ AudioStreamPlaybackPlaylist::~AudioStreamPlaybackPlaylist() {
 
 void AudioStreamPlaybackPlaylist::stop() {
 	active = false;
-	instance->reset;
+	instance->reset();
 }
 
 void AudioStreamPlaybackPlaylist::start(float p_from_pos) {
 	seek(p_from_pos);
+	current = 0;
+	fading = false;
+	fading_time = instance->beat_size / 128; //fading time was not set anywhere else in the pseudocode
+			// which made fading_samples_total=0 when the first fade is triggered
+			// that makes for a division by 0 when from_volume and to_volume are calculated in fade logic
+			// so i assigned a very small value based on BPM to fading time
+			// alternative would be to have the fading time set by the user
+	fading_samples_total = fading_time * instance->sample_rate;
+	beat_amount_remaining = instance->audio_streams[current].beat_count * instance->beat_size;
+	instance->audio_streams[current].playback->start();
 	active = true;
 }
 
@@ -102,17 +148,70 @@ void AudioStreamPlaybackPlaylist::seek(float p_time) {
 	instance->set_position(uint64_t(p_time * instance->sample_rate) << MIX_FRAC_BITS);
 }
 
-void AudioStreamPlaybackPlaylist::mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames) {
-	ERR_FAIL_COND(!active);
-	if (!active) {
-		return;
+void AudioStreamPlaybackPlaylist::add_stream_to_buffer(Ref<AudioStreamPlayback> playback, int samples, float p_rate_scale, float initial_volume, float final_volume) {
+	playback->mix(aux_buffer, p_rate_scale, samples);
+	for (int i = 0; i < samples; i++) {
+		float c = float(i) / samples;
+		float volume = initial_volume * (1.0 - c) + final_volume * c;
+		pcm_buffer[i] += aux_buffer[i] * volume;
 	}
-	zeromem(pcm_buffer,buffer_size);
-	int16_t *buf = (int16_t *)pcm_buffer;
-	instance->play; //function from audiostreamplaylist which fills the buffer
+}
 
-	for (int i = 0; i < p_frames; i++) {
-		float sample = float(buf[i]) / 32767.0;
-		p_buffer[i] = AudioFrame(sample, sample);
+void AudioStreamPlaybackPlaylist::clear_buffer(int samples) {
+	for (int i = 0; i < samples; i++) {
+		pcm_buffer[i] = AudioFrame(0.0, 0.0);
 	}
+}
+
+void AudioStreamPlaybackPlaylist::mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames) {
+	int dst_offset = 0;
+	int fading_samples = 0;
+	while (p_frames > 0) {
+
+		if (beat_amount_remaining == 0) {
+			fading = true;
+			current = (current + 1) % instance->stream_count;
+			instance->audio_streams[current].playback->start();
+			fading_samples = fading_samples_total;
+			beat_amount_remaining = instance->audio_streams[current].beat_count * instance->beat_size;
+		}
+
+		int to_mix = MIN(buffer_size, MIN(p_frames, beat_amount_remaining));
+
+		clear_buffer(to_mix);
+
+		if (fading) {
+			int to_fade = MIN(fading_samples, to_mix);
+			float from_volume = 1.0 - float(fading_samples) / fading_samples_total;
+			float to_volume = 1.0 - float(fading_samples + to_fade) / fading_samples_total;
+			add_stream_to_buffer(instance->audio_streams[current-1].playback, to_fade, p_rate_scale, from_volume, to_volume);
+			fading_samples -= to_fade;
+			if (fading_samples == 0) {
+				fading = false;
+				instance->audio_streams[current - 1].playback->stop();
+			}
+		}
+
+		add_stream_to_buffer(instance->audio_streams[current].playback, to_mix, p_rate_scale, 1.0, 1.0);
+
+		for (int i = 0; i < to_mix; i++) {
+			p_buffer[i + dst_offset] = pcm_buffer[i];
+		}
+		dst_offset += to_mix;
+		p_frames -= to_mix;
+		beat_amount_remaining -= to_mix;
+	}
+}
+
+int AudioStreamPlaybackPlaylist::get_loop_count() const {
+	return 0;
+}
+float AudioStreamPlaybackPlaylist::get_playback_position() const {
+	return 0.0;
+}
+float AudioStreamPlaybackPlaylist::get_length() const {
+	return 0.0;
+}
+bool AudioStreamPlaybackPlaylist::is_playing() const {
+	return active;
 }
--- modules/InteractiveMusic/audio_stream_playlist.h
+++ modules/InteractiveMusic/audio_stream_playlist.h
@@ -9,27 +9,56 @@ class AudioStreamPlaylist : public AudioStream {
 private:
 	friend class AudioStreamPlaybackPlaylist;
 	uint64_t pos;
-	int sample_rate;
+	int sample_rate=44100;
 	bool stereo;
 	int stream_count;
+	
 	int bpm;
-	//double beat_size = 60 / bpm;
-	//double time;
+	double beat_size = (60 / bpm)*sample_rate;
+	double time;
+	
 	enum OrderMode {
 		Sequence,
 		Shuffle
 	};
+
+	enum {
+		MAX_STREAMS = 64
+	};
+
+	struct Streams {
+		Ref<AudioStream> stream;
+		Ref<AudioStreamPlayback> playback;
+
+		int beat_count;
+	};
+
+	Streams audio_streams[MAX_STREAMS];
+
 	OrderMode order_mode;
-	Vector<Ref<AudioStream> > audio_streams;
+
+
+	
+	
+	
 
 public:
 	void reset();
 	void set_position(uint64_t pos);
 	void set_stereo();
-	void set_bpm(int beats);
+	void set_stream_beats(int p_stream,int beats);
+	int get_stream_beats(int p_stream);
+	void set_bpm(int beats_per_minute);
+	int get_bpm();
 	void set_stream_count(int count);
+	int get_stream_count();
+	void set_list_stream(int stream_number, const Ref<AudioStream> &p_stream);
+	Ref<AudioStream> get_list_stream(int stream_number);
 	void set_order(OrderMode p_order);
-	virtual void play(Vector<Ref<AudioStream> > audio_streams, int stream_count);
+	OrderMode get_order(){
+		return order_mode;
+	};
+	
 	virtual Ref<AudioStreamPlayback> instance_playback();
 	virtual String get_stream_name() const;
 	virtual float get_length() const { return 0; }
@@ -54,16 +83,28 @@ private:
 		MIX_FRAC_MASK = MIX_FRAC_LEN - 1,
 	};
 	AudioFrame *pcm_buffer;
+	AudioFrame *aux_buffer;
+	friend class AudioStreamPlaylist;
+	
 	Ref<AudioStreamPlaylist> instance;
+
+	int current;
+	bool fading;
+	int fading_samples_total;
+	int fading_time;
+	int beat_amount_remaining;
+
 	bool active;
 
 public:
 	virtual void start(float p_from_pos = 0.0);
 	virtual void stop();
 	virtual bool is_playing() const;
+	void clear_buffer(int samples);
 	virtual int get_loop_count() const; // times it looped
 	virtual float get_playback_position() const;
 	virtual void seek(float p_time);
+	void add_stream_to_buffer(Ref<AudioStreamPlayback> playback, int samples, float p_rate_scale, float initial_volume, float final_volume);
 	virtual void mix(AudioFrame *p_buffer, float p_rate_scale, int p_frames);
 	virtual float get_length() const;
 	AudioStreamPlaybackPlaylist();
