09737ef6a732c5ed31cf92e13b95c17c11ff2349
Nathan Warden <nathan@nathanwarden.com>
2019-06-22 10:27:09 -0500

    Updated lightmap baker to dynamically calculate lightmap sizes based on surface area.

--- scene/3d/voxel_light_baker.h
+++ scene/3d/voxel_light_baker.h
@@ -177,7 +177,7 @@ public:
 		PoolVector<float> light;
 	};
 
-	Error make_lightmap(const Transform &p_xform, Ref<Mesh> &p_mesh, LightMapData &r_lightmap, bool (*p_bake_time_func)(void *, float, float) = NULL, void *p_bake_time_ud = NULL);
+	Error make_lightmap(const Transform &p_xform, Ref<Mesh> &p_mesh, float default_texels_per_unit, LightMapData &r_lightmap, bool (*p_bake_time_func)(void *, float, float) = NULL, void *p_bake_time_ud = NULL);
 
 	PoolVector<int> create_gi_probe_data();
 	Ref<MultiMesh> create_debug_multimesh(DebugMode p_mode = DEBUG_ALBEDO);
