7a9eb587fd929a903c999f449ccb6314ec235ab5
Gwyneth Lowe <GlaceGwyneth@gmail.com>
2019-06-24 00:17:27 -0500

    Correct typo that broke custom selected font color
    Change several font_selected_color to font_color_selected; the actual name of the override
    
    (cherry picked from commit 7142064110936593d15409dcf35c566638f87d20)

--- scene/gui/text_edit.h
+++ scene/gui/text_edit.h
@@ -170,7 +170,7 @@ private:
 		Color line_number_color;
 		Color safe_line_number_color;
 		Color font_color;
-		Color font_selected_color;
+		Color font_color_selected;
 		Color keyword_color;
 		Color number_color;
 		Color function_color;
