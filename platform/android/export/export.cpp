dea414abc8ae9a11b5f8616969093c5eebe27c75
fhuya <fhuyakou@gmail.com>
2019-07-02 09:22:47 -0700

    Make the meta-data name attribute settable. This will facilitate reuse for ARCore integration.

--- platform/android/export/export.cpp
+++ platform/android/export/export.cpp
@@ -824,11 +824,17 @@ class EditorExportPlatformAndroid : public EditorExportPlatform {
 							encode_uint32(min_gles3 ? 0x00030000 : 0x00020000, &p_manifest.write[iofs + 16]);
 						}
 
-						if (tname == "meta-data" && attrname == "value") {
+						if (tname == "meta-data" && attrname == "name" && string_table[attr_value] == "xr_mode_metadata_name") {
+							// Update the meta-data 'android:name' attribute based on the selected XR mode.
+							if (xr_mode_index == 1 /* XRMode.OVR */) {
+								string_table.write[attr_value] = "com.samsung.android.vr.application.mode";
+							}
+						}
+
+						if (tname == "meta-data" && attrname == "value" && string_table[attr_value] == "xr_mode_metadata_value") {
+							// Update the meta-data 'android:value' attribute based on the selected XR mode.
 							if (xr_mode_index == 1 /* XRMode.OVR */) {
 								string_table.write[attr_value] = "vr_only";
-							} else {
-								string_table.write[attr_value] = "";
 							}
 						}
 
