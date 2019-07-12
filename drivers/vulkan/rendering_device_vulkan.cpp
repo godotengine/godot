9d8eb9ee1472d59b5f056652b235919424112f45
bruvzg <7645683+bruvzg@users.noreply.github.com>
2019-07-12 16:18:30 +0300

    Add runtime GLES2 / Vulkan context selection.

--- drivers/vulkan/rendering_device_vulkan.cpp
+++ drivers/vulkan/rendering_device_vulkan.cpp
@@ -5095,7 +5095,9 @@ RenderingDevice::DrawListID RenderingDeviceVulkan::draw_list_begin_for_screen(in
 	VkCommandBuffer command_buffer = frames[frame].draw_command_buffer;
 	draw_list = memnew(DrawList);
 	draw_list->command_buffer = command_buffer;
+#ifdef DEBUG_ENABLED
 	draw_list->validation.framebuffer_format = screen_get_framebuffer_format();
+#endif
 	draw_list_count = 0;
 	draw_list_split = false;
 
@@ -5294,7 +5296,9 @@ RenderingDevice::DrawListID RenderingDeviceVulkan::draw_list_begin(RID p_framebu
 
 	draw_list = memnew(DrawList);
 	draw_list->command_buffer = command_buffer;
+#ifdef DEBUG_ENABLED
 	draw_list->validation.framebuffer_format = framebuffer->format_id;
+#endif
 	draw_list_count = 0;
 	draw_list_split = false;
 
@@ -5442,7 +5446,9 @@ Error RenderingDeviceVulkan::draw_list_begin_split(RID p_framebuffer, uint32_t p
 		}
 
 		draw_list[i].command_buffer = command_buffer;
+#ifdef DEBUG_ENABLED
 		draw_list[i].validation.framebuffer_format = framebuffer->format_id;
+#endif
 
 		VkViewport viewport;
 		viewport.x = viewport_offset.x;
@@ -5639,9 +5645,9 @@ void RenderingDeviceVulkan::draw_list_bind_vertex_array(DrawListID p_list, RID p
 
 #ifdef DEBUG_ENABLED
 	dl->validation.vertex_format = vertex_array->description;
-	dl->validation.vertex_array_size = vertex_array->vertex_count;
 	dl->validation.vertex_max_instances_allowed = vertex_array->max_instances_allowed;
 #endif
+	dl->validation.vertex_array_size = vertex_array->vertex_count;
 	vkCmdBindVertexBuffers(dl->command_buffer, 0, vertex_array->buffers.size(), vertex_array->buffers.ptr(), vertex_array->offsets.ptr());
 }
 void RenderingDeviceVulkan::draw_list_bind_index_array(DrawListID p_list, RID p_index_array) {
@@ -5665,10 +5671,11 @@ void RenderingDeviceVulkan::draw_list_bind_index_array(DrawListID p_list, RID p_
 
 	dl->state.index_array = p_index_array;
 #ifdef DEBUG_ENABLED
-	dl->validation.index_array_size = index_array->indices;
 	dl->validation.index_array_max_index = index_array->max_index;
-	dl->validation.index_array_offset = index_array->offset;
 #endif
+	dl->validation.index_array_size = index_array->indices;
+	dl->validation.index_array_offset = index_array->offset;
+
 	vkCmdBindIndexBuffer(dl->command_buffer, index_array->buffer, index_array->offset, index_array->index_type);
 }
 
@@ -5676,10 +5683,13 @@ void RenderingDeviceVulkan::draw_list_set_line_width(DrawListID p_list, float p_
 
 	DrawList *dl = _get_draw_list_ptr(p_list);
 	ERR_FAIL_COND(!dl);
+
+#ifdef DEBUG_ENABLED
 	if (!dl->validation.active) {
 		ERR_EXPLAIN("Submitted Draw Lists can no longer be modified.");
 		ERR_FAIL();
 	}
+#endif
 
 	vkCmdSetLineWidth(dl->command_buffer, p_width);
 }
@@ -5702,7 +5712,9 @@ void RenderingDeviceVulkan::draw_list_set_push_constant(DrawListID p_list, void 
 	}
 #endif
 	vkCmdPushConstants(dl->command_buffer, dl->state.pipeline_layout, dl->state.pipeline_push_constant_stages, 0, p_data_size, p_data);
+#ifdef DEBUG_ENABLED
 	dl->validation.pipeline_push_constant_suppplied = true;
+#endif
 }
 
 void RenderingDeviceVulkan::draw_list_draw(DrawListID p_list, bool p_use_indices, uint32_t p_instances) {
