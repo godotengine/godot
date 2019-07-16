0584911f9fcce8ab79a50a99a5eff0a8e2f32bce
Cameron Reikes <cameronreikes@gmail.com>
2019-07-16 15:40:59 -0700

    CameraServer is needed for editor node
     - Editor asks ClassDB for docs
     - ClassDB asks CameraFeed for default parameters
     - In CameraFeed's constructor, polls CameraServer

--- platform/server/os_server.cpp
+++ platform/server/os_server.cpp
@@ -88,6 +88,8 @@ Error OS_Server::initialize(const VideoMode &p_desired, int p_video_driver, int 
 	visual_server = memnew(VisualServerRaster);
 	visual_server->init();
 
+	camera_server = memnew(CameraServer);
+
 	AudioDriverManager::initialize(p_audio_driver);
 
 	input = memnew(InputDefault);
@@ -116,6 +118,8 @@ void OS_Server::finalize() {
 	memdelete(visual_server);
 
 	memdelete(input);
+
+	memdelete(camera_server);
 
 	memdelete(power_manager);
 
