ec679dfffa15ac531e46df8b72aa414ef9c90cab
Ignacio Etcheverry <ignalfonsore@gmail.com>
2019-07-20 14:26:46 +0200

    Fix mono module build errors for release templates

--- modules/mono/class_db_api_json.h
+++ modules/mono/class_db_api_json.h
@@ -31,9 +31,16 @@
 #ifndef CLASS_DB_API_JSON_H
 #define CLASS_DB_API_JSON_H
 
+// 'core/method_bind.h' defines DEBUG_METHODS_ENABLED, but it looks like we
+// cannot include it here. That's why we include it through 'core/class_db.h'.
 #include "core/class_db.h"
+
+#ifdef DEBUG_METHODS_ENABLED
+
 #include "core/ustring.h"
 
 void class_db_api_to_json(const String &p_output_file, ClassDB::APIType p_api);
+
+#endif // DEBUG_METHODS_ENABLED
 
 #endif // CLASS_DB_API_JSON_H
