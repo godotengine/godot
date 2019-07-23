a2ea0ec504f11d2a4265d646464533324227e863
RevoluPowered <gordon@gordonite.tech>
2019-07-23 19:47:49 +0100

    Clang formatting

--- main/tests/test_main.cpp
+++ main/tests/test_main.cpp
@@ -31,7 +31,6 @@
 #include "core/list.h"
 #include "core/os/main_loop.h"
 
-
 #ifdef DEBUG_ENABLED
 
 #include <thirdparty/doctest/doctest.h>
@@ -76,13 +75,13 @@ MainLoop *test_main(String p_test, const List<String> &p_args) {
 
 	// tests should be ordered by name for humans
 	test_context.setOption("order-by", "name");
-	test_context.setOption("abort-after", 5);  
-	test_context.setOption("no-breaks", true);       
+	test_context.setOption("abort-after", 5);
+	test_context.setOption("no-breaks", true);
 	test_context.run(); // run tests
 
 	if (test_context.shouldExit())
 		return NULL;
-		
+
 	return NULL;
 
 	/*if (p_test == "string") {
