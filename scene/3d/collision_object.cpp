4ff360b73b0b0241292c51b2358817244d277734
Michael Alexsander Silva Dias <michaelalexsander@protonmail.com>
2019-07-25 11:53:54 -0300

    Fix wrong placement of new line in 'PhysicsBody' warning

--- scene/3d/collision_object.cpp
+++ scene/3d/collision_object.cpp
@@ -370,7 +370,7 @@ String CollisionObject::get_configuration_warning() const {
 	String warning = Spatial::get_configuration_warning();
 
 	if (shapes.empty()) {
-		if (warning == String()) {
+		if (!warning.empty()) {
 			warning += "\n\n";
 		}
 		warning += TTR("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape or CollisionPolygon as a child to define its shape.");
