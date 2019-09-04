ce19d3e9e72d26ad8962bc2a7863fc2467127eb9
leela <52065244+leela52452@users.noreply.github.com>
2019-09-04 15:41:09 +0000

    typo ellipsis, added

--- editor/editor_audio_buses.cpp
+++ editor/editor_audio_buses.cpp
@@ -1203,7 +1203,7 @@ void EditorAudioBuses::_select_layout() {
 void EditorAudioBuses::_save_as_layout() {
 
 	file_dialog->set_mode(EditorFileDialog::MODE_SAVE_FILE);
-	file_dialog->set_title(TTR("Save Audio Bus Layout As..."));
+	file_dialog->set_title(TTR("Save Audio Bus Layout As…"));
 	file_dialog->set_current_path(edited_path);
 	file_dialog->popup_centered_ratio();
 	new_layout = false;
