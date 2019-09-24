82b15ab4690f14c4e19d3532f834e3931d6daed4
George Marques <george@gmarqu.es>
2019-09-24 18:03:50 -0300

    GDScript: Fix type resolution not being return in some cases
    Some situations caused the parser node type to not being update when
    trying to resolve the type, returning invalid data and breaking the
    parsing when it shouldn't. This patch fix the behavior.
    
    (cherry picked from commit 64d09b7de532b75038c24e689d16a48a36678654)

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -6518,7 +6518,8 @@ GDScriptParser::DataType GDScriptParser::_reduce_node_type(Node *p_node) {
 		default: {}
 	}
 
-	p_node->set_datatype(_resolve_type(node_type, p_node->line));
+	node_type = _resolve_type(node_type, p_node->line);
+	p_node->set_datatype(node_type);
 	return node_type;
 }
 
