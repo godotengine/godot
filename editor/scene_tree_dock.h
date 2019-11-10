9fee540e6eb3768f9b31c41c067deff827485512
Shivansh Anand <shivansh586@gmail.com>
2019-11-10 21:28:56 +0530

    fixes #33439. The feature now changes according to the feature profile.

--- editor/scene_tree_dock.h
+++ editor/scene_tree_dock.h
@@ -112,6 +112,8 @@ class SceneTreeDock : public VBoxContainer {
 	ToolButton *button_create_script;
 	ToolButton *button_clear_script;
 
+	Button *button_3d;
+
 	HBoxContainer *button_hb;
 	ToolButton *edit_local, *edit_remote;
 	SceneTreeEditor *scene_tree;
