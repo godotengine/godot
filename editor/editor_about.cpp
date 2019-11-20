8587c04e2e8136c4401b8c0f18d6e2c55e1eea6c
Craig Hupin <althar93@hotmail.com>
2019-11-20 22:21:44 +0000

    Implemented 'Auto width' behaviour to the ItemList to complement the exiting 'Auto height'

--- editor/editor_about.cpp
+++ editor/editor_about.cpp
@@ -95,6 +95,7 @@ ScrollContainer *EditorAbout::_populate_list(const String &p_name, const List<St
 			ItemList *il = memnew(ItemList);
 			il->set_h_size_flags(Control::SIZE_EXPAND_FILL);
 			il->set_same_column_width(true);
+			il->set_auto_width(false);
 			il->set_auto_height(true);
 			il->set_mouse_filter(Control::MOUSE_FILTER_IGNORE);
 			il->add_constant_override("hseparation", 16 * EDSCALE);
