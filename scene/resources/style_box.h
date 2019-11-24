1120de862d131160cf43e2fdb7f736b47bc77550
PouleyKetchoupp <pouleyketchoup@gmail.com>
2019-11-24 16:26:30 +0100

    StyleBox preview adjusted to fit all drawn content
    This change allows StyleBox preview to take shadows and content margins into account to display how a whole panel would be rendered.
    
    The preview control clips contents so that in any case it doesn't bleed on controls around.
    
    Fixes #33801

--- scene/resources/style_box.h
+++ scene/resources/style_box.h
@@ -56,6 +56,7 @@ public:
 	float get_margin(Margin p_margin) const;
 	virtual Size2 get_center_size() const;
 
+	virtual Rect2 get_draw_rect(const Rect2 &p_rect) const;
 	virtual void draw(RID p_canvas_item, const Rect2 &p_rect) const = 0;
 
 	CanvasItem *get_current_item_drawn() const;
@@ -133,6 +134,7 @@ public:
 	void set_modulate(const Color &p_modulate);
 	Color get_modulate() const;
 
+	virtual Rect2 get_draw_rect(const Rect2 &p_rect) const;
 	virtual void draw(RID p_canvas_item, const Rect2 &p_rect) const;
 
 	StyleBoxTexture();
@@ -227,6 +229,7 @@ public:
 
 	virtual Size2 get_center_size() const;
 
+	virtual Rect2 get_draw_rect(const Rect2 &p_rect) const;
 	virtual void draw(RID p_canvas_item, const Rect2 &p_rect) const;
 
 	StyleBoxFlat();
