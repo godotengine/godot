29ba673fca58d2fb8a1adf226f7c663a82734475
bruvzg <7645683+bruvzg@users.noreply.github.com>
2019-12-08 18:29:31 +0200

    [macOS] Send resize event without actually resizing window on backing change.
    Co-authored-by: Haoyu Qiu <timothyqiu32@gmail.com>

--- platform/osx/os_osx.mm
+++ platform/osx/os_osx.mm
@@ -340,12 +340,8 @@ static Vector2 get_mouse_pos(NSPoint locationInWindow, CGFloat backingScaleFacto
 
 		//Update context
 		if (OS_OSX::singleton->main_loop) {
-			[OS_OSX::singleton->context update];
-
-			//Force window resize ???
-			NSRect frame = [OS_OSX::singleton->window_object frame];
-			[OS_OSX::singleton->window_object setFrame:NSMakeRect(frame.origin.x, frame.origin.y, 1, 1) display:YES];
-			[OS_OSX::singleton->window_object setFrame:frame display:YES];
+			//Force window resize event
+			[self windowDidResize:notification];
 		}
 	}
 }
