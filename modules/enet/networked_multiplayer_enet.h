2aafe2b25e11705fb9c9753f6e6ccf11afa14d02
Fabian <supagu@gmail.com>
2020-01-20 17:13:12 +1030

    Add set_peer_timeout to NetworkedMultiplayerENet.
    (cherry picked from commit 1b54de3039d39bae6ec381321888d211fafaef2e)

--- modules/enet/networked_multiplayer_enet.h
+++ modules/enet/networked_multiplayer_enet.h
@@ -129,6 +129,7 @@ public:
 
 	virtual IP_Address get_peer_address(int p_peer_id) const;
 	virtual int get_peer_port(int p_peer_id) const;
+	void set_peer_timeout(int p_peer_id, int p_timeout_limit, int p_timeout_min, int p_timeout_max);
 
 	Error create_server(int p_port, int p_max_clients = 32, int p_in_bandwidth = 0, int p_out_bandwidth = 0);
 	Error create_client(const String &p_address, int p_port, int p_in_bandwidth = 0, int p_out_bandwidth = 0, int p_client_port = 0);
