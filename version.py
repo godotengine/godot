9823dab9542e4c54d20c5269007dcf4bc43c263e
Rémi Verschelde <rverschelde@gmail.com>
2020-01-29 13:35:24 +0100

    Bump version to 4.0-dev
    No peace for the weary. :)

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
-major = 3
-minor = 2
+major = 4
+minor = 0
 patch = 0
-status = "stable"
+status = "dev"
 module_config = ""
 year = 2020
 website = "https://godotengine.org"
