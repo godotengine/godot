8737269275c371c8d56fcb56d783f8df1aaa2b88
Yuri Roubinsky <chaosus89@gmail.com>
2020-02-03 17:15:24 +0300

    Fix restoring window from fullscreen to normal on Linux

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -1429,7 +1429,7 @@ void OS_X11::set_window_fullscreen(bool p_enabled) {
 		set_window_maximized(true);
 	}
 	set_wm_fullscreen(p_enabled);
-	if (!p_enabled && !current_videomode.always_on_top) {
+	if (!p_enabled && current_videomode.always_on_top) {
 		// Restore
 		set_window_maximized(false);
 	}
