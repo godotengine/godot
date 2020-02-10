b58a600073b3a268ed0e9f166a2292b040f85b03
Yuri Sizov <yuris@humnom.net>
2020-02-10 16:04:15 +0300

    Add a button to quickly repeat last search in files

--- editor/find_in_files.h
+++ editor/find_in_files.h
@@ -163,6 +163,7 @@ protected:
 private:
 	void _on_result_found(String fpath, int line_number, int begin, int end, String text);
 	void _on_finished();
+	void _on_refresh_button_clicked();
 	void _on_cancel_button_clicked();
 	void _on_result_selected();
 	void _on_item_edited();
@@ -190,6 +191,7 @@ private:
 	Label *_search_text_label;
 	Tree *_results_display;
 	Label *_status_label;
+	Button *_refresh_button;
 	Button *_cancel_button;
 	ProgressBar *_progress_bar;
 	Map<String, TreeItem *> _file_items;
