791337721e76350294e2cf607baaa6917f1b749f
sashikknox <sashikknox@gmail.com>
2020-02-11 16:42:55 +0300

    in progress Sailfish Export

--- platform/sailfish/export/export.h
+++ platform/sailfish/export/export.h
@@ -27,7 +27,7 @@
 /* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
-#include <core/object.h>
+#include "core/object.h"
 #include <editor/editor_export.h>
 
 void register_sailfish_exporter();
