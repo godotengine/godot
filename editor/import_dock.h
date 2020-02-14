341ebd41b68158fe764824bedde840fac1a29cb1
Michael Alexsander <michaelalexsander@protonmail.com>
2020-02-13 22:58:19 -0300

    Fix multiple issues with the "Import" dock

--- editor/import_dock.h
+++ editor/import_dock.h
@@ -62,6 +62,7 @@ class ImportDock : public VBoxContainer {
 	void _preset_selected(int p_idx);
 	void _importer_selected(int i_idx);
 	void _update_options(const Ref<ConfigFile> &p_config = Ref<ConfigFile>());
+	void _update_preset_menu();
 
 	void _property_toggled(const StringName &p_prop, bool p_checked);
 	void _reimport_attempt();
