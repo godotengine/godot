c1dda0d0d5b5969e6921f328c6ddc0049674102c
Fabian Mathews <supagu@gmail.com>
2020-02-15 22:16:33 +1030

    Fixed overwrite of image format with incorrect format

--- servers/visual/rasterizer_rd/rasterizer_storage_rd.cpp
+++ servers/visual/rasterizer_rd/rasterizer_storage_rd.cpp
@@ -169,7 +169,6 @@ Ref<Image> RasterizerStorageRD::_validate_texture_format(const Ref<Image> &p_ima
 				image->convert(Image::FORMAT_RGBAH);
 			}
 
-			r_format.format = RD::DATA_FORMAT_R16G16B16A16_SFLOAT;
 			r_format.swizzle_r = RD::TEXTURE_SWIZZLE_R;
 			r_format.swizzle_g = RD::TEXTURE_SWIZZLE_G;
 			r_format.swizzle_b = RD::TEXTURE_SWIZZLE_B;
