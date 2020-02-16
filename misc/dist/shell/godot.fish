e7e6a84bb33f1feba71edcf2ca29a2975d42ec08
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-02-16 15:18:20 +0100

    Add a fish shell completion file for the Godot editor
    (cherry picked from commit 4d8dce160670b4f8e4db89e2dbca31980a0ef09d)

--- misc/dist/shell/godot.fish
+++ misc/dist/shell/godot.fish
@@ -25,7 +25,7 @@
 
 function godot_video_driver_args
     # Use a function instead of a fixed string to customize the argument descriptions.
-    echo -e "Vulkan\tVulkan renderer"
+    echo -e "GLES3\tOpenGL ES 2.0 renderer"
     echo -e "GLES2\tOpenGL ES 2.0 renderer"
 end
 
