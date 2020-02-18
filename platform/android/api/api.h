3430ac7a386b033840eaedad9fc69f80aaa0c54d
Twarit <wtwarit@gmail.com>
2020-02-19 00:04:27 +0530

    Add missing header guards in android api.h

--- platform/android/api/api.h
+++ platform/android/api/api.h
@@ -28,5 +28,10 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
+#ifndef API_H
+#define API_H
+
 void register_android_api();
 void unregister_android_api();
+
+#endif // API_H
