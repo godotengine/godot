43aa8a8940cc1f87a334f894243fb25f5906d4cd
Dominik 'dreamsComeTrue' Jasiński <dominikjasinski@o2.pl>
2020-03-01 09:44:48 +0100

    Take correct part of extension with File Dialog
    Fixes #36697

--- editor/editor_file_dialog.cpp
+++ editor/editor_file_dialog.cpp
@@ -691,7 +691,7 @@ void EditorFileDialog::update_file_name() {
 		String base_name = file_str.get_basename();
 		Vector<String> filter_substr = filter_str.split(";");
 		if (filter_substr.size() >= 2) {
-			file_str = base_name + "." + filter_substr[1].strip_edges().to_lower();
+			file_str = base_name + "." + filter_substr[0].strip_edges().lstrip("*.").to_lower();
 		} else {
 			file_str = base_name + "." + filter_str.get_extension().strip_edges().to_lower();
 		}
