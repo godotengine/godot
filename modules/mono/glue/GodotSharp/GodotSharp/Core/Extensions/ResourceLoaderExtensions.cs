93a5df05f258f5e20843b6762dec1f7c2bb4c1b7
Phischermen <kevfischermen@gmail.com>
2020-03-05 22:11:38 -0800

    Added parameters to Load()

--- modules/mono/glue/GodotSharp/GodotSharp/Core/Extensions/ResourceLoaderExtensions.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/Extensions/ResourceLoaderExtensions.cs
@@ -2,9 +2,9 @@ namespace Godot
 {
     public static partial class ResourceLoader
     {
-        public static T Load<T>(string path) where T : class
+        public static T Load<T>(string path, string typeHint = null, bool noCache = false) where T : class
         {
-            return (T)(object)Load(path);
+            return (T)(object)Load(path, typeHint, noCache);
         }
     }
 }
