4b9e1ff6ebf17ed60af94fe37eb29af0de4dda38
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-03-07 16:40:59 +0200

    Fix doctool misleading error message

--- main/main.cpp
+++ main/main.cpp
@@ -1620,7 +1620,7 @@ bool Main::start() {
 
 		{
 			DirAccessRef da = DirAccess::open(doc_tool);
-			ERR_FAIL_COND_V_MSG(!da, false, "Argument supplied to --doctool must be a base Godot build directory.");
+			ERR_FAIL_COND_V_MSG(!da, false, "Argument supplied to --doctool must be a valid directory path.");
 		}
 
 #ifndef MODULE_MONO_ENABLED
