c38af0ec1607b73f09bc812a4631fccf53b740e3
Rémi Verschelde <rverschelde@gmail.com>
2020-03-10 09:11:16 +0100

    mbedtls: Re-add patch to disable VIA padlock
    The comment mentioned a conflict with libwebsockets, but we actually
    still get this conflict even now that we don't use libwebsockets.
    Not sure what component is clashing but we should basically just keep
    this patch.
    
    Follow-up to #36823.

--- thirdparty/mbedtls/include/mbedtls/config.h
+++ thirdparty/mbedtls/include/mbedtls/config.h
@@ -2542,7 +2542,9 @@
  *
  * This modules adds support for the VIA PadLock on x86.
  */
-#define MBEDTLS_PADLOCK_C
+// -- GODOT start --
+// #define MBEDTLS_PADLOCK_C
+// -- GODOT end --
 
 /**
  * \def MBEDTLS_PEM_PARSE_C
