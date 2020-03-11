5dcc40130bc9b2c9ecadf0787521e853296b5ee4
Juan Linietsky <reduzio@gmail.com>
2020-03-11 09:50:20 -0300

    Windows support

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -10,10 +10,13 @@
 #include "drivers/unix/ip_unix.h"
 #include "drivers/wasapi/audio_driver_wasapi.h"
 #include "drivers/winmidi/midi_driver_winmidi.h"
+#include "joypad_windows.h"
 #include "key_mapping_windows.h"
 #include "servers/audio_server.h"
 #include "servers/visual/rasterizer.h"
+#include "servers/visual/rasterizer_rd/rasterizer_rd.h"
 #include "servers/visual_server.h"
+
 #ifdef XAUDIO2_ENABLED
 #include "drivers/xaudio2/audio_driver_xaudio2.h"
 #endif
@@ -93,7 +96,7 @@ typedef struct tagPOINTER_PEN_INFO {
 	INT32 tiltY;
 } POINTER_PEN_INFO;
 
-#endif
+#endif //POINTER_STRUCTURES
 
 typedef BOOL(WINAPI *GetPointerTypePtr)(uint32_t p_id, POINTER_INPUT_TYPE *p_type);
 typedef BOOL(WINAPI *GetPointerPenInfoPtr)(uint32_t p_id, POINTER_PEN_INFO *p_pen_info);
@@ -125,8 +128,19 @@ class DisplayServerWindows : public DisplayServer {
 	static GetPointerTypePtr win8p_GetPointerType;
 	static GetPointerPenInfoPtr win8p_GetPointerPenInfo;
 
+	void GetMaskBitmaps(HBITMAP hSourceBitmap, COLORREF clrTransparent, OUT HBITMAP &hAndMaskBitmap, OUT HBITMAP &hXorMaskBitmap);
+
 	enum {
 		KEY_EVENT_BUFFER_SIZE = 512
+	};
+
+	struct KeyEvent {
+
+		WindowID window_id;
+		bool alt, shift, control, meta;
+		UINT uMsg;
+		WPARAM wParam;
+		LPARAM lParam;
 	};
 
 	KeyEvent key_event_buffer[KEY_EVENT_BUFFER_SIZE];
@@ -150,6 +164,7 @@ class DisplayServerWindows : public DisplayServer {
 
 	int pressrc;
 	HINSTANCE hInstance; // Holds The Instance Of The Application
+	String rendering_driver;
 
 	struct WindowData {
 		HWND hWnd;
@@ -160,9 +175,12 @@ class DisplayServerWindows : public DisplayServer {
 		RECT pre_fs_rect;
 		bool maximized = false;
 		bool minimized = false;
+		bool fullscreen = false;
 		bool borderless = false;
+		bool resizable = true;
 		bool window_focused = false;
 		bool was_maximized = false;
+		bool always_on_top = false;
 		bool window_has_focus = false;
 
 		HBITMAP hBitmap; //DIB section for layered window
@@ -171,25 +189,42 @@ class DisplayServerWindows : public DisplayServer {
 		HDC hDC_dib;
 		Size2 min_size;
 		Size2 max_size;
+		int width = 0, height = 0;
 
 		Size2 window_rect;
+		Point2 last_pos;
 
 		// IME
 		HIMC im_himc;
 		Vector2 im_position;
 
 		bool layered_window = false;
+
+		Callable resize_callback;
+		Callable event_callback;
+		Callable input_event_callback;
+		Callable input_text_callback;
+		Callable drop_files_callback;
+
+		WindowID transient_parent = INVALID_WINDOW_ID;
+		Set<WindowID> transient_children;
 	};
 
+	JoypadWindows *joypad;
+
+	WindowID _create_window(WindowMode p_mode, const Rect2i &p_rect);
+	WindowID window_id_counter = MAIN_WINDOW_ID;
 	Map<WindowID, WindowData> windows;
 
-	Point2 last_pos;
+	WindowID last_focused_window = INVALID_WINDOW_ID;
 
 	uint32_t move_timer_id;
 
 	HCURSOR hCursor;
 
 	WNDPROC user_proc;
+
+	void _send_window_event(const WindowData &wd, WindowEvent p_event);
 
 	MouseMode mouse_mode;
 	bool alt_mem = false;
@@ -203,26 +238,142 @@ class DisplayServerWindows : public DisplayServer {
 	bool drop_events = false;
 	bool console_visible = false;
 
+	WNDCLASSEXW wc;
+
 	HCURSOR cursors[CURSOR_MAX] = { NULL };
 	CursorShape cursor_shape;
 	Map<CursorShape, Vector<Variant> > cursors_cache;
 
-	void _drag_event(float p_x, float p_y, int idx);
-	void _touch_event(bool p_pressed, float p_x, float p_y, int idx);
+	void _drag_event(WindowID p_window, float p_x, float p_y, int idx);
+	void _touch_event(WindowID p_window, bool p_pressed, float p_x, float p_y, int idx);
 
-	void _update_window_style(bool p_repaint = true, bool p_maximized = false);
+	void _update_window_style(WindowID p_window, bool p_repaint = true, bool p_maximized = false);
+
+	void _update_real_mouse_position(WindowID p_window);
 
 	void _set_mouse_mode_impl(MouseMode p_mode);
 
-	struct ProcessInfo {
+	void _process_key_events();
 
-		STARTUPINFO si;
-		PROCESS_INFORMATION pi;
-	};
-	Map<ProcessID, ProcessInfo> *process_map;
+	static void _dispatch_input_events(const Ref<InputEvent> &p_event);
+	void _dispatch_input_event(const Ref<InputEvent> &p_event);
 
 public:
+	LRESULT WndProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
+
+	virtual bool has_feature(Feature p_feature) const;
+	virtual String get_name() const;
+
+	virtual void alert(const String &p_alert, const String &p_title = "ALERT!");
+
+	virtual void mouse_set_mode(MouseMode p_mode);
+	virtual MouseMode mouse_get_mode() const;
+
+	virtual void mouse_warp_to_position(const Point2i &p_to);
+	virtual Point2i mouse_get_position() const;
+	virtual int mouse_get_button_state() const;
+
+	virtual void clipboard_set(const String &p_text);
+	virtual String clipboard_get() const;
+
+	virtual int get_screen_count() const;
+	virtual Point2i screen_get_position(int p_screen = SCREEN_OF_MAIN_WINDOW) const;
+	virtual Size2i screen_get_size(int p_screen = SCREEN_OF_MAIN_WINDOW) const;
+	virtual int screen_get_dpi(int p_screen = SCREEN_OF_MAIN_WINDOW) const;
+	virtual bool screen_is_touchscreen(int p_screen = SCREEN_OF_MAIN_WINDOW) const;
+
+	virtual void screen_set_orientation(ScreenOrientation p_orientation, int p_screen = SCREEN_OF_MAIN_WINDOW);
+	ScreenOrientation screen_get_orientation(int p_screen = SCREEN_OF_MAIN_WINDOW) const;
+
+	virtual void screen_set_keep_on(bool p_enable); //disable screensaver
+	virtual bool screen_is_kept_on() const;
+
+	virtual Vector<int> get_window_list() const;
+
+	virtual WindowID create_sub_window(WindowMode p_mode, uint32_t p_flags, const Rect2i &p_rect = Rect2i());
+	virtual void delete_sub_window(WindowID p_window);
+
+	virtual void window_set_resize_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual void window_set_window_event_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID);
+	virtual void window_set_input_event_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID);
+	virtual void window_set_input_text_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual void window_set_drop_files_callback(const Callable &p_callable, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual void window_set_title(const String &p_title, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual int window_get_current_screen(WindowID p_window = MAIN_WINDOW_ID) const;
+	virtual void window_set_current_screen(int p_screen, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual Point2i window_get_position(WindowID p_window = MAIN_WINDOW_ID) const;
+	virtual void window_set_position(const Point2i &p_position, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual void window_set_transient(WindowID p_window, WindowID p_parent);
+
+	virtual void window_set_max_size(const Size2i p_size, WindowID p_window = MAIN_WINDOW_ID);
+	virtual Size2i window_get_max_size(WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual void window_set_min_size(const Size2i p_size, WindowID p_window = MAIN_WINDOW_ID);
+	virtual Size2i window_get_min_size(WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual void window_set_size(const Size2i p_size, WindowID p_window = MAIN_WINDOW_ID);
+	virtual Size2i window_get_size(WindowID p_window = MAIN_WINDOW_ID) const;
+	virtual Size2i window_get_real_size(WindowID p_window = MAIN_WINDOW_ID) const; //wtf is this? should probable use proper name
+
+	virtual void window_set_mode(WindowMode p_mode, WindowID p_window = MAIN_WINDOW_ID);
+	virtual WindowMode window_get_mode(WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual bool window_is_maximize_allowed(WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual void window_set_flag(WindowFlags p_flag, bool p_enabled, WindowID p_window = MAIN_WINDOW_ID);
+	virtual bool window_get_flag(WindowFlags p_flag, WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual void window_request_attention(WindowID p_window = MAIN_WINDOW_ID);
+	virtual void window_move_to_foreground(WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual bool window_can_draw(WindowID p_window = MAIN_WINDOW_ID) const;
+
+	virtual bool can_any_window_draw() const;
+
+	virtual void window_set_ime_active(const bool p_active, WindowID p_window = MAIN_WINDOW_ID);
+	virtual void window_set_ime_position(const Point2i &p_pos, WindowID p_window = MAIN_WINDOW_ID);
+
+	virtual void console_set_visible(bool p_enabled);
+	virtual bool is_console_visible() const;
+
+	virtual void cursor_set_shape(CursorShape p_shape);
+	virtual CursorShape cursor_get_shape() const;
+	virtual void cursor_set_custom_image(const RES &p_cursor, CursorShape p_shape = CURSOR_ARROW, const Vector2 &p_hotspot = Vector2());
+
+	virtual bool get_swap_ok_cancel();
+
+	virtual void enable_for_stealing_focus(OS::ProcessID pid);
+
+	virtual LatinKeyboardVariant get_latin_keyboard_variant() const;
+
+	virtual void process_events();
+
+	virtual void force_process_and_drop_events();
+
+	virtual void release_rendering_thread();
+	virtual void make_rendering_thread();
+	virtual void swap_buffers();
+
+	virtual void set_native_icon(const String &p_filename);
+	virtual void set_icon(const Ref<Image> &p_icon);
+
+	virtual void vsync_set_use_via_compositor(bool p_enable);
+	virtual bool vsync_is_using_via_compositor() const;
+
+	virtual void set_context(Context p_context);
+
+	static DisplayServer *create_func(const String &p_rendering_driver, WindowMode p_mode, uint32_t p_flags, const Vector2i &p_resolution, Error &r_error);
+	static Vector<String> get_rendering_drivers_func();
+	static void register_windows_driver();
+
 	DisplayServerWindows(const String &p_rendering_driver, WindowMode p_mode, uint32_t p_flags, const Vector2i &p_resolution, Error &r_error);
+	~DisplayServerWindows();
 };
 
 #endif // DISPLAY_SERVER_WINDOWS_H
--- servers/display_server.cpp
+++ servers/display_server.cpp
@@ -4,6 +4,8 @@
 DisplayServer *DisplayServer::singleton = nullptr;
 DisplayServer::SwitchVSyncCallbackInThread DisplayServer::switch_vsync_function = nullptr;
 
+bool DisplayServer::hidpi_allowed = false;
+
 DisplayServer::DisplayServerCreate DisplayServer::server_create_functions[DisplayServer::MAX_SERVERS];
 int DisplayServer::server_create_count = 0;
 
