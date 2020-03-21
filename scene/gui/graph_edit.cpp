c9ff175871f3208a68263c9b33b0b13d4867831e
Thakee Nathees <thakeenathees@gmail.com>
2020-03-21 22:41:32 +0530

    GraphEdit use mousewheel for zoom implemented
    in GraphEdit the mousewheel is used for scrolling
    which seems a bit annoying, since godot editor uses
    mousewheel for zooming.

--- scene/gui/graph_edit.cpp
+++ scene/gui/graph_edit.cpp
@@ -1016,26 +1016,11 @@ void GraphEdit::_gui_input(const Ref<InputEvent> &p_ev) {
 			top_layer->update();
 		}
 
-		if (b->get_button_index() == BUTTON_WHEEL_UP && b->is_pressed()) {
-			//too difficult to get right
-			//set_zoom(zoom*ZOOM_SCALE);
+		if (b->get_button_index() == BUTTON_WHEEL_UP) {
+			set_zoom(zoom * ZOOM_SCALE);
 		}
-
-		if (b->get_button_index() == BUTTON_WHEEL_DOWN && b->is_pressed()) {
-			//too difficult to get right
-			//set_zoom(zoom/ZOOM_SCALE);
-		}
-		if (b->get_button_index() == BUTTON_WHEEL_UP && !Input::get_singleton()->is_key_pressed(KEY_SHIFT)) {
-			v_scroll->set_value(v_scroll->get_value() - v_scroll->get_page() * b->get_factor() / 8);
-		}
-		if (b->get_button_index() == BUTTON_WHEEL_DOWN && !Input::get_singleton()->is_key_pressed(KEY_SHIFT)) {
-			v_scroll->set_value(v_scroll->get_value() + v_scroll->get_page() * b->get_factor() / 8);
-		}
-		if (b->get_button_index() == BUTTON_WHEEL_RIGHT || (b->get_button_index() == BUTTON_WHEEL_DOWN && Input::get_singleton()->is_key_pressed(KEY_SHIFT))) {
-			h_scroll->set_value(h_scroll->get_value() + h_scroll->get_page() * b->get_factor() / 8);
-		}
-		if (b->get_button_index() == BUTTON_WHEEL_LEFT || (b->get_button_index() == BUTTON_WHEEL_UP && Input::get_singleton()->is_key_pressed(KEY_SHIFT))) {
-			h_scroll->set_value(h_scroll->get_value() - h_scroll->get_page() * b->get_factor() / 8);
+		if (b->get_button_index() == BUTTON_WHEEL_DOWN) {
+			set_zoom(zoom / ZOOM_SCALE);
 		}
 	}
 
