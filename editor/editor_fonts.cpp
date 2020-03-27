21c211674417e80a67bf1a5bc2abaebfe67fdc33
bruvzg <7645683+bruvzg@users.noreply.github.com>
2020-03-27 14:13:01 +0200

    Fix default editor font. Fix popup_centered_clamped definition typo.

--- editor/editor_fonts.cpp
+++ editor/editor_fonts.cpp
@@ -231,7 +231,7 @@ void editor_register_fonts(Ref<Theme> p_theme) {
 
 	// Default font
 	MAKE_DEFAULT_FONT(df, default_font_size);
-	p_theme->set_default_theme_font(df);
+	p_theme->set_font("font", "Node", df); // Default theme font
 	p_theme->set_font("main", "EditorFonts", df);
 
 	// Bold font
