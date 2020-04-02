c677ff21f162bf92a432ccd180f5ec569e21188b
HaSa1002 <johawitt@outlook.de>
2020-04-02 16:06:04 +0200

    Fix input event being dispatched multiple times on Windows

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -388,6 +388,7 @@ class DisplayServerWindows : public DisplayServer {
 	uint32_t last_button_state = 0;
 	bool use_raw_input = false;
 	bool drop_events = false;
+	bool in_dispatch_input_event = false;
 	bool console_visible = false;
 
 	WNDCLASSEXW wc;
