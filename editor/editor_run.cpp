4ff99cb51b5122fb2b45e101e3ef87fbd3b0eca7
TerraAr <37753757+TerraAr@users.noreply.github.com>
2020-04-03 01:57:29 -0300

    Code optimization
    Two code optimizations implemented:
    
    - Exchanged a if-else structure for a switch, to make the compiler use the switch's jump table, and
    - Exchanged a printf("\n") to a puts(""), that's faster

--- editor/editor_run.cpp
+++ editor/editor_run.cpp
@@ -69,25 +69,30 @@ Error EditorRun::run(const String &p_scene, const String &p_custom_args, const L
 	}
 
 	int screen = EditorSettings::get_singleton()->get("run/window_placement/screen");
-	if (screen == 0) {
-		// Same as editor
-		screen = DisplayServer::get_singleton()->window_get_current_screen();
-	} else if (screen == 1) {
-		// Previous monitor (wrap to the other end if needed)
-		screen = Math::wrapi(
-				DisplayServer::get_singleton()->window_get_current_screen() - 1,
-				0,
-				DisplayServer::get_singleton()->get_screen_count());
-	} else if (screen == 2) {
-		// Next monitor (wrap to the other end if needed)
-		screen = Math::wrapi(
-				DisplayServer::get_singleton()->window_get_current_screen() + 1,
-				0,
-				DisplayServer::get_singleton()->get_screen_count());
-	} else {
-		// Fixed monitor ID
-		// There are 3 special options, so decrement the option ID by 3 to get the monitor ID
-		screen -= 3;
+	switch (screen) {
+		case 0:
+			// Same as editor
+			screen = DisplayServer::get_singleton()->window_get_current_screen();
+			break;
+		case 1:
+			// Previous monitor (wrap to the other end if needed)
+			screen = Math::wrapi(
+					DisplayServer::get_singleton()->window_get_current_screen() - 1,
+					0,
+					DisplayServer::get_singleton()->get_screen_count());
+			break;
+		case 2:
+			// Next monitor (wrap to the other end if needed)
+			screen = Math::wrapi(
+					DisplayServer::get_singleton()->window_get_current_screen() + 1,
+					0,
+					DisplayServer::get_singleton()->get_screen_count());
+			break;
+		default:
+			// Fixed monitor ID
+			// There are 3 special options, so decrement the option ID by 3 to get the monitor ID
+			screen -= 3;
+			break;
 	}
 
 	if (OS::get_singleton()->is_disable_crash_handler()) {
@@ -190,7 +195,7 @@ Error EditorRun::run(const String &p_scene, const String &p_custom_args, const L
 
 		printf(" %ls", E->get().c_str());
 	};
-	printf("\n");
+	puts("");
 
 	int instances = EditorSettings::get_singleton()->get_project_metadata("debug_options", "run_debug_instances", 1);
 	for (int i = 0; i < instances; i++) {
