1d807f272af561339e404bc8d90e12c6cacc614c
Yuri Sizov <pycbouh@users.noreply.github.com>
2020-04-05 17:19:24 +0300

    Expose methods to play scene from plugin code

--- editor/editor_run.h
+++ editor/editor_run.h
@@ -46,9 +46,11 @@ public:
 
 private:
 	Status status;
+	String running_scene;
 
 public:
 	Status get_status() const;
+	String get_running_scene() const;
 	Error run(const String &p_scene, const String &p_custom_args, const List<String> &p_breakpoints, const bool &p_skip_breakpoints = false);
 	void run_native_notify() { status = STATUS_PLAY; }
 	void stop();
