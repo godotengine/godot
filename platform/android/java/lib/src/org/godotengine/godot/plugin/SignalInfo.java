d0ced7a481fb6cec9f58f234c28cd0ad946f855b
thebestnom <shoval.arad@gmail.com>
2020-04-16 11:48:53 +0300

    Android: Migrate deprecated support library to AndroidX

--- platform/android/java/lib/src/org/godotengine/godot/plugin/SignalInfo.java
+++ platform/android/java/lib/src/org/godotengine/godot/plugin/SignalInfo.java
@@ -30,8 +30,9 @@
 
 package org.godotengine.godot.plugin;
 
-import android.support.annotation.NonNull;
 import android.text.TextUtils;
+
+import androidx.annotation.NonNull;
 
 import java.util.Arrays;
 
