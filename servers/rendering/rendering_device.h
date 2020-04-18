c44959d99c430345d4ed3b7657f4999d12250f7b
Juan Linietsky <reduzio@gmail.com>
2020-04-18 20:30:57 -0300

    Ability to create local RenderingDevice instances.

--- servers/rendering/rendering_device.h
+++ servers/rendering/rendering_device.h
@@ -1024,6 +1024,11 @@ public:
 
 	virtual uint32_t get_frame_delay() const = 0;
 
+	virtual void submit() = 0;
+	virtual void sync() = 0;
+
+	virtual RenderingDevice *create_local_device() = 0;
+
 	static RenderingDevice *get_singleton();
 	RenderingDevice();
 };
