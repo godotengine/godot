3cf9f739cc7beb97863e491ab1fe64eed64df3c7
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-04-26 15:41:19 +0200

    Increase the default Camera3D field of view to 75
    A vertical FOV of 75 degrees is roughly equivalent to a 91 degree
    horizontal FOV on a 4:3 display (~107.51 degrees on 16:9),
    which is close to the typical default FOV used in PC games.
    
    Note that this doesn't apply to the in-editor camera which keeps its
    FOV to 70. This is because it doesn't display in fullscreen;
    its viewport only displays in the center of the editor (roughly).
    This means the viewport won't cover the viewer's eyes as much. Therefore,
    the editor camera FOV should be slightly lower to account for this.
    
    Since this changes the default value, this may break existing projects
    slightly.
    
    For the record, this was already done in
    https://github.com/godotengine/godot-demo-projects/pull/260
    for the official demo projects.

--- scene/3d/camera_3d.cpp
+++ scene/3d/camera_3d.cpp
@@ -689,7 +689,7 @@ Camera3D::Camera3D() {
 	viewport = nullptr;
 	force_change = false;
 	mode = PROJECTION_PERSPECTIVE;
-	set_perspective(70.0, 0.05, 100.0);
+	set_perspective(75.0, 0.05, 100.0);
 	keep_aspect = KEEP_HEIGHT;
 	layers = 0xfffff;
 	v_offset = 0;
