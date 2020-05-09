c32548413406975e8e39af3416ac113f0437e88a
Marcus Elg <marcusaccounts@yahoo.se>
2020-05-09 14:24:46 +0200

    Rename Lineshapes d to distance

--- scene/resources/line_shape_2d.cpp
+++ scene/resources/line_shape_2d.cpp
@@ -35,7 +35,7 @@
 
 bool LineShape2D::_edit_is_selected_on_click(const Point2 &p_point, double p_tolerance) const {
 
-	Vector2 point = get_d() * get_normal();
+	Vector2 point = get_distance() * get_normal();
 	Vector2 l[2][2] = { { point - get_normal().tangent() * 100, point + get_normal().tangent() * 100 }, { point, point + get_normal() * 30 } };
 
 	for (int i = 0; i < 2; i++) {
@@ -51,7 +51,7 @@ void LineShape2D::_update_shape() {
 
 	Array arr;
 	arr.push_back(normal);
-	arr.push_back(d);
+	arr.push_back(distance);
 	PhysicsServer2D::get_singleton()->shape_set_data(get_rid(), arr);
 	emit_changed();
 }
@@ -62,9 +62,9 @@ void LineShape2D::set_normal(const Vector2 &p_normal) {
 	_update_shape();
 }
 
-void LineShape2D::set_d(real_t p_d) {
+void LineShape2D::set_distance(real_t p_distance) {
 
-	d = p_d;
+	distance = p_distance;
 	_update_shape();
 }
 
@@ -72,14 +72,14 @@ Vector2 LineShape2D::get_normal() const {
 
 	return normal;
 }
-real_t LineShape2D::get_d() const {
+real_t LineShape2D::get_distance() const {
 
-	return d;
+	return distance;
 }
 
 void LineShape2D::draw(const RID &p_to_rid, const Color &p_color) {
 
-	Vector2 point = get_d() * get_normal();
+	Vector2 point = get_distance() * get_normal();
 
 	Vector2 l1[2] = { point - get_normal().tangent() * 100, point + get_normal().tangent() * 100 };
 	RS::get_singleton()->canvas_item_add_line(p_to_rid, l1[0], l1[1], p_color, 3);
@@ -88,7 +88,7 @@ void LineShape2D::draw(const RID &p_to_rid, const Color &p_color) {
 }
 Rect2 LineShape2D::get_rect() const {
 
-	Vector2 point = get_d() * get_normal();
+	Vector2 point = get_distance() * get_normal();
 
 	Vector2 l1[2] = { point - get_normal().tangent() * 100, point + get_normal().tangent() * 100 };
 	Vector2 l2[2] = { point, point + get_normal() * 30 };
@@ -101,7 +101,7 @@ Rect2 LineShape2D::get_rect() const {
 }
 
 real_t LineShape2D::get_enclosing_radius() const {
-	return d;
+	return distance;
 }
 
 void LineShape2D::_bind_methods() {
@@ -109,17 +109,17 @@ void LineShape2D::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_normal", "normal"), &LineShape2D::set_normal);
 	ClassDB::bind_method(D_METHOD("get_normal"), &LineShape2D::get_normal);
 
-	ClassDB::bind_method(D_METHOD("set_d", "d"), &LineShape2D::set_d);
-	ClassDB::bind_method(D_METHOD("get_d"), &LineShape2D::get_d);
+	ClassDB::bind_method(D_METHOD("set_distance", "distance"), &LineShape2D::set_distance);
+	ClassDB::bind_method(D_METHOD("get_distance"), &LineShape2D::get_distance);
 
 	ADD_PROPERTY(PropertyInfo(Variant::VECTOR2, "normal"), "set_normal", "get_normal");
-	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "d"), "set_d", "get_d");
+	ADD_PROPERTY(PropertyInfo(Variant::FLOAT, "distance"), "set_distance", "get_distance");
 }
 
 LineShape2D::LineShape2D() :
 		Shape2D(PhysicsServer2D::get_singleton()->line_shape_create()) {
 
 	normal = Vector2(0, 1);
-	d = 0;
+	distance = 0;
 	_update_shape();
 }
