3e10392d4845d8f3aa3a271186812377023cc19f
Thakee Nathees <thakeenathees@gmail.com>
2020-05-10 10:07:23 +0530

    range() with non-numeric const argument crash fix

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -3160,6 +3160,8 @@ void GDScriptParser::_parse_block(BlockNode *p_block, bool p_static) {
 								ConstantNode *c = static_cast<ConstantNode *>(op->arguments[i]);
 								if (c->value.get_type() == Variant::FLOAT || c->value.get_type() == Variant::INT) {
 									constants.push_back(c->value);
+								} else {
+									constant = false;
 								}
 							} else {
 								constant = false;
