22e51de6b601e1a9eb660de68d482b656352cf7f
TwistedTwigleg <beard.noah@gmail.com>
2020-05-26 14:17:11 -0400

    Made low level changes to the Skeleton3D class and Skeleton3D inspector. Changes listed below:
    * Added helper functions to Skeleton3D for converting transforms from bone space to global space, and vice versa.
    * Updated the Skeleton3D class reference.
    * Changed the icon used for bones in the Skeleton3D inspector to use BoneAttachement3D's icon.
    * Changed the Skeleton3D inspector to use EditorPropertyTransform and EditorPropertyVector3 when possible.
    * Placed the Transform/Matrix for each bone in a sub-section, so it is visually similar to the Node3D inspector.

--- editor/editor_properties.h
+++ editor/editor_properties.h
@@ -392,6 +392,8 @@ protected:
 
 public:
 	virtual void update_property();
+	virtual void update_using_vector(Vector3 p_vector);
+	virtual Vector3 get_vector();
 	void setup(double p_min, double p_max, double p_step, bool p_no_slider);
 	EditorPropertyVector3(bool p_force_wide = false);
 };
@@ -536,6 +538,7 @@ protected:
 
 public:
 	virtual void update_property();
+	virtual void update_using_transform(Transform p_transform);
 	void setup(double p_min, double p_max, double p_step, bool p_no_slider);
 	EditorPropertyTransform();
 };
