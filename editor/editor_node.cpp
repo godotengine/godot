8cc3d4bda694abe08d44c2f41ea65381e061b402
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-05-30 22:55:56 +0200

    Keep the bottom panel visible when enabling the distraction-free mode
    - Document the `EditorInterface.distraction_free_mode` property.
    
    This closes https://github.com/godotengine/godot-proposals/issues/951.
    
    (cherry picked from commit e6352d1daae4295e6f1ac6c9624e2d84fbc54f15)

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -4296,7 +4296,6 @@ void EditorNode::_update_dock_slots_visibility() {
 		}
 
 		right_hsplit->hide();
-		bottom_panel->hide();
 	} else {
 		for (int i = 0; i < DOCK_SLOT_MAX; i++) {
 
@@ -4326,7 +4325,6 @@ void EditorNode::_update_dock_slots_visibility() {
 				dock_slot[i]->set_current_tab(0);
 			}
 		}
-		bottom_panel->show();
 
 		if (right_l_vsplit->is_visible() || right_r_vsplit->is_visible())
 			right_hsplit->show();
