ab62fda53af8f79976401175c26bf8a3a1a8460b
HaSa1002 <johawitt@outlook.de>
2020-06-03 18:59:43 +0200

    Abstract all big chunks of _process_line into functions

--- scene/gui/bbcode.h
+++ scene/gui/bbcode.h
@@ -1,10 +1,42 @@
+/*************************************************************************/
+/*  panel.h                                                              */
+/*************************************************************************/
+/*                       This file is part of:                           */
+/*                           GODOT ENGINE                                */
+/*                      https://godotengine.org                          */
+/*************************************************************************/
+/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/*                                                                       */
+/* Permission is hereby granted, free of charge, to any person obtaining */
+/* a copy of this software and associated documentation files (the       */
+/* "Software"), to deal in the Software without restriction, including   */
+/* without limitation the rights to use, copy, modify, merge, publish,   */
+/* distribute, sublicense, and/or sell copies of the Software, and to    */
+/* permit persons to whom the Software is furnished to do so, subject to */
+/* the following conditions:                                             */
+/*                                                                       */
+/* The above copyright notice and this permission notice shall be        */
+/* included in all copies or substantial portions of the Software.       */
+/*                                                                       */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,       */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF    */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.*/
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY  */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,  */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE     */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
+/*************************************************************************/
+
+#ifndef BBCODE_H
+#define BBCODE_H
 
 #include "core/math/vector2.h"
 #include "rich_text_effect.h"
 #include "scene/gui/rich_text_label.h"
 
-class BbCodeParser : Control {
-	GDCLASS(BbCodeParser, Control);
+class BbCodeParser : Label {
+	GDCLASS(BbCodeParser, Label);
 	//We use the classes from RTL here, to have a quick dependency overview
 	//Later, we should move them over here, if we no longer have dependencies in the RTL
 	using Line = RichTextLabel::Line;
@@ -30,16 +62,20 @@ class BbCodeParser : Control {
 	using ItemImage = RichTextLabel::ItemImage;
 	using ItemTable = RichTextLabel::ItemTable;
 
+protected:
+	static void _bind_methods();
+
 private:
 	//localy declared vars in _process_line
+	//Actual line processed by process line (determined using p_frame and p_line)
 	Line &l = Line();
 	Item *it = nullptr;
+	RID ci = get_canvas_item();
 	int line_ofs = 0;
 	int margin = 0;
 	Align align = Align::ALIGN_LEFT;
 	int line = 0;
 	int spaces = 0;
-	int height = 0;
 	int wofs = 0;
 	int spaces_size = 0;
 	int align_ofs = 0;
@@ -70,6 +106,13 @@ private:
 	bool _advance(int m_width); //rename?
 	void _check_height(int m_height); //rename?
 	bool _y_range_visible(int m_top, int m_height);
+
+private:
+	//abstraction functions
+	bool _parse_text(ItemText *text);
+	bool _parse_table(ItemTable *table);
+	bool _parse_image(ItemImage *img);
+	bool _parse_detect_click(Item *previous_item);
 
 private:
 	//Copied functions
@@ -129,3 +172,5 @@ public:
 	BbCodeParser();
 	~BbCodeParser();
 };
+
+#endif // BBCODE_H
