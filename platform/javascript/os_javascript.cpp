8b321354d8913874c328a6bd2fffd94bbb51a5b8
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2020-06-04 19:59:45 +0200

    Swtich HTML5 key detection from keyCode to code.
    The value of this, does not include the layout.
    The code has extra logic to map the unicode value to our keylist,
    supporting ASCII and Latin-1.
    
    (cherry picked from commit 0a35b97b62dc1f275a8c84397fe8319ecfc233e7
    with help of https://github.com/Faless/godot/tree/js/keyCode)

--- platform/javascript/os_javascript.cpp
+++ platform/javascript/os_javascript.cpp
@@ -251,7 +251,7 @@ static Ref<InputEventKey> setup_key_event(const EmscriptenKeyboardEvent *emscrip
 	ev.instance();
 	ev->set_echo(emscripten_event->repeat);
 	dom2godot_mod(emscripten_event, ev);
-	ev->set_scancode(dom2godot_scancode(emscripten_event->keyCode));
+	ev->set_scancode(dom_code2godot_scancode(emscripten_event->code, emscripten_event->key));
 
 	String unicode = String::utf8(emscripten_event->key);
 	// Check if empty or multi-character (e.g. `CapsLock`).
