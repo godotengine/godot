aeb5513babbb1840c4c210bd534a2c2bf3b4400f
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-06-04 22:30:56 +0200

    Tweak the GDScript error message about passed argument type mismatch
    This makes it less confusing.
    
    This closes https://github.com/godotengine/godot-proposals/issues/670.
    
    (cherry picked from commit 30053de1824c932a6b9f3620b46f8ba056a43c2c)

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -7375,8 +7375,8 @@ GDScriptParser::DataType GDScriptParser::_reduce_function_call_type(const Operat
 		} else if (!_is_type_compatible(arg_types[i - arg_diff], par_type, true)) {
 			// Supertypes are acceptable for dynamic compliance
 			if (!_is_type_compatible(par_type, arg_types[i - arg_diff])) {
-				_set_error("At \"" + callee_name + "()\" call, argument " + itos(i - arg_diff + 1) + ". Assigned type (" +
-								   par_type.to_string() + ") doesn't match the function argument's type (" +
+				_set_error("At \"" + callee_name + "()\" call, argument " + itos(i - arg_diff + 1) + ". The passed argument's type (" +
+								   par_type.to_string() + ") doesn't match the function's expected argument type (" +
 								   arg_types[i - arg_diff].to_string() + ").",
 						p_call->line);
 				return DataType();
