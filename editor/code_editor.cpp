727b0d951e4dfb5a85b9f7b9da2713cb034c7dbc
Maganty Rushyendra <mrushyendra@yahoo.com.sg>
2020-06-12 17:28:43 +0800

    Fix whole word search slowdown in editor
    Reduce repeated iteration through the full text
    when counting the number of occurrences of whole
    words while searching a file in the editor.

--- editor/code_editor.cpp
+++ editor/code_editor.cpp
@@ -308,7 +308,7 @@ void FindReplaceBar::_update_results_count() {
 		}
 
 		if (is_whole_words()) {
-			from_pos++; // Making sure we won't hit the same match next time, if we get out via a continue.
+			from_pos = pos + 1; // Making sure we won't hit the same match next time, if we get out via a continue.
 			if (pos > 0 && !is_symbol(full_text[pos - 1])) {
 				continue;
 			}
