175d43738ac58afcf2d0531177eefeb260c4f296
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2020-06-15 00:46:13 -0700

    Enable the ability to use Godot as a subview within an Android app

--- platform/android/java/lib/src/org/godotengine/godot/FullScreenGodotApp.java
+++ platform/android/java/lib/src/org/godotengine/godot/FullScreenGodotApp.java
@@ -1,5 +1,5 @@
 /*************************************************************************/
-/*  GodotApp.java                                                        */
+/*  FullScreenGodotApp.java                                              */
 /*************************************************************************/
 /*                       This file is part of:                           */
 /*                           GODOT ENGINE                                */
@@ -28,13 +28,52 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
-package com.godot.game;
+package org.godotengine.godot;
 
-import org.godotengine.godot.FullScreenGodotApp;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.KeyEvent;
+
+import androidx.fragment.app.FragmentActivity;
 
 /**
- * Template activity for Godot Android custom builds.
- * Feel free to extend and modify this class for your custom logic.
+ * Base activity for Android apps intending to use Godot as the primary and only screen.
+ *
+ * It's also a reference implementation for how to setup and use the {@link Godot} fragment
+ * within an Android app.
  */
-public class GodotApp extends FullScreenGodotApp {
+public abstract class FullScreenGodotApp extends FragmentActivity {
+	protected Godot godotFragment;
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.godot_app_layout);
+		godotFragment = new Godot();
+		getSupportFragmentManager().beginTransaction().replace(R.id.godot_fragment_container, godotFragment).setPrimaryNavigationFragment(godotFragment).commitNowAllowingStateLoss();
+	}
+
+	@Override
+	public void onNewIntent(Intent intent) {
+		if (godotFragment != null) {
+			godotFragment.onNewIntent(intent);
+		}
+	}
+
+	@Override
+	public void onBackPressed() {
+		if (godotFragment != null) {
+			godotFragment.onBackPressed();
+		} else {
+			super.onBackPressed();
+		}
+	}
+
+	@Override
+	public boolean onKeyMultiple(final int inKeyCode, int repeatCount, KeyEvent event) {
+		if (godotFragment != null && godotFragment.onKeyMultiple(inKeyCode, repeatCount, event)) {
+			return true;
+		}
+		return super.onKeyMultiple(inKeyCode, repeatCount, event);
+	}
 }
