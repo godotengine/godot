c02dd3f103f21912571d892cf59a30a8d10642b3
--==RIX==-- <zeze0556@gmail.com>
2020-06-21 15:38:05 +0800

    fix #22136 JavaClassWrapper.wrap should be ok
    Class function called also should be ok
    Class.new function also should be ok
    All of function in plugins also should be visited.

--- platform/android/jni_utils.h
+++ platform/android/jni_utils.h
@@ -51,6 +51,6 @@ Variant _jobject_to_variant(JNIEnv *env, jobject obj);
 
 Variant::Type get_jni_type(const String &p_type);
 
-const char *get_jni_sig(const String &p_type);
+const String get_jni_sig(const String &p_type);
 
 #endif // JNI_UTILS_H
