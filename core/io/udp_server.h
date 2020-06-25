5deb60e793d72d465d4f6046cc8245bc22e16b6f
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2020-06-25 15:32:50 +0200

    UDPServer handles PacketPeerUDP-client association
    UDPServer now uses a single socket which is shared with the
    PacketPeerUDP it creates and has a new `poll` function to read incoming
    packets on that socket and delivers them to the appropriate peer.
    PacketPeerUDP created this way never reads from the socket, but are
    allowed to write on it using sendto.
    
    This is needed because Windows (unlike Linux/BSD) does not support
    packet routing when multiple sockets are bound on the same address/port.

--- core/io/udp_server.h
+++ core/io/udp_server.h
@@ -38,15 +38,40 @@ class UDPServer : public Reference {
 	GDCLASS(UDPServer, Reference);
 
 protected:
-	static void _bind_methods();
-	int bind_port;
+	enum {
+		PACKET_BUFFER_SIZE = 65536
+	};
+
+	struct Peer {
+		PacketPeerUDP *peer;
+		IP_Address ip;
+		uint16_t port = 0;
+
+		bool operator==(const Peer &p_other) const {
+			return (ip == p_other.ip && port == p_other.port);
+		}
+	};
+	uint8_t recv_buffer[PACKET_BUFFER_SIZE];
+
+	int bind_port = 0;
 	IP_Address bind_address;
+
+	List<Peer> peers;
+	List<Peer> pending;
+	int max_pending_connections = 16;
+
 	Ref<NetSocket> _sock;
 
+	static void _bind_methods();
+
 public:
+	void remove_peer(IP_Address p_ip, int p_port);
 	Error listen(uint16_t p_port, const IP_Address &p_bind_address = IP_Address("*"));
+	Error poll();
 	bool is_listening() const;
 	bool is_connection_available() const;
+	void set_max_pending_connections(int p_max);
+	int get_max_pending_connections() const;
 	Ref<PacketPeerUDP> take_connection();
 
 	void stop();
