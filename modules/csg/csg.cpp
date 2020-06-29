33fd81a183bfc33b4a5c8044776897f0c9d333e6
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-06-29 17:52:36 +0100

    Use is_equal_approx() instead of vertex_snap when checking if ray is colliding with equiplanar CSG faces.

--- modules/csg/csg.cpp
+++ modules/csg/csg.cpp
@@ -524,7 +524,7 @@ void CSGBrushOperation::MeshMerge::_add_distance(List<real_t> &r_intersectionsA,
 
 	// Check if distance exists.
 	for (const List<real_t>::Element *E = intersections.front(); E; E = E->next())
-		if (Math::abs(**E - p_distance) < vertex_snap) return;
+		if (Math::is_equal_approx(**E, p_distance)) return;
 
 	intersections.push_back(p_distance);
 }
