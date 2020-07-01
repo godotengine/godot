9b02151b9d6e6b63ed31a9773aa9e0ab6b02a7c7
Juan Linietsky <reduzio@gmail.com>
2020-07-01 14:44:19 -0300

    Fix some popups menu missing screen transform.
    Closes #38591

--- editor/scene_tree_editor.cpp
+++ editor/scene_tree_editor.cpp
@@ -1072,7 +1072,7 @@ void SceneTreeEditor::drop_data_fw(const Point2 &p_point, const Variant &p_data,
 }
 
 void SceneTreeEditor::_rmb_select(const Vector2 &p_pos) {
-	emit_signal("rmb_pressed", tree->get_global_transform().xform(p_pos));
+	emit_signal("rmb_pressed", tree->get_screen_transform().xform(p_pos));
 }
 
 void SceneTreeEditor::_warning_changed(Node *p_for_node) {
