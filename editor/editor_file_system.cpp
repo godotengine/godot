130ad08d476e20600b144eb58f3a17fc4aadecc1
Chistpohe LY <christophely@live.fr>
2020-07-09 07:25:45 +0200

    auto re-import atlas, fixes #40047
    (cherry picked from commit f34f00bccc276d85acf27032375e461357c4a8bf)

--- editor/editor_file_system.cpp
+++ editor/editor_file_system.cpp
@@ -563,6 +563,7 @@ bool EditorFileSystem::_update_scan_actions() {
 				if (_test_for_reimport(full_path, false)) {
 					//must reimport
 					reimports.push_back(full_path);
+					reimports.append_array(_get_dependencies(full_path));
 				} else {
 					//must not reimport, all was good
 					//update modified times, to avoid reimport
