3644d50a5f49a28e5dfe5e5cf0c10073279ecc3a
clayjohn <claynjohn@gmail.com>
2020-07-10 17:07:30 -0700

    Add incremental update mode to sky

--- scene/resources/sky.cpp
+++ scene/resources/sky.cpp
@@ -83,8 +83,8 @@ void Sky::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_material"), &Sky::get_material);
 
 	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "sky_material", PROPERTY_HINT_RESOURCE_TYPE, "ShaderMaterial,PanoramaSkyMaterial,ProceduralSkyMaterial,PhysicalSkyMaterial"), "set_material", "get_material");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "process_mode", PROPERTY_HINT_ENUM, "Automatic,HighQuality,HighQualityIncremental,RealTime"), "set_process_mode", "get_process_mode");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "radiance_size", PROPERTY_HINT_ENUM, "32,64,128,256,512,1024,2048"), "set_radiance_size", "get_radiance_size");
-	ADD_PROPERTY(PropertyInfo(Variant::INT, "process_mode", PROPERTY_HINT_ENUM, "HighQuality,RealTime"), "set_process_mode", "get_process_mode");
 
 	BIND_ENUM_CONSTANT(RADIANCE_SIZE_32);
 	BIND_ENUM_CONSTANT(RADIANCE_SIZE_64);
@@ -95,12 +95,14 @@ void Sky::_bind_methods() {
 	BIND_ENUM_CONSTANT(RADIANCE_SIZE_2048);
 	BIND_ENUM_CONSTANT(RADIANCE_SIZE_MAX);
 
+	BIND_ENUM_CONSTANT(PROCESS_MODE_AUTOMATIC);
 	BIND_ENUM_CONSTANT(PROCESS_MODE_QUALITY);
+	BIND_ENUM_CONSTANT(PROCESS_MODE_INCREMENTAL);
 	BIND_ENUM_CONSTANT(PROCESS_MODE_REALTIME);
 }
 
 Sky::Sky() {
-	mode = PROCESS_MODE_QUALITY;
+	mode = PROCESS_MODE_AUTOMATIC;
 	radiance_size = RADIANCE_SIZE_256;
 	sky = RS::get_singleton()->sky_create();
 }
