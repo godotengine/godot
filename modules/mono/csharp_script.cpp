dbdbf3a60b7915d5a24c66729b1c4904f5dc921c
Aaron Franke <arnfranke@yahoo.com>
2020-07-13 14:13:38 -0400

    Commit other files changed by file_format.sh

--- modules/mono/csharp_script.cpp
+++ modules/mono/csharp_script.cpp
@@ -1963,7 +1963,7 @@ MonoObject *CSharpInstance::_internal_new_managed() {
 
 		bool die = _unreference_owner_unsafe();
 		// Not ok for the owner to die here. If there is a situation where this can happen, it will be considered a bug.
-		CRASH_COND(die == true);
+		CRASH_COND(die);
 
 		owner = nullptr;
 
@@ -2298,7 +2298,7 @@ CSharpInstance::~CSharpInstance() {
 		// Unreference the owner here, before the new "instance binding" references it.
 		// Otherwise, the unsafe reference debug checks will incorrectly detect a bug.
 		bool die = _unreference_owner_unsafe();
-		CRASH_COND(die == true); // `owner_keep_alive` holds a reference, so it can't die
+		CRASH_COND(die); // `owner_keep_alive` holds a reference, so it can't die
 
 		void *data = owner->get_script_instance_binding(CSharpLanguage::get_singleton()->get_language_index());
 		CRASH_COND(data == nullptr);
@@ -3136,7 +3136,7 @@ CSharpInstance *CSharpScript::_create_instance(const Variant **p_args, int p_arg
 
 		bool die = instance->_unreference_owner_unsafe();
 		// Not ok for the owner to die here. If there is a situation where this can happen, it will be considered a bug.
-		CRASH_COND(die == true);
+		CRASH_COND(die);
 
 		p_owner->set_script_instance(nullptr);
 		r_error.error = Callable::CallError::CALL_ERROR_INSTANCE_IS_NULL;
