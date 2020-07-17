f2ea66da863788c0925481c159aba728c749db1e
PouleyKetchoupp <pouleyketchoup@gmail.com>
2020-07-17 17:44:13 +0200

    Fix Return key events in LineEdit & TextEdit on Android
    Depending on the device implementation, editor actions could be
    received with different action ids or not at all for multi-line.
    
    Added a parameter to virtual keyboards to properly handle single-line
    and multi-line cases in all situations.
    
    Single-line:
    Input type set to text without multiline to make sure actions are sent.
    IME options are set to DONE action to force action id consistency.
    
    Multi-line:
    Input type set to text and multiline to make sure enter triggers new lines.
    Actions are disabled by the multiline flag, so '\n' characters are
    handled in text changed callbacks.

--- servers/display_server.cpp
+++ servers/display_server.cpp
@@ -31,6 +31,7 @@
 #include "display_server.h"
 
 #include "core/input/input.h"
+#include "core/method_bind_ext.gen.inc"
 #include "scene/resources/texture.h"
 
 DisplayServer *DisplayServer::singleton = nullptr;
@@ -217,7 +218,7 @@ bool DisplayServer::is_console_visible() const {
 	return false;
 }
 
-void DisplayServer::virtual_keyboard_show(const String &p_existing_text, const Rect2 &p_screen_rect, int p_max_length, int p_cursor_start, int p_cursor_end) {
+void DisplayServer::virtual_keyboard_show(const String &p_existing_text, const Rect2 &p_screen_rect, bool p_multiline, int p_max_length, int p_cursor_start, int p_cursor_end) {
 	WARN_PRINT("Virtual keyboard not supported by this display server.");
 }
 
@@ -459,7 +460,7 @@ void DisplayServer::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("console_set_visible", "console_visible"), &DisplayServer::console_set_visible);
 	ClassDB::bind_method(D_METHOD("is_console_visible"), &DisplayServer::is_console_visible);
 
-	ClassDB::bind_method(D_METHOD("virtual_keyboard_show", "existing_text", "position", "max_length", "cursor_start", "cursor_end"), &DisplayServer::virtual_keyboard_show, DEFVAL(Rect2i()), DEFVAL(-1), DEFVAL(-1), DEFVAL(-1));
+	ClassDB::bind_method(D_METHOD("virtual_keyboard_show", "existing_text", "position", "multiline", "max_length", "cursor_start", "cursor_end"), &DisplayServer::virtual_keyboard_show, DEFVAL(Rect2i()), DEFVAL(false), DEFVAL(-1), DEFVAL(-1), DEFVAL(-1));
 	ClassDB::bind_method(D_METHOD("virtual_keyboard_hide"), &DisplayServer::virtual_keyboard_hide);
 
 	ClassDB::bind_method(D_METHOD("virtual_keyboard_get_height"), &DisplayServer::virtual_keyboard_get_height);
