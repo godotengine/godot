5bf9a9f824299439d76c2e597ccbfd14689f3883
RevoluPowered <gordon@gordonite.tech>
2020-07-20 17:35:34 +0100

    t Add unit testing to Godot using DocTest and added to GitHub Actions CI
    Implements exit codes into the engine so tests can return their statuses.
    Ideally we don't do this, and we use FIXUP logic to 'begin' and 'end' the engine execution for tests specifically.
    
    Since realistically we're initialising the engine here we don't want to do that, since String should not require an engine startup to test a single header.
    
    This lowers the complexity of running the unit tests and even for
    physics should be possible to implement such a fix.

--- platform/osx/godot_main_osx.mm
+++ platform/osx/godot_main_osx.mm
@@ -37,7 +37,7 @@
 
 int main(int argc, char **argv) {
 #if defined(VULKAN_ENABLED)
-	//MoltenVK - enable full component swizzling support
+	// MoltenVK - enable full component swizzling support
 	setenv("MVK_CONFIG_FULL_IMAGE_VIEW_SWIZZLE", "1", 1);
 #endif
 
@@ -59,6 +59,9 @@ int main(int argc, char **argv) {
 
 	OS_OSX os;
 	Error err;
+
+	// We must override main when testing is enabled
+	TEST_MAIN_OVERRIDE
 
 	if (os.open_with_filename != "") {
 		char *argv_c = (char *)malloc(os.open_with_filename.utf8().size());
