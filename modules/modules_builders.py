60f53140b85748f6ce61c353facc15f45fd7ae4a
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-07-26 14:46:44 +0300

    Enable support for C++ modules tests
    Modules-specific tests can be written under respective module folders.
    Each module should have "tests" folder created with the tests implemented
    as `doctest` headers, so they can be collected by the buildsystem and
    included directly in `tests/test_main.cpp` to be compiled.

--- modules/modules_builders.py
+++ modules/modules_builders.py
@@ -12,5 +12,16 @@ def generate_modules_enabled(target, source, env):
             f.write("#define %s\n" % ("MODULE_" + module.upper() + "_ENABLED"))
 
 
+def generate_modules_tests(target, source, env):
+    import os
+    import glob
+
+    with open(target[0].path, "w") as f:
+        for name, path in env.module_list.items():
+            headers = glob.glob(os.path.join(path, "tests", "*.h"))
+            for h in headers:
+                f.write('#include "%s"\n' % (os.path.normpath(h)))
+
+
 if __name__ == "__main__":
     subprocess_main(globals())
