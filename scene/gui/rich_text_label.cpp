b509d0341437a74c690c148aa450bf0151a99e1d
Umang Kalra <umangkalra10@gmail.com>
2020-07-31 00:36:46 +0530

    Fix RichTextLabel center alignment bug
    Fixes #40207.

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -188,7 +188,7 @@ int RichTextLabel::_process_line(ItemFrame *p_frame, const Vector2 &p_ofs, int &
 		wofs += line_ofs;
 	}
 
-	int begin = wofs;
+	int begin = margin;
 
 	Ref<Font> cfont = _find_font(it);
 	if (cfont.is_null()) {
