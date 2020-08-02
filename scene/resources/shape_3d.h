8e1c9ff1c12f2a668b7e9b6cedaac84e5e340ad9
Arkadiusz Marcin Kołek <aksoftware91@gmail.com>
2020-08-02 11:10:43 +0200

    Shape3D::get_debug_mesh_lines const methods

--- scene/resources/shape_3d.h
+++ scene/resources/shape_3d.h
@@ -56,7 +56,7 @@ public:
 	virtual RID get_rid() const override { return shape; }
 
 	Ref<ArrayMesh> get_debug_mesh();
-	virtual Vector<Vector3> get_debug_mesh_lines() = 0; // { return Vector<Vector3>(); }
+	virtual Vector<Vector3> get_debug_mesh_lines() const = 0; // { return Vector<Vector3>(); }
 	/// Returns the radius of a sphere that fully enclose this shape
 	virtual real_t get_enclosing_radius() const = 0;
 
