f91880a7fbd08782d486b8b8b8a09d3c03b96e3c
TwistedTwigleg <beard.noah@gmail.com>
2020-08-03 18:29:27 -0400

    Changes: * SkeletonModification3DLookAt: Added a fully working implementation of the LookAt modification. * RegisterSceneTypes: Registered the 3D LookAt modification.
    NOTE: The changes in this commit are squashed from the following verbose branch: https://github.com/TwistedTwigleg/godot/tree/GSOC_2020_Working_Branch_IK_VERBOSE_ARCHIVE

--- scene/resources/skeleton_modification_3d.cpp
+++ scene/resources/skeleton_modification_3d.cpp
@@ -260,3 +260,203 @@ SkeletonModification3D::SkeletonModification3D() {
 	stack = nullptr;
 	is_setup = false;
 }
+
+///////////////////////////////////////
+// LookAt
+///////////////////////////////////////
+
+void SkeletonModification3DLookAt::execute(float delta) {
+	ERR_FAIL_COND_MSG(!stack || !is_setup || stack->skeleton == nullptr,
+			"Modification is not setup and therefore cannot execute!");
+	if (!enabled) {
+		return;
+	}
+
+	if (target_node_cache.is_null()) {
+		update_cache();
+		WARN_PRINT("Target cache is out of date. Updating...");
+		return;
+	}
+
+	if (bone_idx <= -2) {
+		bone_idx = stack->skeleton->find_bone(bone_name);
+	}
+
+	Node3D *target = Object::cast_to<Node3D>(ObjectDB::get_instance(target_node_cache));
+	ERR_FAIL_COND_MSG(!target, "Target node is not a Node3D-based node. Cannot execute modification!");
+	ERR_FAIL_COND_MSG(!target->is_inside_tree(), "Target node is not in the scene tree. Cannot execute modification!");
+	ERR_FAIL_COND_MSG(bone_idx <= -1, "Bone index is invalid. Cannot execute modification!");
+
+	Transform new_bone_trans = stack->skeleton->local_pose_to_global_pose(bone_idx, stack->skeleton->get_bone_local_pose_override(bone_idx));
+	Vector3 target_pos = stack->skeleton->world_transform_to_global_pose(target->get_global_transform()).origin;
+
+	// Limit rotation by changing the target's position.
+	// NOTE: this does not work when two limits are applied. Will need to adjust.
+	if (lock_rotation_x) {
+		target_pos.x = new_bone_trans.origin.x;
+	}
+	if (lock_rotation_y) {
+		target_pos.y = new_bone_trans.origin.y;
+	}
+	if (lock_rotation_z) {
+		target_pos.z = new_bone_trans.origin.z;
+	}
+
+	// Look at the target!
+	new_bone_trans = new_bone_trans.looking_at(target_pos, Vector3(0, 1, 0));
+	// Convert from Z-forward to whatever direction the bone faces.
+	stack->skeleton->update_bone_rest_forward_vector(bone_idx);
+	new_bone_trans.basis = stack->skeleton->global_pose_z_forward_to_bone_forward(bone_idx, new_bone_trans.basis);
+
+	// Apply additional rotation
+	new_bone_trans.basis.rotate_local(Vector3(1, 0, 0), Math::deg2rad(additional_rotation.x));
+	new_bone_trans.basis.rotate_local(Vector3(0, 1, 0), Math::deg2rad(additional_rotation.y));
+	new_bone_trans.basis.rotate_local(Vector3(0, 0, 1), Math::deg2rad(additional_rotation.z));
+
+	new_bone_trans = stack->skeleton->global_pose_to_local_pose(bone_idx, new_bone_trans);
+	stack->skeleton->set_bone_local_pose_override(bone_idx, new_bone_trans, stack->strength, true);
+	stack->skeleton->force_update_bone_children_transforms(bone_idx);
+}
+
+void SkeletonModification3DLookAt::setup_modification(SkeletonModificationStack3D *p_stack) {
+	stack = p_stack;
+
+	if (stack != nullptr) {
+		is_setup = true;
+		update_cache();
+	}
+}
+
+void SkeletonModification3DLookAt::set_bone_name(String p_name) {
+	bone_name = p_name;
+	if (stack && stack->skeleton) {
+		bone_idx = stack->skeleton->find_bone(bone_name);
+	}
+	_change_notify();
+}
+
+String SkeletonModification3DLookAt::get_bone_name() const {
+	return bone_name;
+}
+
+int SkeletonModification3DLookAt::get_bone_index() const {
+	return bone_idx;
+}
+
+void SkeletonModification3DLookAt::set_bone_index(int p_bone_idx) {
+	ERR_FAIL_COND_MSG(p_bone_idx < 0, "Bone index is out of range: The index is too low!");
+	bone_idx = p_bone_idx;
+
+	if (stack) {
+		if (stack->skeleton) {
+			bone_name = stack->skeleton->get_bone_name(p_bone_idx);
+		}
+	}
+	_change_notify();
+}
+
+void SkeletonModification3DLookAt::update_cache() {
+	if (!is_setup || !stack) {
+		WARN_PRINT("Cannot update cache: modification is not properly setup!");
+		return;
+	}
+
+	target_node_cache = ObjectID();
+	if (stack->skeleton) {
+		if (stack->skeleton->is_inside_tree()) {
+			if (stack->skeleton->has_node(target_node)) {
+				Node *node = stack->skeleton->get_node(target_node);
+				ERR_FAIL_COND_MSG(!node || stack->skeleton == node,
+						"Cannot update cache: Target node is this modification's skeleton or cannot be found!");
+				target_node_cache = node->get_instance_id();
+			}
+		}
+	}
+}
+
+void SkeletonModification3DLookAt::set_target_node(const NodePath &p_target_node) {
+	target_node = p_target_node;
+	update_cache();
+}
+
+NodePath SkeletonModification3DLookAt::get_target_node() const {
+	return target_node;
+}
+
+Vector3 SkeletonModification3DLookAt::get_rotation_offset() const {
+	return additional_rotation;
+}
+
+void SkeletonModification3DLookAt::set_rotation_offset(Vector3 p_offset) {
+	additional_rotation = p_offset;
+}
+
+bool SkeletonModification3DLookAt::get_lock_rotation_x() const {
+	return lock_rotation_x;
+}
+
+bool SkeletonModification3DLookAt::get_lock_rotation_y() const {
+	return lock_rotation_y;
+}
+
+bool SkeletonModification3DLookAt::get_lock_rotation_z() const {
+	return lock_rotation_z;
+}
+
+void SkeletonModification3DLookAt::set_lock_rotation_x(bool p_lock) {
+	lock_rotation_x = p_lock;
+}
+
+void SkeletonModification3DLookAt::set_lock_rotation_y(bool p_lock) {
+	lock_rotation_y = p_lock;
+}
+
+void SkeletonModification3DLookAt::set_lock_rotation_z(bool p_lock) {
+	lock_rotation_z = p_lock;
+}
+
+void SkeletonModification3DLookAt::_bind_methods() {
+	ClassDB::bind_method(D_METHOD("set_bone_name", "name"), &SkeletonModification3DLookAt::set_bone_name);
+	ClassDB::bind_method(D_METHOD("get_bone_name"), &SkeletonModification3DLookAt::get_bone_name);
+
+	ClassDB::bind_method(D_METHOD("set_bone_index", "bone_idx"), &SkeletonModification3DLookAt::set_bone_index);
+	ClassDB::bind_method(D_METHOD("get_bone_index"), &SkeletonModification3DLookAt::get_bone_index);
+
+	ClassDB::bind_method(D_METHOD("set_target_node", "target_nodepath"), &SkeletonModification3DLookAt::set_target_node);
+	ClassDB::bind_method(D_METHOD("get_target_node"), &SkeletonModification3DLookAt::get_target_node);
+
+	ClassDB::bind_method(D_METHOD("set_rotation_offset", "offset"), &SkeletonModification3DLookAt::set_rotation_offset);
+	ClassDB::bind_method(D_METHOD("get_rotation_offset"), &SkeletonModification3DLookAt::get_rotation_offset);
+
+	ClassDB::bind_method(D_METHOD("set_lock_rotation_x", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_x);
+	ClassDB::bind_method(D_METHOD("get_lock_rotation_x"), &SkeletonModification3DLookAt::get_lock_rotation_x);
+	ClassDB::bind_method(D_METHOD("set_lock_rotation_y", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_y);
+	ClassDB::bind_method(D_METHOD("get_lock_rotation_y"), &SkeletonModification3DLookAt::get_lock_rotation_y);
+	ClassDB::bind_method(D_METHOD("set_lock_rotation_z", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_z);
+	ClassDB::bind_method(D_METHOD("get_lock_rotation_z"), &SkeletonModification3DLookAt::get_lock_rotation_z);
+
+	ADD_PROPERTY(PropertyInfo(Variant::STRING, "bone_name"), "set_bone_name", "get_bone_name");
+	ADD_PROPERTY(PropertyInfo(Variant::INT, "bone_index"), "set_bone_index", "get_bone_index");
+	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "target_nodepath", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_target_node", "get_target_node");
+	ADD_GROUP("Additional Settings", "");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_x"), "set_lock_rotation_x", "get_lock_rotation_x");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_y"), "set_lock_rotation_y", "get_lock_rotation_y");
+	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_z"), "set_lock_rotation_z", "get_lock_rotation_z");
+	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "rotation_offset"), "set_rotation_offset", "get_rotation_offset");
+	ADD_GROUP("", "");
+}
+
+SkeletonModification3DLookAt::SkeletonModification3DLookAt() {
+	stack = nullptr;
+	is_setup = false;
+	bone_name = "";
+	bone_idx = -2;
+	additional_rotation = Vector3();
+	lock_rotation_x = false;
+	lock_rotation_y = false;
+	lock_rotation_z = false;
+	enabled = true;
+}
+
+SkeletonModification3DLookAt::~SkeletonModification3DLookAt() {
+}
