7f1e4f493916a7ac5847a82c25364833a6a1d936
Stephen Nichols <snichols@certainaffinity.com>
2020-08-05 14:41:46 -0500

    Fixing null callee crash.

--- modules/gdscript/gdscript_parser.h
+++ modules/gdscript/gdscript_parser.h
@@ -383,6 +383,14 @@ public:
 		CallNode() {
 			type = CALL;
 		}
+
+		Type get_callee_type() const {
+			if (callee == nullptr) {
+				return Type::NONE;
+			} else {
+				return callee->type;
+			}
+		}
 	};
 
 	struct CastNode : public ExpressionNode {
