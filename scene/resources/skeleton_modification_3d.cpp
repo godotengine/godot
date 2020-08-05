b194f8132218486adcc4b8dc66711fb1485639f5
TwistedTwigleg <beard.noah@gmail.com>
2020-08-05 12:14:10 -0400

    Changes: * SkeletonModification3D: Added code to print a warning if the modification could not be setup when setup_modification is called. * SkeletonModification3DLookAt: Moved the settings that were previously in "additional settings" out of that property group and added them using get_property_list. * SkeletonModification3DLookAt: The rotation applied after the modification is now called "additional_rotation" throughout the code and in the functions. * SkeletonModification3DLookAt: Condensed the properties for limiting rotation to limit to a plane, since that is what the code was already doing. Renamed and adjusted code accordingly.   * In the future, I may look into adding individual rotation axis locking. * SkeletonModification3DLookAt: Rotation limits are now performed in local space and do not change when the rotation of the Skeleton3D changes or the rotation of the parent bone. * The additional rotation getters and setters now return the rotation in radians. The editor uses still exposes this property using degrees and converts internally to radians.

--- scene/resources/skeleton_modification_3d.cpp
+++ scene/resources/skeleton_modification_3d.cpp
@@ -244,6 +244,8 @@ void SkeletonModification3D::setup_modification(SkeletonModificationStack3D *p_s
 	stack = p_stack;
 	if (stack) {
 		is_setup = true;
+	} else {
+		WARN_PRINT("Could not setup modification with name " + this->get_name());
 	}
 }
 
@@ -274,6 +276,49 @@ SkeletonModification3D::SkeletonModification3D() {
 // LookAt
 ///////////////////////////////////////
 
+bool SkeletonModification3DLookAt::_set(const StringName &p_path, const Variant &p_value) {
+
+	if (p_path == "lock_rotation_to_plane") {
+		set_lock_rotation_to_plane(p_value);
+	} else if (p_path == "lock_rotation_plane") {
+		set_lock_rotation_plane(p_value);
+	} else if (p_path == "additional_rotation") {
+		Vector3 tmp = p_value;
+		tmp.x = Math::deg2rad(tmp.x);
+		tmp.y = Math::deg2rad(tmp.y);
+		tmp.z = Math::deg2rad(tmp.z);
+		set_additional_rotation(tmp);
+	}
+
+	return true;
+}
+
+bool SkeletonModification3DLookAt::_get(const StringName &p_path, Variant &r_ret) const {
+	
+	if (p_path == "lock_rotation_to_plane") {
+		r_ret = get_lock_rotation_to_plane();
+	} else if (p_path == "lock_rotation_plane") {
+		r_ret = get_lock_rotation_plane();
+	} else if (p_path == "additional_rotation") {
+		Vector3 tmp = get_additional_rotation();
+		tmp.x = Math::rad2deg(tmp.x);
+		tmp.y = Math::rad2deg(tmp.y);
+		tmp.z = Math::rad2deg(tmp.z);
+		r_ret = tmp;
+	}
+
+	return true;
+}
+
+void SkeletonModification3DLookAt::_get_property_list(List<PropertyInfo> *p_list) const {
+
+	p_list->push_back(PropertyInfo(Variant::BOOL, "lock_rotation_to_plane", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT));
+	if (lock_rotation_to_plane) {
+		p_list->push_back(PropertyInfo(Variant::INT, "lock_rotation_plane", PROPERTY_HINT_ENUM, "X plane, Y plane, Z plane", PROPERTY_USAGE_DEFAULT));
+	}
+	p_list->push_back(PropertyInfo(Variant::VECTOR3, "additional_rotation", PROPERTY_HINT_NONE, "", PROPERTY_USAGE_DEFAULT));
+}
+
 void SkeletonModification3DLookAt::execute(float delta) {
 	ERR_FAIL_COND_MSG(!stack || !is_setup || stack->skeleton == nullptr,
 			"Modification is not setup and therefore cannot execute!");
@@ -296,19 +341,18 @@ void SkeletonModification3DLookAt::execute(float delta) {
 	ERR_FAIL_COND_MSG(!target->is_inside_tree(), "Target node is not in the scene tree. Cannot execute modification!");
 	ERR_FAIL_COND_MSG(bone_idx <= -1, "Bone index is invalid. Cannot execute modification!");
 
-	Transform new_bone_trans = stack->skeleton->local_pose_to_global_pose(bone_idx, stack->skeleton->get_bone_local_pose_override(bone_idx));
-	Vector3 target_pos = stack->skeleton->world_transform_to_global_pose(target->get_global_transform()).origin;
+	Transform new_bone_trans = stack->skeleton->get_bone_local_pose_override(bone_idx);
+	Vector3 target_pos = stack->skeleton->global_pose_to_local_pose(bone_idx, stack->skeleton->world_transform_to_global_pose(target->get_global_transform())).origin;
 
-	// Limit rotation by changing the target's position.
-	// NOTE: this does not work when two limits are applied. Will need to adjust.
-	if (lock_rotation_x) {
-		target_pos.x = new_bone_trans.origin.x;
-	}
-	if (lock_rotation_y) {
-		target_pos.y = new_bone_trans.origin.y;
-	}
-	if (lock_rotation_z) {
-		target_pos.z = new_bone_trans.origin.z;
+	// Lock the rotation to a plane relative to the bone by changing the target position
+	if (lock_rotation_to_plane) {
+		if (lock_rotation_plane == ROTATION_PLANE::ROTATION_PLANE_X) {
+			target_pos.x = new_bone_trans.origin.x;
+		} else if (lock_rotation_plane == ROTATION_PLANE::ROTATION_PLANE_Y) {
+			target_pos.y = new_bone_trans.origin.y;
+		} else if (lock_rotation_plane == ROTATION_PLANE::ROTATION_PLANE_Z) {
+			target_pos.z = new_bone_trans.origin.z;
+		}
 	}
 
 	// Look at the target!
@@ -318,11 +362,11 @@ void SkeletonModification3DLookAt::execute(float delta) {
 	new_bone_trans.basis = stack->skeleton->global_pose_z_forward_to_bone_forward(bone_idx, new_bone_trans.basis);
 
 	// Apply additional rotation
-	new_bone_trans.basis.rotate_local(Vector3(1, 0, 0), Math::deg2rad(additional_rotation.x));
-	new_bone_trans.basis.rotate_local(Vector3(0, 1, 0), Math::deg2rad(additional_rotation.y));
-	new_bone_trans.basis.rotate_local(Vector3(0, 0, 1), Math::deg2rad(additional_rotation.z));
+	new_bone_trans.basis.rotate_local(Vector3(1, 0, 0), additional_rotation.x);
+	new_bone_trans.basis.rotate_local(Vector3(0, 1, 0), additional_rotation.y);
+	new_bone_trans.basis.rotate_local(Vector3(0, 0, 1), additional_rotation.z);
 
-	new_bone_trans = stack->skeleton->global_pose_to_local_pose(bone_idx, new_bone_trans);
+	//new_bone_trans = stack->skeleton->global_pose_to_local_pose(bone_idx, new_bone_trans);
 	stack->skeleton->set_bone_local_pose_override(bone_idx, new_bone_trans, stack->strength, true);
 	stack->skeleton->force_update_bone_children_transforms(bone_idx);
 }
@@ -392,36 +436,29 @@ NodePath SkeletonModification3DLookAt::get_target_node() const {
 	return target_node;
 }
 
-Vector3 SkeletonModification3DLookAt::get_rotation_offset() const {
+Vector3 SkeletonModification3DLookAt::get_additional_rotation() const {
 	return additional_rotation;
 }
 
-void SkeletonModification3DLookAt::set_rotation_offset(Vector3 p_offset) {
+void SkeletonModification3DLookAt::set_additional_rotation(Vector3 p_offset) {
 	additional_rotation = p_offset;
 }
 
-bool SkeletonModification3DLookAt::get_lock_rotation_x() const {
-	return lock_rotation_x;
+bool SkeletonModification3DLookAt::get_lock_rotation_to_plane() const {
+	return lock_rotation_plane;
 }
 
-bool SkeletonModification3DLookAt::get_lock_rotation_y() const {
-	return lock_rotation_y;
+void SkeletonModification3DLookAt::set_lock_rotation_to_plane(bool p_lock_rotation) {
+	lock_rotation_to_plane = p_lock_rotation;
+	_change_notify();
 }
 
-bool SkeletonModification3DLookAt::get_lock_rotation_z() const {
-	return lock_rotation_z;
+int SkeletonModification3DLookAt::get_lock_rotation_plane() const {
+	return lock_rotation_plane;
 }
 
-void SkeletonModification3DLookAt::set_lock_rotation_x(bool p_lock) {
-	lock_rotation_x = p_lock;
-}
-
-void SkeletonModification3DLookAt::set_lock_rotation_y(bool p_lock) {
-	lock_rotation_y = p_lock;
-}
-
-void SkeletonModification3DLookAt::set_lock_rotation_z(bool p_lock) {
-	lock_rotation_z = p_lock;
+void SkeletonModification3DLookAt::set_lock_rotation_plane(int p_plane) {
+	lock_rotation_plane = p_plane;
 }
 
 void SkeletonModification3DLookAt::_bind_methods() {
@@ -434,25 +471,17 @@ void SkeletonModification3DLookAt::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_target_node", "target_nodepath"), &SkeletonModification3DLookAt::set_target_node);
 	ClassDB::bind_method(D_METHOD("get_target_node"), &SkeletonModification3DLookAt::get_target_node);
 
-	ClassDB::bind_method(D_METHOD("set_rotation_offset", "offset"), &SkeletonModification3DLookAt::set_rotation_offset);
-	ClassDB::bind_method(D_METHOD("get_rotation_offset"), &SkeletonModification3DLookAt::get_rotation_offset);
+	ClassDB::bind_method(D_METHOD("set_additional_rotation", "additional_rotation"), &SkeletonModification3DLookAt::set_additional_rotation);
+	ClassDB::bind_method(D_METHOD("get_additional_rotation"), &SkeletonModification3DLookAt::get_additional_rotation);
 
-	ClassDB::bind_method(D_METHOD("set_lock_rotation_x", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_x);
-	ClassDB::bind_method(D_METHOD("get_lock_rotation_x"), &SkeletonModification3DLookAt::get_lock_rotation_x);
-	ClassDB::bind_method(D_METHOD("set_lock_rotation_y", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_y);
-	ClassDB::bind_method(D_METHOD("get_lock_rotation_y"), &SkeletonModification3DLookAt::get_lock_rotation_y);
-	ClassDB::bind_method(D_METHOD("set_lock_rotation_z", "lock"), &SkeletonModification3DLookAt::set_lock_rotation_z);
-	ClassDB::bind_method(D_METHOD("get_lock_rotation_z"), &SkeletonModification3DLookAt::get_lock_rotation_z);
+	ClassDB::bind_method(D_METHOD("set_lock_rotation_to_plane", "lock_to_plane"), &SkeletonModification3DLookAt::set_lock_rotation_to_plane);
+	ClassDB::bind_method(D_METHOD("get_lock_rotation_to_plane"), &SkeletonModification3DLookAt::get_lock_rotation_to_plane);
+	ClassDB::bind_method(D_METHOD("set_lock_rotation_plane", "plane"), &SkeletonModification3DLookAt::set_lock_rotation_plane);
+	ClassDB::bind_method(D_METHOD("get_lock_rotation_plane"), &SkeletonModification3DLookAt::get_lock_rotation_plane);
 
 	ADD_PROPERTY(PropertyInfo(Variant::STRING, "bone_name"), "set_bone_name", "get_bone_name");
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "bone_index"), "set_bone_index", "get_bone_index");
 	ADD_PROPERTY(PropertyInfo(Variant::NODE_PATH, "target_nodepath", PROPERTY_HINT_NODE_PATH_VALID_TYPES, "Node3D"), "set_target_node", "get_target_node");
-	ADD_GROUP("Additional Settings", "");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_x"), "set_lock_rotation_x", "get_lock_rotation_x");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_y"), "set_lock_rotation_y", "get_lock_rotation_y");
-	ADD_PROPERTY(PropertyInfo(Variant::BOOL, "lock_rotation_z"), "set_lock_rotation_z", "get_lock_rotation_z");
-	ADD_PROPERTY(PropertyInfo(Variant::VECTOR3, "rotation_offset"), "set_rotation_offset", "get_rotation_offset");
-	ADD_GROUP("", "");
 }
 
 SkeletonModification3DLookAt::SkeletonModification3DLookAt() {
@@ -461,9 +490,7 @@ SkeletonModification3DLookAt::SkeletonModification3DLookAt() {
 	bone_name = "";
 	bone_idx = -2;
 	additional_rotation = Vector3();
-	lock_rotation_x = false;
-	lock_rotation_y = false;
-	lock_rotation_z = false;
+	lock_rotation_to_plane = false;
 	enabled = true;
 }
 
