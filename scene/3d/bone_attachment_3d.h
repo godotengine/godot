29902680b1dcf80dcbec80828762d079ed2e8796
TwistedTwigleg <beard.noah@gmail.com>
2020-08-13 12:03:04 -0400

    Changes: * BoneAttachment3D: Added the option to use an external skeleton! Now BoneAttachment3D nodes do not have to be a child of a Skeleton3D node to function.   * Because of this, scenes instanced from 3D files do not necessarily have to loose all their BoneAttachment3Ds on reimport, as the BoneAttachment3D nodes can now be outside of the instanced scene.   * The property that controls whether an external skeleton is used is turned off by default, so existing BoneAttachment3D nodes are not affected. * BoneAttachement3D: Moved the override properties and the external skeleton properties to the property list, so their properties dynamically appear/disappear as the options are used. * BoneAttachement3D: The signal/callable binding is now a deferred call, as it would crash when reparenting otherwise.

--- scene/3d/bone_attachment_3d.h
+++ scene/3d/bone_attachment_3d.h
@@ -49,13 +49,22 @@ class BoneAttachment3D : public Node3D {
 		MODE_CUSTOM_POSE
 	};
 
+	bool use_external_skeleton = false;
+	NodePath external_skeleton_node;
+	ObjectID external_skeleton_node_cache;
+
 	void _check_bind();
 	void _check_unbind();
 
 	void _transform_changed();
+	void _update_external_skeleton_cache();
+	Skeleton3D *_get_skeleton3d() const;
 
 protected:
 	virtual void _validate_property(PropertyInfo &property) const override;
+	bool _get(const StringName &p_path, Variant &r_ret) const;
+	bool _set(const StringName &p_path, const Variant &p_value);
+	void _get_property_list(List<PropertyInfo> *p_list) const;
 	void _notification(int p_what);
 
 	static void _bind_methods();
@@ -71,6 +80,11 @@ public:
 	bool get_override_pose() const;
 	void set_override_mode(int p_mode);
 	int get_override_mode() const;
+
+	void set_use_external_skeleton(bool p_external_skeleton);
+	bool get_use_external_skeleton() const;
+	void set_external_skeleton(NodePath p_skeleton);
+	NodePath get_external_skeleton() const;
 
 	virtual void on_bone_pose_update(int p_bone_index);
 
