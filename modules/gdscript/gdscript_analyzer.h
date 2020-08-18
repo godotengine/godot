4bd733f6680fb53acd7aa6617cb70b4f0ba17235
George Marques <george@gmarqu.es>
2020-08-18 17:44:20 -0300

    GDScript: Allow enum values to be set to constant expressions
    Also allow them to access previous values wihout referencing the enum.

--- modules/gdscript/gdscript_analyzer.h
+++ modules/gdscript/gdscript_analyzer.h
@@ -41,6 +41,8 @@ class GDScriptAnalyzer {
 	GDScriptParser *parser = nullptr;
 	HashMap<String, Ref<GDScriptParserRef>> depended_parsers;
 
+	const GDScriptParser::EnumNode *current_enum = nullptr;
+
 	Error resolve_inheritance(GDScriptParser::ClassNode *p_class, bool p_recursive = true);
 	GDScriptParser::DataType resolve_datatype(GDScriptParser::TypeNode *p_type);
 
