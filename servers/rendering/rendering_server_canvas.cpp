1ac16e25c96de942c1111598c153c564086613c6
Manuel Montoto <mail@manuelmontoto.com>
2020-08-26 12:42:40 +0200

    Update rendering_server_canvas.cpp
    Fixes the 2D canvas display positions jumping pseudo randomly when the are located at fractional coordinates and the camera changes location. Fixes #41491, #35606 etc. Godot 4.0 branch.

--- servers/rendering/rendering_server_canvas.cpp
+++ servers/rendering/rendering_server_canvas.cpp
@@ -113,6 +113,7 @@ void RenderingServerCanvas::_cull_canvas_item(Item *p_canvas_item, const Transfo
 
 	Rect2 rect = ci->get_rect();
 	Transform2D xform = p_transform * ci->xform;
+	xform.elements[2] =  xform.elements[2].floor() ; // Clean the undesirable fractional part distorting the final display position
 	Rect2 global_rect = xform.xform(rect);
 	global_rect.position += p_clip_rect.position;
 
