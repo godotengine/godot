f665f40bcbc99856d67d31f08a7a9d172dd67d39
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-09-06 13:40:30 +0100

    Fix header guards in modules: - Add missing header guards to various modules' register_types.h - Add header guard to basis_universal/texture_basisu.h. - Ensure header guard encloses entire header in   webrtc/webrtc_data_channel_js.h.

--- modules/camera/register_types.h
+++ modules/camera/register_types.h
@@ -28,5 +28,10 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                */
 /*************************************************************************/
 
+#ifndef CAMERA_REGISTER_TYPES_H
+#define CAMERA_REGISTER_TYPES_H
+
 void register_camera_types();
 void unregister_camera_types();
+
+#endif // CAMERA_REGISTER_TYPES_H
