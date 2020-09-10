fa81e3621fbe5085b092078da9a438d4561bd082
Haoyu Qiu <timothyqiu32@gmail.com>
2020-09-10 11:08:20 +0800

    Fixes ParallaxLayer offset when ignore camera zoom
    (cherry picked from commit 5e2167631b3913ba8b34ff8c6c3d8027cb24ddf4)

--- scene/2d/parallax_background.cpp
+++ scene/2d/parallax_background.cpp
@@ -109,7 +109,7 @@ void ParallaxBackground::_update_scroll() {
 			continue;
 
 		if (ignore_camera_zoom)
-			l->set_base_offset_and_scale(ofs, 1.0, screen_offset);
+			l->set_base_offset_and_scale((ofs + screen_offset * (scale - 1)) / scale, 1.0, screen_offset);
 		else
 			l->set_base_offset_and_scale(ofs, scale, screen_offset);
 	}
