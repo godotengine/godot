6ea314e6f5c9c79737832f1960806278126ef958
Rémi Verschelde <rverschelde@gmail.com>
2020-09-15 10:47:35 +0200

    makerst: Add missing newline at EOF

--- doc/tools/makerst.py
+++ doc/tools/makerst.py
@@ -1041,7 +1041,7 @@ def make_footer():  # type: () -> str
     return (
         ".. |virtual| replace:: :abbr:`virtual (This method should typically be overridden by the user to have any effect.)`\n"
         ".. |const| replace:: :abbr:`const (This method has no side effects. It doesn't modify any of the instance's member variables.)`\n"
-        ".. |vararg| replace:: :abbr:`vararg (This method accepts any number of arguments after the ones described here.)`"
+        ".. |vararg| replace:: :abbr:`vararg (This method accepts any number of arguments after the ones described here.)`\n"
     )
     # fmt: on
 
