13d7404cd73f815e2cb68290b701f2978e6398d5
Tomasz Chabora <kobewi4e@gmail.com>
2020-09-23 15:39:06 +0200

    Check for null in InputMap::_find_event
    (cherry picked from commit f4e34ac353b736708934abc3bf697fc45e53bbbb)

--- core/input_map.cpp
+++ core/input_map.cpp
@@ -100,6 +100,7 @@ List<StringName> InputMap::get_actions() const {
 }
 
 List<Ref<InputEvent> >::Element *InputMap::_find_event(Action &p_action, const Ref<InputEvent> &p_event, bool *p_pressed, float *p_strength) const {
+	ERR_FAIL_COND_V(!p_event.is_valid(), NULL);
 
 	for (List<Ref<InputEvent> >::Element *E = p_action.inputs.front(); E; E = E->next()) {
 
