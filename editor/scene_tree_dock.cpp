KleirRampage45/redot-engine

Fix changing icons' colors when switch dark and light theme

editor/scene_tree_dock.cpp
@@ -1115,7 +1115,7 @@ void SceneTreeDock::_notification(int p_what) {
 			beginner_node_shortcuts->set_name("BeginnerNodeShortcuts");
 			node_shortcuts->add_child(beginner_node_shortcuts);
 
-			Button *button_2d = memnew(Button);
+			button_2d = memnew(Button);
 			beginner_node_shortcuts->add_child(button_2d);
 			button_2d->set_text(TTR("2D Scene"));
 			button_2d->set_icon(get_theme_icon("Node2D", "EditorIcons"));
@@ -1127,7 +1127,7 @@ void SceneTreeDock::_notification(int p_what) {
 			button_3d->set_icon(get_theme_icon("Node3D", "EditorIcons"));
 			button_3d->connect("pressed", callable_mp(this, &SceneTreeDock::_tool_selected), make_binds(TOOL_CREATE_3D_SCENE, false));
 
-			Button *button_ui = memnew(Button);
+			button_ui = memnew(Button);
 			beginner_node_shortcuts->add_child(button_ui);
 			button_ui->set_text(TTR("User Interface"));
 			button_ui->set_icon(get_theme_icon("Control", "EditorIcons"));
@@ -1137,7 +1137,7 @@ void SceneTreeDock::_notification(int p_what) {
 			favorite_node_shortcuts->set_name("FavoriteNodeShortcuts");
 			node_shortcuts->add_child(favorite_node_shortcuts);
 
-			Button *button_custom = memnew(Button);
+			button_custom = memnew(Button);
 			node_shortcuts->add_child(button_custom);
 			button_custom->set_text(TTR("Other Node"));
 			button_custom->set_icon(get_theme_icon("Add", "EditorIcons"));
@@ -1160,6 +1160,10 @@ void SceneTreeDock::_notification(int p_what) {
 			button_instance->set_icon(get_theme_icon("Instance", "EditorIcons"));
 			button_create_script->set_icon(get_theme_icon("ScriptCreate", "EditorIcons"));
 			button_detach_script->set_icon(get_theme_icon("ScriptRemove", "EditorIcons"));
+			button_2d->set_icon(get_theme_icon("Node2D", "EditorIcons"));
+			button_3d->set_icon(get_theme_icon("Node3D", "EditorIcons"));
+			button_ui->set_icon(get_theme_icon("Control", "EditorIcons"));
+			button_custom->set_icon(get_theme_icon("Add", "EditorIcons"));
 
 			filter->set_right_icon(get_theme_icon("Search", "EditorIcons"));
 			filter->set_clear_button_enabled(true);

godotengine/godot

(cherry picked from commit 3d693856552da4998a98492afaae2dc716cbd8b2)

editor/scene_tree_dock.cpp
@@ -1118,7 +1118,7 @@ void SceneTreeDock::_notification(int p_what) {
 			beginner_node_shortcuts->set_name("BeginnerNodeShortcuts");
 			node_shortcuts->add_child(beginner_node_shortcuts);
 
-			Button *button_2d = memnew(Button);
+			button_2d = memnew(Button);
 			beginner_node_shortcuts->add_child(button_2d);
 			button_2d->set_text(TTR("2D Scene"));
 			button_2d->set_icon(get_icon("Node2D", "EditorIcons"));
@@ -1129,7 +1129,7 @@ void SceneTreeDock::_notification(int p_what) {
 			button_3d->set_icon(get_icon("Spatial", "EditorIcons"));
 			button_3d->connect("pressed", this, "_tool_selected", make_binds(TOOL_CREATE_3D_SCENE, false));
 
-			Button *button_ui = memnew(Button);
+			button_ui = memnew(Button);
 			beginner_node_shortcuts->add_child(button_ui);
 			button_ui->set_text(TTR("User Interface"));
 			button_ui->set_icon(get_icon("Control", "EditorIcons"));
@@ -1139,7 +1139,7 @@ void SceneTreeDock::_notification(int p_what) {
 			favorite_node_shortcuts->set_name("FavoriteNodeShortcuts");
 			node_shortcuts->add_child(favorite_node_shortcuts);
 
-			Button *button_custom = memnew(Button);
+			button_custom = memnew(Button);
 			node_shortcuts->add_child(button_custom);
 			button_custom->set_text(TTR("Other Node"));
 			button_custom->set_icon(get_icon("Add", "EditorIcons"));
@@ -1162,6 +1162,10 @@ void SceneTreeDock::_notification(int p_what) {
 			button_instance->set_icon(get_icon("Instance", "EditorIcons"));
 			button_create_script->set_icon(get_icon("ScriptCreate", "EditorIcons"));
 			button_detach_script->set_icon(get_icon("ScriptRemove", "EditorIcons"));
+			button_2d->set_icon(get_icon("Node2D", "EditorIcons"));
+			button_3d->set_icon(get_icon("Node3D", "EditorIcons"));
+			button_ui->set_icon(get_icon("Control", "EditorIcons"));
+			button_custom->set_icon(get_icon("Add", "EditorIcons"));
 
 			filter->set_right_icon(get_icon("Search", "EditorIcons"));
 			filter->set_clear_button_enabled(true);