0d3e8538ef4703955499fc0f8d532a99c92cb689
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-09-24 15:11:06 +0100

    Update Bullet Area overlaps when Area properties or shapes change.

--- modules/bullet/collision_object_bullet.cpp
+++ modules/bullet/collision_object_bullet.cpp
@@ -181,6 +181,7 @@ void CollisionObjectBullet::on_exit_area(AreaBullet *p_area) {
 
 void CollisionObjectBullet::set_godot_object_flags(int flags) {
 	bt_collision_object->setUserIndex2(flags);
+	updated = true;
 }
 
 int CollisionObjectBullet::get_godot_object_flags() const {
@@ -214,7 +215,7 @@ const btTransform &CollisionObjectBullet::get_transform__bullet() const {
 }
 
 void CollisionObjectBullet::notify_transform_changed() {
-	isTransformChanged = true;
+	updated = true;
 }
 
 RigidCollisionObjectBullet::~RigidCollisionObjectBullet() {
