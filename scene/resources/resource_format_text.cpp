ebb5821bcec8cde79c9e1491878f7259fed186b0
Danil Alexeev <danil@alexeev.xyz>
2020-09-29 14:01:01 +0300

    Improve appearance of [connection] and [editable] sections in .tscn files
    (cherry picked from commit 02c0edac60edda48a83cb007ad1e7bd2e58556ad)

--- scene/resources/resource_format_text.cpp
+++ scene/resources/resource_format_text.cpp
@@ -1741,6 +1741,9 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 		}
 
 		for (int i = 0; i < state->get_connection_count(); i++) {
+			if (i == 0) {
+				f->store_line("");
+			}
 
 			String connstr = "[connection";
 			connstr += " signal=\"" + String(state->get_connection_signal(i)) + "\"";
@@ -1765,7 +1768,10 @@ Error ResourceFormatSaverTextInstance::save(const String &p_path, const RES &p_r
 
 		Vector<NodePath> editable_instances = state->get_editable_instances();
 		for (int i = 0; i < editable_instances.size(); i++) {
-			f->store_line("\n[editable path=\"" + editable_instances[i].operator String() + "\"]");
+			if (i == 0) {
+				f->store_line("");
+			}
+			f->store_line("[editable path=\"" + editable_instances[i].operator String() + "\"]");
 		}
 	}
 
