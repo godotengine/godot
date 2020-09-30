2db23f3b9b974c15b4017d085e38a1b0fc0099cc
Haoyu Qiu <timothyqiu32@gmail.com>
2020-09-30 15:46:07 +0800

    Fixes FileSystem tree preview icon size on HiDPI

--- editor/editor_resource_preview.cpp
+++ editor/editor_resource_preview.cpp
@@ -164,7 +164,6 @@ void EditorResourcePreview::_generate_preview(Ref<ImageTexture> &r_texture, Ref<
 		r_texture = generated;
 
 		int small_thumbnail_size = EditorNode::get_singleton()->get_theme_base()->get_theme_icon("Object", "EditorIcons")->get_width(); // Kind of a workaround to retrieve the default icon size
-		small_thumbnail_size *= EDSCALE;
 
 		if (preview_generators[i]->can_generate_small_preview()) {
 			Ref<Texture2D> generated_small;
