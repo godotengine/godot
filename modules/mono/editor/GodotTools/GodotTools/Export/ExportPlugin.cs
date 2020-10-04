a047c0c228648c4bbf1e5ae97d36a44148f9b2f6
Ignacio Etcheverry <ignalfonsore@gmail.com>
2020-10-04 02:11:53 +0200

    C#: Re-work solution build output panel
    - Removed item list that displayed multiple build
      configurations launched. Now we only display
      the last build that was launched.
    - Display build output next to the issues list.
      Its visibility can be toggled off/on.
      This build output is obtained from the MSBuild
      process rather than the MSBuild logger. As such
      it displays some MSBuild fatal errors that
      previously couldn't be displayed.
    - Added a context menu to the issues list with
      the option to copy the issue text.
    - Replaced the 'Build Project' button in the panel
      with a popup menu with the options:
      - Build Solution
      - Rebuild Solution
      - Clean Solution
    - The bottom panel button was renamed from 'Mono'
      to 'MSBuild' and now display an error/warning icon
      if the last build had issues.

--- modules/mono/editor/GodotTools/GodotTools/Export/ExportPlugin.cs
+++ modules/mono/editor/GodotTools/GodotTools/Export/ExportPlugin.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Runtime.CompilerServices;
+using GodotTools.Build;
 using GodotTools.Core;
 using GodotTools.Internals;
 using JetBrains.Annotations;
@@ -143,6 +144,8 @@ namespace GodotTools.Export
 
         private void _ExportBeginImpl(string[] features, bool isDebug, string path, int flags)
         {
+            _ = flags; // Unused
+
             if (!File.Exists(GodotSharpDirs.ProjectSlnPath))
                 return;
 
@@ -154,12 +157,10 @@ namespace GodotTools.Export
 
             string buildConfig = isDebug ? "ExportDebug" : "ExportRelease";
 
-            string scriptsMetadataPath = Path.Combine(GodotSharpDirs.ResMetadataDir, $"scripts_metadata.{(isDebug ? "debug" : "release")}");
-            CsProjOperations.GenerateScriptsMetadata(GodotSharpDirs.ProjectCsProjPath, scriptsMetadataPath);
-
+            string scriptsMetadataPath = BuildManager.GenerateExportedGameScriptMetadata(isDebug);
             AddFile(scriptsMetadataPath, scriptsMetadataPath);
 
-            if (!BuildManager.BuildProjectBlocking(buildConfig, platform))
+            if (!BuildManager.BuildProjectBlocking(buildConfig, platform: platform))
                 throw new Exception("Failed to build project");
 
             // Add dependency assemblies
