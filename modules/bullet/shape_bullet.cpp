1c94b093634f5120a0a7248b4cda0bed5e399351
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-10-08 16:19:34 +0100

    Reapply -Avoid adding margin twice along capsule Y axis
    Co-authored-by: Andrea Catania <info@andreacatania.com>

--- modules/bullet/shape_bullet.cpp
+++ modules/bullet/shape_bullet.cpp
@@ -275,7 +275,7 @@ void CapsuleShapeBullet::setup(real_t p_height, real_t p_radius) {
 }
 
 btCollisionShape *CapsuleShapeBullet::create_bt_shape(const btVector3 &p_implicit_scale, real_t p_extra_edge) {
-	return prepare(ShapeBullet::create_shape_capsule(radius * p_implicit_scale[0] + p_extra_edge, height * p_implicit_scale[1] + p_extra_edge));
+	return prepare(ShapeBullet::create_shape_capsule(radius * p_implicit_scale[0] + p_extra_edge, height * p_implicit_scale[1]));
 }
 
 /* Cylinder */
