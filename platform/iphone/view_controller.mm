30ada535ef89d6ed5893bba21f653263913a5a0e
Sergey Minakov <naithar@icloud.com>
2020-10-08 14:30:53 +0300

    iOS: fix native video
    Fixed view not being displayed.
    Fixed view orientation change.

--- platform/iphone/view_controller.mm
+++ platform/iphone/view_controller.mm
@@ -218,8 +218,11 @@
 	} else {
 		// Create autoresizing view for video playback.
 		GodotNativeVideoView *videoView = [[GodotNativeVideoView alloc] initWithFrame:self.view.bounds];
-		videoView.autoresizingMask = UIViewAutoresizingFlexibleWidth & UIViewAutoresizingFlexibleHeight;
+		videoView.autoresizingMask = UIViewAutoresizingFlexibleWidth | UIViewAutoresizingFlexibleHeight;
 		[self.view addSubview:videoView];
+
+		self.videoView = videoView;
+
 		return [self.videoView playVideoAtPath:filePath volume:videoVolume audio:audioTrack subtitle:subtitleTrack];
 	}
 }
