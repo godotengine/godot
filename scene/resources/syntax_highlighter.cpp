b7079203553d802db906bf6241cf9223b882955c
Paulb23 <p_batty@hotmail.co.uk>
2020-10-11 17:07:33 +0100

    Update SyntaxHighlighter documentation

--- scene/resources/syntax_highlighter.cpp
+++ scene/resources/syntax_highlighter.cpp
@@ -120,6 +120,7 @@ void SyntaxHighlighter::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_clear_highlighting_cache"), &SyntaxHighlighter::_clear_highlighting_cache);
 
 	BIND_VMETHOD(MethodInfo(Variant::DICTIONARY, "_get_line_syntax_highlighting", PropertyInfo(Variant::INT, "p_line")));
+	BIND_VMETHOD(MethodInfo("_clear_highlighting_cache"));
 	BIND_VMETHOD(MethodInfo("_update_cache"));
 }
 
