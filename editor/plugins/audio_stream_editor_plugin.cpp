9c3a33a4e8166f814d58549555e602912ee0d604
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-10-19 19:34:35 +0300

    Fixup `ColorRect` and `TextureRect` renames

--- editor/plugins/audio_stream_editor_plugin.cpp
+++ editor/plugins/audio_stream_editor_plugin.cpp
@@ -44,8 +44,8 @@ void AudioStreamEditor::_notification(int p_what) {
 	if (p_what == NOTIFICATION_THEME_CHANGED || p_what == NOTIFICATION_ENTER_TREE) {
 		_play_button->set_icon(get_theme_icon("MainPlay", "EditorIcons"));
 		_stop_button->set_icon(get_theme_icon("Stop", "EditorIcons"));
-		_preview->set_frame_color(get_theme_color("dark_color_2", "Editor"));
-		set_frame_color(get_theme_color("dark_color_1", "Editor"));
+		_preview->set_color(get_theme_color("dark_color_2", "Editor"));
+		set_color(get_theme_color("dark_color_1", "Editor"));
 
 		_indicator->update();
 		_preview->update();
