KleirRampage45/redot-engine

SCons: Refactor and cleanup warnings definition

platform/uwp/detect.py
@@ -80,6 +80,9 @@ def configure(env):
     env["ENV"] = os.environ
     vc_base_path = os.environ["VCTOOLSINSTALLDIR"] if "VCTOOLSINSTALLDIR" in os.environ else os.environ["VCINSTALLDIR"]
 
+    # Force to use Unicode encoding
+    env.Append(MSVC_FLAGS=["/utf-8"])
+
     # ANGLE
     angle_root = os.getenv("ANGLE_SRC_PATH")
     env.Prepend(CPPPATH=[angle_root + "/include"])

platform/windows/detect.py
@@ -222,8 +222,8 @@ def configure_msvc(env, manual_msvc_config):
     ## Compile/link flags
 
     env.AppendUnique(CCFLAGS=["/MT", "/Gd", "/GR", "/nologo"])
-    if int(env["MSVC_VERSION"].split(".")[0]) >= 14:  # vs2015 and later
-        env.AppendUnique(CCFLAGS=["/utf-8"])
+    # Force to use Unicode encoding
+    env.Append(MSVC_FLAGS=["/utf-8"])
     env.AppendUnique(CXXFLAGS=["/TP"])  # assume all sources are C++
     if manual_msvc_config:  # should be automatic if SCons found it
         if os.getenv("WindowsSdkDir") is not None:

godotengine/godot

(cherry picked from commits 97f116d36bdc8a335f26eebb70a7480d96536ecb
and 56f3aba7b2c1c74556cfc1dd8f9b24af71c33b16)

platform/uwp/detect.py
@@ -78,6 +78,9 @@ def configure(env):
     env["ENV"] = os.environ
     vc_base_path = os.environ["VCTOOLSINSTALLDIR"] if "VCTOOLSINSTALLDIR" in os.environ else os.environ["VCINSTALLDIR"]
 
+    # Force to use Unicode encoding
+    env.AppendUnique(CCFLAGS=["/utf-8"])
+
     # ANGLE
     angle_root = os.getenv("ANGLE_SRC_PATH")
     env.Prepend(CPPPATH=[angle_root + "/include"])

platform/windows/detect.py
@@ -209,8 +209,8 @@ def configure_msvc(env, manual_msvc_config):
     ## Compile/link flags
 
     env.AppendUnique(CCFLAGS=["/MT", "/Gd", "/GR", "/nologo"])
-    if int(env["MSVC_VERSION"].split(".")[0]) >= 14:  # vs2015 and later
-        env.AppendUnique(CCFLAGS=["/utf-8"])
+    # Force to use Unicode encoding
+    env.AppendUnique(CCFLAGS=["/utf-8"])
     env.AppendUnique(CXXFLAGS=["/TP"])  # assume all sources are C++
     if manual_msvc_config:  # should be automatic if SCons found it
         if os.getenv("WindowsSdkDir") is not None: