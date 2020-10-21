6e99aab4cdf120b065d1b82f6380e5c115a3bdeb
George Marques <george@gmarqu.es>
2020-10-21 20:27:44 -0300

    GDScript: Don't clear depended parsers too soon
    It can wait until the analyzer itself is destructed, otherwise other
    phases might be using freed parsers.

--- modules/gdscript/gdscript_analyzer.cpp
+++ modules/gdscript/gdscript_analyzer.cpp
@@ -3370,7 +3370,6 @@ Error GDScriptAnalyzer::resolve_program() {
 		}
 		depended_parsers[E->get()]->raise_status(GDScriptParserRef::FULLY_SOLVED);
 	}
-	depended_parsers.clear();
 	return parser->errors.empty() ? OK : ERR_PARSE_ERROR;
 }
 
