958d23968d3ea600af867e6b0d6957723286e547
Tomasz Chabora <kobewi4e@gmail.com>
2020-10-30 23:29:07 +0100

    Disable active editors when node gets deselected

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -1807,6 +1807,7 @@ void EditorNode::push_item(Object *p_object, const String &p_property, bool p_in
 		node_dock->set_node(nullptr);
 		scene_tree_dock->set_selected(nullptr);
 		inspector_dock->update(nullptr);
+		_display_top_editors(false);
 		return;
 	}
 
