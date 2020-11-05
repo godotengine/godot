320a4444b27ee82442b3e9551aaed0663484213b
Andrii Doroshenko (Xrayez) <xrayez@gmail.com>
2020-11-05 18:05:58 +0200

    Remove unused `PHI` define in math funcs

--- core/math/math_funcs.cpp
+++ core/math/math_funcs.cpp
@@ -34,8 +34,6 @@
 
 RandomPCG Math::default_rand(RandomPCG::DEFAULT_SEED, RandomPCG::DEFAULT_INC);
 
-#define PHI 0x9e3779b9
-
 uint32_t Math::rand_from_seed(uint64_t *seed) {
 	RandomPCG rng = RandomPCG(*seed, RandomPCG::DEFAULT_INC);
 	uint32_t r = rng.rand();
