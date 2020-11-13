5df707002bfc393d441a0e5f14161ad4dc7b9b43
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-11-14 00:13:32 +0100

    Add a test suite for JSON
    This test suite doesn't attempt to test error/invalid cases since
    the current JSON parser has a lot of false negatives.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -42,6 +42,7 @@
 #include "test_expression.h"
 #include "test_gradient.h"
 #include "test_gui.h"
+#include "test_json.h"
 #include "test_list.h"
 #include "test_math.h"
 #include "test_method_bind.h"
