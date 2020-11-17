9450717571c1b1cd43be5b188e55442dcd412c51
Pedro J. Estébanez <pedrojrulez@gmail.com>
2020-11-17 10:13:41 +0100

    Fix crash in resoure duplicate

--- core/io/resource.cpp
+++ core/io/resource.cpp
@@ -209,8 +209,8 @@ Ref<Resource> Resource::duplicate(bool p_subresources) const {
 	List<PropertyInfo> plist;
 	get_property_list(&plist);
 
-	Resource *r = (Resource *)ClassDB::instance(get_class());
-	ERR_FAIL_COND_V(!r, Ref<Resource>());
+	Ref<Resource> r = (Resource *)ClassDB::instance(get_class());
+	ERR_FAIL_COND_V(r.is_null(), Ref<Resource>());
 
 	for (List<PropertyInfo>::Element *E = plist.front(); E; E = E->next()) {
 		if (!(E->get().usage & PROPERTY_USAGE_STORAGE)) {
@@ -230,7 +230,7 @@ Ref<Resource> Resource::duplicate(bool p_subresources) const {
 		}
 	}
 
-	return Ref<Resource>(r);
+	return r;
 }
 
 void Resource::_set_path(const String &p_path) {
