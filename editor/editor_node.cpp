fe69bb262ef3beefc23796ea9b9ad1f1c029b32e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2020-11-22 15:26:17 +0100

    Bind the editor's Search Help function to F1 by default
    Now that the F1 key is available, we can bind Search Help to the key
    generally used in other applications to open the help menu.
    
    (cherry picked from commit 42685f514f4f730cd88f654fd8d162f10d6e1d53)

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -6989,7 +6989,7 @@ EditorNode::EditorNode() {
 	ED_SHORTCUT("editor/editor_3d", TTR("Open 3D Editor"), KEY_MASK_CTRL | KEY_F2);
 	ED_SHORTCUT("editor/editor_script", TTR("Open Script Editor"), KEY_MASK_CTRL | KEY_F3);
 	ED_SHORTCUT("editor/editor_assetlib", TTR("Open Asset Library"), KEY_MASK_CTRL | KEY_F4);
-	ED_SHORTCUT("editor/editor_help", TTR("Search Help"), KEY_MASK_SHIFT | KEY_F1);
+	ED_SHORTCUT("editor/editor_help", TTR("Search Help"), KEY_F1);
 #endif
 	ED_SHORTCUT("editor/editor_next", TTR("Open the next Editor"));
 	ED_SHORTCUT("editor/editor_prev", TTR("Open the previous Editor"));
