076908bed940adac80ab0a058e8a6d6b2583d84e
clayjohn <claynjohn@gmail.com>
2020-11-22 17:51:31 -0800

    Environment brightness, contrast, saturation restore with color correction.
    Allow gradients and 2d images.
    
    Use shader versions for LUT in tonemap
    
    Co-authored-by: alex-poe <3957610+CptPotato@users.noreply.github.com>
    Co-authored-by: QbieShay <cislaghi.ilaria@gmail.com>
    Co-authored-by: Clay John <claynjohn@gmail.com>

--- servers/rendering_server.h
+++ servers/rendering_server.h
@@ -872,7 +872,7 @@ public:
 	};
 
 	virtual void environment_set_tonemap(RID p_env, EnvironmentToneMapper p_tone_mapper, float p_exposure, float p_white, bool p_auto_exposure, float p_min_luminance, float p_max_luminance, float p_auto_exp_speed, float p_auto_exp_grey) = 0;
-	virtual void environment_set_adjustment(RID p_env, bool p_enable, float p_brightness, float p_contrast, float p_saturation, RID p_ramp) = 0;
+	virtual void environment_set_adjustment(RID p_env, bool p_enable, float p_brightness, float p_contrast, float p_saturation, bool p_use_1d_color_correction, RID p_color_correction) = 0;
 
 	virtual void environment_set_ssr(RID p_env, bool p_enable, int p_max_steps, float p_fade_in, float p_fade_out, float p_depth_tolerance) = 0;
 
