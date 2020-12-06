1396c74734f289faf3e648ab232e691df6e963ae
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2020-12-06 19:25:31 +0100

    [HTML5] Use regular unix FileAccess implementation.

--- platform/javascript/os_javascript.cpp
+++ platform/javascript/os_javascript.cpp
@@ -51,7 +51,6 @@
 // Lifecycle
 void OS_JavaScript::initialize() {
 	OS_Unix::initialize_core();
-	FileAccess::make_default<FileAccessBufferedFA<FileAccessUnix>>(FileAccess::ACCESS_RESOURCES);
 	DisplayServerJavaScript::register_javascript_driver();
 
 #ifdef MODULE_WEBSOCKET_ENABLED
