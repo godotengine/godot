014efeb272977577cd8de6d59022693f78383ca8
Emmanuel Leblond <emmanuel.leblond@gmail.com>
2020-12-08 00:56:01 +0100

    Allow PluginScript to customize language's can_inherit_from_file attribute

--- modules/gdnative/pluginscript/pluginscript_language.cpp
+++ modules/gdnative/pluginscript/pluginscript_language.cpp
@@ -142,6 +142,10 @@ bool PluginScriptLanguage::supports_builtin_mode() const {
 	return _desc.supports_builtin_mode;
 }
 
+bool PluginScriptLanguage::can_inherit_from_file() {
+	return _desc.can_inherit_from_file;
+}
+
 int PluginScriptLanguage::find_function(const String &p_function, const String &p_code) const {
 	if (_desc.find_function) {
 		return _desc.find_function(_data, (godot_string *)&p_function, (godot_string *)&p_code);
