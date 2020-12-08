a6e44bd16c89b454dba697c96a0ee47c6a7ff156
Marcel Admiraal <madmiraal@users.noreply.github.com>
2020-12-08 17:35:30 +0000

    Rename Node3D's property translation to position

--- scene/3d/xr_nodes.cpp
+++ scene/3d/xr_nodes.cpp
@@ -493,7 +493,7 @@ TypedArray<String> XRAnchor3D::get_configuration_warnings() const {
 };
 
 Plane XRAnchor3D::get_plane() const {
-	Vector3 location = get_translation();
+	Vector3 location = get_position();
 	Basis orientation = get_transform().basis;
 
 	Plane plane(location, orientation.get_axis(1).normalized());
