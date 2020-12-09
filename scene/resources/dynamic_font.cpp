1b4fab3318734104ba29e725a6c5a5fb6c85464d
bruvzg <7645683+bruvzg@users.noreply.github.com>
2020-12-09 11:20:55 +0200

    [3.2] Fix fallback emoji font color.

--- scene/resources/dynamic_font.cpp
+++ scene/resources/dynamic_font.cpp
@@ -326,7 +326,7 @@ float DynamicFontAtSize::draw_char(RID p_canvas_item, const Point2 &p_pos, CharT
 			cpos.y -= font->get_ascent();
 			cpos.y += ch->v_align;
 			Color modulate = p_modulate;
-			if (FT_HAS_COLOR(face)) {
+			if (FT_HAS_COLOR(font->face)) {
 				modulate.r = modulate.g = modulate.b = 1.0;
 			}
 			RID texture = font->textures[ch->texture_idx].texture->get_rid();
