7f231feb753590618b743054c05e97e8eba2ec49
Michael Alexsander <michaelalexsander@protonmail.com>
2020-12-10 16:11:57 -0300

    Fix tooltip position in embedded window

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -1580,7 +1580,7 @@ void Viewport::_gui_show_tooltip() {
 	Point2 tooltip_offset = ProjectSettings::get_singleton()->get("display/mouse_cursor/tooltip_position_offset");
 	Rect2 r(gui.tooltip_pos + tooltip_offset, gui.tooltip_popup->get_contents_minimum_size());
 
-	Rect2i vr = gui.tooltip_popup->get_parent_visible_window()->get_usable_parent_rect();
+	Rect2i vr = gui.tooltip_popup->get_usable_parent_rect();
 
 	if (r.size.x + r.position.x > vr.size.x + vr.position.x) {
 		r.position.x = vr.position.x + vr.size.x - r.size.x;
