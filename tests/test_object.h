76d1886bce0004ed79486b525dba7572b2437d4a
Marco <mrodrigues3e8@gmail.com>
2020-12-14 22:22:37 -0500

    Add unit tests for object

--- tests/test_object.h
+++ tests/test_object.h
@@ -87,6 +87,97 @@ TEST_CASE("[Object] Metadata") {
 			meta_list2.size() == 0,
 			"The metadata list should contain 0 items after removing all metadata items.");
 }
+
+TEST_CASE("[Object] IAPI methods") {
+	Object object;
+    bool *rvalid = memnew(bool(false));
+
+    object.set(StringName("script"), "script_source", rvalid);
+	CHECK_MESSAGE(
+			*rvalid == true,
+			"set() should update the passed bool to the expected value.");
+
+    *rvalid = false;
+	CHECK_MESSAGE(
+			object.get(StringName("script"), rvalid) == "script_source",
+			"get() should return the expected value.");
+	CHECK_MESSAGE(
+			*rvalid == true,
+			"get() should update the passed bool to the expected value.");
+    
+    Vector<StringName> p_names;
+    object.set_indexed(p_names, "value", rvalid);
+    CHECK_MESSAGE(
+		*rvalid == false,
+		"set_indexed() should update the passed bool to the expected value for empty Vector<StringName>.");
+
+    *rvalid = true;
+    CHECK_MESSAGE(
+		object.get_indexed(p_names, rvalid) == Variant(),
+		"get_indexed() should return the expected value for empty Vector<StringName>.");
+    CHECK_MESSAGE(
+		*rvalid == false,
+		"get_indexed() should update the passed bool to the expected value for empty Vector<StringName>.");
+
+    p_names.push_back(StringName("script"));
+    object.set_indexed(p_names, "indexed_script_source", rvalid);
+    CHECK_MESSAGE(
+		*rvalid == true,
+		"set_indexed() should update the passed bool to the expected value for valid Vector<StringName> of size 1.");
+
+    *rvalid = false;
+    CHECK_MESSAGE(
+		object.get_indexed(p_names, rvalid) == "indexed_script_source",
+		"get_indexed() should return the expected value for valid Vector<StringName> of size 1.");
+    CHECK_MESSAGE(
+		*rvalid == true,
+		"get_indexed() should update the passed bool to the expected value for valid Vector<StringName> of size 1.");
+
+    CHECK_MESSAGE(
+		object.has_method(StringName("free")) == true,
+		"has_method should return the expected value for StringName('free')");
+
+    CHECK_MESSAGE(
+		object.has_method(StringName("_to_string")) == false,
+		"has_method should return the expected value for non-method");
+
+    CHECK_MESSAGE(
+		object.to_string() == "[Object:" + itos(object.get_instance_id()) + "]",
+		"to_string should return the expected value.");
+
+    *rvalid = false;
+    CHECK_MESSAGE(
+		object.getvar(StringName("script"), rvalid) == "indexed_script_source",
+		"getvar should return the expected value.");
+    CHECK_MESSAGE(
+		*rvalid == true,
+		"getvar should update the passed bool to the expected value.");
+
+    *rvalid = false;
+    object.setvar(StringName("script"), "var_script", rvalid);
+    CHECK_MESSAGE(
+		*rvalid == true,
+		"setvar should update the passed bool to the expected value.");
+    CHECK_MESSAGE(
+		object.getvar(StringName("script"), rvalid) == "var_script",
+		"getvar should return the expected value.");
+
+    object.setvar(StringName("non-CoreStringName"), "nonvalid_script", rvalid);
+    CHECK_MESSAGE(
+		*rvalid == false,
+		"setvar should update the passed bool to the expected value.");
+
+	memdelete(rvalid);
+}
+
+TEST_CASE("[Object] Script setter and getter") {
+	Object object;
+
+    object.set_script("script_source");
+	CHECK_MESSAGE(
+			object.get_script() == "script_source",
+			"get_script() should return the expected value.");
+}
 } // namespace TestObject
 
 #endif // TEST_OBJECT_H
