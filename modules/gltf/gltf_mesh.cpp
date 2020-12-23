4c4a405887ff8e57c05cb5c61972f716f242d068
Lyuma <xn.lyuma@gmail.com>
2020-12-23 02:11:13 -0800

    Fix mistake in GLTFMesh.mesh property

--- modules/gltf/gltf_mesh.cpp
+++ modules/gltf/gltf_mesh.cpp
@@ -37,7 +37,7 @@ void GLTFMesh::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_blend_weights"), &GLTFMesh::get_blend_weights);
 	ClassDB::bind_method(D_METHOD("set_blend_weights", "blend_weights"), &GLTFMesh::set_blend_weights);
 
-	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "EditorSceneImporterMesh"), "set_mesh", "get_mesh");
+	ADD_PROPERTY(PropertyInfo(Variant::OBJECT, "mesh"), "set_mesh", "get_mesh");
 	ADD_PROPERTY(PropertyInfo(Variant::PACKED_FLOAT32_ARRAY, "blend_weights"), "set_blend_weights", "get_blend_weights"); // Vector<float>
 }
 
