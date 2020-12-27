ca8c794d0422f2e64a0afa0dab9b31a6589c7024
Yuri Roubinsky <chaosus89@gmail.com>
2020-12-27 15:18:47 +0300

    Added optional id parameter to `PopupMenu::add_separator`

--- scene/gui/popup_menu.cpp
+++ scene/gui/popup_menu.cpp
@@ -1349,10 +1349,10 @@ void PopupMenu::remove_item(int p_idx) {
 	child_controls_changed();
 }
 
-void PopupMenu::add_separator(const String &p_text) {
+void PopupMenu::add_separator(const String &p_text, int p_id) {
 	Item sep;
 	sep.separator = true;
-	sep.id = -1;
+	sep.id = p_id;
 	if (p_text != String()) {
 		sep.text = p_text;
 		sep.xl_text = tr(p_text);
@@ -1600,7 +1600,7 @@ void PopupMenu::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("remove_item", "idx"), &PopupMenu::remove_item);
 
-	ClassDB::bind_method(D_METHOD("add_separator", "label"), &PopupMenu::add_separator, DEFVAL(String()));
+	ClassDB::bind_method(D_METHOD("add_separator", "label", "id"), &PopupMenu::add_separator, DEFVAL(String()), DEFVAL(-1));
 	ClassDB::bind_method(D_METHOD("clear"), &PopupMenu::clear);
 
 	ClassDB::bind_method(D_METHOD("_set_items"), &PopupMenu::_set_items);
