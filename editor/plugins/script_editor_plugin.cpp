KleirRampage45/redot-engine

Fix filename disambiguation on scripts in certain occasions

editor/plugins/script_editor_plugin.cpp
@@ -1954,7 +1954,7 @@ void ScriptEditor::_update_script_names() {
 		Vector<String> disambiguated_script_names;
 		Vector<String> full_script_paths;
 		for (int j = 0; j < sedata.size(); j++) {
-			disambiguated_script_names.append(sedata[j].name.replace("(*)", ""));
+			disambiguated_script_names.append(sedata[j].name.replace("(*)", "").get_file());
 			full_script_paths.append(sedata[j].tooltip);
 		}
 

godotengine/godot

(cherry picked from commit ed488b72a64b14558b8367841a67fefbaa78ecf2)

editor/plugins/script_editor_plugin.cpp
@@ -1907,7 +1907,7 @@ void ScriptEditor::_update_script_names() {
 		Vector<String> disambiguated_script_names;
 		Vector<String> full_script_paths;
 		for (int j = 0; j < sedata.size(); j++) {
-			disambiguated_script_names.push_back(sedata[j].name.replace("(*)", ""));
+			disambiguated_script_names.push_back(sedata[j].name.replace("(*)", "").get_file());
 			full_script_paths.push_back(sedata[j].tooltip);
 		}
 