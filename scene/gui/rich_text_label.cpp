d73281a35090357cef72b869a051ff2b83fbac4c
Alexander Pech <alexpech12@gmail.com>
2021-01-02 21:46:44 +1100

    Keep RichTextLabel visible character properties in sync
    The RichTextLabel class is inconsistent in how it updates the
    visible_characters and percent_visible properties when either is changed.
    
    To keep both properties consistent, update percent_visible when setting the
    visible_characters property.
    For both properties, when setting one, notify change for the other.
    
    Docs updated for member set_visible_characters on RichTextLabel class.
    
    (cherry picked from commit 463e2002abc0ecd7439fbf00966cef83842babce)

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -2688,6 +2688,7 @@ void RichTextLabel::set_percent_visible(float p_percent) {
 		visible_characters = get_total_character_count() * p_percent;
 		percent_visible = p_percent;
 	}
+	_change_notify("visible_characters");
 	update();
 }
 
@@ -2870,6 +2871,15 @@ void RichTextLabel::_bind_methods() {
 
 void RichTextLabel::set_visible_characters(int p_visible) {
 	visible_characters = p_visible;
+	if (p_visible == -1) {
+		percent_visible = 1;
+	} else {
+		int total_char_count = get_total_character_count();
+		if (total_char_count > 0) {
+			percent_visible = (float)p_visible / (float)total_char_count;
+		}
+	}
+	_change_notify("percent_visible");
 	update();
 }
 
