ff55157d3f1f14673317af19142c0df7a01a4540
Firepal <badbrad@outlook.fr>
2021-01-22 20:33:27 +0100

    Don't use prepass threshold with alpha scissor

--- drivers/gles2/shaders/scene.glsl
+++ drivers/gles2/shaders/scene.glsl
@@ -1695,9 +1695,13 @@ FRAGMENT_SHADER_CODE
 #endif // ALPHA_SCISSOR_USED
 
 #ifdef USE_DEPTH_PREPASS
+#if !defined(ALPHA_SCISSOR_USED)
+
 	if (alpha < 0.1) {
 		discard;
 	}
+
+#endif // not ALPHA_SCISSOR_USED
 #endif // USE_DEPTH_PREPASS
 
 #endif // !USE_SHADOW_TO_OPACITY
@@ -2255,9 +2259,13 @@ FRAGMENT_SHADER_CODE
 #endif // ALPHA_SCISSOR_USED
 
 #ifdef USE_DEPTH_PREPASS
+#if !defined(ALPHA_SCISSOR_USED)
+
 	if (alpha < 0.1) {
 		discard;
 	}
+
+#endif // not ALPHA_SCISSOR_USED
 #endif // USE_DEPTH_PREPASS
 
 #endif // !USE_SHADOW_TO_OPACITY
