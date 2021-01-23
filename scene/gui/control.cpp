349f25429b9e27bfa637576be2833614e4f860c7
Nils Reid <nils.reid@neoshadow.co>
2021-01-23 23:34:44 +0100

    Exposed find_next_valid_focus and find_prev_valid_focus.
    (cherry picked from commit 3e94c23fa528a71f320ca3103d50e6ce40ed7f1b)

--- scene/gui/control.cpp
+++ scene/gui/control.cpp
@@ -2865,6 +2865,8 @@ void Control::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("has_focus"), &Control::has_focus);
 	ClassDB::bind_method(D_METHOD("grab_focus"), &Control::grab_focus);
 	ClassDB::bind_method(D_METHOD("release_focus"), &Control::release_focus);
+	ClassDB::bind_method(D_METHOD("find_prev_valid_focus"), &Control::find_prev_valid_focus);
+	ClassDB::bind_method(D_METHOD("find_next_valid_focus"), &Control::find_next_valid_focus);
 	ClassDB::bind_method(D_METHOD("get_focus_owner"), &Control::get_focus_owner);
 
 	ClassDB::bind_method(D_METHOD("set_h_size_flags", "flags"), &Control::set_h_size_flags);
