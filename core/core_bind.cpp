5791283de4b2febf2f079d768a38633fc89329d2
Bastiaan Olij <mux213@gmail.com>
2021-02-25 22:20:13 +1100

    Expose set_environment to GDScript

--- core/core_bind.cpp
+++ core/core_bind.cpp
@@ -276,6 +276,10 @@ String _OS::get_environment(const String &p_var) const {
 	return OS::get_singleton()->get_environment(p_var);
 }
 
+bool _OS::set_environment(const String &p_var, const String &p_value) const {
+	return OS::get_singleton()->set_environment(p_var, p_value);
+}
+
 String _OS::get_name() const {
 	return OS::get_singleton()->get_name();
 }
@@ -712,6 +716,7 @@ void _OS::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_process_id"), &_OS::get_process_id);
 
 	ClassDB::bind_method(D_METHOD("get_environment", "environment"), &_OS::get_environment);
+	ClassDB::bind_method(D_METHOD("set_environment", "environment", "value"), &_OS::set_environment);
 	ClassDB::bind_method(D_METHOD("has_environment", "environment"), &_OS::has_environment);
 
 	ClassDB::bind_method(D_METHOD("get_name"), &_OS::get_name);
