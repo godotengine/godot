5cd5722f6aeebebd0a76dd45657190c2efac478e
kobewi <kobewi4e@gmail.com>
2021-03-03 19:39:29 +0100

    Deselect column only if belongs to deselected item

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -3354,10 +3354,10 @@ void Tree::item_selected(int p_column, TreeItem *p_item) {
 void Tree::item_deselected(int p_column, TreeItem *p_item) {
 	if (selected_item == p_item) {
 		selected_item = nullptr;
-	}
 
-	if (selected_col == p_column) {
-		selected_col = -1;
+		if (selected_col == p_column) {
+			selected_col = -1;
+		}
 	}
 
 	if (select_mode == SELECT_MULTI || select_mode == SELECT_SINGLE) {
