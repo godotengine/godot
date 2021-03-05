c8868fcaab22fbe81d88a0afcaecf222cd362e0a
Johannes <johawitt@outlook.de>
2021-03-05 18:24:06 +0100

    Fix removal of `rect_min_size` not triggering resize
    fixes #46672

--- scene/gui/control.cpp
+++ scene/gui/control.cpp
@@ -285,15 +285,11 @@ void Control::_update_minimum_size() {
 	}
 
 	Size2 minsize = get_combined_minimum_size();
-	if (minsize.x > data.size_cache.x ||
-			minsize.y > data.size_cache.y) {
-		_size_changed();
-	}
-
 	data.updating_last_minimum_size = false;
 
 	if (minsize != data.last_minimum_size) {
 		data.last_minimum_size = minsize;
+		_size_changed();
 		emit_signal(SceneStringNames::get_singleton()->minimum_size_changed);
 	}
 }
