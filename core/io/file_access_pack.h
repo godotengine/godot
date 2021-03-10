6aa8f7d85b2facd59ea43c6f76f1a6d35e833cb0
bruvzg <7645683+bruvzg@users.noreply.github.com>
2021-03-10 10:29:43 +0200

    Add symlink API to the DirAccess (on macOS and Linux).

--- core/io/file_access_pack.h
+++ core/io/file_access_pack.h
@@ -235,6 +235,10 @@ public:
 	virtual Error rename(String p_from, String p_to);
 	virtual Error remove(String p_name);
 
+	virtual bool is_link(String p_file) { return false; }
+	virtual String read_link(String p_file) { return p_file; }
+	virtual Error create_link(String p_source, String p_target) { return FAILED; }
+
 	uint64_t get_space_left();
 
 	virtual String get_filesystem_type() const;
