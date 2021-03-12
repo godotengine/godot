5c5f6b0f25d434f09c0666a51ea2fffeb3ca852f
jfons <joan.fonssanchez@gmail.com>
2021-03-12 12:57:04 +0100

    Fix visibility toggle for baked GridMaps

--- modules/gridmap/grid_map.cpp
+++ modules/gridmap/grid_map.cpp
@@ -715,6 +715,10 @@ void GridMap::_update_visibility() {
 			RS::get_singleton()->instance_set_visible(mi.instance, is_visible_in_tree());
 		}
 	}
+
+	for (int i = 0; i < baked_meshes.size(); i++) {
+		RS::get_singleton()->instance_set_visible(baked_meshes[i].instance, is_visible_in_tree());
+	}
 }
 
 void GridMap::_queue_octants_dirty() {
