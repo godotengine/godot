dfc39586d3c07a9360996b3791f5e3d95382e557
Yakov Borevich <j.borevich@gmail.com>
2021-03-13 14:58:08 +0300

    Optionally disable srgb decode gles3 feature (globally or per vendor)
    * this fixes dark textures on android+mali devices

--- servers/visual_server.cpp
+++ servers/visual_server.cpp
@@ -2439,6 +2439,9 @@ VisualServer::VisualServer() {
 	GLOBAL_DEF("rendering/quality/depth_prepass/enable", true);
 	GLOBAL_DEF("rendering/quality/depth_prepass/disable_for_vendors", "PowerVR,Mali,Adreno,Apple");
 
+	GLOBAL_DEF("rendering/quality/srgb_decode/enable", true);
+	GLOBAL_DEF("rendering/quality/srgb_decode/disable_for_vendors", "Mali");
+
 	GLOBAL_DEF("rendering/quality/filters/anisotropic_filter_level", 4);
 	ProjectSettings::get_singleton()->set_custom_property_info("rendering/quality/filters/anisotropic_filter_level", PropertyInfo(Variant::INT, "rendering/quality/filters/anisotropic_filter_level", PROPERTY_HINT_RANGE, "1,16,1"));
 	GLOBAL_DEF("rendering/quality/filters/use_nearest_mipmap_filter", false);
