c28428fe4dcd78886b972afcad6b239a58b4b973
Alex Hirsch <w4rh4wk@bluephoenix.at>
2021-03-16 22:55:11 +0100

    Allow nullptr with zero length in FileAccess get_buffer
    fix #47071

--- drivers/unix/file_access_unix.cpp
+++ drivers/unix/file_access_unix.cpp
@@ -240,7 +240,7 @@ uint8_t FileAccessUnix::get_8() const {
 }
 
 int FileAccessUnix::get_buffer(uint8_t *p_dst, int p_length) const {
-	ERR_FAIL_COND_V(!p_dst, -1);
+	ERR_FAIL_COND_V(!p_dst && p_length > 0, -1);
 	ERR_FAIL_COND_V(p_length < 0, -1);
 	ERR_FAIL_COND_V_MSG(!f, -1, "File must be opened before use.");
 	int read = fread(p_dst, 1, p_length, f);
