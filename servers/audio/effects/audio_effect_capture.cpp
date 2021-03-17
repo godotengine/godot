0fd0d4013c394c222af9a57e2c11dfb5b18f2b7d
jmb462 <jmb462@gmail.com>
2021-03-17 19:18:22 +0100

    Fix AudioEffectCapture buffer length cannot be changed

--- servers/audio/effects/audio_effect_capture.cpp
+++ servers/audio/effects/audio_effect_capture.cpp
@@ -91,8 +91,6 @@ Ref<AudioEffectInstance> AudioEffectCapture::instance() {
 }
 
 void AudioEffectCapture::set_buffer_length(float p_buffer_length_seconds) {
-	ERR_FAIL_COND(buffer_initialized);
-
 	buffer_length_seconds = p_buffer_length_seconds;
 }
 
