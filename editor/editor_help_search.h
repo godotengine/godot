917630107ca0196d330a1a620b543f400ad40ba9
Michael Alexsander <michaelalexsander@protonmail.com>
2021-03-19 18:49:21 -0300

    Select non-perfect matches if necessary in the Search Help dialog
    (cherry picked from commit 77597ea47cb6c5e7e0b9506cc8e8c305f0f59d9c)

--- editor/editor_help_search.h
+++ editor/editor_help_search.h
@@ -125,6 +125,7 @@ class EditorHelpSearch::Runner : public Reference {
 	TreeItem *root_item;
 	Map<String, TreeItem *> class_items;
 	TreeItem *matched_item;
+	float match_highest_score = 0;
 
 	bool _is_class_disabled_by_feature_profile(const StringName &p_class);
 
