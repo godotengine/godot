30842b2efefac83441d60686a79cb35acacfbf72
Haoyu Qiu <timothyqiu32@gmail.com>
2021-03-20 17:32:38 +0800

    Fix project icon size in Project Manager

--- editor/editor_themes.h
+++ editor/editor_themes.h
@@ -31,10 +31,13 @@
 #ifndef EDITOR_THEMES_H
 #define EDITOR_THEMES_H
 
+#include "scene/resources/texture.h"
 #include "scene/resources/theme.h"
 
 Ref<Theme> create_editor_theme(Ref<Theme> p_theme = nullptr);
 
 Ref<Theme> create_custom_theme(Ref<Theme> p_theme = nullptr);
+
+Ref<ImageTexture> create_unscaled_default_project_icon();
 
 #endif
