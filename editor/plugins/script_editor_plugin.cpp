756a71ca4f9af414d7659bf648eae9cc642c0987
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-03-24 19:50:41 +0100

    Rename the `.shader` file extension to `.gdshader`
    This lets third-party software recognize Godot shaders more easily,
    without relying on guesswork since the `.shader` extension is generic.

--- editor/plugins/script_editor_plugin.cpp
+++ editor/plugins/script_editor_plugin.cpp
@@ -3188,7 +3188,7 @@ void ScriptEditor::_on_find_in_files_result_selected(String fpath, int line_numb
 	if (ResourceLoader::exists(fpath)) {
 		RES res = ResourceLoader::load(fpath);
 
-		if (fpath.get_extension() == "shader") {
+		if (fpath.get_extension() == "gdshader") {
 			ShaderEditorPlugin *shader_editor = Object::cast_to<ShaderEditorPlugin>(EditorNode::get_singleton()->get_editor_data().get_editor("Shader"));
 			shader_editor->edit(res.ptr());
 			shader_editor->make_visible(true);
