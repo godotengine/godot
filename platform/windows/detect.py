f7a999c8149f766f23ce47895cda534945467e5f
Rémi Verschelde <rverschelde@gmail.com>
2021-03-29 14:33:05 +0200

    Windows: Disable ASLR protection w/ MinGW, it breaks Mono
    We might be able to make it work by building Mono itself with
    ASLR protection too, but there might still be issues when loading
    e.g. GDNative DLLs built without ASLR protection.
    
    In the short term this is not a goal, but we can reconsider later
    what is actually needed for ASLR protection to work and keep things
    user-friendly.

--- platform/windows/detect.py
+++ platform/windows/detect.py
@@ -397,7 +397,7 @@ def configure_mingw(env):
     ## Compile flags
 
     env.Append(CCFLAGS=["-mwindows"])
-    env.Append(LINKFLAGS=["-Wl,--nxcompat", "-Wl,--dynamicbase"])
+    env.Append(LINKFLAGS=["-Wl,--nxcompat"])  # DEP protection. Not enabling ASLR for now, Mono crashes.
     env.Append(CPPDEFINES=["WINDOWS_ENABLED", "OPENGL_ENABLED", "WASAPI_ENABLED", "WINMIDI_ENABLED"])
     env.Append(CPPDEFINES=[("WINVER", env["target_win_version"]), ("_WIN32_WINNT", env["target_win_version"])])
     env.Append(
