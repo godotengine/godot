b38a36923afc52a8f7470b59f92b8ad2c3021373
Kyle <eichlinkyle@gmail.com>
2021-03-31 15:00:31 -0400

    Fix gdnative config file set as null
    Fixes #45997.
    
    Setting a GDNativeLibrary config file as null or any other object but a ConfigFile will now cause an error.
    
    (cherry picked from commit 618dd892f59be0f1d527da29b8cd904ad67bc915)

--- modules/gdnative/gdnative.cpp
+++ modules/gdnative/gdnative.cpp
@@ -148,6 +148,7 @@ void GDNativeLibrary::_get_property_list(List<PropertyInfo> *p_list) const {
 }
 
 void GDNativeLibrary::set_config_file(Ref<ConfigFile> p_config_file) {
+	ERR_FAIL_COND(p_config_file.is_null());
 
 	set_singleton(p_config_file->get_value("general", "singleton", default_singleton));
 	set_load_once(p_config_file->get_value("general", "load_once", default_load_once));
