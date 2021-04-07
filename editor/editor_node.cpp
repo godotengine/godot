ee272826708fcf930ec37b3fc0e41e246e19326d
myood <myoodster@gmail.com>
2021-04-07 11:02:02 +0200

    Display Gradle's standard output/error to the console when exporting to Android #2559

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -5462,6 +5462,7 @@ int EditorNode::execute_and_show_output(const String &p_title, const String &p_p
 				String to_add = eta.output.substr(prev_len, eta.output.length());
 				prev_len = eta.output.length();
 				execute_outputs->add_text(to_add);
+				print_verbose(to_add);
 				Main::iteration();
 			}
 		}
