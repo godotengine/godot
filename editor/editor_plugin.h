e16fc9283e76b29caec316e6bc67a118597701af
kobewi <kobewi4e@gmail.com>
2021-04-07 22:08:41 +0200

    Expose edit_node() for editor plugins

--- editor/editor_plugin.h
+++ editor/editor_plugin.h
@@ -71,6 +71,7 @@ public:
 
 	Control *get_editor_main_control();
 	void edit_resource(const Ref<Resource> &p_resource);
+	void edit_node(Node *p_node);
 	void open_scene_from_path(const String &scene_path);
 	void reload_scene_from_path(const String &scene_path);
 
