3a3f39095241fbefcfec49f70dcbc7a0105548a7
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-04-11 18:53:23 +0200

    Allow selecting frames by holding down the mouse in SpriteFrames editor
    This complements the existing (Ctrl +) Shift + Left mouse button
    multiple frame (de)selection.

--- editor/plugins/sprite_frames_editor_plugin.h
+++ editor/plugins/sprite_frames_editor_plugin.h
@@ -87,6 +87,7 @@ class SpriteFramesEditor : public HSplitContainer {
 	Button *split_sheet_zoom_in;
 	EditorFileDialog *file_split_sheet;
 	Set<int> frames_selected;
+	Set<int> frames_toggled_by_mouse_hover;
 	int last_frame_selected;
 
 	float scale_ratio;
