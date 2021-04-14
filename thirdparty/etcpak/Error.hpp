638cfec853cfdbc95b5ccc67477afbaa3a53bc33
Rémi Verschelde <rverschelde@gmail.com>
2021-04-14 16:45:14 +0200

    etcpak: We only need the compression code, remove rest of etcpak app
    We do our own image loading, threading, and memory management in Godot already,
    so the only components we need from etcpak (at least as of now) are the
    `Compress*` methods defined in `ProcessDxtc.cpp` and `ProcessRGB.cpp`.
    
    So we don't need to compile or vendor the rest.

--- thirdparty/etcpak/Error.hpp
+++
@@ -1,9 +0,0 @@
-#ifndef __ERROR_HPP__
-#define __ERROR_HPP__
-
-#include "Bitmap.hpp"
-
-float CalcMSE3( const Bitmap& bmp, const Bitmap& out );
-float CalcMSE1( const Bitmap& bmp, const Bitmap& out );
-
-#endif
--- thirdparty/etcpak/Timing.hpp
+++
@@ -1,8 +0,0 @@
-#ifndef __DARKRL__TIMING_HPP__
-#define __DARKRL__TIMING_HPP__
-
-#include <stdint.h>
-
-uint64_t GetTime();
-
-#endif
