b4cc8ed6f2c3080f1c5e58d5b085b90e3564095e
MaxStgs <peters2011.pm@gmail.com>
2021-04-30 14:08:45 +0500

    Fix BakedLightmap bias bound check

--- scene/3d/baked_lightmap.cpp
+++ scene/3d/baked_lightmap.cpp
@@ -1446,7 +1446,7 @@ int BakedLightmap::get_bounces() const {
 }
 
 void BakedLightmap::set_bias(float p_bias) {
-	ERR_FAIL_COND(p_bias < 0.00001);
+	ERR_FAIL_COND(p_bias < 0.00001f);
 	bias = p_bias;
 }
 
