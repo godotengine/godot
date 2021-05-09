39f9b92f545113fe9ea3ad82e846fde6ace2bf70
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-05-09 12:29:50 +0200

    Save the current script when pressing Ctrl + S if no scene is open
    - Tweak the dialog messages to be more informative.
      - The "Saved N modified resources" dialog is not a warning per se,
        so make it more explicit.

--- editor/plugins/script_editor_plugin.h
+++ editor/plugins/script_editor_plugin.h
@@ -312,7 +312,6 @@ class ScriptEditor : public PanelContainer {
 
 	String _get_debug_tooltip(const String &p_text, Node *_se);
 
-	void _save_current_script();
 	void _resave_scripts(const String &p_str);
 	void _reload_scripts();
 
@@ -464,6 +463,7 @@ public:
 
 	void get_breakpoints(List<String> *p_breakpoints);
 
+	void save_current_script();
 	void save_all_scripts();
 
 	void set_window_layout(Ref<ConfigFile> p_layout);
