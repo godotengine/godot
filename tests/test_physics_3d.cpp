d1bc88d426b3751a37beb9c81513ccd16c7e2860
Morris Tabor <80684659+mortarroad@users.noreply.github.com>
2021-05-21 09:58:01 +0200

    Replace QuickHull with Bullet's convex hull computer.
    The code is based on the current version of thirdparty/vhacd and modified to use Godot's types and code style.
    
    Additional changes:
    - extended PagedAllocator to allow leaked objects
    - applied patch from https://github.com/bulletphysics/bullet3/pull/3037

--- tests/test_physics_3d.cpp
+++ tests/test_physics_3d.cpp
@@ -30,8 +30,8 @@
 
 #include "test_physics_3d.h"
 
+#include "core/math/convex_hull.h"
 #include "core/math/math_funcs.h"
-#include "core/math/quick_hull.h"
 #include "core/os/main_loop.h"
 #include "core/os/os.h"
 #include "core/string/print_string.h"
@@ -169,7 +169,7 @@ protected:
 
 		RID convex_mesh = vs->mesh_create();
 		Geometry3D::MeshData convex_data = Geometry3D::build_convex_mesh(convex_planes);
-		QuickHull::build(convex_data.vertices, convex_data);
+		ConvexHullComputer::convex_hull(convex_data.vertices, convex_data);
 		vs->mesh_add_surface_from_mesh_data(convex_mesh, convex_data);
 
 		type_mesh_map[PhysicsServer3D::SHAPE_CONVEX_POLYGON] = convex_mesh;
