5aa3e1ae9f805d8163f5b2d873d1c4ab9b10eb75
Moritz Kaltenbrunner <moritzkalt37@gmail.com>
2021-05-24 09:10:35 +0200

    Fixed tooltip popping up if window is unfocused while the timer is running

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -1618,7 +1618,8 @@ void Viewport::_gui_show_tooltip() {
 	gui.tooltip_popup->set_position(r.position);
 	gui.tooltip_popup->set_size(r.size);
 
-	gui.tooltip_popup->show();
+	if (window->has_focus())
+		gui.tooltip_popup->show();
 	gui.tooltip_popup->child_controls_changed();
 }
 
