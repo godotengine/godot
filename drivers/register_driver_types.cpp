fba05d1c954f1dc92bbfd6a0947caf6de33c670a
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2021-05-25 16:06:09 +0200

    Add DisplayServerDummy for headless display.
    Uses RasterizerDummy internally. This is always compiled in, maybe we
    should add a switch to enable/disable it via scons?

--- drivers/register_driver_types.cpp
+++ drivers/register_driver_types.cpp
@@ -30,6 +30,7 @@
 
 #include "register_driver_types.h"
 
+#include "drivers/dummy/display_server_headless.h"
 #include "drivers/png/image_loader_png.h"
 #include "drivers/png/resource_saver_png.h"
 
@@ -37,6 +38,8 @@ static ImageLoaderPNG *image_loader_png;
 static Ref<ResourceSaverPNG> resource_saver_png;
 
 void register_core_driver_types() {
+	DisplayServerHeadless::register_headless_driver();
+
 	image_loader_png = memnew(ImageLoaderPNG);
 	ImageLoader::add_image_format_loader(image_loader_png);
 
