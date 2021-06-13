2176204c710be365aa40249e179ae3707d2b94f7
Pedro J. Estébanez <pedrojrulez@gmail.com>
2021-06-13 13:32:44 +0200

    Fix slow load/save of scenes with many instances of the same script

--- modules/gdscript/gdscript.h
+++ modules/gdscript/gdscript.h
@@ -148,7 +148,7 @@ class GDScript : public Script {
 
 #endif
 
-	bool _update_exports(bool *r_err = nullptr, bool p_recursive_call = false);
+	bool _update_exports(bool *r_err = nullptr, bool p_recursive_call = false, PlaceHolderScriptInstance *p_instance_to_update = nullptr);
 
 	void _save_orphaned_subclasses();
 	void _init_rpc_methods_properties();
