42c9fefcca2191197b1cd7f7cc023904b1afcb2e
Marcel Admiraal <madmiraal@users.noreply.github.com>
2021-06-19 17:46:57 +0100

    Add adb output to error message when install fails
    (cherry picked from commit de2acbd4955950b5d37eca396b6ec4aadb872578)

--- platform/android/export/export.cpp
+++ platform/android/export/export.cpp
@@ -1910,7 +1910,7 @@ public:
 		err = OS::get_singleton()->execute(adb, args, true, nullptr, &output, &rv, true);
 		print_verbose(output);
 		if (err || rv != 0) {
-			EditorNode::add_io_error("Could not install to device.");
+			EditorNode::add_io_error("Could not install to device: " + output);
 			CLEANUP_AND_RETURN(ERR_CANT_CREATE);
 		}
 
