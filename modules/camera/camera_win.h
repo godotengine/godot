71d2ee84cbf568e41f84294adc7890316340bde1
Ben Armstrong <ben@avsoftlabs.com>
2021-06-21 01:23:01 +1000

    Implemented camera support on Windows

--- modules/camera/camera_win.h
+++ modules/camera/camera_win.h
@@ -35,12 +35,11 @@
 #include "servers/camera_server.h"
 
 class CameraWindows : public CameraServer {
-private:
-	void add_active_cameras();
-
 public:
 	CameraWindows();
 	~CameraWindows();
+
+	void update_feeds();
 };
 
 #endif /* CAMERAWIN_H */
