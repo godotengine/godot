c0c54a36ce3621e0713bc0fcca73d3b769bd2023
Pitanov V.V <dtho47@yandex.ru>
2021-07-02 03:26:26 +1000

    Fix GridMap erase Octans

--- modules/gridmap/grid_map.cpp
+++ modules/gridmap/grid_map.cpp
@@ -780,7 +780,7 @@ void GridMap::_update_octants_callback() {
 
 	while (to_delete.front()) {
 		octant_map.erase(to_delete.front()->get());
-		to_delete.pop_back();
+		to_delete.pop_front();
 	}
 
 	_update_visibility();
