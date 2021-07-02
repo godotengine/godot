e10f65d6356099b636cf49711a90501134e6ed34
Nick H <62965063+SirQuartz@users.noreply.github.com>
2021-07-02 00:02:28 -0400

    Add `get_dead_zone()` method to `InputMap`
    This commit adds a new method to the `InputMap`, allowing the user to get the value of an action's dead zone as a float.

--- core/input/input_map.cpp
+++ core/input/input_map.cpp
@@ -45,6 +45,7 @@ void InputMap::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("erase_action", "action"), &InputMap::erase_action);
 
 	ClassDB::bind_method(D_METHOD("action_set_deadzone", "action", "deadzone"), &InputMap::action_set_deadzone);
+	ClassDB::bind_method(D_METHOD("action_get_deadzone", "action"), &InputMap::action_get_deadzone);
 	ClassDB::bind_method(D_METHOD("action_add_event", "action", "event"), &InputMap::action_add_event);
 	ClassDB::bind_method(D_METHOD("action_has_event", "action", "event"), &InputMap::action_has_event);
 	ClassDB::bind_method(D_METHOD("action_erase_event", "action", "event"), &InputMap::action_erase_event);
