82bb8f15311929e7e6875c6f3937cb33a698fd68
Daniel Doran <ddoran3@gmail.com>
2021-07-03 16:22:35 -0600

    Fix transparency sorting

--- servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.cpp
+++ servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.cpp
@@ -374,7 +374,7 @@ void RenderForwardMobile::_render_scene(RenderDataRD *p_render_data, const Color
 
 	_fill_render_list(RENDER_LIST_OPAQUE, p_render_data, PASS_MODE_COLOR);
 	render_list[RENDER_LIST_OPAQUE].sort_by_key();
-	render_list[RENDER_LIST_ALPHA].sort_by_depth();
+	render_list[RENDER_LIST_ALPHA].sort_by_reverse_depth_and_priority();
 
 	// we no longer use this...
 	_fill_instance_data(RENDER_LIST_OPAQUE);
