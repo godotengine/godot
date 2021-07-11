c88d1608abcc08071c1bd55c4af92841c18908bb
ne0fhyk <fhuyakou@gmail.com>
2021-07-10 18:39:31 -0700

    Add partial support for Android scoped storage.
    This is done by providing API access to app specific directories which don't have any limitations and allows us to bump the target sdk version to 30.
    In addition, we're also bumping the min sdk version to 19 as version 18 is no longer supported by Google Play Services and only account of 0.3% of Android devices.

--- platform/x11/os_x11.cpp
+++ platform/x11/os_x11.cpp
@@ -3250,7 +3250,7 @@ String OS_X11::get_cache_path() const {
 	return get_config_path();
 }
 
-String OS_X11::get_system_dir(SystemDir p_dir) const {
+String OS_X11::get_system_dir(SystemDir p_dir, bool p_shared_storage) const {
 	String xdgparam;
 
 	switch (p_dir) {
