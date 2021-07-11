f84ae146f026ae60d417eda97ec6fe7afb0052fe
JestemStefan <37214990+JestemStefan@users.noreply.github.com>
2021-07-11 07:53:14 +0200

    Limit scale of node2D to EPSILON 0.00001 to prevent det==0 bug

--- scene/2d/node_2d.cpp
+++ scene/2d/node_2d.cpp
@@ -170,10 +170,10 @@ void Node2D::set_scale(const Size2 &p_scale) {
 	}
 	_scale = p_scale;
 	// Avoid having 0 scale values, can lead to errors in physics and rendering.
-	if (_scale.x == 0) {
+	if (Math::is_zero_approx(_scale.x)) {
 		_scale.x = CMP_EPSILON;
 	}
-	if (_scale.y == 0) {
+	if (Math::is_zero_approx(_scale.y)) {
 		_scale.y = CMP_EPSILON;
 	}
 	_update_transform();
