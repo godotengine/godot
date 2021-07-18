0098e9243cdeea1c000b4d390a4b71ae76e9f7e9
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-07-19 01:40:05 +0200

    Improve tooltips in the editor profiler to mention the script name
    Co-authored-by: CrispyPin <crispin@tasa.se>

--- editor/debugger/editor_profiler.cpp
+++ editor/debugger/editor_profiler.cpp
@@ -356,7 +356,7 @@ void EditorProfiler::_update_frame() {
 			item->set_metadata(1, it.script);
 			item->set_metadata(2, it.line);
 			item->set_text_align(2, TreeItem::ALIGN_RIGHT);
-			item->set_tooltip(0, it.script + ":" + itos(it.line));
+			item->set_tooltip(0, it.name + "\n" + it.script + ":" + itos(it.line));
 
 			float time = dtime == DISPLAY_SELF_TIME ? it.self : it.total;
 
