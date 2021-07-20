abb45f5fe7a1d5a44511675ee8695cc3859698b1
David Snopek <dsnopek@gmail.com>
2021-07-20 11:44:40 -0500

    Add get_buffered_amount() to WebRTCDataChannel

--- modules/webrtc/webrtc_data_channel_gdnative.cpp
+++ modules/webrtc/webrtc_data_channel_gdnative.cpp
@@ -111,6 +111,11 @@ bool WebRTCDataChannelGDNative::is_negotiated() const {
 	return interface->is_negotiated(interface->data);
 }
 
+int WebRTCDataChannelGDNative::get_buffered_amount() const {
+	ERR_FAIL_COND_V(interface == NULL, 0);
+	return interface->get_buffered_amount(interface->data);
+}
+
 Error WebRTCDataChannelGDNative::get_packet(const uint8_t **r_buffer, int &r_buffer_size) {
 	ERR_FAIL_COND_V(interface == nullptr, ERR_UNCONFIGURED);
 	return (Error)interface->get_packet(interface->data, r_buffer, &r_buffer_size);
