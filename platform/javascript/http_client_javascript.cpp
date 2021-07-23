KleirRampage45/redot-engine

[HTML5] Fix HTTPClient request_raw.
Now send data according to the spec, properly handle null data.
Simplify JS code since we are at it.

platform/javascript/http_client_javascript.cpp
@@ -108,8 +108,12 @@ Error HTTPClient::request_raw(Method p_method, const String &p_url, const Vector
 	Error err = prepare_request(p_method, p_url, p_headers);
 	if (err != OK)
 		return err;
-	PoolByteArray::Read read = p_body.read();
-	godot_xhr_send_data(xhr_id, read.ptr(), p_body.size());
+	if (p_body.empty()) {
+		godot_xhr_send(xhr_id, nullptr, 0);
+	} else {
+		PoolByteArray::Read read = p_body.read();
+		godot_xhr_send(xhr_id, read.ptr(), p_body.size());
+	}
 	return OK;
 }
 
@@ -118,7 +122,12 @@ Error HTTPClient::request(Method p_method, const String &p_url, const Vector<Str
 	Error err = prepare_request(p_method, p_url, p_headers);
 	if (err != OK)
 		return err;
-	godot_xhr_send_string(xhr_id, p_body.utf8().get_data());
+	if (p_body.empty()) {
+		godot_xhr_send(xhr_id, nullptr, 0);
+	} else {
+		const CharString cs = p_body.utf8();
+		godot_xhr_send(xhr_id, cs.get_data(), cs.length());
+	}
 	return OK;
 }
 

godotengine/godot

platform/javascript/http_client_javascript.cpp
@@ -104,7 +104,11 @@ Error HTTPClient::request_raw(Method p_method, const String &p_url, const Vector
 	Error err = prepare_request(p_method, p_url, p_headers);
 	if (err != OK)
 		return err;
-	godot_xhr_send_data(xhr_id, p_body.ptr(), p_body.size());
+	if (p_body.is_empty()) {
+		godot_xhr_send(xhr_id, nullptr, 0);
+	} else {
+		godot_xhr_send(xhr_id, p_body.ptr(), p_body.size());
+	}
 	return OK;
 }
 
@@ -112,7 +116,12 @@ Error HTTPClient::request(Method p_method, const String &p_url, const Vector<Str
 	Error err = prepare_request(p_method, p_url, p_headers);
 	if (err != OK)
 		return err;
-	godot_xhr_send_string(xhr_id, p_body.utf8().get_data());
+	if (p_body.is_empty()) {
+		godot_xhr_send(xhr_id, nullptr, 0);
+	} else {
+		const CharString cs = p_body.utf8();
+		godot_xhr_send(xhr_id, cs.get_data(), cs.length());
+	}
 	return OK;
 }
 