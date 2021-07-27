8d35dfa6d2ed0741ba5d95bf6d2f205b33f59561
vitika9 <vitika.program@gmail.com>
2021-07-27 15:26:00 +0530

    Removed Redundant initialization of probe_half_size

--- servers/rendering/renderer_rd/renderer_scene_gi_rd.cpp
+++ servers/rendering/renderer_rd/renderer_scene_gi_rd.cpp
@@ -793,8 +793,7 @@ void RendererSceneGIRD::SDFGI::update(RendererSceneEnvironmentRD *p_env, const V
 		SDFGI::Cascade &cascade = cascades[i];
 		cascade.dirty_regions = Vector3i();
 
-		Vector3 probe_half_size = Vector3(1, 1, 1) * cascade.cell_size * float(cascade_size / SDFGI::PROBE_DIVISOR) * 0.5;
-		probe_half_size = Vector3(0, 0, 0);
+		Vector3 probe_half_size = Vector3(0, 0, 0);
 
 		Vector3 world_position = p_world_position;
 		world_position.y *= y_mult;
