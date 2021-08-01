4574c671f6bee8c7b7f6316183362aa68dd254b0
Antokolos <antokolos@gmail.com>
2021-08-02 02:48:10 +0600

    UWP: Fix app crash when `managed_object->update_clipboard()` is called
    (cherry picked from commit f5693969f0bbb4cba8b1cfa2513b169c43c04ede)

--- platform/uwp/app.h
+++ platform/uwp/app.h
@@ -69,6 +69,7 @@ namespace GodotUWP
 		// Window event handlers.
 		void OnWindowSizeChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::WindowSizeChangedEventArgs^ args);
 		void OnVisibilityChanged(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::VisibilityChangedEventArgs^ args);
+		void OnWindowActivated(CoreWindow ^ sender, WindowActivatedEventArgs ^ args);
 		void OnWindowClosed(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::CoreWindowEventArgs^ args);
 
 		void pointer_event(Windows::UI::Core::CoreWindow^ sender, Windows::UI::Core::PointerEventArgs^ args, bool p_pressed, bool p_is_wheel = false);
