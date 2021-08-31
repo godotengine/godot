ea13ff860de2227ad343b8a2d9a2e37150473d09
Vincent <vinsteimes@gmail.com>
2021-09-01 01:17:33 +0200

    implement individual mesh transform for meshlibrary items
    (cherry picked from commit 70108fd8509a09230933872c3042a7c7ef2545b8)

--- editor/plugins/mesh_library_editor_plugin.h
+++ editor/plugins/mesh_library_editor_plugin.h
@@ -41,8 +41,10 @@ class MeshLibraryEditor : public Control {
 
 	EditorNode *editor;
 	MenuButton *menu;
-	ConfirmationDialog *cd;
+	ConfirmationDialog *cd_remove;
+	ConfirmationDialog *cd_update;
 	EditorFileDialog *file;
+	bool apply_xforms;
 	int to_erase;
 
 	enum {
@@ -50,15 +52,17 @@ class MeshLibraryEditor : public Control {
 		MENU_OPTION_ADD_ITEM,
 		MENU_OPTION_REMOVE_ITEM,
 		MENU_OPTION_UPDATE_FROM_SCENE,
-		MENU_OPTION_IMPORT_FROM_SCENE
+		MENU_OPTION_IMPORT_FROM_SCENE,
+		MENU_OPTION_IMPORT_FROM_SCENE_APPLY_XFORMS
 	};
 
 	int option;
 	void _import_scene_cbk(const String &p_str);
 	void _menu_cbk(int p_option);
-	void _menu_confirm();
+	void _menu_remove_confirm();
+	void _menu_update_confirm(bool p_apply_xforms);
 
-	static void _import_scene(Node *p_scene, Ref<MeshLibrary> p_library, bool p_merge);
+	static void _import_scene(Node *p_scene, Ref<MeshLibrary> p_library, bool p_merge, bool p_apply_xforms);
 
 protected:
 	static void _bind_methods();
@@ -67,7 +71,7 @@ public:
 	MenuButton *get_menu_button() const { return menu; }
 
 	void edit(const Ref<MeshLibrary> &p_mesh_library);
-	static Error update_library_file(Node *p_base_scene, Ref<MeshLibrary> ml, bool p_merge = true);
+	static Error update_library_file(Node *p_base_scene, Ref<MeshLibrary> ml, bool p_merge = true, bool p_apply_xforms = false);
 
 	MeshLibraryEditor(EditorNode *p_editor);
 };
--- scene/resources/mesh_library.h
+++ scene/resources/mesh_library.h
@@ -52,6 +52,7 @@ public:
 		Vector<ShapeData> shapes;
 		Ref<Texture> preview;
 		Transform navmesh_transform;
+		Transform mesh_transform;
 		Ref<NavigationMesh> navmesh;
 	};
 
@@ -71,12 +72,14 @@ public:
 	void create_item(int p_item);
 	void set_item_name(int p_item, const String &p_name);
 	void set_item_mesh(int p_item, const Ref<Mesh> &p_mesh);
+	void set_item_mesh_transform(int p_item, const Transform &p_transform);
 	void set_item_navmesh(int p_item, const Ref<NavigationMesh> &p_navmesh);
 	void set_item_navmesh_transform(int p_item, const Transform &p_transform);
 	void set_item_shapes(int p_item, const Vector<ShapeData> &p_shapes);
 	void set_item_preview(int p_item, const Ref<Texture> &p_preview);
 	String get_item_name(int p_item) const;
 	Ref<Mesh> get_item_mesh(int p_item) const;
+	Transform get_item_mesh_transform(int p_item) const;
 	Ref<NavigationMesh> get_item_navmesh(int p_item) const;
 	Transform get_item_navmesh_transform(int p_item) const;
 	Vector<ShapeData> get_item_shapes(int p_item) const;
