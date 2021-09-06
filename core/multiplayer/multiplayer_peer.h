7c81ce43ec903001b5556aa4189674c15be541ff
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2021-09-06 18:09:29 +0200

    [Net] Bind Multiplayer to GDExtension.

--- core/multiplayer/multiplayer_peer.h
+++ core/multiplayer/multiplayer_peer.h
@@ -34,6 +34,10 @@
 #include "core/io/packet_peer.h"
 #include "core/multiplayer/multiplayer.h"
 
+#include "core/object/gdvirtual.gen.inc"
+#include "core/object/script_language.h"
+#include "core/variant/native_ptr.h"
+
 class MultiplayerPeer : public PacketPeer {
 	GDCLASS(MultiplayerPeer, PacketPeer);
 
@@ -70,11 +74,66 @@ public:
 	virtual bool is_refusing_new_connections() const = 0;
 
 	virtual ConnectionStatus get_connection_status() const = 0;
+
 	uint32_t generate_unique_id() const;
 
 	MultiplayerPeer() {}
 };
 
-VARIANT_ENUM_CAST(MultiplayerPeer::ConnectionStatus)
+VARIANT_ENUM_CAST(MultiplayerPeer::ConnectionStatus);
+
+class MultiplayerPeerExtension : public MultiplayerPeer {
+	GDCLASS(MultiplayerPeerExtension, MultiplayerPeer);
+
+protected:
+	static void _bind_methods();
+
+public:
+	/* PacketPeer */
+	virtual int get_available_packet_count() const override;
+	virtual Error get_packet(const uint8_t **r_buffer, int &r_buffer_size) override; ///< buffer is GONE after next get_packet
+	virtual Error put_packet(const uint8_t *p_buffer, int p_buffer_size) override;
+	virtual int get_max_packet_size() const override;
+
+	/* MultiplayerPeer */
+	virtual void set_transfer_channel(int p_channel) override;
+	virtual int get_transfer_channel() const override;
+	virtual void set_transfer_mode(Multiplayer::TransferMode p_mode) override;
+	virtual Multiplayer::TransferMode get_transfer_mode() const override;
+	virtual void set_target_peer(int p_peer_id) override;
+
+	virtual int get_packet_peer() const override;
+
+	virtual bool is_server() const override;
+
+	virtual void poll() override;
+
+	virtual int get_unique_id() const override;
+
+	virtual void set_refuse_new_connections(bool p_enable) override;
+	virtual bool is_refusing_new_connections() const override;
+
+	virtual ConnectionStatus get_connection_status() const override;
+
+	/* PacketPeer GDExtension */
+	GDVIRTUAL0RC(int, _get_available_packet_count);
+	GDVIRTUAL2R(int, _get_packet, GDNativeConstPtr<const uint8_t *>, GDNativePtr<int>);
+	GDVIRTUAL2R(int, _put_packet, GDNativeConstPtr<const uint8_t>, int);
+	GDVIRTUAL0RC(int, _get_max_packet_size);
+
+	/* MultiplayerPeer GDExtension */
+	GDVIRTUAL1(_set_transfer_channel, int);
+	GDVIRTUAL0RC(int, _get_transfer_channel);
+	GDVIRTUAL1(_set_transfer_mode, int);
+	GDVIRTUAL0RC(int, _get_transfer_mode);
+	GDVIRTUAL1(_set_target_peer, int);
+	GDVIRTUAL0RC(int, _get_packet_peer);
+	GDVIRTUAL0RC(bool, _is_server);
+	GDVIRTUAL0R(int, _poll);
+	GDVIRTUAL0RC(int, _get_unique_id);
+	GDVIRTUAL1(_set_refuse_new_connections, bool);
+	GDVIRTUAL0RC(bool, _is_refusing_new_connections);
+	GDVIRTUAL0RC(int, _get_connection_status);
+};
 
 #endif // NETWORKED_MULTIPLAYER_PEER_H
