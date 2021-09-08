e0e57a49a522cb76b18f3760ef84d8e06729cf32
Jean Pierre Cimalando <jp-dev@inbox.ru>
2021-09-08 23:47:57 +0200

    Fix some formatting problems

--- drivers/jack/audio_driver_jack.cpp
+++ drivers/jack/audio_driver_jack.cpp
@@ -5,8 +5,8 @@
 /*                           GODOT ENGINE                                */
 /*                      https://godotengine.org                          */
 /*************************************************************************/
-/* Copyright (c) 2007-2020 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2020 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -31,8 +31,8 @@
 #ifdef JACK_ENABLED
 
 #include "audio_driver_jack.h"
-#include "core/os/os.h"
 #include "core/config/project_settings.h"
+#include "core/os/os.h"
 #include "core/version.h"
 
 #include <stdio.h>
@@ -330,8 +330,7 @@ int AudioDriverJACK::process_func(jack_nframes_t total_frames, void *p_udata) {
 
 			if (capture_channels >= 2) {
 				ch_in2 = (float *)jack_port_get_buffer(capture_ports[1], total_frames) + frame_index;
-			}
-			else {
+			} else {
 				ch_in2 = ch_in1; // mono device
 			}
 
