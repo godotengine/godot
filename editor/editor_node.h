8f66e6148dd8e7b25e9ae023a721cd8240e5b3c2
Matthew Newall <matthewnewall@hotmail.com>
2021-09-11 01:35:13 +0000

    Corrected Save Scene and Save All Scenes not working when the scene's dir no longer exists
    (cherry picked from commit ed2280528f875d9fbaa6d07fd71bcf4931b0342e)

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -311,6 +311,7 @@ private:
 	ConfirmationDialog *pick_main_scene;
 	Button *select_current_scene_button;
 	AcceptDialog *accept;
+	AcceptDialog *save_accept;
 	EditorAbout *about;
 	AcceptDialog *warning;
 
@@ -802,6 +803,7 @@ public:
 	Ref<Texture> get_class_icon(const String &p_class, const String &p_fallback = "Object") const;
 
 	void show_accept(const String &p_text, const String &p_title);
+	void show_save_accept(const String &p_text, const String &p_title);
 	void show_warning(const String &p_text, const String &p_title = TTR("Warning!"));
 
 	void _copy_warning(const String &p_str);
