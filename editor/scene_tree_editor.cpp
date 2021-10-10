KleirRampage45/redot-engine

Fix right clicking sub nodes after renaming parent

editor/scene_tree_editor.cpp
@@ -527,7 +527,7 @@ void SceneTreeEditor::_node_removed(Node *p_node) {
 }
 
 void SceneTreeEditor::_node_renamed(Node *p_node) {
-	if (!get_scene_node()->is_ancestor_of(p_node)) {
+	if (p_node != get_scene_node() && !get_scene_node()->is_ancestor_of(p_node)) {
 		return;
 	}
 

godotengine/godot

(cherry picked from commit f25a698f8084d547d1486e814315b588f1d4eb13)

editor/scene_tree_editor.cpp
@@ -541,7 +541,7 @@ void SceneTreeEditor::_node_removed(Node *p_node) {
 }
 
 void SceneTreeEditor::_node_renamed(Node *p_node) {
-	if (!get_scene_node()->is_a_parent_of(p_node)) {
+	if (p_node != get_scene_node() && !get_scene_node()->is_a_parent_of(p_node)) {
 		return;
 	}
 