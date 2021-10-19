fa2feb9bec63e66d4c870612d20724450dcb136f
Kemikal1 <alex_goku2007@yahoo.com>
2021-10-19 16:40:07 +0300

    Change the unreacheable return value of shape_find_owner()
    (cherry picked from commit 563d3dbf9408c03ff89056d03616c28c1e2c586f)

--- scene/3d/collision_object.cpp
+++ scene/3d/collision_object.cpp
@@ -525,7 +525,7 @@ void CollisionObject::shape_owner_clear_shapes(uint32_t p_owner) {
 }
 
 uint32_t CollisionObject::shape_find_owner(int p_shape_index) const {
-	ERR_FAIL_INDEX_V(p_shape_index, total_subshapes, 0);
+	ERR_FAIL_INDEX_V(p_shape_index, total_subshapes, UINT32_MAX);
 
 	for (const Map<uint32_t, ShapeData>::Element *E = shapes.front(); E; E = E->next()) {
 		for (int i = 0; i < E->get().shapes.size(); i++) {
@@ -536,7 +536,7 @@ uint32_t CollisionObject::shape_find_owner(int p_shape_index) const {
 	}
 
 	//in theory it should be unreachable
-	return 0;
+	ERR_FAIL_V_MSG(UINT32_MAX, "Can't find owner for shape index " + itos(p_shape_index) + ".");
 }
 
 CollisionObject::CollisionObject(RID p_rid, bool p_area) {
