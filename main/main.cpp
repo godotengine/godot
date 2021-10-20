73f6351e2862f349f08892997f1c401fd66c8e2c
Rémi Verschelde <rverschelde@gmail.com>
2021-10-20 12:04:59 +0200

    Main: Add documentation for `--dump-extension-api`

--- main/main.cpp
+++ main/main.cpp
@@ -376,7 +376,9 @@ void Main::print_help(const char *p_binary) {
 	OS::get_singleton()->print("  --doctool [<path>]                           Dump the engine API reference to the given <path> (defaults to current dir) in XML format, merging if existing files are found.\n");
 	OS::get_singleton()->print("  --no-docbase                                 Disallow dumping the base types (used with --doctool).\n");
 	OS::get_singleton()->print("  --build-solutions                            Build the scripting solutions (e.g. for C# projects). Implies --editor and requires a valid project to edit.\n");
+	OS::get_singleton()->print("  --dump-extension-api                         Generate JSON dump of the Godot API for GDExtension bindings named 'extension_api.json' in the current folder.\n");
 #ifdef DEBUG_METHODS_ENABLED
+	// TODO: Should be removed together with nativescript eventually.
 	OS::get_singleton()->print("  --gdnative-generate-json-api <path>          Generate JSON dump of the Godot API for GDNative bindings and save it on the file specified in <path>.\n");
 	OS::get_singleton()->print("  --gdnative-generate-json-builtin-api <path>  Generate JSON dump of the Godot API of the builtin Variant types and utility functions for GDNative bindings and save it on the file specified in <path>.\n");
 #endif
@@ -901,21 +903,21 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 			auto_build_solutions = true;
 			editor = true;
 			cmdline_tool = true;
-
+#ifdef DEBUG_METHODS_ENABLED
 		} else if (I->get() == "--gdnative-generate-json-api" || I->get() == "--gdnative-generate-json-builtin-api") {
 			// Register as an editor instance to use low-end fallback if relevant.
 			editor = true;
 			cmdline_tool = true;
-
-			// We still pass it to the main arguments since the argument handling itself is not done in this function
+			// We still pass it to the main arguments since the argument handling itself is not done in this function,
+			// it's done in nativescript init code.
 			main_args.push_back(I->get());
+#endif
 		} else if (I->get() == "--dump-extension-api") {
 			// Register as an editor instance to use low-end fallback if relevant.
 			editor = true;
 			cmdline_tool = true;
 			dump_extension_api = true;
-			print_line("dump extension?");
-			main_args.push_back(I->get());
+			print_line("Dumping Extension API");
 		} else if (I->get() == "--export" || I->get() == "--export-debug" ||
 				   I->get() == "--export-pack") { // Export project
 			// Actually handling is done in start().
