d5c32e8e3de8854250c2a4708bf696d572c8a486
Hugo Locurcio <hugo.locurcio@hugo.pro>
2021-10-26 17:19:02 +0200

    Remove outdated comment about OpenGL Compatibility Profile

--- platform/linuxbsd/gl_manager_x11.cpp
+++ platform/linuxbsd/gl_manager_x11.cpp
@@ -175,8 +175,6 @@ Error GLManager_X11::_create_context(GLDisplay &gl_display) {
 
 	switch (context_type) {
 		case GLES_3_0_COMPATIBLE: {
-			// FIXME: Use `GLX_CONTEXT_CORE_PROFILE_BIT_ARB` instead of compatibility profile
-			// once deprecated API usages are fixed.
 			static int context_attribs[] = {
 				GLX_CONTEXT_MAJOR_VERSION_ARB, 3,
 				GLX_CONTEXT_MINOR_VERSION_ARB, 3,
