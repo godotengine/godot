1239b85cf39aae19e4a1e14a6ad03df0ea76d20f
Jussi Pakkanen <jpakkane@gmail.com>
2021-10-29 16:58:13 +0300

    Write generated gl headers to build dir.

--- glsl_builders.py
+++ glsl_builders.py
@@ -115,16 +115,13 @@ def include_file_in_rd_header(filename: str, header_data: RDHeaderStruct, depth:
     return header_data
 
 
-def build_rd_header(
-    filename: str, optional_output_filename: str = None, header_data: Optional[RDHeaderStruct] = None
-) -> None:
+def build_rd_header(filename, out_file, header_data = None):
     header_data = header_data or RDHeaderStruct()
     include_file_in_rd_header(filename, header_data, 0)
 
-    if optional_output_filename is None:
-        out_file = filename + ".gen.h"
-    else:
-        out_file = optional_output_filename
+    fd = open(out_file, "w")
+
+    fd.write("/* WARNING, THIS FILE WAS GENERATED, DO NOT EDIT */\n")
 
     out_file_base = out_file
     out_file_base = out_file_base[out_file_base.rfind("/") + 1 :]
@@ -171,8 +168,9 @@ public:
 
 
 def build_rd_headers(target, source, env):
-    for x in source:
-        build_rd_header(filename=str(x))
+    assert len(target) == len(source)
+    for i in range(len(source)):
+        build_rd_header(source[i], target[i])
 
 
 class RAWHeaderStruct:
@@ -199,16 +197,13 @@ def include_file_in_raw_header(filename: str, header_data: RAWHeaderStruct, dept
     fs.close()
 
 
-def build_raw_header(
-    filename: str, optional_output_filename: str = None, header_data: Optional[RAWHeaderStruct] = None
-):
+def build_raw_header(filename, out_file, header_data=None):
     header_data = header_data or RAWHeaderStruct()
     include_file_in_raw_header(filename, header_data, 0)
 
-    if optional_output_filename is None:
-        out_file = filename + ".gen.h"
-    else:
-        out_file = optional_output_filename
+    fd = open(out_file, "w")
+
+    fd.write("/* WARNING, THIS FILE WAS GENERATED, DO NOT EDIT */\n")
 
     out_file_base = out_file.replace(".glsl.gen.h", "_shader_glsl")
     out_file_base = out_file_base[out_file_base.rfind("/") + 1 :]
@@ -230,8 +225,9 @@ static const char {out_file_base}[] = {{
 
 
 def build_raw_headers(target, source, env):
-    for x in source:
-        build_raw_header(filename=str(x))
+    assert len(target) == len(source)
+    for i in range(len(source)):
+        build_raw_header(str(source[i]), str(target[i]))
 
 
 if __name__ == "__main__":
