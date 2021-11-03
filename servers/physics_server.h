c2a108fe7d9a95dd45f9af6360a12c829e61586e
PouleyKetchoupp <pouleyketchoup@gmail.com>
2021-11-03 16:32:16 -0700

    Expose intersect_point in 3D physics server
    Also cleaned a few things in the documentation for 2D.
    
    (cherry picked from commit fc2bd63ca09d99c9a1f06293caf2d2d13b3f1866)

--- servers/physics_server.h
+++ servers/physics_server.h
@@ -143,6 +143,7 @@ class PhysicsDirectSpaceState : public Object {
 
 private:
 	Dictionary _intersect_ray(const Vector3 &p_from, const Vector3 &p_to, const Vector<RID> &p_exclude = Vector<RID>(), uint32_t p_collision_mask = 0, bool p_collide_with_bodies = true, bool p_collide_with_areas = false);
+	Array _intersect_point(const Vector3 &p_point, int p_max_results = 32, const Vector<RID> &p_exclude = Vector<RID>(), uint32_t p_layers = 0, bool p_collide_with_bodies = true, bool p_collide_with_areas = false);
 	Array _intersect_shape(const Ref<PhysicsShapeQueryParameters> &p_shape_query, int p_max_results = 32);
 	Array _cast_motion(const Ref<PhysicsShapeQueryParameters> &p_shape_query, const Vector3 &p_motion);
 	Array _collide_shape(const Ref<PhysicsShapeQueryParameters> &p_shape_query, int p_max_results = 32);
