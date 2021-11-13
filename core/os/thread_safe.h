8844df0db867fea9a6be0553948fbd6e207526ef
Hanif Ariffin <hanif.ariffin.4326@gmail.com>
2021-11-13 22:46:37 +0800

    Fix formatting issue
    Signed-off-by: Hanif Ariffin <hanif.ariffin.4326@gmail.com>

--- core/os/thread_safe.h
+++ core/os/thread_safe.h
@@ -34,7 +34,7 @@
 #include "core/os/mutex.h"
 
 #define _THREAD_SAFE_CLASS_ mutable Mutex _thread_safe_;
-#define _THREAD_SAFE_METHOD_ MutexLock<decltype( _thread_safe_)> _thread_safe_method_(_thread_safe_);
+#define _THREAD_SAFE_METHOD_ MutexLock<decltype(_thread_safe_)> _thread_safe_method_(_thread_safe_);
 #define _THREAD_SAFE_LOCK_ _thread_safe_.lock();
 #define _THREAD_SAFE_UNLOCK_ _thread_safe_.unlock();
 
