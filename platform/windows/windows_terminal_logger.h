a4b7de6b236d273843b97fdf47114d1823a30271
Yuri Roubinsky <chaosus89@gmail.com>
2021-11-26 12:18:15 +0300

    Fix console colors on Windows

--- platform/windows/windows_terminal_logger.h
+++ platform/windows/windows_terminal_logger.h
@@ -37,8 +37,8 @@
 
 class WindowsTerminalLogger : public StdLogger {
 public:
-	virtual void logv(const char *p_format, va_list p_list, bool p_err);
-	virtual void log_error(const char *p_function, const char *p_file, int p_line, const char *p_code, const char *p_rationale, ErrorType p_type = ERR_ERROR);
+	virtual void logv(const char *p_format, va_list p_list, bool p_err) override;
+	virtual void log_error(const char *p_function, const char *p_file, int p_line, const char *p_code, const char *p_rationale, bool p_editor_notify = false, ErrorType p_type = ERR_ERROR) override;
 	virtual ~WindowsTerminalLogger();
 };
 
