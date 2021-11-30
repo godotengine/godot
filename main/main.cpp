f9bafe4ba46053cc95de27b9a74cac34b7e64966
Rémi Verschelde <rverschelde@gmail.com>
2021-11-30 12:36:56 +0100

    Remove editor splash screen with sponsors logo
    The Godot Project Leadership Committe has decided to update the sponsoring
    tiers so that the Platinum Sponsorship no longer includes a logo on the
    editor splash screen.
    
    This lets us reclaim the editor splash screen space for community-related
    content instead of sponsors (e.g. a different community-designed splash
    screen for each stable branch?).
    
    Also removes two Platinum Sponsors whose sponsorship has expired earlier this
    year.
    
    (cherry picked from commit c283fce698ed2d2bcf101ecc8857be5d66afecc9)

--- main/main.cpp
+++ main/main.cpp
@@ -55,7 +55,6 @@
 #include "main/main_timer_sync.h"
 #include "main/performance.h"
 #include "main/splash.gen.h"
-#include "main/splash_editor.gen.h"
 #include "main/tests/test_main.h"
 #include "modules/register_module_types.h"
 #include "platform/register_platform_apis.h"
@@ -79,6 +78,9 @@
 #include "editor/editor_settings.h"
 #include "editor/progress_dialog.h"
 #include "editor/project_manager.h"
+#ifndef NO_EDITOR_SPLASH
+#include "main/splash_editor.gen.h"
+#endif
 #endif
 
 /* Static members */
