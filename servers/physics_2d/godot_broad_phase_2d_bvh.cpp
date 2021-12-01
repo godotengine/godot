6a4e4234423400904b681fa9992acd28a33b92fa
BimDav <67792882+BimDav@users.noreply.github.com>
2021-12-01 15:35:42 +0100

    separate trees in bvh for static bodies and non monitorable areas

--- servers/physics_2d/godot_broad_phase_2d_bvh.cpp
+++ servers/physics_2d/godot_broad_phase_2d_bvh.cpp
@@ -42,7 +42,7 @@ void GodotBroadPhase2DBVH::move(ID p_id, const Rect2 &p_aabb) {
 
 void GodotBroadPhase2DBVH::set_static(ID p_id, bool p_static) {
 	GodotCollisionObject2D *it = bvh.get(p_id - 1);
-	bvh.set_pairable(p_id - 1, !p_static, 1 << it->get_type(), p_static ? 0 : 0xFFFFF, false); // Pair everything, don't care?
+	bvh.set_pairable(p_id - 1, !p_static, 1 << it->get_type(), 0xFFFFF, false); // Pair everything, don't care?
 }
 
 void GodotBroadPhase2DBVH::remove(ID p_id) {
