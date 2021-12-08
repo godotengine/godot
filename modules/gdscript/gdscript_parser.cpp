b5b75cad430efe148b1555f126930b73cf33d2e8
Nathan Franke <natfra@pm.me>
2021-12-08 00:34:17 -0600

    Use OrderedHashMap for enum_values

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -3510,12 +3510,12 @@ bool GDScriptParser::export_annotations(const AnnotationNode *p_annotation, Node
 				variable->export_info.hint = PROPERTY_HINT_ENUM;
 
 				String enum_hint_string;
-				for (const Map<StringName, int>::Element *E = export_type.enum_values.front(); E; E = E->next()) {
-					enum_hint_string += E->key().operator String().capitalize().xml_escape();
+				for (OrderedHashMap<StringName, int>::Element E = export_type.enum_values.front(); E; E = E.next()) {
+					enum_hint_string += E.key().operator String().capitalize().xml_escape();
 					enum_hint_string += ":";
-					enum_hint_string += String::num_int64(E->get()).xml_escape();
+					enum_hint_string += String::num_int64(E.value()).xml_escape();
 
-					if (E->next()) {
+					if (E.next()) {
 						enum_hint_string += ",";
 					}
 				}
