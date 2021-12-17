9f250e837073b2914ec4920ab0012bdf170e7dfc
Marcel Admiraal <madmiraal@users.noreply.github.com>
2021-12-17 17:20:43 +0000

    Revert "[3.x] Fixed event spam when using the Nintendo Switch controller"

--- main/input_default.cpp
+++ main/input_default.cpp
@@ -811,10 +811,7 @@ void InputDefault::joy_axis(int p_device, int p_axis, const JoyAxis &p_value) {
 
 	Joypad &joy = joy_names[p_device];
 
-	// Make sure that we don't generate events for up to 5% jitter
-	// This is needed for Nintendo Switch Pro controllers, which jitter at rest
-	const float MIN_AXIS_CHANGE = 0.05f;
-	if (fabs(joy.last_axis[p_axis] - p_value.value) < MIN_AXIS_CHANGE) {
+	if (joy.last_axis[p_axis] == p_value.value) {
 		return;
 	}
 
