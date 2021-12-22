150eb3533b9bf21436bad6e8997c7c70b0eadef9
Bastiaan Olij <mux213@gmail.com>
2021-12-22 11:46:37 +1100

    Fix issue with external textures being freed by Godot
    (cherry picked from commit 5fbc24f3e0cb39829af7b114d1c83573babf30ab)

--- drivers/gles3/rasterizer_storage_gles3.cpp
+++ drivers/gles3/rasterizer_storage_gles3.cpp
@@ -6823,6 +6823,7 @@ void RasterizerStorageGLES3::_render_target_clear(RenderTarget *rt) {
 
 		// clean up our texture
 		Texture *t = texture_owner.get(rt->external.texture);
+		t->tex_id = 0;
 		t->alloc_height = 0;
 		t->alloc_width = 0;
 		t->width = 0;
@@ -7332,6 +7333,7 @@ void RasterizerStorageGLES3::render_target_set_external_texture(RID p_render_tar
 
 			// clean up our texture
 			Texture *t = texture_owner.get(rt->external.texture);
+			t->tex_id = 0;
 			t->alloc_height = 0;
 			t->alloc_width = 0;
 			t->width = 0;
