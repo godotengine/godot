7f133a8e879304bab43e2cb6082836431789ff3f
bruvzg <7645683+bruvzg@users.noreply.github.com>
2022-01-12 16:01:08 +0200

    Enable optional minimal SteamAPI integration for usage time tracking (editor only).

--- SConstruct
+++ SConstruct
@@ -207,6 +207,7 @@ opts.Add(BoolVariable("modules_enabled_by_default", "If no, disable all modules 
 opts.Add(BoolVariable("no_editor_splash", "Don't use the custom splash screen for the editor", True))
 opts.Add("system_certs_path", "Use this path as SSL certificates default for editor (for package maintainers)", "")
 opts.Add(BoolVariable("use_precise_math_checks", "Math checks use very precise epsilon (debug option)", False))
+opts.Add(BoolVariable("steamapi", "Enable minimal SteamAPI integration for usage time tracking (editor only)", False))
 
 # Thirdparty libraries
 opts.Add(BoolVariable("builtin_certs", "Use the built-in SSL certificates bundles", True))
--- main/main.cpp
+++ main/main.cpp
@@ -101,6 +101,10 @@
 #endif
 #endif
 
+#if defined(STEAMAPI_ENABLED)
+#include "main/steam_tracker.h"
+#endif
+
 #include "modules/modules_enabled.gen.h" // For mono.
 
 /* Static members */
@@ -121,6 +125,10 @@ static ZipArchive *zip_packed_data = nullptr;
 #endif
 static FileAccessNetworkClient *file_access_network_client = nullptr;
 static MessageQueue *message_queue = nullptr;
+
+#if defined(STEAMAPI_ENABLED)
+static SteamTracker *steam_tracker = nullptr;
+#endif
 
 // Initialized in setup2()
 static AudioServer *audio_server = nullptr;
@@ -1844,6 +1852,12 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 
 	engine->startup_benchmark_end_measure(); // core
 
+#if defined(STEAMAPI_ENABLED)
+	if (editor || project_manager) {
+		steam_tracker = memnew(SteamTracker);
+	}
+#endif
+
 	if (p_second_phase) {
 		return setup2();
 	}
@@ -1903,6 +1917,13 @@ error:
 	if (message_queue) {
 		memdelete(message_queue);
 	}
+
+#if defined(STEAMAPI_ENABLED)
+	if (steam_tracker) {
+		memdelete(steam_tracker);
+	}
+#endif
+
 	OS::get_singleton()->finalize_core();
 	locale = String();
 
@@ -3380,6 +3401,12 @@ void Main::cleanup(bool p_force) {
 	// Now should be safe to delete MessageQueue (famous last words).
 	message_queue->flush();
 	memdelete(message_queue);
+
+#if defined(STEAMAPI_ENABLED)
+	if (steam_tracker) {
+		memdelete(steam_tracker);
+	}
+#endif
 
 	unregister_core_driver_types();
 	unregister_core_extensions();
