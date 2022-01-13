a627cdafc53bd9d986471fcaceac7e51213a7a68
Rémi Verschelde <rverschelde@gmail.com>
2022-01-13 09:45:09 +0100

    Update copyright statements to 2022
    Happy new year to the wonderful Godot community!

--- editor/editor_about.cpp
+++ editor/editor_about.cpp
@@ -5,8 +5,8 @@
 /*                           GODOT ENGINE                                */
 /*                      https://godotengine.org                          */
 /*************************************************************************/
-/* Copyright (c) 2007-2021 Juan Linietsky, Ariel Manzur.                 */
-/* Copyright (c) 2014-2021 Godot Engine contributors (cf. AUTHORS.md).   */
+/* Copyright (c) 2007-2022 Juan Linietsky, Ariel Manzur.                 */
+/* Copyright (c) 2014-2022 Godot Engine contributors (cf. AUTHORS.md).   */
 /*                                                                       */
 /* Permission is hereby granted, free of charge, to any person obtaining */
 /* a copy of this software and associated documentation files (the       */
@@ -148,7 +148,7 @@ EditorAbout::EditorAbout() {
 
 	Label *about_text = memnew(Label);
 	about_text->set_v_size_flags(Control::SIZE_SHRINK_CENTER);
-	about_text->set_text(String::utf8("\xc2\xa9 2007-2021 Juan Linietsky, Ariel Manzur.\n\xc2\xa9 2014-2021 ") +
+	about_text->set_text(String::utf8("\xc2\xa9 2007-2022 Juan Linietsky, Ariel Manzur.\n\xc2\xa9 2014-2022 ") +
 			TTR("Godot Engine contributors") + "\n");
 	version_info_vbc->add_child(about_text);
 
--- version.py
+++ version.py
@@ -5,6 +5,6 @@ minor = 5
 patch = 0
 status = "beta"
 module_config = ""
-year = 2021
+year = 2022
 website = "https://godotengine.org"
 docs = "3.4"
