55fda17742f85309aba5a60a2a969a5565d944b9
Marcus Brummer <mbrlabs7@gmail.com>
2022-02-11 11:36:47 +0100

    Deselect nodes in the 3D editor when pressing ESC

--- editor/plugins/node_3d_editor_plugin.cpp
+++ editor/plugins/node_3d_editor_plugin.cpp
@@ -1888,7 +1888,12 @@ void Node3DEditorViewport::_sinput(const Ref<InputEvent> &p_event) {
 			}
 		}
 
-		if (_edit.mode != TRANSFORM_NONE) {
+		if (_edit.mode == TRANSFORM_NONE) {
+			if (k->get_keycode() == Key::ESCAPE && !cursor.region_select) {
+				_clear_selected();
+				return;
+			}
+		} else {
 			// We're actively transforming, handle keys specially
 			TransformPlane new_plane = TRANSFORM_VIEW;
 			String new_message;
