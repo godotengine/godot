b760bd1ada271a8974eb67f87cad1c1aa99f2725
Jean-Michel Bernard <jmb462@gmail.com>
2022-02-11 20:10:57 +0100

    Hide Polygon2D lines and handles when node is not visible in tree.
    (cherry picked from commit b0ebbd8ecebac52836fa9ecfc27635f5d12268f2)

--- editor/plugins/abstract_polygon_2d_editor.cpp
+++ editor/plugins/abstract_polygon_2d_editor.cpp
@@ -273,6 +273,10 @@ bool AbstractPolygon2DEditor::forward_gui_input(const Ref<InputEvent> &p_event) 
 		return false;
 	}
 
+	if (!_get_node()->is_visible_in_tree()) {
+		return false;
+	}
+
 	Ref<InputEventMouseButton> mb = p_event;
 
 	if (!_has_resource()) {
@@ -503,6 +507,10 @@ bool AbstractPolygon2DEditor::forward_gui_input(const Ref<InputEvent> &p_event) 
 
 void AbstractPolygon2DEditor::forward_canvas_draw_over_viewport(Control *p_overlay) {
 	if (!_get_node()) {
+		return;
+	}
+
+	if (!_get_node()->is_visible_in_tree()) {
 		return;
 	}
 
