1a002b5b5a7d1170090bd2b217735e3ff8dadb71
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-02-17 18:16:11 +0100

    Use versioned URL for "Online Documentation" buttons in the editor
    This also avoids a redirect, making the page appear to load faster.
    
    (cherry picked from commit 958912a33f18650b7d06fa5157d61e3a528281d3)

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -2863,7 +2863,7 @@ void EditorNode::_menu_option_confirm(int p_option, bool p_confirmed) {
 			emit_signal("request_help_search", "");
 		} break;
 		case HELP_DOCS: {
-			OS::get_singleton()->shell_open("https://docs.godotengine.org/");
+			OS::get_singleton()->shell_open(VERSION_DOCS_URL "/");
 		} break;
 		case HELP_QA: {
 			OS::get_singleton()->shell_open("https://godotengine.org/qa/");
