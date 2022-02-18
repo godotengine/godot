e1c303fa79d53ac5f0a0568d70bd582bb354f20f
Rémi Verschelde <rverschelde@gmail.com>
2022-02-18 14:18:24 +0100

    Revert "Fix extension registration order."
    This reverts commit 94ef200bab8feb5024b2c64ac14daa24e0c4cc55.
    
    This broke extension loading.
    Fixes #58273.

--- core/register_core_types.cpp
+++ core/register_core_types.cpp
@@ -316,11 +316,9 @@ void register_core_extensions() {
 	native_extension_manager->initialize_extensions(NativeExtension::INITIALIZATION_LEVEL_CORE);
 }
 
-void unregister_core_extensions() {
-	native_extension_manager->deinitialize_extensions(NativeExtension::INITIALIZATION_LEVEL_CORE);
-}
-
 void unregister_core_types() {
+	native_extension_manager->deinitialize_extensions(NativeExtension::INITIALIZATION_LEVEL_CORE);
+
 	memdelete(native_extension_manager);
 
 	memdelete(resource_uid);
