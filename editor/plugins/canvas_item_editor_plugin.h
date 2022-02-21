f8cd3bbc36bfe84fd16ab5f6315a9d91debc2737
kobewi <kobewi4e@gmail.com>
2022-02-21 01:22:11 +0100

    Add temporary pivot for rotating multiple 2D nodes

--- editor/plugins/canvas_item_editor_plugin.h
+++ editor/plugins/canvas_item_editor_plugin.h
@@ -174,6 +174,7 @@ private:
 		DRAG_SCALE_BOTH,
 		DRAG_ROTATE,
 		DRAG_PIVOT,
+		DRAG_TEMP_PIVOT,
 		DRAG_V_GUIDE,
 		DRAG_H_GUIDE,
 		DRAG_DOUBLE_GUIDE,
@@ -251,6 +252,7 @@ private:
 	bool key_scale = false;
 
 	bool pan_pressed = false;
+	Vector2 temp_pivot = Vector2(INFINITY, INFINITY);
 
 	bool ruler_tool_active = false;
 	Point2 ruler_tool_origin;
