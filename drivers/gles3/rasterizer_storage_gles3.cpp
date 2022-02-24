b1a295b739eb056557e9f2fe47ac80be6cc6d79d
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-02-24 22:55:14 +0100

    Implement distance fade properties in OmniLight3D and SpotLight3D
    This can be used to fade lights and their shadows in the distance,
    similar to Decal nodes. This can bring significant performance
    improvements, especially for lights with shadows enabled and when
    using higher-than-default shadow quality settings.
    
    While lights can be smoothly faded out over distance, shadows are
    currently "all or nothing" since per-light shadow color is no longer
    customizable in the Vulkan renderer. This may result in noticeable
    pop-in when leaving the shadow cutoff distance, but depending on the
    scene, it may not always be that noticeable.

--- drivers/gles3/rasterizer_storage_gles3.cpp
+++ drivers/gles3/rasterizer_storage_gles3.cpp
@@ -2267,6 +2267,9 @@ void RasterizerStorageGLES3::light_set_negative(RID p_light, bool p_enable) {
 void RasterizerStorageGLES3::light_set_cull_mask(RID p_light, uint32_t p_mask) {
 }
 
+void RasterizerStorageGLES3::light_set_distance_fade(RID p_light, bool p_enabled, float p_begin, float p_shadow, float p_length) {
+}
+
 void RasterizerStorageGLES3::light_set_reverse_cull_face_mode(RID p_light, bool p_enabled) {
 }
 
--- servers/rendering/renderer_storage.h
+++ servers/rendering/renderer_storage.h
@@ -321,6 +321,7 @@ public:
 	virtual void light_set_projector(RID p_light, RID p_texture) = 0;
 	virtual void light_set_negative(RID p_light, bool p_enable) = 0;
 	virtual void light_set_cull_mask(RID p_light, uint32_t p_mask) = 0;
+	virtual void light_set_distance_fade(RID p_light, bool p_enabled, float p_begin, float p_shadow, float p_length) = 0;
 	virtual void light_set_reverse_cull_face_mode(RID p_light, bool p_enabled) = 0;
 	virtual void light_set_bake_mode(RID p_light, RS::LightBakeMode p_bake_mode) = 0;
 	virtual void light_set_max_sdfgi_cascade(RID p_light, uint32_t p_cascade) = 0;
