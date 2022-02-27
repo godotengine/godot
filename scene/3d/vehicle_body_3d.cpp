b8d9694b58310dc4e0b254ff7613c6cd2a656355
Daniel Tritscher <daniel.j.tritscher@gmail.com>
2022-02-28 11:48:19 +1300

    explicit condition == true for formatting.

--- scene/3d/vehicle_body_3d.cpp
+++ scene/3d/vehicle_body_3d.cpp
@@ -441,7 +441,7 @@ real_t VehicleBody3D::_ray_cast(int p_idx, PhysicsDirectBodyState3D *s) {
 			wheel.m_raycastInfo.m_groundObject = nullptr;
 			bool col = ss->intersect_ray(ray_params, rr);
 
-			if (col) 
+			if (col == true) 
 			{
 				param = source.distance_to(rr.position) / source.distance_to(target);
 				real_t hitDistance = raylen * param;
