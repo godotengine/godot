godotengine/godot

commit 1963c63b918cdab81f10d3ab48bbb225c95b9246

Load assets before enabling editor plugins
Moves the code for enabling plugins from NOTIFICATION_READY to after the first scan has been completed.

editor/editor_node.cpp
@@ -595,19 +595,6 @@ void EditorNode::_notification(int p_what) {
 		} break;
 
 		case NOTIFICATION_READY: {
-			{
-				_initializing_addons = true;
-				Vector<String> addons;
-				if (ProjectSettings::get_singleton()->has_setting("editor_plugins/enabled")) {
-					addons = ProjectSettings::get_singleton()->get("editor_plugins/enabled");
-				}
-
-				for (int i = 0; i < addons.size(); i++) {
-					set_addon_plugin_enabled(addons[i], true);
-				}
-				_initializing_addons = false;
-			}
-
 			RenderingServer::get_singleton()->viewport_set_disable_2d(get_scene_root()->get_viewport_rid(), true);
 			RenderingServer::get_singleton()->viewport_set_disable_environment(get_viewport()->get_viewport_rid(), true);
 
@@ -970,6 +957,18 @@ void EditorNode::_sources_changed(bool p_exist) {
 			load_scene(defer_load_scene);
 			defer_load_scene = "";
 		}
+
+		// Only enable addons once resources have been imported
+		_initializing_addons = true;
+		Vector<String> addons;
+		if (ProjectSettings::get_singleton()->has_setting("editor_plugins/enabled")) {
+			addons = ProjectSettings::get_singleton()->get("editor_plugins/enabled");
+		}
+
+		for (int i = 0; i < addons.size(); i++) {
+			set_addon_plugin_enabled(addons[i], true);
+		}
+		_initializing_addons = false;
 	}
 }
 

commit 14ecad4cdebf22c2121f200b6737af093ae8e9ac

Import assets before loading editor plugins
Take 2: This time, the addons folders are scanned first by the EditorFileSystem, then addons (including import plugins) are loaded, then the EditorFileSystem is updated and scans the rest of the project. This approach should prevent the regressions present in 1963c63.

doc/classes/EditorFileSystem.xml
@@ -69,6 +69,11 @@
 		</method>
 	</methods>
 	<signals>
+		<signal name="addons_scanned">
+			<description>
+				Emitted on the first scan of the filesystem, after the addons folder has been scanned, but before [signal filesystem_changed] is emitted.
+			</description>
+		</signal>
 		<signal name="filesystem_changed">
 			<description>
 				Emitted if the filesystem changed.

editor/editor_file_system.cpp
@@ -305,6 +305,35 @@ void EditorFileSystem::_scan_filesystem() {
 		d->remove(update_cache); //bye bye update cache
 	}
 
+	DirAccess *d = DirAccess::create(DirAccess::ACCESS_RESOURCES);
+
+	Vector<String> addons;
+	addon_root_directories.clear();
+	if (ProjectSettings::get_singleton()->has_setting("editor_plugins/enabled")) {
+		addons = ProjectSettings::get_singleton()->get("editor_plugins/enabled");
+	}
+
+	EditorProgressBG addons_scan_progress("efs_addons", "ScanFSAddons", 1000);
+
+	ScanProgress addons_sp;
+	addons_sp.low = 0;
+	addons_sp.hi = 1;
+	addons_sp.progress = &addons_scan_progress;
+
+	for (int i = 0; i < addons.size(); i++) {
+		String addon_root_directory_path = addons[i].get_base_dir();
+		EditorFileSystemDirectory *addon_root_directory = memnew(EditorFileSystemDirectory);
+		d->change_dir(addon_root_directory_path);
+		PackedStringArray path_parts = addon_root_directory_path.split("/");
+		path_parts.reverse();
+		addon_root_directory->name = path_parts[0];
+		_scan_new_dir(addon_root_directory, d, addons_sp.get_sub(i, addons.size()));
+		addon_root_directories.set(addon_root_directory_path, addon_root_directory);
+	}
+
+	emit_signal("addons_scanned");
+	_update_extensions();
+
 	EditorProgressBG scan_progress("efs", "ScanFS", 1000);
 
 	ScanProgress sp;
@@ -315,7 +344,6 @@ void EditorFileSystem::_scan_filesystem() {
 	new_filesystem = memnew(EditorFileSystemDirectory);
 	new_filesystem->parent = nullptr;
 
-	DirAccess *d = DirAccess::create(DirAccess::ACCESS_RESOURCES);
 	d->change_dir("res://");
 	_scan_new_dir(new_filesystem, d, sp);
 
@@ -707,6 +735,9 @@ void EditorFileSystem::_scan_new_dir(EditorFileSystemDirectory *p_dir, DirAccess
 
 	p_dir->modified_time = FileAccess::get_modified_time(cd);
 
+	List<String> addon_root_directory_paths;
+	addon_root_directories.get_key_list(&addon_root_directory_paths);
+
 	da->list_dir_begin();
 	while (true) {
 		String f = da->get_next();
@@ -751,10 +782,16 @@ void EditorFileSystem::_scan_new_dir(EditorFileSystemDirectory *p_dir, DirAccess
 			} else {
 				EditorFileSystemDirectory *efd = memnew(EditorFileSystemDirectory);
 
-				efd->parent = p_dir;
-				efd->name = E->get();
-
-				_scan_new_dir(efd, da, p_progress.get_sub(idx, total));
+				// Directories containing addon plugins are scanned separately,
+				// but they still need to be visible in the file system dock.
+				if (addon_root_directory_paths.find(cd.plus_file(E->get()))) {
+					efd = addon_root_directories.get(cd.plus_file(E->get()));
+					efd->parent = p_dir;
+				} else {
+					efd->parent = p_dir;
+					efd->name = E->get();
+					_scan_new_dir(efd, da, p_progress.get_sub(idx, total));
+				}
 
 				int idx2 = 0;
 				for (int i = 0; i < p_dir->subdirs.size(); i++) {
@@ -2342,6 +2379,7 @@ void EditorFileSystem::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("get_file_type", "path"), &EditorFileSystem::get_file_type);
 	ClassDB::bind_method(D_METHOD("update_script_classes"), &EditorFileSystem::update_script_classes);
 
+	ADD_SIGNAL(MethodInfo("addons_scanned"));
 	ADD_SIGNAL(MethodInfo("filesystem_changed"));
 	ADD_SIGNAL(MethodInfo("sources_changed", PropertyInfo(Variant::BOOL, "exist")));
 	ADD_SIGNAL(MethodInfo("resources_reimported", PropertyInfo(Variant::PACKED_STRING_ARRAY, "resources")));

editor/editor_file_system.h
@@ -256,6 +256,8 @@ class EditorFileSystem : public Node {
 
 	static ResourceUID::ID _resource_saver_get_resource_id_for_path(const String &p_path, bool p_generate);
 
+	HashMap<String, EditorFileSystemDirectory *> addon_root_directories;
+
 	bool _scan_extensions();
 
 protected:

editor/editor_node.cpp
@@ -625,19 +625,6 @@ void EditorNode::_notification(int p_what) {
 		} break;
 
 		case NOTIFICATION_READY: {
-			{
-				_initializing_addons = true;
-				Vector<String> addons;
-				if (ProjectSettings::get_singleton()->has_setting("editor_plugins/enabled")) {
-					addons = ProjectSettings::get_singleton()->get("editor_plugins/enabled");
-				}
-
-				for (int i = 0; i < addons.size(); i++) {
-					set_addon_plugin_enabled(addons[i], true);
-				}
-				_initializing_addons = false;
-			}
-
 			RenderingServer::get_singleton()->viewport_set_disable_2d(get_scene_root()->get_viewport_rid(), true);
 			RenderingServer::get_singleton()->viewport_set_disable_environment(get_viewport()->get_viewport_rid(), true);
 
@@ -4120,6 +4107,19 @@ void EditorNode::progress_end_task_bg(const String &p_task) {
 	singleton->progress_hb->end_task(p_task);
 }
 
+void EditorNode::_initialize_addons() {
+	_initializing_addons = true;
+	Vector<String> addons;
+	if (ProjectSettings::get_singleton()->has_setting("editor_plugins/enabled")) {
+		addons = ProjectSettings::get_singleton()->get("editor_plugins/enabled");
+	}
+
+	for (int i = 0; i < addons.size(); i++) {
+		set_addon_plugin_enabled(addons[i], true);
+	}
+	_initializing_addons = false;
+}
+
 Ref<Texture2D> EditorNode::_file_dialog_get_icon(const String &p_path) {
 	EditorFileSystemDirectory *efsd = EditorFileSystem::get_singleton()->get_filesystem_path(p_path.get_base_dir());
 	if (efsd) {
@@ -7134,6 +7134,7 @@ EditorNode::EditorNode() {
 	EditorFileSystem::get_singleton()->connect("filesystem_changed", callable_mp(this, &EditorNode::_fs_changed));
 	EditorFileSystem::get_singleton()->connect("resources_reimported", callable_mp(this, &EditorNode::_resources_reimported));
 	EditorFileSystem::get_singleton()->connect("resources_reload", callable_mp(this, &EditorNode::_resources_changed));
+	EditorFileSystem::get_singleton()->connect("addons_scanned", callable_mp(this, &EditorNode::_initialize_addons));
 
 	_build_icon_type_cache();
 

editor/editor_node.h
@@ -551,6 +551,7 @@ private:
 
 	bool _initializing_addons;
 	Map<String, EditorPlugin *> plugin_addons;
+	void _initialize_addons();
 
 	static Ref<Texture2D> _file_dialog_get_icon(const String &p_path);
 	static void _file_dialog_register(FileDialog *p_dialog);