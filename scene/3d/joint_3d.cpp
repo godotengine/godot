9e100c3e7a38fa0a8338809729e4677e5c8e8edb
Ricardo Buring <ricardo.buring@gmail.com>
2022-02-28 22:21:32 +0100

    Joint2D and Joint3D: update joint on NOTIFICATION_POST_ENTER_TREE
    This allows reparenting, etc.

--- scene/3d/joint_3d.cpp
+++ scene/3d/joint_3d.cpp
@@ -166,15 +166,18 @@ int Joint3D::get_solver_priority() const {
 
 void Joint3D::_notification(int p_what) {
 	switch (p_what) {
-		case NOTIFICATION_READY: {
+		case NOTIFICATION_POST_ENTER_TREE: {
+			if (is_configured()) {
+				_disconnect_signals();
+			}
 			_update_joint();
 		} break;
 
 		case NOTIFICATION_EXIT_TREE: {
 			if (is_configured()) {
 				_disconnect_signals();
-				_update_joint(true);
 			}
+			_update_joint(true);
 		} break;
 	}
 }
