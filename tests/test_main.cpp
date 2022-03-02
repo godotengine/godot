92b392e3dba0c2cd8bb2d989cce3d9cbb82c0740
Timon Bestebreur <timonbestebreur@gmail.com>
2022-03-02 13:55:26 +0100

    Add unit tests for Shortcut
    Next to that, add entry for the test file in test_main.cpp.
    
    These test cases test the basic functionality of the shortcut module.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -30,6 +30,7 @@
 
 #include "test_main.h"
 
+#include "tests/core/input/test_shortcut.h"
 #include "tests/core/io/test_config_file.h"
 #include "tests/core/io/test_file_access.h"
 #include "tests/core/io/test_image.h"
