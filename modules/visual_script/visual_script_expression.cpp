743f2d232a699332bb94087fb7fee7d8599fbf6d
Haoyu Qiu <timothyqiu32@gmail.com>
2022-03-11 23:36:16 +0800

    Use `RTR()` for VisualScriptNode captions and texts
    (cherry picked from commit 77f80aa4ee1a93beeb0eacaf35646fc97ddf2abb)

--- modules/visual_script/visual_script_expression.cpp
+++ modules/visual_script/visual_script_expression.cpp
@@ -172,7 +172,7 @@ PropertyInfo VisualScriptExpression::get_output_value_port_info(int p_idx) const
 }
 
 String VisualScriptExpression::get_caption() const {
-	return TTR("Expression");
+	return RTR("Expression");
 }
 String VisualScriptExpression::get_text() const {
 	return expression;
