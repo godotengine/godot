997f18f686fb106a1f7a11bc20f490d6b7ac4f5b
Timon Bestebreur <timonbestebreur@gmail.com>
2022-03-16 09:44:47 +0100

    Add Unit Tests for InputEventKey

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -30,6 +30,7 @@
 
 #include "test_main.h"
 
+#include "tests/core/input/test_input_event_key.h"
 #include "tests/core/input/test_shortcut.h"
 #include "tests/core/io/test_config_file.h"
 #include "tests/core/io/test_file_access.h"
