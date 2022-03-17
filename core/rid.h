c2f901bab7616847097bfa2887e9cd0d6f2c3b16
Rémi Verschelde <rverschelde@gmail.com>
2022-03-17 11:03:03 +0100

    RID: Change comparison operators to use RID_Data id instead of address
    This should helps making sorting more deterministic in physics and rendering.
    
    The same change was done for 4.0 in 4f163972bbd9c7379b01a1f9aa5310646ca7865e.

--- core/rid.h
+++ core/rid.h
@@ -68,13 +68,13 @@ public:
 		return _data == p_rid._data;
 	}
 	_FORCE_INLINE_ bool operator<(const RID &p_rid) const {
-		return _data < p_rid._data;
+		return get_id() < p_rid.get_id();
 	}
 	_FORCE_INLINE_ bool operator<=(const RID &p_rid) const {
-		return _data <= p_rid._data;
+		return get_id() <= p_rid.get_id();
 	}
 	_FORCE_INLINE_ bool operator>(const RID &p_rid) const {
-		return _data > p_rid._data;
+		return get_id() > p_rid.get_id();
 	}
 	_FORCE_INLINE_ bool operator!=(const RID &p_rid) const {
 		return _data != p_rid._data;
