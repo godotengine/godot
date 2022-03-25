fc16188d1077d4b5d950ae73f5babf91db65473f
Riteo <riteo@posteo.net>
2022-03-25 02:21:43 +0100

    Make last_bytes_read signed
    Oops, forgot this. Without it we we can't catch errors and the CI gets
    very angry.

--- platform/linuxbsd/display_server_wayland.cpp
+++ platform/linuxbsd/display_server_wayland.cpp
@@ -1009,7 +1009,7 @@ String DisplayServerWayland::clipboard_get() const {
 		uint32_t bytes_read = 0;
 
 		while (true) {
-			uint32_t last_bytes_read = read(fds[0], data.ptr() + bytes_read, chunk_size);
+			int32_t last_bytes_read = read(fds[0], data.ptr() + bytes_read, chunk_size);
 			if (last_bytes_read < 0) {
 				ERR_PRINT(vformat("Clipboard: read error %d.", errno));
 			}
