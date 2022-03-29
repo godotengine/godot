17ce0fcc7c26bbc79bdf936c858d1f7f139bfc15
Haoyu Qiu <timothyqiu32@gmail.com>
2022-03-29 15:00:31 +0800

    Simplify FBX importer project settings registering

--- modules/fbx/editor_scene_importer_fbx.cpp
+++ modules/fbx/editor_scene_importer_fbx.cpp
@@ -58,24 +58,8 @@
 
 void EditorSceneImporterFBX::get_extensions(List<String> *r_extensions) const {
 	// register FBX as the one and only format for FBX importing
-	const String import_setting_string = "filesystem/import/fbx/";
-	const String fbx_str = "fbx";
-	Vector<String> exts;
-	exts.push_back(fbx_str);
-	_register_project_setting_import(fbx_str, import_setting_string, exts, r_extensions, true);
-}
-
-void EditorSceneImporterFBX::_register_project_setting_import(const String generic,
-		const String import_setting_string,
-		const Vector<String> &exts,
-		List<String> *r_extensions,
-		const bool p_enabled) const {
-	const String use_generic = "use_" + generic;
-	_GLOBAL_DEF(import_setting_string + use_generic, p_enabled, true);
-	if (ProjectSettings::get_singleton()->get(import_setting_string + use_generic)) {
-		for (int32_t i = 0; i < exts.size(); i++) {
-			r_extensions->push_back(exts[i]);
-		}
+	if (GLOBAL_GET("filesystem/import/fbx/use_fbx")) {
+		r_extensions->push_back("fbx");
 	}
 }
 
@@ -1461,4 +1445,8 @@ void EditorSceneImporterFBX::BuildDocumentNodes(
 			BuildDocumentNodes(new_node->pivot_transform, state, p_doc, current_node_id, new_node);
 		}
 	}
+}
+
+EditorSceneImporterFBX::EditorSceneImporterFBX() {
+	_GLOBAL_DEF("filesystem/import/fbx/use_fbx", true, true);
 }
