2bb930475d6560e7c09700c1885b46a061bc251c
Paweł Fertyk <pfertyk@pfertyk.me>
2022-03-31 22:28:40 +0200

    Fix get_visible_line_count in RichTextLabel with newlines
    Fixed #59359.

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -714,7 +714,6 @@ int RichTextLabel::_process_line(ItemFrame *p_frame, const Vector2 &p_ofs, int &
 
 				if (p_mode != PROCESS_CACHE) {
 					lh = line < l.height_caches.size() ? l.height_caches[line] : 1;
-					line_is_blank = true;
 				}
 
 			} break;
