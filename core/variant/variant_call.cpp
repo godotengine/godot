a0915e6dee4e54563a98ca6adb8968dbdcea1af1
kobewi <kobewi4e@gmail.com>
2022-04-04 18:46:55 +0200

    Add Dictionary.merge()

--- core/variant/variant_call.cpp
+++ core/variant/variant_call.cpp
@@ -1808,6 +1808,7 @@ static void _register_variant_builtin_methods() {
 	bind_method(Dictionary, size, sarray(), varray());
 	bind_method(Dictionary, is_empty, sarray(), varray());
 	bind_method(Dictionary, clear, sarray(), varray());
+	bind_method(Dictionary, merge, sarray("dictionary", "overwrite"), varray(false));
 	bind_method(Dictionary, has, sarray("key"), varray());
 	bind_method(Dictionary, has_all, sarray("keys"), varray());
 	bind_method(Dictionary, erase, sarray("key"), varray());
