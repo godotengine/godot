b2f1f6faedb0700950b27365e42556b33e48ff14
Ethan <epruhl102@gmail.com>
2022-04-10 01:00:32 -0400

    Added outline to polygon 2d indices per #38009

--- editor/plugins/abstract_polygon_2d_editor.cpp
+++ editor/plugins/abstract_polygon_2d_editor.cpp
@@ -562,11 +562,13 @@ void AbstractPolygon2DEditor::forward_canvas_draw_over_viewport(Control *p_overl
 			p_overlay->draw_texture(handle, point - handle->get_size() * 0.5, modulate);
 
 			if (vertex == hover_point) {
-				Ref<Font> font = get_theme_font(SNAME("font"), SNAME("Label"));
-				int font_size = get_theme_font_size(SNAME("font_size"), SNAME("Label"));
+				Ref<Font> font = get_theme_font(SNAME("bold"), SNAME("EditorFonts"));
+				int font_size = get_theme_font_size(SNAME("bold_size"), SNAME("EditorFonts"));
 				String num = String::num(vertex.vertex);
 				Size2 num_size = font->get_string_size(num, font_size);
-				p_overlay->draw_string(font, point - num_size * 0.5, num, HORIZONTAL_ALIGNMENT_LEFT, -1, font_size, Color(1.0, 1.0, 1.0, 0.5));
+				Color outline_color = Color(1.0, 1.0, 1.0, 1.0).inverted();
+
+				p_overlay->draw_string(font, point - num_size * 0.5, num, HORIZONTAL_ALIGNMENT_LEFT, -1, font_size, Color(1.0, 1.0, 1.0, 0.5), Math::round(2 * EDSCALE), outline_color);
 			}
 		}
 	}
