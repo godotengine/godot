2d9ad7ba6635c044abf0daa5d510fec8e0b690e3
Gilles Roudière <gilles.roudiere@gmail.com>
2022-04-13 15:00:50 +0200

    Fix item_collapsed signal being triggered without user input

--- editor/scene_tree_editor.cpp
+++ editor/scene_tree_editor.cpp
@@ -192,6 +192,7 @@ bool SceneTreeEditor::_add_nodes(Node *p_node, TreeItem *p_parent, bool p_scroll
 		bool collapsed = p_node->is_displayed_folded();
 		if (collapsed) {
 			item->set_collapsed(true);
+			_cell_collapsed(item);
 		}
 	}
 
@@ -762,6 +763,7 @@ void SceneTreeEditor::set_selected(Node *p_node, bool p_emit_selected) {
 			TreeItem *node = item->get_parent();
 			while (node && node != tree->get_root()) {
 				node->set_collapsed(false);
+				_cell_collapsed(node);
 				node = node->get_parent();
 			}
 			item->select(0);
