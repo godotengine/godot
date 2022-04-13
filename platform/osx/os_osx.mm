52973a45055a9b676622c5f6951a06417273256c
bruvzg <7645683+bruvzg@users.noreply.github.com>
2022-04-13 10:34:09 +0300

    [macOS, 3.x] Fix OpenGL color space on HDR displays.

--- platform/osx/os_osx.mm
+++ platform/osx/os_osx.mm
@@ -1612,6 +1612,8 @@ Error OS_OSX::initialize(const VideoMode &p_desired, int p_video_driver, int p_a
 		[window_view setWantsBestResolutionOpenGLSurface:NO];
 	}
 
+	[window_object setColorSpace:[NSColorSpace sRGBColorSpace]];
+
 	//[window_object setTitle:[NSString stringWithUTF8String:"GodotEnginies"]];
 	[window_object setContentView:window_view];
 	[window_object setDelegate:window_delegate];
