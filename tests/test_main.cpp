c1d445f1e514e69e457bd7607aa0fad266469f80
Paulb23 <p_batty@hotmail.co.uk>
2022-04-22 17:53:58 +0100

    Add inital TextEdit UnitTests

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -76,6 +76,7 @@
 #include "tests/scene/test_curve.h"
 #include "tests/scene/test_gradient.h"
 #include "tests/scene/test_path_3d.h"
+#include "tests/scene/test_text_edit.h"
 #include "tests/servers/test_text_server.h"
 #include "tests/test_validate_testing.h"
 
