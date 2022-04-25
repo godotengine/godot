925e54ad65d30eb1eecdde31502dd554bf00bce7
Nathan Franke <me@nathan.sh>
2022-04-25 14:56:47 -0500

    fix lines ending in comments causing auto indent

--- scene/gui/code_edit.cpp
+++ scene/gui/code_edit.cpp
@@ -994,7 +994,8 @@ void CodeEdit::_new_line(bool p_split_current_line, bool p_above) {
 			}
 
 			/* Make sure this is the last char, trailing whitespace or comments are okay. */
-			if (should_indent && (!is_whitespace(c) && is_in_comment(cl, cc) == -1)) {
+			/* Increment column for comments because the delimiter (#) should be ignored. */
+			if (should_indent && (!is_whitespace(c) && is_in_comment(cl, line_col + 1) == -1)) {
 				should_indent = false;
 			}
 		}
