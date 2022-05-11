d36c5514d3893578f1c8cce4d0a2256abfcdd1ed
bruvzg <7645683+bruvzg@users.noreply.github.com>
2022-05-11 15:15:58 +0300

    Fix ZipIO crash when reused (and possible leaks).

--- platform/javascript/api/javascript_tools_editor_plugin.cpp
+++ platform/javascript/api/javascript_tools_editor_plugin.cpp
@@ -64,7 +64,8 @@ void JavaScriptToolsEditorPlugin::_download_zip(Variant p_v) {
 	}
 	String resource_path = ProjectSettings::get_singleton()->get_resource_path();
 
-	zlib_filefunc_def io = zipio_create_io();
+	Ref<FileAccess> io_fa;
+	zlib_filefunc_def io = zipio_create_io(&io_fa);
 
 	// Name the downloaded ZIP file to contain the project name and download date for easier organization.
 	// Replace characters not allowed (or risky) in Windows file names with safe characters.
