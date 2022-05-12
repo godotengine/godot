a369bc7325073913df826817e6fb20b1c6188572
FireForge <67974470+fire-forge@users.noreply.github.com>
2022-05-12 16:20:12 -0500

    Use the "Move" mouse icon in inspector arrays

--- editor/editor_inspector.cpp
+++ editor/editor_inspector.cpp
@@ -1939,6 +1939,7 @@ void EditorInspectorArray::_setup() {
 		// Move button.
 		ae.move_texture_rect = memnew(TextureRect);
 		ae.move_texture_rect->set_stretch_mode(TextureRect::STRETCH_KEEP_CENTERED);
+		ae.move_texture_rect->set_default_cursor_shape(Control::CURSOR_MOVE);
 		if (is_inside_tree()) {
 			ae.move_texture_rect->set_texture(get_theme_icon(SNAME("TripleBar"), SNAME("EditorIcons")));
 		}
