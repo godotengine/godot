e6c443a353cb3923b80f4fa112667a08159ee971
reduz <reduzio@gmail.com>
2022-05-16 15:44:43 +0200

    Implement read-only dictionaries.
    * Add ability to set them read only.
    * If read-only, it can't be modified.
    
    This is added in order to optionally make const dictionaries (and eventually arrays) properly read-only in GDScript.

--- core/variant/dictionary.h
+++ core/variant/dictionary.h
@@ -84,6 +84,9 @@ public:
 	Dictionary duplicate(bool p_deep = false) const;
 	Dictionary recursive_duplicate(bool p_deep, int recursion_count) const;
 
+	void set_read_only(bool p_enable);
+	bool is_read_only() const;
+
 	const void *id() const;
 
 	Dictionary(const Dictionary &p_from);
