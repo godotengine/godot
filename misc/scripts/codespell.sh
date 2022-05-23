88dba8e625a12d17759a988249afa5246632a9ee
Rémi Verschelde <rverschelde@gmail.com>
2022-05-23 21:41:46 +0200

    Fix typos with codespell
    Using codespell 2.2-dev from current git.

--- misc/scripts/codespell.sh
+++ misc/scripts/codespell.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-SKIP_LIST="./thirdparty,*.gen.*,*.po,*.pot,package-lock.json,./core/string/locales.h,./DONORS.md,./misc/scripts/codespell.sh"
-IGNORE_LIST="ba,childs,curvelinear,doubleclick,expct,fave,findn,gird,inout,leapyear,lod,nd,numer,ois,readded,ro,statics,te,varn"
+SKIP_LIST="./thirdparty,*.gen.*,*.po,*.pot,package-lock.json,./core/string/locales.h,./DONORS.md,./misc/dist/linux/org.godotengine.Godot.desktop,./misc/scripts/codespell.sh"
+IGNORE_LIST="ba,childs,commiting,complies,curvelinear,doubleclick,expct,fave,findn,gird,inout,leapyear,lod,nd,numer,ois,readded,ro,statics,switchs,te,varius,varn"
 
 codespell -w -q 3 -S "${SKIP_LIST}" -L "${IGNORE_LIST}"
