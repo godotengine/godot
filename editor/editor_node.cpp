42b48496dedf20d7dd43756efac4408e0a9c639f
Rémi Verschelde <rverschelde@gmail.com>
2022-05-25 14:19:01 +0200

    HTML5: Always disable Asset Library
    GitHub doesn't allow CORS so we can't download assets from it.
    
    There'd also be more work needed for the Asset Library plugin to be
    usable in the Web editor even if that was supported.

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -7025,11 +7025,15 @@ EditorNode::EditorNode() {
 	ScriptTextEditor::register_editor(); // Register one for text scripts.
 	TextEditor::register_editor();
 
+	// Asset Library can't work on Web editor for now as most assets are sourced
+	// directly from GitHub which does not set CORS.
+#ifndef JAVASCRIPT_ENABLED
 	if (StreamPeerSSL::is_available()) {
 		add_editor_plugin(memnew(AssetLibraryEditorPlugin));
 	} else {
 		WARN_PRINT("Asset Library not available, as it requires SSL to work.");
 	}
+#endif
 
 	// Add interface before adding plugins.
 
