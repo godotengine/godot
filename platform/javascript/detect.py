3ff6d794c0aac0365f236cd078b5e5aeea0d996e
Rémi Verschelde <rverschelde@gmail.com>
2022-05-25 14:19:49 +0200

    HTML5: Enable mbedTLS module for Crypto object
    Increases the size of the wasm by around 3% (~300-350 KiB).
    
    This enables using the Crypto object for hashing, signing and encryption,
    and therefore reduces the gap between the features of the HTML5 platform
    and other platforms.
    
    Closes https://github.com/godotengine/godot-proposals/issues/3574.

--- platform/javascript/detect.py
+++ platform/javascript/detect.py
@@ -48,11 +48,6 @@ def get_flags():
     return [
         ("tools", False),
         ("builtin_pcre2_with_jit", False),
-        # Disabling the mbedtls module reduces file size.
-        # The module has little use due to the limited networking functionality
-        # in this platform. For the available networking methods, the browser
-        # manages TLS.
-        ("module_mbedtls_enabled", False),
         ("vulkan", False),
     ]
 
