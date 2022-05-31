6d53c60bf75f0070e8d5bd42e9470abb986e008c
Yuri Sizov <yuris@humnom.net>
2022-05-31 21:23:16 +0300

    Mark node groups as dirty for every children if parent is moved

--- scene/main/node.h
+++ scene/main/node.h
@@ -199,6 +199,7 @@ private:
 	void _propagate_physics_interpolated(bool p_interpolated);
 	void _print_stray_nodes();
 	void _propagate_pause_owner(Node *p_owner);
+	void _propagate_groups_dirty();
 	Array _get_node_and_resource(const NodePath &p_path);
 
 	void _duplicate_signals(const Node *p_original, Node *p_copy) const;
