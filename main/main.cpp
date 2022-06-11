b853b8d065b6f8bafbd5765e2d5b89aee69cfb5e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-06-12 00:31:40 +0200

    Add a property hint for the iOS touch delay project setting
    (cherry picked from commit 21da2fbbb08664c7cedac450d7e7331a6f270ade)

--- main/main.cpp
+++ main/main.cpp
@@ -1252,7 +1252,11 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 	}
 
 	GLOBAL_DEF("display/window/ios/hide_home_indicator", true);
-	GLOBAL_DEF("input_devices/pointing/ios/touch_delay", 0.150);
+	GLOBAL_DEF("input_devices/pointing/ios/touch_delay", 0.15);
+	ProjectSettings::get_singleton()->set_custom_property_info("input_devices/pointing/ios/touch_delay",
+			PropertyInfo(Variant::REAL,
+					"input_devices/pointing/ios/touch_delay",
+					PROPERTY_HINT_RANGE, "0,1,0.001"));
 
 	Engine::get_singleton()->set_frame_delay(frame_delay);
 
