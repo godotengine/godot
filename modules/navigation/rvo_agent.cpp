bc38ebe4f07724850095094873d1b42bf914c6f3
smix8 <52464204+smix8@users.noreply.github.com>
2022-06-14 13:04:02 +0200

    Fix crash of navigation agents callback when object is invalid
    Fix crash of navigation agents callback when object is invalid.

--- modules/navigation/rvo_agent.cpp
+++ modules/navigation/rvo_agent.cpp
@@ -65,8 +65,9 @@ void RvoAgent::dispatch_callback() {
 		return;
 	}
 	Object *obj = ObjectDB::get_instance(callback.id);
-	if (obj == nullptr) {
+	if (!obj) {
 		callback.id = ObjectID();
+		return;
 	}
 
 	Callable::CallError responseCallError;
