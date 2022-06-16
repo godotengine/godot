525b5e0e16372923a571f4c024f53398798c2e95
Raul Santos <raulsntos@gmail.com>
2022-06-16 17:21:47 +0200

    Fix EditorScenePostImport templates for C#

--- modules/mono/editor/script_templates/EditorScenePostImport/basic_import_script.cs
+++ modules/mono/editor/script_templates/EditorScenePostImport/basic_import_script.cs
@@ -7,12 +7,10 @@ using System;
 [Tool]
 public partial class _CLASS_ : _BASE_
 {
-    public override Object _PostImport(Node scene)
+    public override Godot.Object _PostImport(Node scene)
     {
-        // Modify the contents of the scene upon import. For example, setting up LODs:
-//      scene.GetNode<MeshInstance3D>("HighPolyMesh").DrawDistanceEnd = 5.0
-//      scene.GetNode<MeshInstance3D>("LowPolyMesh").DrawDistanceBegin = 5.0
-        return scene // Return the modified root node when you're done.
+        // Modify the contents of the scene upon import.
+        return scene; // Return the modified root node when you're done.
     }
 }
 #endif
