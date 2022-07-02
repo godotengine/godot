903e8565bdc396ae7b5474820f59ff17eb065047
Hana <48352564+Piralein@users.noreply.github.com>
2022-07-02 20:53:02 +0200

    remove redundant check preventing checkbox theme style to apply hover_pressed

--- scene/gui/button.cpp
+++ scene/gui/button.cpp
@@ -104,7 +104,7 @@ void Button::_notification(int p_what) {
 					}
 				} break;
 				case DRAW_HOVER_PRESSED: {
-					if (has_stylebox("hover_pressed") && has_stylebox_override("hover_pressed")) {
+					if (has_stylebox("hover_pressed")) {
 						style = get_stylebox("hover_pressed");
 						if (!flat) {
 							style->draw(ci, Rect2(Point2(0, 0), size));
