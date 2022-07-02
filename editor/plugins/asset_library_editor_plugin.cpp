efd1bed9d84ec43107f2fc4c04dcede3d937375d
python273 <iam@python273.pw>
2022-07-02 23:15:42 +0300

    Make asset library's column count dynamic
    (cherry picked from commit d65eb592b09112284bf3246d1f910495ffbbd597)

--- editor/plugins/asset_library_editor_plugin.cpp
+++ editor/plugins/asset_library_editor_plugin.cpp
@@ -617,6 +617,10 @@ void EditorAssetLibrary::_notification(int p_what) {
 			filter->set_clear_button_enabled(true);
 		} break;
 
+		case NOTIFICATION_RESIZED: {
+			_update_asset_items_columns();
+		} break;
+
 		case EditorSettings::NOTIFICATION_EDITOR_SETTINGS_CHANGED: {
 			_update_repository_options();
 			setup_http_request(request);
@@ -1208,7 +1212,7 @@ void EditorAssetLibrary::_http_request_completed(int p_status, int p_code, const
 			library_vb->add_child(asset_top_page);
 
 			asset_items = memnew(GridContainer);
-			asset_items->set_columns(2);
+			_update_asset_items_columns();
 			asset_items->add_constant_override("hseparation", 10 * EDSCALE);
 			asset_items->add_constant_override("vseparation", 10 * EDSCALE);
 
@@ -1340,12 +1344,17 @@ void EditorAssetLibrary::_install_external_asset(String p_zip_path, String p_tit
 	emit_signal("install_asset", p_zip_path, p_title);
 }
 
-void EditorAssetLibrary::disable_community_support() {
-	support->get_popup()->set_item_checked(SUPPORT_COMMUNITY, false);
+void EditorAssetLibrary::_update_asset_items_columns() {
+	int new_columns = get_size().x / (450.0 * EDSCALE);
+	new_columns = MAX(1, new_columns);
+
+	if (new_columns != asset_items->get_columns()) {
+		asset_items->set_columns(new_columns);
+	}
 }
 
-void EditorAssetLibrary::set_columns(const int p_columns) {
-	asset_items->set_columns(p_columns);
+void EditorAssetLibrary::disable_community_support() {
+	support->get_popup()->set_item_checked(SUPPORT_COMMUNITY, false);
 }
 
 void EditorAssetLibrary::_bind_methods() {
@@ -1522,7 +1531,7 @@ EditorAssetLibrary::EditorAssetLibrary(bool p_templates_only) {
 	library_vb->add_child(asset_top_page);
 
 	asset_items = memnew(GridContainer);
-	asset_items->set_columns(2);
+	_update_asset_items_columns();
 	asset_items->add_constant_override("hseparation", 10 * EDSCALE);
 	asset_items->add_constant_override("vseparation", 10 * EDSCALE);
 
