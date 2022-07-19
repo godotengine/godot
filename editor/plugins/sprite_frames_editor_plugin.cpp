468157bfe07e5b03e7a45960fe91a7e48abcf741
FireForge <67974470+fire-forge@users.noreply.github.com>
2022-07-18 20:15:50 -0500

    Capitalize the SpriteFrames search bar placeholder Follow-up to https://github.com/godotengine/godot/pull/49488

--- editor/plugins/sprite_frames_editor_plugin.cpp
+++ editor/plugins/sprite_frames_editor_plugin.cpp
@@ -1169,7 +1169,7 @@ SpriteFramesEditor::SpriteFramesEditor() {
 	anim_search_box = memnew(LineEdit);
 	hbc_animlist->add_child(anim_search_box);
 	anim_search_box->set_h_size_flags(SIZE_EXPAND_FILL);
-	anim_search_box->set_placeholder(TTR("Filter animations"));
+	anim_search_box->set_placeholder(TTR("Filter Animations"));
 	anim_search_box->set_clear_button_enabled(true);
 	anim_search_box->connect("text_changed", callable_mp(this, &SpriteFramesEditor::_animation_search_text_changed));
 
