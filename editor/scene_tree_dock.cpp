bb6aff1ebc46b00b20679fd515ca188cad6cc5ba
kobewi <kobewi4e@gmail.com>
2022-07-21 23:57:52 +0200

    Fix node dragging not working when dock is moved
    (cherry picked from commit b42bbca2666bb0b52156b8b396af54a1429c7077)

--- editor/scene_tree_dock.cpp
+++ editor/scene_tree_dock.cpp
@@ -63,7 +63,7 @@ void SceneTreeDock::_input(Ref<InputEvent> p_event) {
 	Ref<InputEventMouseButton> mb = p_event;
 
 	if (mb.is_valid() && (mb->get_button_index() == BUTTON_LEFT || mb->get_button_index() == BUTTON_RIGHT)) {
-		if (mb->is_pressed() && scene_tree->get_rect().has_point(mb->get_position())) {
+		if (mb->is_pressed() && scene_tree->get_rect().has_point(scene_tree->get_local_mouse_position())) {
 			tree_clicked = true;
 		} else if (!mb->is_pressed()) {
 			tree_clicked = false;
