6887b3f8eea79e5bdbf91650b15b5597c6e8155c
Aaron Franke <arnfranke@yahoo.com>
2022-07-23 18:52:46 -0500

    GLTF: Only list used extensions when they're actually used

--- modules/gltf/gltf_state.h
+++ modules/gltf/gltf_state.h
@@ -60,6 +60,7 @@ class GLTFState : public Resource {
 	Vector<uint8_t> glb_data;
 
 	bool use_named_skin_binds = false;
+	bool use_khr_texture_transform = false;
 	bool discard_meshes_and_materials = false;
 
 	Vector<Ref<GLTFNode>> nodes;
