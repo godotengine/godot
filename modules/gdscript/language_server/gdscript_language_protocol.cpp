83bce3d59e4d7146b806d0d72608621c59bce3b7
Lamia <git@lamia.io>
2022-07-24 21:42:32 +1000

    LSP: Improve handling of file URI scheme
    Fixes #63205.
    
    (cherry picked from commits 3fa943fe2320f28fd6c7a6910f27cf9c91f41e46,
    42a16ef76e4100c285a4a8c17f22a199986072a7 and
    2ff69d61813e1176daca5e6baee5e9f6106bf371)

--- modules/gdscript/language_server/gdscript_language_protocol.cpp
+++ modules/gdscript/language_server/gdscript_language_protocol.cpp
@@ -183,7 +183,9 @@ Dictionary GDScriptLanguageProtocol::initialize(const Dictionary &p_params) {
 	if (root_uri.length() && is_same_workspace) {
 		workspace->root_uri = root_uri;
 	} else {
-		workspace->root_uri = "file://" + workspace->root;
+		String r_root = workspace->root;
+		r_root = r_root.lstrip("/");
+		workspace->root_uri = "file:///" + r_root;
 
 		Dictionary params;
 		params["path"] = workspace->root;
