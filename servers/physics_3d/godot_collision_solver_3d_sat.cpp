f5b76400808e877d4b0c4c30ad22506f5241222d
Peter Eastman <peastman@stanford.edu>
2022-07-30 16:55:58 -0700

    Faster algorithm for mesh collision detection

--- servers/physics_3d/godot_collision_solver_3d_sat.cpp
+++ servers/physics_3d/godot_collision_solver_3d_sat.cpp
@@ -2075,117 +2075,166 @@ static void _collision_cylinder_face(const GodotShape3D *p_a, const Transform3D 
 	separator.generate_contacts();
 }
 
+static Vector3 _compute_support(const GodotConvexPolygonShape3D* obj1, const GodotConvexPolygonShape3D* obj2, const Transform3D& transform, const Vector3& direction) {
+    return obj1->get_support(direction) - transform.xform(obj2->get_support(transform.basis.xform_inv(-direction)));
+}
+
+template <bool withMargin>
+static void _add_contact(const Vector3 axis, const GodotConvexPolygonShape3D *p_a, const Transform3D &p_transform_a, const GodotConvexPolygonShape3D *p_b, const Transform3D &p_transform_b, _CollectorCallback *p_collector, real_t p_margin_a, real_t p_margin_b) {
+	SeparatorAxisTest<GodotConvexPolygonShape3D, GodotConvexPolygonShape3D, withMargin> separator(p_a, p_transform_a, p_b, p_transform_b, p_collector, p_margin_a, p_margin_b);
+	separator.best_axis = axis;
+	separator.generate_contacts();
+}
+
+/*
+ * The following routine implements Minkowski Portal Refinement as described in
+ *
+ * G. Snethen, Xenocollide: Complex collision made simple, Game Programming Gems 7, 2008
+ * 
+ * It is adapted from an implementation in Simbody (https://github.com/simbody/simbody),
+ * which is released under the following license.
+ */
+
+/* -------------------------------------------------------------------------- *
+ *                        SimTK Simbody: SimTKmath                            *
+ * -------------------------------------------------------------------------- *
+ * This is part of the SimTK biosimulation toolkit originating from           *
+ * Simbios, the NIH National Center for Physics-Based Simulation of           *
+ * Biological Structures at Stanford, funded under the NIH Roadmap for        *
+ * Medical Research, grant U54 GM072970. See https://simtk.org.               *
+ *                                                                            *
+ * Portions copyright (c) 2008 Stanford University and the Authors.           *
+ * Authors: Peter Eastman                                                     *
+ * Contributors:                                                              *
+ *                                                                            *
+ * Permission is hereby granted, free of charge, to any person obtaining a    *
+ * copy of this software and associated documentation files (the "Software"), *
+ * to deal in the Software without restriction, including without limitation  *
+ * the rights to use, copy, modify, merge, publish, distribute, sublicense,   *
+ * and/or sell copies of the Software, and to permit persons to whom the      *
+ * Software is furnished to do so, subject to the following conditions:       *
+ *                                                                            *
+ * The above copyright notice and this permission notice shall be included in *
+ * all copies or substantial portions of the Software.                        *
+ *                                                                            *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR *
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,   *
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL    *
+ * THE AUTHORS, CONTRIBUTORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM,    *
+ * DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR      *
+ * OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE  *
+ * USE OR OTHER DEALINGS IN THE SOFTWARE.                                     *
+ * -------------------------------------------------------------------------- */
+
 template <bool withMargin>
 static void _collision_convex_polygon_convex_polygon(const GodotShape3D *p_a, const Transform3D &p_transform_a, const GodotShape3D *p_b, const Transform3D &p_transform_b, _CollectorCallback *p_collector, real_t p_margin_a, real_t p_margin_b) {
-	const GodotConvexPolygonShape3D *convex_polygon_A = static_cast<const GodotConvexPolygonShape3D *>(p_a);
-	const GodotConvexPolygonShape3D *convex_polygon_B = static_cast<const GodotConvexPolygonShape3D *>(p_b);
+	const GodotConvexPolygonShape3D *obj1 = static_cast<const GodotConvexPolygonShape3D *>(p_a);
+	const GodotConvexPolygonShape3D *obj2 = static_cast<const GodotConvexPolygonShape3D *>(p_b);
+    Transform3D transform = p_transform_a.inverse()*p_transform_b;
 
-	SeparatorAxisTest<GodotConvexPolygonShape3D, GodotConvexPolygonShape3D, withMargin> separator(convex_polygon_A, p_transform_a, convex_polygon_B, p_transform_b, p_collector, p_margin_a, p_margin_b);
+    // Compute a point that is known to be inside the Minkowski difference, and
+    // a ray directed from that point to the origin.
 
-	if (!separator.test_previous_axis()) {
-		return;
-	}
+    Vector3 v0 = _compute_support(obj1, obj2, transform, Vector3(1, 0, 0)) + _compute_support(obj1, obj2, transform, Vector3(-1, 0, 0));
+    if (v0 == Vector3(0, 0, 0)) {
+        // This is a pathological case: the two objects are directly on top of
+        // each other with their centers at exactly the same place. Just
+        // return *some* vaguely plausible contact.
 
-	const Geometry3D::MeshData &mesh_A = convex_polygon_A->get_mesh();
+		_add_contact<withMargin>(Vector3(0, 1, 0), obj1, p_transform_a, obj2, p_transform_b, p_collector, p_margin_a, p_margin_b);
+        return;
+    }
 
-	const Geometry3D::MeshData::Face *faces_A = mesh_A.faces.ptr();
-	int face_count_A = mesh_A.faces.size();
-	const Geometry3D::MeshData::Edge *edges_A = mesh_A.edges.ptr();
-	int edge_count_A = mesh_A.edges.size();
-	const Vector3 *vertices_A = mesh_A.vertices.ptr();
-	int vertex_count_A = mesh_A.vertices.size();
+    // Select three points that define the initial portal.
 
-	const Geometry3D::MeshData &mesh_B = convex_polygon_B->get_mesh();
+    Vector3 dir1 = -v0.normalized();
+    Vector3 v1 = _compute_support(obj1, obj2, transform, dir1);
+    if (v1.dot(dir1) <= 0.0)
+        return;
+    if (v1.cross(v0) == Vector3(0, 0, 0)) {
+		_add_contact<withMargin>(p_transform_a.basis.xform(dir1), obj1, p_transform_a, obj2, p_transform_b, p_collector, p_margin_a, p_margin_b);
+        return;
+    }
+    Vector3 dir2 = v1.cross(v0).normalized();
+    Vector3 v2 = _compute_support(obj1, obj2, transform, dir2);
+    if (v2.dot(dir2) <= 0.0)
+        return;
+    Vector3 dir3 = (v1-v0).cross(v2-v0).normalized();
+    if (dir3.dot(v0) > 0) {
+        Vector3 swap1 = dir1;
+        Vector3 swap2 = v1;
+        dir1 = dir2;
+        v1 = v2;
+        dir2 = swap1;
+        v2 = swap2;
+        dir3 = -dir3;
+    }
+    Vector3 v3 = _compute_support(obj1, obj2, transform, dir3);
+    if (v3.dot(dir3) <= 0.0)
+        return;
+    while (true) {
+        if (v0.dot(v1.cross(v3)) < -1e-14) {
+            dir2 = dir3;
+            v2 = v3;
+        }
+        else if (v0.dot(v3.cross(v2)) < -1e-14) {
+            dir1 = dir3;
+            v1 = v3;
+        }
+        else
+            break;
+        dir3 = (v1-v0).cross(v2-v0).normalized();
+        v3 = _compute_support(obj1, obj2, transform, dir3);
+    }
 
-	const Geometry3D::MeshData::Face *faces_B = mesh_B.faces.ptr();
-	int face_count_B = mesh_B.faces.size();
-	const Geometry3D::MeshData::Edge *edges_B = mesh_B.edges.ptr();
-	int edge_count_B = mesh_B.edges.size();
-	const Vector3 *vertices_B = mesh_B.vertices.ptr();
-	int vertex_count_B = mesh_B.vertices.size();
+    // We have a portal that the origin ray passes through. Now we need to
+    // refine it.
 
-	// Precalculating this makes the transforms faster.
-	Basis a_xform_normal = p_transform_b.basis.inverse().transposed();
+	int extra_iterations = 0;
+    while (true) {
+        Vector3 portalDir = (v2-v1).cross(v3-v1).normalized();
+        if (portalDir.dot(v0) > 0)
+            portalDir = -portalDir;
+        real_t dist1 = portalDir.dot(v1);
+        Vector3 v4 = _compute_support(obj1, obj2, transform, portalDir);
+        real_t dist4 = portalDir.dot(v4);
+        if (dist1 >= 0.0) {
+            // The origin is inside the portal, so we have an intersection.
+            // If the portal is sufficiently small to give a precise collision
+			// axis, record the contact.  Otherwise, try iterating a little
+			// longer to define it more precisely.
 
-	// faces of A
-	for (int i = 0; i < face_count_A; i++) {
-		Vector3 axis = a_xform_normal.xform(faces_A[i].plane.normal).normalized();
-
-		if (!separator.test_axis(axis)) {
-			return;
-		}
-	}
-
-	// Precalculating this makes the transforms faster.
-	Basis b_xform_normal = p_transform_b.basis.inverse().transposed();
-
-	// faces of B
-	for (int i = 0; i < face_count_B; i++) {
-		Vector3 axis = b_xform_normal.xform(faces_B[i].plane.normal).normalized();
-
-		if (!separator.test_axis(axis)) {
-			return;
-		}
-	}
-
-	// A<->B edges
-	for (int i = 0; i < edge_count_A; i++) {
-		Vector3 e1 = p_transform_a.basis.xform(vertices_A[edges_A[i].a]) - p_transform_a.basis.xform(vertices_A[edges_A[i].b]);
-
-		for (int j = 0; j < edge_count_B; j++) {
-			Vector3 e2 = p_transform_b.basis.xform(vertices_B[edges_B[j].a]) - p_transform_b.basis.xform(vertices_B[edges_B[j].b]);
-
-			Vector3 axis = e1.cross(e2).normalized();
-
-			if (!separator.test_axis(axis)) {
+			if (extra_iterations == 5 || (dir1.dot(dir2) > 0.995 && dir2.dot(dir3) > 0.995 && dir3.dot(dir1) > 0.995)) {
+				Vector3 n = (v2-v1).cross(v3-v1).normalized();
+				_add_contact<withMargin>(p_transform_a.basis.xform(-n), obj1, p_transform_a, obj2, p_transform_b, p_collector, p_margin_a, p_margin_b);
 				return;
 			}
-		}
-	}
-
-	if (withMargin) {
-		//vertex-vertex
-		for (int i = 0; i < vertex_count_A; i++) {
-			Vector3 va = p_transform_a.xform(vertices_A[i]);
-
-			for (int j = 0; j < vertex_count_B; j++) {
-				if (!separator.test_axis((va - p_transform_b.xform(vertices_B[j])).normalized())) {
-					return;
-				}
-			}
-		}
-		//edge-vertex (shell)
-
-		for (int i = 0; i < edge_count_A; i++) {
-			Vector3 e1 = p_transform_a.basis.xform(vertices_A[edges_A[i].a]);
-			Vector3 e2 = p_transform_a.basis.xform(vertices_A[edges_A[i].b]);
-			Vector3 n = (e2 - e1);
-
-			for (int j = 0; j < vertex_count_B; j++) {
-				Vector3 e3 = p_transform_b.xform(vertices_B[j]);
-
-				if (!separator.test_axis((e1 - e3).cross(n).cross(n).normalized())) {
-					return;
-				}
-			}
-		}
-
-		for (int i = 0; i < edge_count_B; i++) {
-			Vector3 e1 = p_transform_b.basis.xform(vertices_B[edges_B[i].a]);
-			Vector3 e2 = p_transform_b.basis.xform(vertices_B[edges_B[i].b]);
-			Vector3 n = (e2 - e1);
-
-			for (int j = 0; j < vertex_count_A; j++) {
-				Vector3 e3 = p_transform_a.xform(vertices_A[j]);
-
-				if (!separator.test_axis((e1 - e3).cross(n).cross(n).normalized())) {
-					return;
-				}
-			}
-		}
-	}
-
-	separator.generate_contacts();
+			extra_iterations += 1;
+        }
+        if (dist4 <= 0.0)
+            return;
+        Vector3 cross = v4.cross(v0);
+        if (v1.dot(cross) > 0.0) {
+            if (v2.dot(cross) > 0.0) {
+                dir1 = portalDir;
+                v1 = v4;
+            }
+            else {
+                dir3 = portalDir;
+                v3 = v4;
+            }
+        }
+        else {
+            if (v3.dot(cross) > 0.0) {
+                dir2 = portalDir;
+                v2 = v4;
+            }
+            else {
+                dir1 = portalDir;
+                v1 = v4;
+            }
+        }
+    }
 }
 
 template <bool withMargin>
