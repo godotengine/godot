2e7a470e75539ba3f1b33ce11db307d3808f7597
Rémi Verschelde <rverschelde@gmail.com>
2022-08-05 18:51:38 +0200

    Bump version to 3.6-beta

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 3
-minor = 5
+minor = 6
 patch = 0
-status = "stable"
+status = "beta"
 module_config = ""
 year = 2022
 website = "https://godotengine.org"
