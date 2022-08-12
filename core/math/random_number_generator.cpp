717a297e5fe06fbdf77b89c08131d16b714dc09c
kobewi <kobewi4e@gmail.com>
2022-08-12 15:01:50 +0200

    Add shuffle_array method to RandomNumberGenerator

--- core/math/random_number_generator.cpp
+++ core/math/random_number_generator.cpp
@@ -30,6 +30,10 @@
 
 #include "random_number_generator.h"
 
+void RandomNumberGenerator::shuffle_array(Array p_array) {
+	p_array.rng_shuffle(this);
+}
+
 void RandomNumberGenerator::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("set_seed", "seed"), &RandomNumberGenerator::set_seed);
 	ClassDB::bind_method(D_METHOD("get_seed"), &RandomNumberGenerator::get_seed);
@@ -43,6 +47,8 @@ void RandomNumberGenerator::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("randf_range", "from", "to"), &RandomNumberGenerator::randf_range);
 	ClassDB::bind_method(D_METHOD("randi_range", "from", "to"), &RandomNumberGenerator::randi_range);
 	ClassDB::bind_method(D_METHOD("rand_weighted", "weights"), &RandomNumberGenerator::rand_weighted);
+	ClassDB::bind_method(D_METHOD("shuffle_array", "array"), &RandomNumberGenerator::shuffle_array);
+
 	ClassDB::bind_method(D_METHOD("randomize"), &RandomNumberGenerator::randomize);
 
 	ADD_PROPERTY(PropertyInfo(Variant::INT, "seed"), "set_seed", "get_seed");
