200dd38d6410d95d2c64369accdbe6d9e0ca123e
Pedro J. Estébanez <pedrojrulez@gmail.com>
2022-08-13 21:52:03 +0200

    Overhaul CLI argument forwarding to processes started by the editor

--- main/main.h
+++ main/main.h
@@ -35,6 +35,9 @@
 #include "core/os/thread.h"
 #include "core/typedefs.h"
 
+template <class T>
+class Vector;
+
 class Main {
 	static void print_help(const char *p_binary);
 	static uint64_t last_ticks;
@@ -47,6 +50,13 @@ class Main {
 
 public:
 	static bool is_project_manager();
+#ifdef TOOLS_ENABLED
+	enum CLIScope {
+		CLI_SCOPE_TOOL, // Editor and project manager.
+		CLI_SCOPE_PROJECT,
+	};
+	static const Vector<String> &get_forwardable_cli_arguments(CLIScope p_scope);
+#endif
 
 	static Error setup(const char *execpath, int argc, char *argv[], bool p_second_phase = true);
 	static Error setup2(Thread::ID p_main_tid_override = 0);
