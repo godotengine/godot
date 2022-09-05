ce20d02d34ce2837aa03023b1b47325fc802978d
kobewi <kobewi4e@gmail.com>
2022-09-05 15:58:22 +0200

    Add Dictionary.find_key()

--- core/dictionary.h
+++ core/dictionary.h
@@ -66,6 +66,7 @@ public:
 
 	bool has(const Variant &p_key) const;
 	bool has_all(const Array &p_keys) const;
+	Variant find_key(const Variant &p_value) const;
 
 	bool erase(const Variant &p_key);
 
