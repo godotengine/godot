b63bbfc354f1dd130df76ba53025b9bd6a82d319
Void <arandomdude75@gmail.com>
2022-09-05 16:21:48 +0100

    added benchmark module

---
+++ modules/Str_word_wrap_benchmark/register_types.cpp
@@ -0,0 +1,9 @@
+#include "register_types.h"
+
+void initialize_Str_word_wrap_benchmark_module(ModuleInitializationLevel p_level) {
+	
+}
+
+void uninitialize_Str_word_wrap_benchmark_module(ModuleInitializationLevel p_level) {
+	
+}
