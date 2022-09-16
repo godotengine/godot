12fd3555ab453cffa61ab473d671355eee0b4f46
kobewi <kobewi4e@gmail.com>
2022-09-16 17:42:06 +0200

    Cleanup function state connections when destroying instance
    Co-authored-by: Adam Scott <ascott.ca@gmail.com>

--- modules/gdscript/gdscript_function.cpp
+++ modules/gdscript/gdscript_function.cpp
@@ -296,6 +296,15 @@ void GDScriptFunctionState::_clear_stack() {
 	}
 }
 
+void GDScriptFunctionState::_clear_connections() {
+	List<Object::Connection> conns;
+	get_signals_connected_to_this(&conns);
+
+	for (Object::Connection &c : conns) {
+		c.signal.disconnect(c.callable);
+	}
+}
+
 void GDScriptFunctionState::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("resume", "arg"), &GDScriptFunctionState::resume, DEFVAL(Variant()));
 	ClassDB::bind_method(D_METHOD("is_valid", "extended_check"), &GDScriptFunctionState::is_valid, DEFVAL(false));
