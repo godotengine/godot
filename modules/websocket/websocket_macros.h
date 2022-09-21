5408af1407581fe8c0aebb425ef5148bf27e1b15
Rémi Verschelde <rverschelde@gmail.com>
2022-09-21 12:48:48 +0200

    Style: Ensure consistent formatting with clang-format 15
    When going from version 14 to 15 it would introduce a tiny change in
    `websocket_macros.h` just before the comment re-enabling clang-format,
    but this can be solved by just letting it do its work.
    
    Bonus cosmetic change in `math_fieldwise.cpp` where clang-format isn't
    used, and bump recommended versions for pre-commit hook to [13; 15].

--- modules/websocket/websocket_macros.h
+++ modules/websocket/websocket_macros.h
@@ -35,34 +35,32 @@
 #define DEF_PKT_SHIFT 10
 #define DEF_BUF_SHIFT 16
 
-/* clang-format off */
-#define GDCICLASS(CNAME) \
-public:\
-	static CNAME *(*_create)();\
-\
-	static Ref<CNAME > create_ref() {\
-\
-		if (!_create)\
-			return Ref<CNAME >();\
-		return Ref<CNAME >(_create());\
-	}\
-\
-	static CNAME *create() {\
-\
-		if (!_create)\
-			return nullptr;\
-		return _create();\
-	}\
-protected:\
+#define GDCICLASS(CNAME)              \
+public:                               \
+	static CNAME *(*_create)();       \
+                                      \
+	static Ref<CNAME> create_ref() {  \
+		if (!_create)                 \
+			return Ref<CNAME>();      \
+		return Ref<CNAME>(_create()); \
+	}                                 \
+                                      \
+	static CNAME *create() {          \
+		if (!_create)                 \
+			return nullptr;           \
+		return _create();             \
+	}                                 \
+                                      \
+protected:
 
 #define GDCINULL(CNAME) \
-CNAME *(*CNAME::_create)() = nullptr;
+	CNAME *(*CNAME::_create)() = nullptr;
 
-#define GDCIIMPL(IMPNAME, CNAME) \
-public:\
-	static CNAME *_create() { return memnew(IMPNAME); }\
-	static void make_default() { CNAME::_create = IMPNAME::_create; }\
-protected:\
-/* clang-format on */
+#define GDCIIMPL(IMPNAME, CNAME)                                      \
+public:                                                               \
+	static CNAME *_create() { return memnew(IMPNAME); }               \
+	static void make_default() { CNAME::_create = IMPNAME::_create; } \
+                                                                      \
+protected:
 
 #endif // WEBSOCKET_MACROS_H
