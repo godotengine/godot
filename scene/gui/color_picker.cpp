2ab79719b25376b599219aecbd71009dc7eeacc2
Micky <micheledevita2@gmail.com>
2022-09-24 15:43:41 +0200

    Fix ColorPicker always emitting color_changed on html submit
    The color change was always emitted when the the modal was closed, even if it was exactly the same as before.
    
    (cherry picked from commit 544727ac1ea7616c9f44116360d038cfcda5f763)

--- scene/gui/color_picker.cpp
+++ scene/gui/color_picker.cpp
@@ -195,12 +195,15 @@ void ColorPicker::_html_entered(const String &p_html) {
 		return;
 	}
 
-	float last_alpha = color.a;
+	Color previous_color = color;
 	color = Color::html(p_html);
 	if (!is_editing_alpha()) {
-		color.a = last_alpha;
+		color.a = previous_color.a;
 	}
 
+	if (color == previous_color) {
+		return;
+	}
 	if (!is_inside_tree()) {
 		return;
 	}
