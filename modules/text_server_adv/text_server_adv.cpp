51978dde5aaa350376c638c708bb25d6abf70874
bruvzg <7645683+bruvzg@users.noreply.github.com>
2022-09-28 12:43:34 +0300

    [Text Server] Clear textures after "generate mipmaps" property change.

--- modules/text_server_adv/text_server_adv.cpp
+++ modules/text_server_adv/text_server_adv.cpp
@@ -2035,6 +2035,7 @@ void TextServerAdvanced::font_set_generate_mipmaps(const RID &p_font_rid, bool p
 		for (KeyValue<Vector2i, FontForSizeAdvanced *> &E : fd->cache) {
 			for (int i = 0; i < E.value->textures.size(); i++) {
 				E.value->textures.write[i].dirty = true;
+				E.value->textures.write[i].texture = Ref<ImageTexture>();
 			}
 		}
 		fd->mipmaps = p_generate_mipmaps;
--- modules/text_server_fb/text_server_fb.cpp
+++ modules/text_server_fb/text_server_fb.cpp
@@ -1129,6 +1129,7 @@ void TextServerFallback::font_set_generate_mipmaps(const RID &p_font_rid, bool p
 		for (KeyValue<Vector2i, FontForSizeFallback *> &E : fd->cache) {
 			for (int i = 0; i < E.value->textures.size(); i++) {
 				E.value->textures.write[i].dirty = true;
+				E.value->textures.write[i].texture = Ref<ImageTexture>();
 			}
 		}
 		fd->mipmaps = p_generate_mipmaps;
