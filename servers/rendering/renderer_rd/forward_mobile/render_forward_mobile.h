3126619aa501e0731895a0c6ced3eb2cc1e38ec5
clayjohn <claynjohn@gmail.com>
2022-09-30 14:10:46 -0700

    Update Instance flags in shaders to match instance flags in engine

--- servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.h
+++ servers/rendering/renderer_rd/forward_mobile/render_forward_mobile.h
@@ -371,7 +371,7 @@ protected:
 
 	/* Geometry instance */
 
-	// check which ones of these apply, probably all except GI and SDFGI
+	// When changing any of these enums, remember to change the corresponding enums in the shader files as well.
 	enum {
 		INSTANCE_DATA_FLAGS_NON_UNIFORM_SCALE = 1 << 4,
 		INSTANCE_DATA_FLAG_USE_GI_BUFFERS = 1 << 5,
