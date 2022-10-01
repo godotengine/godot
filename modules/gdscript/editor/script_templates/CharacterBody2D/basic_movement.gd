aaa8345393141d5b46d29246cb9272efbd76e402
Chris Bradfield <cb@scribe.net>
2022-10-01 13:04:14 -0700

    changes to default characterbody scripts

--- modules/gdscript/editor/script_templates/CharacterBody2D/basic_movement.gd
+++ modules/gdscript/editor/script_templates/CharacterBody2D/basic_movement.gd
@@ -6,25 +6,21 @@ extends _BASE_
 const SPEED = 300.0
 const JUMP_VELOCITY = -400.0
 
-# Get the gravity from the project settings to be synced with RigidBody nodes.
-var gravity: int = ProjectSettings.get_setting("physics/2d/default_gravity")
+# Get the global gravity from the project settings. RigidBody2D nodes also use this value.
+var gravity: float = ProjectSettings.get_setting("physics/2d/default_gravity")
 
 
 func _physics_process(delta: float) -> void:
 	# Add the gravity.
-	if not is_on_floor():
-		velocity.y += gravity * delta
+	velocity.y += gravity * delta
 
-	# Handle Jump.
+	# Allow jumping only when on the floor.
 	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
+	# Get the input direction and handle the movement.
+	# As good practice, you should replace UI actions with custom input actions.
 	var direction := Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+	velocity.x = direction * SPEED
 
 	move_and_slide()
