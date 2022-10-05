ad16187a4a606ed5e8b19cd909fd5764c5c02e2e
jbcolli2 <jbcolli2@gmail.com>
2022-10-05 08:02:00 -0400

    PrimitiveMesh unit tests.

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -90,6 +90,7 @@
 #include "tests/scene/test_curve.h"
 #include "tests/scene/test_gradient.h"
 #include "tests/scene/test_path_3d.h"
+#include "tests/scene/test_primitives.h"
 #include "tests/scene/test_sprite_frames.h"
 #include "tests/scene/test_text_edit.h"
 #include "tests/scene/test_theme.h"
