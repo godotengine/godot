b18a553a06bc15b1247068a0ec3537ae55fdd697
Arthur J <59916285+OvercookedRoach@users.noreply.github.com>
2022-10-07 18:36:37 -0300

    declare "_validate_property" as protected
    In other classes, the function _validate_property is declared as protected (in the case of Area2D), but in Area3D it's declared as private, which causes compile errors when trying to extend the class in a C++ module for example.

--- scene/3d/area_3d.h
+++ scene/3d/area_3d.h
@@ -141,13 +141,12 @@ private:
 	float reverb_amount = 0.0;
 	float reverb_uniformity = 0.0;
 
-	void _validate_property(PropertyInfo &p_property) const;
-
 	void _initialize_wind();
 
 protected:
 	void _notification(int p_what);
 	static void _bind_methods();
+	void _validate_property(PropertyInfo &p_property) const;
 
 public:
 	void set_gravity_space_override_mode(SpaceOverride p_mode);
