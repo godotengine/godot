f5981ff19dfa98ac2252900ce63d3ae6a2f0c003
kobewi <kobewi4e@gmail.com>
2022-10-17 16:51:52 +0200

    Add a separate hue gradient for OKHSL mode

--- scene/resources/default_theme/default_theme.cpp
+++ scene/resources/default_theme/default_theme.cpp
@@ -888,6 +888,7 @@ void fill_default_theme(Ref<Theme> &theme, const Ref<Font> &default_font, const 
 	theme->set_icon("shape_rect_wheel", "ColorPicker", icons["picker_shape_rectangle_wheel"]);
 	theme->set_icon("add_preset", "ColorPicker", icons["add"]);
 	theme->set_icon("color_hue", "ColorPicker", icons["color_picker_hue"]);
+	theme->set_icon("color_okhsl_hue", "ColorPicker", icons["color_picker_okhsl_hue"]);
 	theme->set_icon("sample_bg", "ColorPicker", icons["mini_checkerboard"]);
 	theme->set_icon("overbright_indicator", "ColorPicker", icons["color_picker_overbright"]);
 	theme->set_icon("bar_arrow", "ColorPicker", icons["color_picker_bar_arrow"]);
