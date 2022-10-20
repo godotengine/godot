5ede9e6b81704060268adaf954b3ef4493eb77df
Micky <micheledevita2@gmail.com>
2022-10-20 17:28:04 +0200

    Unexpose ERR_HELP constant

--- core/error/error_list.h
+++ core/error/error_list.h
@@ -90,9 +90,9 @@ enum Error {
 	ERR_PARSE_ERROR,
 	ERR_BUSY,
 	ERR_SKIP, // (45)
-	ERR_HELP, ///< user requested help!!
 	ERR_BUG, ///< a bug in the software certainly happened, due to a double check failing or unexpected behavior.
 	ERR_PRINTER_ON_FIRE, /// the parallel port printer is engulfed in flames
+	ERR_HELP, ///< user requested help!! See --version and --help. Unexposed.
 	ERR_MAX, // Not being returned, value represents the number of errors
 };
 
