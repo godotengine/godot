3e373c4eec6d2c9edb1ff96969926431751d583e
kleonc <9283098+kleonc@users.noreply.github.com>
2022-11-02 14:07:06 +0100

    Add `ShapeCast2D/3D::get_collider_rid` method

--- scene/3d/shape_cast.cpp
+++ scene/3d/shape_cast.cpp
@@ -116,6 +116,7 @@ void ShapeCast::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("force_shapecast_update"), &ShapeCast::force_shapecast_update);
 
 	ClassDB::bind_method(D_METHOD("get_collider", "index"), &ShapeCast::get_collider);
+	ClassDB::bind_method(D_METHOD("get_collider_rid", "index"), &ShapeCast::get_collider_rid);
 	ClassDB::bind_method(D_METHOD("get_collider_shape", "index"), &ShapeCast::get_collider_shape);
 	ClassDB::bind_method(D_METHOD("get_collision_point", "index"), &ShapeCast::get_collision_point);
 	ClassDB::bind_method(D_METHOD("get_collision_normal", "index"), &ShapeCast::get_collision_normal);
@@ -289,6 +290,11 @@ Object *ShapeCast::get_collider(int p_idx) const {
 		return nullptr;
 	}
 	return ObjectDB::get_instance(result[p_idx].collider_id);
+}
+
+RID ShapeCast::get_collider_rid(int p_idx) const {
+	ERR_FAIL_INDEX_V_MSG(p_idx, result.size(), RID(), "No collider RID found.");
+	return result[p_idx].rid;
 }
 
 int ShapeCast::get_collider_shape(int p_idx) const {
