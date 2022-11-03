2cd39e532b9ac752dfcc29ef967e022912f1c2a8
David Snopek <dsnopek@gmail.com>
2022-11-03 08:47:28 -0500

    Collapse three seperate texture storage methods into render_target_set_override()

--- servers/rendering/renderer_rd/storage_rd/texture_storage.h
+++ servers/rendering/renderer_rd/storage_rd/texture_storage.h
@@ -718,12 +718,10 @@ public:
 	virtual void render_target_set_vrs_texture(RID p_render_target, RID p_texture) override;
 	virtual RID render_target_get_vrs_texture(RID p_render_target) const override;
 
-	virtual void render_target_set_override_color(RID p_render_target, RID p_texture) override;
+	virtual void render_target_set_override(RID p_render_target, RID p_color_texture, RID p_depth_texture, RID p_velocity_texture) override;
 	virtual RID render_target_get_override_color(RID p_render_target) const override;
-	virtual void render_target_set_override_depth(RID p_render_target, RID p_texture) override;
 	virtual RID render_target_get_override_depth(RID p_render_target) const override;
 	RID render_target_get_override_depth_slice(RID p_render_target, const uint32_t p_layer) const;
-	virtual void render_target_set_override_velocity(RID p_render_target, RID p_texture) override;
 	virtual RID render_target_get_override_velocity(RID p_render_target) const override;
 	RID render_target_get_override_velocity_slice(RID p_render_target, const uint32_t p_layer) const;
 
