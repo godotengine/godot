cdac36d0443f437021ef7a72839290794f2aa095
Riteo <riteo@posteo.net>
2022-11-21 11:37:34 +0100

    Remove EGL dependency from Wayland
    Now we have the GLAD loader, so it isn't needed anymore.

--- platform/linuxbsd/detect.py
+++ platform/linuxbsd/detect.py
@@ -371,7 +371,6 @@ def configure(env: "Environment"):
 
         if env["wayland"]:
             env.Append(LIBS=["rt"])  # Needed by glibc, used by _allocate_shm_file
-            env.ParseConfig("pkg-config egl --cflags --libs")
             env.ParseConfig("pkg-config wayland-egl --cflags")  # Only cflags, we dlopen the library.
 
     env.Append(LIBS=["pthread"])
