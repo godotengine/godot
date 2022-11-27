9b7c295c46e02b0f16649175eed6e83a263c97f1
Bartosz Bielecki <bielecki.b@gmail.com>
2022-11-27 22:38:04 +0100

    Acknowledge that a CLASS kind of a DataType might not have an identifier

--- modules/gdscript/gdscript_parser.cpp
+++ modules/gdscript/gdscript_parser.cpp
@@ -3785,15 +3785,14 @@ bool GDScriptParser::export_annotations(const AnnotationNode *p_annotation, Node
 				}
 				break;
 			case GDScriptParser::DataType::CLASS:
-				// Can assume type is a global GDScript class.
 				if (ClassDB::is_parent_class(export_type.native_type, SNAME("Resource"))) {
 					variable->export_info.type = Variant::OBJECT;
 					variable->export_info.hint = PROPERTY_HINT_RESOURCE_TYPE;
-					variable->export_info.hint_string = export_type.class_type->identifier->name;
+					variable->export_info.hint_string = export_type.to_string();
 				} else if (ClassDB::is_parent_class(export_type.native_type, SNAME("Node"))) {
 					variable->export_info.type = Variant::OBJECT;
 					variable->export_info.hint = PROPERTY_HINT_NODE_TYPE;
-					variable->export_info.hint_string = export_type.class_type->identifier->name;
+					variable->export_info.hint_string = export_type.to_string();
 				} else {
 					push_error(R"(Export type can only be built-in, a resource, a node or an enum.)", variable);
 					return false;
