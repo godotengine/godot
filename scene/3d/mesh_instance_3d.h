29dea68baba6409d789e689343ddd2af928a6ac3
clayjohn <claynjohn@gmail.com>
2022-12-02 23:20:39 -0800

    Index override surface name starting from 1 to match surface name

--- scene/3d/mesh_instance_3d.h
+++ scene/3d/mesh_instance_3d.h
@@ -57,6 +57,7 @@ protected:
 	bool _set(const StringName &p_name, const Variant &p_value);
 	bool _get(const StringName &p_name, Variant &r_ret) const;
 	void _get_property_list(List<PropertyInfo> *p_list) const;
+	bool old_surface_index = false;
 
 	void _notification(int p_what);
 	static void _bind_methods();
