08449ab16ecba2c04511b5da464f75d241eaff95
rune-scape <allie.smith.epic@gmail.com>
2022-12-12 07:27:41 -0500

    Fix String type compatibility being too permissive

--- modules/gdscript/tests/scripts/analyzer/features/array_string_stringname_equivalent.gd
+++ modules/gdscript/tests/scripts/analyzer/features/array_string_stringname_equivalent.gd
@@ -1,4 +1,11 @@
+
+var m_string_array: Array[String] = [&"abc"]
+var m_stringname_array: Array[StringName] = ["abc"]
+
 func test():
+	print(m_string_array)
+	print(m_stringname_array)
+
 	# Converted to String when initialized
 	var string_array: Array[String] = [&"abc"]
 	print(string_array)
