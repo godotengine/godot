8b205a2548c969576ca0daecbfc684cc79c85741
Rémi Verschelde <rverschelde@gmail.com>
2022-12-13 10:05:26 +0100

    graphite: Update to latest Git, switch to MIT license
    Graphite is now available under:
    MIT OR MPL-2.0 OR LGPL-2.1-or-later OR GPL-2.0-or-later
    
    We pick MIT which is the same as Godot's main license for simplicity.
    
    Remove define to skip deprecation warnings, upstream fixed those.

--- thirdparty/graphite/src/inc/bits.h
+++ thirdparty/graphite/src/inc/bits.h
@@ -1,29 +1,6 @@
-/*  GRAPHITE2 LICENSING
+// SPDX-License-Identifier: MIT OR MPL-2.0 OR LGPL-2.1-or-later OR GPL-2.0-or-later
+// Copyright 2012, SIL International, All rights reserved.
 
-    Copyright 2012, SIL International
-    All rights reserved.
-
-    This library is free software; you can redistribute it and/or modify
-    it under the terms of the GNU Lesser General Public License as published
-    by the Free Software Foundation; either version 2.1 of License, or
-    (at your option) any later version.
-
-    This program is distributed in the hope that it will be useful,
-    but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
-    Lesser General Public License for more details.
-
-    You should also have received a copy of the GNU Lesser General Public
-    License along with this library in the file named "LICENSE".
-    If not, write to the Free Software Foundation, 51 Franklin Street,
-    Suite 500, Boston, MA 02110-1335, USA or visit their web page on the
-    internet at http://www.fsf.org/licenses/lgpl.html.
-
-Alternatively, the contents of this file may be used under the terms of the
-Mozilla Public License (http://mozilla.org/MPL) or the GNU General Public
-License, as published by the Free Software Foundation, either version 2
-of the License or (at your option) any later version.
-*/
 #pragma once
 
 namespace graphite2
