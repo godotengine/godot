3a0ffddc3154290eb8222bf8e9045842ce1dd0fc
clayjohn <claynjohn@gmail.com>
2022-12-13 11:24:02 -0800

    Added gl_compatibility as an option to the project creation screen
    Last selected option is saved as default for next time

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -734,6 +734,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 
 	// TRANSLATORS: Project Manager here refers to the tool used to create/manage Godot projects.
 	EDITOR_SETTING(Variant::INT, PROPERTY_HINT_ENUM, "project_manager/sorting_order", 0, "Last Edited,Name,Path")
+	EDITOR_SETTING(Variant::STRING, PROPERTY_HINT_NONE, "project_manager/default_renderer", "forward_plus", "forward_plus,mobile,gl_compatibility")
 
 	if (p_extra_config.is_valid()) {
 		if (p_extra_config->has_section("init_projects") && p_extra_config->has_section_key("init_projects", "list")) {
