073785006169388e3832240749457c1abff7769c
Fredia Huya-Kouadio <fhuya@meta.com>
2022-12-16 09:54:50 -0800

    Add boot splash for the Godot Editor

--- main/main.cpp
+++ main/main.cpp
@@ -1422,7 +1422,7 @@ Error Main::setup2(Thread::ID p_main_tid_override) {
 
 	MAIN_PRINT("Main: Setup Logo");
 
-#if defined(JAVASCRIPT_ENABLED) || defined(ANDROID_ENABLED)
+#if !defined(TOOLS_ENABLED) && (defined(JAVASCRIPT_ENABLED) || defined(ANDROID_ENABLED))
 	bool show_logo = false;
 #else
 	bool show_logo = true;
