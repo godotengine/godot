06e82ac51a3ec3539f2520394fb1340681fd1828
Hugo Locurcio <hugo.locurcio@hugo.pro>
2022-12-17 18:45:21 +0100

    Move transparent background project setting to a subsection
    This prevents the project setting from being located directly within
    a root category, which is confusing from an UX perspective in the
    project settings editor.

--- scene/main/scene_tree.cpp
+++ scene/main/scene_tree.cpp
@@ -1422,7 +1422,7 @@ SceneTree::SceneTree() {
 	ProjectSettings::get_singleton()->set_custom_property_info("rendering/anti_aliasing/quality/msaa_3d", PropertyInfo(Variant::INT, "rendering/anti_aliasing/quality/msaa_3d", PROPERTY_HINT_ENUM, String::utf8("Disabled (Fastest),2× (Average),4× (Slow),8× (Slowest)")));
 	root->set_msaa_3d(Viewport::MSAA(msaa_mode_3d));
 
-	const bool transparent_background = GLOBAL_DEF("rendering/transparent_background", false);
+	const bool transparent_background = GLOBAL_DEF("rendering/viewport/transparent_background", false);
 	root->set_transparent_background(transparent_background);
 
 	const int ssaa_mode = GLOBAL_DEF_BASIC("rendering/anti_aliasing/quality/screen_space_aa", 0);
