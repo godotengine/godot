78cb71354ad4fe19af54e545588acc27067ee407
Rémi Verschelde <rverschelde@gmail.com>
2022-12-20 18:14:02 +0100

    Fix some Clang -Wunused-but-set-variable warnings
    (cherry picked from commit e771804e783513a746734474f90c1cd653bb8297)

--- editor/import/resource_importer_scene.cpp
+++ editor/import/resource_importer_scene.cpp
@@ -1148,14 +1148,12 @@ void ResourceImporterScene::_replace_owner(Node *p_node, Node *p_scene, Node *p_
 }
 
 void ResourceImporterScene::_add_shapes(Node *p_node, const List<Ref<Shape>> &p_shapes) {
-	int idx = 0;
 	for (const List<Ref<Shape>>::Element *E = p_shapes.front(); E; E = E->next()) {
 		CollisionShape *cshape = memnew(CollisionShape);
 		cshape->set_shape(E->get());
 		p_node->add_child(cshape);
 
 		cshape->set_owner(p_node->get_owner());
-		idx++;
 	}
 }
 
