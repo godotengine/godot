274d49790dd2d68828a312e2c0802c1dbf09ac8f
Dmitrii Maganov <vonagam@gmail.com>
2022-12-29 09:34:13 +0200

    GDScript: Fix extending abstract classes, forbid their construction

--- editor/create_dialog.cpp
+++ editor/create_dialog.cpp
@@ -148,6 +148,11 @@ bool CreateDialog::_should_hide_type(const String &p_type) const {
 			return true;
 		}
 
+		StringName native_type = ScriptServer::get_global_class_native_base(p_type);
+		if (ClassDB::class_exists(native_type) && !ClassDB::can_instantiate(native_type)) {
+			return true;
+		}
+
 		String script_path = ScriptServer::get_global_class_path(p_type);
 		if (script_path.begins_with("res://addons/")) {
 			if (!EditorNode::get_singleton()->is_addon_plugin_enabled(script_path.get_slicec('/', 3))) {
---
+++ modules/gdscript/tests/scripts/analyzer/features/extend_abstract_class.gd
@@ -0,0 +1,12 @@
+class A extends CanvasItem:
+	func _init():
+		pass
+
+class B extends A:
+	pass
+
+class C extends CanvasItem:
+	pass
+
+func test():
+	print('ok')
