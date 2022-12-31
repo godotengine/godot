ce5727971d8f3f4ccd6ecdaa238a8b54f7932ecc
James Mintram <james@rattlecan.games>
2022-12-31 20:32:32 +0000

    Add missing case statement to handle re-parent to new node

--- editor/scene_tree_dock.cpp
+++ editor/scene_tree_dock.cpp
@@ -134,6 +134,8 @@ void SceneTreeDock::shortcut_input(const Ref<InputEvent> &p_event) {
 		_tool_selected(TOOL_MOVE_DOWN);
 	} else if (ED_IS_SHORTCUT("scene_tree/reparent", p_event)) {
 		_tool_selected(TOOL_REPARENT);
+	} else if (ED_IS_SHORTCUT("scene_tree/reparent_to_new_node", p_event)) {
+		_tool_selected(TOOL_REPARENT_TO_NEW_NODE);
 	} else if (ED_IS_SHORTCUT("scene_tree/save_branch_as_scene", p_event)) {
 		_tool_selected(TOOL_NEW_SCENE_FROM);
 	} else if (ED_IS_SHORTCUT("scene_tree/delete_no_confirm", p_event)) {
