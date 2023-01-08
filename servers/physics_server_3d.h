8ee6264cc914fe1d007faf440fe4fda28b567cc6
Lily Garcia <staticdroidpop@gmail.com>
2023-01-07 22:34:28 -0500

    Implement collision impulse in Godot Physics 3D

--- servers/physics_server_3d.h
+++ servers/physics_server_3d.h
@@ -95,7 +95,7 @@ public:
 
 	virtual Vector3 get_contact_local_position(int p_contact_idx) const = 0;
 	virtual Vector3 get_contact_local_normal(int p_contact_idx) const = 0;
-	virtual real_t get_contact_impulse(int p_contact_idx) const = 0;
+	virtual Vector3 get_contact_impulse(int p_contact_idx) const = 0;
 	virtual int get_contact_local_shape(int p_contact_idx) const = 0;
 
 	virtual RID get_contact_collider(int p_contact_idx) const = 0;
