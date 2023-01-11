19293d1813ed49bc1e7c193a8a6e723a95024533
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-01-11 10:13:21 +0200

    Automatically reparent editor progress dialog to avoid error spam.

--- scene/main/window.h
+++ scene/main/window.h
@@ -285,6 +285,7 @@ public:
 	bool is_wrapping_controls() const;
 	void child_controls_changed();
 
+	Window *get_exclusive_child() const { return exclusive_child; };
 	Window *get_parent_visible_window() const;
 	Viewport *get_parent_viewport() const;
 	void popup(const Rect2i &p_rect = Rect2i());
