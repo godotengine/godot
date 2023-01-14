7d9810beca55fc44ce5ce88cfdb0162728bb433e
Yuri Rubinsky <chaosus89@gmail.com>
2023-01-14 15:58:42 +0300

    Fix GDScript script templates to use a PascalCase style for `_CLASS_`

--- modules/gdscript/gdscript_editor.cpp
+++ modules/gdscript/gdscript_editor.cpp
@@ -80,7 +80,7 @@ Ref<Script> GDScriptLanguage::make_template(const String &p_template, const Stri
 	}
 
 	processed_template = processed_template.replace("_BASE_", p_base_class_name)
-								 .replace("_CLASS_", p_class_name)
+								 .replace("_CLASS_", p_class_name.to_pascal_case())
 								 .replace("_TS_", _get_indentation());
 	scr->set_source_code(processed_template);
 	return scr;
