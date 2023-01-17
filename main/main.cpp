a44ecfcf27884e23d3527bfaba2ef734bc12c3f6
Mikael Hermansson <mikael@hermansson.io>
2023-01-17 14:04:19 +0100

    Make physics servers `end_sync` on exit

--- main/main.cpp
+++ main/main.cpp
@@ -3030,6 +3030,9 @@ bool Main::iteration() {
 		PhysicsServer2D::get_singleton()->flush_queries();
 
 		if (OS::get_singleton()->get_main_loop()->physics_process(physics_step * time_scale)) {
+			PhysicsServer3D::get_singleton()->end_sync();
+			PhysicsServer2D::get_singleton()->end_sync();
+
 			exit = true;
 			break;
 		}
