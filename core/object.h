KleirRampage45/redot-engine

Add optional 'default' argument to get_meta()

core/object/object.h
@@ -761,7 +761,7 @@ public:
 	bool has_meta(const StringName &p_name) const;
 	void set_meta(const StringName &p_name, const Variant &p_value);
 	void remove_meta(const StringName &p_name);
-	Variant get_meta(const StringName &p_name) const;
+	Variant get_meta(const StringName &p_name, const Variant &p_default = Variant()) const;
 	void get_meta_list(List<StringName> *p_list) const;
 
 #ifdef TOOLS_ENABLED

godotengine/godot

(cherry picked from commit 3eb7fc4bfd376bc27ea3313ccd1da4679c48f4cb)

core/object.h
@@ -685,12 +685,10 @@ public:
 	void set_script(const RefPtr &p_script);
 	RefPtr get_script() const;
 
-	/* SCRIPT */
-
 	bool has_meta(const String &p_name) const;
 	void set_meta(const String &p_name, const Variant &p_value);
 	void remove_meta(const String &p_name);
-	Variant get_meta(const String &p_name) const;
+	Variant get_meta(const String &p_name, const Variant &p_default = Variant()) const;
 	void get_meta_list(List<String> *p_list) const;
 
 #ifdef TOOLS_ENABLED