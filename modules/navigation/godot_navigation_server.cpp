KleirRampage45/redot-engine

Add NavigationServer2D/3D API functions to find missing RID info
Utility functions for NavigationServer2D/3D to find missing RID information when working with Server API directly. e.g. from map to regions and agents, from agent or region to map, from region to map and agents and so on ....

Requirement to work with NavigationServer API exklusive without SceneTree nodes and when juggling agents and regions between multiple navigation maps.

modules/navigation/godot_navigation_server.cpp
@@ -233,6 +233,38 @@ RID GodotNavigationServer::map_get_closest_point_owner(RID p_map, const Vector3 
 	return map->get_closest_point_owner(p_point);
 }
 
+Array GodotNavigationServer::map_get_regions(RID p_map) const {
+	Array regions_rids;
+	const NavMap *map = map_owner.get_or_null(p_map);
+	ERR_FAIL_COND_V(map == nullptr, regions_rids);
+	for (NavRegion *region : map->get_regions()) {
+		regions_rids.push_back(region->get_self());
+	}
+	return regions_rids;
+}
+
+Array GodotNavigationServer::map_get_agents(RID p_map) const {
+	Array agents_rids;
+	const NavMap *map = map_owner.get_or_null(p_map);
+	ERR_FAIL_COND_V(map == nullptr, agents_rids);
+	for (RvoAgent *agent : map->get_agents()) {
+		agents_rids.push_back(agent->get_self());
+	}
+	return agents_rids;
+}
+
+RID GodotNavigationServer::region_get_map(RID p_region) const {
+	NavRegion *region = region_owner.get_or_null(p_region);
+	ERR_FAIL_COND_V(region == nullptr, RID());
+	return region->get_map()->get_self();
+}
+
+RID GodotNavigationServer::agent_get_map(RID p_agent) const {
+	RvoAgent *agent = agent_owner.get_or_null(p_agent);
+	ERR_FAIL_COND_V(agent == nullptr, RID());
+	return agent->get_map()->get_self();
+}
+
 RID GodotNavigationServer::region_create() const {
 	GodotNavigationServer *mut_this = const_cast<GodotNavigationServer *>(this);
 	MutexLock lock(mut_this->operations_mutex);

godotengine/godot

(cherry picked from commit 371054e3e5a20325200cc7ba7bbbce6b6a4588c3)

modules/navigation/godot_navigation_server.cpp
@@ -250,6 +250,38 @@ RID GodotNavigationServer::map_get_closest_point_owner(RID p_map, const Vector3 
 	return map->get_closest_point_owner(p_point);
 }
 
+Array GodotNavigationServer::map_get_regions(RID p_map) const {
+	Array regions_rids;
+	const NavMap *map = map_owner.getornull(p_map);
+	ERR_FAIL_COND_V(map == nullptr, regions_rids);
+	for (NavRegion *region : map->get_regions()) {
+		regions_rids.push_back(region->get_self());
+	}
+	return regions_rids;
+}
+
+Array GodotNavigationServer::map_get_agents(RID p_map) const {
+	Array agents_rids;
+	const NavMap *map = map_owner.getornull(p_map);
+	ERR_FAIL_COND_V(map == nullptr, agents_rids);
+	for (RvoAgent *agent : map->get_agents()) {
+		agents_rids.push_back(agent->get_self());
+	}
+	return agents_rids;
+}
+
+RID GodotNavigationServer::region_get_map(RID p_region) const {
+	NavRegion *region = region_owner.getornull(p_region);
+	ERR_FAIL_COND_V(region == nullptr, RID());
+	return region->get_map()->get_self();
+}
+
+RID GodotNavigationServer::agent_get_map(RID p_agent) const {
+	RvoAgent *agent = agent_owner.getornull(p_agent);
+	ERR_FAIL_COND_V(agent == nullptr, RID());
+	return agent->get_map()->get_self();
+}
+
 RID GodotNavigationServer::region_create() const {
 	auto mut_this = const_cast<GodotNavigationServer *>(this);
 	MutexLock lock(mut_this->operations_mutex);