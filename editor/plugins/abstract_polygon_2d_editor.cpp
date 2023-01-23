KleirRampage45/redot-engine

Fix axis snapping of CollisionPolygon2d's newly created vertex

editor/plugins/abstract_polygon_2d_editor.cpp
@@ -294,9 +294,9 @@ bool AbstractPolygon2DEditor::forward_gui_input(const Ref<InputEvent> &p_event) 
 							_commit_action();
 							return true;
 						} else {
-							pre_move_edit = vertices;
 							edited_point = PosVertex(insert.polygon, insert.vertex + 1, xform.affine_inverse().xform(insert.pos));
 							vertices.insert(edited_point.vertex, edited_point.pos);
+							pre_move_edit = vertices;
 							selected_point = Vertex(edited_point.polygon, edited_point.vertex);
 							edge_point = PosVertex();
 

godotengine/godot

(cherry picked from commit ba58491ca724bd660257bf4c5a3d76d70a15ad3e)

editor/plugins/abstract_polygon_2d_editor.cpp
@@ -319,9 +319,9 @@ bool AbstractPolygon2DEditor::forward_gui_input(const Ref<InputEvent> &p_event) 
 							return true;
 						} else {
 							Vector<Vector2> vertices2 = _get_polygon(insert.polygon);
-							pre_move_edit = vertices2;
 							edited_point = PosVertex(insert.polygon, insert.vertex + 1, xform.affine_inverse().xform(insert.pos));
 							vertices2.insert(edited_point.vertex, edited_point.pos);
+							pre_move_edit = vertices2;
 							selected_point = edited_point;
 							edge_point = PosVertex();
 