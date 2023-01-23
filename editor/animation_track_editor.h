KleirRampage45/redot-engine

Fix some Animation panel icons not updating after theme change

editor/animation_track_editor.h
@@ -182,6 +182,8 @@ class AnimationTrackEdit : public Control {
 	void _play_position_draw();
 	bool _is_value_key_valid(const Variant &p_key_value, Variant::Type &r_valid_type) const;
 
+	Ref<Texture2D> _get_key_type_icon() const;
+
 	mutable int dropping_at;
 	float insert_at_pos;
 	bool moving_selection_attempt;

editor/plugins/animation_player_editor_plugin.cpp
@@ -156,6 +156,8 @@ void AnimationPlayerEditor::_notification(int p_what) {
 			ITEM_ICON(TOOL_EDIT_TRANSITIONS, "Blend");
 			ITEM_ICON(TOOL_EDIT_RESOURCE, "Edit");
 			ITEM_ICON(TOOL_REMOVE_ANIM, "Remove");
+
+			_update_animation_list_icons();
 		} break;
 	}
 }
@@ -859,22 +861,13 @@ void AnimationPlayerEditor::_update_player() {
 
 	int active_idx = -1;
 	for (const StringName &E : animlist) {
-		Ref<Texture2D> icon;
-		if (E == player->get_autoplay()) {
-			if (E == SceneStringNames::get_singleton()->RESET) {
-				icon = autoplay_reset_icon;
-			} else {
-				icon = autoplay_icon;
-			}
-		} else if (E == SceneStringNames::get_singleton()->RESET) {
-			icon = reset_icon;
-		}
-		animation->add_icon_item(icon, E);
+		animation->add_item(E);
 
 		if (player->get_assigned_animation() == E) {
 			active_idx = animation->get_item_count() - 1;
 		}
 	}
+	_update_animation_list_icons();
 
 	updating = false;
 	if (active_idx != -1) {
@@ -901,6 +894,30 @@ void AnimationPlayerEditor::_update_player() {
 	}
 
 	_update_animation();
+}
+
+void AnimationPlayerEditor::_update_animation_list_icons() {
+	List<StringName> animlist;
+	if (player) {
+		player->get_animation_list(&animlist);
+	}
+
+	for (int i = 0; i < animation->get_item_count(); i++) {
+		String name = animation->get_item_text(i);
+
+		Ref<Texture2D> icon;
+		if (name == player->get_autoplay()) {
+			if (name == SceneStringNames::get_singleton()->RESET) {
+				icon = autoplay_reset_icon;
+			} else {
+				icon = autoplay_icon;
+			}
+		} else if (name == SceneStringNames::get_singleton()->RESET) {
+			icon = reset_icon;
+		}
+
+		animation->set_item_icon(i, icon);
+	}
 }
 
 void AnimationPlayerEditor::edit(AnimationPlayer *p_player) {

godotengine/godot

(cherry picked from commit 83828c7d1b1a7175421b34273ae236c61f621a7f)

editor/animation_track_editor.h
@@ -180,6 +180,8 @@ class AnimationTrackEdit : public Control {
 	void _play_position_draw();
 	bool _is_value_key_valid(const Variant &p_key_value, Variant::Type &r_valid_type) const;
 
+	Ref<Texture> _get_key_type_icon() const;
+
 	mutable int dropping_at;
 	float insert_at_pos;
 	bool moving_selection_attempt;

editor/plugins/animation_player_editor_plugin.cpp
@@ -152,6 +152,8 @@ void AnimationPlayerEditor::_notification(int p_what) {
 			ITEM_ICON(TOOL_EDIT_TRANSITIONS, "Blend");
 			ITEM_ICON(TOOL_EDIT_RESOURCE, "Edit");
 			ITEM_ICON(TOOL_REMOVE_ANIM, "Remove");
+
+			_update_animation_list_icons();
 		} break;
 	}
 }
@@ -860,22 +862,13 @@ void AnimationPlayerEditor::_update_player() {
 
 	int active_idx = -1;
 	for (List<StringName>::Element *E = animlist.front(); E; E = E->next()) {
-		Ref<Texture> icon;
-		if (E->get() == player->get_autoplay()) {
-			if (E->get() == "RESET") {
-				icon = autoplay_reset_icon;
-			} else {
-				icon = autoplay_icon;
-			}
-		} else if (E->get() == "RESET") {
-			icon = reset_icon;
-		}
-		animation->add_icon_item(icon, E->get());
+		animation->add_item(E->get());
 
 		if (player->get_assigned_animation() == E->get()) {
 			active_idx = animation->get_item_count() - 1;
 		}
 	}
+	_update_animation_list_icons();
 
 	updating = false;
 	if (active_idx != -1) {
@@ -902,6 +895,30 @@ void AnimationPlayerEditor::_update_player() {
 	}
 
 	_update_animation();
+}
+
+void AnimationPlayerEditor::_update_animation_list_icons() {
+	List<StringName> animlist;
+	if (player) {
+		player->get_animation_list(&animlist);
+	}
+
+	for (int i = 0; i < animation->get_item_count(); i++) {
+		String name = animation->get_item_text(i);
+
+		Ref<Texture> icon;
+		if (name == player->get_autoplay()) {
+			if (name == "RESET") {
+				icon = autoplay_reset_icon;
+			} else {
+				icon = autoplay_icon;
+			}
+		} else if (name == "RESET") {
+			icon = reset_icon;
+		}
+
+		animation->set_item_icon(i, icon);
+	}
 }
 
 void AnimationPlayerEditor::edit(AnimationPlayer *p_player) {