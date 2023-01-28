56a5f559aa113c8c8b46bf898bbed4c11dfbb616
Josh Jones <kilauea.jones@gmail.com>
2023-01-27 20:15:10 -0800

    Use Callable for Navigation Agent callbacks

--- servers/navigation_server_3d.h
+++ servers/navigation_server_3d.h
@@ -238,7 +238,7 @@ public:
 	virtual bool agent_is_map_changed(RID p_agent) const = 0;
 
 	/// Callback called at the end of the RVO process
-	virtual void agent_set_callback(RID p_agent, ObjectID p_object_id, StringName p_method, Variant p_udata = Variant()) = 0;
+	virtual void agent_set_callback(RID p_agent, Callable p_callback) = 0;
 
 	/// Destroy the `RID`
 	virtual void free(RID p_object) = 0;
