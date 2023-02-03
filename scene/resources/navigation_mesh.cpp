7caf08ec75519a237db2ec7777627fee8f594f4c
smix8 <52464204+smix8@users.noreply.github.com>
2023-02-03 11:54:13 +0100

    Fix NavigationMesh baking AABB Editor handling and visuals
    Fixes handling and visuals for Navigation Mesh baking AABB in the Editor.

--- scene/resources/navigation_mesh.cpp
+++ scene/resources/navigation_mesh.cpp
@@ -278,7 +278,7 @@ bool NavigationMesh::get_filter_walkable_low_height_spans() const {
 
 void NavigationMesh::set_filter_baking_aabb(const AABB &p_aabb) {
 	filter_baking_aabb = p_aabb;
-	notify_property_list_changed();
+	emit_changed();
 }
 
 AABB NavigationMesh::get_filter_baking_aabb() const {
@@ -287,7 +287,7 @@ AABB NavigationMesh::get_filter_baking_aabb() const {
 
 void NavigationMesh::set_filter_baking_aabb_offset(const Vector3 &p_aabb_offset) {
 	filter_baking_aabb_offset = p_aabb_offset;
-	notify_property_list_changed();
+	emit_changed();
 }
 
 Vector3 NavigationMesh::get_filter_baking_aabb_offset() const {
