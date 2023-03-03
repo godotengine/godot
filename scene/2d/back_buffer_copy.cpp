7688d3a9fdd99c3ab1460eb54298808d25b7da0e
Haoyu Qiu <timothyqiu32@gmail.com>
2023-03-03 17:57:58 +0800

    Notify child controls when BackBufferCopy's rect changed

--- scene/2d/back_buffer_copy.cpp
+++ scene/2d/back_buffer_copy.cpp
@@ -62,6 +62,7 @@ Rect2 BackBufferCopy::get_anchorable_rect() const {
 void BackBufferCopy::set_rect(const Rect2 &p_rect) {
 	rect = p_rect;
 	_update_copy_mode();
+	item_rect_changed();
 }
 
 Rect2 BackBufferCopy::get_rect() const {
