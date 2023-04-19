ab603e347f057737fdc0bdf47e2437ad32924cad
clayjohn <claynjohn@gmail.com>
2023-04-18 17:26:17 -0700

    Clamp normal when calculating 2D lighting to avoid artifacts

--- drivers/gles3/shaders/canvas.glsl
+++ drivers/gles3/shaders/canvas.glsl
@@ -589,7 +589,7 @@ void main() {
 		if (bool(read_draw_data_flags & FLAGS_FLIP_V)) {
 			normal.y = -normal.y;
 		}
-		normal.z = sqrt(1.0 - dot(normal.xy, normal.xy));
+		normal.z = sqrt(max(0.0, 1.0 - dot(normal.xy, normal.xy)));
 		normal_used = true;
 	} else {
 		normal = vec3(0.0, 0.0, 1.0);
