3e8692eeb0e811265e2bb8bc10347e1e1ff8eb1e
Dylan Murphey <dylanpmurphey@gmail.com>
2023-04-26 16:36:11 -0600

    remove extra semicolon in gdscript_editor.cpp

--- modules/gdscript/gdscript_editor.cpp
+++ modules/gdscript/gdscript_editor.cpp
@@ -2474,7 +2474,7 @@ static void _find_call_arguments(GDScriptParser::CompletionContext &p_context, c
 	GDScriptParser::DataType base_type = p_base.type;
 
 	const String quote_style = EDITOR_GET("text_editor/completion/use_single_quotes") ? "'" : "\"";
-	const bool is_external_editor = EDITOR_GET("text_editor/external/use_external_editor");;
+	const bool is_external_editor = EDITOR_GET("text_editor/external/use_external_editor");
 
 	while (base_type.is_set() && !base_type.is_variant()) {
 		switch (base_type.kind) {
@@ -2511,8 +2511,8 @@ static void _find_call_arguments(GDScriptParser::CompletionContext &p_context, c
 								if (opt.is_quoted()) {
 									opt = opt.unquote();
 								}
-								
-								if(!is_external_editor) {
+
+								if (!is_external_editor) {
 									opt = opt.quote(quote_style);
 								}
 
