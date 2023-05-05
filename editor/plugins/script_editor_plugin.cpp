d56c4f960cb5f386ba3561bbf3cf7ff98c7a6d9e
kobewi <kobewi4e@gmail.com>
2023-05-05 22:26:19 +0200

    Remove erroneous signal connection
    (cherry picked from commit 88ea3dd4db53cf817df10cec12d7b0c111e723c3)

--- editor/plugins/script_editor_plugin.cpp
+++ editor/plugins/script_editor_plugin.cpp
@@ -4090,8 +4090,6 @@ ScriptEditor::ScriptEditor() {
 	Ref<EditorJSONSyntaxHighlighter> json_syntax_highlighter;
 	json_syntax_highlighter.instantiate();
 	register_syntax_highlighter(json_syntax_highlighter);
-
-	EditorNode::get_singleton()->connect("scene_closed", callable_mp(this, &ScriptEditor::_close_builtin_scripts_from_scene));
 }
 
 ScriptEditor::~ScriptEditor() {
