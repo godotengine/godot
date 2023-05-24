66a8418a2d6d35ee0631cb096ce42872d6a2278a
smix8 <52464204+smix8@users.noreply.github.com>
2023-05-24 23:57:08 +0200

    Remove unnecessary Clipper lib includes
    Removes unnecessary Clipper lib includes.

--- core/math/geometry_3d.cpp
+++ core/math/geometry_3d.cpp
@@ -30,7 +30,6 @@
 
 #include "geometry_3d.h"
 
-#include "thirdparty/misc/clipper.hpp"
 #include "thirdparty/misc/polypartition.h"
 
 void Geometry3D::get_closest_points_between_segments(const Vector3 &p_p0, const Vector3 &p_p1, const Vector3 &p_q0, const Vector3 &p_q1, Vector3 &r_ps, Vector3 &r_qt) {
