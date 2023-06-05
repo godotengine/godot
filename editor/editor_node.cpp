b616b70baca0b98f09d12832cd05fdd588acfddf
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-06-05 17:06:30 +0200

    Don't create a new scene when double-clicking empty space in scene tabs
    This behavior was often accidentally triggered when using the scroll
    tab arrow buttons. Web browsers also lack equivalent behavior, which
    means that it's not an UX pattern that people often rely upon.
    
    If you need to create a scene quickly from the scene tabs, a "+"
    button remains visible at the right of the last scene tab.

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -5582,11 +5582,8 @@ void EditorNode::_scene_tab_input(const Ref<InputEvent> &p_input) {
 			if (mb->get_button_index() == MouseButton::MIDDLE && mb->is_pressed()) {
 				_scene_tab_closed(scene_tabs->get_hovered_tab());
 			}
-		} else {
-			if (mb->get_button_index() == MouseButton::LEFT && mb->is_double_click()) {
-				_menu_option_confirm(FILE_NEW_SCENE, true);
-			}
 		}
+
 		if (mb->get_button_index() == MouseButton::RIGHT && mb->is_pressed()) {
 			// Context menu.
 			scene_tabs_context_menu->clear();
