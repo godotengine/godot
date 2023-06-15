6fc09121378d78026f799a820ce689f2d30bb0c9
Rémi Verschelde <rverschelde@gmail.com>
2023-06-15 13:41:52 +0200

    minimp3: Add a .cpp file to simplify building the single-header implementation
    Single-header libraries like this require passing a bunch of defines _once_
    before including the header, but not multiple times. This can make it tricky
    in user code to know when to request the implementation, if the header needs
    to be included in multiple files.
    
    So properly compiling a .cpp file for the implementation solves this, and
    also enables us to properly disable warnings on thirdparty implementation code.

--- modules/minimp3/audio_stream_mp3.cpp
+++ modules/minimp3/audio_stream_mp3.cpp
@@ -28,11 +28,6 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-#define MINIMP3_ONLY_MP3
-#define MINIMP3_FLOAT_OUTPUT
-#define MINIMP3_IMPLEMENTATION
-#define MINIMP3_NO_STDIO
-
 #include "audio_stream_mp3.h"
 
 #include "core/io/file_access.h"
---
+++ thirdparty/minimp3/minimp3_ex.cpp
@@ -0,0 +1,5 @@
+#define MINIMP3_IMPLEMENTATION
+#define MINIMP3_FLOAT_OUTPUT
+#define MINIMP3_ONLY_MP3
+#define MINIMP3_NO_STDIO
+#include "minimp3_ex.h"
