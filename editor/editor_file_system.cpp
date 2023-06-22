fc6a6dd0f4a32fdd4261be2981c2e74d4ec4c208
NiskashY <roman.pantus@mail.ru>
2023-06-22 17:33:13 +0300

    Fix reimporting files with non lowercase name extension

--- editor/editor_file_system.cpp
+++ editor/editor_file_system.cpp
@@ -544,7 +544,7 @@ bool EditorFileSystem::_scan_import_support(Vector<String> reimports) {
 	}
 
 	for (int i = 0; i < reimports.size(); i++) {
-		HashMap<String, int>::Iterator E = import_support_test.find(reimports[i].get_extension());
+		HashMap<String, int>::Iterator E = import_support_test.find(reimports[i].get_extension().to_lower());
 		if (E) {
 			import_support_tested.write[E->value] = true;
 		}
