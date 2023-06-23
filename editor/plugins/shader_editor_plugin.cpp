44bec97d5ea354458c1a48f3d7a03263b05fa8c1
kobewi <kobewi4e@gmail.com>
2023-06-23 12:21:56 +0200

    Unify window title format

--- editor/plugins/shader_editor_plugin.cpp
+++ editor/plugins/shader_editor_plugin.cpp
@@ -559,7 +559,7 @@ void ShaderEditorPlugin::_notification(int p_what) {
 
 ShaderEditorPlugin::ShaderEditorPlugin() {
 	window_wrapper = memnew(WindowWrapper);
-	window_wrapper->set_window_title(TTR("Shader Editor - Godot Engine"));
+	window_wrapper->set_window_title(vformat(TTR("%s - Godot Engine"), TTR("Shader Editor")));
 	window_wrapper->set_margins_enabled(true);
 
 	main_split = memnew(HSplitContainer);
