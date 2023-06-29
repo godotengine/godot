35ed7c770bd7616bdd43b32c1ea433d574452de5
clayjohn <claynjohn@gmail.com>
2023-06-29 13:24:40 -0700

    Initialize particles instance buffer in case it is used before being updated

--- servers/rendering/renderer_rd/storage_rd/particles_storage.cpp
+++ servers/rendering/renderer_rd/storage_rd/particles_storage.cpp
@@ -1288,7 +1288,10 @@ void ParticlesStorage::_particles_update_buffers(Particles *particles) {
 
 		particles->userdata_count = userdata_count;
 
-		particles->particle_instance_buffer = RD::get_singleton()->storage_buffer_create(sizeof(float) * 4 * (xform_size + 1 + 1) * total_amount);
+		PackedByteArray data;
+		data.resize_zeroed(sizeof(float) * 4 * (xform_size + 1 + 1) * total_amount);
+
+		particles->particle_instance_buffer = RD::get_singleton()->storage_buffer_create(sizeof(float) * 4 * (xform_size + 1 + 1) * total_amount, data);
 		//needs to clear it
 
 		{
