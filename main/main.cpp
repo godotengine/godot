54a7620e99dbb09c8ddf420f464900c19ff19bf0
Kais Wass <kwassilima@gmail.com>
2023-07-09 01:44:22 +0200

    fix non-conforming reference to a non-static 'Engine::_print_header' member

--- main/main.cpp
+++ main/main.cpp
@@ -1613,7 +1613,7 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 		quiet_stdout = true;
 	}
 	if(bool(GLOBAL_GET("application/run/quit_header"))){
-		Engine::_print_header = true;
+		Engine::get_singleton()->_print_header = true;
 	}
 
 	if (bool(GLOBAL_GET("application/run/disable_stderr"))) {
