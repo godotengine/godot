d3ca91ad6ae51460d5b7008cdc81a50f26a0544c
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-07-17 12:12:24 +0300

    [Windows] Implement native file selection dialog support.

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -511,6 +511,8 @@ public:
 	virtual bool is_dark_mode() const override;
 	virtual Color get_accent_color() const override;
 
+	virtual Error file_dialog_show(const String &p_title, const String &p_current_directory, const String &p_filename, bool p_show_hidden, FileDialogMode p_mode, const Vector<String> &p_filters, const Callable &p_callback) override;
+
 	virtual void mouse_set_mode(MouseMode p_mode) override;
 	virtual MouseMode mouse_get_mode() const override;
 
