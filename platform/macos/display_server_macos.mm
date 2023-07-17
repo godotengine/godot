4e06fcff8244b69a4491b60453556e9ba1d77714
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-07-17 10:29:49 +0300

    [macOS] Fix uncapped frame rate for windows in the non-active workspaces.

--- platform/macos/display_server_macos.mm
+++ platform/macos/display_server_macos.mm
@@ -3256,14 +3256,14 @@ bool DisplayServerMacOS::window_is_focused(WindowID p_window) const {
 }
 
 bool DisplayServerMacOS::window_can_draw(WindowID p_window) const {
-	return window_get_mode(p_window) != WINDOW_MODE_MINIMIZED;
+	return (window_get_mode(p_window) != WINDOW_MODE_MINIMIZED) && [windows[p_window].window_object isOnActiveSpace];
 }
 
 bool DisplayServerMacOS::can_any_window_draw() const {
 	_THREAD_SAFE_METHOD_
 
 	for (const KeyValue<WindowID, WindowData> &E : windows) {
-		if (window_get_mode(E.key) != WINDOW_MODE_MINIMIZED) {
+		if ((window_get_mode(E.key) != WINDOW_MODE_MINIMIZED) && [E.value.window_object isOnActiveSpace]) {
 			return true;
 		}
 	}
