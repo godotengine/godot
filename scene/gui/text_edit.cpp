11e5726c5dfbe2f3010fe8a49a524bbee004a88b
Hugo Locurcio <hugo.locurcio@hugo.pro>
2023-07-25 03:13:07 +0200

    Add theme sound items and playback on common UI events
    This allows themes to play sounds when hovering/pressing buttons,
    typing text and using sliders.
    
    A new "audio" theme item type is added to allow custom controls
    to define their own audio streams, as well as benefit from the theme
    override system.
    
    Playback is handled by adding internal nodes at the top of the SceneTree,
    and is not affected by pause or scene changes. Nodes are automatically freed
    once playback is done. Polyphony is implicitly supported by creating a node
    for each audio playback event.

--- scene/gui/text_edit.cpp
+++ scene/gui/text_edit.cpp
@@ -2609,6 +2609,7 @@ void TextEdit::gui_input(const Ref<InputEvent> &p_gui_input) {
 					_push_current_op();
 					set_caret_line(line, false, true, -1, caret);
 					set_caret_column(col, false, caret);
+					play_theme_sound(theme_cache.caret_moved_sound);
 					selection_drag_attempt = false;
 					bool caret_moved = get_caret_column(caret) != prev_col || get_caret_line(caret) != prev_line;
 
@@ -3444,6 +3445,7 @@ void TextEdit::_move_caret_left(bool p_select, bool p_move_by_word) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_right(bool p_select, bool p_move_by_word) {
@@ -3494,6 +3496,7 @@ void TextEdit::_move_caret_right(bool p_select, bool p_move_by_word) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_up(bool p_select) {
@@ -3520,6 +3523,7 @@ void TextEdit::_move_caret_up(bool p_select) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_down(bool p_select) {
@@ -3542,6 +3546,7 @@ void TextEdit::_move_caret_down(bool p_select) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_to_line_start(bool p_select) {
@@ -3573,6 +3578,7 @@ void TextEdit::_move_caret_to_line_start(bool p_select) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_to_line_end(bool p_select) {
@@ -3598,6 +3604,7 @@ void TextEdit::_move_caret_to_line_end(bool p_select) {
 		}
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_page_up(bool p_select) {
@@ -3614,6 +3621,7 @@ void TextEdit::_move_caret_page_up(bool p_select) {
 		set_caret_line(n_line, i == 0, false, next_line.y, i);
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_page_down(bool p_select) {
@@ -3630,6 +3638,7 @@ void TextEdit::_move_caret_page_down(bool p_select) {
 		set_caret_line(n_line, i == 0, false, next_line.y, i);
 	}
 	merge_overlapping_carets();
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_do_backspace(bool p_word, bool p_all_to_left) {
@@ -3649,6 +3658,7 @@ void TextEdit::_do_backspace(bool p_word, bool p_all_to_left) {
 		}
 
 		if (get_caret_column(caret_index) == 0 && get_caret_line(caret_index) == 0 && !has_selection(caret_index)) {
+			play_theme_sound(theme_cache.text_change_rejected_sound);
 			continue;
 		}
 
@@ -3790,6 +3800,7 @@ void TextEdit::_move_caret_document_start(bool p_select) {
 
 	set_caret_line(0, false, true, -1);
 	set_caret_column(0);
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 void TextEdit::_move_caret_document_end(bool p_select) {
@@ -3802,6 +3813,7 @@ void TextEdit::_move_caret_document_end(bool p_select) {
 
 	set_caret_line(get_last_unhidden_line(), true, false, -1);
 	set_caret_column(text[get_caret_line()].length());
+	play_theme_sound(theme_cache.caret_moved_sound);
 }
 
 bool TextEdit::_clear_carets_and_selection() {
@@ -8296,6 +8308,11 @@ void TextEdit::_bind_methods() {
 	BIND_THEME_ITEM(Theme::DATA_TYPE_COLOR, TextEdit, current_line_color);
 	BIND_THEME_ITEM(Theme::DATA_TYPE_COLOR, TextEdit, word_highlighted_color);
 
+	BIND_THEME_ITEM(Theme::DATA_TYPE_SOUND, TextEdit, focus_sound);
+	BIND_THEME_ITEM(Theme::DATA_TYPE_SOUND, TextEdit, caret_moved_sound);
+	BIND_THEME_ITEM(Theme::DATA_TYPE_SOUND, TextEdit, text_changed_sound);
+	BIND_THEME_ITEM(Theme::DATA_TYPE_SOUND, TextEdit, text_change_rejected_sound);
+
 	/* Settings. */
 	GLOBAL_DEF(PropertyInfo(Variant::FLOAT, "gui/timers/text_edit_idle_detect_sec", PROPERTY_HINT_RANGE, "0,10,0.01,or_greater"), 3);
 	GLOBAL_DEF(PropertyInfo(Variant::INT, "gui/common/text_edit_undo_stack_max_size", PROPERTY_HINT_RANGE, "0,10000,1,or_greater"), 1024);
@@ -9784,6 +9801,8 @@ void TextEdit::_text_changed() {
 }
 
 void TextEdit::_emit_text_changed() {
+	play_theme_sound(theme_cache.text_changed_sound);
+
 	emit_signal(SceneStringName(text_changed));
 	text_changed_dirty = false;
 }
