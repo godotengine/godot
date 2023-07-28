3871a2a7af7caa98bca9bcd7ef6309cb30288894
BooksBaum <15612932+Booksbaum@users.noreply.github.com>
2023-07-28 18:21:04 +0200

    Space after `//`

--- modules/gdscript/language_server/gdscript_extend_parser.cpp
+++ modules/gdscript/language_server/gdscript_extend_parser.cpp
@@ -623,7 +623,7 @@ String ExtendGDScriptParser::get_text_for_completion(const lsp::Position &p_curs
 	for (int i = 0; i < len; i++) {
 		if (i == p_cursor.line) {
 			longthing += lines[i].substr(0, p_cursor.character);
-			longthing += String::chr(0xFFFF); //Not unicode, represents the cursor.
+			longthing += String::chr(0xFFFF); // Not unicode, represents the cursor.
 			longthing += lines[i].substr(p_cursor.character, lines[i].size());
 		} else {
 			longthing += lines[i];
@@ -658,7 +658,7 @@ String ExtendGDScriptParser::get_text_for_lookup_symbol(const lsp::Position &p_c
 			}
 
 			longthing += first_part;
-			longthing += String::chr(0xFFFF); //Not unicode, represents the cursor.
+			longthing += String::chr(0xFFFF); // Not unicode, represents the cursor.
 			if (p_func_required) {
 				longthing += "("; // Tell the parser this is a function call.
 			}
