a0a5cbad57be88181da11a80c594fcea2c4abadb
Yuri Sizov <yuris@humnom.net>
2023-08-02 14:25:50 +0200

    Revert "Fix get_bone_pose_global_no_override()"
    This reverts commit 9307d851a94ac9fc75372ffa8c047a2193401d8d.

--- scene/3d/skeleton_3d.cpp
+++ scene/3d/skeleton_3d.cpp
@@ -922,18 +922,18 @@ void Skeleton3D::force_update_bone_children_transforms(int p_bone_idx) {
 
 			if (b.parent >= 0) {
 				b.pose_global = bonesptr[b.parent].pose_global * pose;
-				b.pose_global_no_override = bonesptr[b.parent].pose_global_no_override * pose;
+				b.pose_global_no_override = b.pose_global;
 			} else {
 				b.pose_global = pose;
-				b.pose_global_no_override = pose;
+				b.pose_global_no_override = b.pose_global;
 			}
 		} else {
 			if (b.parent >= 0) {
 				b.pose_global = bonesptr[b.parent].pose_global * b.rest;
-				b.pose_global_no_override = bonesptr[b.parent].pose_global_no_override * b.rest;
+				b.pose_global_no_override = b.pose_global;
 			} else {
 				b.pose_global = b.rest;
-				b.pose_global_no_override = b.rest;
+				b.pose_global_no_override = b.pose_global;
 			}
 		}
 		if (rest_dirty) {
