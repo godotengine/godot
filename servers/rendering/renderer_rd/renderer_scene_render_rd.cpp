aae20e5b10782530c936f064bdc28c7e338b1692
Rémi Verschelde <rverschelde@gmail.com>
2023-08-07 12:59:23 +0200

    Fix various typos with codespell
    Also includes typo fixes from #79993, #80068, #80276, and #80303.
    
    Co-authored-by: betalars <contact@betalars.de>
    Co-authored-by: spaceyjase <429978+spaceyjase@users.noreply.github.com>
    Co-authored-by: Swarkin <102416174+Swarkin@users.noreply.github.com>
    Co-authored-by: Raul Santos <raulsntos@gmail.com>
    
    (cherry picked from commit faaf27f28492650cf8bfb71328ab21ab934d0dd7)

--- servers/rendering/renderer_rd/renderer_scene_render_rd.cpp
+++ servers/rendering/renderer_rd/renderer_scene_render_rd.cpp
@@ -368,7 +368,7 @@ void RendererSceneRenderRD::_render_buffers_post_process_and_tonemap(const Rende
 				buffers.base_texture = rb->get_internal_texture(i);
 				buffers.depth_texture = rb->get_depth_texture(i);
 
-				// In stereo p_render_data->z_near and p_render_data->z_far can be offset for our combined frustrum
+				// In stereo p_render_data->z_near and p_render_data->z_far can be offset for our combined frustum.
 				float z_near = p_render_data->scene_data->view_projection[i].get_z_near();
 				float z_far = p_render_data->scene_data->view_projection[i].get_z_far();
 				bokeh_dof->bokeh_dof_compute(buffers, p_render_data->camera_attributes, z_near, z_far, p_render_data->scene_data->cam_orthogonal);
@@ -391,7 +391,7 @@ void RendererSceneRenderRD::_render_buffers_post_process_and_tonemap(const Rende
 				buffers.depth_texture = rb->get_depth_texture(i);
 				buffers.base_fb = FramebufferCacheRD::get_singleton()->get_cache(buffers.base_texture); // TODO move this into bokeh_dof_raster, we can do this internally
 
-				// In stereo p_render_data->z_near and p_render_data->z_far can be offset for our combined frustrum
+				// In stereo p_render_data->z_near and p_render_data->z_far can be offset for our combined frustum.
 				float z_near = p_render_data->scene_data->view_projection[i].get_z_near();
 				float z_far = p_render_data->scene_data->view_projection[i].get_z_far();
 				bokeh_dof->bokeh_dof_raster(buffers, p_render_data->camera_attributes, z_near, z_far, p_render_data->scene_data->cam_orthogonal);
