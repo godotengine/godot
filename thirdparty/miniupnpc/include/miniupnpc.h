a658a42119ac5aac0f30dcf77e87b86266a61202
Rémi Verschelde <rverschelde@gmail.com>
2023-08-07 17:50:40 +0200

    miniupnpc: Update to version 2.2.5
    No change for the files we ship, so just bumping the version number.
    
    (cherry picked from commit 74f21cb6666084a3cec8f2238c003411ffcfd9da)

--- thirdparty/miniupnpc/include/miniupnpc.h
+++ thirdparty/miniupnpc/include/miniupnpc.h
@@ -1,4 +1,4 @@
-/* $Id: miniupnpc.h,v 1.61 2022/10/21 21:15:02 nanard Exp $ */
+/* $Id: miniupnpc.h,v 1.62 2023/06/11 23:25:46 nanard Exp $ */
 /* vim: tabstop=4 shiftwidth=4 noexpandtab
  * Project: miniupnp
  * http://miniupnp.free.fr/ or https://miniupnp.tuxfamily.org/
@@ -20,7 +20,7 @@
 #define UPNPDISCOVER_MEMORY_ERROR (-102)
 
 /* versions : */
-#define MINIUPNPC_VERSION	"2.2.4"
+#define MINIUPNPC_VERSION	"2.2.5"
 #define MINIUPNPC_API_VERSION	17
 
 /* Source port:
