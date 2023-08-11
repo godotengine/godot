638d5dbf48aed586211dbea212ee4eebaa8773b6
Pavel Kraynyukhov <pkpro@users.noreply.github.com>
2023-08-11 12:56:25 +0200

    Style fix
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- core/io/marshalls.h
+++ core/io/marshalls.h
@@ -217,7 +217,7 @@ public:
 Error decode_variant(Variant &r_variant, const uint8_t *p_buffer, int p_len, int *r_len = nullptr, bool p_allow_objects = false, int p_depth = 0);
 Error encode_variant(const Variant &p_variant, uint8_t *r_buffer, int &r_len, bool p_full_objects = false, int p_depth = 0);
 
-Error to_raw_bytes(const Variant &p_variant, PackedByteArray &out_sink);
+Error to_raw_bytes(const Variant &p_variant, PackedByteArray &r_out_sink);
 
 Vector<float> vector3_to_float32_array(const Vector3 *vecs, size_t count);
 
