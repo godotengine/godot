e21c30ec11ed6ac90cc0a2a915879a850922a43e
kobewi <kobewi4e@gmail.com>
2023-08-22 16:29:00 +0200

    Unfocus LineEdit when pressing Escape

--- scene/gui/line_edit.cpp
+++ scene/gui/line_edit.cpp
@@ -482,6 +482,11 @@ void LineEdit::gui_input(const Ref<InputEvent> &p_event) {
 			return;
 		}
 
+		if (k->is_action("ui_cancel")) {
+			release_focus();
+			return;
+		}
+
 		if (is_shortcut_keys_enabled()) {
 			if (k->is_action("ui_copy", true)) {
 				copy_text();
