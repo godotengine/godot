cd0aaab48c789cbca3e6ddfa5e0daf1e4cb6b0db
kobewi <kobewi4e@gmail.com>
2023-08-24 22:39:40 +0200

    Cleanup some GLOBAL_DEFs

--- editor/import/editor_import_collada.cpp
+++ editor/import/editor_import_collada.cpp
@@ -208,7 +208,7 @@ Error ColladaImport::_create_scene(Collada::Node *p_node, Node3D *p_parent) {
 						return OK; //do nothing not needed
 					}
 
-					if (!bool(GLOBAL_DEF("collada/use_ambient", false))) {
+					if (!bool(GLOBAL_GET("collada/use_ambient"))) {
 						return OK;
 					}
 					//well, it's an ambient light..
