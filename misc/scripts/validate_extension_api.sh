e36bd595fa2f861ddc4766c5e2cf72aecd6d9a49
RedworkDE <10944644+RedworkDE@users.noreply.github.com>
2023-09-05 14:09:52 +0200

    CI: Compat checks: Make fetching the reference API more robust

--- misc/scripts/validate_extension_api.sh
+++ misc/scripts/validate_extension_api.sh
@@ -58,7 +58,7 @@ while read -r file; do
     get_expected_output "$file"
 
     # Download the reference extension_api.json
-    wget -qcO "$reference_file" "https://raw.githubusercontent.com/godotengine/godot-cpp/godot-$reference_tag/gdextension/extension_api.json"
+    wget -nv --retry-on-http-error=503 --tries=5 --timeout=60 -cO "$reference_file" "https://raw.githubusercontent.com/godotengine/godot-cpp/godot-$reference_tag/gdextension/extension_api.json" || has_problems=1
     # Validate the current API against the reference
     "$1" --headless --validate-extension-api "$reference_file" 2>&1 | tee "$validate" | awk '!/^Validate extension JSON:/' - || true
     # Collect the expected and actual validation errors
