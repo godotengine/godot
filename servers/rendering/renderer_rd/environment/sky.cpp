9b91750fb1cecea72fbd8ee155d3ad22754917c1
Dario <dariosamo@gmail.com>
2023-09-05 11:41:57 -0300

    Fix mipmap bias behavior by refactoring how samplers are created by Material Storage.
    Introduces a new structure to store samplers created with certain parameters instead of storing a 'custom' set of samplers. Allows viewports to correctly configure the mipmap bias and use it when rendering the scene.

--- servers/rendering/renderer_rd/environment/sky.cpp
+++ servers/rendering/renderer_rd/environment/sky.cpp
@@ -874,7 +874,7 @@ void sky() {
 			uniforms.push_back(u);
 		}
 
-		uniforms.append_array(material_storage->get_default_sampler_uniforms(SAMPLERS_BINDING_FIRST_INDEX));
+		uniforms.append_array(material_storage->samplers_rd_get_default().get_uniforms(SAMPLERS_BINDING_FIRST_INDEX));
 
 		sky_scene_state.uniform_set = RD::get_singleton()->uniform_set_create(uniforms, sky_shader.default_shader_rd, SKY_SET_UNIFORMS);
 	}
