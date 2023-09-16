367d6006543301812fe8d25db1bb42b4d3d3eada
CaTaTo <vahnenko2003@gmail.com>
2023-09-16 21:39:32 +0300

    add selection vulkan gpu in project settings

--- main/main.cpp
+++ main/main.cpp
@@ -1679,6 +1679,9 @@ Error Main::setup(const char *execpath, int argc, char *argv[], bool p_second_ph
 		driver_hints = "vulkan";
 #endif
 
+		//Set default Vulkan gpu to run, -1 will be default
+		GLOBAL_DEF("rendering/rendering_device/gpu_index", -1);
+
 		String default_driver = driver_hints.get_slice(",", 0);
 
 		// For now everything defaults to vulkan when available. This can change in future updates.
