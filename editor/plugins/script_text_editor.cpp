16efa7f5f67bece058db38f7aab17c0e3a063eb1
eterlan <703246769@qq.com>
2023-09-27 02:29:29 +0800

    add Mac shortcut override as CMD + M

--- editor/plugins/script_text_editor.cpp
+++ editor/plugins/script_text_editor.cpp
@@ -2427,6 +2427,7 @@ void ScriptTextEditor::register_editor() {
 	ED_SHORTCUT_OVERRIDE("script_text_editor/contextual_help", "macos", KeyModifierMask::ALT | KeyModifierMask::SHIFT | Key::SPACE);
 
 	ED_SHORTCUT("script_text_editor/lookup_symbol", TTR("Lookup Symbol"), Key::F12);
+	ED_SHORTCUT_OVERRIDE("script_text_editor/lookup_symbol", "macos", KeyModifierMask::CMD_OR_CTRL | Key::M);
 
 	ED_SHORTCUT("script_text_editor/toggle_bookmark", TTR("Toggle Bookmark"), KeyModifierMask::CMD_OR_CTRL | KeyModifierMask::ALT | Key::B);
 	ED_SHORTCUT("script_text_editor/goto_next_bookmark", TTR("Go to Next Bookmark"), KeyModifierMask::CMD_OR_CTRL | Key::B);
