9308d20d1b33d96b1ec59080ef83dbfd3282ecb6
bruvzg <7645683+bruvzg@users.noreply.github.com>
2023-09-27 21:15:17 +0300

    [iOS] Fix build with Xcode 15.
    (cherry picked from commit e1c7104e46fd3d2f97d365cb4b816b9c78e9e5df)

--- platform/ios/godot_view.mm
+++ platform/ios/godot_view.mm
@@ -82,10 +82,10 @@ static const float earth_gravity = 9.80665;
 		layer = [GodotMetalLayer layer];
 #endif
 	} else if ([driverName isEqualToString:@"opengl3"]) {
-		if (@available(iOS 13, *)) {
-			NSLog(@"OpenGL ES is deprecated on iOS 13");
-		}
+#pragma clang diagnostic push
+#pragma clang diagnostic ignored "-Wdeprecated-declarations" // OpenGL is deprecated in iOS 12.0
 		layer = [GodotOpenGLLayer layer];
+#pragma clang diagnostic pop
 	} else {
 		return nil;
 	}
