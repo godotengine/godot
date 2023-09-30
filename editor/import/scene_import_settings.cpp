17364e20b5f4c0651e02e62f6fff9136ed0dbee0
Shnazzy <22860318+Shnazzy@users.noreply.github.com>
2023-09-29 23:02:22 -0400

    Fixed the Advanced Import Settings window's camera rotation order in the 3D viewport when moving the mouse.

--- editor/import/scene_import_settings.cpp
+++ editor/import/scene_import_settings.cpp
@@ -550,7 +550,7 @@ void SceneImportSettings::_update_camera() {
 	camera->set_orthogonal(camera_size * zoom, 0.0001, camera_size * 2);
 
 	Transform3D xf;
-	xf.basis = Basis(Vector3(1, 0, 0), rot_x) * Basis(Vector3(0, 1, 0), rot_y);
+	xf.basis = Basis(Vector3(0, 1, 0), rot_y) * Basis(Vector3(1, 0, 0), rot_x);
 	xf.origin = center;
 	xf.translate_local(0, 0, camera_size);
 
