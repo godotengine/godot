644a2192b328b8fab255e9a25e6a4e95b666cee1
Stronkkey <Ail45000@proton.me>
2023-10-01 17:53:09 +0200

    Update editor/find_in_files.cpp
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- editor/find_in_files.cpp
+++ editor/find_in_files.cpp
@@ -46,7 +46,6 @@
 #include "scene/gui/line_edit.h"
 #include "scene/gui/progress_bar.h"
 #include "scene/gui/tree.h"
-#include "scene/resources/texture.h"
 
 const char *FindInFiles::SIGNAL_RESULT_FOUND = "result_found";
 const char *FindInFiles::SIGNAL_FINISHED = "finished";
