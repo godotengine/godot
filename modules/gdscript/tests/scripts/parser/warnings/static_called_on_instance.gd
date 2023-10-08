86cd191edb826952ff688994d84815a876c0743b
Saif Kandil <74428638+k0T0z@users.noreply.github.com>
2023-10-08 19:57:33 +0300

    improving static_called_on_instance.gd based on #82999
    Signed-off-by: Saif Kandil <74428638+k0T0z@users.noreply.github.com>

--- modules/gdscript/tests/scripts/parser/warnings/static_called_on_instance.gd
+++ modules/gdscript/tests/scripts/parser/warnings/static_called_on_instance.gd
@@ -1,6 +1,12 @@
 class Player:
 	var x = 3
 
+static func box(x : int, y : int, z : int, x_max : int, y_max : int) -> int:
+	return (z * x_max * y_max) + (y * x_max) + x
+	
+static func boxv(v : Vector3i, x_max : int, y_max : int) -> int:
+	return box(v.x, v.y, v.z, x_max, y_max)
+
 func test():
 	# These should not emit a warning.
 	var _player = Player.new()
@@ -9,3 +15,6 @@ func test():
 	# This should emit a warning.
 	var some_string = String()
 	print(some_string.num_uint64(8589934592)) # 2 ^ 33
+
+	# These should not emit a warning.
+	Globals.boxv(Vector3i(0, 0, 0), 10, 10)
