fd582dca62e33a9f65592072f3cab02fa3c73b11
Aaron Franke <arnfranke@yahoo.com>
2023-10-10 22:00:15 -0500

    Move 2D-only navigation code to its own subfolder

--- modules/navigation/register_types.cpp
+++ modules/navigation/register_types.cpp
@@ -30,8 +30,8 @@
 
 #include "register_types.h"
 
+#include "2d/godot_navigation_server_2d.h"
 #include "godot_navigation_server.h"
-#include "godot_navigation_server_2d.h"
 
 #ifndef DISABLE_DEPRECATED
 #ifndef _3D_DISABLED
