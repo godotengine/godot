f8e10a2610329114b4cf9770d590cb9ea77c013e
Patchcoat <spiencewelch@gmail.com>
2023-10-18 23:49:05 -0500

    android keystore username and password are checked on export

--- platform/android/export/export_plugin.h
+++ platform/android/export/export_plugin.h
@@ -228,6 +228,7 @@ public:
 
 	virtual bool has_valid_export_configuration(const Ref<EditorExportPreset> &p_preset, String &r_error, bool &r_missing_templates, bool p_debug = false) const override;
 	virtual bool has_valid_project_configuration(const Ref<EditorExportPreset> &p_preset, String &r_error) const override;
+	static bool has_valid_username_and_password(const Ref<EditorExportPreset> &p_preset, String &r_error);
 
 	virtual List<String> get_binary_extensions(const Ref<EditorExportPreset> &p_preset) const override;
 
