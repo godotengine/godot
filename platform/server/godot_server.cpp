7c3d37ec1da6166e5730c232819d69263b4b8554
TurboHz <turbohz@gmail.com>
2023-10-20 11:21:33 +0200

    Return exit code 0 when running --version or --help
    Fixes https://github.com/godotengine/godot/issues/83661

--- platform/server/godot_server.cpp
+++ platform/server/godot_server.cpp
@@ -35,8 +35,12 @@ int main(int argc, char *argv[]) {
 	OS_Server os;
 
 	Error err = Main::setup(argv[0], argc - 1, &argv[1]);
-	if (err != OK)
+	if (err != OK) {
+		if (err == ERR_HELP) { // Returned by --help and --version, so success.
+			return 0;
+		}
 		return 255;
+	}
 
 	if (Main::start())
 		os.run(); // it is actually the OS that decides how to run
