cd87916d98ad2d17e1a6282ee33be5234c336436
BlueCube3310 <53150244+BlueCube3310@users.noreply.github.com>
2023-10-23 21:26:38 +0200

    Fix Scene Importer crashing when mesh or animation save paths are invalid

--- editor/import/3d/resource_importer_scene.h
+++ editor/import/3d/resource_importer_scene.h
@@ -214,6 +214,7 @@ class ResourceImporterScene : public ResourceImporter {
 		SHAPE_TYPE_CAPSULE,
 	};
 
+	static Error _check_resource_save_paths(const Dictionary &p_data);
 	Array _get_skinned_pose_transforms(ImporterMeshInstance3D *p_src_mesh_node);
 	void _replace_owner(Node *p_node, Node *p_scene, Node *p_new_owner);
 	Node *_generate_meshes(Node *p_node, const Dictionary &p_mesh_data, bool p_generate_lods, bool p_create_shadow_meshes, LightBakeMode p_light_bake_mode, float p_lightmap_texel_size, const Vector<uint8_t> &p_src_lightmap_cache, Vector<Vector<uint8_t>> &r_lightmap_caches);
