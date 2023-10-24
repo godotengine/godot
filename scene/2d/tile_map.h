99262705a82101e51496ca4b47a5cb7ed0574c00
kobewi <kobewi4e@gmail.com>
2023-10-24 13:38:29 +0200

    Fix TileMap layer reverts and defaults

--- scene/2d/tile_map.h
+++ scene/2d/tile_map.h
@@ -461,6 +461,7 @@ private:
 
 	// Layers.
 	LocalVector<Ref<TileMapLayer>> layers;
+	Ref<TileMapLayer> default_layer; // Dummy layer to fetch default values.
 	int selected_layer = -1;
 	bool pending_update = false;
 
@@ -479,6 +480,8 @@ protected:
 	bool _set(const StringName &p_name, const Variant &p_value);
 	bool _get(const StringName &p_name, Variant &r_ret) const;
 	void _get_property_list(List<PropertyInfo> *p_list) const;
+	bool _property_can_revert(const StringName &p_name) const;
+	bool _property_get_revert(const StringName &p_name, Variant &r_property) const;
 
 	void _notification(int p_what);
 	static void _bind_methods();
