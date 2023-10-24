5591f289d75ffa8f39a946f1ffa64bd3b6b34975
clayjohn <claynjohn@gmail.com>
2023-10-24 18:02:36 +0200

    Uses screen-aligned quads for origin lines to avoid issues on NVidia
    While we are add it, use alpha-antialised lines to make them look nice

--- editor/plugins/node_3d_editor_plugin.h
+++ editor/plugins/node_3d_editor_plugin.h
@@ -597,7 +597,8 @@ private:
 
 	ToolMode tool_mode;
 
-	RID origin;
+	RID origin_mesh;
+	RID origin_multimesh;
 	RID origin_instance;
 	bool origin_enabled = false;
 	RID grid[3];
@@ -631,7 +632,7 @@ private:
 	RID indicators_instance;
 	RID cursor_mesh;
 	RID cursor_instance;
-	Ref<StandardMaterial3D> indicator_mat;
+	Ref<ShaderMaterial> origin_mat;
 	Ref<ShaderMaterial> grid_mat[3];
 	Ref<StandardMaterial3D> cursor_material;
 
