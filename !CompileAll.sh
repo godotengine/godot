6fb42c90257230e91a4aba6ba35442662f25b98e
GopherDevGames <135842382+GopherDevGames@users.noreply.github.com>
2023-11-11 17:46:48 +0100

    better compilation script

--- !CompileAll.sh
+++ !CompileAll.sh
@@ -1,11 +1,19 @@
 export SCONS_CACHE="./.SCache"
 export SCONS_CACHE_LIMIT=5000
 
+
+# Clear current compiled things
+rm -rf ./bin
+
+#Compile Editor
+scons target=editor profile=EditorCustom.py 
+
+# Linux
 scons profile=TemplatesCustom.py platform=windows target=template_release arch=x86_64
-# scons platform=linux target=template_release arch=x86_32
-scons profile=TemplatesCustom.py platform=linux target=template_release arch=x86_64
+scons profile=TemplatesCustom.py platform=linuxbsd target=template_release arch=x86_64
 
 # Android Templates
+export ANDROID_SDK_ROOT=/home/gopher/Android/Sdk/
 scons profile=TemplatesCustom.py platform=android target=template_release arch=armv7
 scons profile=TemplatesCustom.py platform=android target=template_release arch=arm64v8
 cd platform/android/java
@@ -13,6 +21,6 @@ call .\gradlew cleanGodotTemplates
 call .\gradlew generateGodotTemplates
 cd ../../..
 
-REM Web Templates
+# Web Template
 scons profile=TemplatesWeb.py platform=web target=template_release javascript_eval=no
 echo "Compiled!"
