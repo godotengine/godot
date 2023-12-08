c2a19dae892f9448a563a82ddf4ba57b5a13d8f4
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-12-08 01:17:09 +0100

    Allow using [MustBeVariant] in typed generic attributes

---
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Sample/MustBeVariantSamples.cs
@@ -0,0 +1,52 @@
+using System;
+
+namespace Godot.SourceGenerators.Sample;
+
+public class MustBeVariantMethods
+{
+    public MustBeVariantMethods()
+    {
+        Method<string>();
+
+        // This call must fail
+        Method<NotVariant>();
+    }
+
+    public void Method<[MustBeVariant] T>() where T : class
+    {
+    }
+}
+
+public class MustBeVariantAnnotatedMethods
+{
+    [GenericTypeAttribute<string>()]
+    public void MethodWithAttribute()
+    {
+    }
+
+    // This method must fail
+    [GenericTypeAttribute<NotVariant>()]
+    public void MethodWithWrongAttribute()
+    {
+    }
+}
+
+[GenericTypeAttribute<string>()]
+public class ClassVariantAnnotated
+{
+}
+
+// This class must fail
+[GenericTypeAttribute<NotVariant>()]
+public class ClassNonVariantAnnotated
+{
+}
+
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
+public class GenericTypeAttribute<[MustBeVariant] T> : Attribute where T : class
+{
+}
+
+public class NotVariant
+{
+}
