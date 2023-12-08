64fcaf0fe5b65a92e7f5cb479e799efb4670b6bb
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-12-08 01:47:39 +0100

    Commented failing code

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Sample/MustBeVariantSamples.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Sample/MustBeVariantSamples.cs
@@ -9,7 +9,7 @@ public class MustBeVariantMethods
         Method<string>();
 
         // This call must fail
-        Method<NotVariant>();
+        // Method<NotVariant>();
     }
 
     public void Method<[MustBeVariant] T>() where T : class
@@ -24,11 +24,13 @@ public class MustBeVariantAnnotatedMethods
     {
     }
 
-    // This method must fail
+    // This method fails
+    /*
     [GenericTypeAttribute<NotVariant>()]
     public void MethodWithWrongAttribute()
     {
     }
+    */
 }
 
 [GenericTypeAttribute<string>()]
@@ -36,11 +38,14 @@ public class ClassVariantAnnotated
 {
 }
 
-// This class must fail
+
+// This class fail
+/*
 [GenericTypeAttribute<NotVariant>()]
 public class ClassNonVariantAnnotated
 {
 }
+*/
 
 [AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true)]
 public class GenericTypeAttribute<[MustBeVariant] T> : Attribute where T : class
