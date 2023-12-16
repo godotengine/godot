b20ba6bd12902e89df9f84b3a4daa503a10d24dd
Silc Lizard (Tokage) Renew <61938263+TokageItLab@users.noreply.github.com>
2023-12-16 14:19:50 +0900

    Fix discrete key retrieval method after start
    (cherry picked from commit b438e28509a266dc1b57a83ae63f8f8bf62dc052)

--- scene/animation/animation_player.cpp
+++ scene/animation/animation_player.cpp
@@ -233,7 +233,9 @@ void AnimationPlayer::_process_playback_data(PlaybackData &cd, double p_delta, f
 		pi.delta = delta;
 		pi.seeked = p_seeked;
 	}
-	pi.is_external_seeking = true; // AnimationPlayer doesn't have internal seeking.
+	// AnimationPlayer doesn't have internal seeking.
+	// However, immediately after playback, discrete keys should be retrieved with EXACT mode since behind keys must be ignored at that time.
+	pi.is_external_seeking = !p_started;
 	pi.looped_flag = looped_flag;
 	pi.weight = p_blend;
 	make_animation_instance(cd.from->name, pi);
