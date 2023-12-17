58f5d895a77dc76b28d899fa05ecae6257de45a1
Alberto Vilches <194074+avilches@users.noreply.github.com>
2023-12-17 22:10:34 +0100

    Ensure parentSyntax is the right type in every case

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/MustBeVariantAnalyzer.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/MustBeVariantAnalyzer.cs
@@ -109,13 +109,20 @@ namespace Godot.SourceGenerators
         /// <returns><see langword="true"/> if the type must be variant and must be analyzed.</returns>
         private bool ShouldCheckTypeArgument(SyntaxNodeAnalysisContext context, SyntaxNode parentSyntax, ISymbol parentSymbol, TypeSyntax typeArgumentSyntax, ITypeSymbol typeArgumentSymbol, int typeArgumentIndex)
         {
-            var typeParamSymbol = parentSymbol switch
+            ITypeParameterSymbol? typeParamSymbol = parentSymbol switch
             {
-                IMethodSymbol methodSymbol => methodSymbol.TypeParameters.Length > 0
-                    ? methodSymbol.TypeParameters[typeArgumentIndex]
-                    : methodSymbol.ContainingType.TypeParameters[typeArgumentIndex],
-                INamedTypeSymbol typeSymbol => typeSymbol.TypeParameters[typeArgumentIndex],
-                _ => null,
+                IMethodSymbol methodSymbol when parentSyntax.Parent is ExpressionSyntax &&
+                                                methodSymbol.TypeParameters.Length > 0
+                    => methodSymbol.TypeParameters[typeArgumentIndex],
+
+                IMethodSymbol methodSymbol when parentSyntax.Parent is AttributeSyntax &&
+                                                methodSymbol.ContainingType.TypeParameters.Length > 0
+                    => methodSymbol.ContainingType.TypeParameters[typeArgumentIndex],
+
+                INamedTypeSymbol { TypeParameters: { Length: > 0 } } typeSymbol
+                    => typeSymbol.TypeParameters[typeArgumentIndex],
+                _
+                    => null
             };
 
             if (typeParamSymbol == null)
