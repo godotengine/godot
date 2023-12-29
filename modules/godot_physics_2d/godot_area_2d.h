f5aff87405f8169c346e6b7a70d6640253d031ec
Aaron Franke <arnfranke@yahoo.com>
2023-12-29 00:23:06 -0600

    Add a Target gravity type to Area for a custom target point

--- modules/godot_physics_2d/godot_area_2d.h
+++ modules/godot_physics_2d/godot_area_2d.h
@@ -56,6 +56,7 @@ class GodotArea2D : public GodotCollisionObject2D {
 	Callable monitor_callback;
 
 	Callable area_monitor_callback;
+	Callable gravity_target_callback;
 
 	SelfList<GodotArea2D> monitor_query_list;
 	SelfList<GodotArea2D> moved_list;
@@ -104,6 +105,8 @@ public:
 
 	void set_area_monitor_callback(const Callable &p_callback);
 	_FORCE_INLINE_ bool has_area_monitor_callback() const { return area_monitor_callback.is_valid(); }
+
+	void set_gravity_target_callback(const Callable &p_callback);
 
 	_FORCE_INLINE_ void add_body_to_query(GodotBody2D *p_body, uint32_t p_body_shape, uint32_t p_area_shape);
 	_FORCE_INLINE_ void remove_body_from_query(GodotBody2D *p_body, uint32_t p_body_shape, uint32_t p_area_shape);
