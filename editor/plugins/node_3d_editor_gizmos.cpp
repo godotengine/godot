3dd1d489762bb2edf3e704cec851ae88547eeede
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-01-06 15:41:50 +0100

    [Editor] Add missing virtual bind to `EditorNode3DGizmo(Plugin)`
    Method `_begin_handle_action` was not bound
    
    (cherry picked from commit c794ce195be505d26b3654a6e1588ee7b5f9fa9c)

--- editor/plugins/node_3d_editor_gizmos.cpp
+++ editor/plugins/node_3d_editor_gizmos.cpp
@@ -825,6 +825,7 @@ void EditorNode3DGizmo::_bind_methods() {
 	GDVIRTUAL_BIND(_is_handle_highlighted, "id", "secondary");
 
 	GDVIRTUAL_BIND(_get_handle_value, "id", "secondary");
+	GDVIRTUAL_BIND(_begin_handle_action, "id", "secondary");
 	GDVIRTUAL_BIND(_set_handle, "id", "secondary", "camera", "point");
 	GDVIRTUAL_BIND(_commit_handle, "id", "secondary", "restore", "cancel");
 
@@ -1045,6 +1046,7 @@ void EditorNode3DGizmoPlugin::_bind_methods() {
 	GDVIRTUAL_BIND(_is_handle_highlighted, "gizmo", "handle_id", "secondary");
 	GDVIRTUAL_BIND(_get_handle_value, "gizmo", "handle_id", "secondary");
 
+	GDVIRTUAL_BIND(_begin_handle_action, "gizmo", "handle_id", "secondary");
 	GDVIRTUAL_BIND(_set_handle, "gizmo", "handle_id", "secondary", "camera", "screen_pos");
 	GDVIRTUAL_BIND(_commit_handle, "gizmo", "handle_id", "secondary", "restore", "cancel");
 
