aaada60bed831d524255e0754623dc7e88f1c745
Delton Ding <dsh0416@gmail.com>
2024-01-11 14:10:28 +0800

    Update csharp_script.cpp

--- modules/mono/csharp_script.cpp
+++ modules/mono/csharp_script.cpp
@@ -1856,10 +1856,8 @@ void CSharpInstance::mono_object_disposed(GCHandleIntPtr p_gchandle_to_free) {
 }
 
 void CSharpInstance::mono_object_disposed_baseref(GCHandleIntPtr p_gchandle_to_free, bool p_is_finalizer, bool &r_delete_owner, bool &r_remove_script_instance) {
-#ifdef DEBUG_ENABLED
-	CRASH_COND(!base_ref_counted);
-	CRASH_COND(!gchandle.is_weak() && gchandle.is_released());
-#endif
+	DEV_ASSERT(base_ref_counted);
+	DEV_ASSERT(gchandle.is_weak() || !gchandle.is_released());
 
 	// Must make sure event signals are not left dangling
 	disconnect_event_signals();
