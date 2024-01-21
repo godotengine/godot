c00caa766ee753e37a5714931491baaffd8628a3
nfrmtk <lykov.ivan.02@mail.ru>
2024-01-21 22:24:53 +0300

    add check for negative buffer_size inside start_(de)compression method.
    (cherry picked from commit da886006a052a9f9c697878dfe317a37ef413ba7)

--- core/io/stream_peer_gzip.cpp
+++ core/io/stream_peer_gzip.cpp
@@ -76,6 +76,7 @@ Error StreamPeerGZIP::start_decompression(bool p_is_deflate, int buffer_size) {
 
 Error StreamPeerGZIP::_start(bool p_compress, bool p_is_deflate, int buffer_size) {
 	ERR_FAIL_COND_V(ctx != nullptr, ERR_ALREADY_IN_USE);
+	ERR_FAIL_COND_V_MSG(buffer_size <= 0, ERR_INVALID_PARAMETER, "Invalid buffer size. It should be a positive integer.");
 	clear();
 	compressing = p_compress;
 	rb.resize(nearest_shift(buffer_size - 1));
