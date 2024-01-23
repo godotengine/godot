98cfa8bcf23c011ab44188ee0d17d2e67a9917cc
Rémi Verschelde <rverschelde@gmail.com>
2024-01-23 20:20:18 +0100

    VideoPlayer: Fix reloading translation remapped stream
    Fixes #43917.
    
    (cherry picked from commit 7d0c561e02d37a27884162128387064f4fb1b90b)

--- scene/gui/video_stream_player.cpp
+++ scene/gui/video_stream_player.cpp
@@ -224,6 +224,12 @@ bool VideoStreamPlayer::has_expand() const {
 void VideoStreamPlayer::set_stream(const Ref<VideoStream> &p_stream) {
 	stop();
 
+	// Make sure to handle stream changes seamlessly, e.g. when done via
+	// translation remapping.
+	if (stream.is_valid()) {
+		stream->disconnect("changed", callable_mp(this, &VideoStreamPlayer::set_stream));
+	}
+
 	AudioServer::get_singleton()->lock();
 	mix_buffer.resize(AudioServer::get_singleton()->thread_get_mix_buffer_size());
 	stream = p_stream;
@@ -234,6 +240,10 @@ void VideoStreamPlayer::set_stream(const Ref<VideoStream> &p_stream) {
 		playback = Ref<VideoStreamPlayback>();
 	}
 	AudioServer::get_singleton()->unlock();
+
+	if (stream.is_valid()) {
+		stream->connect("changed", callable_mp(this, &VideoStreamPlayer::set_stream).bind(stream));
+	}
 
 	if (!playback.is_null()) {
 		playback->set_paused(paused);
