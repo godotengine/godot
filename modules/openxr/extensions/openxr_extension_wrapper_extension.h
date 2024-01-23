3d2cbb216e21de44703bb0e9412fbd2d09514088
David Snopek <dsnopek@gmail.com>
2024-01-23 11:14:47 -0600

    OpenXR: Make it possible to implement vendor extensions to hand tracking from GDExtension

--- modules/openxr/extensions/openxr_extension_wrapper_extension.h
+++ modules/openxr/extensions/openxr_extension_wrapper_extension.h
@@ -58,12 +58,14 @@ public:
 	virtual void *set_instance_create_info_and_get_next_pointer(void *p_next_pointer) override;
 	virtual void *set_session_create_and_get_next_pointer(void *p_next_pointer) override;
 	virtual void *set_swapchain_create_info_and_get_next_pointer(void *p_next_pointer) override;
+	virtual void *set_hand_joint_locations_and_get_next_pointer(int p_hand_index, void *p_next_pointer) override;
 
 	//TODO workaround as GDExtensionPtr<void> return type results in build error in godot-cpp
 	GDVIRTUAL1R(uint64_t, _set_system_properties_and_get_next_pointer, GDExtensionPtr<void>);
 	GDVIRTUAL1R(uint64_t, _set_instance_create_info_and_get_next_pointer, GDExtensionPtr<void>);
 	GDVIRTUAL1R(uint64_t, _set_session_create_and_get_next_pointer, GDExtensionPtr<void>);
 	GDVIRTUAL1R(uint64_t, _set_swapchain_create_info_and_get_next_pointer, GDExtensionPtr<void>);
+	GDVIRTUAL2R(uint64_t, _set_hand_joint_locations_and_get_next_pointer, int, GDExtensionPtr<void>);
 
 	virtual void on_register_metadata() override;
 	virtual void on_before_instance_created() override;
