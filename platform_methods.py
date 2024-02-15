8567cfebd35006fd17ba6a319e46f951e4e86350
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2024-02-15 14:51:07 +0100

    [3.x] Fix X11/Server cross-bits compilation non-x86
    When making a 32 bits build from 64 machine (and vice versa) the built
    system assumed x86 (forcing `-m32` or `-m64`).
    
    This commit backports the arch detection function from 4.x and only use
    it in the X11 and server platforms to avoid adding the incorrect flags.
    
    I believe that unlike the whole "arch" refactor, this change is more
    likely to be backported to 3.5, so we can cross compile for arm32
    without having to edit the build system.

--- platform_methods.py
+++ platform_methods.py
@@ -4,6 +4,7 @@ import json
 import uuid
 import functools
 import subprocess
+import platform
 
 # NOTE: The multiprocessing module is not compatible with SCons due to conflict on cPickle
 
@@ -11,6 +12,39 @@ if sys.version_info[0] < 3:
     JSON_SERIALIZABLE_TYPES = (bool, int, long, float, basestring)
 else:
     JSON_SERIALIZABLE_TYPES = (bool, int, float, str)
+
+# CPU architecture options.
+architectures = ["x86_32", "x86_64", "arm32", "arm64", "rv64", "ppc32", "ppc64", "wasm32"]
+architecture_aliases = {
+    "x86": "x86_32",
+    "x64": "x86_64",
+    "amd64": "x86_64",
+    "armv7": "arm32",
+    "armv8": "arm64",
+    "arm64v8": "arm64",
+    "aarch64": "arm64",
+    "rv": "rv64",
+    "riscv": "rv64",
+    "riscv64": "rv64",
+    "ppcle": "ppc32",
+    "ppc": "ppc32",
+    "ppc64le": "ppc64",
+}
+
+
+def detect_arch():
+    host_machine = platform.machine().lower()
+    if host_machine in architectures:
+        return host_machine
+    elif host_machine in architecture_aliases.keys():
+        return architecture_aliases[host_machine]
+    elif "86" in host_machine:
+        # Catches x86, i386, i486, i586, i686, etc.
+        return "x86_32"
+    else:
+        print("Unsupported CPU architecture: " + host_machine)
+        print("Falling back to x86_64.")
+        return "x86_64"
 
 
 def run_in_subprocess(builder_function):
