2e128652655db8f0834e674f87454bd1d52e06e2
Rama Del Maestro <rdelmaestro@gmail.com>
2024-02-20 17:21:28 -0800

    Add unit tests for Camera2D

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -102,6 +102,7 @@
 #include "tests/scene/test_arraymesh.h"
 #include "tests/scene/test_audio_stream_wav.h"
 #include "tests/scene/test_bit_map.h"
+#include "tests/scene/test_camera_2d.h"
 #include "tests/scene/test_code_edit.h"
 #include "tests/scene/test_color_picker.h"
 #include "tests/scene/test_control.h"
