97851f03401537b7fa0affc50a5800eda351c4b8
Raul Santos <raulsntos@gmail.com>
2024-02-27 21:09:04 +0100

    Move dotnet-format script to pre-commit
    Co-authored-by: Paul Joannon <437025+paulloz@users.noreply.github.com>

---
+++ misc/scripts/dotnet_format.py
@@ -0,0 +1,30 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import glob
+import os
+import sys
+
+# Create dummy generated files.
+for path in [
+    "modules/mono/SdkPackageVersions.props",
+]:
+    os.makedirs(os.path.dirname(path), exist_ok=True)
+    with open(path, "w") as f:
+        f.write("<Project />")
+
+# Avoid importing GeneratedIncludes.props.
+os.environ["GodotSkipGenerated"] = "true"
+
+# Match all the input files to their respective C# project.
+input_files = [os.path.normpath(x) for x in sys.argv]
+projects = {
+    path: [f for f in sys.argv if os.path.commonpath([f, path]) == path]
+    for path in [os.path.dirname(f) for f in glob.glob("**/*.csproj", recursive=True)]
+}
+
+# Run dotnet format on all projects with more than 0 modified files.
+for path, files in projects.items():
+    if len(files) > 0:
+        command = f"dotnet format {path} --include {' '.join(files)}"
+        os.system(command)
