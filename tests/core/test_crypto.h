54eee895a8cef54e348a072b2053b1b16e08b83e
Edward Moulsdale <eddie.moulsdale@gmail.com>
2024-02-28 22:35:06 +0000

    Add unit tests for CryptoKey

--- tests/core/test_crypto.h
+++ tests/core/test_crypto.h
@@ -57,6 +57,45 @@ PackedByteArray raw_to_pba(const uint8_t *arr, size_t len) {
 	return pba;
 }
 
+Ref<CryptoKey> create_crypto_key() {
+	const Ref<CryptoKey> crypto_key = Ref<CryptoKey>(CryptoKey::create());
+	const String priv_path = TestUtils::get_data_path("crypto/in.key");
+	crypto_key->load(priv_path);
+	return crypto_key;
+}
+
+TEST_CASE("[Crypto] CryptoKey public_only") {
+	const Ref<CryptoKey> crypto_key = create_crypto_key();
+	bool public_only = crypto_key->is_public_only();
+	CHECK(!public_only);
+}
+
+TEST_CASE("[Crypto] CryptoKey save private") {
+	const Ref<CryptoKey> crypto_key = create_crypto_key();
+	const String priv_out_path = TestUtils::get_data_path("crypto/out.key");
+	crypto_key->save(priv_out_path);
+	const String priv_path = TestUtils::get_data_path("crypto/in.key");
+	Ref<FileAccess> f_priv_out = FileAccess::open(priv_out_path, FileAccess::READ);
+	REQUIRE(!f_priv_out.is_null());
+	String s_priv_out = f_priv_out->get_as_utf8_string();
+	Ref<FileAccess> f_priv_in = FileAccess::open(priv_path, FileAccess::READ);
+	String s_priv_in = f_priv_in->get_as_utf8_string();
+	CHECK(s_priv_out == s_priv_in);
+}
+
+TEST_CASE("[Crypto] CryptoKey save public") {
+	const Ref<CryptoKey> crypto_key = create_crypto_key();
+	const String pub_out_path = TestUtils::get_data_path("crypto/out.pub");
+	crypto_key->save(pub_out_path, true);
+	const String pub_path = TestUtils::get_data_path("crypto/in.pub");
+	Ref<FileAccess> f_pub_out = FileAccess::open(pub_out_path, FileAccess::READ);
+	REQUIRE(!f_pub_out.is_null());
+	String s_pub_out = f_pub_out->get_as_utf8_string();
+	Ref<FileAccess> f_pub_in = FileAccess::open(pub_path, FileAccess::READ);
+	String s_pub_in = f_pub_in->get_as_utf8_string();
+	CHECK(s_pub_out == s_pub_in);
+}
+
 TEST_CASE("[Crypto] PackedByteArray constant time compare") {
 	const uint8_t hm1[] = { 144, 140, 176, 38, 88, 113, 101, 45, 71, 105, 10, 91, 248, 16, 117, 244, 189, 30, 238, 29, 219, 134, 82, 130, 212, 114, 161, 166, 188, 169, 200, 106 };
 	const uint8_t hm2[] = { 80, 30, 144, 228, 108, 38, 188, 125, 150, 64, 165, 127, 221, 118, 144, 232, 45, 100, 15, 248, 193, 244, 245, 34, 116, 147, 132, 200, 110, 27, 38, 75 };
