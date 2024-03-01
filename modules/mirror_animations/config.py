220fe2d893ff5e11781d9eea9fcceaf1330e89bd
Denis Germ <denis.germ@outlook.com>
2024-03-01 23:45:13 +0100

    feat: add mirror option animations in advanved import dialog

---
+++ modules/mirror_animations/config.py
@@ -0,0 +1,5 @@
+def can_build(env, platform):
+    return not env["disable_3d"]
+
+def configure(env):
+    pass
---
+++ modules/mirror_animations/register_types.h
@@ -0,0 +1,6 @@
+#pragma once
+
+#include "modules/register_module_types.h"
+
+void initialize_mirror_animations_module(ModuleInitializationLevel p_level);
+void uninitialize_mirror_animations_module(ModuleInitializationLevel p_level);
