6add074895d9e3b81b93e938aad107b1a6ee8ab1
Gilles Roudière <gilles.roudiere@gmail.com>
2024-03-05 17:53:57 +0100

    Save PackedByteArrays as base64 encoded

--- core/variant/variant_parser.h
+++ core/variant/variant_parser.h
@@ -141,6 +141,7 @@ private:
 
 	template <typename T>
 	static Error _parse_construct(Stream *p_stream, Vector<T> &r_construct, int &line, String &r_err_str);
+	static Error _parse_byte_array(Stream *p_stream, Vector<uint8_t> &r_construct, int &line, String &r_err_str);
 	static Error _parse_enginecfg(Stream *p_stream, Vector<String> &strings, int &line, String &r_err_str);
 	static Error _parse_dictionary(Dictionary &object, Stream *p_stream, int &line, String &r_err_str, ResourceParser *p_res_parser = nullptr);
 	static Error _parse_array(Array &array, Stream *p_stream, int &line, String &r_err_str, ResourceParser *p_res_parser = nullptr);
