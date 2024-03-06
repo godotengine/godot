1383401c71c5e0c7a9fad604caf63085c81b489a
Logan Lang <devloglogan@gmail.com>
2024-03-06 08:43:19 -0600

    Expose profile_changed signal to XRController3D

--- scene/3d/xr_nodes.h
+++ scene/3d/xr_nodes.h
@@ -135,6 +135,7 @@ protected:
 	void _button_released(const String &p_name);
 	void _input_float_changed(const String &p_name, float p_value);
 	void _input_vector2_changed(const String &p_name, Vector2 p_value);
+	void _profile_changed(const String &p_role);
 
 public:
 	bool is_button_pressed(const StringName &p_name) const;
