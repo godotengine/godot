036c9715c249daa7d358f508f7afebdb6904c7f8
Mikael Hermansson <mikael@hermansson.io>
2024-03-08 15:24:51 +0100

    Allow LSP to process multiple messages per poll
    (cherry picked from commit e2485044a1b33628e6149d4f930b6fe065743c9d)

--- modules/gdscript/language_server/gdscript_language_server.cpp
+++ modules/gdscript/language_server/gdscript_language_server.cpp
@@ -42,6 +42,7 @@ GDScriptLanguageServer::GDScriptLanguageServer() {
 	_EDITOR_DEF("network/language_server/enable_smart_resolve", true);
 	_EDITOR_DEF("network/language_server/show_native_symbols_in_editor", false);
 	_EDITOR_DEF("network/language_server/use_thread", use_thread);
+	_EDITOR_DEF("network/language_server/poll_limit_usec", poll_limit_usec);
 }
 
 void GDScriptLanguageServer::_notification(int p_what) {
@@ -56,7 +57,7 @@ void GDScriptLanguageServer::_notification(int p_what) {
 
 		case NOTIFICATION_INTERNAL_PROCESS: {
 			if (started && !use_thread) {
-				protocol.poll();
+				protocol.poll(poll_limit_usec);
 			}
 		} break;
 
@@ -64,7 +65,8 @@ void GDScriptLanguageServer::_notification(int p_what) {
 			String remote_host = String(_EDITOR_GET("network/language_server/remote_host"));
 			int remote_port = (int)_EDITOR_GET("network/language_server/remote_port");
 			bool remote_use_thread = (bool)_EDITOR_GET("network/language_server/use_thread");
-			if (remote_host != host || remote_port != port || remote_use_thread != use_thread) {
+			int remote_poll_limit = (int)_EDITOR_GET("network/language_server/poll_limit_usec");
+			if (remote_host != host || remote_port != port || remote_use_thread != use_thread || remote_poll_limit != poll_limit_usec) {
 				stop();
 				start();
 			}
@@ -77,7 +79,7 @@ void GDScriptLanguageServer::thread_main(void *p_userdata) {
 	GDScriptLanguageServer *self = static_cast<GDScriptLanguageServer *>(p_userdata);
 	while (self->thread_running) {
 		// Poll 20 times per second
-		self->protocol.poll();
+		self->protocol.poll(self->poll_limit_usec);
 		OS::get_singleton()->delay_usec(50000);
 	}
 }
@@ -86,6 +88,7 @@ void GDScriptLanguageServer::start() {
 	host = String(_EDITOR_GET("network/language_server/remote_host"));
 	port = (int)_EDITOR_GET("network/language_server/remote_port");
 	use_thread = (bool)_EDITOR_GET("network/language_server/use_thread");
+	poll_limit_usec = (int)_EDITOR_GET("network/language_server/poll_limit_usec");
 	if (protocol.start(port, IPAddress(host)) == OK) {
 		EditorNode::get_log()->add_message("--- GDScript language server started ---", EditorLog::MSG_TYPE_EDITOR);
 		if (use_thread) {
