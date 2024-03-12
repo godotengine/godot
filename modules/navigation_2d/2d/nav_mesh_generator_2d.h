ecf95bad2b72606c8afa2c8dc59a98d0a056839a
Aaron Franke <arnfranke@yahoo.com>
2024-03-11 20:27:45 -0700

    Move server files into their subfolders

--- modules/navigation_2d/2d/nav_mesh_generator_2d.h
+++ modules/navigation_2d/2d/nav_mesh_generator_2d.h
@@ -35,7 +35,7 @@
 #include "core/object/class_db.h"
 #include "core/object/worker_thread_pool.h"
 #include "core/templates/rid_owner.h"
-#include "servers/navigation_server_2d.h"
+#include "servers/navigation_2d/navigation_server_2d.h"
 
 class Node;
 class NavigationPolygon;
