4a0f09e7103e21495c066833f116b113bdcbc8fe
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2024-03-18 17:18:47 +0100

    [Core] Make `Dictionary.get_or_add` check for read-only

--- tests/core/variant/test_dictionary.h
+++ tests/core/variant/test_dictionary.h
@@ -72,6 +72,11 @@ TEST_CASE("[Dictionary] Assignment using bracket notation ([])") {
 	map.make_read_only();
 	CHECK(int(map["This key does not exist"].get_type()) == Variant::NIL);
 	CHECK(map.size() == length);
+
+	ERR_PRINT_OFF;
+	CHECK(int(map.get_or_add("This key does not exist", String()).get_type()) == Variant::NIL);
+	CHECK(map.size() == length);
+	ERR_PRINT_ON;
 }
 
 TEST_CASE("[Dictionary] List init") {
