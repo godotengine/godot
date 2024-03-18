8aff153ea2fb33b1d96c3bc07f9726752c16d701
Danil Alexeev <danil@alexeev.xyz>
2024-03-18 09:49:21 +0300

    Editor: Fix same-name (sub)groups interfering in Inspector
    Clear (sub)group hashmaps when changing category.
    
    (cherry picked from commit 9b7dcfa0e2c2669f7ba2c34cd45482279028d5b5)

--- editor/editor_inspector.cpp
+++ editor/editor_inspector.cpp
@@ -2774,6 +2774,9 @@ void EditorInspector::update_tree() {
 			subgroup_base = "";
 			section_depth = 0;
 
+			vbox_per_path.clear();
+			editor_inspector_array_per_prefix.clear();
+
 			if (!show_categories) {
 				continue;
 			}
