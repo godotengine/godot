a97a34b5a7aed1db499050b05ef0fe06e2c2e5a1
Louis Prud'homme <louisph@bam.tech>
2024-03-21 10:46:37 +0100

    fix: reset RenderingDevice to prevent second run crash

--- servers/rendering/rendering_device.cpp
+++ servers/rendering/rendering_device.cpp
@@ -6634,6 +6634,12 @@ RenderingDevice::~RenderingDevice() {
 	}
 }
 
+RenderingDevice::~RenderingDevice() {
+	if (singleton == this) {
+		singleton = nullptr;
+	}
+}
+
 RenderingDevice::RenderingDevice() {
 	if (singleton == nullptr) {
 		singleton = this;
