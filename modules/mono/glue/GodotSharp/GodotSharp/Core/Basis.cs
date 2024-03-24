1db5090dcd7bebcf35b587dc148f37a2368c1530
kleonc <9283098+kleonc@users.noreply.github.com>
2024-03-24 09:22:38 +0100

    [C#] Fix `Transform3D.InterpolateWith` applying rotation before scale
    (cherry picked from commit e2ed63b313e3eff714e32f2cd2d01db9f0e2ea9d)

--- modules/mono/glue/GodotSharp/GodotSharp/Core/Basis.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/Basis.cs
@@ -253,7 +253,7 @@ namespace Godot
 
         private void Rotate(Quat quaternion)
         {
-            this *= new Basis(quaternion);
+            this = new Basis(quaternion) * this;
         }
 
         private void SetDiagonal(Vector3 diagonal)
