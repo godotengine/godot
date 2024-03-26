ae092ade7f9faa148e0bc5e118c33b67637e1f9a
kobewi <kobewi4e@gmail.com>
2024-03-26 17:04:14 +0100

    Always add decimal when printing float

--- modules/gdscript/tests/scripts/analyzer/features/const_conversions.gd
+++ modules/gdscript/tests/scripts/analyzer/features/const_conversions.gd
@@ -8,17 +8,17 @@ const const_packed_ints: PackedFloat64Array = [52]
 @warning_ignore("assert_always_true")
 func test():
 	assert(typeof(const_float_int) == TYPE_FLOAT)
-	assert(str(const_float_int) == '19')
+	assert(str(const_float_int) == '19.0')
 	assert(typeof(const_float_plus) == TYPE_FLOAT)
-	assert(str(const_float_plus) == '34')
+	assert(str(const_float_plus) == '34.0')
 	assert(typeof(const_float_cast) == TYPE_FLOAT)
-	assert(str(const_float_cast) == '76')
+	assert(str(const_float_cast) == '76.0')
 
 	assert(typeof(const_packed_empty) == TYPE_PACKED_FLOAT64_ARRAY)
 	assert(str(const_packed_empty) == '[]')
 	assert(typeof(const_packed_ints) == TYPE_PACKED_FLOAT64_ARRAY)
-	assert(str(const_packed_ints) == '[52]')
+	assert(str(const_packed_ints) == '[52.0]')
 	assert(typeof(const_packed_ints[0]) == TYPE_FLOAT)
-	assert(str(const_packed_ints[0]) == '52')
+	assert(str(const_packed_ints[0]) == '52.0')
 
 	print('ok')
