37e0be37d8faa0c16059611017ac07bdc84c7410
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-03-27 22:09:13 +0200

    [macOS] Fix non-global native menu shortcuts.

--- platform/macos/display_server_macos.mm
+++ platform/macos/display_server_macos.mm
@@ -290,6 +290,10 @@ void DisplayServerMacOS::_update_displays_arrangement() {
 	displays_arrangement_dirty = false;
 }
 
+void DisplayServerMacOS::set_menu_delegate(NSMenu *p_menu) {
+	[p_menu setDelegate:menu_delegate];
+}
+
 Point2i DisplayServerMacOS::_get_screens_origin() const {
 	// Returns the native top-left screen coordinate of the smallest rectangle
 	// that encompasses all screens. Needed in get_screen_position(),
