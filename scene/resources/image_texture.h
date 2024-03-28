6261bf577ea65a74163ff2c393cb5cb446b7f87f
kobewi <kobewi4e@gmail.com>
2024-03-28 16:12:52 +0100

    Change how ImageTexture's image is defined

--- scene/resources/image_texture.h
+++ scene/resources/image_texture.h
@@ -49,11 +49,6 @@ class ImageTexture : public Texture2D {
 
 protected:
 	virtual void reload_from_file() override;
-
-	bool _set(const StringName &p_name, const Variant &p_value);
-	bool _get(const StringName &p_name, Variant &r_ret) const;
-	void _get_property_list(List<PropertyInfo> *p_list) const;
-
 	static void _bind_methods();
 
 public:
