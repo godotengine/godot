d96141d43043bdef888568cee73e72100042a915
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-04-10 20:46:20 +0200

    Add unit tests for GradientTexture1D and GradientTexture2D
    Co-authored-by: ArthyChaux <53626057+ArthyChaux@users.noreply.github.com>

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -110,6 +110,7 @@
 #include "tests/scene/test_curve_2d.h"
 #include "tests/scene/test_curve_3d.h"
 #include "tests/scene/test_gradient.h"
+#include "tests/scene/test_gradient_texture.h"
 #include "tests/scene/test_image_texture.h"
 #include "tests/scene/test_image_texture_3d.h"
 #include "tests/scene/test_instance_placeholder.h"
