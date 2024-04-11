5881dfd699b6998b2926bc5d56a43119c67895d2
smix8 <52464204+smix8@users.noreply.github.com>
2024-04-11 03:53:26 +0200

    Use threads for baking navigation mesh inside editor
    Enables threaded navigation mesh baking inside the editor.

--- modules/navigation/2d/nav_mesh_generator_2d.cpp
+++ modules/navigation/2d/nav_mesh_generator_2d.cpp
@@ -72,7 +72,7 @@ NavMeshGenerator2D::NavMeshGenerator2D() {
 
 	// Using threads might cause problems on certain exports or with the Editor on certain devices.
 	// This is the main switch to turn threaded navmesh baking off should the need arise.
-	use_threads = baking_use_multiple_threads && !Engine::get_singleton()->is_editor_hint();
+	use_threads = baking_use_multiple_threads;
 }
 
 NavMeshGenerator2D::~NavMeshGenerator2D() {
