c63c02ecf74187063b7a3a42f3dff7c71adbaf67
clayjohn <claynjohn@gmail.com>
2024-04-17 15:19:20 -0700

    Use a src rect for copying from screen with CanvasGroup in the mobile backend

--- servers/rendering/renderer_rd/effects/copy_effects.cpp
+++ servers/rendering/renderer_rd/effects/copy_effects.cpp
@@ -533,7 +533,7 @@ void CopyEffects::copy_to_atlas_fb(RID p_source_rd_texture, RID p_dest_framebuff
 	RD::get_singleton()->draw_list_draw(draw_list, true);
 }
 
-void CopyEffects::copy_to_fb_rect(RID p_source_rd_texture, RID p_dest_framebuffer, const Rect2i &p_rect, bool p_flip_y, bool p_force_luminance, bool p_alpha_to_zero, bool p_srgb, RID p_secondary, bool p_multiview, bool p_alpha_to_one, bool p_linear, bool p_normal) {
+void CopyEffects::copy_to_fb_rect(RID p_source_rd_texture, RID p_dest_framebuffer, const Rect2i &p_rect, bool p_flip_y, bool p_force_luminance, bool p_alpha_to_zero, bool p_srgb, RID p_secondary, bool p_multiview, bool p_alpha_to_one, bool p_linear, bool p_normal, const Rect2 &p_src_rect) {
 	UniformSetCacheRD *uniform_set_cache = UniformSetCacheRD::get_singleton();
 	ERR_FAIL_NULL(uniform_set_cache);
 	MaterialStorage *material_storage = MaterialStorage::get_singleton();
@@ -566,6 +566,14 @@ void CopyEffects::copy_to_fb_rect(RID p_source_rd_texture, RID p_dest_framebuffe
 
 	if (p_normal) {
 		copy_to_fb.push_constant.flags |= COPY_TO_FB_FLAG_NORMAL;
+	}
+
+	if (p_src_rect != Rect2()) {
+		copy_to_fb.push_constant.section[0] = p_src_rect.position.x;
+		copy_to_fb.push_constant.section[1] = p_src_rect.position.y;
+		copy_to_fb.push_constant.section[2] = p_src_rect.size.x;
+		copy_to_fb.push_constant.section[3] = p_src_rect.size.y;
+		copy_to_fb.push_constant.flags |= COPY_TO_FB_FLAG_USE_SRC_SECTION;
 	}
 
 	// setup our uniforms
