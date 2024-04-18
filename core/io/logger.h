413d243c2580fcea9421b7a564db5f51d666cbbc
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-04-18 05:07:07 +0200

    Strip ANSI escape codes from file logging
    Text editors cannot display ANSI escape codes, so these should be
    stripped from log files to ensure readability.
    
    Since this uses a regex, this works both for `print_rich()`
    and manually inserted ANSI escape codes.
    
    Co-authored-by: bruvzg <7645683+bruvzg@users.noreply.github.com>

--- core/io/logger.h
+++ core/io/logger.h
@@ -34,6 +34,10 @@
 #include "core/io/file_access.h"
 #include "core/string/ustring.h"
 #include "core/templates/vector.h"
+#include "modules/modules_enabled.gen.h" // For regex.
+#ifdef MODULE_REGEX_ENABLED
+#include "modules/regex/regex.h"
+#endif // MODULE_REGEX_ENABLED
 
 #include <stdarg.h>
 
@@ -85,6 +89,10 @@ class RotatedFileLogger : public Logger {
 
 	void clear_old_backups();
 	void rotate_file();
+
+#ifdef MODULE_REGEX_ENABLED
+	Ref<RegEx> strip_ansi_regex;
+#endif // MODULE_REGEX_ENABLED
 
 public:
 	explicit RotatedFileLogger(const String &p_base_path, int p_max_files = 10);
