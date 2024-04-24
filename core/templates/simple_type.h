dee65d14351a419cfeaac0e1e0da0a497bd445fb
Pedro J. Estébanez <pedrojrulez@gmail.com>
2024-04-24 11:41:58 +0200

    Redefine GetSimpleTypeT<> in terms of <type_traits>

--- core/templates/simple_type.h
+++ core/templates/simple_type.h
@@ -31,26 +31,9 @@
 #ifndef SIMPLE_TYPE_H
 #define SIMPLE_TYPE_H
 
-/* Batch of specializations to obtain the actual simple type */
+#include <type_traits>
 
 template <typename T>
-struct GetSimpleTypeT {
-	typedef T type_t;
-};
-
-template <typename T>
-struct GetSimpleTypeT<T &> {
-	typedef T type_t;
-};
-
-template <typename T>
-struct GetSimpleTypeT<T const> {
-	typedef T type_t;
-};
-
-template <typename T>
-struct GetSimpleTypeT<T const &> {
-	typedef T type_t;
-};
+using GetSimpleTypeT = typename std::remove_cv_t<std::remove_reference_t<T>>;
 
 #endif // SIMPLE_TYPE_H
