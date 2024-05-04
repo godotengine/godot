34fe4d521e11b82cb58c70f14d0a6afb3198f164
peachey2k2 <ogzpolat@gmail.com>
2024-05-04 18:54:49 +0300

    replace the variable in window.h to the right one

--- scene/main/window.cpp
+++ scene/main/window.cpp
@@ -2523,7 +2523,7 @@ void Window::begin_bulk_theme_override() {
 
 void Window::end_bulk_theme_override() {
 	ERR_MAIN_THREAD_GUARD;
-	ERR_FAIL_COND_MSG(data.bulk_theme_override <= 0, "end_bulk_theme_override() called without a matching begin_bulk_theme_override().");
+	ERR_FAIL_COND_MSG(bulk_theme_override <= 0, "end_bulk_theme_override() called without a matching begin_bulk_theme_override().");
 
 	bulk_theme_override -= 1;
 	_notify_theme_override_changed();
