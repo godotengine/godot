b941459719d32f3b81798a9abcb95b133417f050
Daylily-Zeleen <daylily-zeleen@foxmail.com>
2024-05-06 23:20:59 +0800

    Limit icon size in EditorDebuggerTree.

--- editor/debugger/editor_debugger_tree.cpp
+++ editor/debugger/editor_debugger_tree.cpp
@@ -31,6 +31,7 @@
 #include "editor_debugger_tree.h"
 
 #include "editor/editor_node.h"
+#include "editor/editor_string_names.h"
 #include "editor/gui/editor_file_dialog.h"
 #include "editor/scene_tree_dock.h"
 #include "scene/debugger/scene_debugger.h"
@@ -61,6 +62,10 @@ void EditorDebuggerTree::_notification(int p_what) {
 			connect("cell_selected", callable_mp(this, &EditorDebuggerTree::_scene_tree_selected));
 			connect("item_collapsed", callable_mp(this, &EditorDebuggerTree::_scene_tree_folded));
 			connect("item_mouse_selected", callable_mp(this, &EditorDebuggerTree::_scene_tree_rmb_selected));
+		} break;
+
+		case NOTIFICATION_ENTER_TREE: {
+			update_icon_max_width();
 		} break;
 	}
 }
@@ -291,6 +296,10 @@ Variant EditorDebuggerTree::get_drag_data(const Point2 &p_point) {
 	}
 
 	return vformat("\"%s\"", path);
+}
+
+void EditorDebuggerTree::update_icon_max_width() {
+	add_theme_constant_override("icon_max_width", get_theme_constant("class_icon_size", EditorStringName(Editor)));
 }
 
 String EditorDebuggerTree::get_selected_path() {
