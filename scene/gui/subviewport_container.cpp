a23ecd728e5f3a148f09cea31ac32d6838f78630
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-05-07 04:23:20 -0700

    Define uninitialized values for send in subviewport container.

--- scene/gui/subviewport_container.cpp
+++ scene/gui/subviewport_container.cpp
@@ -189,7 +189,7 @@ void SubViewportContainer::_propagate_nonpositional_event(const Ref<InputEvent> 
 		return;
 	}
 
-	bool send;
+	bool send = false;
 	if (GDVIRTUAL_CALL(_propagate_input_event, p_event, send)) {
 		if (!send) {
 			return;
@@ -210,7 +210,7 @@ void SubViewportContainer::gui_input(const Ref<InputEvent> &p_event) {
 		return;
 	}
 
-	bool send;
+	bool send = false;
 	if (GDVIRTUAL_CALL(_propagate_input_event, p_event, send)) {
 		if (!send) {
 			return;
