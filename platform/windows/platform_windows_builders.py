15d37ed2a891c099858abcddc1a470563b3166a4
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-05-08 11:09:50 +0300

    [MinGW] Restore executable size check.

--- platform/windows/platform_windows_builders.py
+++ platform/windows/platform_windows_builders.py
@@ -5,13 +5,16 @@ from detect import get_mingw_tool
 
 
 def make_debug_mingw(target, source, env):
-    objcopy = get_mingw_tool("objcopy", env["mingw_prefix"], env["arch"])
-    strip = get_mingw_tool("strip", env["mingw_prefix"], env["arch"])
+    dst = str(target[0])
+    # Force separate debug symbols if executable size is larger than 1.9 GB.
+    if env["separate_debug_symbols"] or os.stat(dst).st_size >= 2040109465:
+        objcopy = get_mingw_tool("objcopy", env["mingw_prefix"], env["arch"])
+        strip = get_mingw_tool("strip", env["mingw_prefix"], env["arch"])
 
-    if not objcopy or not strip:
-        print('`separate_debug_symbols` requires both "objcopy" and "strip" to function.')
-        return
+        if not objcopy or not strip:
+            print('`separate_debug_symbols` requires both "objcopy" and "strip" to function.')
+            return
 
-    os.system("{0} --only-keep-debug {1} {1}.debugsymbols".format(objcopy, target[0]))
-    os.system("{0} --strip-debug --strip-unneeded {1}".format(strip, target[0]))
-    os.system("{0} --add-gnu-debuglink={1}.debugsymbols {1}".format(objcopy, target[0]))
+        os.system("{0} --only-keep-debug {1} {1}.debugsymbols".format(objcopy, dst))
+        os.system("{0} --strip-debug --strip-unneeded {1}".format(strip, dst))
+        os.system("{0} --add-gnu-debuglink={1}.debugsymbols {1}".format(objcopy, dst))
