1fd1adc285163b4e62dafd98fcdbeb73f87463e5
Yuri Rubinsky <chaosus89@gmail.com>
2024-05-09 14:46:58 +0300

    Fix placement of ColorPicker in the editor

--- scene/gui/box_container.cpp
+++ scene/gui/box_container.cpp
@@ -243,8 +243,8 @@ Size2 BoxContainer::get_minimum_size() const {
 	bool first = true;
 
 	for (int i = 0; i < get_child_count(); i++) {
-		Control *c = as_sortable_control(get_child(i));
-		if (!c) {
+		Control *c = Object::cast_to<Control>(get_child(i));
+		if (!c || !c->is_visible() || c->is_set_as_top_level()) {
 			continue;
 		}
 
