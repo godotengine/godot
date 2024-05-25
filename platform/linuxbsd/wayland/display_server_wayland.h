9734feb44917c6c64530c34a0c36defe9dbff472
Riteo <riteo@posteo.net>
2024-05-25 14:11:52 +0200

    Wayland: Implement is_window_transparency_available

--- platform/linuxbsd/wayland/display_server_wayland.h
+++ platform/linuxbsd/wayland/display_server_wayland.h
@@ -280,6 +280,8 @@ public:
 
 	virtual void set_context(Context p_context) override;
 
+	virtual bool is_window_transparency_available() const override;
+
 	static DisplayServer *create_func(const String &p_rendering_driver, WindowMode p_mode, VSyncMode p_vsync_mode, uint32_t p_flags, const Point2i *p_position, const Size2i &p_resolution, int p_screen, Context p_context, Error &r_error);
 	static Vector<String> get_rendering_drivers_func();
 
