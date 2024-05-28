3b9d074fd76b06eba8b70d4bb366ee68b3cc1b9e
clayjohn <claynjohn@gmail.com>
2024-05-28 15:13:24 -0700

    Ensure MovieWriter output is in gamma space when using HDR 2D

--- servers/rendering/renderer_viewport.h
+++ servers/rendering/renderer_viewport.h
@@ -262,6 +262,8 @@ public:
 	void viewport_set_transparent_background(RID p_viewport, bool p_enabled);
 	void viewport_set_use_hdr_2d(RID p_viewport, bool p_use_hdr_2d);
 
+	bool viewport_is_using_hdr_2d(RID p_viewport) const;
+
 	void viewport_set_global_canvas_transform(RID p_viewport, const Transform2D &p_transform);
 	void viewport_set_canvas_stacking(RID p_viewport, RID p_canvas, int p_layer, int p_sublayer);
 
