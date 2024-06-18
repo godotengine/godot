ef9b82a0760a769459f3e3402515e56f269844d4
rune-scape <spartacrafter@gmail.com>
2024-06-18 02:18:11 -0700

    fix rare physics crash

--- servers/physics_2d/godot_body_2d.cpp
+++ servers/physics_2d/godot_body_2d.cpp
@@ -616,6 +616,8 @@ void GodotBody2D::integrate_velocities(real_t p_step) {
 		return;
 	}
 
+	ERR_FAIL_NULL(get_space());
+
 	if (fi_callback_data || body_state_callback.is_valid()) {
 		get_space()->body_add_to_state_query_list(&direct_state_query_list);
 	}
@@ -704,6 +706,8 @@ bool GodotBody2D::sleep_test(real_t p_step) {
 	} else if (!can_sleep) {
 		return false;
 	}
+
+	ERR_FAIL_NULL_V(get_space(), true);
 
 	if (Math::abs(angular_velocity) < get_space()->get_body_angular_velocity_sleep_threshold() && Math::abs(linear_velocity.length_squared()) < get_space()->get_body_linear_velocity_sleep_threshold() * get_space()->get_body_linear_velocity_sleep_threshold()) {
 		still_time += p_step;
