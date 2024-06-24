2d3f6963b2400b9098d32c1c4ed335d27644edda
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2024-06-24 06:45:31 -0700

    Update the visibility for the custom templates for all platforms
    Hide the custom template options behind the `Advanced Options` toggle

--- platform/web/export/export_plugin.h
+++ platform/web/export/export_plugin.h
@@ -112,6 +112,7 @@ public:
 	virtual void get_preset_features(const Ref<EditorExportPreset> &p_preset, List<String> *r_features) const override;
 
 	virtual void get_export_options(List<ExportOption> *r_options) const override;
+	virtual bool get_export_option_visibility(const EditorExportPreset *p_preset, const String &p_option) const override;
 
 	virtual String get_name() const override;
 	virtual String get_os_name() const override;
