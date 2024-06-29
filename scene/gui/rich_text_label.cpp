86e8974bbef95260051e087572abc5cf327d2e1f
Mark DiBarry <markdibarry@protonmail.com>
2024-06-29 16:37:43 -0400

    Change RTL snap from transform to control

--- scene/gui/rich_text_label.cpp
+++ scene/gui/rich_text_label.cpp
@@ -1248,7 +1248,7 @@ int RichTextLabel::_draw_line(ItemFrame *p_frame, int p_line, const Vector2 &p_o
 						}
 					}
 
-					if (is_inside_tree() && get_viewport()->is_snap_2d_transforms_to_pixel_enabled()) {
+					if (is_inside_tree() && get_viewport()->is_snap_controls_to_pixels_enabled()) {
 						fx_offset = (fx_offset + Point2(0.5, 0.5)).floor();
 					}
 
