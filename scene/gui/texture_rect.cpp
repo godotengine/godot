05803b6d8bf463f02d8b2e5b7922b6e91f1dcb2e
kleonc <9283098+kleonc@users.noreply.github.com>
2024-07-01 21:22:25 +0200

    Fix AtlasTexture::draw_rect flipping for non-zero margin

--- scene/gui/texture_rect.cpp
+++ scene/gui/texture_rect.cpp
@@ -30,7 +30,6 @@
 
 #include "texture_rect.h"
 
-#include "scene/resources/atlas_texture.h"
 #include "servers/rendering_server.h"
 
 void TextureRect::_notification(int p_what) {
@@ -90,15 +89,6 @@ void TextureRect::_notification(int p_what) {
 					region.position = ((scaled_tex_size - size) / scale).abs() / 2.0f;
 					region.size = size / scale;
 				} break;
-			}
-
-			Ref<AtlasTexture> p_atlas = texture;
-
-			if (p_atlas.is_valid() && !region.has_area()) {
-				Size2 scale_size(size.width / texture->get_width(), size.height / texture->get_height());
-
-				offset.width += hflip ? p_atlas->get_margin().get_position().width * scale_size.width * 2 : 0;
-				offset.height += vflip ? p_atlas->get_margin().get_position().height * scale_size.height * 2 : 0;
 			}
 
 			size.width *= hflip ? -1.0f : 1.0f;
