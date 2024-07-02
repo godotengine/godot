e311997065726e0db57973d89d0cca98edb65690
Yahkub-R <62478788+Yahkub-R@users.noreply.github.com>
2024-07-02 09:40:26 -0400

    Clang Fix

--- editor/gui/scene_tree_editor.cpp
+++ editor/gui/scene_tree_editor.cpp
@@ -334,7 +334,7 @@ void SceneTreeEditor::_add_nodes(Node *p_node, TreeItem *p_parent) {
 		}
 		if (p_node->is_exposed_in_owner()) {
 			item->add_button(0, get_editor_theme_icon(SNAME("SceneExposedNode")), BUTTON_EXPOSED, p_node->get_owner() != EditorNode::get_singleton()->get_edited_scene(), TTR("This node will exposed when this scene is instantiated.\nClick to disable this."));
-			if(p_node->get_owner() != get_scene_node()) {
+			if (p_node->get_owner() != get_scene_node()) {
 				_set_item_custom_color(item, get_theme_color(SNAME("warning_color"), EditorStringName(Editor)));
 			}
 		}
