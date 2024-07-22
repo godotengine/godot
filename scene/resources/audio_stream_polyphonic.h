espumita00/test

Fix bug in AudioStreamPlaybackPolyphonic where stream IDs returned from play_stream may not work

scene/resources/audio_stream_polyphonic.h
@@ -54,7 +54,7 @@ public:
 class AudioStreamPlaybackPolyphonic : public AudioStreamPlayback {
 	GDCLASS(AudioStreamPlaybackPolyphonic, AudioStreamPlayback)
 
-	enum {
+	enum : int64_t {
 		INTERNAL_BUFFER_LEN = 128,
 		ID_MASK = 0xFFFFFFFF,
 		INDEX_SHIFT = 32

godotengine/godot

scene/resources/audio_stream_polyphonic.h
@@ -60,7 +60,7 @@ public:
 class AudioStreamPlaybackPolyphonic : public AudioStreamPlayback {
 	GDCLASS(AudioStreamPlaybackPolyphonic, AudioStreamPlayback)
 
-	enum {
+	enum : uint32_t {
 		INTERNAL_BUFFER_LEN = 128,
 		ID_MASK = 0xFFFFFFFF,
 		INDEX_SHIFT = 32