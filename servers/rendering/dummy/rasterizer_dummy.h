1eb0039b6eae3f4fab75ca6bb746712a233d32cb
Bastiaan Olij <mux213@gmail.com>
2024-07-29 10:49:48 +1000

    Fix regression around OpenGL swapchain optimisation for OpenXR

--- servers/rendering/dummy/rasterizer_dummy.h
+++ servers/rendering/dummy/rasterizer_dummy.h
@@ -88,7 +88,7 @@ public:
 
 	void blit_render_targets_to_screen(int p_screen, const BlitToScreen *p_render_targets, int p_amount) override {}
 
-	void end_viewport(bool p_swap_buffers) override {}
+	void gl_end_frame(bool p_swap_buffers) override {}
 
 	void end_frame(bool p_swap_buffers) override {
 		if (p_swap_buffers) {
