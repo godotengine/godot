f44d6a235f198e3f8c5189161840315f43cfdd2e
Joel Croteau <joelcroteau@joelcroteau.com>
2024-08-04 11:00:56 -0600

    Fix reload of GDExtension libraries in framework package on macos
    `GDExtension::open_library` has a check in it to see if the library was loaded
    from a temp file, and if it was to restore the original name as that is the one
    we actually care about. This check is breaking extension reloading on Mac when
    the library path is to a framework folder, as the file inside the framework
    will not generally be the same name as the folder.
    
    This check also shouldn't be necessary even on Windows, which is the only
    platform that uses `generate_temp_files`, since disposal of the created temp
    file is handled within `OS_Windows::open_dynamic_library`, and
    `GDExtension::open_library` (which is the only function to call
    `open_dynamic_library` with a `p_data` argument) only cares about the original
    library file path and has to do extra work to remove the name of the temp file.
    Instead, I have removed that check and set `OS_Windows::open_dynamic_library`
    to return the name of the original file and not the name of the copy.
    
    This fixes GDExtension reloading on macOS. I do not have a Windows machine
    available to test that it still works properly on Windows, so someone should
    check that before merging this.

--- drivers/unix/file_access_unix.cpp
+++ drivers/unix/file_access_unix.cpp
@@ -383,7 +383,7 @@ uint64_t FileAccessUnix::_get_modified_time(const String &p_file) {
 	if (!err) {
 		return status.st_mtime;
 	} else {
-		print_verbose("Failed to get modified time for: " + p_file + "");
+		WARN_PRINT("Failed to get modified time for: " + p_file);
 		return 0;
 	}
 }
