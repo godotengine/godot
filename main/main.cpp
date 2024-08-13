964671857227dbfab7b298db88d03687aaf4305e
风青山 <idleman@yeah.net>
2024-08-13 21:05:49 +0800

    Improve gamepad support on Linux
    Previously, the controller input mapping relied on `core/input/gamecontrollerdb.txt`,
    so if the device is newer, the device input mapping may be confused.
    
    For gamepads not documented in this file, they are now simply mapped according to the
    enumeration semantics.
    
    Reference: https://docs.kernel.org/input/gamepad.html#linux-gamepad-specification
    
    Add a project setting so that users can decide whether to auto map.

--- main/main.cpp
+++ main/main.cpp
@@ -3251,6 +3251,9 @@ Error Main::setup2(bool p_show_boot_logo) {
 			bool agile_input_event_flushing = GLOBAL_DEF("input_devices/buffering/agile_event_flushing", false);
 			id->set_agile_input_event_flushing(agile_input_event_flushing);
 
+			bool unknown_gamepad_auto_mapped = GLOBAL_DEF("input_devices/gamepad/unknown_gamepad_auto_mapped", true);
+			id->set_unknown_gamepad_auto_mapped(unknown_gamepad_auto_mapped);
+
 			if (bool(GLOBAL_DEF_BASIC("input_devices/pointing/emulate_touch_from_mouse", false)) &&
 					!(editor || project_manager)) {
 				if (!DisplayServer::get_singleton()->is_touchscreen_available()) {
