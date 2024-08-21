a9d22cd9bb976dcb9b42403d626fbe54b62bda53
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-08-21 08:11:53 -0700

    Add double precision support for World Triplanar Mapping
    This commit adds emulated doubles support for the World UV Triplanar Mapping.
    It also adds some QoL features to both ease the "integration" with the double
    precision pipeline and also to allow greater flexibility when dealing
    with Triplanar UVs.
    
    Features:
      - New mat4 TRIPLANAR_MATRIX vertex-only property (the same as MODEL_MATRIX by default).
      - New "hint_triplanar_mat" hint for mat4 uniforms, which allow users
        to specify a custom TRIPLANAR_MATRIX. Naturally, only a single
        instance of the hint is allowed per shader.
      - New vec3 TRIPLANAR_POSITION vertex-only property, which holds the
        VERTEX transformed by the TRIPLANAR_MATRIX (With emulated doubles, if
        applicable).
      - Updated Standard 3D Material to make use of the above features.
      - Updated VisualShader to include the TRIPLANAR_MATRIX and
        TRIPLANAR_POSITION as valid Inputs.
      - Added a "hint_triplanar_enabled" toggle to the VisualShaderNodeTransformParameter to allow
        the new "hint_triplanar_mat" hint to be set.
    
    Note: The shader related changes were applied to all renderers (Forward+,
    Mobile and Compatibility).
    
    Note²: Given the lack of emulated doubles support for the Compatibility
    renderer, only the single precision path was implemented.
    
    Co-Authored-By: =?UTF-8?q?Jo=C3=A3o=20Pedro=20Braz?= <brazjoaopedro@ymail.com>

--- servers/rendering/shader_language.h
+++ servers/rendering/shader_language.h
@@ -181,6 +181,7 @@ public:
 		TK_HINT_SCREEN_TEXTURE,
 		TK_HINT_NORMAL_ROUGHNESS_TEXTURE,
 		TK_HINT_DEPTH_TEXTURE,
+		TK_HINT_TRIPLANAR_MAT,
 		TK_FILTER_NEAREST,
 		TK_FILTER_LINEAR,
 		TK_FILTER_NEAREST_MIPMAP,
@@ -641,6 +642,7 @@ public:
 				HINT_SCREEN_TEXTURE,
 				HINT_NORMAL_ROUGHNESS_TEXTURE,
 				HINT_DEPTH_TEXTURE,
+				HINT_TRIPLANAR_MAT,
 				HINT_MAX
 			};
 
@@ -691,6 +693,8 @@ public:
 		Vector<Function> vfunctions;
 		Vector<Constant> vconstants;
 		Vector<Struct> vstructs;
+
+		bool uses_triplanar_matrix = false;
 
 		ShaderNode() :
 				Node(NODE_TYPE_SHADER) {}
