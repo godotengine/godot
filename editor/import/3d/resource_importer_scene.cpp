7e580150f97e14d5cb3a7a34a7bff46d1be6b48d
Adam Scott <ascott.ca@gmail.com>
2024-08-25 12:48:32 -0400

    Add import tab "import_script/path" relative path support

--- editor/import/3d/resource_importer_scene.cpp
+++ editor/import/3d/resource_importer_scene.cpp
@@ -3058,6 +3058,10 @@ Error ResourceImporterScene::import(const String &p_source_file, const String &p
 	progress.step(TTR("Running Custom Script..."), 2);
 
 	String post_import_script_path = p_options["import_script/path"];
+	if (post_import_script_path.is_relative_path()) {
+		post_import_script_path = p_source_file.get_base_dir().path_join(post_import_script_path);
+	}
+
 	Ref<EditorScenePostImport> post_import_script;
 
 	if (!post_import_script_path.is_empty()) {
