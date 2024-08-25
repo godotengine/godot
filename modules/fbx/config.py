9a3f751a19c30c21f5b9c59c0c86497b79f17006
Naming-things-is-hard-btw <ahmadmhamad12331233@gmail.com>
2024-08-25 09:19:44 +0300

    Fix FBX runtime import
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
    Co-authored-by: K. S. Ernest (iFire) Lee <fire@users.noreply.github.com>

--- modules/fbx/config.py
+++ modules/fbx/config.py
@@ -12,6 +12,7 @@ def get_doc_classes():
         "EditorSceneFormatImporterFBX2GLTF",
         "EditorSceneFormatImporterUFBX",
         "FBXDocument",
+        "FBXDocumentExtensionConvertImporterMesh",
         "FBXState",
     ]
 
