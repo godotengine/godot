3da43ebec475d71071b907c0c8c5ded2575adb92
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-09-02 22:48:58 +0300

    [MenuBar] Make menu start index more consistent.

--- scene/gui/menu_bar.cpp
+++ scene/gui/menu_bar.cpp
@@ -218,15 +218,18 @@ void MenuBar::bind_global_menu() {
 	int global_start_idx = -1;
 	int count = nmenu->get_item_count(main_menu);
 	String prev_tag;
-	for (int i = 0; i < count; i++) {
-		String tag = nmenu->get_item_tag(main_menu, i).operator String().get_slice("#", 1);
-		if (!tag.is_empty() && tag != prev_tag) {
-			if (i >= start_index) {
-				global_start_idx = i;
-				break;
+	if (start_index >= 0) {
+		for (int i = 0; i < count; i++) {
+			String tag = nmenu->get_item_tag(main_menu, i).operator String().get_slice("#", 1);
+			if (!tag.is_empty() && tag != prev_tag) {
+				MenuBar *mb = Object::cast_to<MenuBar>(ObjectDB::get_instance(ObjectID(static_cast<uint64_t>(tag.to_int()))));
+				if (mb && mb->get_start_index() >= start_index) {
+					global_start_idx = i;
+					break;
+				}
 			}
+			prev_tag = tag;
 		}
-		prev_tag = tag;
 	}
 	if (global_start_idx == -1) {
 		global_start_idx = count;
