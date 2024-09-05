a3a52a62a39e7954b21f1988c85dc73c47417da2
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-09-05 12:12:59 +0300

    [FileSystem Dock] Add option to show some unsupported files in the dock.

--- editor/filesystem_dock.cpp
+++ editor/filesystem_dock.cpp
@@ -270,7 +270,7 @@ bool FileSystemDock::_create_tree(TreeItem *p_parent, EditorFileSystemDirectory 
 		List<FileInfo> file_list;
 		for (int i = 0; i < p_dir->get_file_count(); i++) {
 			String file_type = p_dir->get_file_type(i);
-			if (file_type != "TextFile" && _is_file_type_disabled_by_feature_profile(file_type)) {
+			if (file_type != "TextFile" && file_type != "OtherFile" && _is_file_type_disabled_by_feature_profile(file_type)) {
 				// If type is disabled, file won't be displayed.
 				continue;
 			}
