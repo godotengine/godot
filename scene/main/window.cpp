cca6793221249b8627986ad7c2ee0666dc56e19d
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-09-08 15:11:13 +0300

    [Label3D] Fix updating labels in subvieports on oversampling change.

--- scene/main/window.cpp
+++ scene/main/window.cpp
@@ -1223,6 +1223,7 @@ void Window::_update_viewport_size() {
 			TS->font_set_global_oversampling(font_oversampling);
 			if (!ci_updated) {
 				update_canvas_items();
+				emit_signal(SNAME("size_changed"));
 			}
 		}
 	}
