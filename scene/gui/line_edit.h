5224aa7933cd6eca46a956c3dd5eca5486ca5353
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2024-09-10 13:43:31 +0000

    Prevent `LineEdit` from losing focus when text is submitted or rejected.

--- scene/gui/line_edit.h
+++ scene/gui/line_edit.h
@@ -86,6 +86,7 @@ public:
 private:
 	HorizontalAlignment alignment = HORIZONTAL_ALIGNMENT_LEFT;
 
+	bool editing = false;
 	bool editable = false;
 	bool pass = false;
 	bool text_changed_dirty = false;
@@ -205,6 +206,9 @@ private:
 		float base_scale = 1.0;
 	} theme_cache;
 
+	void _edit();
+	void _unedit();
+
 	void _clear_undo_stack();
 	void _clear_redo();
 	void _create_undo_state();
@@ -257,6 +261,8 @@ protected:
 	virtual void gui_input(const Ref<InputEvent> &p_event) override;
 
 public:
+	bool is_editing() const;
+
 	void set_horizontal_alignment(HorizontalAlignment p_alignment);
 	HorizontalAlignment get_horizontal_alignment() const;
 
