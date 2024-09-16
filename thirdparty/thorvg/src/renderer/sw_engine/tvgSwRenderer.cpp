f273c7c77352f0e50846e7fe3aab9040e64033c0
Rémi Verschelde <rverschelde@gmail.com>
2024-09-16 17:29:56 +0200

    thorvg: Update to 0.14.10
    More fixes to rendering of SVG files with broken text tags.
    
    Also backports upstream patch to fix #97078.
    
    (cherry picked from commit 0c0336f7049fb792c6e0121082b60815ad3cf1e0)

--- thirdparty/thorvg/src/renderer/sw_engine/tvgSwRenderer.cpp
+++ thirdparty/thorvg/src/renderer/sw_engine/tvgSwRenderer.cpp
@@ -446,7 +446,7 @@ bool SwRenderer::renderShape(RenderData data)
 }
 
 
-bool SwRenderer::blend(BlendMethod method, bool direct)
+bool SwRenderer::blend(BlendMethod method)
 {
     if (surface->blendMethod == method) return true;
     surface->blendMethod = method;
@@ -459,7 +459,7 @@ bool SwRenderer::blend(BlendMethod method, bool direct)
             surface->blender = opBlendScreen;
             break;
         case BlendMethod::Multiply:
-            surface->blender = direct ? opBlendDirectMultiply : opBlendMultiply;
+            surface->blender = opBlendMultiply;
             break;
         case BlendMethod::Overlay:
             surface->blender = opBlendOverlay;
@@ -606,7 +606,10 @@ Compositor* SwRenderer::target(const RenderRegion& region, ColorSpace cs)
     cmp->w = cmp->compositor->image.w;
     cmp->h = cmp->compositor->image.h;
 
-    rasterClear(cmp, x, y, w, h);
+    /* TODO: Currently, only blending might work.
+       Blending and composition must be handled together. */
+    auto color = (surface->blender && !surface->compositor) ? 0x00ffffff : 0x00000000;
+    rasterClear(cmp, x, y, w, h, color);
 
     //Switch render target
     surface = cmp;
