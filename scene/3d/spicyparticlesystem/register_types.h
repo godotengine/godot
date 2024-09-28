596ca9dc8a15fd96457011f0e64a114023cee0e4
zhangjiangen11 <279878713@qq.com>
2024-09-28 19:03:36 +0800

    cccc

--- scene/3d/spicyparticlesystem/register_types.h
+++
@@ -1,7 +0,0 @@
-// #ifndef SPICYPARTICLESYSTEM_REGISTER_TYPES_H
-// #define SPICYPARTICLESYSTEM_REGISTER_TYPES_H
-
-// void initialize_spicyparticlesystem_module();
-// void uninitialize_spicyparticlesystem_module();
-
-// #endif // PICYPARTICLESYSTEM_REGISTER_TYPES_H
