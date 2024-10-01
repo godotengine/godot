7b07cbde69944f42f0f63cbeb22402d0f54309ee
Leon Semmens <35655841+Synthetic-Dev@users.noreply.github.com>
2024-10-01 16:04:24 +0100

    Add `IntersectsRay` to Aabb

--- modules/mono/glue/GodotSharp/GodotSharp/Core/Aabb.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/Aabb.cs
@@ -173,10 +173,10 @@ namespace Godot
                 case 7:
                     return new Vector3(_position.X + _size.X, _position.Y + _size.Y, _position.Z + _size.Z);
                 default:
-                {
-                    throw new ArgumentOutOfRangeException(nameof(idx),
-                        $"Index is {idx}, but a value from 0 to 7 is expected.");
-                }
+                    {
+                        throw new ArgumentOutOfRangeException(nameof(idx),
+                            $"Index is {idx}, but a value from 0 to 7 is expected.");
+                    }
             }
         }
 
@@ -507,6 +507,62 @@ namespace Godot
             }
 
             return under && over;
+        }
+
+        /// <summary>
+        /// Returns <see langword="true"/> if the <see cref="Aabb"/> intersects
+        /// the ray along <paramref name="dir"/> positioned at <paramref name="from"/>.
+        /// </summary>
+        /// <param name="origin">The origin of the ray.</param>
+        /// <param name="dir">The direction of the ray.</param>
+        /// <returns>
+        /// A <see langword="bool"/> for whether or not the <see cref="Aabb"/> intersects the ray.
+        /// </returns>
+        public readonly bool IntersectsRay(Vector3 from, Vector3 dir)
+        {
+            if (HasPoint(from)) return true;
+
+            real_t tmin = -1e20;
+            real_t tmax = 1e20;
+
+            for (int i = 0; i < 3; i++)
+            {
+                if (dir[i] == 0)
+                {
+                    if ((from[i] < Position[i]) || (from[i] > End[i]))
+                    {
+                        return false;
+                    }
+                }
+                else
+                { // ray not parallel to planes in this direction
+                    real_t t1 = (Position[i] - from[i]) / dir[i];
+                    real_t t2 = (End[i] - from[i]) / dir[i];
+
+                    if (t1 > t2)
+                    {
+                        (t2, t1) = (t1, t2);
+                    }
+                    if (t1 >= tmin)
+                    {
+                        tmin = t1;
+                    }
+                    if (t2 < tmax)
+                    {
+                        if (t2 < 0)
+                        {
+                            return false;
+                        }
+                        tmax = t2;
+                    }
+                    if (tmin > tmax)
+                    {
+                        return false;
+                    }
+                }
+            }
+
+            return true;
         }
 
         /// <summary>
