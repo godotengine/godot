3dc4fc912e72fa5d458ae84436d1644176f33ab1
Jayden Sipe <jayden.sipe@gmail.com>
2024-09-30 22:31:54 -0400

    Revert "Removed added warning box to reuse already defined one"
    This reverts commit 68358684b367693ae0011e004456035e83635002.

--- editor/action_map_editor.cpp
+++ editor/action_map_editor.cpp
@@ -59,7 +59,8 @@ void ActionMapEditor::_event_config_confirmed() {
 	// Check for duplicate events on action
 	for (Ref<InputEvent> event : events) {
 		if (ev->is_match(event, true)) {
-			show_message(TTR(vformat("Action \"%s\" already has an Event with this Input.", current_action_name)));
+			warning->set_text(TTR(vformat("Action \"%s\" already has an Event with this Input.", current_action_name)));
+			warning->popup_centered();
 			return;
 		}
 	}
@@ -628,4 +629,9 @@ ActionMapEditor::ActionMapEditor() {
 
 	message = memnew(AcceptDialog);
 	add_child(message);
+
+	warning = memnew(AcceptDialog);
+	add_child(warning);
+	warning->set_title(TTR("Input Configuration Warning!"));
+	warning->set_flag(Window::FLAG_POPUP, true);
 }
