b6ae8e82ba26aff0f7584bc116ada4979e857d89
vPumpking <monmonerp71@gmail.com>
2024-10-07 18:51:54 +0200

    Improve space management for EditorFileDialog
    Move buttons related to the file view on the container above so that they take less horizontal space and make search bar similar to the one in FileDialog

--- editor/gui/editor_file_dialog.h
+++ editor/gui/editor_file_dialog.h
@@ -128,8 +128,11 @@ private:
 	Button *refresh = nullptr;
 	Button *favorite = nullptr;
 	Button *show_hidden = nullptr;
+	Button *show_search_filter_button = nullptr;
 
 	String search_string;
+	bool show_search_filter = false;
+	HBoxContainer *filter_hb = nullptr;
 	LineEdit *filter_box = nullptr;
 	FileSortOption file_sort = FileSortOption::FILE_SORT_NAME;
 	MenuButton *file_sort_button = nullptr;
@@ -165,6 +168,7 @@ private:
 		Ref<Texture2D> back_folder;
 		Ref<Texture2D> reload;
 		Ref<Texture2D> toggle_hidden;
+		Ref<Texture2D> toggle_filename_filter;
 		Ref<Texture2D> favorite;
 		Ref<Texture2D> mode_thumbnails;
 		Ref<Texture2D> mode_list;
@@ -206,6 +210,7 @@ private:
 	void update_dir();
 	void update_file_name();
 	void update_file_list();
+	void update_search_filter_gui();
 	void update_filters();
 
 	void _focus_file_text();
@@ -239,6 +244,7 @@ private:
 
 	void _focus_filter_box();
 	void _filter_changed(const String &p_text);
+	void _search_filter_selected();
 	void _file_sort_popup(int p_id);
 
 	void _delete_items();
@@ -301,6 +307,9 @@ public:
 	void add_filter(const String &p_filter, const String &p_description = "");
 	void set_filters(const Vector<String> &p_filters);
 	Vector<String> get_filters() const;
+	void clear_search_filter();
+	void set_search_filter(const String &p_search_filter);
+	String get_search_filter() const;
 
 	void set_enable_multiple_selection(bool p_enable);
 	Vector<String> get_selected_files() const;
@@ -341,6 +350,7 @@ public:
 	static void set_default_show_hidden_files(bool p_show);
 	static void set_default_display_mode(DisplayMode p_mode);
 	void set_show_hidden_files(bool p_show);
+	void set_show_search_filter(bool p_show);
 	bool is_showing_hidden_files() const;
 
 	void invalidate();
