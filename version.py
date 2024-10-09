b84e9153d69278ddc08e39f093839ffec36113f8
SleeeepyZhou <sleeeepyzhou@yeah.net>
2024-10-09 15:56:40 +0800

    Test

--- version.py
+++ version.py
@@ -1,9 +1,9 @@
 short_name = "godot"
 name = "Godot Engine"
 major = 4
-minor = 4
+minor = 3
 patch = 0
-status = "dev"
+status = "stable"
 module_config = ""
 website = "https://godotengine.org"
-docs = "latest"
+docs = "4.3"
