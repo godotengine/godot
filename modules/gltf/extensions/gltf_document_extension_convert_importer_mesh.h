7e057b9437a5729d32de9ccfaa6e94397107cc46
Aaron Franke <arnfranke@yahoo.com>
2024-10-09 23:53:58 -0700

    GLTF: Fix bad pointer to ImporterMeshInstance3D root node at runtime

--- modules/gltf/extensions/gltf_document_extension_convert_importer_mesh.h
+++ modules/gltf/extensions/gltf_document_extension_convert_importer_mesh.h
@@ -33,6 +33,8 @@
 
 #include "gltf_document_extension.h"
 
+class MeshInstance3D;
+
 class GLTFDocumentExtensionConvertImporterMesh : public GLTFDocumentExtension {
 	GDCLASS(GLTFDocumentExtensionConvertImporterMesh, GLTFDocumentExtension);
 
@@ -40,6 +42,7 @@ protected:
 	static void _copy_meta(Object *p_src_object, Object *p_dst_object);
 
 public:
+	static MeshInstance3D *convert_importer_mesh_instance_3d(ImporterMeshInstance3D *p_importer_mesh_instance_3d);
 	Error import_post(Ref<GLTFState> p_state, Node *p_root) override;
 };
 
