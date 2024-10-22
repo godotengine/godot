0f8523ea001c8505fdf3442d3508eda1d64e4feb
Ivan Skorikov <kroleggg@gmail.com>
2024-10-22 23:57:58 +0200

    Add dynamic exec flags update when exec path is set for an external text editor.

--- editor/editor_settings.h
+++ editor/editor_settings.h
@@ -121,6 +121,7 @@ private:
 	void _load_defaults(Ref<ConfigFile> p_extra_config = Ref<ConfigFile>());
 	void _load_godot2_text_editor_theme();
 	void _load_default_visual_shader_editor_theme();
+	void _exec_path_changed(const String & p_value);
 	bool _save_text_editor_theme(const String &p_file);
 	bool _is_default_text_editor_theme(const String &p_theme_name);
 	const String _get_project_metadata_path() const;
