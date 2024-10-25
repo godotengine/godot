88366206a9f85e41c3a8f25ae47d2ff25c666583
Andrés Botero <0xafbf@gmail.com>
2024-10-24 20:50:43 -0500

    Fix linux builds with debug symbols when there is a space in the path.

--- platform/linuxbsd/platform_linuxbsd_builders.py
+++ platform/linuxbsd/platform_linuxbsd_builders.py
@@ -5,6 +5,6 @@ import os
 
 def make_debug_linuxbsd(target, source, env):
     dst = str(target[0])
-    os.system("objcopy --only-keep-debug {0} {0}.debugsymbols".format(dst))
-    os.system("strip --strip-debug --strip-unneeded {0}".format(dst))
-    os.system("objcopy --add-gnu-debuglink={0}.debugsymbols {0}".format(dst))
+    os.system('objcopy --only-keep-debug "{0}" "{0}.debugsymbols"'.format(dst))
+    os.system('strip --strip-debug --strip-unneeded "{0}"'.format(dst))
+    os.system('objcopy --add-gnu-debuglink="{0}.debugsymbols" "{0}"'.format(dst))
