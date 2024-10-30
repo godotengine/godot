988690ff4a3826a1daccb287bd51054c60c119ae
bruvzg <7645683+bruvzg@users.noreply.github.com>
2024-10-30 11:14:11 +0200

    [TextServer] Reset subpixel shift on blank glyphs and import option to enable/disable it.

--- servers/text_server.h
+++ servers/text_server.h
@@ -315,6 +315,9 @@ public:
 	virtual void font_set_subpixel_positioning(const RID &p_font_rid, SubpixelPositioning p_subpixel) = 0;
 	virtual SubpixelPositioning font_get_subpixel_positioning(const RID &p_font_rid) const = 0;
 
+	virtual void font_set_keep_rounding_remainders(const RID &p_font_rid, bool p_keep_rounding_remainders) = 0;
+	virtual bool font_get_keep_rounding_remainders(const RID &p_font_rid) const = 0;
+
 	virtual void font_set_embolden(const RID &p_font_rid, double p_strength) = 0;
 	virtual double font_get_embolden(const RID &p_font_rid) const = 0;
 
