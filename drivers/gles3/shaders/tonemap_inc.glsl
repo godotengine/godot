fd12600ac13af03a8acac4f4896e61c0edc97e55
Christoffer Sundbom <christoffer_karlsson@live.se>
2024-11-01 23:17:56 +0100

    fixed some minor clang style errors2

--- drivers/gles3/shaders/tonemap_inc.glsl
+++ drivers/gles3/shaders/tonemap_inc.glsl
@@ -195,11 +195,11 @@ vec3 apply_tonemapping(vec3 color, float p_white) { // inputs are LINEAR
 		return tonemap_agx(max(vec3(0.0f), color), true);
 	} else if (tonemapper == TONEMAPPER_TONY_MC_MAPFACE) {
 #ifdef SKY_SHADER
-	// Sampling the Tony McMapface LUT in the sky shader leads to pitch black shadows if the "Sky" background
-	// mode is enabled for the environment. Avoid this by returning the color as is.
-	return color;
+		// Sampling the Tony McMapface LUT in the sky shader leads to pitch black shadows if the "Sky" background
+		// mode is enabled for the environment. Avoid this by returning the color as is.
+		return color;
 #else
-	return tonemap_tony_mc_mapface(max(vec3(0.0f), color));
+		return tonemap_tony_mc_mapface(max(vec3(0.0f), color));
 #endif
 	} else {
 		return color;
