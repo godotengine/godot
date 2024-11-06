7fcea29557b0c8e29eb68260c4da02a112c4ba76
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-11-06 13:00:18 -0800

    Cleanup can build.

--- modules/sqlite/config.py
+++ modules/sqlite/config.py
@@ -2,42 +2,6 @@ import subprocess
 
 
 def can_build(env, platform):
-    if platform in ("ios", "web", "android"):
-        return False
-
-    try:
-        subprocess.check_output(["rustup", "--version"], stderr=subprocess.STDOUT)
-        subprocess.check_output(["cargo", "--version"], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError:
-        print("Cargo or Rustup not found. mvsqlite build skipped.")
-        if platform == "windows":
-            print("Use `scoop install rustup-gnu` to install rust.")
-        return False
-
-    if platform == "windows":
-        use_mingw = env["use_mingw"]
-        if not use_mingw:
-            print("MSVC target is installed. mvsqlite build skipped.")
-            return False
-
-        try:
-            subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            print("Windows Rust target not found. Installing...")
-            subprocess.check_call(["rustup", "target", "add", "x86_64-pc-windows-gnu"])  # or other Windows target
-    else:
-        try:
-            subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            print("x86_64-apple-darwin target not found. Installing...")
-            subprocess.check_call(["rustup", "target", "add", "x86_64-apple-darwin"])
-
-        try:
-            subprocess.check_output(["rustup", "target", "list", "--installed"], stderr=subprocess.STDOUT)
-        except subprocess.CalledProcessError:
-            print("aarch64-apple-darwin target not found. Installing...")
-            subprocess.check_call(["rustup", "target", "add", "aarch64-apple-darwin"])
-
     return True
 
 
