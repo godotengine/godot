11a5e3dcfb00113d1d53d6c84d0af3d5c7083c40
Antonio Caggiano <info@antoniocaggiano.eu>
2024-11-11 08:40:38 +0100

    raytracing: Initial Vulkan support
    - Vulkan implementations in `RenderingDeviceDriverVulkan`.
    - Raytracing instruction list in `RenderingDeviceGraph`.
    - Functions to create acceleration structures and raytracing pipelines
      in `RenderingDevice`.
    - Raygen, Miss, and ClosestHit shader stages support.
    - GDScript bindings.
    - Update classes documentation.
    - Include a-johnston RenderingDeviceDriverMetal changes.
    - Unimplemented placeholders for Metal and D3D12.
    - Apply Mickeon docs suggestions.
    - Build acceleration structure command.
    - Expose a shader preprocessor define.
    - Align build scratch address.
    - Create STB after creating the pipeline.
    - Separate acceleration structure barriers.
    - Use transforms in TLAS instances.
    - Other modifications suggested by DarioSamo.
    - AnyHit and Intersection stages
    - Optionally set acceleration structure build input buffer usage.
    - Introduce instances buffer.
    - Move scratch buffers to RenderingDevice.

--- modules/glslang/register_types.cpp
+++ modules/glslang/register_types.cpp
@@ -48,7 +48,12 @@ static Vector<uint8_t> _compile_shader_glsl(RenderingDevice::ShaderStage p_stage
 		EShLangFragment,
 		EShLangTessControl,
 		EShLangTessEvaluation,
-		EShLangCompute
+		EShLangCompute,
+		EShLangRayGen,
+		EShLangAnyHit,
+		EShLangClosestHit,
+		EShLangMiss,
+		EShLangIntersect,
 	};
 
 	int ClientInputSemanticsVersion = 100; // maps to, say, #define VULKAN 100
@@ -128,6 +133,10 @@ static Vector<uint8_t> _compile_shader_glsl(RenderingDevice::ShaderStage p_stage
 
 	if (p_render_device->has_feature(RD::SUPPORTS_MULTIVIEW)) {
 		preamble += "#define has_VK_KHR_multiview 1\n";
+	}
+
+	if (p_render_device->has_feature(RD::SUPPORTS_RAYTRACING)) {
+		preamble += "#define has_VK_KHR_ray_tracing_pipeline 1\n";
 	}
 
 	if (!preamble.empty()) {
