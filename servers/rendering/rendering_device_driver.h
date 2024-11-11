51a6b08edd6b1cd5b4caeadcd798b70ff9716f69
Antonio Caggiano <info@antoniocaggiano.eu>
2024-11-11 08:40:38 +0100

    raytracing: Initial Vulkan support
    - Vulkan implementations in `RenderingDeviceDriverVulkan`.
    - Raytracing instruction list in `RenderingDeviceGraph`.
    - Functions to create acceleration structures and raytracing pipelines
      in `RenderingDevice`.
    - Raygen, Miss, and ClosestHit shader stages support.
    - GDScript bindings.
    - Update classes documentation.
    - Include a-johnston RenderingDeviceDriverMetal changes.
    - Unimplemented placeholders for Metal and D3D12.
    - Apply Mickeon docs suggestions.
    - Build acceleration structure command.
    - Expose a shader preprocessor define.
    - Align build scratch address.
    - Create STB after creating the pipeline.
    - Separate acceleration structure barriers.
    - Use transforms in TLAS instances.
    - Other modifications suggested by DarioSamo.
    - AnyHit and Intersection stages
    - Optionally set acceleration structure build input buffer usage.
    - Introduce instances buffer.
    - Move scratch buffers to RenderingDevice.

--- servers/rendering/rendering_device_driver.h
+++ servers/rendering/rendering_device_driver.h
@@ -166,6 +166,8 @@ public:
 	DEFINE_ID(QueryPool);
 	DEFINE_ID(Fence);
 	DEFINE_ID(Semaphore);
+	DEFINE_ID(AccelerationStructure);
+	DEFINE_ID(RaytracingPipeline);
 
 public:
 	/*****************/
@@ -196,7 +198,10 @@ public:
 		BUFFER_USAGE_INDEX_BIT = (1 << 6),
 		BUFFER_USAGE_VERTEX_BIT = (1 << 7),
 		BUFFER_USAGE_INDIRECT_BIT = (1 << 8),
+		BUFFER_USAGE_SHADER_BINDING_TABLE_BIT = (1 << 10),
 		BUFFER_USAGE_DEVICE_ADDRESS_BIT = (1 << 17),
+		BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT = (1 << 19),
+		BUFFER_USAGE_ACCELERATION_STRUCTURE_STORAGE_BIT = (1 << 20),
 	};
 
 	enum {
@@ -340,8 +345,10 @@ public:
 		PIPELINE_STAGE_ALL_GRAPHICS_BIT = (1 << 15),
 		PIPELINE_STAGE_ALL_COMMANDS_BIT = (1 << 16),
 		PIPELINE_STAGE_CLEAR_STORAGE_BIT = (1 << 17),
+		PIPELINE_STAGE_RAY_TRACING_SHADER_BIT = (2 << 20),
 		PIPELINE_STAGE_FRAGMENT_SHADING_RATE_ATTACHMENT_BIT = (1 << 22),
 		PIPELINE_STAGE_FRAGMENT_DENSITY_PROCESS_BIT = (1 << 23),
+		PIPELINE_STAGE_ACCELERATION_STRUCTURE_BUILD_BIT = (2 << 24),
 	};
 
 	enum BarrierAccessBits {
@@ -367,6 +374,8 @@ public:
 		BARRIER_ACCESS_RESOLVE_READ_BIT = (1 << 25),
 		BARRIER_ACCESS_RESOLVE_WRITE_BIT = (1 << 26),
 		BARRIER_ACCESS_STORAGE_CLEAR_BIT = (1 << 27),
+		BARRIER_ACCESS_ACCELERATION_STRUCTURE_READ_BIT = (2 << 20),
+		BARRIER_ACCESS_ACCELERATION_STRUCTURE_WRITE_BIT = (4 << 20),
 	};
 
 	struct MemoryBarrier {
@@ -391,13 +400,22 @@ public:
 		TextureSubresourceRange subresources;
 	};
 
+	struct AccelerationStructureBarrier {
+		AccelerationStructureID acceleration_structure;
+		BitField<BarrierAccessBits> src_access;
+		BitField<BarrierAccessBits> dst_access;
+		uint64_t offset = 0;
+		uint64_t size = 0;
+	};
+
 	virtual void command_pipeline_barrier(
 			CommandBufferID p_cmd_buffer,
 			BitField<PipelineStageBits> p_src_stages,
 			BitField<PipelineStageBits> p_dst_stages,
 			VectorView<MemoryBarrier> p_memory_barriers,
 			VectorView<BufferBarrier> p_buffer_barriers,
-			VectorView<TextureBarrier> p_texture_barriers) = 0;
+			VectorView<TextureBarrier> p_texture_barriers,
+			VectorView<AccelerationStructureBarrier> p_acceleration_structure_barriers) = 0;
 
 	/****************/
 	/**** FENCES ****/
@@ -734,6 +752,41 @@ public:
 
 	virtual PipelineID compute_pipeline_create(ShaderID p_shader, VectorView<PipelineSpecializationConstant> p_specialization_constants) = 0;
 
+	/********************/
+	/**** RAYTRACING ****/
+	/********************/
+
+	// ----- ACCELERATION STRUCTURE -----
+
+	enum AccelerationStructureType {
+		ACCELERATION_STRUCTURE_TYPE_BLAS,
+		ACCELERATION_STRUCTURE_TYPE_TLAS,
+	};
+
+	enum GeometryBits {
+		GEOMETRY_OPAQUE = 1 << 0,
+		GEOMETRY_NO_DUPLICATE_ANY_HIT_INVOCATION = 1 << 1,
+	};
+
+	virtual AccelerationStructureID blas_create(BufferID p_vertex_buffer, uint64_t p_vertex_offset, VertexFormatID p_vertex_format, uint32_t p_vertex_count, BufferID p_index_buffer, IndexBufferFormat p_index_format, uint64_t p_index_offset, uint32_t p_index_count, BitField<GeometryBits> p_geometry_bits) = 0;
+	virtual uint32_t tlas_instances_buffer_get_size_bytes(uint32_t p_instance_count) = 0;
+	virtual void tlas_instances_buffer_fill(BufferID p_instances_buffer, const LocalVector<AccelerationStructureID> &p_blases, const Vector<Transform3D> &p_transforms) = 0;
+	virtual AccelerationStructureID tlas_create(BufferID p_instances_buffer) = 0;
+	virtual void acceleration_structure_free(AccelerationStructureID p_acceleration_structure) = 0;
+	virtual uint32_t acceleration_structure_get_scratch_size_bytes(AccelerationStructureID p_acceleration_structure) = 0;
+
+	// ----- PIPELINE -----
+
+	virtual RaytracingPipelineID raytracing_pipeline_create(ShaderID p_shader, VectorView<PipelineSpecializationConstant> p_specialization_constants) = 0;
+	virtual void raytracing_pipeline_free(RaytracingPipelineID p_pipeline) = 0;
+
+	// ----- COMMANDS -----
+
+	virtual void command_build_acceleration_structure(CommandBufferID p_cmd_buffer, AccelerationStructureID p_acceleration_structure, BufferID p_scratch_buffer) = 0;
+	virtual void command_bind_raytracing_pipeline(CommandBufferID p_cmd_buffer, RaytracingPipelineID p_pipeline) = 0;
+	virtual void command_bind_raytracing_uniform_set(CommandBufferID p_cmd_buffer, UniformSetID p_uniform_set, ShaderID p_shader, uint32_t p_set_index) = 0;
+	virtual void command_trace_rays(CommandBufferID p_cmd_buffer, uint32_t p_width, uint32_t p_height) = 0;
+
 	/******************/
 	/**** CALLBACK ****/
 	/******************/
@@ -786,6 +839,8 @@ public:
 		OBJECT_TYPE_SHADER,
 		OBJECT_TYPE_UNIFORM_SET,
 		OBJECT_TYPE_PIPELINE,
+		OBJECT_TYPE_ACCELERATION_STRUCTURE,
+		OBJECT_TYPE_RAYTRACING_PIPELINE,
 	};
 
 	struct MultiviewCapabilities {
