180575ece65db30d2ce01cedb3fee1a3565de8c8
Elias Pozewaunig <64259275+elpozewaunig@users.noreply.github.com>
2024-11-15 02:00:19 +0100

    Added web support for all missing splash screen properties
    show_image, fullsize and use_filter are now supported too.
    
    bg_color, image and minimum_display_time were already supported previously.

--- platform/web/export/export_plugin.cpp
+++ platform/web/export/export_plugin.cpp
@@ -170,6 +170,12 @@ void EditorExportPlatformWeb::_fix_html(Vector<uint8_t> &p_html, const Ref<Edito
 	replaces["$GODOT_HEAD_INCLUDE"] = head_include + custom_head_include;
 	replaces["$GODOT_CONFIG"] = str_config;
 	replaces["$GODOT_SPLASH_COLOR"] = "#" + Color(GLOBAL_GET("application/boot_splash/bg_color")).to_html(false);
+
+	LocalVector<String> godot_splash_classes;
+	godot_splash_classes.push_back("show-image--" + String(GLOBAL_GET("application/boot_splash/show_image")));
+	godot_splash_classes.push_back("fullsize--" + String(GLOBAL_GET("application/boot_splash/fullsize")));
+	godot_splash_classes.push_back("use-filter--" + String(GLOBAL_GET("application/boot_splash/use_filter")));
+	replaces["$GODOT_SPLASH_CLASSES"] = String(" ").join(godot_splash_classes);
 	replaces["$GODOT_SPLASH"] = p_name + ".png";
 
 	if (p_preset->get("variant/thread_support")) {
