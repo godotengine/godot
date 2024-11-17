1de1d4d8f19f28d2715fb544b6880c0aaf67ad68
Anish Mishra <mishragames@gmail.com>
2024-11-17 22:06:06 +0530

    Fix Android boot splash and gradle build issue

--- platform/android/export/gradle_export_util.cpp
+++ platform/android/export/gradle_export_util.cpp
@@ -171,8 +171,9 @@ Error store_string_at_path(const String &p_path, const String &p_data) {
 // This method will be called ONLY when gradle build is enabled.
 Error rename_and_store_file_in_gradle_project(void *p_userdata, const String &p_path, const Vector<uint8_t> &p_data, int p_file, int p_total, const Vector<String> &p_enc_in_filters, const Vector<String> &p_enc_ex_filters, const Vector<uint8_t> &p_key, uint64_t p_seed) {
 	CustomExportData *export_data = static_cast<CustomExportData *>(p_userdata);
-	String dst_path = p_path.replace_first("res://", export_data->assets_directory + "/");
-	print_verbose("Saving project files from " + p_path + " into " + dst_path);
+	const String path = ResourceUID::ensure_path(p_path);
+	const String dst_path = path.replace_first("res://", export_data->assets_directory + "/");
+	print_verbose("Saving project files from " + path + " into " + dst_path);
 	Error err = store_file_at_path(dst_path, p_data);
 	return err;
 }
