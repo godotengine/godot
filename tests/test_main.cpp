8014c351a27ae156b8ed60407f0ef298b072e9f5
Sequoia Haynes <supersequoia11@gmail.com>
2024-11-18 19:46:13 -0800

    Add test cases for Occluder Shapes

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -165,7 +165,10 @@
 
 #include "tests/scene/test_arraymesh.h"
 #include "tests/scene/test_camera_3d.h"
-#include "tests/scene/test_height_map_shape_3d.h"
+#include "tests/scene/test_convert_transform_modifier_3d.h"
+#include "tests/scene/test_copy_transform_modifier_3d.h"
+#include "tests/scene/test_gltf_document.h"
+#include "tests/scene/test_occluder_3d.h"
 #include "tests/scene/test_path_3d.h"
 #include "tests/scene/test_path_follow_3d.h"
 #include "tests/scene/test_primitives.h"
