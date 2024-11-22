1a75280e67bb899f2cd141fe0b6095be779468e7
Rob Mayoff <mayoff@dqd.com>
2024-11-22 13:13:41 -0600

    fix copy/paste error (fixes #99518)
    Replace erroneous use of `/` with `%`.

--- core/variant/variant_op.h
+++ core/variant/variant_op.h
@@ -317,7 +317,7 @@ public:
 		*VariantGetInternalPtr<Vector2i>::get_ptr(r_ret) = *VariantGetInternalPtr<Vector2i>::get_ptr(left) % *VariantGetInternalPtr<Vector2i>::get_ptr(right);
 	}
 	static void ptr_evaluate(const void *left, const void *right, void *r_ret) {
-		PtrToArg<Vector2i>::encode(PtrToArg<Vector2i>::convert(left) / PtrToArg<Vector2i>::convert(right), r_ret);
+		PtrToArg<Vector2i>::encode(PtrToArg<Vector2i>::convert(left) % PtrToArg<Vector2i>::convert(right), r_ret);
 	}
 	static Variant::Type get_return_type() { return GetTypeInfo<Vector2i>::VARIANT_TYPE; }
 };
