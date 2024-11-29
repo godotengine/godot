6af9d6421f0226a9d83373b258e8474a17c1e72d
demolke <demolke@gmail.com>
2024-11-29 21:22:49 +0100

    Destroy EditorFilesystem/EditorSettings on test shutdown
    Otherwise asan complains if a test tries to use these.
    
    Split off from https://github.com/godotengine/godot/pull/98909

--- tests/test_main.cpp
+++ tests/test_main.cpp
@@ -370,6 +370,9 @@ struct GodotTestCaseListener : public doctest::IReporter {
 		if (EditorSettings::get_singleton()) {
 			EditorSettings::destroy();
 		}
+		if (EditorPaths::get_singleton()) {
+			EditorPaths::free();
+		}
 #endif // TOOLS_ENABLED
 
 		Engine::get_singleton()->set_editor_hint(false);
