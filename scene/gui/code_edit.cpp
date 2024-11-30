65ac78c2c0c122be3df8087b14851f09ebcc3d9a
Malcolm Anderson <malcolminyo@gmail.com>
2024-11-30 09:50:08 -0800

    Support for deprecated display in language server
    CodeCompletionOptions need to be given the correct deprecated status still
    
    Variables marked as deprecated are recognized
    
    Attempt to get deprecated info about GDScript functions
    
    Consult DocData for more things being marked deprecated
    
    @GDScript methods and other things work now!
    
    Support for methods on built-in classes
    
    Add support for enums, and clean up some of the code to not use iterators when not necessary
    
    Added more deprecated checks
    
    Attempt to understand named enum property documentation
    
    Display deprecated functions with strikethrough in built-in script editor
    
    Named enum members now working!
    
    Recognize deprecated built-in classes when used as types
    
    Add deprecated checks for some spots, while trying to find autoload spot
    
    Added better TODO note
    
    Autoloads do technically kinda work, but only if you clear .godot
    
    Add documentation for most code completion options, and remove some old debugging code
    
    Improve comments
    
    Improve documentation, remove some debugging statements
    
    Revert use of tags in language server
    
    Provide example in comments for native enum value in context with native enum type
    
    Replace HashMaps with HashSets for better performance
    
    Make naming of `class_doc_map` consistent
    
    Scale strikethrough to match editor scale
    
    Add support for local identifiers
    
    Check for validity of `EditorHelp::get_doc_data` return value before attempting to use it
    
    Update CodeEdit and its tests
    
    Update glue
    
    Fix glue more (compiles on my machine)
    
    Update documentation for CodeEdit to include `deprecated` argument for `add_code_completion_option`
    
    Add compatibility method to pass CI tests
    
    Continue to try to get compatibility method to work
    
    Avoid making changes to original compatibility method
    
    Maybe it will work this time??
    
    Please
    
    Add another API compatibility check thing
    
    Updated API validation thing
    
    Replaced uses of `get_global_name()` with `get_doc_class_name()` as per dalexeev's suggestion
    
    Update doc/classes/CodeEdit.xml
    
    Co-authored-by: Hugo Locurcio <hugo.locurcio@hugo.pro>
    
    Attempt to use ascent instead of whole enclosing rect for strikethrough position
    
    Apply suggestions from code review
    
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
    Co-authored-by: HolonProduction <holonproduction@gmail.com>
    
    Some cleanup
    
    Move `deprecated` field for completion item to correct position
    
    Incorporate suggestions by HolonProduction
    
    Incorporate more suggestions by HolonProduction
    
    Remove ERR_FAIL_COND_MSG usages, as they would cause tests to fail
    
    # Conflicts:
    #	misc/extension_api_validation/4.4-stable_4.5-stable.expected
    #	modules/gdscript/gdscript_editor.cpp
    
    Fix API compatibility breakage thing (maybe)
    
    # Conflicts:
    #	misc/extension_api_validation/4.5-stable.expected
    
    Update misc/extension_api_validation/4.5-stable.expected
    
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
    
    # Conflicts:
    #	misc/extension_api_validation/4.5-stable.expected
    
    # Conflicts:
    #	misc/extension_api_validation/4.5-stable.expected
    #	modules/gdscript/gdscript_editor.cpp

--- scene/gui/code_edit.cpp
+++ scene/gui/code_edit.cpp
@@ -227,6 +227,18 @@ void CodeEdit::_notification(int p_what) {
 							tl->set_horizontal_alignment(HORIZONTAL_ALIGNMENT_LEFT);
 						}
 
+						if (code_completion_options[i].deprecated) {
+							RID rid = tl->get_rid();
+							double l_ascent = TS->shaped_text_get_ascent(rid);
+							float st_width = MAX(1.0, TS->shaped_text_get_underline_thickness(rid) * get_theme_default_base_scale());
+
+							draw_line(
+									Vector2(title_pos.x, title_pos.y + l_ascent / 2 + st_width / 2),
+									Vector2(title_pos.x + tl->get_line_width(), title_pos.y + l_ascent / 2 + st_width / 2),
+									code_completion_options[l].font_color,
+									st_width);
+						}
+
 						Point2 match_pos = Point2(code_completion_rect.position.x + icon_area_size.x + theme_cache.code_completion_icon_separation, code_completion_rect.position.y + i * row_height);
 
 						for (int j = 0; j < code_completion_options[l].matches.size(); j++) {
@@ -2237,7 +2249,7 @@ void CodeEdit::request_code_completion(bool p_force) {
 	}
 }
 
-void CodeEdit::add_code_completion_option(CodeCompletionKind p_type, const String &p_display_text, const String &p_insert_text, const Color &p_text_color, const Ref<Resource> &p_icon, const Variant &p_value, int p_location) {
+void CodeEdit::add_code_completion_option(CodeCompletionKind p_type, const String &p_display_text, const String &p_insert_text, const Color &p_text_color, const Ref<Resource> &p_icon, const Variant &p_value, int p_location, bool p_deprecated) {
 	ScriptLanguage::CodeCompletionOption completion_option;
 	completion_option.kind = (ScriptLanguage::CodeCompletionKind)p_type;
 	completion_option.display = p_display_text;
@@ -2246,6 +2258,7 @@ void CodeEdit::add_code_completion_option(CodeCompletionKind p_type, const Strin
 	completion_option.icon = p_icon;
 	completion_option.default_value = p_value;
 	completion_option.location = p_location;
+	completion_option.deprecated = p_deprecated;
 	code_completion_option_submitted.push_back(completion_option);
 }
 
@@ -2272,6 +2285,7 @@ TypedArray<Dictionary> CodeEdit::get_code_completion_options() const {
 		option["icon"] = code_completion_options[i].icon;
 		option["location"] = code_completion_options[i].location;
 		option["default_value"] = code_completion_options[i].default_value;
+		option["deprecated"] = code_completion_options[i].deprecated;
 		completion_options[i] = option;
 	}
 	return completion_options;
@@ -2291,6 +2305,7 @@ Dictionary CodeEdit::get_code_completion_option(int p_index) const {
 	option["icon"] = code_completion_options[p_index].icon;
 	option["location"] = code_completion_options[p_index].location;
 	option["default_value"] = code_completion_options[p_index].default_value;
+	option["deprecated"] = code_completion_options[p_index].deprecated;
 	return option;
 }
 
@@ -2870,7 +2885,7 @@ void CodeEdit::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("get_text_for_code_completion"), &CodeEdit::get_text_for_code_completion);
 	ClassDB::bind_method(D_METHOD("request_code_completion", "force"), &CodeEdit::request_code_completion, DEFVAL(false));
-	ClassDB::bind_method(D_METHOD("add_code_completion_option", "type", "display_text", "insert_text", "text_color", "icon", "value", "location"), &CodeEdit::add_code_completion_option, DEFVAL(Color(1, 1, 1)), DEFVAL(Ref<Resource>()), DEFVAL(Variant()), DEFVAL(LOCATION_OTHER));
+	ClassDB::bind_method(D_METHOD("add_code_completion_option", "type", "display_text", "insert_text", "text_color", "icon", "value", "location", "deprecated"), &CodeEdit::add_code_completion_option, DEFVAL(Color(1, 1, 1)), DEFVAL(Ref<Resource>()), DEFVAL(Variant()), DEFVAL(LOCATION_OTHER), DEFVAL(false));
 	ClassDB::bind_method(D_METHOD("update_code_completion_options", "force"), &CodeEdit::update_code_completion_options);
 	ClassDB::bind_method(D_METHOD("get_code_completion_options"), &CodeEdit::get_code_completion_options);
 	ClassDB::bind_method(D_METHOD("get_code_completion_option", "index"), &CodeEdit::get_code_completion_option);
@@ -3505,6 +3520,7 @@ void CodeEdit::_filter_code_completion_candidates_impl() {
 			option["icon"] = E.icon;
 			option["default_value"] = E.default_value;
 			option["location"] = E.location;
+			option["deprecated"] = E.deprecated;
 			completion_options_sources[i] = option;
 			i++;
 		}
