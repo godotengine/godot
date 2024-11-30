9e78504f497128361d522ef1c402978979e5642c
Rémi Verschelde <rverschelde@gmail.com>
2024-11-30 01:40:16 +0100

    basis_universal: Clarify encoder-only dependencies, only used in editor builds

--- modules/basis_universal/config.py
+++ modules/basis_universal/config.py
@@ -1,5 +1,6 @@
 def can_build(env, platform):
-    env.module_add_dependencies("basis_universal", ["jpg"])
+    if env.editor_build:  # Encoder dependencies
+        env.module_add_dependencies("basis_universal", ["jpg", "tinyexr"])
     return True
 
 
