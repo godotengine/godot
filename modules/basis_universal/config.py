2ac6d5be55e30f730750aa263f1d584d294e8b2d
Rémi Verschelde <rverschelde@gmail.com>
2024-11-30 01:25:53 +0100

    Revert "basis_universal: Make tinyexr dependency explicit"
    This reverts commit 220f2611cc19d2d4bd46050e55f5368c044dbb9b.

--- modules/basis_universal/config.py
+++ modules/basis_universal/config.py
@@ -1,5 +1,5 @@
 def can_build(env, platform):
-    env.module_add_dependencies("basis_universal", ["jpg", "tinyexr"])
+    env.module_add_dependencies("basis_universal", ["jpg"])
     return True
 
 
