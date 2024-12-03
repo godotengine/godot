14e4e3424f4e41400524822bbd493cac8e5b121c
Fabio Alessandrelli <fabio.alessandrelli@gmail.com>
2024-12-03 14:04:25 +0100

    [Web] Implement dummy NetSocketWeb
    This makes the class available to avoid spam from classes using it, even
    if without any actual implementation (since raw sockets are not
    available on the web).

--- platform/web/os_web.cpp
+++ platform/web/os_web.cpp
@@ -34,6 +34,7 @@
 #include "display_server_web.h"
 #include "godot_js.h"
 #include "ip_web.h"
+#include "net_socket_web.h"
 
 #include "core/config/project_settings.h"
 #include "core/debugger/engine_debugger.h"
@@ -55,6 +56,7 @@ void OS_Web::alert(const String &p_alert, const String &p_title) {
 void OS_Web::initialize() {
 	OS_Unix::initialize_core();
 	IPWeb::make_default();
+	NetSocketWeb::make_default();
 	DisplayServerWeb::register_web_driver();
 }
 
