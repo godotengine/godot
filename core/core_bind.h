9320b756ec98d2fc99a3bc4fe4d060cf9cb04449
Ryan <73148864+Ryan-000@users.noreply.github.com>
2024-12-03 17:05:29 -0500

    Expose ResourceLoader::get_resource_type and ResourceLoader::get_resource_script_class
    Co-Authored-By: Pedro J. Estébanez <RandomShaper@users.noreply.github.com>

--- core/core_bind.h
+++ core/core_bind.h
@@ -85,6 +85,7 @@ public:
 	bool exists(const String &p_path, const String &p_type_hint = "");
 	ResourceUID::ID get_resource_uid(const String &p_path);
 	String get_resource_type(const String &p_path);
+	String get_resource_script_class(const String &p_path);
 
 	Vector<String> list_directory(const String &p_directory);
 
