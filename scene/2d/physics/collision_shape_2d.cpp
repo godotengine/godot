ba83383a1fb52f897a300d65291ec525300ebb16
smix8 <52464204+smix8@users.noreply.github.com>
2024-12-09 00:37:38 +0100

    Add basic editor editing for ConvexPolygonShape2D and ConcavePolygonShape2D
    Adds basic editor tooling to move the existing points of a ConvexPolygonShape2D or move the  segments of a ConcavePolygonShape2D while a CollisionShape2D node is selected.

--- scene/2d/physics/collision_shape_2d.cpp
+++ scene/2d/physics/collision_shape_2d.cpp
@@ -190,7 +190,7 @@ PackedStringArray CollisionShape2D::get_configuration_warnings() const {
 	Ref<ConvexPolygonShape2D> convex = shape;
 	Ref<ConcavePolygonShape2D> concave = shape;
 	if (convex.is_valid() || concave.is_valid()) {
-		warnings.push_back(RTR("Polygon-based shapes are not meant be used nor edited directly through the CollisionShape2D node. Please use the CollisionPolygon2D node instead."));
+		warnings.push_back(RTR("The CollisionShape2D node has limited editing options for polygon-based shapes. Consider using a CollisionPolygon2D node instead."));
 	}
 
 	return warnings;
