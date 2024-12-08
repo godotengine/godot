sheddy1/Synk

Add a diagnostic to Godot.NET.Sdk to prevent tools from exporting members of non-tool types

modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/Common.cs
@@ -107,6 +107,16 @@ namespace Godot.SourceGenerators
                 "Types not derived from Node should not export Node members. Node export is only supported in Node-derived classes.",
                 helpLinkUri: string.Format(_helpLinkFormat, "GD0107"));
 
+        public static readonly DiagnosticDescriptor ToolsCanOnlyExportToolsRule =
+            new DiagnosticDescriptor(id: "GD0108",
+                title: "Tools can only export tools or core types",
+                messageFormat: "Tools can only export tools or core types",
+                category: "Usage",
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: true,
+                "Types with the '[Tool]' attribute should not export members of custom types that do not have the '[Tool]' attribute.",
+                helpLinkUri: string.Format(_helpLinkFormat, "GD0108"));
+
         public static readonly DiagnosticDescriptor SignalDelegateMissingSuffixRule =
             new DiagnosticDescriptor(id: "GD0201",
                 title: "The name of the delegate must end with 'EventHandler'",

godotengine/godot

modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/Common.cs
@@ -137,6 +137,16 @@ namespace Godot.SourceGenerators
                 "The exported tool button is not a Callable. The '[ExportToolButton]' attribute is only supported on members of type Callable.",
                 helpLinkUri: string.Format(_helpLinkFormat, "GD0110"));
 
+        public static readonly DiagnosticDescriptor ToolsCanOnlyExportToolsRule =
+            new DiagnosticDescriptor(id: "GD0111",
+                title: "Tools can only export tools or native types",
+                messageFormat: "Tools can only export tools or native types",
+                category: "Usage",
+                DiagnosticSeverity.Error,
+                isEnabledByDefault: true,
+                "Types with the '[Tool]' attribute should not export members of script types that do not have the '[Tool]' attribute.",
+                helpLinkUri: string.Format(_helpLinkFormat, "GD0111"));
+
         public static readonly DiagnosticDescriptor SignalDelegateMissingSuffixRule =
             new DiagnosticDescriptor(id: "GD0201",
                 title: "The name of the delegate must end with 'EventHandler'",