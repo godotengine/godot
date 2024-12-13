c97813e61c05aeeaf835e77913cdde6d684146ef
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2024-12-13 05:01:28 -0800

    Push some warnings for CSG non manifold and other errors.
    In the scene tree add warnings.

--- modules/csg/csg_shape.h
+++ modules/csg/csg_shape.h
@@ -119,6 +119,7 @@ protected:
 	void _notification(int p_what);
 	virtual CSGBrush *_build_brush() = 0;
 	void _make_dirty(bool p_parent_removing = false);
+	PackedStringArray get_configuration_warnings() const override;
 
 	static void _bind_methods();
 
