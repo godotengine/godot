9406926f8bb75c2fa17a6e64906928daf3bde7d8
Pedro J. Estébanez <pedrojrulez@gmail.com>
2024-12-13 12:49:10 +0100

    Fix deadlock possibility in threaded load of materials

--- scene/resources/material.cpp
+++ scene/resources/material.cpp
@@ -1878,9 +1878,13 @@ void BaseMaterial3D::flush_changes() {
 }
 
 void BaseMaterial3D::_queue_shader_change() {
+	if (!_is_initialized()) {
+		return;
+	}
+
 	MutexLock lock(material_mutex);
 
-	if (_is_initialized() && !element.in_list()) {
+	if (!element.in_list()) {
 		dirty_materials.add(&element);
 	}
 }
