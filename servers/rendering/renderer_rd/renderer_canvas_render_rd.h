d00fdbf466b9cd867bddc0b97da54c6f4e208153
clayjohn <claynjohn@gmail.com>
2024-12-16 21:00:11 -0800

    Add positional light count into a specialization constant for canvas item shader.
    This breaks batching when light count changes, but it allows the shader compiler to unroll the shader and make it much for efficient.
    
    Since the lighting code is heavily GPU ALU bound, this is a net win for games using 2D lighting.

--- servers/rendering/renderer_rd/renderer_canvas_render_rd.h
+++ servers/rendering/renderer_rd/renderer_canvas_render_rd.h
@@ -65,7 +65,7 @@ class RendererCanvasRenderRD : public RendererCanvasRender {
 	};
 
 	enum {
-		INSTANCE_FLAGS_LIGHT_COUNT_SHIFT = 0, // 4 bits for light count.
+		// 4 bits free at the front.
 
 		INSTANCE_FLAGS_CLIP_RECT_UV = (1 << 4),
 		INSTANCE_FLAGS_TRANSPOSE_RECT = (1 << 5),
@@ -119,7 +119,8 @@ class RendererCanvasRenderRD : public RendererCanvasRender {
 	struct ShaderSpecialization {
 		union {
 			struct {
-				uint32_t use_lighting : 1;
+				uint32_t use_directional_lighting : 1;
+				uint32_t positional_light_count : 4;
 			};
 
 			uint32_t packed_0;
@@ -498,7 +499,8 @@ class RendererCanvasRenderRD : public RendererCanvasRender {
 		Item::Command::Type command_type = Item::Command::TYPE_ANIMATION_SLICE; // Can default to any type that doesn't form a batch.
 		ShaderVariant shader_variant = SHADER_VARIANT_QUAD;
 		RD::RenderPrimitive render_primitive = RD::RENDER_PRIMITIVE_TRIANGLES;
-		bool use_lighting = false;
+		bool use_directional_lighting = false;
+		uint32_t positional_light_count = 0;
 
 		// batch-specific data
 		union {
