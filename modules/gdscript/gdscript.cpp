4f75ff5c952f945c9d8059752b3872b602c292d0
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2024-12-20 00:48:06 +0800

    Remove unused variable in GDScriptLanguage.

--- modules/gdscript/gdscript.cpp
+++ modules/gdscript/gdscript.cpp
@@ -2727,8 +2727,6 @@ void GDScriptLanguage::reload_tool_script(const Ref<Script> &p_script, bool p_so
 }
 
 void GDScriptLanguage::frame() {
-	calls = 0;
-
 #ifdef DEBUG_ENABLED
 	if (profiling) {
 		MutexLock lock(mutex);
@@ -2942,7 +2940,6 @@ String GDScriptLanguage::get_global_class_name(const String &p_path, String *r_b
 thread_local GDScriptLanguage::CallStack GDScriptLanguage::_call_stack;
 
 GDScriptLanguage::GDScriptLanguage() {
-	calls = 0;
 	ERR_FAIL_COND(singleton);
 	singleton = this;
 	strings._init = StaticCString::create("_init");
