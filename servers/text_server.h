ebea3898d92b23b0fcefce39e9204d1c7baca860
Thaddeus Crews <repiteo@outlook.com>
2024-12-23 20:08:12 -0600

    Core: Bump C++ Standard to C++20

--- servers/text_server.h
+++ servers/text_server.h
@@ -595,7 +595,6 @@ struct Glyph {
 	int span_index = -1;
 
 	bool operator==(const Glyph &p_a) const;
-	bool operator!=(const Glyph &p_a) const;
 
 	bool operator<(const Glyph &p_a) const;
 	bool operator>(const Glyph &p_a) const;
