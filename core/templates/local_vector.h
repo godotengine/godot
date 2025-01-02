ba146581b2a6ab39f087865e6ef100c78c587c01
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-01-02 16:48:54 +0100

    Rename `LocalVector.invert()` -> `LocalVector.reverse()` to match the `Vector`, `String` and `List` APIs.

--- core/templates/local_vector.h
+++ core/templates/local_vector.h
@@ -126,11 +126,14 @@ public:
 		return occurrences;
 	}
 
-	void invert() {
+	void reverse() {
 		for (U i = 0; i < count / 2; i++) {
 			SWAP(data[i], data[count - i - 1]);
 		}
 	}
+#ifndef DISABLE_DEPRECATED
+	[[deprecated("Use reverse() instead")]] void invert() { reverse(); }
+#endif
 
 	_FORCE_INLINE_ void clear() { resize(0); }
 	_FORCE_INLINE_ void reset() {
