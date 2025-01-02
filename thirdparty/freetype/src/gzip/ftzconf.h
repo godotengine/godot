1eca27f9669d4a44ce95cb748f7a9874e628b7b1
bherdm <bherdm@gmail.com>
2025-01-02 11:16:12 -0600

    OSX: Fix compiling with Xcode 16 on macOS 15
    Updated a line in the included FreeType 1.8.1 from FreeType 1.12.0+ to be compatible with Xcode 16.

--- thirdparty/freetype/src/gzip/ftzconf.h
+++ thirdparty/freetype/src/gzip/ftzconf.h
@@ -215,7 +215,7 @@
 #   define FAR
 #endif
 
-#if !defined(MACOS) && !defined(TARGET_OS_MAC)
+#if !defined(__MACTYPES__)
 typedef unsigned char  Byte;  /* 8 bits */
 #endif
 typedef unsigned int   uInt;  /* 16 bits or more */
