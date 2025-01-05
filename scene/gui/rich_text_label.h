13ab41c3f9b7445566a527b47cc37b7c042fa4f1
Tyler Paul <tylerpaul2329@gmail.com>
2025-01-05 17:20:18 -0500

    Added RichTextLabel::set_clearing_enabled
    Adds a clear option to the context menu. Disabled by default. Enabled in EditorLog

--- scene/gui/rich_text_label.h
+++ scene/gui/rich_text_label.h
@@ -100,6 +100,7 @@ public:
 	};
 
 	enum MenuItems {
+		MENU_CLEAR,
 		MENU_COPY,
 		MENU_SELECT_ALL,
 		MENU_MAX
@@ -547,6 +548,8 @@ private:
 	bool context_menu_enabled = false;
 	bool shortcut_keys_enabled = true;
 
+	bool clearing_enabled = false;
+
 	// Context menu.
 	PopupMenu *menu = nullptr;
 	void _generate_context_menu();
@@ -761,6 +764,9 @@ public:
 
 	void set_fit_content(bool p_enabled);
 	bool is_fit_content_enabled() const;
+
+	void set_clearing_enabled(bool p_clear);
+	bool is_clearing_enabled();
 
 	bool search(const String &p_string, bool p_from_selection = false, bool p_search_previous = false);
 
