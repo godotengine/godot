0fa1149a433872e97a232acb111784419536466d
Rémi Verschelde <rverschelde@gmail.com>
2025-01-08 10:40:17 +0100

    Fix GCC warning about potential stringop-overflow in AudioEffectPitchShift
    Fixes #101236.

--- servers/audio/effects/audio_effect_pitch_shift.h
+++ servers/audio/effects/audio_effect_pitch_shift.h
@@ -77,7 +77,7 @@ class AudioEffectPitchShift : public AudioEffect {
 public:
 	friend class AudioEffectPitchShiftInstance;
 
-	enum FFTSize {
+	enum FFTSize : unsigned int {
 		FFT_SIZE_256,
 		FFT_SIZE_512,
 		FFT_SIZE_1024,
