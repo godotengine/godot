21fcb56547010b2c85debcf7c7a358b6387d6328
Rémi Verschelde <rverschelde@gmail.com>
2025-01-14 10:59:40 +0100

    Remove unused `EditorSceneFormatImporter::_get_import_flags`
    This has never been used since Godot was open sourced.
    
    Import flags are used but directly through `_import_scene`.

--- modules/fbx/editor/editor_scene_importer_fbx2gltf.cpp
+++ modules/fbx/editor/editor_scene_importer_fbx2gltf.cpp
@@ -38,10 +38,6 @@
 
 #include "modules/gltf/gltf_document.h"
 
-uint32_t EditorSceneFormatImporterFBX2GLTF::get_import_flags() const {
-	return ImportFlags::IMPORT_SCENE | ImportFlags::IMPORT_ANIMATION;
-}
-
 void EditorSceneFormatImporterFBX2GLTF::get_extensions(List<String> *r_extensions) const {
 	r_extensions->push_back("fbx");
 }
