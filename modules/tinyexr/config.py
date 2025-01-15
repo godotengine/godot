e2cdd2592b27792a30e6b61f86100ed6505b60ad
metamuffin <metamuffin@disroot.org>
2025-01-15 17:28:08 +0100

    enable tinyexr by default

--- modules/tinyexr/config.py
+++ modules/tinyexr/config.py
@@ -1,13 +1,5 @@
 def can_build(env, platform):
-    return env["tinyexr_export_templates"] or env.editor_build
-
-
-def get_opts(platform):
-    from SCons.Variables import BoolVariable
-
-    return [
-        BoolVariable("tinyexr_export_templates", "Enable tinyexr module in non-editor builds too", False),
-    ]
+    return True
 
 
 def configure(env):
