8162bf6d7b637a70e76f88e5d850d9329e8e5c89
Bastiaan Olij <mux213@gmail.com>
2025-01-23 17:50:07 +1100

    Adding support for the OpenXR futures extension

--- modules/openxr/extensions/openxr_ml2_controller_extension.h
+++ modules/openxr/extensions/openxr_ml2_controller_extension.h
@@ -33,6 +33,11 @@
 #include "openxr_extension_wrapper.h"
 
 class OpenXRML2ControllerExtension : public OpenXRExtensionWrapper {
+	GDCLASS(OpenXRML2ControllerExtension, OpenXRExtensionWrapper);
+
+protected:
+	static void _bind_methods() {}
+
 public:
 	virtual HashMap<String, bool *> get_requested_extensions() override;
 
