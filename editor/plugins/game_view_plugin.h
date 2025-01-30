e8df7f115706b3f0ac7958c7bc609c85abeb6bf5
Matt Enad <m22mail@comcast.net>
2025-01-29 19:19:38 -0500

    Add embedded window selector & dual game view
    This commit allows the `GameView` to select either the "Main Window" (default view of the running game) or a subwindow to become embedded when running the project. This information is communicated to the built game via a new `--swid` (subwindow ID) command line parameter which matches the `--wid` implementation created for the original embedding feature. The subwindow is chosen by matching the name of the window to a user provided name which is given in a text box in the `GameView`.
    
    This is implemented only for Windows and a follow up PR should be able to match the functionality for Linux easily.
    
    Additionally, to utilize this new behavior, there is now the option to enable a second `GameView` that sits above the main one. The second view can embed a different window and has its own embedding controls to improve user debugging. For now, in order to keep the PR smaller, the secondary `GameView` does not utilize its own SceneDebugger, so only the main view can use the runtime selection options. A future PR can open up more functionality for parity with the main view.

--- editor/plugins/game_view_plugin.h
+++ editor/plugins/game_view_plugin.h
@@ -36,11 +36,16 @@
 #include "editor/plugins/editor_plugin.h"
 #include "scene/debugger/scene_debugger.h"
 #include "scene/gui/box_container.h"
+#include "scene/gui/line_edit.h"
+#include "scene/gui/option_button.h"
+#include "scene/gui/split_container.h"
 
 class EmbeddedProcess;
 class VSeparator;
 class WindowWrapper;
 class ScriptEditorDebugger;
+class GameViewPlugin;
+class GameView;
 
 class GameViewDebugger : public EditorDebuggerPlugin {
 	GDCLASS(GameViewDebugger, EditorDebuggerPlugin);
@@ -54,8 +59,9 @@ private:
 	int select_mode = RuntimeNodeSelect::SELECT_MODE_SINGLE;
 	bool mute_audio = false;
 	EditorDebuggerNode::CameraOverride camera_override_mode = EditorDebuggerNode::OVERRIDE_INGAME;
+	GameView *game_view = nullptr;
 
-	void _session_started(Ref<EditorDebuggerSession> p_session);
+	void _session_started(Ref<EditorDebuggerSession> p_session, GameView *p_game_view);
 	void _session_stopped();
 
 	void _feature_profile_changed();
@@ -80,6 +86,8 @@ public:
 	void reset_camera_2d_position();
 	void reset_camera_3d_position();
 
+	void set_game_view(GameView *p_game_view);
+
 	virtual void setup_session(int p_session_id) override;
 
 	GameViewDebugger();
@@ -95,6 +103,10 @@ class GameView : public VBoxContainer {
 		CAMERA_MODE_EDITORS,
 		EMBED_RUN_GAME_EMBEDDED,
 		EMBED_MAKE_FLOATING_ON_PLAY,
+		WINDOW_SELECT_MAIN,
+		WINDOW_SELECT_CUSTOM,
+		GAME_VIEW_SINGLE,
+		GAME_VIEW_DUAL,
 	};
 
 	enum EmbedSizeMode {
@@ -103,22 +115,15 @@ class GameView : public VBoxContainer {
 		SIZE_MODE_STRETCH,
 	};
 
-	enum EmbedAvailability {
-		EMBED_AVAILABLE,
-		EMBED_NOT_AVAILABLE_FEATURE_NOT_SUPPORTED,
-		EMBED_NOT_AVAILABLE_MINIMIZED,
-		EMBED_NOT_AVAILABLE_MAXIMIZED,
-		EMBED_NOT_AVAILABLE_FULLSCREEN,
-		EMBED_NOT_AVAILABLE_SINGLE_WINDOW_MODE,
-		EMBED_NOT_AVAILABLE_PROJECT_DISPLAY_DRIVER,
-	};
-
-	inline static GameView *singleton = nullptr;
-
 	Ref<GameViewDebugger> debugger;
+	GameViewPlugin *plugin = nullptr;
 	WindowWrapper *window_wrapper = nullptr;
 
+	String setting_prefix = "";
+	Dictionary *state_dictionary;
+
 	bool is_feature_enabled = true;
+	bool debugger_controls_enabled = true;
 	int active_sessions = 0;
 	int screen_index_before_start = -1;
 	ScriptEditorDebugger *embedded_script_debugger = nullptr;
@@ -128,6 +133,8 @@ class GameView : public VBoxContainer {
 	EmbedSizeMode embed_size_mode = SIZE_MODE_FIXED;
 	bool paused = false;
 	Size2 size_paused;
+	bool show_embedded_window_title = false;
+	String embedded_window_title = "";
 
 	Rect2i floating_window_rect;
 	int floating_window_screen = -1;
@@ -136,13 +143,18 @@ class GameView : public VBoxContainer {
 
 	Button *suspend_button = nullptr;
 	Button *next_frame_button = nullptr;
+	VSeparator *time_buttons_separator = nullptr;
 
 	Button *node_type_button[RuntimeNodeSelect::NODE_TYPE_MAX];
+	VSeparator *node_type_separator = nullptr;
 	Button *select_mode_button[RuntimeNodeSelect::SELECT_MODE_MAX];
+	VSeparator *select_mode_separator = nullptr;
 
 	Button *hide_selection = nullptr;
+	VSeparator *hide_selection_separator = nullptr;
 
 	Button *debug_mute_audio_button = nullptr;
+	VSeparator *debug_mute_audio_separator = nullptr;
 
 	Button *camera_override_button = nullptr;
 	MenuButton *camera_override_menu = nullptr;
@@ -154,6 +166,8 @@ class GameView : public VBoxContainer {
 	MenuButton *embed_options_menu = nullptr;
 	Label *game_size_label = nullptr;
 	Panel *panel = nullptr;
+	OptionButton *window_select_dropdown = nullptr;
+	LineEdit *window_select_text = nullptr;
 	EmbeddedProcess *embedded_process = nullptr;
 	Label *state_label = nullptr;
 
@@ -167,10 +181,10 @@ class GameView : public VBoxContainer {
 	void _select_mode_pressed(int p_option);
 	void _embed_options_menu_menu_id_pressed(int p_id);
 	void _size_mode_button_pressed(int size_mode);
+	void _select_window_dropdown_pressed(int p_id);
+	void _window_select_text_changed(String p_text);
 
 	void _play_pressed();
-	static void _instance_starting_static(int p_idx, List<String> &r_arguments);
-	void _instance_starting(int p_idx, List<String> &r_arguments);
 	void _stop_pressed();
 	void _embedding_completed();
 	void _embedding_failed();
@@ -178,12 +192,8 @@ class GameView : public VBoxContainer {
 	void _embedded_process_focused();
 	void _editor_or_project_settings_changed();
 
-	EmbedAvailability _get_embed_available();
 	void _update_ui();
 	void _update_embed_menu_options();
-	void _update_embed_window_size();
-	void _update_arguments_for_instance(int p_idx, List<String> &r_arguments);
-	void _show_update_window_wrapper();
 
 	void _hide_selection_toggled(bool p_pressed);
 
@@ -206,49 +216,94 @@ protected:
 	void _notification(int p_what);
 
 public:
-	void set_state(const Dictionary &p_state);
-	Dictionary get_state() const;
+	void set_is_feature_enabled(bool p_enabled);
+	void set_time_buttons_enabled(bool p_enabled);
+	void set_debugger_controls_enabled(bool p_enabled);
+	void set_embedded_window_title(String p_embedded_window_title);
+	String get_embedded_window_title();
+	bool get_embed_on_play();
+	bool get_make_floating_on_play();
+	bool is_view_embedding();
+
+	EmbeddedProcess *get_embedded_process();
+	GameViewPlugin *get_plugin();
+
+	void update_all_ui();
+	void update_embed_window_size();
+	void show_update_window_wrapper();
 
 	void set_window_layout(Ref<ConfigFile> p_layout);
 	void get_window_layout(Ref<ConfigFile> p_layout);
 
-	GameView(Ref<GameViewDebugger> p_debugger, WindowWrapper *p_wrapper);
+	GameView(Ref<GameViewDebugger> p_debugger, GameViewPlugin *p_plugin, WindowWrapper *p_wrapper, String p_setting_prefix);
 };
 
 class GameViewPlugin : public EditorPlugin {
 	GDCLASS(GameViewPlugin, EditorPlugin);
 
+	inline static GameViewPlugin *singleton = nullptr;
+
+	VSplitContainer *game_view_layout = nullptr;
+
 #ifndef ANDROID_ENABLED
 	GameView *game_view = nullptr;
 	WindowWrapper *window_wrapper = nullptr;
+	GameView *top_game_view = nullptr;
+	WindowWrapper *top_window_wrapper = nullptr;
 #endif // ANDROID_ENABLED
 
+	Ref<GameViewDebugger> top_debugger;
 	Ref<GameViewDebugger> debugger;
 
 	String last_editor;
 
-#ifndef ANDROID_ENABLED
+	bool is_feature_enabled = true;
+	bool dual_view_enabled = false;
+
+	void _feature_profile_changed();
 	void _window_visibility_changed(bool p_visible);
-#endif // ANDROID_ENABLED
 	void _save_last_editor(const String &p_editor);
 	void _focus_another_editor();
 	bool _is_window_wrapper_enabled() const;
+
+	static void _instance_starting_static(int p_idx, List<String> &r_arguments);
+	void _instance_starting(int p_idx, List<String> &r_arguments);
+	void _update_arguments_for_game_instance(int p_idx, List<String> &r_arguments);
 
 protected:
 	void _notification(int p_what);
 
 public:
 	virtual String get_plugin_name() const override { return TTRC("Game"); }
+
+	enum EmbedAvailability {
+		EMBED_AVAILABLE,
+		EMBED_NOT_AVAILABLE_FEATURE_NOT_SUPPORTED,
+		EMBED_NOT_AVAILABLE_MINIMIZED,
+		EMBED_NOT_AVAILABLE_MAXIMIZED,
+		EMBED_NOT_AVAILABLE_FULLSCREEN,
+		EMBED_NOT_AVAILABLE_SINGLE_WINDOW_MODE,
+		EMBED_NOT_AVAILABLE_PROJECT_DISPLAY_DRIVER,
+	};
+
 	bool has_main_screen() const override { return true; }
 	virtual void edit(Object *p_object) override {}
 	virtual bool handles(Object *p_object) const override { return false; }
 	virtual void selected_notify() override;
 
 	Ref<GameViewDebugger> get_debugger() const { return debugger; }
+	GameView *get_game_view_with_main_screen_embed();
+	GameView *get_game_view_with_selected_window(String p_window_title);
+	EmbedAvailability get_embed_available();
+	GameView *get_top_game_view();
+	GameView *get_main_game_view();
+
+	void game_view_changed_window_target();
+	void enable_dual_game_view(bool p_enabled);
+	void determine_dual_view_visible();
 
 #ifndef ANDROID_ENABLED
 	virtual void make_visible(bool p_visible) override;
-
 	virtual void set_window_layout(Ref<ConfigFile> p_layout) override;
 	virtual void get_window_layout(Ref<ConfigFile> p_layout) override;
 #endif // ANDROID_ENABLED
