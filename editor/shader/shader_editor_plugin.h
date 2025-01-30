b829bfd8e5a5ac3c477ff4858f0ece3b0e30591d
Martin Delille <martin@delille.org>
2025-01-30 15:04:29 +0100

    Focus shader text editor when opened with quick open dialog

--- editor/shader/shader_editor_plugin.h
+++ editor/shader/shader_editor_plugin.h
@@ -95,6 +95,7 @@ class ShaderEditorPlugin : public EditorPlugin {
 	ShaderCreateDialog *shader_create_dialog = nullptr;
 
 	float text_shader_zoom_factor = 1.0f;
+	bool restoring_layout = false;
 
 	Ref<Resource> _get_current_shader();
 	void _update_shader_list();
@@ -124,7 +125,7 @@ class ShaderEditorPlugin : public EditorPlugin {
 	void _set_text_shader_zoom_factor(float p_zoom_factor);
 	void _update_shader_editor_zoom_factor(CodeTextEditor *p_shader_editor) const;
 
-	void _switch_to_editor(ShaderEditor *p_editor);
+	void _switch_to_editor(ShaderEditor *p_editor, bool p_focus = false);
 
 protected:
 	void _notification(int p_what);
