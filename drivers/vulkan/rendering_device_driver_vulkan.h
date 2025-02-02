89ba8daaa219e4edeb18171281f15e3033b52a1d
Josh Jones <kilauea.jones@gmail.com>
2025-02-02 15:12:45 -0800

    Support output to HDR monitors
    Co-authored-by: Alvin Wong <alvinhochun@gmail.com>
    Co-authored-by: Allen Pestaluky <allenpestaluky@gmail.com>

--- drivers/vulkan/rendering_device_driver_vulkan.h
+++ drivers/vulkan/rendering_device_driver_vulkan.h
@@ -375,6 +375,7 @@ private:
 #endif
 	};
 
+	bool _determine_swap_chain_format(RenderingContextDriver::SurfaceID p_surface, VkFormat &r_format, VkColorSpaceKHR &r_color_space);
 	void _swap_chain_release(SwapChain *p_swap_chain);
 
 public:
@@ -385,6 +386,7 @@ public:
 	virtual int swap_chain_get_pre_rotation_degrees(SwapChainID p_swap_chain) override final;
 	virtual DataFormat swap_chain_get_format(SwapChainID p_swap_chain) override final;
 	virtual void swap_chain_set_max_fps(SwapChainID p_swap_chain, int p_max_fps) override final;
+	virtual ColorSpace swap_chain_get_color_space(SwapChainID p_swap_chain) override final;
 	virtual void swap_chain_free(SwapChainID p_swap_chain) override final;
 
 private:
