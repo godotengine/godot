f9c82f406aed86ba12c6f14cf6a21687f5c06125
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-02-03 04:34:29 -0800

    Add support for using an Android Service to host the Godot engine - Provide a `GodotService` Android service implementation which can be used to host an instance of the Godot engine - Provide a `RemoteGodotFragment` Android fragment implementation which provides the view and logic to wrap connection to a `GodotService` instance

--- platform/android/java/lib/src/org/godotengine/godot/GodotActivity.kt
+++ platform/android/java/lib/src/org/godotengine/godot/GodotActivity.kt
@@ -39,6 +39,7 @@ import android.util.Log
 import androidx.annotation.CallSuper
 import androidx.annotation.LayoutRes
 import androidx.fragment.app.FragmentActivity
+import org.godotengine.godot.utils.CommandLineFileParser
 import org.godotengine.godot.utils.PermissionsUtil
 import org.godotengine.godot.utils.ProcessPhoenix
 
@@ -73,6 +74,13 @@ abstract class GodotActivity : FragmentActivity(), GodotHost {
 
 	@CallSuper
 	override fun onCreate(savedInstanceState: Bundle?) {
+		val assetsCommandLine = try {
+			CommandLineFileParser.parseCommandLine(assets.open("_cl_"))
+		} catch (ignored: Exception) {
+			mutableListOf()
+		}
+		commandLineParams.addAll(assetsCommandLine)
+
 		val params = intent.getStringArrayExtra(EXTRA_COMMAND_LINE_PARAMS)
 		Log.d(TAG, "Starting intent $intent with parameters ${params.contentToString()}")
 		commandLineParams.addAll(params ?: emptyArray())
@@ -107,12 +115,7 @@ abstract class GodotActivity : FragmentActivity(), GodotHost {
 
 	protected fun triggerRebirth(bundle: Bundle?, intent: Intent) {
 		// Launch a new activity
-		val godot = godot
-		if (godot != null) {
-			godot.destroyAndKillProcess {
-				ProcessPhoenix.triggerRebirth(this, bundle, intent)
-			}
-		} else {
+		Godot.getInstance(applicationContext).destroyAndKillProcess {
 			ProcessPhoenix.triggerRebirth(this, bundle, intent)
 		}
 	}
@@ -159,8 +162,6 @@ abstract class GodotActivity : FragmentActivity(), GodotHost {
 		intent = newIntent
 
 		handleStartIntent(newIntent, false)
-
-		godotFragment?.onNewIntent(newIntent)
 	}
 
 	private fun handleStartIntent(intent: Intent, newLaunch: Boolean) {
@@ -215,5 +216,6 @@ abstract class GodotActivity : FragmentActivity(), GodotHost {
 		return GodotFragment()
 	}
 
+	@CallSuper
 	override fun getCommandLine(): MutableList<String> = commandLineParams
 }
