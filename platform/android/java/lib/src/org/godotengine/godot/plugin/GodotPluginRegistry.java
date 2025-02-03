d99dac55bcf63cd52871ba499c93d93efdc2e137
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-02-03 04:34:29 -0800

    Add support for using an Android Service to host the Godot engine - Provide an `GodotService` Android service implementation which can be used to host an instance of the Godot engine - Provide an `RemoteGodotFragment` Android fragment implementation which provides the view and logic to wrap connection to a `GodotService` instance

--- platform/android/java/lib/src/org/godotengine/godot/plugin/GodotPluginRegistry.java
+++ platform/android/java/lib/src/org/godotengine/godot/plugin/GodotPluginRegistry.java
@@ -32,7 +32,7 @@ package org.godotengine.godot.plugin;
 
 import org.godotengine.godot.Godot;
 
-import android.app.Activity;
+import android.content.Context;
 import android.content.pm.ApplicationInfo;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
@@ -134,10 +134,10 @@ public final class GodotPluginRegistry {
 
 		// Register the manifest plugins
 		try {
-			final Activity activity = godot.getActivity();
-			ApplicationInfo appInfo = activity
+			final Context context = godot.getContext();
+			ApplicationInfo appInfo = context
 											  .getPackageManager()
-											  .getApplicationInfo(activity.getPackageName(),
+											  .getApplicationInfo(context.getPackageName(),
 													  PackageManager.GET_META_DATA);
 			Bundle metaData = appInfo.metaData;
 			if (metaData == null || metaData.isEmpty()) {
