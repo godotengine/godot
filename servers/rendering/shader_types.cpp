d16bd6ca0375a4c086c0ed90f7dbcb8f45d72c7a
Colin O'Rourke <csorourk@ucsc.edu>
2025-02-09 02:20:35 -0800

    DrawableTextures
    Implementing DrawableTextures based on: https://github.com/godotengine/godot-proposals/issues/7379

--- servers/rendering/shader_types.cpp
+++ servers/rendering/shader_types.cpp
@@ -516,11 +516,35 @@ ShaderTypes::ShaderTypes() {
 	shader_modes[RS::SHADER_FOG].functions["fog"].built_ins["EMISSION"] = ShaderLanguage::TYPE_VEC3;
 	shader_modes[RS::SHADER_FOG].functions["fog"].main_function = true;
 
+	/************ TEXTURE_BLIT **************************/
+
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["constants"].built_ins["PI"] = constt(ShaderLanguage::TYPE_FLOAT);
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["constants"].built_ins["TAU"] = constt(ShaderLanguage::TYPE_FLOAT);
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["constants"].built_ins["E"] = constt(ShaderLanguage::TYPE_FLOAT);
+
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["FRAGCOORD"] = constt(ShaderLanguage::TYPE_VEC4);
+
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["UV"] = constt(ShaderLanguage::TYPE_VEC2);
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["MODULATE"] = constt(ShaderLanguage::TYPE_VEC4);
+
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["COLOR"] = ShaderLanguage::TYPE_VEC4;
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["COLOR2"] = ShaderLanguage::TYPE_VEC4;
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["COLOR3"] = ShaderLanguage::TYPE_VEC4;
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].built_ins["COLOR4"] = ShaderLanguage::TYPE_VEC4;
+
+	shader_modes[RS::SHADER_TEXTURE_BLIT].functions["blit"].main_function = true;
+
+	// Texture Blit Modes
+	{
+		shader_modes[RS::SHADER_TEXTURE_BLIT].modes.push_back({ PNAME("blend"), "mix", "add", "sub", "mul", "disabled" });
+	}
+
 	shader_types_list.push_back("spatial");
 	shader_types_list.push_back("canvas_item");
 	shader_types_list.push_back("particles");
 	shader_types_list.push_back("sky");
 	shader_types_list.push_back("fog");
+	shader_types_list.push_back("texture_blit");
 
 	for (const String &type : shader_types_list) {
 		shader_types.insert(type);
