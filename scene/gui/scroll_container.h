fb9176a653082624d80a8c4342236f72fe2bc2e8
风青山 <idleman@yeah.net>
2025-02-11 09:33:15 +0800

    Improve `ScrollContainer::ensure_control_visible`
    Ensure minimum scrolling and maximum visibility in a single axis.
    
    For the normal case (rotation 0 or 90 degrees), this works fine.
    
    In some extreme cases (rotated or skewed), the controls may not be visible.
    Or the scroll position is not precise enough.
    
    Implement `Rect2::intersection_transformed()`
    
    A rect that just contains all the intersection points will be returned.
    
    Use `Rect2::intersection_transformed()` to get a more precise intersection rectangle

--- scene/gui/scroll_container.h
+++ scene/gui/scroll_container.h
@@ -92,6 +92,9 @@ private:
 	ScrollHintMode scroll_hint_mode = SCROLL_HINT_MODE_DISABLED;
 	bool tile_scroll_hint = false;
 
+	bool following = false;
+	Vector2 scroll_diff;
+
 	struct ThemeCache {
 		Ref<StyleBox> panel_style;
 		Ref<StyleBox> focus_style;
@@ -118,6 +121,8 @@ private:
 	bool child_has_focus();
 
 	Size2 _get_minimum_size(bool p_use_desired_sizes) const;
+
+	Rect2 _get_local_visible_rect() const;
 
 protected:
 	Size2 get_minimum_size() const override;
