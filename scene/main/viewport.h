54f68256cffea1fa236eceb6aa15dbf0d04bc27d
KerekesDavid <drakediskeve@gmail.com>
2025-02-12 01:18:58 +0100

    Fix wrong viewport scaling at intermediate resolutions
    Fixes viewport scale being wrong at resolutions that are not
    integer multiples of content_scale_factor.
    
    This is done by allowing size_2d_override to be Size2 instead of
    Size2i. This change is not propagated to the exposed SubViewport
    to keep compatibility for now.

--- scene/main/viewport.h
+++ scene/main/viewport.h
@@ -252,7 +252,7 @@ private:
 	Transform2D stretch_transform;
 
 	Size2i size = Size2i(512, 512);
-	Size2i size_2d_override;
+	Size2 size_2d_override;
 	bool size_allocated = false;
 
 	RID contact_2d_debug;
@@ -492,10 +492,10 @@ private:
 	void _window_start_resize(SubWindowResize p_edge, Window *p_window);
 
 protected:
-	bool _set_size(const Size2i &p_size, const Size2i &p_size_2d_override, bool p_allocated);
+	bool _set_size(const Size2i &p_size, const Size2 &p_size_2d_override, bool p_allocated);
 
 	Size2i _get_size() const;
-	Size2i _get_size_2d_override() const;
+	Size2 _get_size_2d_override() const;
 	bool _is_size_allocated() const;
 
 	void _notification(int p_what);
