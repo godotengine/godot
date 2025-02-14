9d157faacb6f50f3e0e3f5a8b50f1b843c3c068e
James Clancey <james.clancey@gmail.com>
2025-02-13 17:03:46 -0900

    WebGL can handle astc, and it should on mobile devices. However we don't check for WEBGL_compressed_texture_astc.

--- drivers/gles3/storage/config.cpp
+++ drivers/gles3/storage/config.cpp
@@ -78,7 +78,7 @@ Config::Config() {
 #endif
 
 	bptc_supported = extensions.has("GL_ARB_texture_compression_bptc") || extensions.has("EXT_texture_compression_bptc");
-	astc_supported = extensions.has("GL_KHR_texture_compression_astc") || extensions.has("GL_OES_texture_compression_astc") || extensions.has("GL_KHR_texture_compression_astc_ldr") || extensions.has("GL_KHR_texture_compression_astc_hdr");
+	astc_supported = extensions.has("WEBGL_compressed_texture_astc") || extensions.has("GL_KHR_texture_compression_astc") || extensions.has("GL_OES_texture_compression_astc") || extensions.has("GL_KHR_texture_compression_astc_ldr") || extensions.has("GL_KHR_texture_compression_astc_hdr");
 	astc_hdr_supported = extensions.has("GL_KHR_texture_compression_astc_hdr");
 	astc_layered_supported = extensions.has("GL_KHR_texture_compression_astc_sliced_3d");
 
