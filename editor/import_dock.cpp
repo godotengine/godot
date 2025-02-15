a044174bb694f8bed5f07dd86e9d505092180074
erodozer <ero@erodozer.moe>
2025-02-15 15:08:56 -0500

    Support multi part extensions in import plugins

--- editor/import_dock.cpp
+++ editor/import_dock.cpp
@@ -124,7 +124,7 @@ void ImportDock::set_edit_path(const String &p_path) {
 	_update_options(p_path, config);
 
 	List<Ref<ResourceImporter>> importers;
-	ResourceFormatImporter::get_singleton()->get_importers_for_extension(p_path.get_extension(), &importers);
+	ResourceFormatImporter::get_singleton()->get_importers_for_file(p_path, &importers);
 	List<Pair<String, String>> importer_names;
 
 	for (const Ref<ResourceImporter> &E : importers) {
@@ -314,7 +314,7 @@ void ImportDock::set_edit_multiple_paths(const Vector<String> &p_paths) {
 	params->update();
 
 	List<Ref<ResourceImporter>> importers;
-	ResourceFormatImporter::get_singleton()->get_importers_for_extension(p_paths[0].get_extension(), &importers);
+	ResourceFormatImporter::get_singleton()->get_importers_for_file(p_paths[0], &importers);
 	List<Pair<String, String>> importer_names;
 
 	for (const Ref<ResourceImporter> &E : importers) {
