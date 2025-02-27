13412ff84de4847a8931f8c65376c38358571ef9
lawnjelly <lawnjelly@gmail.com>
2025-02-27 10:38:06 +0000

    Physics Interpolation - Add editor warning for non-interpolated physics body
    Also add warning for interpolated Spatials that are children of non-interpolated nodes.

--- scene/3d/physics_body.cpp
+++ scene/3d/physics_body.cpp
@@ -98,6 +98,19 @@ void PhysicsBody::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("_get_layers"), &PhysicsBody::_get_layers);
 }
 
+String PhysicsBody::get_configuration_warning() const {
+	String warning = CollisionObject::get_configuration_warning();
+
+	if (!is_physics_interpolated()) {
+		if (!warning.empty()) {
+			warning += "\n\n";
+		}
+		warning += TTR("PhysicsBody will not work correctly on a non-interpolated branch of the SceneTree.\nCheck the node's inherited physics_interpolation_mode.");
+	}
+
+	return warning;
+}
+
 PhysicsBody::PhysicsBody(PhysicsServer::BodyMode p_mode) :
 		CollisionObject(RID_PRIME(PhysicsServer::get_singleton()->body_create(p_mode)), false) {
 }
@@ -783,7 +796,7 @@ Array RigidBody::get_colliding_bodies() const {
 String RigidBody::get_configuration_warning() const {
 	Transform t = get_transform();
 
-	String warning = CollisionObject::get_configuration_warning();
+	String warning = PhysicsBody::get_configuration_warning();
 
 	if ((get_mode() == MODE_RIGID || get_mode() == MODE_CHARACTER) && (ABS(t.basis.get_axis(0).length() - 1.0) > 0.05 || ABS(t.basis.get_axis(1).length() - 1.0) > 0.05 || ABS(t.basis.get_axis(2).length() - 1.0) > 0.05)) {
 		if (warning != String()) {
