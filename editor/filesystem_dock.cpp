9dcf720f50c9953fdfde84aec458d7e659af9c62
kobewi <kobewi4e@gmail.com>
2025-03-07 00:26:02 +0100

    Re-grab FileSystem focus after rescan

--- editor/filesystem_dock.cpp
+++ editor/filesystem_dock.cpp
@@ -1338,6 +1338,10 @@ void FileSystemDock::_fs_changed() {
 	}
 
 	set_process(false);
+	if (had_focus) {
+		had_focus->grab_focus();
+		had_focus = nullptr;
+	}
 }
 
 void FileSystemDock::_set_scanning_mode() {
@@ -2675,6 +2679,12 @@ bool FileSystemDock::_matches_all_search_tokens(const String &p_text) {
 }
 
 void FileSystemDock::_rescan() {
+	if (tree->has_focus()) {
+		had_focus = tree;
+	} else if (files->has_focus()) {
+		had_focus = files;
+	}
+
 	_set_scanning_mode();
 	EditorFileSystem::get_singleton()->scan();
 }
