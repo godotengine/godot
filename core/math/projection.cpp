c28e0f0402cd9b96a93d7375f457b14e8d0ed128
MaxIsJoe <34368774+MaxIsJoe@users.noreply.github.com>
2025-03-07 02:02:05 +0200

    Update projection.cpp

--- core/math/projection.cpp
+++ core/math/projection.cpp
@@ -38,7 +38,7 @@
 #include "core/string/ustring.h"
 
 real_t Projection::determinant() const {
-
+	// https://en.wikipedia.org/wiki/Laplace_expansion
 	real_t m0, m1, m2, m3, m4, m5, m6, m7, m8, m9, m10, m11, m12, m13, m14, m15;
 
 	m0 = columns[0][0]; m4 = columns[1][0]; m8 =  columns[2][0]; m12 = columns[3][0];
