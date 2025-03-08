13c5cc45d0849907202b35465eb0ccdd9b34eada
风青山 <idleman@yeah.net>
2025-03-08 13:16:41 +0800

    Fix memory leak caused by hidden tooltip controls
    Using hidden controls can override the default tooltip behavior, but these
    controls are forgotten to be deleted.
    
    (cherry picked from commit ac19f2ca68e89f53c33c8553775bf1c402d88206)

--- scene/main/viewport.cpp
+++ scene/main/viewport.cpp
@@ -1509,6 +1509,7 @@ void Viewport::_gui_show_tooltip() {
 	// This way, the custom tooltip from `ConnectionsDockTree` can create
 	// its own tooltip without conflicting with the default one, even an empty tooltip.
 	if (base_tooltip && !base_tooltip->is_visible()) {
+		memdelete(base_tooltip);
 		return;
 	}
 
