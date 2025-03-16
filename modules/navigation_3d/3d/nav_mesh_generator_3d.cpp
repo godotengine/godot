1a46e0148ba3f5bb9a38b6bfdbfb50e19479baa8
smix8 <52464204+smix8@users.noreply.github.com>
2025-03-16 17:46:22 +0100

    Emit changed signal after baking navigation mesh
    Emits changed signal after baking navigation mesh.

--- modules/navigation_3d/3d/nav_mesh_generator_3d.cpp
+++ modules/navigation_3d/3d/nav_mesh_generator_3d.cpp
@@ -92,6 +92,7 @@ void NavMeshGenerator3D::sync() {
 				if (generator_task->callback.is_valid()) {
 					generator_emit_callback(generator_task->callback);
 				}
+				generator_task->navigation_mesh->emit_changed();
 				memdelete(generator_task);
 			}
 		}
@@ -149,6 +150,7 @@ void NavMeshGenerator3D::bake_from_source_geometry_data(Ref<NavigationMesh> p_na
 		if (p_callback.is_valid()) {
 			generator_emit_callback(p_callback);
 		}
+		p_navigation_mesh->emit_changed();
 		return;
 	}
 
@@ -168,6 +170,8 @@ void NavMeshGenerator3D::bake_from_source_geometry_data(Ref<NavigationMesh> p_na
 	if (p_callback.is_valid()) {
 		generator_emit_callback(p_callback);
 	}
+
+	p_navigation_mesh->emit_changed();
 }
 
 void NavMeshGenerator3D::bake_from_source_geometry_data_async(Ref<NavigationMesh> p_navigation_mesh, Ref<NavigationMeshSourceGeometryData3D> p_source_geometry_data, const Callable &p_callback) {
@@ -179,6 +183,7 @@ void NavMeshGenerator3D::bake_from_source_geometry_data_async(Ref<NavigationMesh
 		if (p_callback.is_valid()) {
 			generator_emit_callback(p_callback);
 		}
+		p_navigation_mesh->emit_changed();
 		return;
 	}
 
