ab4c855b3a8ffcf5ce27df3792f7cf841552243c
Pablo Andres Fuente <pablo.andres.fuente@gmail.com>
2025-03-18 00:02:38 +0100

    Fix `ScrollContainer` focus border issue
    Fixes #100176
    
    Instead of using no clipping technique to draw the focus border because
    it causes problems if a `ScrollContainer` is nested in another
    `ScrollContainer`, now the focus border is drawn using an internal
    `PanelContainer`.

--- scene/gui/scroll_container.h
+++ scene/gui/scroll_container.h
@@ -34,6 +34,8 @@
 
 #include "scroll_bar.h"
 
+class PanelContainer;
+
 class ScrollContainer : public Container {
 	GDCLASS(ScrollContainer, Container);
 
@@ -49,6 +51,7 @@ public:
 private:
 	HScrollBar *h_scroll = nullptr;
 	VScrollBar *v_scroll = nullptr;
+	PanelContainer *focus_panel = nullptr;
 
 	mutable Size2 largest_child_min_size; // The largest one among the min sizes of all available child controls.
 
