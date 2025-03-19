420de74d0df58a2a305ddcda163f8e2d1fafae61
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2025-03-19 15:15:01 +0100

    Shaders

--- scu_builders.py
+++ scu_builders.py
@@ -301,6 +301,7 @@ def generate_scu_files(max_includes_per_scu):
     process_folder(["editor/plugins"])
     process_folder(["editor/plugins/animation"])
     process_folder(["editor/plugins/gizmos"])
+    process_folder(["editor/plugins/shader"])
     process_folder(["editor/plugins/tiles"])
 
     process_folder(["platform/android/export"])
