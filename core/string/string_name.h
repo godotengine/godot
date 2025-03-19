112b3d0a83ecac831375b7fa4fe51ac0a70d9a99
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-03-19 17:23:16 +0100

    Optimize `StringName` creation by using `LocalVector` instead of linked list entries, reducing cache misses.

--- core/string/string_name.h
+++ core/string/string_name.h
@@ -44,11 +44,7 @@ struct StaticCString {
 };
 
 class StringName {
-	enum {
-		STRING_TABLE_BITS = 16,
-		STRING_TABLE_LEN = 1 << STRING_TABLE_BITS,
-		STRING_TABLE_MASK = STRING_TABLE_LEN - 1
-	};
+	struct TableEntry;
 
 	struct _Data {
 		SafeRefCount refcount;
@@ -59,19 +55,14 @@ class StringName {
 		uint32_t debug_references = 0;
 #endif
 		String get_name() const { return cname ? String(cname) : name; }
+
 		bool operator==(const String &p_name) const;
-		bool operator!=(const String &p_name) const;
 		bool operator==(const char *p_name) const;
-		bool operator!=(const char *p_name) const;
 
 		int idx = 0;
 		uint32_t hash = 0;
-		_Data *prev = nullptr;
-		_Data *next = nullptr;
 		_Data() {}
 	};
-
-	static inline _Data *_table[STRING_TABLE_LEN];
 
 	_Data *_data = nullptr;
 
@@ -160,10 +151,6 @@ public:
 
 		return String();
 	}
-
-	static StringName search(const char *p_name);
-	static StringName search(const char32_t *p_name);
-	static StringName search(const String &p_name);
 
 	struct AlphCompare {
 		template <typename LT, typename RT>
