4c3a6e3b9bfb8ca66b882aaf8d51371d539469f1
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2025-03-19 15:33:07 +0100

    2D

--- editor/plugins/2d/physics/collision_polygon_2d_editor_plugin.h
+++ editor/plugins/2d/physics/collision_polygon_2d_editor_plugin.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  navigation_obstacle_2d_editor_plugin.h                                */
+/*  collision_polygon_2d_editor_plugin.h                                  */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -30,29 +30,22 @@
 
 #pragma once
 
-#include "editor/plugins/abstract_polygon_2d_editor.h"
-#include "scene/2d/navigation/navigation_obstacle_2d.h"
+#include "editor/plugins/2d/abstract_polygon_2d_editor.h"
+#include "scene/2d/physics/collision_polygon_2d.h"
 
-class NavigationObstacle2DEditor : public AbstractPolygon2DEditor {
-	GDCLASS(NavigationObstacle2DEditor, AbstractPolygon2DEditor);
+class CollisionPolygon2DEditor : public AbstractPolygon2DEditor {
+	GDCLASS(CollisionPolygon2DEditor, AbstractPolygon2DEditor);
 
-	NavigationObstacle2D *node = nullptr;
+	CollisionPolygon2D *node = nullptr;
 
 protected:
 	virtual Node2D *_get_node() const override;
 	virtual void _set_node(Node *p_polygon) override;
-
-	virtual Variant _get_polygon(int p_idx) const override;
-	virtual void _set_polygon(int p_idx, const Variant &p_polygon) const override;
-
-	virtual void _action_add_polygon(const Variant &p_polygon) override;
-	virtual void _action_remove_polygon(int p_idx) override;
-	virtual void _action_set_polygon(int p_idx, const Variant &p_previous, const Variant &p_polygon) override;
 };
 
-class NavigationObstacle2DEditorPlugin : public AbstractPolygon2DEditorPlugin {
-	GDCLASS(NavigationObstacle2DEditorPlugin, AbstractPolygon2DEditorPlugin);
+class CollisionPolygon2DEditorPlugin : public AbstractPolygon2DEditorPlugin {
+	GDCLASS(CollisionPolygon2DEditorPlugin, AbstractPolygon2DEditorPlugin);
 
 public:
-	NavigationObstacle2DEditorPlugin();
+	CollisionPolygon2DEditorPlugin();
 };
