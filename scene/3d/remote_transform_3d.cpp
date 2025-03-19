93131a46f59b2d187b9a688702c373c43afb66e9
Aaron Franke <arnfranke@yahoo.com>
2025-03-19 00:21:08 -0700

    Rename global transform notification to contain "global"

--- scene/3d/remote_transform_3d.cpp
+++ scene/3d/remote_transform_3d.cpp
@@ -118,7 +118,7 @@ void RemoteTransform3D::_notification(int p_what) {
 		} break;
 
 		case NOTIFICATION_LOCAL_TRANSFORM_CHANGED:
-		case NOTIFICATION_TRANSFORM_CHANGED: {
+		case NOTIFICATION_GLOBAL_TRANSFORM_CHANGED: {
 			if (!is_inside_tree()) {
 				break;
 			}
@@ -155,7 +155,7 @@ void RemoteTransform3D::set_use_global_coordinates(const bool p_enable) {
 
 	use_global_coordinates = p_enable;
 
-	set_notify_transform(use_global_coordinates);
+	set_notify_global_transform(use_global_coordinates);
 	set_notify_local_transform(!use_global_coordinates);
 	_update_remote();
 }
@@ -239,6 +239,6 @@ void RemoteTransform3D::_bind_methods() {
 }
 
 RemoteTransform3D::RemoteTransform3D() {
-	set_notify_transform(use_global_coordinates);
+	set_notify_global_transform(use_global_coordinates);
 	set_notify_local_transform(!use_global_coordinates);
 }
