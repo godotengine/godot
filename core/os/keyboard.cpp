3487e1821bde503cb2d988c6b6139e539419e5fe
Yufeng Ying <nbyyf2002@mail.ustc.edu.cn>
2025-03-20 10:16:05 +0800

    size() <= 0 and size() < 1.

--- core/os/keyboard.cpp
+++ core/os/keyboard.cpp
@@ -410,7 +410,7 @@ String keycode_get_string(Key p_code) {
 Key find_keycode(const String &p_codestr) {
 	Key keycode = Key::NONE;
 	Vector<String> code_parts = p_codestr.split("+");
-	if (code_parts.size() < 1) {
+	if (code_parts.is_empty()) {
 		return keycode;
 	}
 
