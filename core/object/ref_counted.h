0b233910288619ed1f286102229de86029a5c5e1
Tom <tom_full@hotmail.co.uk>
2025-03-26 19:01:56 +0000

    Add missing initializer_list constructor for TypedDictionary

--- core/object/ref_counted.h
+++ core/object/ref_counted.h
@@ -85,6 +85,10 @@ class Ref {
 
 	//virtual RefCounted * get_reference() const { return reference; }
 public:
+	static _FORCE_INLINE_ String get_class_static() {
+		return T::get_class_static();
+	}
+
 	_FORCE_INLINE_ bool operator==(const T *p_ptr) const {
 		return reference == p_ptr;
 	}
