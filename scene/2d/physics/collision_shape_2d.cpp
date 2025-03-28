5d0d0b0fe12ae4006ae858ba1e8dcf0a9b9bceb5
Sam Szuflita <sam@hathora.dev>
2025-03-28 15:32:22 -0400

    Queue redraw if direction is updated

--- scene/2d/physics/collision_shape_2d.cpp
+++ scene/2d/physics/collision_shape_2d.cpp
@@ -237,6 +237,7 @@ void CollisionShape2D::set_one_way_collision_direction(Vector2 p_one_way_collisi
 	if (collision_object) {
 		collision_object->shape_owner_set_one_way_collision_direction(owner_id, p_one_way_collision_direction);
 	}
+	queue_redraw();
 }
 
 Vector2 CollisionShape2D::get_one_way_collision_direction() const {
