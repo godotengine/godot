9f58ef8b943497a92ea8f0c2ac5967fba262a824
Malcolm Anderson <malcolminyo@gmail.com>
2025-04-02 13:15:58 -0700

    GDScript: Add warning for deprecated symbol usage
    This PR also adds editor-only helper methods that fetch the deprecation status of various GDScript symbol types.

--- modules/gdscript/gdscript_warning.h
+++ modules/gdscript/gdscript_warning.h
@@ -93,6 +93,7 @@ public:
 		GET_NODE_DEFAULT_WITHOUT_ONREADY, // A class variable uses `get_node()` (or the `$` notation) as its default value, but does not use the @onready annotation.
 		ONREADY_WITH_EXPORT, // The `@onready` annotation will set the value after `@export` which is likely not intended.
 		ONREADY_WITH_CAST, // The cast will silently assign `null` if the node has a wrong type. This is likely not intended.
+		DEPRECATED_USAGE, // A symbol being used is marked as deprecated in the documentation.
 #ifndef DISABLE_DEPRECATED
 		PROPERTY_USED_AS_FUNCTION, // Function not found, but there's a property with the same name.
 		CONSTANT_USED_AS_FUNCTION, // Function not found, but there's a constant with the same name.
@@ -153,6 +154,7 @@ public:
 		ERROR, // GET_NODE_DEFAULT_WITHOUT_ONREADY // May not work as expected.
 		ERROR, // ONREADY_WITH_EXPORT // May not work as expected.
 		WARN, // ONREADY_WITH_CAST
+		WARN, // DEPRECATED_USAGE
 #ifndef DISABLE_DEPRECATED
 		WARN, // PROPERTY_USED_AS_FUNCTION
 		WARN, // CONSTANT_USED_AS_FUNCTION
