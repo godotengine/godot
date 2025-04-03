9c574c6373cc77c3ca375de70ba081c00e7f10d5
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-04-03 12:15:27 +0300

    Detect KDE/LXQt and swap OK/Cancel buttons to Windows style.

--- platform/linuxbsd/x11/display_server_x11.h
+++ platform/linuxbsd/x11/display_server_x11.h
@@ -359,6 +359,7 @@ class DisplayServerX11 : public DisplayServer {
 	void _update_context(WindowData &wd);
 
 	Context context = CONTEXT_ENGINE;
+	bool swap_cancel_ok = false;
 
 	WindowID _get_focused_window_or_popup() const;
 	bool _window_focus_check();
@@ -549,6 +550,8 @@ public:
 	virtual void cursor_set_shape(CursorShape p_shape) override;
 	virtual CursorShape cursor_get_shape() const override;
 	virtual void cursor_set_custom_image(const Ref<Resource> &p_cursor, CursorShape p_shape, const Vector2 &p_hotspot) override;
+
+	virtual bool get_swap_cancel_ok() override;
 
 	virtual int keyboard_get_layout_count() const override;
 	virtual int keyboard_get_current_layout() const override;
