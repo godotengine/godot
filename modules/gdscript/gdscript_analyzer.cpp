4d093476418cd7d891dd70801a7261a5697b7ca6
Malcolm Anderson <malcolminyo@gmail.com>
2025-04-04 22:19:43 -0700

    Recognize global autoload marked as `@deprecated`

--- modules/gdscript/gdscript_analyzer.cpp
+++ modules/gdscript/gdscript_analyzer.cpp
@@ -4576,6 +4576,14 @@ void GDScriptAnalyzer::reduce_identifier(GDScriptParser::IdentifierNode *p_ident
 					}
 				}
 			}
+#if DEBUG_ENABLED
+			DocTools *dd = EditorHelp::get_doc_data();
+			if (dd && dd->class_list.has(name)) {
+				if (dd->class_list[name].is_deprecated) {
+					parser->push_warning(p_identifier, GDScriptWarning::DEPRECATED_IDENTIFIER);
+				}
+			}
+#endif
 			result.is_constant = true;
 			p_identifier->set_datatype(result);
 			return;
