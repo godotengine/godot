a754dab7d42f1ce074f10897d8ce2aea2def9980
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-04-09 14:53:16 +0200

    Use `BinaryMutex` instead of `Mutex` for `StringName`.

--- core/string/string_name.cpp
+++ core/string/string_name.cpp
@@ -253,13 +253,6 @@ StringName::StringName(const StringName &p_name) {
 	}
 }
 
-void StringName::assign_static_unique_class_name(StringName *ptr, const char *p_name) {
-	MutexLock lock(mutex);
-	if (*ptr == StringName()) {
-		*ptr = StringName(p_name, true);
-	}
-}
-
 StringName::StringName(const char *p_name, bool p_static) {
 	_data = nullptr;
 
