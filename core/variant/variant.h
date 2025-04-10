ef581994ab52ed1bf45b5982afefae65fc0177b3
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-04-10 18:03:17 +0200

    Introduce `VariantImplicitConvert<>` template for types that can be implicitly converted to and from `Variant`. De-duplicate a lot of `VariantGetInternalPtr`, `VariantInternalAccessor`, `VariantInitializer` and `VariantDefaultInitializer`.

--- core/variant/variant.h
+++ core/variant/variant.h
@@ -173,6 +173,12 @@ private:
 
 	friend struct _VariantCall;
 	friend class VariantInternal;
+	template <typename>
+	friend struct _VariantGetInternalPtrLocal;
+	template <typename>
+	friend struct _VariantGetInternalPtrElsewhere;
+	template <typename>
+	friend struct _VariantGetInternalPtrPackedArrayRef;
 	// Variant takes 24 bytes when real_t is float, and 40 bytes if double.
 	// It only allocates extra memory for AABB/Transform2D (24, 48 if double),
 	// Basis/Transform3D (48, 96 if double), Projection (64, 128 if double),
