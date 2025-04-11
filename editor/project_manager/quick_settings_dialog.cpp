5f7e9a9198e4df2a4d06fe8deef8b6ad11d592ae
Jayden Sipe <jayden.sipe@gmail.com>
2025-04-11 16:26:17 -0400

    Rename editor setting to `Check for Updates`

--- editor/project_manager/quick_settings_dialog.cpp
+++ editor/project_manager/quick_settings_dialog.cpp
@@ -48,7 +48,7 @@ void QuickSettingsDialog::_fetch_setting_values() {
 	editor_themes.clear();
 	editor_scales.clear();
 	editor_network_modes.clear();
-	editor_engine_version_update_modes.clear();
+	editor_check_for_updates.clear();
 	editor_directory_naming_conventions.clear();
 
 	{
@@ -66,8 +66,8 @@ void QuickSettingsDialog::_fetch_setting_values() {
 				editor_scales = pi.hint_string.split(",");
 			} else if (pi.name == "network/connection/network_mode") {
 				editor_network_modes = pi.hint_string.split(",");
-			} else if (pi.name == "network/connection/engine_version_update_mode") {
-				editor_engine_version_update_modes = pi.hint_string.split(",");
+			} else if (pi.name == "network/connection/check_for_updates") {
+				editor_check_for_updates = pi.hint_string.split(",");
 			} else if (pi.name == "project_manager/directory_naming_convention") {
 				editor_directory_naming_conventions = pi.hint_string.split(",");
 			}
@@ -133,18 +133,18 @@ void QuickSettingsDialog::_update_current_values() {
 		}
 	}
 
-	// Engine version update mode options.
+	// Check for updates options.
 	{
-		const int current_update_mode = EDITOR_GET("network/connection/engine_version_update_mode");
+		const int current_update_mode = EDITOR_GET("network/connection/check_for_updates");
 
-		for (int i = 0; i < editor_engine_version_update_modes.size(); i++) {
-			const String &engine_version_update_mode_value = editor_engine_version_update_modes[i];
+		for (int i = 0; i < editor_check_for_updates.size(); i++) {
+			const String &check_for_update_value = editor_check_for_updates[i];
 			if (current_update_mode == i) {
-				engine_version_update_mode_button->set_text(engine_version_update_mode_value);
-				engine_version_update_mode_button->select(i);
+				check_for_update_button->set_text(check_for_update_value);
+				check_for_update_button->select(i);
 
-				// Disables Engine Version Update Mode selection if Network mode is set to Offline.
-				engine_version_update_mode_button->set_disabled(!EDITOR_GET("network/connection/network_mode"));
+				// Disables Check for Updates selection if Network mode is set to Offline.
+				check_for_update_button->set_disabled(!EDITOR_GET("network/connection/network_mode"));
 			}
 		}
 	}
@@ -197,12 +197,12 @@ void QuickSettingsDialog::_scale_selected(int p_id) {
 void QuickSettingsDialog::_network_mode_selected(int p_id) {
 	_set_setting_value("network/connection/network_mode", p_id);
 
-	// Disables Engine Version Update Mode selection if Network mode is set to Offline.
-	engine_version_update_mode_button->set_disabled(!p_id);
+	// Disables Check for Updates selection if Network mode is set to Offline.
+	check_for_update_button->set_disabled(!p_id);
 }
 
-void QuickSettingsDialog::_engine_version_update_mode_selected(int p_id) {
-	_set_setting_value("network/connection/engine_version_update_mode", p_id);
+void QuickSettingsDialog::_check_for_update_selected(int p_id) {
+	_set_setting_value("network/connection/check_for_updates", p_id);
 }
 
 void QuickSettingsDialog::_directory_naming_convention_selected(int p_id) {
@@ -346,18 +346,18 @@ QuickSettingsDialog::QuickSettingsDialog() {
 			_add_setting_control(TTRC("Network Mode"), network_mode_option_button);
 		}
 
-		// Engine version update mode options.
+		// Check for updates options.
 		{
-			engine_version_update_mode_button = memnew(OptionButton);
-			engine_version_update_mode_button->set_fit_to_longest_item(false);
-			engine_version_update_mode_button->connect(SceneStringName(item_selected), callable_mp(this, &QuickSettingsDialog::_engine_version_update_mode_selected));
+			check_for_update_button = memnew(OptionButton);
+			check_for_update_button->set_fit_to_longest_item(false);
+			check_for_update_button->connect(SceneStringName(item_selected), callable_mp(this, &QuickSettingsDialog::_check_for_update_selected));
 
-			for (int i = 0; i < editor_engine_version_update_modes.size(); i++) {
-				const String &engine_version_update_mode_value = editor_engine_version_update_modes[i];
-				engine_version_update_mode_button->add_item(engine_version_update_mode_value, i);
+			for (int i = 0; i < editor_check_for_updates.size(); i++) {
+				const String &check_for_update_value = editor_check_for_updates[i];
+				check_for_update_button->add_item(check_for_update_value, i);
 			}
 
-			_add_setting_control(TTRC("Engine Version Update Mode"), engine_version_update_mode_button);
+			_add_setting_control(TTRC("Check for Updates"), check_for_update_button);
 		}
 
 		// Project directory naming options.
