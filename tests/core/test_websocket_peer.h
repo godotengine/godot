6e771be4d7938b2383491ffaa9216b9e7a46ccc1
lilcfar <lilyfarr.daisy@gmail.com>
2025-04-14 10:45:00 -0400

    Add unit test for WebSocketPeer get_ready_state

---
+++ tests/core/test_websocket_peer.h
@@ -0,0 +1,25 @@
+/*
+.poll() behavior?
+.close() call and updated state?
+set_write_mode()
+get_connected_host()
+*/
+
+#include "modules/websocket/websocket_peer.h"
+#include "tests/test_macros.h"
+
+namespace TestWebSocketPeer {
+
+TEST_CASE("[WebSocketPeer] after creation state is closed") {
+	WebSocketPeer *peer = WebSocketPeer::create();
+	CHECK_MESSAGE(peer != nullptr, "WebSocketPeer::create() returned nullptr");
+
+	if (peer) {
+		CHECK(peer->get_ready_state() == WebSocketPeer::STATE_CLOSED);
+		memdelete(peer);
+	}
+
+}
+
+} // namespace TestWebSocketPeer
+
