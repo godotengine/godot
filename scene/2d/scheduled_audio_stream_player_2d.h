ac58fbf43c8de055e2b537c8ff5257957a20771b
PizzaLovers007 <trex@parkvue.com>
2025-04-17 16:51:30 -0500

    v2 Implement audio absolute time and scheduled play
    This is a rewrite of #105510 that moves the silence frames logic into a separate AudioStreamPlaybackScheduled class and introduces new ScheduledAudioStreamPlayer/2D/3D nodes. The rewrite allows both the AudioServer and the player (mostly) to treat it as if it were a generic playback.
    
    Main differences:
    - ScheduledAudioStreamPlayer/2D/3D are new nodes inheriting from their AudioStreamPlayer/2D/3D counterparts. These nodes only contain one new method: play_scheduled.
    - play_scheduled returns an AudioStreamPlaybackScheduled instance, which is tied to the player that created it.
    - The start time can be changed after scheduling.
    - You can now set an end time for the playback.
    - The scheduled playback can be cancelled separately from other playbacks on the player.
    
    Co-Authored-By: K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>

---
+++ scene/2d/scheduled_audio_stream_player_2d.h
@@ -0,0 +1,45 @@
+/**************************************************************************/
+/*  scheduled_audio_stream_player_2d.h                                    */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "audio_stream_player_2d.h"
+
+class AudioStreamPlaybackScheduled;
+
+class ScheduledAudioStreamPlayer2D : public AudioStreamPlayer2D {
+	GDCLASS(ScheduledAudioStreamPlayer2D, AudioStreamPlayer2D);
+
+protected:
+	static void _bind_methods();
+
+public:
+	Ref<AudioStreamPlaybackScheduled> play_scheduled(double p_abs_time, double p_from_pos = 0.0);
+};
