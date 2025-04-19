c7fb32c18da48ccc172557dfedd7d241340dcb46
Adriano Sudário <sudario.adriano@gmail.com>
2025-04-18 22:50:10 -0300

    AudioStreamSynchronized refactoring + Prevent stream sync from stopping when set, if playing

---
+++ tests/scene/test_audio_stream_synchronized.h
@@ -0,0 +1,156 @@
+/**************************************************************************/
+/*  test_audio_stream_synchronized.h                                      */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "modules/interactive_music/audio_stream_synchronized.h"
+#include "tests/test_macros.h"
+
+namespace TestAudioStreamSynchronized {
+
+Ref<AudioStreamSynchronized> gen_default_audio_stream_synchronized(double first_wav_length = 1, double second_wav_length = 1, bool is_looped = true) {
+	Ref<AudioStreamSynchronized> audio_stream_synchronized = memnew(AudioStreamSynchronized);
+	Ref<AudioStreamWAV> stream_wav1 = TestUtils::gen_audio_stream_wav(AudioStreamWAV::FORMAT_8_BITS, true, first_wav_length);
+	Ref<AudioStreamWAV> stream_wav2 = TestUtils::gen_audio_stream_wav(AudioStreamWAV::FORMAT_8_BITS, true, second_wav_length);
+	
+	if (is_looped) {
+		float default_wav_rate = 44100;
+
+		stream_wav1->set_loop_mode(AudioStreamWAV::LoopMode::LOOP_FORWARD);
+		stream_wav1->set_loop_end(default_wav_rate * first_wav_length);
+
+		stream_wav2->set_loop_mode(AudioStreamWAV::LoopMode::LOOP_FORWARD);
+		stream_wav2->set_loop_end(default_wav_rate * second_wav_length);
+	}
+	
+	audio_stream_synchronized->set_stream_count(2);
+	audio_stream_synchronized->set_sync_stream(0, stream_wav1);
+	audio_stream_synchronized->set_sync_stream(1, stream_wav2);
+
+	return audio_stream_synchronized;
+}
+
+TEST_CASE("[Audio][AudioStreamSynchronized] Constructor") {
+	Ref<AudioStreamSynchronized> audio_stream_synchronized = memnew(AudioStreamSynchronized);
+	CHECK(audio_stream_synchronized->get_bpm() == 0);
+	CHECK(audio_stream_synchronized->get_beat_count() == 0);
+	CHECK(audio_stream_synchronized->get_bar_beats() == 0);
+	CHECK(audio_stream_synchronized->get_stream_count() == 0);
+	CHECK(audio_stream_synchronized->has_loop() == false);
+	CHECK(audio_stream_synchronized->get_length() == 0);
+	CHECK(audio_stream_synchronized->is_meta_stream() == true);
+	CHECK(audio_stream_synchronized->get_stream_name() == "Synchronized");
+}
+
+TEST_CASE("[Audio][AudioStreamSynchronized] get_length") {
+	Ref<AudioStreamSynchronized> audio_stream_synchronized = gen_default_audio_stream_synchronized(1, 1.5);
+	
+	double expected_length = 1.5;
+	
+	CHECK(audio_stream_synchronized->get_length() == expected_length);
+}
+
+TEST_CASE("[Audio][AudioStreamSynchronized][AudioStreamPlaybackSynchronized] seek, start") {
+	Ref<AudioStreamSynchronized> audio_stream_synchronized = gen_default_audio_stream_synchronized(1, 1.5);
+	Ref<AudioStreamPlayback> playback_sync = audio_stream_synchronized->instantiate_playback();
+	// Ref<AudioStreamSynchronized> audio_stream_synchronized = memnew(AudioStreamSynchronized);
+	// Ref<AudioStreamPlayback> playback_sync = audio_stream_synchronized->instantiate_playback();
+	// Ref<AudioStreamWAV> stream_wav1 = TestUtils::gen_audio_stream_wav(AudioStreamWAV::FORMAT_8_BITS, true, 1);
+	// Ref<AudioStreamWAV> stream_wav2 = TestUtils::gen_audio_stream_wav(AudioStreamWAV::FORMAT_8_BITS, true, 1.5);
+	
+	// float default_wav_rate = 44100;
+
+	// Ref<AudioStreamPlaybackWAV> wav1_playback = stream_wav1->instantiate_playback();
+	// stream_wav1->set_loop_mode(AudioStreamWAV::LoopMode::LOOP_FORWARD);
+	// stream_wav1->set_loop_end(default_wav_rate);
+	
+	// Ref<AudioStreamPlaybackWAV> wav2_playback = stream_wav2->instantiate_playback();
+	// stream_wav2->set_loop_mode(AudioStreamWAV::LoopMode::LOOP_FORWARD);
+	// stream_wav2->set_loop_end(default_wav_rate * 1.5);
+	
+	// audio_stream_synchronized->set_stream_count(2);
+	// audio_stream_synchronized->set_sync_stream(0, stream_wav1);
+	// audio_stream_synchronized->set_sync_stream(1, stream_wav2);
+	// Ref<AudioStream> wav1_playback = audio_stream_synchronized->get_sync_stream(0);
+	// Ref<AudioStreamPlaybackWAV> wav2_playback = audio_stream_synchronized->get_sync_stream(1);
+	
+	playback_sync->start();
+	playback_sync->seek(1.3);
+	
+	double expected_position = 1.3;
+	
+	CHECK(playback_sync->get_playback_position() == doctest::Approx(expected_position));
+	playback_sync->stop();
+
+	// SUBCASE("..") {
+	// 	playback_sync->start();
+	// 	playback_sync->seek(1.3);
+		
+	// 	double wav1_expected_position = 0.3;
+	// 	double wav2_expected_position = 1.3;
+		
+	// 	CHECK(wav1_playback->get_playback_position() == doctest::Approx(wav1_expected_position));
+	// 	CHECK(wav2_playback->get_playback_position() == doctest::Approx(wav2_expected_position));
+
+	// 	playback_sync->stop();
+	// }
+
+
+
+
+
+
+	// SUBCASE("Whenever a stream has higher length than another, if the one with lower length has loop and seek value is higher than total length, it should adjust accordingly") {
+	// 	playback_sync->start();
+	// 	playback_sync->seek(1.3);
+		
+	// 	double wav1_expected_position = 0.3;
+	// 	double wav2_expected_position = 1.3;
+		
+	// 	CHECK(wav1_playback->get_playback_position() == doctest::Approx(wav1_expected_position));
+	// 	CHECK(wav2_playback->get_playback_position() == doctest::Approx(wav2_expected_position));
+
+	// 	playback_sync->stop();
+	// }
+
+	// SUBCASE("Same behavior as above must be applied with start") {
+	// 	playback_sync->start(1.3);
+		
+	// 	double wav1_expected_position = 0.3;
+	// 	double wav2_expected_position = 1.3;
+		
+	// 	CHECK(wav1_playback->get_playback_position() == doctest::Approx(wav1_expected_position));
+	// 	CHECK(wav2_playback->get_playback_position() == doctest::Approx(wav2_expected_position));
+
+	// 	playback_sync->stop();
+	// }
+}
+
+} // namespace TestAudioStreamSynchronized
