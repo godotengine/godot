a5f874c38123db58943a3d3266443d37cb638d42
hamid <zmn-hamid@proton.me>
2025-04-21 15:04:09 +0330

    Add viewport zoom by dragging with Ctrl+MiddleMouse button

--- editor/editor_settings.cpp
+++ editor/editor_settings.cpp
@@ -528,6 +528,7 @@ void EditorSettings::_load_defaults(Ref<ConfigFile> p_extra_config) {
 
 	EDITOR_SETTING_BASIC(Variant::INT, PROPERTY_HINT_ENUM, "interface/editor/vsync_mode", 1, "Disabled,Enabled,Adaptive,Mailbox")
 	EDITOR_SETTING(Variant::BOOL, PROPERTY_HINT_NONE, "interface/editor/update_continuously", false, "")
+	_initial_set("interface/editor/zoom_drag_sensivity", 1.0);
 
 	_initial_set("interface/editors/show_scene_tree_root_selection", true);
 	_initial_set("interface/editors/derive_script_globals_by_name", true);
