c8169ea274049c486aa232aa1c509c3ad657149a
Rémi Verschelde <rverschelde@gmail.com>
2025-04-25 15:30:29 +0200

    Style: Apply fixes from codespell

--- scene/2d/line_builder.cpp
+++ scene/2d/line_builder.cpp
@@ -367,7 +367,7 @@ void LineBuilder::build() {
 			}
 
 			if (intersection_result != SEGMENT_INTERSECT) {
-				// In this case the joint is too corrupted to be re-used,
+				// In this case the joint is too corrupted to be reused,
 				// start again the strip with fallback points
 				strip_begin(pos_up0, pos_down0, color1, uvx1);
 			}
