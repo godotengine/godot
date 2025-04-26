66353c44b3dbf457355a1ab72ed1171dc69a6e0b
Nazarii <nazarii.yablonskyi.pp.2022@lpnu.ua>
2025-04-26 13:21:12 +0300

    Use AHashMap for RBMap nodes and HashMap input_activity

--- scene/animation/animation_blend_tree.h
+++ scene/animation/animation_blend_tree.h
@@ -409,7 +409,7 @@ class AnimationNodeBlendTree : public AnimationRootNode {
 		Vector<StringName> connections;
 	};
 
-	RBMap<StringName, Node, StringName::AlphCompare> nodes;
+	AHashMap<StringName, Node> nodes;
 
 	Vector2 graph_offset;
 
