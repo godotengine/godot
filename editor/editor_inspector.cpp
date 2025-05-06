fad6224a16e56161285c4f59883a83b487b86029
kobewi <kobewi4e@gmail.com>
2025-05-06 13:59:56 +0200

    Allow undoredo actions to not make history unsaved

--- editor/editor_inspector.cpp
+++ editor/editor_inspector.cpp
@@ -4661,7 +4661,7 @@ void EditorInspector::_edit_set(const String &p_name, const Variant &p_value, bo
 		_edit_request_change(object, p_name);
 		emit_signal(_prop_edited, p_name);
 	} else {
-		undo_redo->create_action(vformat(TTR("Set %s"), p_name), UndoRedo::MERGE_ENDS);
+		undo_redo->create_action(vformat(TTR("Set %s"), p_name), UndoRedo::MERGE_ENDS, nullptr, false, mark_unsaved);
 		undo_redo->add_do_property(object, p_name, p_value);
 		bool valid = false;
 		Variant value = object->get(p_name, &valid);
