cc7fbe6aec1611de4d6c0246145efdc574a596d4
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-05-13 08:27:36 -0700

    Bump the NDK to version 28.1.13356709
    Doing so automatically adds support for 16kib page to the Godot Android shared libraries.
    See https://developer.android.com/guide/practices/page-sizes#compile-16-kb-alignment for details.

--- platform/android/detect.py
+++ platform/android/detect.py
@@ -64,7 +64,7 @@ def get_android_ndk_root(env: "SConsEnvironment"):
 
 # This is kept in sync with the value in 'platform/android/java/app/config.gradle'.
 def get_ndk_version():
-    return "27.2.12479018"
+    return "28.1.13356709"
 
 
 # This is kept in sync with the value in 'platform/android/java/app/config.gradle'.
