52444828e01b689740eb120a6ab1c5b7933bbb50
aaronp64 <aaronp.code@gmail.com>
2025-05-19 09:35:40 -0400

    Combine CharBuffer/StringBuffer classes, and use in more places
    - Made StringBuffer character type templated, to support both char and char32_t buffers
    - Trimmed down implementation to focus on only appending one character at a time (other methods were unused)
    - Replaced CharBuffer class from file_access.cpp, and renamed StringBuffer to CharBuffer
    - Updated JSON parsing and String uri encoding/decoding to use CharBuffer

--- core/io/file_access.cpp
+++ core/io/file_access.cpp
@@ -39,6 +39,7 @@
 #include "core/io/marshalls.h"
 #include "core/os/os.h"
 #include "core/os/time.h"
+#include "core/string/char_buffer.h"
 
 Ref<FileAccess> FileAccess::create(AccessType p_access) {
 	ERR_FAIL_INDEX_V(p_access, ACCESS_MAX, nullptr);
@@ -410,69 +411,22 @@ String FileAccess::get_token() const {
 	return String::utf8(token.get_data());
 }
 
-class CharBuffer {
-	Vector<char> vector;
-	char stack_buffer[256];
-
-	char *buffer = nullptr;
-	int capacity = 0;
-	int written = 0;
-
-	bool grow() {
-		if (vector.resize(next_power_of_2(1 + written)) != OK) {
-			return false;
-		}
-
-		if (buffer == stack_buffer) { // first chunk?
-
-			for (int i = 0; i < written; i++) {
-				vector.write[i] = stack_buffer[i];
-			}
-		}
-
-		buffer = vector.ptrw();
-		capacity = vector.size();
-		ERR_FAIL_COND_V(written >= capacity, false);
-
-		return true;
-	}
-
-public:
-	_FORCE_INLINE_ CharBuffer() :
-			buffer(stack_buffer),
-			capacity(std::size(stack_buffer)) {
-	}
-
-	_FORCE_INLINE_ void push_back(char c) {
-		if (written >= capacity) {
-			ERR_FAIL_COND(!grow());
-		}
-
-		buffer[written++] = c;
-	}
-
-	_FORCE_INLINE_ const char *get_data() const {
-		return buffer;
-	}
-};
-
 String FileAccess::get_line() const {
-	CharBuffer line;
+	CharBuffer<char, 256> line;
 
 	uint8_t c = get_8();
 
 	while (!eof_reached()) {
 		if (c == '\n' || c == '\0' || get_error() != OK) {
-			line.push_back(0);
-			return String::utf8(line.get_data());
+			return String::utf8(line.get_terminated_buffer());
 		} else if (c != '\r') {
-			line.push_back(char(c));
+			line += char(c);
 		}
 
 		c = get_8();
 	}
-	line.push_back(0);
-	return String::utf8(line.get_data());
+
+	return String::utf8(line.get_terminated_buffer());
 }
 
 Vector<String> FileAccess::get_csv_line(const String &p_delim) const {
