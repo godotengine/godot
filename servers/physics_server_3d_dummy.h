e6c75b5f2ca1f5c0485a33c0bea53a0e528bcf96
tucono <tucono@gmail.com>
2025-05-20 13:15:37 -0500

    Add intersect_ray_multiple to dummy and extension

--- servers/physics_server_3d_dummy.h
+++ servers/physics_server_3d_dummy.h
@@ -111,7 +111,7 @@ class PhysicsDirectSpaceState3DDummy : public PhysicsDirectSpaceState3D {
 
 public:
 	virtual bool intersect_ray(const RayParameters &p_parameters, RayResult &r_result) override { return false; }
-
+	virtual int intersect_ray_multiple(const RayParameters &p_parameters, RayResult *r_results, int p_result_max) override { return 0; }
 	virtual int intersect_point(const PointParameters &p_parameters, ShapeResult *r_results, int p_result_max) override { return 0; }
 
 	virtual int intersect_shape(const ShapeParameters &p_parameters, ShapeResult *r_results, int p_result_max) override { return 0; }
