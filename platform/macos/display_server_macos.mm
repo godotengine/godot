10fe2b99d9e9a8d09fa62c7d7006c056aadb1944
SheepYhangCN <sheepyuhang@163.com>
2025-05-21 18:23:27 +0800

    Move GodotProgressView to separate files
    Co-Authored-By: bruvzg <7645683+bruvzg@users.noreply.github.com>

--- platform/macos/display_server_macos.mm
+++ platform/macos/display_server_macos.mm
@@ -37,6 +37,7 @@
 #import "godot_menu_delegate.h"
 #import "godot_menu_item.h"
 #import "godot_open_save_delegate.h"
+#import "godot_progress_view.h"
 #import "godot_status_item.h"
 #import "godot_window.h"
 #import "godot_window_delegate.h"
@@ -2904,78 +2905,6 @@ void DisplayServerMacOS::window_request_attention(WindowID p_window) {
 	// It's app global, ignore window id.
 	[NSApp requestUserAttention:NSCriticalRequest];
 }
-
-@interface GodotProgressView : NSView {
-	DisplayServer::ProgressState pr_state;
-	float pr_value;
-	float pr_offset;
-}
-- (void)setValue:(float)value;
-- (void)setState:(DisplayServer::ProgressState)state;
-@end
-
-@implementation GodotProgressView
-
-- (id)init {
-	self = [super init];
-	pr_state = DisplayServer::PROGRESS_STATE_NOPROGRESS;
-	pr_value = 0.f;
-	pr_offset = 0.f;
-	return self;
-}
-
-- (void)setValue:(float)value {
-	pr_value = value;
-}
-
-- (void)setState:(DisplayServer::ProgressState)state {
-	pr_state = state;
-}
-
-- (void)drawRect:(NSRect)dirtyRect {
-	// Icon draw.
-	[[NSGraphicsContext currentContext] setImageInterpolation:NSImageInterpolationHigh];
-	[[NSApp applicationIconImage] drawInRect:self.bounds];
-
-	if (pr_state == DisplayServer::PROGRESS_STATE_NOPROGRESS) {
-		return;
-	}
-
-	// Border draw.
-	NSRect rect = NSMakeRect(1.f, 1.f, self.bounds.size.width - 2.f, 16.f);
-	NSBezierPath *bezier_path = [NSBezierPath bezierPathWithRoundedRect:rect xRadius:8.f yRadius:8.f];
-	[bezier_path setLineWidth:2.0];
-	[[NSColor grayColor] set];
-	[bezier_path stroke];
-
-	// Fill clip path.
-	rect = NSMakeRect(2.f, 2.f, self.bounds.size.width - 4.f, 14.f);
-	bezier_path = [NSBezierPath bezierPathWithRoundedRect:rect xRadius:7.f yRadius:7.f];
-	[bezier_path setLineWidth:1.0];
-	[bezier_path addClip];
-
-	// Fill draw.
-	if (pr_state == DisplayServer::PROGRESS_STATE_INDETERMINATE) {
-		rect.size.width /= 5.0;
-		pr_offset += rect.size.width / 10.0;
-		if (pr_offset > self.bounds.size.width - rect.size.width) {
-			pr_offset = 0.f;
-		}
-		rect.origin.x += pr_offset;
-	} else {
-		rect.size.width = floor(rect.size.width * pr_value);
-	}
-	if (pr_state == DisplayServer::PROGRESS_STATE_ERROR) {
-		[[NSColor colorWithSRGBRed:1.0 green:0.2 blue:0.2 alpha:1.0] set];
-	} else if (pr_state == DisplayServer::PROGRESS_STATE_PAUSED) {
-		[[NSColor colorWithSRGBRed:1.0 green:1.0 blue:0.2 alpha:1.0] set];
-	} else {
-		[[NSColor colorWithSRGBRed:0.2 green:0.6 blue:1.0 alpha:1.0] set];
-	}
-	NSRectFill(rect);
-}
-
-@end
 
 void DisplayServerMacOS::window_set_taskbar_progress_value(float p_value, WindowID p_window) {
 	ERR_FAIL_COND(p_window != MAIN_WINDOW_ID);
