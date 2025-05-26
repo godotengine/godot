0140ca719406a41da4604f98b2f43506b886305b
avnotaklu <sukhmander111101@gmail.com>
2025-05-26 22:38:47 +0530

    Fix: Removed freed up FontData's dangling data in Advanced and Fallback text server

--- modules/text_server_fb/text_server_fb.cpp
+++ modules/text_server_fb/text_server_fb.cpp
@@ -121,6 +121,12 @@ void TextServerFallback::_free_rid(const RID &p_rid) {
 		MutexLock ftlock(ft_mutex);
 
 		FontFallback *fd = font_owner.get_or_null(p_rid);
+		for (const KeyValue<Vector2i, FontForSizeFallback *> &ffsd : fd->cache) {
+			OversamplingLevel *ol = oversampling_levels.getptr(ffsd.value->viewport_oversampling);
+			if (ol != nullptr) {
+				ol->fonts.erase(ffsd.value);
+			}
+		}
 		{
 			MutexLock lock(fd->mutex);
 			font_owner.free(p_rid);
