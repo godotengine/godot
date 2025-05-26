c9e8d7e8a91b1ce335936ae345fa1ad7b851e504
Michael Alexsander <michaelalexsander@protonmail.com>
2025-05-26 13:50:40 -0300

    Reorganize scroll and zoom elements in the audio import dialog

--- editor/import/audio_stream_import_settings.h
+++ editor/import/audio_stream_import_settings.h
@@ -35,7 +35,6 @@
 #include "scene/gui/color_rect.h"
 #include "scene/gui/dialogs.h"
 #include "scene/gui/spin_box.h"
-#include "scene/resources/texture.h"
 
 class CheckBox;
 
@@ -59,7 +58,6 @@ class AudioStreamImportSettingsDialog : public ConfirmationDialog {
 	Label *_duration_label = nullptr;
 
 	HScrollBar *zoom_bar = nullptr;
-	Control *zoom_spacer = nullptr;
 	Button *zoom_in = nullptr;
 	Button *zoom_reset = nullptr;
 	Button *zoom_out = nullptr;
