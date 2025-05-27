8062f7fa47c2ec2b1ad73611b60d9547c436271b
Cyril B. <53737317+Cykyrios@users.noreply.github.com>
2025-05-27 15:04:49 +0200

    Add --color option to command line arguments

--- core/os/os.h
+++ core/os/os.h
@@ -52,6 +52,7 @@ class OS {
 	bool _delta_smoothing_enabled = false;
 	bool _verbose_stdout = false;
 	bool _debug_stdout = false;
+	bool _colored_stdout = false;
 	String _local_clipboard;
 	// Assume success by default, all failure cases need to set EXIT_FAILURE explicitly.
 	int _exit_code = EXIT_SUCCESS;
@@ -255,6 +256,7 @@ public:
 
 	bool is_stdout_verbose() const;
 	bool is_stdout_debug_enabled() const;
+	bool is_stdout_colored() const;
 
 	bool is_stdout_enabled() const;
 	bool is_stderr_enabled() const;
