08343189dc89678bfbb108c78ae923b66d5949af
daniel080400 <d90027121@gmail.com>
2025-05-31 16:57:51 +0800

    Rework scene preview thumbnails

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -638,7 +638,6 @@ private:
 	bool _is_scene_unsaved(int p_idx);
 
 	void _find_node_types(Node *p_node, int &count_2d, int &count_3d);
-	void _save_scene_with_preview(String p_file, int p_idx = -1);
 	void _close_save_scene_progress();
 
 	bool _find_scene_in_use(Node *p_node, const String &p_path) const;
@@ -935,11 +934,8 @@ public:
 	Control *get_gui_base() { return gui_base; }
 
 	void save_scene_to_path(String p_file, bool p_with_preview = true) {
-		if (p_with_preview) {
-			_save_scene_with_preview(p_file);
-		} else {
-			_save_scene(p_file);
-		}
+		// p_with_preview has no effect, now generates preview at EditorPackedScenePreviewPlugin
+		_save_scene(p_file);
 	}
 
 	bool is_scene_in_use(const String &p_path);
