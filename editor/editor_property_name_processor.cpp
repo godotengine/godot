8d10d55f598b66338983e7eace50c62ecf832ebe
unknown <pkowal1982@gmail.com>
2025-06-02 21:44:42 +0200

    Export: Modify template without rcedit

--- editor/editor_property_name_processor.cpp
+++ editor/editor_property_name_processor.cpp
@@ -258,7 +258,6 @@ EditorPropertyNameProcessor::EditorPropertyNameProcessor() {
 	capitalize_string_remaps["ppc64"] = "ppc64";
 	capitalize_string_remaps["pvrtc"] = "PVRTC";
 	capitalize_string_remaps["pvs"] = "PVS";
-	capitalize_string_remaps["rcedit"] = "rcedit";
 	capitalize_string_remaps["rcodesign"] = "rcodesign";
 	capitalize_string_remaps["rdo"] = "RDO";
 	capitalize_string_remaps["rgb"] = "RGB";
@@ -312,7 +311,6 @@ EditorPropertyNameProcessor::EditorPropertyNameProcessor() {
 	capitalize_string_remaps["webp"] = "WebP";
 	capitalize_string_remaps["webrtc"] = "WebRTC";
 	capitalize_string_remaps["websocket"] = "WebSocket";
-	capitalize_string_remaps["wine"] = "wine";
 	capitalize_string_remaps["wintab"] = "WinTab";
 	capitalize_string_remaps["winink"] = "Windows Ink";
 	capitalize_string_remaps["wifi"] = "Wi-Fi";
