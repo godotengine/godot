1bede2345b6263b5d33cb07fae81a92ad66694cc
ProgrammerOnCoffee <171874668+ProgrammerOnCoffee@users.noreply.github.com>
2025-06-03 09:52:28 -0400

    Make `BaseButton`s' shortcut feedback timers internal

--- scene/gui/base_button.cpp
+++ scene/gui/base_button.cpp
@@ -452,7 +452,7 @@ void BaseButton::shortcut_input(const Ref<InputEvent> &p_event) {
 			if (shortcut_feedback_timer == nullptr) {
 				shortcut_feedback_timer = memnew(Timer);
 				shortcut_feedback_timer->set_one_shot(true);
-				add_child(shortcut_feedback_timer);
+				add_child(shortcut_feedback_timer, false, INTERNAL_MODE_BACK);
 				shortcut_feedback_timer->set_wait_time(GLOBAL_GET_CACHED(double, "gui/timers/button_shortcut_feedback_highlight_time"));
 				shortcut_feedback_timer->connect("timeout", callable_mp(this, &BaseButton::_shortcut_feedback_timeout));
 			}
