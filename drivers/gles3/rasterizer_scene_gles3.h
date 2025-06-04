288b819c2ca955a849a5947270c63254e58553be
jon1solution <143551917+jon1solution@users.noreply.github.com>
2025-06-04 16:46:04 -0700

    Keeping the top N lights per mesh in the culling phase.
    Previously, each mesh was paired up with the first N of each Omni- and Spot-lights.
    Now each light is scored based on its distance to the mesh, energy, and range.
    The new code uses a heap to track the best N lights (of each type) for predictability.
    
    (cherry picked from commit 979d3e58d08d60261a90505787c1b22b87d01a01)

--- drivers/gles3/rasterizer_scene_gles3.h
+++ drivers/gles3/rasterizer_scene_gles3.h
@@ -353,13 +353,17 @@ private:
 		virtual void set_use_lightmap(RID p_lightmap_instance, const Rect2 &p_lightmap_uv_scale, int p_lightmap_slice_index) override;
 		virtual void set_lightmap_capture(const Color *p_sh9) override;
 
-		virtual void pair_light_instances(const RID *p_light_instances, uint32_t p_light_instance_count) override;
+		virtual void clear_light_instances() override;
+		virtual void pair_light_instance(const RID p_light_instance, RS::LightType light_type, uint32_t placement_idx) override;
 		virtual void pair_reflection_probe_instances(const RID *p_reflection_probe_instances, uint32_t p_reflection_probe_instance_count) override;
 		virtual void pair_decal_instances(const RID *p_decal_instances, uint32_t p_decal_instance_count) override {}
 		virtual void pair_voxel_gi_instances(const RID *p_voxel_gi_instances, uint32_t p_voxel_gi_instance_count) override {}
 
 		virtual void set_softshadow_projector_pairing(bool p_softshadow, bool p_projector) override {}
 	};
+
+	virtual uint32_t get_max_lights_total() override;
+	virtual uint32_t get_max_lights_per_mesh() override;
 
 	enum {
 		INSTANCE_DATA_FLAGS_DYNAMIC = 1 << 3,
