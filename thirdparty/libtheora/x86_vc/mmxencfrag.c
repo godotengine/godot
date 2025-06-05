ff429e0e64e5118ffd776659e26e02b8180ecb4d
Rémi Verschelde <rverschelde@gmail.com>
2025-06-06 01:36:51 +0200

    libtheora: Update to 1.2.0

--- thirdparty/libtheora/x86_vc/mmxencfrag.c
+++ thirdparty/libtheora/x86_vc/mmxencfrag.c
@@ -6,12 +6,11 @@
  * IN 'COPYING'. PLEASE READ THESE TERMS BEFORE DISTRIBUTING.       *
  *                                                                  *
  * THE Theora SOURCE CODE IS COPYRIGHT (C) 2002-2009                *
- * by the Xiph.Org Foundation http://www.xiph.org/                  *
+ * by the Xiph.Org Foundation https://www.xiph.org/                 *
  *                                                                  *
  ********************************************************************
 
   function:
-  last mod: $Id: dsp_mmx.c 14579 2008-03-12 06:42:40Z xiphmont $
 
  ********************************************************************/
 #include <stddef.h>
