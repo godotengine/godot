64e015cb652fd00607ec63ff3221ae4c9286d42b
Silc Lizard (Tokage) Renew <61938263+TokageItLab@users.noreply.github.com>
2025-06-06 02:23:59 +0900

    Add default paramater for AnimationNode as super class

--- scene/animation/animation_tree.cpp
+++ scene/animation/animation_tree.cpp
@@ -52,6 +52,9 @@ void AnimationNode::get_parameter_list(List<PropertyInfo> *r_list) const {
 
 Variant AnimationNode::get_parameter_default_value(const StringName &p_parameter) const {
 	Variant ret;
+	if (p_parameter == current_length || p_parameter == current_position || p_parameter == current_delta) {
+		return 0.0;
+	}
 	GDVIRTUAL_CALL(_get_parameter_default_value, p_parameter, ret);
 	return ret;
 }
