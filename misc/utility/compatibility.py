00b529d67a6405d88a0ccb524c2bfa4efb8bdd37
Ozelot Vanilla <ozelotvanilla@gmail.com>
2025-06-06 15:11:53 +0900

    ongoing: Fix `scons` compability on enabling C++ 20 module

---
+++ misc/utility/compatibility.py
@@ -0,0 +1,52 @@
+#!/usr/bin/env python
+import methods
+
+import os
+import subprocess
+
+is_able_to_use_module_cache = None
+
+def try_use_cxx20_module(env):
+    """
+    Use C++20's `module` if current compiler supports that.
+    Referencing https://en.cppreference.com/w/cpp/compiler_support/20.
+    """
+    global is_able_to_use_module_cache
+
+    if is_able_to_use_module_cache is not None:
+        return is_able_to_use_module_cache
+    else:
+        is_able_to_use_module_cache = True
+
+        # According to the reference, currently (06/03/2025, mm/dd/yyyy),
+        #  only msvc has full support of module.
+        if env.msvc and is_msvc_support_module(env):
+            env.Append(CCFLAGS=["/experimental:module"])
+        # Apple clang also supports module.
+        elif methods.using_clang(env) and is_clang_support_module():
+            env.Append(CCFLAGS=["-fmodules", "-fcxx-modules"])
+        else:
+            is_able_to_use_module_cache = False
+
+
+def is_msvc_support_module(env) -> bool:
+    """
+    MSVC supports C++ modules after 19.2.8.
+    """
+    version = methods.get_compiler_version(env)
+
+    return version["major"] > 19 \
+        or (version["major"] == 19 and version["minor"] >= 2) \
+        or (version["major"] == 19 and version["minor"] == 2 and version["patch"] >= 8)
+
+
+def is_clang_support_module() -> bool:
+    """
+    Check if the clang is Apple clang.
+    """
+    env = os.environ.copy()
+    env["LC_ALL"] = "C"
+    version_text: str = subprocess.check_output(
+        ["clang", "--version"], env=env, encoding="utf-8"
+    )
+    return "Apple clang version" in version_text
