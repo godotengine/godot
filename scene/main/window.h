34a97f9c9baa8e7727e098c9efa358860be18c4a
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-06-15 17:18:03 +0300

    Fix scale and position of popups with `force_native`.

--- scene/main/window.h
+++ scene/main/window.h
@@ -509,6 +509,7 @@ public:
 	virtual Transform2D get_final_transform() const override;
 	virtual Transform2D get_screen_transform_internal(bool p_absolute_position = false) const override;
 	virtual Transform2D get_popup_base_transform() const override;
+	virtual Transform2D get_popup_base_transform_native() const override;
 	virtual Viewport *get_section_root_viewport() const override;
 	virtual bool is_attached_in_viewport() const override;
 
