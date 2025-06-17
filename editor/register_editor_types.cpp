4828c9defbe13e73dc3c501edb0f7041d317503c
Kongfa Waroros <gongpha@hotmail.com>
2025-06-18 04:38:10 +0700

    Implement `EditorRangeDial` on top of `EditorSpinSlider`

--- editor/register_editor_types.cpp
+++ editor/register_editor_types.cpp
@@ -50,6 +50,7 @@
 #include "editor/file_system/editor_file_system.h"
 #include "editor/file_system/editor_paths.h"
 #include "editor/gui/editor_file_dialog.h"
+#include "editor/gui/editor_range_dial.h"
 #include "editor/gui/editor_spin_slider.h"
 #include "editor/gui/editor_toaster.h"
 #include "editor/import/3d/resource_importer_obj.h"
@@ -191,6 +192,8 @@ void register_editor_types() {
 	GDREGISTER_CLASS(EditorScriptPicker);
 	GDREGISTER_ABSTRACT_CLASS(EditorUndoRedoManager);
 	GDREGISTER_CLASS(EditorContextMenuPlugin);
+	GDREGISTER_CLASS(EditorRangeDial);
+	GDREGISTER_CLASS(EditorRangeDialPopup);
 
 	GDREGISTER_ABSTRACT_CLASS(FileSystemDock);
 	GDREGISTER_VIRTUAL_CLASS(EditorFileSystemImportFormatSupportQuery);
