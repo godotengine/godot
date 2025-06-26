d443dffca3c2cb0905f813d3fb65a10b4a184e0d
Adam Scott <ascott.ca@gmail.com>
2025-06-26 08:07:29 -0400

    Revert "Web: Avoid unnecessary gamepad polling when no gamepads are connected"
    This reverts commit 3e7e09f9152288ced7d404b7f7b79e8de9ad165b.

--- platform/web/js/libs/library_godot_input.js
+++ platform/web/js/libs/library_godot_input.js
@@ -205,7 +205,6 @@ const GodotInputGamepads = {
 		sample: function () {
 			const pads = GodotInputGamepads.get_pads();
 			const samples = [];
-			let active = 0;
 			for (let i = 0; i < pads.length; i++) {
 				const pad = pads[i];
 				if (!pad) {
@@ -225,10 +224,8 @@ const GodotInputGamepads = {
 					s.axes.push(pad.axes[a]);
 				}
 				samples.push(s);
-				active++;
 			}
 			GodotInputGamepads.samples = samples;
-			return active;
 		},
 
 		init: function (onchange) {
@@ -662,7 +659,8 @@ const GodotInput = {
 	godot_js_input_gamepad_sample__proxy: 'sync',
 	godot_js_input_gamepad_sample__sig: 'i',
 	godot_js_input_gamepad_sample: function () {
-		return GodotInputGamepads.sample();
+		GodotInputGamepads.sample();
+		return 0;
 	},
 
 	godot_js_input_gamepad_sample_get__proxy: 'sync',
