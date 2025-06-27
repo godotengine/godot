16c9c811c279337c491f89582a7cbaa08f2c0aca
Logan Detrick <logandetrick@ucsb.edu>
2025-06-27 15:08:41 -0700

    Cleanup EditorProperty setup methods

--- editor/inspector/editor_properties.h
+++ editor/inspector/editor_properties.h
@@ -46,6 +46,19 @@ class SceneTreeDialog;
 class TextEdit;
 class TextureButton;
 
+struct EditorPropertyRangeHint {
+	bool or_greater = true;
+	bool or_less = true;
+	double min = 0.0;
+	double max = 0.0;
+	double step = 1.0;
+	String suffix;
+	bool exp_range = false;
+	bool prefer_slider = false;
+	bool hide_control = true;
+	bool radians_as_degrees = false;
+};
+
 class EditorPropertyNil : public EditorProperty {
 	GDCLASS(EditorPropertyNil, EditorProperty);
 	LineEdit *text = nullptr;
@@ -371,7 +384,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(int64_t p_min, int64_t p_max, int64_t p_step, bool p_prefer_slider, bool p_hide_control, bool p_allow_greater, bool p_allow_lesser, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyInteger();
 };
 
@@ -422,7 +435,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, bool p_exp_range, bool p_greater, bool p_lesser, const String &p_suffix = String(), bool p_radians_as_degrees = false);
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyFloat();
 };
 
@@ -478,7 +491,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyRect2(bool p_force_wide = false);
 };
 
@@ -493,7 +506,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(int p_min, int p_max, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyRect2i(bool p_force_wide = false);
 };
 
@@ -508,7 +521,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyPlane(bool p_force_wide = false);
 };
 
@@ -540,7 +553,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String(), bool p_hide_editor = false);
+	void setup(const EditorPropertyRangeHint &p_range_hint, bool p_hide_editor = false);
 	EditorPropertyQuaternion();
 };
 
@@ -555,7 +568,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyAABB();
 };
 
@@ -570,7 +583,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyTransform2D(bool p_include_origin = true);
 };
 
@@ -585,7 +598,7 @@ protected:
 
 public:
 	virtual void update_property() override;
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyBasis();
 };
 
@@ -601,7 +614,7 @@ protected:
 public:
 	virtual void update_property() override;
 	virtual void update_using_transform(Transform3D p_transform);
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyTransform3D();
 };
 
@@ -617,7 +630,7 @@ protected:
 public:
 	virtual void update_property() override;
 	virtual void update_using_transform(Projection p_transform);
-	void setup(double p_min, double p_max, double p_step, bool p_hide_control, const String &p_suffix = String());
+	void setup(const EditorPropertyRangeHint &p_range_hint);
 	EditorPropertyProjection();
 };
 
