37fd6817d7097226afdfbe269f1b8f0f9511c54b
A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>
2025-07-07 11:44:45 +0200

    Use squared values for variables and comparisons

--- editor/scene/2d/physics/collision_shape_2d_editor_plugin.cpp
+++ editor/scene/2d/physics/collision_shape_2d_editor_plugin.cpp
@@ -367,7 +367,7 @@ bool CollisionShape2DEditor::forward_canvas_gui_input(const Ref<InputEvent> &p_e
 		if (mb->get_button_index() == MouseButton::LEFT) {
 			if (mb->is_pressed()) {
 				for (int i = 0; i < handles.size(); i++) {
-					if (xform.xform(handles[i]).distance_to(gpoint) < grab_threshold) {
+					if (xform.xform(handles[i]).distance_squared_to(gpoint) < (grab_threshold * grab_threshold)) {
 						edit_handle = i;
 
 						break;
