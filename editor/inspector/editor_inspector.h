78862d9b578e051a207edf472c4385aafde4cf9d
Haoyu Qiu <timothyqiu32@gmail.com>
2025-07-07 21:35:31 +0800

    Make editor property clipboard static

--- editor/inspector/editor_inspector.h
+++ editor/inspector/editor_inspector.h
@@ -38,14 +38,12 @@
 
 class AddMetadataDialog;
 class AcceptDialog;
-class ConfirmationDialog;
 class EditorInspector;
 class EditorValidationPanel;
 class HSeparator;
 class LineEdit;
 class MarginContainer;
 class OptionButton;
-class PanelContainer;
 class PopupMenu;
 class SpinBox;
 class StyleBoxFlat;
@@ -779,7 +777,8 @@ class EditorInspector : public ScrollContainer {
 
 	String property_prefix; // Used for sectioned inspector.
 	String object_class;
-	Variant property_clipboard;
+
+	static inline Variant property_clipboard;
 
 	bool restrict_to_basic = false;
 
@@ -850,6 +849,9 @@ public:
 	static void initialize_category_theme(EditorInspectorCategory::ThemeCache &p_cache, Control *p_control);
 	static void initialize_property_theme(EditorProperty::ThemeCache &p_cache, Control *p_control);
 
+	static void set_property_clipboard(const Variant &p_value);
+	static Variant get_property_clipboard();
+
 	bool is_main_editor_inspector() const;
 	String get_selected_path() const;
 
@@ -908,8 +910,6 @@ public:
 	void set_use_deletable_properties(bool p_enabled);
 
 	void set_restrict_to_basic_settings(bool p_restrict);
-	void set_property_clipboard(const Variant &p_value);
-	Variant get_property_clipboard() const;
 
 	EditorInspector();
 };
