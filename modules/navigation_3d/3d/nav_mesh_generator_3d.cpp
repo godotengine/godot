fcb059b55aca530c1e66c83a4f4f81f9ece8094a
dementive <87823030+dementive@users.noreply.github.com>
2025-07-10 22:09:56 -0500

    Optimize scene tree groups

--- modules/navigation_3d/3d/nav_mesh_generator_3d.cpp
+++ modules/navigation_3d/3d/nav_mesh_generator_3d.cpp
@@ -278,12 +278,12 @@ void NavMeshGenerator3D::set_generator_parsers(LocalVector<NavMeshGeometryParser
 }
 
 void NavMeshGenerator3D::generator_parse_source_geometry_data(const Ref<NavigationMesh> &p_navigation_mesh, Ref<NavigationMeshSourceGeometryData3D> p_source_geometry_data, Node *p_root_node) {
-	List<Node *> parse_nodes;
+	Vector<Node *> parse_nodes;
 
 	if (p_navigation_mesh->get_source_geometry_mode() == NavigationMesh::SOURCE_GEOMETRY_ROOT_NODE_CHILDREN) {
 		parse_nodes.push_back(p_root_node);
 	} else {
-		p_root_node->get_tree()->get_nodes_in_group(p_navigation_mesh->get_source_group_name(), &parse_nodes);
+		parse_nodes = p_root_node->get_tree()->get_nodes_in_group(p_navigation_mesh->get_source_group_name());
 	}
 
 	Transform3D root_node_transform = Transform3D();
