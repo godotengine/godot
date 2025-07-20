66b3d293d360e9bad3e6be945f699fc0ea9f980a
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-07-20 13:50:51 +0300

    [macOS] Add support for exporting macOS 26 Liquid Glass icons.

--- platform/macos/os_macos.h
+++ platform/macos/os_macos.h
@@ -103,6 +103,7 @@ public:
 	virtual String get_temp_path() const override;
 	virtual String get_bundle_resource_dir() const override;
 	virtual String get_bundle_icon_path() const override;
+	virtual String get_bundle_icon_name() const override;
 	virtual String get_godot_dir_name() const override;
 
 	virtual String get_system_dir(SystemDir p_dir, bool p_shared_storage = true) const override;
