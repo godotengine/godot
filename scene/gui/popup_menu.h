2111ff0e2a0b7fc3e51417b42611b0ac5197b6e2
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2025-07-21 10:03:07 +0300

    Fix menu keyboard and controller navigation.

--- scene/gui/popup_menu.h
+++ scene/gui/popup_menu.h
@@ -168,6 +168,7 @@ class PopupMenu : public Popup {
 	const float DEFAULT_GAMEPAD_EVENT_DELAY_MS = 0.5;
 	const float GAMEPAD_EVENT_REPEAT_RATE_MS = 1.0 / 20;
 	float gamepad_event_delay_ms = DEFAULT_GAMEPAD_EVENT_DELAY_MS;
+	bool joypad_event_process = false;
 
 	struct ThemeCache {
 		Ref<StyleBox> panel_style;
