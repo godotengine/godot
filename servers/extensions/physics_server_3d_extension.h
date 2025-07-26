34c1376f5469bab50506dd4d6675c9bd20bf6a66
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-07-26 14:14:26 -0700

    Add Tapered Capsule 3D Shape and Mesh Classes
    Co-authored-by: Aaron Franke <arnfranke@yahoo.com>

--- servers/extensions/physics_server_3d_extension.h
+++ servers/extensions/physics_server_3d_extension.h
@@ -216,6 +216,7 @@ public:
 	EXBIND0R(RID, sphere_shape_create)
 	EXBIND0R(RID, box_shape_create)
 	EXBIND0R(RID, capsule_shape_create)
+	EXBIND0R(RID, tapered_capsule_shape_create)
 	EXBIND0R(RID, cylinder_shape_create)
 	EXBIND0R(RID, convex_polygon_shape_create)
 	EXBIND0R(RID, concave_polygon_shape_create)
