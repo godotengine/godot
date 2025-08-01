8a8ded445d78c245ba281cd4217aed3f17d90ff9
SheepYhangCN <sheepyuhang@163.com>
2025-08-02 07:50:21 +0800

    Implement `get_available_rendering_drivers`

--- servers/rendering_server.cpp
+++ servers/rendering_server.cpp
@@ -3482,6 +3482,7 @@ void RenderingServer::_bind_methods() {
 
 	ClassDB::bind_method(D_METHOD("get_current_rendering_driver_name"), &RenderingServer::get_current_rendering_driver_name);
 	ClassDB::bind_method(D_METHOD("get_current_rendering_method"), &RenderingServer::get_current_rendering_method);
+	ClassDB::bind_method(D_METHOD("get_available_rendering_drivers"), &RenderingServer::get_available_rendering_drivers);
 
 	ClassDB::bind_method(D_METHOD("make_sphere_mesh", "latitudes", "longitudes", "radius"), &RenderingServer::make_sphere_mesh);
 	ClassDB::bind_method(D_METHOD("get_test_cube"), &RenderingServer::get_test_cube);
