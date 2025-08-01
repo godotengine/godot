xianofantasia/Go

Display a log message when using Reload Saved Scene
This makes it easier to know what's going on when using `print()`
in a `@tool` script.

editor/editor_node.cpp
@@ -2745,6 +2745,7 @@ void EditorNode::_menu_option_confirm(int p_option, bool p_confirmed) {
 			EditorUndoRedoManager::get_singleton()->clear_history(false, editor_data.get_current_edited_scene_history_id());
 			scene_tabs->set_current_tab(cur_idx);
 
+			EditorNode::get_log()->add_message(vformat("--- Scene reloaded: %s ---", filename), EditorLog::MSG_TYPE_EDITOR);
 		} break;
 
 		case FILE_SHOW_IN_FILESYSTEM: {

godotengine/godot

editor/editor_node.cpp
@@ -3243,6 +3243,8 @@ void EditorNode::_menu_option_confirm(int p_option, bool p_confirmed) {
 			}
 
 			_discard_changes();
+
+			EditorNode::get_log()->add_message(vformat("--- Scene reloaded: %s ---", scene_filename), EditorLog::MSG_TYPE_EDITOR);
 		} break;
 
 		case EditorSceneTabs::SCENE_SHOW_IN_FILESYSTEM: {