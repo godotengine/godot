e7a2ae51e1f3595e213c4cc143c0302ab0c96cc4
ritoban23 <ankudutt101@gmail.com>
2025-08-11 15:44:27 +0530

    Fix JSON escape for ASCII control characters by correcting escape sequence format

--- core/string/ustring.cpp
+++ core/string/ustring.cpp
@@ -4752,8 +4752,7 @@ String String::json_escape() const {
 			escaped += "\\t";
 		} else if (c >= 0x00 && c <= 0x1F) {
 			// ASCII control characters must be escaped using \uXXXX format
-			escaped += "\\u";
-			escaped += "00";
+			escaped += "\\u00";
 			escaped += hex_char_table_upper[(c >> 4) & 0xF];
 			escaped += hex_char_table_upper[c & 0xF];
 		} else {
