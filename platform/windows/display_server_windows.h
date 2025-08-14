6aeea36c082602f3ffa935a82b8431b8c3299f7e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2025-08-14 03:33:06 +0200

    Fix performance issues when moving the mouse with high polling rate on Windows
    (cherry picked from commit 8caf6fd456ccdcc2cbfc3c8f2e601597911a1959)
    Co-Authored-By: Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
    Co-Authored-By: Jon Tamayo <volsungn@gmail.com>

--- platform/windows/display_server_windows.h
+++ platform/windows/display_server_windows.h
@@ -538,6 +538,10 @@ class DisplayServerWindows : public DisplayServer {
 	HWND _find_window_from_process_id(OS::ProcessID p_pid, HWND p_current_hwnd);
 
 	void initialize_tts() const;
+	void process_raw_input();
+	Vector2 _get_raw_mouse_motion(const RAWINPUT &p_raw, DisplayServer::WindowID p_window_id);
+	void _process_raw_mouse_motion(const Vector2 &p_relative, bool p_left_button_down, DisplayServer::WindowID p_window_id);
+	void _process_raw_input_event(const RAWINPUT &p_raw, DisplayServer::WindowID p_window_id);
 
 public:
 	LRESULT WndProcFileDialog(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
