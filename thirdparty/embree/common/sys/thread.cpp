25ed4d4258e3f0753d0e2dbcb79f5aab17273439
Guo Wei <me@imkira.com>
2025-08-15 12:25:56 +0800

    Create third party patches

--- thirdparty/embree/common/sys/thread.cpp
+++ thirdparty/embree/common/sys/thread.cpp
@@ -397,13 +397,13 @@ namespace embree
 
     /* set affinity */
 #if defined(__LINUX__) && !defined(__ANDROID__) && !defined(__OPEN_HARMONY__)
-	if (threadID >= 0) {
-		cpu_set_t cset;
-		CPU_ZERO(&cset);
-		threadID = mapThreadID(threadID);
-		CPU_SET(threadID, &cset);
-		pthread_setaffinity_np(*tid, sizeof(cset), &cset);
-	}
+    if (threadID >= 0) {
+      cpu_set_t cset;
+      CPU_ZERO(&cset);
+      threadID = mapThreadID(threadID);
+      CPU_SET(threadID, &cset);
+      pthread_setaffinity_np(*tid, sizeof(cset), &cset);
+    }
 #elif defined(__FreeBSD__)
     if (threadID >= 0) {
       cpuset_t cset;
@@ -412,12 +412,12 @@ namespace embree
       pthread_setaffinity_np(*tid, sizeof(cset), &cset);
     }
 #elif defined(__ANDROID__) || defined(__OPEN_HARMONY__)
-	if (threadID >= 0) {
-		cpu_set_t cset;
-		CPU_ZERO(&cset);
-		CPU_SET(threadID, &cset);
-		sched_setaffinity(pthread_gettid_np(*tid), sizeof(cset), &cset);
-	}
+    if (threadID >= 0) {
+      cpu_set_t cset;
+      CPU_ZERO(&cset);
+      CPU_SET(threadID, &cset);
+      sched_setaffinity(pthread_gettid_np(*tid), sizeof(cset), &cset);
+    }
 #endif
 
     return thread_t(tid);
@@ -438,12 +438,12 @@ namespace embree
   /*! destroy a hardware thread by its handle */
   void destroyThread(thread_t tid) {
 #if defined(__ANDROID__)
-	  FATAL("Can't destroy threads on Android."); // pthread_cancel not implemented.
+    FATAL("Can't destroy threads on Android."); // pthread_cancel not implemented.
 #elif defined(__OPEN_HARMONY__)
-	  FATAL("Can't destroy threads on OpenHarmony."); // pthread_cancel not implemented.
+    FATAL("Can't destroy threads on OpenHarmony."); // pthread_cancel not implemented.
 #else
-	  pthread_cancel(*(pthread_t *)tid);
-	  delete (pthread_t *)tid;
+    pthread_cancel(*(pthread_t *)tid);
+    delete (pthread_t *)tid;
 #endif
   }
 
