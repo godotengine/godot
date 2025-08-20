xianofantasia/Go

Fire warning message if the parameter does not exist

scene/3d/visual_instance_3d.cpp
@@ -352,6 +352,10 @@ float GeometryInstance3D::get_lod_bias() const {
 }
 
 void GeometryInstance3D::set_instance_shader_parameter(const StringName &p_name, const Variant &p_value) {
+	if (!instance_shader_parameters.has(p_name)) {
+		WARN_PRINT_ED(vformat("Instance shader parameter \'%s\' does not exist in base %s.", p_name, get_class_name()));
+	}
+
 	if (p_value.get_type() == Variant::NIL) {
 		Variant def_value = RS::get_singleton()->instance_geometry_get_shader_parameter_default_value(get_instance(), p_name);
 		RS::get_singleton()->instance_geometry_set_shader_parameter(get_instance(), p_name, def_value);

scene/resources/material.cpp
@@ -365,6 +365,10 @@ Ref<Shader> ShaderMaterial::get_shader() const {
 }
 
 void ShaderMaterial::set_shader_parameter(const StringName &p_param, const Variant &p_value) {
+	if (!param_cache.has(p_param)) {
+		WARN_PRINT_ED(vformat("Shader parameter \'%s\' does not exist in base %s.", p_param, get_class_name()));
+	}
+
 	if (p_value.get_type() == Variant::NIL) {
 		param_cache.erase(p_param);
 		RS::get_singleton()->material_set_param(_get_material(), p_param, Variant());

godotengine/godot

scene/3d/visual_instance_3d.cpp
@@ -398,6 +398,10 @@ float GeometryInstance3D::get_lod_bias() const {
 }
 
 void GeometryInstance3D::set_instance_shader_parameter(const StringName &p_name, const Variant &p_value) {
+	if (!instance_shader_parameters.has(p_name)) {
+		ERR_PRINT_ED(vformat("Instance shader parameter \'%s\' does not exist in base %s.", p_name, get_class_name()));
+	}
+
 	if (p_value.get_type() == Variant::NIL) {
 		Variant def_value = RS::get_singleton()->instance_geometry_get_shader_parameter_default_value(get_instance(), p_name);
 		RS::get_singleton()->instance_geometry_set_shader_parameter(get_instance(), p_name, def_value);

scene/resources/material.cpp
@@ -445,6 +445,10 @@ Ref<Shader> ShaderMaterial::get_shader() const {
 }
 
 void ShaderMaterial::set_shader_parameter(const StringName &p_param, const Variant &p_value) {
+	if (!param_cache.has(p_param)) {
+		ERR_PRINT_ED(vformat("Shader parameter \'%s\' does not exist in base %s.", p_param, get_class_name()));
+	}
+
 	RID material_rid = _get_material();
 	if (p_value.get_type() == Variant::NIL) {
 		param_cache.erase(p_param);