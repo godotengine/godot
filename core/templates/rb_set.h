d5c6e541bfde0bf616d6b17bf5dd6554ad8a8c9b
Spartan322 <Megacake1234@gmail.com>
2025-08-21 07:39:49 -0400

    Add ConstIterator conversions for missing container Iterators

--- core/templates/rb_set.h
+++ core/templates/rb_set.h
@@ -83,6 +83,7 @@ public:
 
 	typedef T ValueType;
 
+	struct ConstIterator;
 	struct Iterator {
 		_FORCE_INLINE_ T &operator*() const {
 			return E->get();
@@ -104,6 +105,10 @@ public:
 		Iterator(Element *p_E) { E = p_E; }
 		Iterator() {}
 		Iterator(const Iterator &p_it) { E = p_it.E; }
+
+		operator ConstIterator() const {
+			return ConstIterator(E);
+		}
 
 	private:
 		Element *E = nullptr;
