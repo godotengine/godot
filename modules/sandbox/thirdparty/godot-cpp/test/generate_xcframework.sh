c1006f8cc07e95d90fe72668fe231df8b5db4861
K. S. Ernest (iFire) Lee <ernest.lee@chibifire.com>
2025-08-21 17:22:56 -0700

    Remove godot-cpp.

--- modules/sandbox/thirdparty/godot-cpp/test/generate_xcframework.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-scons arch=universal ios_simulator=yes platform=ios target=$1 $2
-scons arch=arm64 ios_simulator=no platform=ios target=$1 $2
-
-xcodebuild -create-xcframework -library ./project/bin/libgdexample.ios.$1.a -library ./project/bin/libgdexample.ios.$1.simulator.a -output ./project/bin/libgdexample.ios.$1.xcframework
-xcodebuild -create-xcframework -library ../bin/libgodot-cpp.ios.$1.arm64.a -library ../bin/libgodot-cpp.ios.$1.universal.simulator.a  -output ./project/bin/libgodot-cpp.ios.$1.xcframework
