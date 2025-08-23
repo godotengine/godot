8a84f38a02c6b5d50a2a6f49bc03a061be851b7e
kobewi <kobewi4e@gmail.com>
2025-08-24 00:04:00 +0200

    Add setup_color_picker() overload for ColorPickerButton

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -701,6 +701,7 @@ private:
 	void _pick_main_scene_custom_action(const String &p_custom_action_name);
 
 	void _immediate_dialog_confirmed();
+	void _color_picker_popup_created(ColorPickerButton *p_button);
 
 	void _begin_first_scan();
 
@@ -915,6 +916,7 @@ public:
 	bool is_multi_window_enabled() const;
 
 	void setup_color_picker(ColorPicker *p_picker);
+	void setup_color_picker(ColorPickerButton *p_button);
 
 	void request_instantiate_scene(const String &p_path);
 	void request_instantiate_scenes(const Vector<String> &p_files);
