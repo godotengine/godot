d6d60845fe57069d65500e4d6f26e6f6fe0e6f01
Aaron Franke <arnfranke@yahoo.com>
2025-08-26 15:46:32 -0700

    Separate visual shader code out of shader editor plugin and dialog

--- editor/docks/scene_tree_dock.cpp
+++ editor/docks/scene_tree_dock.cpp
@@ -4255,7 +4255,7 @@ void SceneTreeDock::attach_shader_to_selected(int p_preferred_mode) {
 	shader_create_dialog->connect("shader_created", callable_mp(this, &SceneTreeDock::_shader_created));
 	shader_create_dialog->connect(SceneStringName(confirmed), callable_mp(this, &SceneTreeDock::_shader_creation_closed));
 	shader_create_dialog->connect("canceled", callable_mp(this, &SceneTreeDock::_shader_creation_closed));
-	shader_create_dialog->config(path, true, true, -1, p_preferred_mode);
+	shader_create_dialog->config(path, true, true, "", p_preferred_mode);
 	shader_create_dialog->popup_centered();
 }
 
