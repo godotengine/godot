9829582f35e26fbdfd90100138f48a7528433100
Michael Alexsander <michaelalexsander@protonmail.com>
2025-09-03 20:39:18 -0300

    Hide `Control` focus when given via mouse input

--- editor/scene/connections_dialog.cpp
+++ editor/scene/connections_dialog.cpp
@@ -485,7 +485,7 @@ void ConnectDialog::_update_warning_label() {
 }
 
 void ConnectDialog::_post_popup() {
-	callable_mp((Control *)dst_method, &Control::grab_focus).call_deferred();
+	callable_mp((Control *)dst_method, &Control::grab_focus).call_deferred(false);
 	callable_mp(dst_method, &LineEdit::select_all).call_deferred();
 }
 
