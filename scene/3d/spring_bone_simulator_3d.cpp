704baa0005406921bacfc13d9e95212d5c684e9b
Silc Lizard (Tokage) Renew <61938263+TokageItLab@users.noreply.github.com>
2025-09-10 09:46:41 +0900

    Add check is_inside_tree() to SkeletonModifier3D as base class

--- scene/3d/spring_bone_simulator_3d.cpp
+++ scene/3d/spring_bone_simulator_3d.cpp
@@ -1492,9 +1492,6 @@ void SpringBoneSimulator3D::_find_collisions() {
 }
 
 void SpringBoneSimulator3D::_process_collisions() {
-	if (!is_inside_tree()) {
-		return;
-	}
 	for (const ObjectID &oid : collisions) {
 		Object *t_obj = ObjectDB::get_instance(oid);
 		if (!t_obj) {
@@ -1619,10 +1616,6 @@ void SpringBoneSimulator3D::_set_active(bool p_active) {
 }
 
 void SpringBoneSimulator3D::_process_modification(double p_delta) {
-	if (!is_inside_tree()) {
-		return;
-	}
-
 	Skeleton3D *skeleton = get_skeleton();
 	if (!skeleton) {
 		return;
