cb031b26cd02c7f2a2c58e31eedf08d88bb38fa8
teatov <hlebpoimenigleb@gmail.com>
2025-09-12 15:27:00 +1000

    Add MP3 ID3v2 tag metadata support

--- modules/minimp3/audio_stream_mp3.h
+++ modules/minimp3/audio_stream_mp3.h
@@ -107,6 +107,7 @@ class AudioStreamMP3 : public AudioStream {
 	double bpm = 0;
 	int beat_count = 0;
 	int bar_beats = 4;
+	Dictionary tags;
 
 protected:
 	static void _bind_methods();
@@ -129,6 +130,9 @@ public:
 
 	void set_bar_beats(int p_bar_beats);
 	virtual int get_bar_beats() const override;
+
+	void set_tags(const Dictionary &p_tags);
+	virtual Dictionary get_tags() const override;
 
 	virtual Ref<AudioStreamPlayback> instantiate_playback() override;
 	virtual String get_stream_name() const override;
