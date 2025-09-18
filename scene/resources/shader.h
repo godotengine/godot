ea129d1c745e04e77119a4d9a13eafa1aea9bcee
Gergely Kis <gergely.kis@migeran.com>
2025-09-18 12:37:18 +0200

    LibGodot: Shader includes should be handled as proper dependencies for export

--- scene/resources/shader.h
+++ scene/resources/shader.h
@@ -108,6 +108,7 @@ VARIANT_ENUM_CAST(Shader::Mode);
 class ResourceFormatLoaderShader : public ResourceFormatLoader {
 public:
 	virtual Ref<Resource> load(const String &p_path, const String &p_original_path = "", Error *r_error = nullptr, bool p_use_sub_threads = false, float *r_progress = nullptr, CacheMode p_cache_mode = CACHE_MODE_REUSE) override;
+	virtual void get_dependencies(const String &p_path, List<String> *p_dependencies, bool p_add_types = false) override;
 	virtual void get_recognized_extensions(List<String> *p_extensions) const override;
 	virtual bool handles_type(const String &p_type) const override;
 	virtual String get_resource_type(const String &p_path) const override;
