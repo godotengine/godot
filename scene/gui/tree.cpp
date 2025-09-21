932a2a257153fcc91659452e4e05394840aca7d3
Mounir Tohami <53877170+WhalesState@users.noreply.github.com>
2025-09-22 00:34:02 +0300

    Avoid unnecessary accessibility calls when not supported.

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -4839,6 +4839,7 @@ void Tree::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_EXIT_TREE:
 		case NOTIFICATION_ACCESSIBILITY_INVALIDATE: {
+#ifdef ACCESSKIT_ENABLED
 			if (root) {
 				_accessibility_clean_info(root);
 			}
@@ -4846,8 +4847,10 @@ void Tree::_notification(int p_what) {
 				col.accessibility_col_element = RID();
 			}
 			accessibility_scroll_element = RID();
+#endif // ACCESSKIT_ENABLED
 		} break;
 
+#ifdef ACCESSKIT_ENABLED
 		case NOTIFICATION_ACCESSIBILITY_UPDATE: {
 			RID ae = get_accessibility_element();
 			ERR_FAIL_COND(ae.is_null());
@@ -4906,6 +4909,7 @@ void Tree::_notification(int p_what) {
 			DisplayServer::get_singleton()->accessibility_update_set_table_row_count(ae, rows);
 
 		} break;
+#endif // ACCESSKIT_ENABLED
 
 		case NOTIFICATION_FOCUS_ENTER: {
 			if (get_viewport()) {
