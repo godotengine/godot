d47463b8aa32afbf19d9a33813f8de61c148f8d8
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2025-09-24 00:40:37 -0700

    Check if reflection atlas size == 0

--- drivers/gles3/storage/light_storage.cpp
+++ drivers/gles3/storage/light_storage.cpp
@@ -807,6 +807,10 @@ bool LightStorage::reflection_probe_instance_begin_render(RID p_instance, RID p_
 
 	ERR_FAIL_NULL_V(atlas, false);
 
+	if (atlas->count == 0) {
+		return false;
+	}
+
 	ReflectionProbeInstance *rpi = reflection_probe_instance_owner.get_or_null(p_instance);
 	ERR_FAIL_NULL_V(rpi, false);
 
