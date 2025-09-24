921ee1acfeea0a81a5d4050550dba27ccec615e3
Patrick Exner <patrick.exner1@web.de>
2025-09-24 17:57:10 +0200

    Add helper function in project setting to register global shader parameter
    Co-authored-by: jsjtxietian <jsjtxietian@outlook.com>

--- editor/shader/shader_globals_editor.cpp
+++ editor/shader/shader_globals_editor.cpp
@@ -444,6 +444,7 @@ void ShaderGlobalsEditor::_notification(int p_what) {
 	switch (p_what) {
 		case NOTIFICATION_VISIBILITY_CHANGED: {
 			if (is_visible_in_tree()) {
+				_changed();
 				inspector->edit(interface);
 			}
 		} break;
