3d7e97e4554cdc29ea0da600e60eb9378a748d57
Kazox61 <pawelka00florian@gmail.com>
2025-09-25 19:31:07 +0200

    Adds virtual_joystick

--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -94,6 +94,7 @@
 #include "scene/gui/texture_rect.h"
 #include "scene/gui/tree.h"
 #include "scene/gui/video_stream_player.h"
+#include "scene/gui/virtual_joystick.h"
 #include "scene/main/canvas_item.h"
 #include "scene/main/canvas_layer.h"
 #include "scene/main/http_request.h"
@@ -553,6 +554,8 @@ void register_scene_types() {
 
 	GDREGISTER_CLASS(FoldableGroup);
 	GDREGISTER_CLASS(FoldableContainer);
+
+	GDREGISTER_CLASS(VirtualJoystick);
 
 	OS::get_singleton()->yield(); // may take time to init
 
