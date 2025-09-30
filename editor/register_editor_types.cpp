36c7bbb47a2c4aad523d839612b1b7d9c97ff2c8
Malcolm Anderson <malcolminyo@gmail.com>
2025-09-30 16:34:17 -0700

    Visualize MarginContainer margins when selected
    This time as an EditorPlugin!
    
    Improve rendering of inner area of MarginContainer
    
    Decrease thickness and opacity of lines, and render margins as a rectangle rather than lines extending to the edges of the bounding box
    
    Apply suggestions from code review
    
    Co-authored-by: Tomasz Chabora <kobewi4e@gmail.com>
    
    Use `get_margin_size`, change color of viz border, and trigger redraw on MarginContainer's draw signal

--- editor/register_editor_types.cpp
+++ editor/register_editor_types.cpp
@@ -106,6 +106,7 @@
 #include "editor/scene/gradient_editor_plugin.h"
 #include "editor/scene/gui/control_editor_plugin.h"
 #include "editor/scene/gui/font_config_plugin.h"
+#include "editor/scene/gui/margin_container_editor_plugin.h"
 #include "editor/scene/gui/style_box_editor_plugin.h"
 #include "editor/scene/gui/theme_editor_plugin.h"
 #include "editor/scene/material_editor_plugin.h"
@@ -230,6 +231,7 @@ void register_editor_types() {
 	EditorPlugins::add_by_type<GradientTexture2DEditorPlugin>();
 	EditorPlugins::add_by_type<InputEventEditorPlugin>();
 	EditorPlugins::add_by_type<LightmapGIEditorPlugin>();
+	EditorPlugins::add_by_type<MarginContainerEditorPlugin>();
 	EditorPlugins::add_by_type<MaterialEditorPlugin>();
 	EditorPlugins::add_by_type<MeshEditorPlugin>();
 	EditorPlugins::add_by_type<MeshInstance3DEditorPlugin>();
