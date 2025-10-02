7fa373b76b07a6599d21fa2f6b25dd9b6498e89b
HolonProduction <holonproduction@gmail.com>
2025-10-02 15:14:50 +0200

    Optimize `Node::is_ancestor_of` for nodes in tree

--- scene/main/node.cpp
+++ scene/main/node.cpp
@@ -2090,12 +2090,23 @@ Window *Node::get_last_exclusive_window() const {
 
 bool Node::is_ancestor_of(const Node *p_node) const {
 	ERR_FAIL_NULL_V(p_node, false);
-	Node *p = p_node->data.parent;
-	while (p) {
-		if (p == this) {
+
+	const Node *n = p_node;
+
+	if (is_inside_tree() && p_node->data.tree == data.tree) {
+		const int depth = data.depth;
+		while (n->data.depth > depth) {
+			n = n->data.parent;
+		}
+		return n == this;
+	}
+
+	n = p_node->data.parent;
+	while (n) {
+		if (n == this) {
 			return true;
 		}
-		p = p->data.parent;
+		n = n->data.parent;
 	}
 
 	return false;
