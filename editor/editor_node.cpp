1cd1012e554a75e786efd6f2226267b8afb17e40
Allen Pestaluky <allenpestaluky@gmail.com>
2025-10-03 14:33:20 -0400

    Removed `rendering/viewport/tonemap_to_window` project setting. If a user wants to change the main window's `tonemap_to_window` setting, they can do this through the script API; it's such a niche use-case that it doesn't need a dedicated project setting for the main window.

--- editor/editor_node.cpp
+++ editor/editor_node.cpp
@@ -539,10 +539,6 @@ void EditorNode::_update_from_settings() {
 	scene_root->set_use_hdr_2d(use_hdr_2d || force_hdr_2d);
 	get_viewport()->set_use_hdr_2d(use_hdr_2d || force_hdr_2d);
 
-	const bool tonemap_to_window = GLOBAL_GET("rendering/viewport/tonemap_to_window");
-	scene_root->set_tonemap_to_window(tonemap_to_window);
-	get_viewport()->set_tonemap_to_window(tonemap_to_window);
-
 	float mesh_lod_threshold = GLOBAL_GET("rendering/mesh_lod/lod_change/threshold_pixels");
 	scene_root->set_mesh_lod_threshold(mesh_lod_threshold);
 
