e0aff8f6225c092f5bbf7992b30fc3c31586b52e
Adam Scott <ascott.ca@gmail.com>
2025-10-02 21:03:56 -0400

    Remove superfluous file_access import

--- modules/gdscript/tests/test_refactor.h
+++ modules/gdscript/tests/test_refactor.h
@@ -44,7 +44,6 @@
 #include "core/config/project_settings.h"
 #include "core/io/config_file.h"
 #include "core/io/dir_access.h"
-#include "core/io/file_access.h"
 #include "core/object/script_language.h"
 #include "core/variant/dictionary.h"
 #include "core/variant/variant.h"
