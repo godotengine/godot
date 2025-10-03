3316609ddd2a4963e5c2441d167860351f9f222e
passivestar <60579014+passivestar@users.noreply.github.com>
2025-10-03 11:08:22 +0400

    Add missing ItemListSecondary and TreeSecondary theme type variations

--- editor/debugger/script_editor_debugger.cpp
+++ editor/debugger/script_editor_debugger.cpp
@@ -2220,6 +2220,7 @@ ScriptEditorDebugger::ScriptEditorDebugger() {
 		error_tree->set_v_size_flags(SIZE_EXPAND_FILL);
 		error_tree->set_allow_rmb_select(true);
 		error_tree->set_allow_reselect(true);
+		error_tree->set_theme_type_variation("TreeSecondary");
 		error_tree->connect("item_mouse_selected", callable_mp(this, &ScriptEditorDebugger::_error_tree_item_rmb_selected));
 		errors_tab->add_child(error_tree);
 
