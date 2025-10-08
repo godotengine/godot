fc7eda9cbe1b6c44bd38a65af2b1af637773a619
Lukas Tenbrink <lukas.tenbrink@gmail.com>
2025-10-08 11:18:58 +0200

    Remove `compositor.h` include from `world_3d.h`.

--- scene/resources/packed_scene.cpp
+++ scene/resources/packed_scene.cpp
@@ -34,6 +34,7 @@
 #include "core/io/file_access.h"
 #include "core/io/missing_resource.h"
 #include "core/io/resource_loader.h"
+#include "core/object/script_language.h"
 #include "core/templates/local_vector.h"
 #include "scene/2d/node_2d.h"
 #include "scene/gui/control.h"
