5d587e97fc38a4871eb320076889eb9c42ccccaf
Adam Scott <ascott.ca@gmail.com>
2025-10-09 12:09:49 -0400

    Add member_variable rename tests

--- modules/gdscript/tests/scripts/refactor/rename/scripts/noname_a.gd
+++ modules/gdscript/tests/scripts/refactor/rename/scripts/noname_a.gd
@@ -1,6 +1,17 @@
 extends Node
 
-const NoNameB: = preload("res://scripts/noname_b.gd")
+const NoNameB := preload("res://scripts/noname_b.gd")
+var member_variable
+
 
 func _ready() -> void:
 	print("Hello, " + NoNameB.NONAME_B_NAME)
+	print(member_variable)
+
+
+class NoNameAInner:
+	var member_variable: = 2
+
+	func _ready() -> void:
+		print(member_variable)
+		prints("hello", member_variable)
