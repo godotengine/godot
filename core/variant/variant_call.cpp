1e1c646d9c78a6aaa806074e21c1f2f12fada70d
StarryWorm <41305715+StarryWorm@users.noreply.github.com>
2025-10-11 18:23:24 -0400

    rm class_db.h include from variant_call.cpp

--- core/variant/variant_call.cpp
+++ core/variant/variant_call.cpp
@@ -30,11 +30,12 @@
 
 #include "variant.h"
 
+STATIC_ASSERT_INCOMPLETE_TYPE(class, ClassDB);
+
 #include "core/crypto/crypto_core.h"
 #include "core/debugger/engine_debugger.h"
 #include "core/io/compression.h"
 #include "core/io/marshalls.h"
-#include "core/object/class_db.h"
 #include "core/os/os.h"
 #include "core/templates/a_hash_map.h"
 #include "core/templates/local_vector.h"
