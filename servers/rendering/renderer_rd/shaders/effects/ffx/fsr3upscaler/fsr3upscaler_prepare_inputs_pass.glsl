7b3e910a7882e201289e90af826fc82754ca8142
ExMatics HydrogenC <33123710+HydrogenC@users.noreply.github.com>
2025-10-12 19:24:35 +0800

    Initial impl for FSR3 Upscale (untested)

---
+++ servers/rendering/renderer_rd/shaders/effects/ffx/fsr3upscaler/fsr3upscaler_prepare_inputs_pass.glsl
@@ -0,0 +1,8 @@
+#[compute]
+
+#version 450
+
+#VERSION_DEFINES
+
+#include "../../motion_vector_inc.glsl"
+#include "thirdparty/amd-ffx/shaders/fsr3upscaler/ffx_fsr3upscaler_prepare_inputs_pass.glsl"
