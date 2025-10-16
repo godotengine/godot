acc001501c7df217424308359e1812ad65a93045
carsonuderitz@gmail.com <carsonuderitz@gmail.com>
2025-10-16 19:02:57 -0400

    Fix not releasing action when actions are erased

--- core/input/input_map.cpp
+++ core/input/input_map.cpp
@@ -229,6 +229,10 @@ void InputMap::action_erase_event(const StringName &p_action, RequiredParam<Inpu
 void InputMap::action_erase_events(const StringName &p_action) {
 	ERR_FAIL_COND_MSG(!input_map.has(p_action), suggest_actions(p_action));
 
+	if (Input::get_singleton()->is_action_pressed(p_action)) {
+		Input::get_singleton()->action_release(p_action);
+	}
+
 	input_map[p_action].inputs.clear();
 }
 
