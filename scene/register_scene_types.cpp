d5caa747cd65649dda2f043cd55038c00fca7b3b
JekSun97 <jeksun2022@gmail.com>
2025-10-16 20:48:56 +0600

    First implementation

--- scene/register_scene_types.cpp
+++ scene/register_scene_types.cpp
@@ -133,6 +133,7 @@
 #include "scene/resources/environment.h"
 #include "scene/resources/external_texture.h"
 #include "scene/resources/font.h"
+#include "scene/resources/fpslod_level.h"
 #include "scene/resources/gradient.h"
 #include "scene/resources/gradient_texture.h"
 #include "scene/resources/image_texture.h"
@@ -630,6 +631,8 @@ void register_scene_types() {
 	GDREGISTER_CLASS(AnimationNodeTimeSeek);
 	GDREGISTER_CLASS(AnimationNodeTransition);
 	GDREGISTER_CLASS(AnimationNodeObserverTransition);
+
+	GDREGISTER_CLASS(FPSLODLevel);
 
 	GDREGISTER_CLASS(ShaderGlobalsOverride); // can be used in any shader
 
