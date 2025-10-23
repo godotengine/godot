34756daaf8fa92dd2fc70e9d5541c48e9c48bb97
Roland Eigelsreiter <brainfoolong@users.noreply.github.com>
2025-10-23 06:27:36 +0200

    removed unused GodotPinJoint3D::get_applied_torque

--- modules/godot_physics_3d/joints/godot_pin_joint_3d.cpp
+++ modules/godot_physics_3d/joints/godot_pin_joint_3d.cpp
@@ -171,11 +171,6 @@ float GodotPinJoint3D::get_applied_force() const {
 	return 0;
 }
 
-float GodotPinJoint3D::get_applied_torque() const {
-	WARN_PRINT_ONCE("PinJoint3D does not have a value for get_applied_torque()");
-	return 0;
-}
-
 GodotPinJoint3D::GodotPinJoint3D(GodotBody3D *p_body_a, const Vector3 &p_pos_a, GodotBody3D *p_body_b, const Vector3 &p_pos_b) :
 		GodotJoint3D(_arr, 2) {
 	A = p_body_a;
