6483665a7c32cee3784398f140cbdfa95a70aefd
Sat <124409014+SatLess@users.noreply.github.com>
2025-10-25 17:04:22 -0300

    Allow smooth renaming

--- editor/inspector/editor_resource_picker.h
+++ editor/inspector/editor_resource_picker.h
@@ -62,6 +62,7 @@ class EditorResourcePicker : public HBoxContainer {
 
 	ConfirmationDialog *duplicate_resources_dialog = nullptr;
 	Tree *duplicate_resources_tree = nullptr;
+	Tree *rename_resource_tree = nullptr;
 
 	Size2i assign_button_min_size = Size2i(1, 1);
 
@@ -78,6 +79,7 @@ class EditorResourcePicker : public HBoxContainer {
 		OBJ_MENU_PASTE,
 		OBJ_MENU_PASTE_AS_UNIQUE,
 		OBJ_MENU_SHOW_IN_FILE_SYSTEM,
+		OBJ_MENU_RENAME,
 
 		TYPE_BASE_ID = 100,
 		CONVERT_BASE_ID = 1000,
@@ -102,6 +104,7 @@ class EditorResourcePicker : public HBoxContainer {
 
 	void _button_draw();
 	void _button_input(const Ref<InputEvent> &p_event);
+	virtual void shortcut_input(const Ref<InputEvent> &p_event) override;
 
 	String _get_owner_path() const;
 	String _get_resource_type(const Ref<Resource> &p_resource) const;
@@ -117,6 +120,8 @@ class EditorResourcePicker : public HBoxContainer {
 	void _ensure_resource_menu();
 	void _gather_resources_to_duplicate(const Ref<Resource> p_resource, TreeItem *p_item, const String &p_property_name = "") const;
 	void _duplicate_selected_resources();
+	void _perform_resource_rename();
+	void _cancel_resource_rename();
 
 protected:
 	virtual void _update_resource();
