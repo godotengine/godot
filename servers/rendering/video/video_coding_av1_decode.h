03770a8887683b94ffa0ba0efc0808a5f46807bf
ArchercatNEO <tururu.pompella@gmail.com>
2025-10-29 13:22:24 +0000

    feat: AV1 decoding (partial)

--- servers/rendering/video/video_coding_av1_decode.h
+++ servers/rendering/video/video_coding_av1_decode.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  video_coding_common.h                                                 */
+/*  video_coding_av1_decode.h                                             */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -31,30 +31,72 @@
 #pragma once
 
 #include "video_coding_av1.h"
-#include "video_coding_av1_decode.h"
-#include "video_coding_h264.h"
-#include "video_coding_h264_decode.h"
 
-#include <cstdint>
+struct VideoDecodeAV1Frame {
+	VideoCodingAV1FrameType frame_type;
 
-enum VideoCodingOperation {
-	VIDEO_OPERATION_DECODE_H264 = (1 << 0),
-	VIDEO_OPERATION_DECODE_AV1 = (1 << 2),
-};
+	uint32_t current_frame_id;
 
-enum VideoCodingChromaSubsampling {
-	VIDEO_CODING_CHROMA_SUBSAMPLING_MONOCHROME = (1 << 0),
-	VIDEO_CODING_CHROMA_SUBSAMPLING_420 = (1 << 1),
-	VIDEO_CODING_CHROMA_SUBSAMPLING_422 = (1 << 2),
-	VIDEO_CODING_CHROMA_SUBSAMPLING_444 = (1 << 3),
-};
+	bool error_resilient_mode_flag;
+	bool disable_cdf_update_flag;
 
-struct VideoProfile {
-	VideoCodingOperation operation;
-	VideoCodingChromaSubsampling chroma_subsampling = VIDEO_CODING_CHROMA_SUBSAMPLING_420;
-	uint32_t luma_bit_depth = 8;
-	uint32_t chroma_bit_depth = 8;
+	bool allow_screen_content_tools_flag;
+	bool force_integer_mv_flag;
 
-	VideoCodingH264ProfileIdc h264_profile_idc;
-	VideoCodingH264PictureLayout h264_picture_layout;
+	bool frame_size_override_flag;
+
+	uint8_t order_hint;
+
+	uint8_t primary_ref_frame;
+
+	bool buffer_removal_time_flag;
+
+	bool allow_high_precision_mv;
+	bool use_ref_frame_mvs;
+	bool allow_intrabc;
+
+	uint8_t refresh_frame_flags;
+
+	uint8_t ref_order_hint[VIDEO_CODING_AV1_NUM_REF_FRAMES];
+
+	bool use_superres;
+	uint8_t coded_denom;
+	bool render_and_frame_size_different;
+
+	bool frame_refs_short_signaling;
+
+	uint8_t expected_frame_id[VIDEO_CODING_AV1_NUM_REF_FRAMES];
+
+	bool is_filter_switchable;
+	VideoCodingAV1InterpolationFilter interpolation_filter;
+
+	bool is_motion_mode_switchable;
+
+	uint8_t order_hints[VIDEO_CODING_AV1_NUM_REF_FRAMES];
+
+	bool disable_frame_end_update_cdf;
+
+	VideoCodingAV1TileInfo tile_info;
+	VideoCodingAV1Quantization quantization;
+
+	bool segmentation_enabled;
+	bool segmentation_update_map;
+	bool segmentation_temporal_update;
+	bool segmentation_update_data;
+	VideoCodingAV1Segmentation segmentation;
+
+	uint8_t delta_q_res;
+	uint8_t delta_lf_res;
+
+	VideoCodingAV1LoopFilter loop_filter;
+	VideoCodingAV1CDEF cdef;
+	VideoCodingAV1LoopRestoration loop_restoration;
+
+	VideoCodingAV1TxMode tx_mode;
+
+	bool allow_warped_motion;
+	bool reduced_tx_set;
+
+	VideoCodingAV1GlobalMotion global_motion;
+	VideoCodingAV1FilmGrain film_grain;
 };
