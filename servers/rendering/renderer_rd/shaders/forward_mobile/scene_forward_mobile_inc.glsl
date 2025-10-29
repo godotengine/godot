dd6625ec4d1cdc189147166b72dea3e6aa64757c
Hugo Locurcio <hugo.locurcio@hugo.pro>
2025-10-29 20:41:55 +0100

    Fix black reflections when using a plain color as reflection source

--- servers/rendering/renderer_rd/shaders/forward_mobile/scene_forward_mobile_inc.glsl
+++ servers/rendering/renderer_rd/shaders/forward_mobile/scene_forward_mobile_inc.glsl
@@ -141,24 +141,28 @@ bool sc_scene_use_ambient_cubemap() {
 	return ((sc_packed_0() >> 16) & 1U) != 0;
 }
 
-bool sc_scene_use_reflection_cubemap() {
+bool sc_scene_use_reflection_color() {
 	return ((sc_packed_0() >> 17) & 1U) != 0;
 }
 
-bool sc_scene_roughness_limiter_enabled() {
+bool sc_scene_use_reflection_cubemap() {
 	return ((sc_packed_0() >> 18) & 1U) != 0;
 }
 
-bool sc_material_feedback() {
+bool sc_scene_roughness_limiter_enabled() {
 	return ((sc_packed_0() >> 19) & 1U) != 0;
 }
 
+bool sc_material_feedback() {
+	return ((sc_packed_0() >> 20) & 1U) != 0;
+}
+
 uint sc_soft_shadow_samples() {
-	return (sc_packed_0() >> 20) & 63U;
+	return (sc_packed_0() >> 24) & 63U;
 }
 
 uint sc_penumbra_shadow_samples() {
-	return (sc_packed_0() >> 26) & 63U;
+	return (sc_packed_0() >> 30) & 63U;
 }
 
 uint sc_directional_soft_shadow_samples() {
