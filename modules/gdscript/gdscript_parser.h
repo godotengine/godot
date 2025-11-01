cf969e1bcf75e4e517e7b88b8a4343159d196757
Danil Alexeev <dalexeev12@yandex.ru>
2025-11-01 16:08:07 +0300

    GDScript: Add `exclude_addons_exceptions` project setting

--- modules/gdscript/gdscript_parser.h
+++ modules/gdscript/gdscript_parser.h
@@ -1356,7 +1356,10 @@ private:
 		Vector<String> symbols;
 	};
 
-	bool is_ignoring_warnings = false;
+	static bool is_ignoring_warnings;
+	static bool is_excluding_addons;
+	static Vector<String> exclude_addons_exceptions;
+
 	List<GDScriptWarning> warnings;
 	List<PendingWarning> pending_warnings;
 	HashSet<int> warning_ignored_lines[GDScriptWarning::WARNING_MAX];
@@ -1615,7 +1618,9 @@ public:
 		// TODO: Keep track of deps.
 		return List<String>();
 	}
+
 #ifdef DEBUG_ENABLED
+	static void update_project_settings();
 	const List<GDScriptWarning> &get_warnings() const { return warnings; }
 	const HashSet<int> &get_unsafe_lines() const { return unsafe_lines; }
 	int get_last_line_number() const { return current.end_line; }
