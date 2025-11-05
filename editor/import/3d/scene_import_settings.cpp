0e9fdec2457096421f2a116cd21d91c4eb709081
Michael Alexsander <michaelalexsander@protonmail.com>
2025-11-05 16:27:20 -0300

    Add extra panels to some areas of the editor

--- editor/import/3d/scene_import_settings.cpp
+++ editor/import/3d/scene_import_settings.cpp
@@ -1929,6 +1929,7 @@ SceneImportSettingsDialog::SceneImportSettingsDialog() {
 	// Display the same tooltips as in the Import dock.
 	inspector->set_object_class(ResourceImporterScene::get_class_static());
 	inspector->set_use_doc_hints(true);
+	inspector->set_theme_type_variation(SNAME("EditorInspectorForeground"));
 
 	property_split->add_child(inspector);
 
