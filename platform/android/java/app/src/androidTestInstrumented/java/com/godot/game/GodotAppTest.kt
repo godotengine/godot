43e0520a7a4800c7629c4589e13c6b2b7f2b59f8
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-11-06 11:30:07 -0800

    Make use of activity-alias as the launcher mechanism for the Godot editor and the Godot app template

--- platform/android/java/app/src/androidTestInstrumented/java/com/godot/game/GodotAppTest.kt
+++ platform/android/java/app/src/androidTestInstrumented/java/com/godot/game/GodotAppTest.kt
@@ -60,11 +60,6 @@ class GodotAppTest {
 		private val TEST_COMMAND_LINE_PARAMS = arrayOf("This is a test")
 	}
 
-	private fun getTestPlugin(): GodotAppInstrumentedTestPlugin? {
-		return GodotPluginRegistry.getPluginRegistry()
-			.getPlugin("GodotAppInstrumentedTestPlugin") as GodotAppInstrumentedTestPlugin?
-	}
-
 	/**
 	 * Runs the JavaClassWrapper tests via the GodotAppInstrumentedTestPlugin.
 	 */
@@ -72,7 +67,8 @@ class GodotAppTest {
 	fun runJavaClassWrapperTests() {
 		ActivityScenario.launch(GodotApp::class.java).use { scenario ->
 			scenario.onActivity { activity ->
-				val testPlugin = getTestPlugin()
+				val testPlugin = GodotPluginRegistry.getPluginRegistry()
+					.getPlugin("GodotAppInstrumentedTestPlugin") as GodotAppInstrumentedTestPlugin?
 				assertNotNull(testPlugin)
 
 				Log.d(TAG, "Waiting for the Godot main loop to start...")
@@ -84,28 +80,6 @@ class GodotAppTest {
 				result.exceptionOrNull()?.let { throw it }
 				assertTrue(result.isSuccess)
 				Log.d(TAG, "Passed ${result.getOrNull()} tests")
-			}
-		}
-	}
-
-	/**
-	 * Runs file access related tests.
-	 */
-	@Test
-	fun runFileAccessTests() {
-		ActivityScenario.launch(GodotApp::class.java).use { scenario ->
-			scenario.onActivity { activity ->
-				val testPlugin = getTestPlugin()
-				assertNotNull(testPlugin)
-
-				Log.d(TAG, "Waiting for the Godot main loop to start...")
-				testPlugin.waitForGodotMainLoopStarted()
-
-				Log.d(TAG, "Running FileAccess tests...")
-				val result = testPlugin.runFileAccessTests()
-				assertNotNull(result)
-				result.exceptionOrNull()?.let { throw it }
-				assertTrue(result.isSuccess)
 			}
 		}
 	}
