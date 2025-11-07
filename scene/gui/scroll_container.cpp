fc2c827c209cda1c4b949ac2aa6534adb0cabdd3
Michael Alexsander <michaelalexsander@protonmail.com>
2025-11-06 23:01:16 -0300

    Add scroll hints to `ScrollContainer` and `Tree`

--- scene/gui/scroll_container.cpp
+++ scene/gui/scroll_container.cpp
@@ -43,11 +43,15 @@ Size2 ScrollContainer::get_minimum_size() const {
 
 	for (int i = 0; i < get_child_count(); i++) {
 		Control *c = as_sortable_control(get_child(i), SortableVisibilityMode::VISIBLE);
-		if (!c || c == h_scroll || c == v_scroll || c == focus_panel || c == scroll_hint_top_left || c == scroll_hint_bottom_right) {
+		if (!c) {
+			continue;
+		}
+		if (c == h_scroll || c == v_scroll || c == focus_panel) {
 			continue;
 		}
 
 		Size2 child_min_size = c->get_combined_minimum_size();
+
 		largest_child_min_size = largest_child_min_size.max(child_min_size);
 	}
 
@@ -355,30 +359,27 @@ void ScrollContainer::_reposition_children() {
 	}
 
 	if (reserve_vscroll) {
-		int width = v_scroll->get_minimum_size().x + theme_cache.scrollbar_h_separation;
-		size.x -= width;
-		if (rtl) {
-			ofs.x += width;
-		}
+		size.x -= v_scroll->get_minimum_size().x + theme_cache.scrollbar_h_separation;
 	}
 
 	for (int i = 0; i < get_child_count(); i++) {
 		Control *c = as_sortable_control(get_child(i));
-		if (!c || c == h_scroll || c == v_scroll || c == focus_panel || c == scroll_hint_top_left || c == scroll_hint_bottom_right) {
+		if (!c || c->is_internal()) {
 			continue;
 		}
-
 		Size2 minsize = c->get_combined_minimum_size();
-		Rect2 r = Rect2(-Size2(get_h_scroll(), get_v_scroll()), minsize);
 
+		Rect2 r = Rect2(-Size2(get_h_scroll(), get_v_scroll()), minsize);
 		if (c->get_h_size_flags().has_flag(SIZE_EXPAND)) {
 			r.size.width = MAX(size.width, minsize.width);
 		}
 		if (c->get_v_size_flags().has_flag(SIZE_EXPAND)) {
 			r.size.height = MAX(size.height, minsize.height);
 		}
-
 		r.position += ofs;
+		if (rtl && reserve_vscroll) {
+			r.position.x += v_scroll->get_minimum_size().x;
+		}
 		r.position = r.position.floor();
 		fit_child_in_rect(c, r);
 	}
@@ -606,7 +607,7 @@ void ScrollContainer::_update_scrollbars() {
 void ScrollContainer::_update_scroll_hints() {
 	Size2 size = get_size();
 	Rect2 margins = _get_margins();
-	Size2 scroll_size = size - margins.position - margins.size;
+	Size2 scroll_size = size - margins.position + margins.size;
 
 	float v_scroll_value = v_scroll->get_value();
 	bool v_scroll_below_max = v_scroll_value < (largest_child_min_size.height - scroll_size.height - 1);
@@ -769,7 +770,7 @@ PackedStringArray ScrollContainer::get_configuration_warnings() const {
 
 	for (int i = 0; i < get_child_count(); i++) {
 		Control *c = as_sortable_control(get_child(i), SortableVisibilityMode::VISIBLE);
-		if (!c || c == h_scroll || c == v_scroll || c == focus_panel || c == scroll_hint_top_left || c == scroll_hint_bottom_right) {
+		if (!c || c->is_internal()) {
 			continue;
 		}
 
