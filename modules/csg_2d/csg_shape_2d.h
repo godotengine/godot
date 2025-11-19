xianofantasia/Go

Add CSG2D for boolean operations
Adds nodes and functions for 2D CSG boolean operations.

modules/csg_2d/csg_shape_2d.h
@@ -1,0 +1,216 @@
+/**************************************************************************/
+/*  csg_shape_2d.h                                                        */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#ifndef CSG_SHAPE_2D_H
+#define CSG_SHAPE_2D_H
+
+#include "csg_2d.h"
+
+#include "scene/2d/node_2d.h"
+#include "scene/resources/2d/concave_polygon_shape_2d.h"
+#include "scene/resources/2d/convex_polygon_shape_2d.h"
+#include "scene/resources/2d/navigation_polygon.h"
+
+class TPPLPoly;
+
+class CSGShape2D : public Node2D {
+	GDCLASS(CSGShape2D, Node2D);
+
+public:
+	enum Operation {
+		OPERATION_UNION,
+		OPERATION_INTERSECTION,
+		OPERATION_SUBTRACTION,
+	};
+
+	enum CollisionShapeType {
+		COLLISION_SHAPE_TYPE_CONCAVE_SEGMENTS,
+		COLLISION_SHAPE_TYPE_CONVEX_POLYGONS,
+		COLLISION_SHAPE_TYPE_MAX,
+	};
+
+private:
+	Operation operation = OPERATION_UNION;
+	CSGShape2D *parent_shape = nullptr;
+
+	CSGBrush2D *brush = nullptr;
+
+	Rect2 node_rect;
+
+	bool dirty = false;
+	bool last_visible = false;
+
+	bool shape_update_forced = false;
+
+	bool use_vertex_color = false;
+	Color vertex_color = Color(1.0, 1.0, 1.0, 1.0);
+
+	bool use_collision = false;
+	CollisionShapeType collision_shape_type = CollisionShapeType::COLLISION_SHAPE_TYPE_CONCAVE_SEGMENTS;
+	uint32_t collision_layer = 1;
+	uint32_t collision_mask = 1;
+	real_t collision_priority = 1.0;
+	LocalVector<Ref<ConvexPolygonShape2D>> root_collision_shapes_convex;
+	LocalVector<Ref<ConcavePolygonShape2D>> root_collision_shapes_concave;
+	RID root_collision_instance;
+	RID root_collision_debug_instance;
+	Transform2D debug_shape_old_transform;
+
+	Ref<ArrayMesh> root_mesh;
+	Ref<ArrayMesh> root_edge_line_mesh;
+	Ref<ArrayMesh> root_outline_mesh;
+
+protected:
+	Ref<ArrayMesh> brush_mesh;
+	LocalVector<Vector<Vector2>> brush_outlines;
+
+private:
+	Vector<Vector2> mesh_vertices;
+	Vector<int> mesh_triangles;
+	LocalVector<Vector<int>> mesh_convex_polygons;
+	LocalVector<Vector<Vector2>> mesh_outlines;
+	HashMap<Vector2, int> points_map;
+
+	void _update_shape();
+	void _update_collision_shapes();
+	bool _is_debug_collision_shape_visible();
+	void _update_debug_collision_shape();
+	void _clear_debug_collision_shape();
+	void _on_transform_changed();
+
+	void draw_shape();
+
+	void _create_collision();
+	void _free_collision();
+
+	bool debug_show_brush = false;
+
+protected:
+	void _notification(int p_what);
+	virtual CSGBrush2D *_build_brush() = 0;
+	void _make_dirty(bool p_parent_removing = false);
+
+	static void _bind_methods();
+
+	friend class CSGCombiner2D;
+	CSGBrush2D *_get_brush();
+
+	void _validate_property(PropertyInfo &p_property) const;
+
+public:
+	void set_debug_show_brush(bool p_enable);
+	bool get_debug_show_brush() const;
+
+	Vector<Vector2> get_mesh_vertices() { return mesh_vertices; }
+	Vector<int> get_mesh_triangles() { return mesh_triangles; }
+	const LocalVector<Vector<int>> &get_mesh_convex_polygons() { return mesh_convex_polygons; }
+	const LocalVector<Vector<Vector2>> &get_mesh_outlines() { return mesh_outlines; }
+
+	void set_operation(Operation p_operation);
+	Operation get_operation() const;
+
+	virtual Rect2 get_rect() const;
+
+	void set_use_vertex_color(bool p_enable);
+	bool is_using_vertex_color() const;
+
+	void set_vertex_color(const Color &p_color);
+	Color get_vertex_color() const;
+
+	void set_use_collision(bool p_enable);
+	bool is_using_collision() const;
+
+	void set_collision_shape_type(CollisionShapeType p_type);
+	CollisionShapeType get_collision_shape_type() const;
+
+	void set_collision_layer(uint32_t p_layer);
+	uint32_t get_collision_layer() const;
+
+	void set_collision_mask(uint32_t p_mask);
+	uint32_t get_collision_mask() const;
+
+	void set_collision_layer_value(int p_layer_number, bool p_value);
+	bool get_collision_layer_value(int p_layer_number) const;
+
+	void set_collision_mask_value(int p_layer_number, bool p_value);
+	bool get_collision_mask_value(int p_layer_number) const;
+
+	void set_collision_priority(real_t p_priority);
+	real_t get_collision_priority() const;
+
+	bool is_root_shape() const;
+	void force_shape_update();
+
+	Ref<ArrayMesh> bake_static_mesh() const;
+	Array bake_collision_shapes() const;
+	Array bake_light_occluders() const;
+	Ref<NavigationPolygon> bake_navigation_mesh() const;
+
+	bool is_point_in_outlines(const Point2 &p_point) const;
+
+	static void _recursive_process_polytree_items(List<TPPLPoly> &p_tppl_in_polygon, const Clipper2Lib::PolyPathD *p_polypath_item, LocalVector<Vector<Vector2>> &p_mesh_outlines);
+
+	CSGShape2D();
+	~CSGShape2D();
+};
+
+VARIANT_ENUM_CAST(CSGShape2D::Operation)
+VARIANT_ENUM_CAST(CSGShape2D::CollisionShapeType);
+
+class CSGCombiner2D : public CSGShape2D {
+	GDCLASS(CSGCombiner2D, CSGShape2D);
+
+	mutable Rect2 item_rect;
+	mutable bool rect_cache_dirty = true;
+
+private:
+	virtual CSGBrush2D *_build_brush() override;
+
+public:
+#ifdef DEBUG_ENABLED
+	virtual Rect2 _edit_get_rect() const override;
+	virtual bool _edit_use_rect() const override;
+	virtual bool _edit_is_selected_on_click(const Point2 &p_point, double p_tolerance) const override;
+#endif // DEBUG_ENABLED
+
+	CSGCombiner2D() {}
+};
+
+class CSGPrimitive2D : public CSGShape2D {
+	GDCLASS(CSGPrimitive2D, CSGShape2D);
+
+protected:
+	static void _bind_methods();
+
+public:
+	CSGPrimitive2D() {}
+};
+
+#endif // CSG_SHAPE_2D_H

modules/csg_2d/editor/csg_editor_plugin_2d.h
@@ -1,0 +1,160 @@
+/**************************************************************************/
+/*  csg_editor_plugin_2d.h                                                */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#ifndef CSG_EDITOR_PLUGIN_2D_H
+#define CSG_EDITOR_PLUGIN_2D_H
+
+#ifdef TOOLS_ENABLED
+
+#include "../csg_shape_2d.h"
+
+#include "editor/plugins/abstract_polygon_2d_editor.h"
+#include "editor/plugins/editor_plugin.h"
+#include "scene/gui/control.h"
+
+class AcceptDialog;
+class CanvasItemEditor;
+class CSGPolygon2D;
+class MenuButton;
+
+class CSGPolygon2DEditor : public AbstractPolygon2DEditor {
+	GDCLASS(CSGPolygon2DEditor, AbstractPolygon2DEditor);
+
+	CSGPolygon2D *node = nullptr;
+
+protected:
+	virtual Node2D *_get_node() const override;
+	virtual void _set_node(Node *p_polygon) override;
+
+	virtual Variant _get_polygon(int p_idx) const override;
+	virtual void _set_polygon(int p_idx, const Variant &p_polygon) const override;
+
+	virtual void _action_add_polygon(const Variant &p_polygon) override;
+	virtual void _action_remove_polygon(int p_idx) override;
+	virtual void _action_set_polygon(int p_idx, const Variant &p_previous, const Variant &p_polygon) override;
+
+public:
+	CSGPolygon2DEditor();
+};
+
+class CSGShape2DEditor : public Control {
+	GDCLASS(CSGShape2DEditor, Control);
+
+	enum CSGShapeType {
+		NONE,
+		CAPSULE_SHAPE,
+		CIRCLE_SHAPE,
+		MESH_SHAPE,
+		POLYGON_SHAPE,
+		RECTANGLE_SHAPE,
+	};
+
+	enum Menu {
+		MENU_OPTION_BAKE_MESH_INSTANCE,
+		MENU_OPTION_BAKE_COLLISION_SHAPE,
+		MENU_OPTION_BAKE_POLYGON_2D,
+		MENU_OPTION_BAKE_LIGHT_OCCLUDER_2D,
+		MENU_OPTION_BAKE_NAVIGATION_REGION_2D,
+	};
+
+	const Point2 RECT_HANDLES[8] = {
+		Point2(1, 0),
+		Point2(1, 1),
+		Point2(0, 1),
+		Point2(-1, 1),
+		Point2(-1, 0),
+		Point2(-1, -1),
+		Point2(0, -1),
+		Point2(1, -1),
+	};
+
+	CSGPolygon2DEditor *polygon_editor = nullptr;
+	CanvasItemEditor *canvas_item_editor = nullptr;
+	CSGShape2D *node = nullptr;
+	HBoxContainer *toolbar = nullptr;
+	MenuButton *options = nullptr;
+	AcceptDialog *err_dialog = nullptr;
+
+	LocalVector<Point2> handles;
+
+	CSGShapeType shape_type = CSGShapeType::NONE;
+	int edit_handle = -1;
+	bool pressed = false;
+	real_t grab_threshold = 8;
+	Variant original;
+	Transform2D original_transform;
+	Vector2 original_point;
+	Point2 last_point;
+	Vector2 original_mouse_pos;
+
+	Variant get_handle_value(int idx) const;
+	void set_handle(int idx, Point2 &p_point);
+	void commit_handle(int idx, Variant &p_org);
+
+	void _menu_option(int p_option);
+
+	void _create_baked_mesh_instance();
+	void _create_baked_collision_shape();
+	void _create_baked_polygon_2d();
+	void _create_baked_light_occluder_2d();
+	void _create_baked_navigation_region_2d();
+
+protected:
+	void _node_removed(Node *p_node);
+	void _notification(int p_what);
+
+public:
+	bool forward_canvas_gui_input(const Ref<InputEvent> &p_event);
+	void forward_canvas_draw_over_viewport(Control *p_overlay);
+	void edit(CSGShape2D *p_csg_shape);
+	CSGShape2DEditor();
+};
+
+class EditorPluginCSG2D : public EditorPlugin {
+	GDCLASS(EditorPluginCSG2D, EditorPlugin);
+
+	CSGShape2DEditor *csg_shape_editor = nullptr;
+
+public:
+	virtual bool forward_canvas_gui_input(const Ref<InputEvent> &p_event) override { return csg_shape_editor->forward_canvas_gui_input(p_event); }
+	virtual void forward_canvas_draw_over_viewport(Control *p_overlay) override { csg_shape_editor->forward_canvas_draw_over_viewport(p_overlay); }
+
+	virtual String get_plugin_name() const override { return "CSGShape2D"; }
+	bool has_main_screen() const override { return false; }
+	virtual void edit(Object *p_object) override;
+	virtual bool handles(Object *p_object) const override;
+	virtual void make_visible(bool p_visible) override;
+
+	EditorPluginCSG2D();
+};
+
+#endif // TOOLS_ENABLED
+
+#endif // CSG_EDITOR_PLUGIN_2D_H

godotengine/godot

modules/csg_2d/csg_shape_2d.h
@@ -1,0 +1,207 @@
+/**************************************************************************/
+/*  csg_shape_2d.h                                                        */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#include "csg_2d.h"
+
+#include "scene/2d/node_2d.h"
+
+class ConcavePolygonShape2D;
+class ConvexPolygonShape2D;
+class NavigationPolygon;
+
+class TPPLPoly;
+
+class CSGShape2D : public Node2D {
+	GDCLASS(CSGShape2D, Node2D);
+
+public:
+	enum Operation {
+		OPERATION_UNION,
+		OPERATION_INTERSECTION,
+		OPERATION_SUBTRACTION,
+	};
+
+	enum CollisionShapeType {
+		COLLISION_SHAPE_TYPE_CONCAVE_SEGMENTS,
+		COLLISION_SHAPE_TYPE_CONVEX_POLYGONS,
+		COLLISION_SHAPE_TYPE_MAX,
+	};
+
+private:
+	Operation operation = OPERATION_UNION;
+	CSGShape2D *parent_shape = nullptr;
+
+	CSGBrush2D *brush = nullptr;
+
+	Rect2 node_rect;
+
+	bool dirty = false;
+	bool last_visible = false;
+
+	bool use_vertex_color = false;
+	Color vertex_color = Color(1.0, 1.0, 1.0, 1.0);
+
+	bool use_collision = false;
+	CollisionShapeType collision_shape_type = CollisionShapeType::COLLISION_SHAPE_TYPE_CONCAVE_SEGMENTS;
+	uint32_t collision_layer = 1;
+	uint32_t collision_mask = 1;
+	real_t collision_priority = 1.0;
+	LocalVector<Ref<ConvexPolygonShape2D>> root_collision_shapes_convex;
+	LocalVector<Ref<ConcavePolygonShape2D>> root_collision_shapes_concave;
+	RID root_collision_instance;
+	RID root_collision_debug_instance;
+	Transform2D debug_shape_old_transform;
+
+	Ref<ArrayMesh> root_mesh;
+	Ref<ArrayMesh> root_edge_line_mesh;
+	Ref<ArrayMesh> root_outline_mesh;
+
+protected:
+	Ref<ArrayMesh> brush_mesh;
+	LocalVector<Vector<Vector2>> brush_outlines;
+
+private:
+	Vector<Vector2> mesh_vertices;
+	Vector<int> mesh_triangles;
+	LocalVector<Vector<int>> mesh_convex_polygons;
+	LocalVector<Vector<Vector2>> mesh_outlines;
+	HashMap<Vector2, int> points_map;
+
+	void _update_shape();
+	void _update_collision_shapes();
+	bool _is_debug_collision_shape_visible();
+	void _update_debug_collision_shape();
+	void _clear_debug_collision_shape();
+	void _on_transform_changed();
+
+	void draw_shape();
+
+	void _create_collision();
+	void _free_collision();
+
+	bool debug_show_brush = false;
+
+protected:
+	void _notification(int p_what);
+	virtual CSGBrush2D *_build_brush() = 0;
+	void _make_dirty(bool p_parent_removing = false);
+
+	static void _bind_methods();
+
+	friend class CSGCombiner2D;
+	CSGBrush2D *_get_brush();
+
+	void _validate_property(PropertyInfo &p_property) const;
+
+public:
+	void set_debug_show_brush(bool p_enable);
+	bool get_debug_show_brush() const;
+
+	Vector<Vector2> get_mesh_vertices() { return mesh_vertices; }
+	Vector<int> get_mesh_triangles() { return mesh_triangles; }
+	const LocalVector<Vector<int>> &get_mesh_convex_polygons() { return mesh_convex_polygons; }
+	const LocalVector<Vector<Vector2>> &get_mesh_outlines() { return mesh_outlines; }
+
+	void set_operation(Operation p_operation);
+	Operation get_operation() const;
+
+	virtual Rect2 get_rect() const;
+
+	void set_use_vertex_color(bool p_enable);
+	bool is_using_vertex_color() const;
+
+	void set_vertex_color(const Color &p_color);
+	Color get_vertex_color() const;
+
+	void set_use_collision(bool p_enable);
+	bool is_using_collision() const;
+
+	void set_collision_shape_type(CollisionShapeType p_type);
+	CollisionShapeType get_collision_shape_type() const;
+
+	void set_collision_layer(uint32_t p_layer);
+	uint32_t get_collision_layer() const;
+
+	void set_collision_mask(uint32_t p_mask);
+	uint32_t get_collision_mask() const;
+
+	void set_collision_layer_value(int p_layer_number, bool p_value);
+	bool get_collision_layer_value(int p_layer_number) const;
+
+	void set_collision_mask_value(int p_layer_number, bool p_value);
+	bool get_collision_mask_value(int p_layer_number) const;
+
+	void set_collision_priority(real_t p_priority);
+	real_t get_collision_priority() const;
+
+	bool is_root_shape() const;
+	void force_shape_update();
+
+	Ref<ArrayMesh> bake_static_mesh() const;
+	Array bake_collision_shapes() const;
+	Array bake_light_occluders() const;
+	Ref<NavigationPolygon> bake_navigation_mesh() const;
+
+	bool is_point_in_outlines(const Point2 &p_point) const;
+
+	static void _recursive_process_polytree_items(List<TPPLPoly> &p_tppl_in_polygon, const Clipper2Lib::PolyPathD *p_polypath_item, LocalVector<Vector<Vector2>> &p_mesh_outlines);
+
+	CSGShape2D();
+	~CSGShape2D();
+};
+
+VARIANT_ENUM_CAST(CSGShape2D::Operation)
+VARIANT_ENUM_CAST(CSGShape2D::CollisionShapeType);
+
+class CSGCombiner2D : public CSGShape2D {
+	GDCLASS(CSGCombiner2D, CSGShape2D);
+
+private:
+	virtual CSGBrush2D *_build_brush() override;
+
+public:
+#ifdef TOOLS_ENABLED
+	virtual bool _edit_is_selected_on_click(const Point2 &p_point, double p_tolerance) const override;
+#endif // TOOLS_ENABLED
+
+	CSGCombiner2D() {}
+};
+
+class CSGPrimitive2D : public CSGShape2D {
+	GDCLASS(CSGPrimitive2D, CSGShape2D);
+
+protected:
+	static void _bind_methods();
+
+public:
+	CSGPrimitive2D() {}
+};

modules/csg_2d/editor/csg_editor_plugin_2d.h
@@ -1,0 +1,156 @@
+/**************************************************************************/
+/*  csg_editor_plugin_2d.h                                                */
+/**************************************************************************/
+/*                         This file is part of:                          */
+/*                             GODOT ENGINE                               */
+/*                        https://godotengine.org                         */
+/**************************************************************************/
+/* Copyright (c) 2014-present Godot Engine contributors (see AUTHORS.md). */
+/* Copyright (c) 2007-2014 Juan Linietsky, Ariel Manzur.                  */
+/*                                                                        */
+/* Permission is hereby granted, free of charge, to any person obtaining  */
+/* a copy of this software and associated documentation files (the        */
+/* "Software"), to deal in the Software without restriction, including    */
+/* without limitation the rights to use, copy, modify, merge, publish,    */
+/* distribute, sublicense, and/or sell copies of the Software, and to     */
+/* permit persons to whom the Software is furnished to do so, subject to  */
+/* the following conditions:                                              */
+/*                                                                        */
+/* The above copyright notice and this permission notice shall be         */
+/* included in all copies or substantial portions of the Software.        */
+/*                                                                        */
+/* THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,        */
+/* EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF     */
+/* MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. */
+/* IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY   */
+/* CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT,   */
+/* TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE      */
+/* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
+/**************************************************************************/
+
+#pragma once
+
+#ifdef TOOLS_ENABLED
+
+#include "../csg_shape_2d.h"
+
+#include "editor/plugins/editor_plugin.h"
+#include "editor/scene/2d/abstract_polygon_2d_editor.h"
+
+class AcceptDialog;
+class CanvasItemEditor;
+class CSGPolygon2D;
+class MenuButton;
+
+class CSGPolygon2DEditor : public AbstractPolygon2DEditor {
+	GDCLASS(CSGPolygon2DEditor, AbstractPolygon2DEditor);
+
+	CSGPolygon2D *node = nullptr;
+
+protected:
+	virtual Node2D *_get_node() const override;
+	virtual void _set_node(Node *p_polygon) override;
+
+	virtual Variant _get_polygon(int p_idx) const override;
+	virtual void _set_polygon(int p_idx, const Variant &p_polygon) const override;
+
+	virtual void _action_add_polygon(const Variant &p_polygon) override;
+	virtual void _action_remove_polygon(int p_idx) override;
+	virtual void _action_set_polygon(int p_idx, const Variant &p_previous, const Variant &p_polygon) override;
+
+public:
+	CSGPolygon2DEditor();
+};
+
+class CSGShape2DEditor : public Control {
+	GDCLASS(CSGShape2DEditor, Control);
+
+	enum CSGShapeType {
+		NONE,
+		CAPSULE_SHAPE,
+		CIRCLE_SHAPE,
+		MESH_SHAPE,
+		POLYGON_SHAPE,
+		RECTANGLE_SHAPE,
+	};
+
+	enum Menu {
+		MENU_OPTION_BAKE_MESH_INSTANCE,
+		MENU_OPTION_BAKE_COLLISION_SHAPE,
+		MENU_OPTION_BAKE_POLYGON_2D,
+		MENU_OPTION_BAKE_LIGHT_OCCLUDER_2D,
+		MENU_OPTION_BAKE_NAVIGATION_REGION_2D,
+	};
+
+	const Point2 RECT_HANDLES[8] = {
+		Point2(1, 0),
+		Point2(1, 1),
+		Point2(0, 1),
+		Point2(-1, 1),
+		Point2(-1, 0),
+		Point2(-1, -1),
+		Point2(0, -1),
+		Point2(1, -1),
+	};
+
+	CSGPolygon2DEditor *polygon_editor = nullptr;
+	CanvasItemEditor *canvas_item_editor = nullptr;
+	CSGShape2D *node = nullptr;
+	HBoxContainer *toolbar = nullptr;
+	MenuButton *options = nullptr;
+	AcceptDialog *err_dialog = nullptr;
+
+	LocalVector<Point2> handles;
+
+	CSGShapeType shape_type = CSGShapeType::NONE;
+	int edit_handle = -1;
+	bool pressed = false;
+	real_t grab_threshold = 8;
+	Variant original;
+	Transform2D original_transform;
+	Vector2 original_point;
+	Point2 last_point;
+	Vector2 original_mouse_pos;
+
+	Variant get_handle_value(int p_idx) const;
+	void set_handle(int p_idx, const Point2 &p_point);
+	void commit_handle(int p_idx, const Variant &p_org);
+
+	void _menu_option(int p_option);
+
+	void _create_baked_mesh_instance();
+	void _create_baked_collision_shape();
+	void _create_baked_polygon_2d();
+	void _create_baked_light_occluder_2d();
+	void _create_baked_navigation_region_2d();
+
+protected:
+	void _node_removed(Node *p_node);
+	void _notification(int p_what);
+
+public:
+	bool forward_canvas_gui_input(const Ref<InputEvent> &p_event);
+	void forward_canvas_draw_over_viewport(Control *p_overlay);
+	void edit(CSGShape2D *p_csg_shape);
+	CSGShape2DEditor();
+};
+
+class EditorPluginCSG2D : public EditorPlugin {
+	GDCLASS(EditorPluginCSG2D, EditorPlugin);
+
+	CSGShape2DEditor *csg_shape_editor = nullptr;
+
+public:
+	virtual bool forward_canvas_gui_input(const Ref<InputEvent> &p_event) override { return csg_shape_editor->forward_canvas_gui_input(p_event); }
+	virtual void forward_canvas_draw_over_viewport(Control *p_overlay) override { csg_shape_editor->forward_canvas_draw_over_viewport(p_overlay); }
+
+	virtual String get_plugin_name() const override { return "CSGShape2D"; }
+	bool has_main_screen() const override { return false; }
+	virtual void edit(Object *p_object) override;
+	virtual bool handles(Object *p_object) const override;
+	virtual void make_visible(bool p_visible) override;
+
+	EditorPluginCSG2D();
+};
+
+#endif // TOOLS_ENABLED