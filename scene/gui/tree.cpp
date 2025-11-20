9e2599b7db7d1d83c1c168eb7ff487e07927d78d
TheArchCoder <ahmadchawla1432@gmail.com>
2025-11-21 01:29:58 +0500

    Hide TreeItem edit popup on scroll.

--- scene/gui/tree.cpp
+++ scene/gui/tree.cpp
@@ -5769,6 +5769,7 @@ int Tree::get_columns() const {
 void Tree::_scroll_moved(float) {
 	_determine_hovered_item();
 	queue_redraw();
+	popup_editor->hide();
 }
 
 Rect2 Tree::get_custom_popup_rect() const {
