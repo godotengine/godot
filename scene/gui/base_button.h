e05027d4f019f8a1474abd3fbc8e28de67d659de
Danil Alexeev <dalexeev12@yandex.ru>
2025-11-23 10:53:49 +0300

    GUI: Allow buttons to be pressed programmatically

--- scene/gui/base_button.h
+++ scene/gui/base_button.h
@@ -109,14 +109,14 @@ public:
 
 	virtual bool has_point(const Point2 &p_point) const override;
 
-	/* Signals */
-
 	bool is_pressed() const; ///< return whether button is pressed (toggled in)
 	bool is_pressing() const; ///< return whether button is pressed (toggled in)
 	bool is_hovered() const;
 
+	void press();
 	void set_pressed(bool p_pressed); // Only works in toggle mode.
 	void set_pressed_no_signal(bool p_pressed);
+
 	void set_toggle_mode(bool p_on);
 	bool is_toggle_mode() const;
 
