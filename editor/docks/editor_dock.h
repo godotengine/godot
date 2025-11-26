41165a2675cd8405b63d2f3724dd0838ef7a4d6b
kobewi <kobewi4e@gmail.com>
2025-11-26 16:43:49 +0100

    Replace DockSlot with DockTabContainer

--- editor/docks/editor_dock.h
+++ editor/docks/editor_dock.h
@@ -35,6 +35,7 @@
 #include "scene/gui/margin_container.h"
 
 class ConfigFile;
+class DockTabContainer;
 class Shortcut;
 class WindowWrapper;
 
@@ -125,6 +126,11 @@ public:
 
 	String get_display_title() const;
 	String get_effective_layout_key() const;
+
+	DockTabContainer *get_parent_container() const;
+	void set_tab_index(int p_index, bool p_set_current);
+	void update_tab_style();
+	Ref<Texture2D> get_effective_icon(const Callable &p_icon_fetch);
 
 	virtual void update_layout(DockLayout p_layout) { GDVIRTUAL_CALL(_update_layout, p_layout); }
 	virtual void save_layout_to_config(Ref<ConfigFile> &p_layout, const String &p_section) const { GDVIRTUAL_CALL(_save_layout_to_config, p_layout, p_section); }
