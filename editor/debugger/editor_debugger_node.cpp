f4a315b09d8409defc76c65d15548a4fcca39c52
kobewi <kobewi4e@gmail.com>
2025-11-28 20:42:47 +0100

    Assign layout key to Debugger

--- editor/debugger/editor_debugger_node.cpp
+++ editor/debugger/editor_debugger_node.cpp
@@ -63,6 +63,7 @@ EditorDebuggerNode *EditorDebuggerNode::singleton = nullptr;
 EditorDebuggerNode::EditorDebuggerNode() {
 	set_name(TTRC("Debugger"));
 	set_icon_name("Debug");
+	set_layout_key("Debugger");
 	set_dock_shortcut(ED_SHORTCUT_AND_COMMAND("bottom_panels/toggle_debugger_bottom_panel", TTRC("Toggle Debugger Dock"), KeyModifierMask::ALT | Key::D));
 	set_default_slot(DockConstants::DOCK_SLOT_BOTTOM);
 	set_available_layouts(EditorDock::DOCK_LAYOUT_HORIZONTAL);
