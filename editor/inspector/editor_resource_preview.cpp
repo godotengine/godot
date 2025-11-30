cfa8636f350b16efa7609504c4ec2aea3b82df19
daniel080400 <d90027121@gmail.com>
2025-12-01 01:12:41 +0800

    Rework scene preview thumbnails - take 2

--- editor/inspector/editor_resource_preview.cpp
+++ editor/inspector/editor_resource_preview.cpp
@@ -93,7 +93,9 @@ void EditorResourcePreviewGenerator::DrawRequester::request_and_wait(RID p_viewp
 	Callable request_vp_update_once = callable_mp(RS::get_singleton(), &RS::viewport_set_update_mode).bind(p_viewport, RS::VIEWPORT_UPDATE_ONCE);
 
 	if (EditorResourcePreview::get_singleton()->is_threaded()) {
-		RS::get_singleton()->connect(SNAME("frame_pre_draw"), request_vp_update_once, Object::CONNECT_ONE_SHOT);
+		if (!RS::get_singleton()->is_connected(SNAME("frame_pre_draw"), request_vp_update_once)) {
+			RS::get_singleton()->connect(SNAME("frame_pre_draw"), request_vp_update_once, Object::CONNECT_ONE_SHOT);
+		}
 		RS::get_singleton()->request_frame_drawn_callback(callable_mp(this, &EditorResourcePreviewGenerator::DrawRequester::_post_semaphore));
 
 		semaphore.wait();
@@ -123,8 +125,17 @@ Variant EditorResourcePreviewGenerator::DrawRequester::_post_semaphore() {
 	return Variant(); // Needed because of how the callback is used.
 }
 
+bool EditorResourcePreview::can_run_on_thread() const {
+	// Now forces all renderers to generate thumbnail on thread for better UX. (See issue ##107736)
+	//
+	// Previously this returns `RSG::rasterizer->can_create_resources_async()` to prevent
+	// threading on Compatibility renderer for some reason (Need rendering team to confirm),
+	// But after testing thumbnail creation on a thread using GLES3 it has no issues.
+	return true;
+}
+
 bool EditorResourcePreview::is_threaded() const {
-	return RSG::rasterizer->can_create_resources_async();
+	return thread.is_started();
 }
 
 void EditorResourcePreview::_thread_func(void *ud) {
@@ -280,11 +291,14 @@ void EditorResourcePreview::_iterate() {
 
 	QueueItem item = queue.front()->get();
 	queue.pop_front();
+	singleton->processing_item = item;
+	singleton->last_process_msec = OS::get_singleton()->get_ticks_msec();
 
 	if (cache.has(item.path)) {
 		Item cached_item = cache[item.path];
 		// Already has it because someone loaded it, just let it know it's ready.
 		_preview_ready(item.path, cached_item.last_hash, cached_item.preview, cached_item.small_preview, item.callback, cached_item.preview_metadata);
+		singleton->processing_item = QueueItem();
 		preview_mutex.unlock();
 		return;
 	}
@@ -300,6 +314,9 @@ void EditorResourcePreview::_iterate() {
 		Dictionary preview_metadata;
 		_generate_preview(texture, small_texture, item, String(), preview_metadata);
 		_preview_ready(item.path, item.resource->hash_edited_version_for_preview(), texture, small_texture, item.callback, preview_metadata);
+		preview_mutex.lock();
+		singleton->processing_item = QueueItem();
+		preview_mutex.unlock();
 		return;
 	}
 
@@ -338,7 +355,7 @@ void EditorResourcePreview::_iterate() {
 			cache_valid = false;
 			f.unref();
 		} else if (last_modtime != modtime) {
-			String last_md5 = f->get_line();
+			String last_md5 = hash;
 			String md5 = FileAccess::get_md5(item.path);
 			f.unref();
 
@@ -388,6 +405,9 @@ void EditorResourcePreview::_iterate() {
 		}
 	}
 	_preview_ready(item.path, 0, texture, small_texture, item.callback, preview_metadata);
+	preview_mutex.lock();
+	singleton->processing_item = QueueItem();
+	preview_mutex.unlock();
 }
 
 void EditorResourcePreview::_write_preview_cache(Ref<FileAccess> p_file, int p_thumbnail_size, bool p_has_small_texture, uint64_t p_modified_time, const String &p_hash, const Dictionary &p_metadata) {
@@ -424,11 +444,15 @@ void EditorResourcePreview::_idle_callback() {
 		return;
 	}
 
-	// Process preview tasks, trying to leave a little bit of responsiveness worst case.
-	uint64_t start = OS::get_singleton()->get_ticks_msec();
-	while (!singleton->queue.is_empty() && OS::get_singleton()->get_ticks_msec() - start < 100) {
-		singleton->_iterate();
+	// Ensure to process one item at a time
+	singleton->preview_mutex.lock();
+	bool processing = singleton->processing_item.path != "";
+	singleton->preview_mutex.unlock();
+	if (processing) {
+		return;
 	}
+
+	singleton->_iterate();
 }
 
 void EditorResourcePreview::_update_thumbnail_sizes() {
@@ -573,7 +597,7 @@ void EditorResourcePreview::start() {
 		return;
 	}
 
-	if (is_threaded()) {
+	if (can_run_on_thread()) {
 		ERR_FAIL_COND_MSG(thread.is_started(), "Thread already started.");
 		thread.start(_thread_func, this);
 	} else {
@@ -585,23 +609,21 @@ void EditorResourcePreview::start() {
 
 void EditorResourcePreview::stop() {
 	if (is_threaded()) {
-		if (thread.is_started()) {
-			exiting.set();
-			preview_sem.post();
+		exiting.set();
+		preview_sem.post();
 
-			for (int i = 0; i < preview_generators.size(); i++) {
-				preview_generators.write[i]->abort();
-			}
-
-			while (!exited.is_set()) {
-				// Sync pending work.
-				OS::get_singleton()->delay_usec(10000);
-				RenderingServer::get_singleton()->sync();
-				MessageQueue::get_singleton()->flush();
-			}
-
-			thread.wait_to_finish();
+		for (Ref<EditorResourcePreviewGenerator> preview_generator : preview_generators) {
+			preview_generator->abort();
 		}
+
+		while (!exited.is_set()) {
+			// Sync pending work.
+			OS::get_singleton()->delay_usec(10000);
+			RenderingServer::get_singleton()->sync();
+			MessageQueue::get_singleton()->flush();
+		}
+
+		thread.wait_to_finish();
 	}
 }
 
