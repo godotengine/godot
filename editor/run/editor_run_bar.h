4e29217e01f019dba356d49625478f036ae63037
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-12-01 00:25:30 -0800

    Update the shortcuts for 'run the current scene' and 'run specific scene' so they trigger the last selected XR mode option

--- editor/run/editor_run_bar.h
+++ editor/run/editor_run_bar.h
@@ -53,6 +53,12 @@ class EditorRunBar : public MarginContainer {
 		RUN_CUSTOM,
 	};
 
+	enum RunXRModeMenuItem {
+		INVALID = -1,
+		OFF = 0,
+		ON = 1,
+	};
+
 	PanelContainer *main_panel = nullptr;
 	HBoxContainer *main_hbox = nullptr;
 	HBoxContainer *outer_hbox = nullptr;
@@ -90,10 +96,10 @@ class EditorRunBar : public MarginContainer {
 
 	void _movie_maker_item_pressed(int p_id);
 	void _write_movie_toggled(bool p_enabled);
-	void _quick_run_selected(const String &p_file_path, int p_id = -1);
+	void _quick_run_selected(const String &p_file_path, int p_menu_item = RunXRModeMenuItem::INVALID);
 
-	void _play_current_pressed(int p_id = -1);
-	void _play_custom_pressed(int p_id = -1);
+	void _play_current_pressed(int p_menu_item = RunXRModeMenuItem::INVALID);
+	void _play_custom_pressed(int p_menu_item = RunXRModeMenuItem::INVALID);
 
 	void _run_scene(const String &p_scene_path = "", const Vector<String> &p_run_args = Vector<String>());
 	void _run_native(const Ref<EditorExportPreset> &p_preset);
@@ -101,7 +107,7 @@ class EditorRunBar : public MarginContainer {
 	void _profiler_autostart_indicator_pressed();
 
 private:
-	static Vector<String> _get_xr_mode_play_args(int p_xr_mode_id);
+	static Vector<String> _get_xr_mode_play_args(RunXRModeMenuItem p_menu_item);
 
 protected:
 	void _notification(int p_what);
