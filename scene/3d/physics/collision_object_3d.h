2fd0aa99f96a87c153a5e1940496536607dd52c5
David Snopek <dsnopek@gmail.com>
2025-12-03 04:13:09 -0600

    Synchronize the names for `RequiredParam<T>` arguments in header files

--- scene/3d/physics/collision_object_3d.h
+++ scene/3d/physics/collision_object_3d.h
@@ -157,7 +157,7 @@ public:
 	void shape_owner_set_disabled(uint32_t p_owner, bool p_disabled);
 	bool is_shape_owner_disabled(uint32_t p_owner) const;
 
-	void shape_owner_add_shape(uint32_t p_owner, RequiredParam<Shape3D> p_shape);
+	void shape_owner_add_shape(uint32_t p_owner, RequiredParam<Shape3D> rp_shape);
 	int shape_owner_get_shape_count(uint32_t p_owner) const;
 	Ref<Shape3D> shape_owner_get_shape(uint32_t p_owner, int p_shape) const;
 	int shape_owner_get_shape_index(uint32_t p_owner, int p_shape) const;
