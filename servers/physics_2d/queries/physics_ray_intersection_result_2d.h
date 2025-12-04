1730fe98bcab4e9cf0ef2658c0e0e35454d33010
Rizo Hashimi <rizohashimi+github@gmail.com>
2025-12-05 02:47:40 +0500

    Add PhysicsDirectSpaceState2D::intersect_ray_typed

--- servers/physics_2d/queries/physics_ray_intersection_result_2d.h
+++ servers/physics_2d/queries/physics_ray_intersection_result_2d.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  physics_direct_space_state_2d.h                                       */
+/*  physics_ray_intersection_result_2d.h                                  */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -30,35 +30,24 @@
 
 #pragma once
 
-#include "core/variant/type_info.h"
+#include "core/object/ref_counted.h"
 #include "servers/physics_2d/physics_server_2d_types.h"
-#include "servers/physics_2d/queries/physics_point_query_parameters_2d.h"
-#include "servers/physics_2d/queries/physics_ray_intersection_result_2d.h"
-#include "servers/physics_2d/queries/physics_ray_query_parameters_2d.h"
-#include "servers/physics_2d/queries/physics_shape_query_parameters_2d.h"
 
-class PhysicsDirectSpaceState2D : public Object {
-	GDCLASS(PhysicsDirectSpaceState2D, Object);
+class PhysicsRayIntersectionResult2D : public RefCounted {
+	GDCLASS(PhysicsRayIntersectionResult2D, RefCounted);
 
-	Dictionary _intersect_ray(RequiredParam<PhysicsRayQueryParameters2D> rp_ray_query);
-	TypedArray<Dictionary> _intersect_point(RequiredParam<PhysicsPointQueryParameters2D> rp_point_query, int p_max_results = 32);
-	TypedArray<Dictionary> _intersect_shape(RequiredParam<PhysicsShapeQueryParameters2D> rp_shape_query, int p_max_results = 32);
-	Vector<real_t> _cast_motion(RequiredParam<PhysicsShapeQueryParameters2D> rp_shape_query);
-	TypedArray<Vector2> _collide_shape(RequiredParam<PhysicsShapeQueryParameters2D> rp_shape_query, int p_max_results = 32);
-	Dictionary _get_rest_info(RequiredParam<PhysicsShapeQueryParameters2D> rp_shape_query);
+	friend class PhysicsDirectSpaceState2D;
 
-	bool _intersect_ray_typed(RequiredParam<PhysicsRayQueryParameters2D> rp_ray_query, RequiredParam<PhysicsRayIntersectionResult2D> rp_result);
+	PS2DT::RayResult result;
 
 protected:
 	static void _bind_methods();
 
 public:
-	virtual bool intersect_ray(const PS2DT::RayParameters &p_parameters, PS2DT::RayResult &r_result) = 0;
-	virtual int intersect_point(const PS2DT::PointParameters &p_parameters, PS2DT::ShapeResult *r_results, int p_result_max) = 0;
-	virtual int intersect_shape(const PS2DT::ShapeParameters &p_parameters, PS2DT::ShapeResult *r_results, int p_result_max) = 0;
-	virtual bool cast_motion(const PS2DT::ShapeParameters &p_parameters, real_t &p_closest_safe, real_t &p_closest_unsafe) = 0;
-	virtual bool collide_shape(const PS2DT::ShapeParameters &p_parameters, Vector2 *r_results, int p_result_max, int &r_result_count) = 0;
-	virtual bool rest_info(const PS2DT::ShapeParameters &p_parameters, PS2DT::ShapeRestInfo *r_info) = 0;
-
-	PhysicsDirectSpaceState2D();
+	Vector2 get_position() const;
+	Vector2 get_normal() const;
+	RID get_collider_rid() const;
+	ObjectID get_collider_id() const;
+	Object *get_collider() const;
+	int get_collider_shape() const;
 };
