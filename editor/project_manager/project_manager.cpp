547dc96d729b0f0fe3dca67990d22c9bf6e4766f
Jan Haller <bromeon@gmail.com>
2025-12-04 11:40:53 +0100

    Add special compat methods for EditorFileDialog enum fns
    Fixes broken binary compatibility in GDExtension, introduced
    by moving methods from EditorFileDialog to its base class
    FileDialog.
    
    While the general derived-to-base compat mechanism works,
    the function hashes are changed when enum types are also moved
    between classes.
    
    An earlier attempt restored original enums in EditorFileDialog
    and added regular compatibility methods, however this caused
    shadowed symbols in C# bindings. Instead, hash mappings are
    now directly hardcoded as "special compat hashes".

--- editor/project_manager/project_manager.cpp
+++ editor/project_manager/project_manager.cpp
@@ -1370,7 +1370,7 @@ ProjectManager::ProjectManager() {
 		FileDialog::set_get_thumbnail_callback(callable_mp_static(ProjectManager::_file_dialog_get_thumbnail));
 
 		FileDialog::set_default_show_hidden_files(EDITOR_GET("filesystem/file_dialog/show_hidden_files"));
-		FileDialog::set_default_display_mode((EditorFileDialog::DisplayMode)EDITOR_GET("filesystem/file_dialog/display_mode").operator int());
+		FileDialog::set_default_display_mode((FileDialog::DisplayMode)EDITOR_GET("filesystem/file_dialog/display_mode").operator int());
 
 		int swap_cancel_ok = EDITOR_GET("interface/editor/accept_dialog_cancel_ok_buttons");
 		if (swap_cancel_ok != 0) { // 0 is auto, set in register_scene based on DisplayServer.
