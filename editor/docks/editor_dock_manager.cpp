16a1ac67c23879bbfeea470166735d11e29d3e3b
kobewi <kobewi4e@gmail.com>
2025-12-06 19:12:46 +0100

    Fix dock window titles not being translated

--- editor/docks/editor_dock_manager.cpp
+++ editor/docks/editor_dock_manager.cpp
@@ -406,7 +406,7 @@ void EditorDockManager::_open_dock_in_window(EditorDock *p_dock, bool p_show_win
 	Point2 dock_screen_pos = p_dock->get_screen_position();
 
 	WindowWrapper *wrapper = memnew(WindowWrapper);
-	wrapper->set_window_title(vformat(TTR("%s - Godot Engine"), p_dock->get_display_title()));
+	wrapper->set_window_title(vformat(TTR("%s - Godot Engine"), TTR(p_dock->get_display_title())));
 	wrapper->set_margins_enabled(true);
 
 	EditorNode::get_singleton()->get_gui_base()->add_child(wrapper);
