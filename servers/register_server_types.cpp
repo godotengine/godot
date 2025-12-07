e9d8f4be10557a747cb04c4953cad7e5115fe927
Rizo Hashimi <rizohashimi+github@gmail.com>
2025-12-07 18:23:01 +0500

    Add PhysicsDirectSpaceState2D::cast_motion_typed

--- servers/register_server_types.cpp
+++ servers/register_server_types.cpp
@@ -316,6 +316,7 @@ void register_server_types() {
 	GDREGISTER_CLASS(PhysicsPointIntersectionResult2D);
 	GDREGISTER_CLASS(PhysicsShapeQueryParameters2D);
 	GDREGISTER_CLASS(PhysicsShapeIntersectionResult2D);
+	GDREGISTER_CLASS(PhysicsCastMotionResult2D);
 	GDREGISTER_CLASS(PhysicsTestMotionParameters2D);
 	GDREGISTER_CLASS(PhysicsTestMotionResult2D);
 
