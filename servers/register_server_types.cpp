bd7f0e55a2365f2d45f961d96be2d5044b0726c9
Rizo Hashimi <rizohashimi+github@gmail.com>
2025-12-08 16:17:29 +0500

    Refactor PhysicsDirectSpaceState2D::collide_shape to use typed result object

--- servers/register_server_types.cpp
+++ servers/register_server_types.cpp
@@ -297,6 +297,7 @@ void register_server_types() {
 	GDREGISTER_CLASS(PhysicsShapeQueryParameters2D);
 	GDREGISTER_CLASS(PhysicsShapeIntersectionResult2D);
 	GDREGISTER_CLASS(PhysicsShapeCastResult2D);
+	GDREGISTER_CLASS(PhysicsShapeCollisionResult2D);
 	GDREGISTER_CLASS(PhysicsTestMotionParameters2D);
 	GDREGISTER_CLASS(PhysicsTestMotionResult2D);
 
