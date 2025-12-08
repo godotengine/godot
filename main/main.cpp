xianofantasia/Go

Add colored header output
Header messages are now displayed in gray, with the rendering method
being colorized in the message.

ANSI escape codes are now stripped in the editor Output panel as well,
which allows scripts to use custom ANSI escape codes outside of `print_rich()`
while not affecting the Output panel's readability. This change was required
for the newly printed ANSI escape codes to avoid affecting the
editor Output panel.

Additionally, the dim/faint display mode is now used instead of a
"bright black" color for better compatibility across terminals
(some themes could be displaying black text on a black background otherwise,
or white text on a white background).

main/main.cpp
@@ -395,19 +395,19 @@ void finalize_theme_db() {
 #define MAIN_PRINT(m_txt)
 #endif
 
-void Main::print_header(bool p_rich) {
+void Main::print_header(bool p_help) {
 	if (VERSION_TIMESTAMP > 0) {
 		// Version timestamp available.
-		if (p_rich) {
+		if (p_help) {
 			Engine::get_singleton()->print_header_rich("\u001b[38;5;39m" + String(VERSION_NAME) + "\u001b[0m v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(VERSION_TIMESTAMP, true) + " UTC) - \u001b[4m" + String(VERSION_WEBSITE));
 		} else {
-			Engine::get_singleton()->print_header(String(VERSION_NAME) + " v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(VERSION_TIMESTAMP, true) + " UTC) - " + String(VERSION_WEBSITE));
+			Engine::get_singleton()->print_header_rich("\u001b[2m" + String(VERSION_NAME) + " v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(VERSION_TIMESTAMP, true) + " UTC) - \u001b[4m" + String(VERSION_WEBSITE));
 		}
 	} else {
-		if (p_rich) {
+		if (p_help) {
 			Engine::get_singleton()->print_header_rich("\u001b[38;5;39m" + String(VERSION_NAME) + "\u001b[0m v" + get_full_version_string() + " - \u001b[4m" + String(VERSION_WEBSITE));
 		} else {
-			Engine::get_singleton()->print_header(String(VERSION_NAME) + " v" + get_full_version_string() + " - " + String(VERSION_WEBSITE));
+			Engine::get_singleton()->print_header_rich("\u001b[2m" + String(VERSION_NAME) + " v" + get_full_version_string() + " - \u001b[4m" + String(VERSION_WEBSITE));
 		}
 	}
 }
@@ -417,7 +417,7 @@ void Main::print_header(bool p_rich) {
  * automatically added at the end.
  */
 void Main::print_help_copyright(const char *p_notice) {
-	OS::get_singleton()->print("\u001b[90m%s\u001b[0m\n", p_notice);
+	OS::get_singleton()->print("\u001b[2m%s\u001b[0m\n", p_notice);
 }
 
 /**
@@ -475,7 +475,7 @@ void Main::print_help_option(const char *p_option, const char *p_description, CL
 	} else {
 		// Make continuation lines for descriptions faint if the option name is empty.
 		OS::get_singleton()->print(
-				"  \u001b[92m%s   \u001b[0m  \u001b[90m%s",
+				"  \u001b[92m%s   \u001b[0m  \u001b[2m%s\u001b[0m",
 				format_help_option(p_option).utf8().ptr(),
 				p_description);
 	}

godotengine/godot

main/main.cpp
@@ -426,19 +426,19 @@ void finalize_theme_db() {
 #define MAIN_PRINT(m_txt)
 #endif
 
-void Main::print_header(bool p_rich) {
+void Main::print_header(bool p_help) {
 	if (GODOT_VERSION_TIMESTAMP > 0) {
 		// Version timestamp available.
-		if (p_rich) {
+		if (p_help) {
 			Engine::get_singleton()->print_header_rich("\u001b[38;5;39m" + String(GODOT_VERSION_NAME) + "\u001b[0m v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(GODOT_VERSION_TIMESTAMP, true) + " UTC) - \u001b[4m" + String(GODOT_VERSION_WEBSITE));
 		} else {
-			Engine::get_singleton()->print_header(String(GODOT_VERSION_NAME) + " v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(GODOT_VERSION_TIMESTAMP, true) + " UTC) - " + String(GODOT_VERSION_WEBSITE));
+			Engine::get_singleton()->print_header_rich("\u001b[2m" + String(GODOT_VERSION_NAME) + " v" + get_full_version_string() + " (" + Time::get_singleton()->get_datetime_string_from_unix_time(GODOT_VERSION_TIMESTAMP, true) + " UTC) - \u001b[4m" + String(GODOT_VERSION_WEBSITE));
 		}
 	} else {
-		if (p_rich) {
+		if (p_help) {
 			Engine::get_singleton()->print_header_rich("\u001b[38;5;39m" + String(GODOT_VERSION_NAME) + "\u001b[0m v" + get_full_version_string() + " - \u001b[4m" + String(GODOT_VERSION_WEBSITE));
 		} else {
-			Engine::get_singleton()->print_header(String(GODOT_VERSION_NAME) + " v" + get_full_version_string() + " - " + String(GODOT_VERSION_WEBSITE));
+			Engine::get_singleton()->print_header_rich("\u001b[2m" + String(GODOT_VERSION_NAME) + " v" + get_full_version_string() + " - \u001b[4m" + String(GODOT_VERSION_WEBSITE));
 		}
 	}
 }
@@ -448,7 +448,7 @@ void Main::print_header(bool p_rich) {
  * automatically added at the end.
  */
 void Main::print_help_copyright(const char *p_notice) {
-	OS::get_singleton()->print("\u001b[90m%s\u001b[0m\n", p_notice);
+	OS::get_singleton()->print("\u001b[2m%s\u001b[0m\n", p_notice);
 }
 
 /**
@@ -509,7 +509,7 @@ void Main::print_help_option(const char *p_option, const char *p_description, CL
 	} else {
 		// Make continuation lines for descriptions faint if the option name is empty.
 		OS::get_singleton()->print(
-				"  \u001b[92m%s   \u001b[0m  \u001b[90m%s",
+				"  \u001b[92m%s   \u001b[0m  \u001b[2m%s\u001b[0m",
 				format_help_option(p_option).utf8().ptr(),
 				p_description);
 	}
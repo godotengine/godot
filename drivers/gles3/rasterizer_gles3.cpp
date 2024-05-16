d765f8422df2c8cfd86f594cbf8ebd375595550e
Hugo Locurcio <hugo.locurcio@hugo.pro>
2024-05-16 20:49:07 +0200

    Add colored header output
    Header messages are now displayed in gray, with the rendering method
    being colorized in the message.
    
    ANSI escape codes are now stripped in the editor Output panel as well,
    which allows scripts to use custom ANSI escape codes outside of `print_rich()`
    while not affecting the Output panel's readability. This change was required
    for the newly printed ANSI escape codes to avoid affecting the
    editor Output panel.
    
    Additionally, the dim/faint display mode is now used instead of a
    "bright black" color for better compatibility across terminals
    (some themes could be displaying black text on a black background otherwise,
    or white text on a white background).

--- drivers/gles3/rasterizer_gles3.cpp
+++ drivers/gles3/rasterizer_gles3.cpp
@@ -206,7 +206,7 @@ typedef void(GLAPIENTRY *DEBUGPROCARB)(GLenum source,
 typedef void(GLAPIENTRY *DebugMessageCallbackARB)(DEBUGPROCARB callback, const void *userParam);
 
 void RasterizerGLES3::initialize() {
-	Engine::get_singleton()->print_header(vformat("OpenGL API %s - Compatibility - Using Device: %s - %s", RS::get_singleton()->get_video_adapter_api_version(), RS::get_singleton()->get_video_adapter_vendor(), RS::get_singleton()->get_video_adapter_name()));
+	Engine::get_singleton()->print_header_rich(vformat("\u001b[2mOpenGL API %s - \u001b[96mCompatibility\u001b[39;2m - Using Device: %s - %s", RS::get_singleton()->get_video_adapter_api_version(), RS::get_singleton()->get_video_adapter_vendor(), RS::get_singleton()->get_video_adapter_name()));
 }
 
 void RasterizerGLES3::finalize() {
