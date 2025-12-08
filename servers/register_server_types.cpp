f4461efe40798729b6885ceb0ca689e2d5a06f1c
Rizo Hashimi <rizohashimi+github@gmail.com>
2025-12-08 16:17:29 +0500

    Add PhysicsDirectSpaceState2D::collide_shape_typed

--- servers/register_server_types.cpp
+++ servers/register_server_types.cpp
@@ -317,6 +317,7 @@ void register_server_types() {
 	GDREGISTER_CLASS(PhysicsShapeQueryParameters2D);
 	GDREGISTER_CLASS(PhysicsShapeIntersectionResult2D);
 	GDREGISTER_CLASS(PhysicsCastMotionResult2D);
+	GDREGISTER_CLASS(PhysicsShapeCollisionResult2D);
 	GDREGISTER_CLASS(PhysicsTestMotionParameters2D);
 	GDREGISTER_CLASS(PhysicsTestMotionResult2D);
 
