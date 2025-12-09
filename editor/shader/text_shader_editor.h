85f824430ef078617830bd0228a9ed07eaedca7a
nikitalita <69168929+nikitalita@users.noreply.github.com>
2025-12-09 15:57:52 -0800

    Convert 3.x shaders

--- editor/shader/text_shader_editor.h
+++ editor/shader/text_shader_editor.h
@@ -123,6 +123,7 @@ class TextShaderEditor : public ShaderEditor {
 		EDIT_TOGGLE_WORD_WRAP,
 		EDIT_TOGGLE_COMMENT,
 		EDIT_COMPLETE,
+		EDIT_CONVERT,
 		SEARCH_FIND,
 		SEARCH_FIND_NEXT,
 		SEARCH_FIND_PREV,
@@ -148,6 +149,11 @@ class TextShaderEditor : public ShaderEditor {
 	ConfirmationDialog *disk_changed = nullptr;
 
 	ShaderTextEditor *code_editor = nullptr;
+#ifndef DISABLE_DEPRECATED
+	AcceptDialog *shader_convert_error_dialog = nullptr;
+	ConfirmationDialog *confirm_convert_shader = nullptr;
+#endif
+
 	bool compilation_success = true;
 
 	void _menu_option(int p_option);
@@ -155,6 +161,9 @@ class TextShaderEditor : public ShaderEditor {
 	mutable Ref<Shader> shader;
 	mutable Ref<ShaderInclude> shader_inc;
 
+#ifndef DISABLE_DEPRECATED
+	void _convert_shader();
+#endif
 	void _editor_settings_changed();
 	void _apply_editor_settings();
 	void _project_settings_changed();
