eeb891a778b4cb59d8d69e51d5d3f19a7675d670
celyk <humanimations10@gmail.com>
2025-12-16 16:38:43 -0600

    Put UV scale back the way it was

--- drivers/gles3/rasterizer_scene_gles3.cpp
+++ drivers/gles3/rasterizer_scene_gles3.cpp
@@ -3717,7 +3717,7 @@ void RasterizerSceneGLES3::_render_list_template(RenderListParameters *p_params,
 				} else {
 					material_storage->shaders.scene_shader.version_set_uniform(SceneShaderGLES3::COMPRESSED_AABB_POSITION, s->aabb.position, shader->version, instance_variant, spec_constants);
 					material_storage->shaders.scene_shader.version_set_uniform(SceneShaderGLES3::COMPRESSED_AABB_SIZE, -1.0f * s->aabb.size, shader->version, instance_variant, spec_constants);
-					material_storage->shaders.scene_shader.version_set_uniform(SceneShaderGLES3::UV_SCALE, s->uv_scale, shader->version, instance_variant, spec_constants);
+					material_storage->shaders.scene_shader.version_set_uniform(SceneShaderGLES3::UV_SCALE, Vector4(0.0, 0.0, 0.0, 0.0), shader->version, instance_variant, spec_constants);
 				}
 			}
 
