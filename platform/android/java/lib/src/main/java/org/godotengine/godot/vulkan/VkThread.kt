e77d70c01a1d499047167c011ccd614e3d1681bc
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-12-19 07:53:21 -0800

    Fix ANRs when shutting down the engine

--- platform/android/java/lib/src/main/java/org/godotengine/godot/vulkan/VkThread.kt
+++ platform/android/java/lib/src/main/java/org/godotengine/godot/vulkan/VkThread.kt
@@ -32,6 +32,7 @@
 package org.godotengine.godot.vulkan
 
 import android.util.Log
+import java.util.concurrent.TimeUnit
 import java.util.concurrent.locks.ReentrantLock
 import kotlin.concurrent.withLock
 
@@ -104,10 +105,34 @@ internal class VkThread(private val vkSurfaceView: VkSurfaceView, private val vk
 				try {
 					Log.i(TAG, "Waiting on exit for $name")
 					lockCondition.await()
-				} catch (ex: InterruptedException) {
+				} catch (_: InterruptedException) {
 					currentThread().interrupt()
 				}
 			}
+		}
+	}
+
+	/**
+	 * Request the thread to exit and block up to the given [timeInMs] until it's done.
+	 *
+	 * @return true if the thread exited, false otherwise.
+	 */
+	fun requestExitAndWait(timeInMs: Long): Boolean {
+		lock.withLock {
+			shouldExit = true
+			lockCondition.signalAll()
+
+			var remainingTimeInNanos = TimeUnit.MILLISECONDS.toNanos(timeInMs)
+			while (!exited && remainingTimeInNanos > 0) {
+				try {
+					Log.i(TAG, "Waiting on exit for $name for $remainingTimeInNanos")
+					remainingTimeInNanos = lockCondition.awaitNanos(remainingTimeInNanos)
+				} catch (_: InterruptedException) {
+					currentThread().interrupt()
+				}
+			}
+
+			return exited
 		}
 	}
 
