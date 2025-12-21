d86a9934c48952bd961e63822a7923b38d98ffbd
AR <ardev1.deverson@proton.me>
2025-12-21 21:14:42 +0500

    Have the output window keepup when printing every frame

--- editor/editor_log.h
+++ editor/editor_log.h
@@ -126,6 +126,9 @@ private:
 	int line_limit = 10000;
 
 	Vector<LogMessage> messages;
+	Vector<LogMessage> pending_messages;
+	bool flush_pending = false;
+
 	// Maps MessageTypes to LogFilters for convenient access and storage (don't need 1 member per filter).
 	HashMap<MessageType, LogFilter *> type_filter_map;
 
