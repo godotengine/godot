7670ddaf7eb2c1672b15be5a77ff37459f1ef0c9
Kaleb Reid <78945904+Kaleb-Reid@users.noreply.github.com>
2025-12-20 16:48:31 -0800

    Fix builds on FreeBSD, OpenBSD, and NetBSD

--- platform/linuxbsd/platform_config.h
+++ platform/linuxbsd/platform_config.h
@@ -30,18 +30,22 @@
 
 #pragma once
 
+// alloca
 #ifdef __linux__
 #include <alloca.h>
+#elif defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
+#ifdef __cplusplus
+#include <cstdlib>
+#else
+#include <stdio.h>
+#endif
 #endif
 
-#if defined(__FreeBSD__) || defined(__OpenBSD__) || defined(__NetBSD__)
-#include <cstdlib> // alloca
+#if defined(__FreeBSD__) || defined(__OpenBSD__)
 // FreeBSD and OpenBSD use pthread_set_name_np, while other platforms,
 // include NetBSD, use pthread_setname_np. NetBSD's version however requires
 // a different format, we handle this directly in thread_posix.
-#ifdef __NetBSD__
-#define PTHREAD_NETBSD_SET_NAME
-#else
 #define PTHREAD_BSD_SET_NAME
-#endif
+#elif defined(__NetBSD__)
+#define PTHREAD_NETBSD_SET_NAME
 #endif
--- platform/linuxbsd/x11/detect_prime_x11.cpp
+++ platform/linuxbsd/x11/detect_prime_x11.cpp
@@ -47,6 +47,11 @@
 #include <X11/Xutil.h>
 #endif
 
+#ifdef __OpenBSD__
+// quick_exit not available
+#define quick_exit _exit
+#endif
+
 #include <sys/types.h>
 #include <sys/wait.h>
 #include <unistd.h>
