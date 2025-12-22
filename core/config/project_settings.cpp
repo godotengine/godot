6cf2d4b83cc081302172417dafef8781163d8681
PiCode <122909224+PiCode9560@users.noreply.github.com>
2025-12-22 21:07:58 +0300

    Added global resource filesystem
    Changed prefix to "editor://"
    
    Fixed editor resource folder not created.
    
    Add support for editor:// paths in string utilities
    
    Extended String methods to handle 'editor://' paths similarly to 'res://' and 'user://'. Updated path_to and is_resource_file to recognize 'editor://'. Adjusted FileSystemDock navigation to only process 'res://' paths for directory checks.
    
    Make the editor:// prefix work in running project if launched from the editor.
    
    Allow adding editor:// to project settings directory rules, and added extra checks in editor_file_system.cpp
    
    Allowed autoload to load editor:// path.
    
    Added editor:// path to project settings directory rules by default.
    
    Fixes
    
    Allow opening editor:// script path by clicking output error.
    
    Fixed build failed.
    
    undo allowed loading editor:// directory at runtime.
    
    Undo "Allowed autoload to load editor:// path."
    
    Simplify code.
    
    Cleaned up code and doc.
    
    Changed path prefix to `edres://`.
    
    Undo "Changed path prefix to `edres://`".
    
    Changed editor addon path to editor://addons/
    
    Fix build fail. Add return error.
    
    Simplify ProjectSettings::get_editor_resource_path()
    
    Clean up editor_file_system.cpp
    
    Simplify editor_paths.cpp
    
    Add #TOOLS_ENABLED to project settings.
    
    Add TOOLS_ENABLED to file_access and dir_access
    
    Fix TOOLS_ENABLED
    
    fix var name
    
    Fix for Andriod
    
    Fix error check,
    
    Fix rebase bug.
    
    change "editor" keywords to "global"

--- core/config/project_settings.cpp
+++ core/config/project_settings.cpp
@@ -70,6 +70,17 @@ String ProjectSettings::get_resource_path() const {
 	return resource_path;
 }
 
+#ifdef TOOLS_ENABLED
+String ProjectSettings::get_global_resource_path() {
+	ERR_FAIL_COND_V_MSG(global_resource_path.is_empty(), get_resource_path(), "Global resource path is only available in the editor. Returning project resource path instead.");
+	return global_resource_path;
+}
+
+void ProjectSettings::set_global_resource_path(const String &p_path) {
+	global_resource_path = p_path;
+}
+#endif // TOOLS_ENABLED
+
 // This returns paths like "res://.godot/imported".
 String ProjectSettings::get_imported_files_path() const {
 	return get_project_data_path().path_join("imported");
