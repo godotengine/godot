c860baf2b3051c222f4e907cff71d1855533914c
PiCode <122909224+PiCode9560@users.noreply.github.com>
2025-12-22 21:07:58 +0300

    Added global resource filesystem
    Changed prefix to "editor://"
    
    Fixed editor resource folder not created.
    
    Add support for editor:// paths in string utilities
    
    Extended String methods to handle 'editor://' paths similarly to 'res://' and 'user://'. Updated path_to and is_resource_file to recognize 'editor://'. Adjusted FileSystemDock navigation to only process 'res://' paths for directory checks.
    
    Make the editor:// prefix work in running project if launched from the editor.
    
    Allow adding editor:// to project settings directory rules, and added extra checks in editor_file_system.cpp
    
    Allowed autoload to load editor:// path.
    
    Added editor:// path to project settings directory rules by default.
    
    Fixes
    
    Allow opening editor:// script path by clicking output error.
    
    Fixed build failed.
    
    undo allowed loading editor:// directory at runtime.
    
    Undo "Allowed autoload to load editor:// path."
    
    Simplify code.
    
    Cleaned up code and doc.
    
    Changed path prefix to `edres://`.
    
    Undo "Changed path prefix to `edres://`".
    
    Changed editor addon path to editor://addons/
    
    Fix build fail. Add return error.

--- core/io/file_access.cpp
+++ core/io/file_access.cpp
@@ -71,6 +71,8 @@ Ref<FileAccess> FileAccess::create_for_path(const String &p_path) {
 		ret = create(ACCESS_RESOURCES);
 	} else if (p_path.begins_with("user://")) {
 		ret = create(ACCESS_USERDATA);
+	} else if (p_path.begins_with("editor://")) {
+		ret = create(ACCESS_EDITOR_RESOURCES);
 	} else if (p_path.begins_with("pipe://")) {
 		ret = create(ACCESS_PIPE);
 	} else {
@@ -180,7 +182,6 @@ Ref<FileAccess> FileAccess::open(const String &p_path, int p_mode_flags, Error *
 	if (err != OK) {
 		ret.unref();
 	}
-
 	return ret;
 }
 
@@ -285,6 +286,15 @@ String FileAccess::fix_path(const String &p_path) const {
 					return r_path.replace("user:/", data_dir);
 				}
 				return r_path.replace("user://", "");
+			}
+		} break;
+		case ACCESS_EDITOR_RESOURCES: {
+			if (r_path.begins_with("editor://")) {
+				String data_dir = ProjectSettings::get_singleton()->get_editor_resource_path();
+				if (!data_dir.is_empty()) {
+					return r_path.replace("editor:/", data_dir);
+				}
+				return r_path.replace("editor://", "");
 			}
 
 		} break;
