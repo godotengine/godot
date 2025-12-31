10613525750b2d2f8df6cae19de5799aca7a8840
Fredia Huya-Kouadio <fhuyakou@gmail.com>
2025-12-31 02:14:23 -0800

    Add support for PiP mode

--- platform/android/java/lib/src/main/java/org/godotengine/godot/feature/PictureInPictureProvider.kt
+++ platform/android/java/lib/src/main/java/org/godotengine/godot/feature/PictureInPictureProvider.kt
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  GodotApp.java                                                         */
+/*  PictureInPictureProvider.kt                                           */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -28,73 +28,14 @@
 /* SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.                 */
 /**************************************************************************/
 
-package com.godot.game;
-
-import org.godotengine.godot.Godot;
-import org.godotengine.godot.GodotActivity;
-
-import android.os.Bundle;
-import android.util.Log;
-
-import androidx.activity.EdgeToEdge;
-import androidx.core.splashscreen.SplashScreen;
+package org.godotengine.godot.feature
 
 /**
- * Template activity for Godot Android builds.
- * Feel free to extend and modify this class for your custom logic.
+ * Provides APIs to enable picture-in-picture.
  */
-public class GodotApp extends GodotActivity {
-	static {
-		// .NET libraries.
-		if (BuildConfig.FLAVOR.equals("mono")) {
-			try {
-				Log.v("GODOT", "Loading System.Security.Cryptography.Native.Android library");
-				System.loadLibrary("System.Security.Cryptography.Native.Android");
-			} catch (UnsatisfiedLinkError e) {
-				Log.e("GODOT", "Unable to load System.Security.Cryptography.Native.Android library");
-			}
-		}
-	}
+interface PictureInPictureProvider {
 
-	private final Runnable updateWindowAppearance = () -> {
-		Godot godot = getGodot();
-		if (godot != null) {
-			godot.enableImmersiveMode(godot.isInImmersiveMode(), true);
-			godot.enableEdgeToEdge(godot.isInEdgeToEdgeMode(), true);
-			godot.setSystemBarsAppearance();
-		}
-	};
+	fun enterPiPMode()
 
-	@Override
-	public void onCreate(Bundle savedInstanceState) {
-		SplashScreen.installSplashScreen(this);
-		EdgeToEdge.enable(this);
-		super.onCreate(savedInstanceState);
-	}
-
-	@Override
-	public void onResume() {
-		super.onResume();
-		updateWindowAppearance.run();
-	}
-
-	@Override
-	public void onGodotMainLoopStarted() {
-		super.onGodotMainLoopStarted();
-		runOnUiThread(updateWindowAppearance);
-	}
-
-	@Override
-	public void onGodotForceQuit(Godot instance) {
-		if (!BuildConfig.FLAVOR.equals("instrumented")) {
-			// For instrumented builds, we disable force-quitting to allow the instrumented tests to complete
-			// successfully, otherwise they fail when the process crashes.
-			super.onGodotForceQuit(instance);
-		}
-	}
-
-	@Override
-	protected boolean isPiPEnabled() {
-		return true;
-	}
+	fun isPiPModeSupported(): Boolean
 }
