96a52dbba94d9b5a11082af3a14c284773291b1c
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2026-01-07 15:14:30 +0300

    Handle RGB10_A2 storage format in octmap shaders.

--- servers/rendering/renderer_rd/effects/copy_effects.h
+++ servers/rendering/renderer_rd/effects/copy_effects.h
@@ -256,9 +256,11 @@ private:
 	};
 
 	enum OctmapDownsamplerMode {
-		DOWNSAMPLER_MODE_LOW_QUALITY,
-		DOWNSAMPLER_MODE_HIGH_QUALITY,
-		DOWNSAMPLER_MODE_MAX
+		DOWNSAMPLER_MODE_FLAG_HIGH_QUALITY = (1 << 0),
+		DOWNSAMPLER_MODE_FLAG_RGB10_A2 = (1 << 1),
+
+		DOWNSAMPLER_MODE_COMPUTE_MAX = ((DOWNSAMPLER_MODE_FLAG_HIGH_QUALITY | DOWNSAMPLER_MODE_FLAG_RGB10_A2) + 1),
+		DOWNSAMPLER_MODE_RASTER_MAX = (DOWNSAMPLER_MODE_FLAG_HIGH_QUALITY + 1),
 	};
 
 	struct OctmapDownsampler {
@@ -266,16 +268,17 @@ private:
 		OctmapDownsamplerShaderRD compute_shader;
 		OctmapDownsamplerRasterShaderRD raster_shader;
 		RID shader_version;
-		PipelineDeferredRD compute_pipelines[DOWNSAMPLER_MODE_MAX];
-		PipelineCacheRD raster_pipelines[DOWNSAMPLER_MODE_MAX];
+		PipelineDeferredRD compute_pipelines[DOWNSAMPLER_MODE_COMPUTE_MAX];
+		PipelineCacheRD raster_pipelines[DOWNSAMPLER_MODE_RASTER_MAX];
 	} octmap_downsampler;
 
 	enum OctmapFilterMode {
-		FILTER_MODE_HIGH_QUALITY,
-		FILTER_MODE_LOW_QUALITY,
-		FILTER_MODE_HIGH_QUALITY_ARRAY,
-		FILTER_MODE_LOW_QUALITY_ARRAY,
-		FILTER_MODE_MAX,
+		FILTER_MODE_FLAG_HIGH_QUALITY = (1 << 0),
+		FILTER_MODE_FLAG_ARRAY = (1 << 1),
+		FILTER_MODE_FLAG_RGB10_A2 = (1 << 2),
+
+		FILTER_MODE_COMPUTE_MAX = ((FILTER_MODE_FLAG_HIGH_QUALITY | FILTER_MODE_FLAG_ARRAY | FILTER_MODE_FLAG_RGB10_A2) + 1),
+		FILTER_MODE_RASTER_MAX = (FILTER_MODE_FLAG_HIGH_QUALITY + 1),
 	};
 
 	struct OctmapFilterPushConstant {
@@ -294,8 +297,8 @@ private:
 		OctmapFilterShaderRD compute_shader;
 		OctmapFilterRasterShaderRD raster_shader;
 		RID shader_version;
-		PipelineDeferredRD compute_pipelines[FILTER_MODE_MAX];
-		PipelineCacheRD raster_pipelines[FILTER_MODE_MAX];
+		PipelineDeferredRD compute_pipelines[FILTER_MODE_COMPUTE_MAX];
+		PipelineCacheRD raster_pipelines[FILTER_MODE_RASTER_MAX];
 
 		RID uniform_set;
 		RID image_uniform_set;
@@ -303,6 +306,12 @@ private:
 		bool use_high_quality;
 
 	} filter;
+
+	enum OctmapRoughnessMode {
+		ROUGHNESS_MODE_RGBA16F,
+		ROUGHNESS_MODE_RGB10_A2,
+		ROUGHNESS_MODE_MAX,
+	};
 
 	struct OctmapRoughnessPushConstant {
 		uint32_t sample_count;
@@ -320,7 +329,7 @@ private:
 		OctmapRoughnessShaderRD compute_shader;
 		OctmapRoughnessRasterShaderRD raster_shader;
 		RID shader_version;
-		PipelineDeferredRD compute_pipeline;
+		PipelineDeferredRD compute_pipelines[ROUGHNESS_MODE_MAX];
 		PipelineCacheRD raster_pipeline;
 	} roughness;
 
