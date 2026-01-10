8d93cb70bb841dee511bff1c13851ac5b51adb74
PiCode <122909224+PiCode9560@users.noreply.github.com>
2026-01-10 08:47:05 +0300

    Add editor:// path to the filesystem dock.
    Fixed ProjectSettings::localize_path.
    
    Fixed drag and drop on the same path.
    
    Fixed directory create dialog checking the wrong path.
    
    Fixed editor freezing when using the filesystem split mode.
    
    Fixed create resource not on the right path.
    
    Fixed cannot create script on editor:// directory.
    
    Added show editor directory toggle button.
    
    Updated systems to work with editor:// path.
    
    Simplify and added missing DirAccess code.
    
    Made favorites work with editor:// stuff.
    
    Fixed EditorDirDialog still showing editor:// when it is hidden in filesystem.
    
    Fixed editor:// folders collapsed after filesystem scanned, and filesystem not scanning after creating script on editor:// directory.
    
    Fixed filtering `editor://` files in split viewer mode not working properly.
    
    Rebased on master.
    
    Fix style
    
    Optimize queue
    
    fix missing BIND_ENUM_CONSTANT(ACCESS_EDITOR_RESOURCES);
    
    Undo LocalVector on old code.
    
    Fix fail check.
    
    Add docs
    
    Bugfix.
    
    Allow file dialog to change path between res:// and editor://
    
    Fix duplicate uid error on moving file in dock.
    
    Store show editor directory state in project metadata.
    
    simplify FileSystemDock::_try_move_item
    
    unexpose show_editor_directory_changed
    
    remove magic numbers.
    
    Fix _show_editor_directory_changed signal
    
    Simplify _update_dir
    
    Fix favorite display
    
    Simplify code
    
    Fix EditorDirDialog freeze.
    
    Fix editor scenes external changes not prompt
    
    Fix file dialog
    
    - Remove switchable path between editor:// and res://
    - Add error checks for ACCESS_EDITOR_RESOURCES.
    
    Simplify FileSystemDock::get_uncollapsed_paths()
    
    Add #TOOLS_ENABLED to project settings.
    
    Fix CI error
    
    Fix ProjectSettings::localize_path
    
    Fix editor:// uncollapsed_paths
    
    Add missing ACCESS_EDITOR_RESOURCE checks.
    
    Fix TOOLS_ENABLED
    
    store editor:// folders collapse state when hidden.
    
    Fix error.
    
    Minor changes.

--- scene/gui/file_dialog.cpp
+++ scene/gui/file_dialog.cpp
@@ -76,6 +76,10 @@ void FileDialog::_native_popup() {
 		root = ProjectSettings::get_singleton()->get_resource_path();
 	} else if (access == ACCESS_USERDATA) {
 		root = OS::get_singleton()->get_user_data_dir();
+#ifdef TOOLS_ENABLED
+	} else if (access == ACCESS_EDITOR_RESOURCES) {
+		root = ProjectSettings::get_singleton()->get_editor_resource_path();
+#endif
 	}
 
 	// Attach native file dialog to first persistent parent window.
@@ -93,7 +97,7 @@ void FileDialog::_native_popup() {
 }
 
 bool FileDialog::_can_use_native_popup() const {
-	if (access == ACCESS_RESOURCES || access == ACCESS_USERDATA || options.size() > 0) {
+	if (access == ACCESS_RESOURCES || access == ACCESS_USERDATA || access == ACCESS_EDITOR_RESOURCES || options.size() > 0) {
 		return DisplayServer::get_singleton()->has_feature(DisplayServerEnums::FEATURE_NATIVE_DIALOG_FILE_EXTRA);
 	}
 	return DisplayServer::get_singleton()->has_feature(DisplayServerEnums::FEATURE_NATIVE_DIALOG_FILE);
@@ -1533,7 +1537,7 @@ bool FileDialog::is_customization_flag_enabled(Customization p_flag) const {
 }
 
 void FileDialog::set_access(Access p_access) {
-	ERR_FAIL_INDEX(p_access, 3);
+	ERR_FAIL_INDEX((int)p_access, 4);
 	if (access == p_access) {
 		return;
 	}
@@ -1550,6 +1554,9 @@ void FileDialog::set_access(Access p_access) {
 		} break;
 		case ACCESS_RESOURCES: {
 			dir_access = DirAccess::create(DirAccess::ACCESS_RESOURCES);
+		} break;
+		case ACCESS_EDITOR_RESOURCES: {
+			dir_access = DirAccess::create(DirAccess::ACCESS_EDITOR_RESOURCES);
 		} break;
 		case ACCESS_USERDATA: {
 			dir_access = DirAccess::create(DirAccess::ACCESS_USERDATA);
@@ -1638,8 +1645,12 @@ void FileDialog::_select_drive(int p_idx) {
 void FileDialog::_change_dir(const String &p_new_dir) {
 	if (access == ACCESS_RESOURCES && p_new_dir.begins_with("user://")) {
 		ERR_FAIL_MSG("Can't change to userdata folder when using ACCESS_RESOURCES.");
+	} else if (access == ACCESS_EDITOR_RESOURCES && p_new_dir.begins_with("user://")) {
+		ERR_FAIL_MSG("Can't change to userdata folder when using ACCESS_EDITOR_RESOURCES.");
 	} else if (access == ACCESS_USERDATA && p_new_dir.begins_with("res://")) {
 		ERR_FAIL_MSG("Can't change to resources folder when using ACCESS_USERDATA.");
+	} else if (access == ACCESS_USERDATA && p_new_dir.begins_with("editor://")) {
+		ERR_FAIL_MSG("Can't change to editor resources folder when using ACCESS_USERDATA.");
 	}
 
 	if (root_prefix.is_empty()) {
@@ -1932,12 +1943,15 @@ void FileDialog::_update_recent_list() {
 bool FileDialog::_path_matches_access(const String &p_path) const {
 	bool is_res = p_path.begins_with("res://");
 	bool is_user = p_path.begins_with("user://");
+	bool is_editor = p_path.begins_with("editor://");
 	if (access == ACCESS_RESOURCES) {
 		return is_res;
 	} else if (access == ACCESS_USERDATA) {
 		return is_user;
+	} else if (access == ACCESS_EDITOR_RESOURCES) {
+		return is_editor;
 	}
-	return !is_res && !is_user;
+	return !is_res && !is_user && !is_editor;
 }
 
 TypedArray<Dictionary> FileDialog::_get_options() const {
@@ -2205,6 +2219,7 @@ void FileDialog::_bind_methods() {
 	BIND_ENUM_CONSTANT(ACCESS_RESOURCES);
 	BIND_ENUM_CONSTANT(ACCESS_USERDATA);
 	BIND_ENUM_CONSTANT(ACCESS_FILESYSTEM);
+	BIND_ENUM_CONSTANT(ACCESS_EDITOR_RESOURCES);
 
 	BIND_ENUM_CONSTANT(DISPLAY_THUMBNAILS);
 	BIND_ENUM_CONSTANT(DISPLAY_LIST);
