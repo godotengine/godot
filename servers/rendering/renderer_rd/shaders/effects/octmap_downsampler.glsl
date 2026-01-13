146d7fd303ec853e104a9158641d43129ae6f28f
clayjohn <claynjohn@gmail.com>
2026-01-12 18:56:10 -0800

    Fix downsampled radiance map generation.
    Reduce the mipmap levels so that the border is pixel perfect
    
    Always use the high quality downsample to reduce jaggies
    
    Fix the jacobian approximation so that it actually helps account for the octahedral distortion

--- servers/rendering/renderer_rd/shaders/effects/octmap_downsampler.glsl
+++ servers/rendering/renderer_rd/shaders/effects/octmap_downsampler.glsl
@@ -44,7 +44,7 @@ params;
 
 // Use an approximation of the Jacobian.
 float calcWeight(float u, float v) {
-	vec3 d = oct_to_vec3_with_border(vec2(u, v), params.border_size);
+	vec3 d = oct_to_vec3_with_border(vec2(u, v) * 0.5 + 0.5, params.border_size);
 	return 1.0 / pow(abs(d.z) + 1.0, 3.0);
 }
 
@@ -54,7 +54,6 @@ void main() {
 		float inv_size = 1.0 / float(params.size);
 		uvec2 sample_id = id;
 
-#ifdef USE_HIGH_QUALITY
 		float u0 = (float(sample_id.x) * 2.0f + 1.0f - 0.75f) * inv_size - 1.0f;
 		float u1 = (float(sample_id.x) * 2.0f + 1.0f + 0.75f) * inv_size - 1.0f;
 		float v0 = (float(sample_id.y) * 2.0f + 1.0f - 0.75f) * inv_size - 1.0f;
@@ -75,9 +74,5 @@ void main() {
 		color += textureLod(source_octmap, vec2(u0, v1) * 0.5f + 0.5f, 0.0) * weights[2];
 		color += textureLod(source_octmap, vec2(u1, v1) * 0.5f + 0.5f, 0.0) * weights[3];
 		imageStore(dest_octmap, ivec2(id), color);
-#else
-		vec2 uv = (vec2(sample_id.xy) + 0.5) * inv_size;
-		imageStore(dest_octmap, ivec2(id), textureLod(source_octmap, uv, 0.0));
-#endif
 	}
 }
