4baf41b9f8e8afde72210c7a4598583dddc6609e
Luca <lucatuerk@googlemail.com>
2026-01-17 18:29:14 +0100

    Reworked EditorQuickOpenDialog to use UIDs internally and validate resource validity on init fixing issues when files are moved or removed.

--- editor/file_system/editor_file_system.cpp
+++ editor/file_system/editor_file_system.cpp
@@ -129,6 +129,11 @@ String EditorFileSystemDirectory::get_file_path(int p_idx) const {
 	return get_path().path_join(get_file(p_idx));
 }
 
+ResourceUID::ID EditorFileSystemDirectory::get_file_uid(int p_idx) const {
+	ERR_FAIL_INDEX_V(p_idx, files.size(), ResourceUID::INVALID_ID);
+	return files[p_idx]->uid;
+}
+
 Vector<String> EditorFileSystemDirectory::get_file_deps(int p_idx) const {
 	ERR_FAIL_INDEX_V(p_idx, files.size(), Vector<String>());
 	Vector<String> deps;
