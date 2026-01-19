83d839205020434c5b49b9d2844c7233f7c6a90e
Trevor Davenport <trevor.davenport@gmail.com>
2026-01-19 15:20:47 -0700

    When enabled, materials provide feedback on texture usage during rendering via a SSBO buffer.  This material feedback is merged and processed to determine the needed lod for each texture.
    New settings allow a system wide min/max lod selection as well as the
    ability to override these settings per-texture.  Settings also allow
    constraining textures to a memory budget(when possible unless already
    at the minimum quality).
    
    The texture fitting algorithm tries to keep textures in memory if
    possible.  It does a good job at using the memory budget.  There is
    a new performance monitor to help visualize texture memory usage.

--- servers/rendering/dummy/storage/texture_storage.h
+++ servers/rendering/dummy/storage/texture_storage.h
@@ -230,6 +230,11 @@ public:
 
 	virtual void render_target_set_velocity_target_size(RID p_render_target, const Size2i &p_target_size) override {}
 	virtual Size2i render_target_get_velocity_target_size(RID p_render_target) const override { return Size2i(0, 0); }
+
+	virtual void texture_2d_attach_streaming_state(RID p_texture, RID p_streaming_state) override {}
+	virtual RID texture_2d_create_from_texture_allocate() override { return RID(); }
+	virtual void texture_2d_create_from_texture_initialize(RID p_texture, RID p_src_texture, int p_new_width, int p_new_height, uint32_t p_copy_mips_count, const Vector<uint8_t> &p_new_mip_data = Vector<uint8_t>()) override {}
+	virtual void texture_2d_create_from_texture_copy_existing(RID p_texture, RID p_src_texture, uint32_t p_copy_mips_count) override {}
 };
 
 } // namespace RendererDummy
