edbe1cee2682d427a5b9aa8173ce1652586cb94e
Jakub Marcowski <chubercikbattle@gmail.com>
2026-01-19 10:24:17 +0100

    Update various Unicode-related files to version 17.0.0

--- misc/scripts/unicode_ranges_fetch.py
+++ misc/scripts/unicode_ranges_fetch.py
@@ -16,7 +16,7 @@ if __name__ == "__main__":
 
 from methods import generate_copyright_header
 
-URL: Final[str] = "https://www.unicode.org/Public/16.0.0/ucd/Blocks.txt"
+URL: Final[str] = "https://www.unicode.org/Public/17.0.0/ucd/Blocks.txt"
 
 
 ranges: list[tuple[str, str, str]] = []
