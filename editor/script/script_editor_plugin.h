44afa9a7d15050260a065621894149b3810a26c2
Flyn San <flynsarmy@gmail.com>
2026-01-19 13:33:15 +1000

    Add 'Find in Folder...' to FileSystem context menu

--- editor/script/script_editor_plugin.h
+++ editor/script/script_editor_plugin.h
@@ -574,7 +574,7 @@ public:
 	bool is_files_panel_toggled();
 	void apply_scripts() const;
 	void reload_scripts(bool p_refresh_only = false);
-	void open_find_in_files_dialog(const String &text);
+	void open_find_in_files_dialog(const String &text, const String &folder = "");
 	void open_script_create_dialog(const String &p_base_name, const String &p_base_path);
 	void open_text_file_create_dialog(const String &p_base_path, const String &p_base_name = "");
 	Ref<Resource> open_file(const String &p_file);
