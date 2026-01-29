b111be5ff509f21e9cc31903cd0dd05dd22dee52
LanzaSchneider <schneiderlanza@gmail.com>
2026-01-29 19:00:34 +0800

    Fix `script_language_extension` compilation with deprecated=no

--- core/object/script_language_extension.cpp
+++ core/object/script_language_extension.cpp
@@ -104,8 +104,8 @@ void ScriptLanguageExtension::_bind_methods() {
 	GDVIRTUAL_BIND(_validate, "script", "path", "validate_functions", "validate_errors", "validate_warnings", "validate_safe_lines");
 
 	GDVIRTUAL_BIND(_validate_path, "path");
-	GDVIRTUAL_BIND(_create_script);
 #ifndef DISABLE_DEPRECATED
+	GDVIRTUAL_BIND(_create_script);
 	GDVIRTUAL_BIND(_has_named_classes);
 #endif
 	GDVIRTUAL_BIND(_supports_builtin_mode);
