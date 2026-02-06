21bf2cb6a667705bc371e6a516c740e1e47fb213
cdemirer <41021322+cdemirer@users.noreply.github.com>
2026-02-06 22:32:18 +0300

    Fix UAF in destructor of Visual Shader Editor

--- editor/shader/visual_shader_editor_plugin.cpp
+++ editor/shader/visual_shader_editor_plugin.cpp
@@ -5397,6 +5397,10 @@ void VisualShaderEditor::_notification(int p_what) {
 		case NOTIFICATION_DRAG_END: {
 			members->set_drop_mode_flags(0);
 		} break;
+
+		case NOTIFICATION_PREDELETE: {
+			save_editor_layout();
+		} break;
 	}
 }
 
@@ -7827,7 +7831,6 @@ VisualShaderEditor::VisualShaderEditor() {
 }
 
 VisualShaderEditor::~VisualShaderEditor() {
-	save_editor_layout();
 }
 
 class VisualShaderNodePluginInputEditor : public OptionButton {
