61edc8da88f72a80cb92333fd9b9af72f51c4efa
Mikael Hermansson <mikael@hermansson.io>
2026-02-09 18:20:16 +0100

    Zero out padding in `NodePath` serialization

--- core/io/marshalls.cpp
+++ core/io/marshalls.cpp
@@ -1519,7 +1519,9 @@ Error encode_variant(const Variant &p_variant, uint8_t *r_buffer, int &r_len, bo
 					encode_uint32(utf8.length(), buf);
 					buf += 4;
 					memcpy(buf, utf8.get_data(), utf8.length());
-					buf += pad + utf8.length();
+					buf += utf8.length();
+					memset(buf, 0, pad);
+					buf += pad;
 				}
 
 				r_len += 4 + utf8.length() + pad;
