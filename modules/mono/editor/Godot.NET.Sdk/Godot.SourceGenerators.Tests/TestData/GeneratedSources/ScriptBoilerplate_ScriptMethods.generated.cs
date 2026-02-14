3fa430fa92fb03555805997022352e8a7fdfcd48
warappa <spoiltnr1@gmx.at>
2026-02-14 21:04:54 +0100

    Fix source generator tests, fix some code style issues in generated code

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Tests/TestData/GeneratedSources/ScriptBoilerplate_ScriptMethods.generated.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Tests/TestData/GeneratedSources/ScriptBoilerplate_ScriptMethods.generated.cs
@@ -1,6 +1,7 @@
 using Godot;
 using Godot.NativeInterop;
 using Godot.Bridge;
+using System.Runtime.CompilerServices;
 
 partial class ScriptBoilerplate
 {
@@ -8,7 +9,8 @@ partial class ScriptBoilerplate
     /// <summary>
     /// Cached StringNames for the methods contained in this class, for fast lookup.
     /// </summary>
-    public new class MethodName : global::Godot.Node.MethodName {
+    public new class MethodName : global::Godot.Node.MethodName
+    {
         /// <summary>
         /// Cached name for the '_Process' method.
         /// </summary>
@@ -27,25 +29,54 @@ partial class ScriptBoilerplate
     internal new static global::System.Collections.Generic.List<global::Godot.Bridge.MethodInfo> GetGodotMethodList()
     {
         var methods = new global::System.Collections.Generic.List<global::Godot.Bridge.MethodInfo>(2);
-        methods.Add(new(name: MethodName.@_Process, returnVal: new(type: (global::Godot.Variant.Type)0, name: "", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), flags: (global::Godot.MethodFlags)1, arguments: new() { new(type: (global::Godot.Variant.Type)3, name: "delta", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false),  }, defaultArguments: null));
-        methods.Add(new(name: MethodName.@Bazz, returnVal: new(type: (global::Godot.Variant.Type)2, name: "", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), flags: (global::Godot.MethodFlags)1, arguments: new() { new(type: (global::Godot.Variant.Type)21, name: "name", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false),  }, defaultArguments: null));
+        methods.Add(new(name: MethodName.@_Process, returnVal: new(type: (global::Godot.Variant.Type)0, name: "", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), flags: (global::Godot.MethodFlags)1, arguments: new() { new(type: (global::Godot.Variant.Type)3, name: "delta", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), }, defaultArguments: null));
+        methods.Add(new(name: MethodName.@Bazz, returnVal: new(type: (global::Godot.Variant.Type)2, name: "", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), flags: (global::Godot.MethodFlags)1, arguments: new() { new(type: (global::Godot.Variant.Type)21, name: "name", hint: (global::Godot.PropertyHint)0, hintString: "", usage: (global::Godot.PropertyUsageFlags)6, exported: false), }, defaultArguments: null));
         return methods;
     }
 #pragma warning restore CS0109
 
-    public new static readonly ScriptMethodRegistry<ScriptBoilerplate> MethodRegistry = new ScriptMethodRegistry<ScriptBoilerplate>()
+    protected new static readonly ScriptMethodRegistry<ScriptBoilerplate> MethodRegistry = new ScriptMethodRegistry<ScriptBoilerplate>()
         .Register(global::Godot.Node.MethodRegistry)
-        .Register(MethodName._Process, 1, (ScriptBoilerplate scriptInstance, NativeVariantPtrArgs args, out godot_variant ret) =>
-        {
-            scriptInstance._Process(global::Godot.NativeInterop.VariantUtils.ConvertTo<double>(args[0]));
-            ret = default;
-        })
-        .Register(MethodName.Bazz, 1, (ScriptBoilerplate scriptInstance, NativeVariantPtrArgs args, out godot_variant ret) =>
-        {
-            var callRet = scriptInstance.Bazz(global::Godot.NativeInterop.VariantUtils.ConvertTo<global::Godot.StringName>(args[0]));
-            ret = global::Godot.NativeInterop.VariantUtils.CreateFrom<int>(callRet);
-        })
+        .Register(MethodName._Process, 1, ScriptMethodDispatchHelper.CreateScriptMethod__Process1())
+        .Register(MethodName.Bazz, 1, ScriptMethodDispatchHelper.CreateScriptMethod_Bazz1())
         .Compile();
+
+    private sealed class ScriptMethodDispatchHelper
+    {
+        public static ScriptMethod<GodotObject> CreateScriptMethod__Process1()
+        {
+            static godot_variant Impl(GodotObject scriptInstance, scoped in NativeVariantPtrArgs args)
+            {
+                Unsafe.As<GodotObject, ScriptBoilerplate>(ref scriptInstance)._Process(global::Godot.NativeInterop.VariantUtils.ConvertTo<double>(args[0]));
+                godot_variant ret = default;
+                return ret;
+            }
+
+            // Wrap static method into ScriptMethodPtr
+            //return ScriptMethodPtr.Create<ScriptBoilerplate>(&Impl);
+            return Impl;
+        }
+        public static ScriptMethod<GodotObject> CreateScriptMethod_Bazz1()
+        {
+            static godot_variant Impl(GodotObject scriptInstance, scoped in NativeVariantPtrArgs args)
+            {
+                var callRet = Unsafe.As<GodotObject, ScriptBoilerplate>(ref scriptInstance).Bazz(global::Godot.NativeInterop.VariantUtils.ConvertTo<global::Godot.StringName>(args[0]));
+                var ret = global::Godot.NativeInterop.VariantUtils.CreateFrom<int>(callRet);
+                return ret;
+            }
+
+            // Wrap static method into ScriptMethodPtr
+            //return ScriptMethodPtr.Create<ScriptBoilerplate>(&Impl);
+            return Impl;
+        }
+    }
+
+    /// <inheritdoc/>
+    [global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
+    public override ref readonly ScriptMethod<GodotObject> TryGetGodotClassMethod(in godot_string_name method, int argc)
+    {
+        return ref MethodRegistry.TryGetMethodFast(in method, argc);
+    }
 
     /// <inheritdoc/>
     [global::System.ComponentModel.EditorBrowsable(global::System.ComponentModel.EditorBrowsableState.Never)]
@@ -53,7 +84,7 @@ partial class ScriptBoilerplate
     {
         if (MethodRegistry.TryGetMethod(in method, args.Count, out var scriptMethod))
         {
-            scriptMethod(this, args, out ret);
+            ret = scriptMethod(this, args);
             return true;
         }
 
