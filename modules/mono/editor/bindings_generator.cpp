1179688ca4d0ccff1946b06b312cc9cd626674b2
warappa <spoiltnr1@gmx.at>
2026-02-21 00:38:41 +0100

    Fix style issue

--- modules/mono/editor/bindings_generator.cpp
+++ modules/mono/editor/bindings_generator.cpp
@@ -2700,8 +2700,7 @@ Error BindingsGenerator::_generate_cs_type(const TypeInterface &itype, const Str
 			if (!imethod.is_virtual) {
 				continue;
 			}
-			output << "\n"
-				   << INDENT2 ".AddAlias("
+			output << INDENT2 ".AddAlias("
 				   << CS_STATIC_FIELD_METHOD_PROXY_NAME_PREFIX << imethod.name << ", " << itos(imethod.arguments.size()) << ", MethodName." << imethod.proxy_name
 				   << ")\n";
 		}
@@ -2957,8 +2956,7 @@ Error BindingsGenerator::_generate_cs_type(const TypeInterface &itype, const Str
 			// Generate alias names for signals
 
 			for (const SignalInterface &isignal : itype.signals_) {
-				output << "\n"
-					   << INDENT2 ".AddAlias("
+				output << INDENT2 ".AddAlias("
 					   << CS_STATIC_FIELD_SIGNAL_PROXY_NAME_PREFIX << isignal.name << ", " << itos(isignal.arguments.size()) << ", SignalName." << isignal.proxy_name
 					   << ")\n";
 			}
