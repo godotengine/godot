2fb0f188d696f99b9a16774522b3ecb7391d73e9
kitfox <mark@kitfox.com>
2026-02-21 18:09:36 -0500

    Moving volume calculation next to cloth calculation.

--- modules/jolt_physics/objects/jolt_soft_body_3d.cpp
+++ modules/jolt_physics/objects/jolt_soft_body_3d.cpp
@@ -244,87 +244,6 @@ JPH::SoftBodySharedSettings *JoltSoftBody3D::_create_shared_settings_cloth() {
 	return settings;
 }
 
-void JoltSoftBody3D::_apply_environmental_forces(float p_step, JPH::Body &p_jolt_body) {
-	// Get approximation of the center of the soft body.
-	Vector3 com_position = to_godot(p_jolt_body.GetCenterOfMassPosition());
-
-	// Calculate gravity and which areas affect the soft body through wind.
-	bool gravity_done = false;
-	Vector3 gravity;
-	LocalVector<JoltArea3D *> wind_areas;
-	for (JoltArea3D *area : areas) {
-		if (!gravity_done) {
-			gravity_done = JoltArea3D::apply_override(gravity, area->get_gravity_mode(), [&]() {
-				return area->compute_gravity(com_position);
-			});
-		}
-
-		if (area->get_wind_pressure() > CMP_EPSILON) {
-			wind_areas.push_back(area);
-		}
-	}
-
-	// Add default gravity.
-	if (!gravity_done) {
-		gravity += space->get_default_area()->compute_gravity(com_position);
-	}
-
-	// Apply gravity to soft body. Note that this only works so long as vertices have uniform mass (excluding pinned vertices).
-	p_jolt_body.AddForce(to_jolt(gravity) * mass);
-
-	if (!wind_areas.is_empty()) {
-		JPH::SoftBodyMotionProperties &motion_properties = static_cast<JPH::SoftBodyMotionProperties &>(*p_jolt_body.GetMotionPropertiesUnchecked());
-		JPH::Array<JPH::SoftBodyVertex> &physics_vertices = motion_properties.GetVertices();
-
-		for (const JPH::SoftBodySharedSettings::Face &physics_face : motion_properties.GetFaces()) {
-			JPH::SoftBodyVertex &physics_vertex0 = physics_vertices[physics_face.mVertex[0]];
-			JPH::SoftBodyVertex &physics_vertex1 = physics_vertices[physics_face.mVertex[1]];
-			JPH::SoftBodyVertex &physics_vertex2 = physics_vertices[physics_face.mVertex[2]];
-
-			// Calculate the triangle centroid.
-			Vector3 v0 = to_godot(physics_vertex0.mPosition);
-			Vector3 v1 = to_godot(physics_vertex1.mPosition);
-			Vector3 v2 = to_godot(physics_vertex2.mPosition);
-			Vector3 centroid = com_position + (v0 + v1 + v2) * real_t(1.0 / 3.0);
-
-			// Calculate the triangle normal.
-			Vector3 normal = (v2 - v0).cross(v1 - v0);
-			real_t normal_length = normal.length();
-			if (normal_length > real_t(1.0e-6)) { // If the normal is near zero, the area is near zero so we can skip this triangle.
-				normal /= normal_length;
-
-				// Area is half the length of the cross product of two sides.
-				real_t triangle_area = real_t(0.5) * normal_length;
-
-				// Accumulate wind forces from all wind areas.
-				Vector3 wind_force;
-				for (const JoltArea3D *area : wind_areas) {
-					const Vector3 &wind_direction = area->get_wind_direction();
-					const Vector3 &wind_source = area->get_wind_source();
-
-					// Calculate attenuation factor based on distance from wind source to triangle centroid.
-					// We do not allow a projection below 1 to ensure that we never amplify and to avoid NaNs when the value would be negative.
-					real_t projection_toward_centroid = MAX((centroid - wind_source).dot(wind_direction), real_t(1.0));
-					real_t attenuation_over_distance = Math::pow(projection_toward_centroid, -real_t(area->get_wind_attenuation_factor()));
-
-					// Calculate force magnitude.
-					real_t force_magnitude = area->get_wind_pressure() * triangle_area;
-
-					// Calculate the resulting wind force on the triangle by projecting wind direction onto triangle normal.
-					// Divide by 3 to distribute force equally over each vertex.
-					wind_force += (force_magnitude * attenuation_over_distance * real_t(1.0 / 3.0) * normal.dot(wind_direction)) * normal;
-				}
-
-				// Apply the force as an impulse over the timestep.
-				JPH::Vec3 impulse = to_jolt(wind_force * p_step);
-				physics_vertex0.mVelocity += impulse * physics_vertex0.mInvMass;
-				physics_vertex1.mVelocity += impulse * physics_vertex1.mInvMass;
-				physics_vertex2.mVelocity += impulse * physics_vertex2.mInvMass;
-			}
-		}
-	}
-}
-
 JPH::SoftBodySharedSettings *JoltSoftBody3D::_create_shared_settings_volume() {
 	RenderingServer *rendering = RenderingServer::get_singleton();
 
@@ -586,12 +505,6 @@ JPH::SoftBodySharedSettings *JoltSoftBody3D::_create_shared_settings_volume() {
 	pin_vertices(*this, pinned_vertices, mesh_to_physics, settings->mVertices);
 
 	// Since Godot's stiffness is input as a coefficient between 0 and 1, and Jolt uses actual stiffness for its
-	// edge constraints, we crudely map one to the other with an arbitrary constant.
-	//const float stiffness = MAX(Math::pow(stiffness_coefficient, 3.0f) * 100000.0f, 0.000001f);
-	//const float inverse_stiffness = 1.0f / stiffness;
-
-
-	// Since Godot's stiffness is input as a coefficient between 0 and 1, and Jolt uses actual stiffness for its
 	// edge constraints, we must map one to the other.
 	//
 	// Godot uses classic PBD edge constraints, which have a stiffness parameter k that is used in the position correction formula as follows:
@@ -631,6 +544,87 @@ JPH::SoftBodySharedSettings *JoltSoftBody3D::_create_shared_settings_volume() {
 	settings->Optimize();
 
 	return settings;
+}
+
+void JoltSoftBody3D::_apply_environmental_forces(float p_step, JPH::Body &p_jolt_body) {
+	// Get approximation of the center of the soft body.
+	Vector3 com_position = to_godot(p_jolt_body.GetCenterOfMassPosition());
+
+	// Calculate gravity and which areas affect the soft body through wind.
+	bool gravity_done = false;
+	Vector3 gravity;
+	LocalVector<JoltArea3D *> wind_areas;
+	for (JoltArea3D *area : areas) {
+		if (!gravity_done) {
+			gravity_done = JoltArea3D::apply_override(gravity, area->get_gravity_mode(), [&]() {
+				return area->compute_gravity(com_position);
+			});
+		}
+
+		if (area->get_wind_pressure() > CMP_EPSILON) {
+			wind_areas.push_back(area);
+		}
+	}
+
+	// Add default gravity.
+	if (!gravity_done) {
+		gravity += space->get_default_area()->compute_gravity(com_position);
+	}
+
+	// Apply gravity to soft body. Note that this only works so long as vertices have uniform mass (excluding pinned vertices).
+	p_jolt_body.AddForce(to_jolt(gravity) * mass);
+
+	if (!wind_areas.is_empty()) {
+		JPH::SoftBodyMotionProperties &motion_properties = static_cast<JPH::SoftBodyMotionProperties &>(*p_jolt_body.GetMotionPropertiesUnchecked());
+		JPH::Array<JPH::SoftBodyVertex> &physics_vertices = motion_properties.GetVertices();
+
+		for (const JPH::SoftBodySharedSettings::Face &physics_face : motion_properties.GetFaces()) {
+			JPH::SoftBodyVertex &physics_vertex0 = physics_vertices[physics_face.mVertex[0]];
+			JPH::SoftBodyVertex &physics_vertex1 = physics_vertices[physics_face.mVertex[1]];
+			JPH::SoftBodyVertex &physics_vertex2 = physics_vertices[physics_face.mVertex[2]];
+
+			// Calculate the triangle centroid.
+			Vector3 v0 = to_godot(physics_vertex0.mPosition);
+			Vector3 v1 = to_godot(physics_vertex1.mPosition);
+			Vector3 v2 = to_godot(physics_vertex2.mPosition);
+			Vector3 centroid = com_position + (v0 + v1 + v2) * real_t(1.0 / 3.0);
+
+			// Calculate the triangle normal.
+			Vector3 normal = (v2 - v0).cross(v1 - v0);
+			real_t normal_length = normal.length();
+			if (normal_length > real_t(1.0e-6)) { // If the normal is near zero, the area is near zero so we can skip this triangle.
+				normal /= normal_length;
+
+				// Area is half the length of the cross product of two sides.
+				real_t triangle_area = real_t(0.5) * normal_length;
+
+				// Accumulate wind forces from all wind areas.
+				Vector3 wind_force;
+				for (const JoltArea3D *area : wind_areas) {
+					const Vector3 &wind_direction = area->get_wind_direction();
+					const Vector3 &wind_source = area->get_wind_source();
+
+					// Calculate attenuation factor based on distance from wind source to triangle centroid.
+					// We do not allow a projection below 1 to ensure that we never amplify and to avoid NaNs when the value would be negative.
+					real_t projection_toward_centroid = MAX((centroid - wind_source).dot(wind_direction), real_t(1.0));
+					real_t attenuation_over_distance = Math::pow(projection_toward_centroid, -real_t(area->get_wind_attenuation_factor()));
+
+					// Calculate force magnitude.
+					real_t force_magnitude = area->get_wind_pressure() * triangle_area;
+
+					// Calculate the resulting wind force on the triangle by projecting wind direction onto triangle normal.
+					// Divide by 3 to distribute force equally over each vertex.
+					wind_force += (force_magnitude * attenuation_over_distance * real_t(1.0 / 3.0) * normal.dot(wind_direction)) * normal;
+				}
+
+				// Apply the force as an impulse over the timestep.
+				JPH::Vec3 impulse = to_jolt(wind_force * p_step);
+				physics_vertex0.mVelocity += impulse * physics_vertex0.mInvMass;
+				physics_vertex1.mVelocity += impulse * physics_vertex1.mInvMass;
+				physics_vertex2.mVelocity += impulse * physics_vertex2.mInvMass;
+			}
+		}
+	}
 }
 
 void JoltSoftBody3D::_update_mass() {
