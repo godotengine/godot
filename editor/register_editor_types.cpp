2d3ea5f30304c8e4c6b7dfd15917462213ce2dcd
tvenclovas96_bigblackc <144885265+tvenclovas96@users.noreply.github.com>
2026-02-22 15:49:02 +0200

    audio editor button
    Revert "audio editor button"
    
    This reverts commit 73a5e9663697154b535350aa3f877b4a5cf108bc.
    
    editor ploogin

--- editor/register_editor_types.cpp
+++ editor/register_editor_types.cpp
@@ -32,6 +32,7 @@
 
 #include "core/object/script_language.h"
 #include "editor/animation/animation_tree_editor_plugin.h"
+#include "editor/audio/audio_play_button_editor_plugin.h"
 #include "editor/audio/audio_stream_editor_plugin.h"
 #include "editor/audio/audio_stream_randomizer_editor_plugin.h"
 #include "editor/debugger/debug_adapter/debug_adapter_server.h"
@@ -215,6 +216,7 @@ void register_editor_types() {
 
 	// This list is alphabetized, and plugins that depend on Node2D are in their own section below.
 	EditorPlugins::add_by_type<AnimationTreeEditorPlugin>();
+	EditorPlugins::add_by_type<AudioPlayButtonEditorPlugin>();
 	EditorPlugins::add_by_type<AudioStreamEditorPlugin>();
 	EditorPlugins::add_by_type<AudioStreamRandomizerEditorPlugin>();
 	EditorPlugins::add_by_type<BitMapEditorPlugin>();
