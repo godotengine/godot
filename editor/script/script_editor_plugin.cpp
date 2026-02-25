a83022510313135004b52df24626f276aebdc624
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-02-24 19:11:21 -0800

    Add ScriptEditor::close_file()

--- editor/script/script_editor_plugin.cpp
+++ editor/script/script_editor_plugin.cpp
@@ -2602,6 +2602,19 @@ Ref<Resource> ScriptEditor::open_file(const String &p_file) {
 	return Ref<Resource>();
 }
 
+void ScriptEditor::close_file(const String &p_file) {
+	for (int i = 0; i < tab_container->get_tab_count(); i++) {
+		ScriptEditorBase *se = Object::cast_to<ScriptEditorBase>(tab_container->get_tab_control(i));
+		if (!se) {
+			continue;
+		}
+		if (se->get_edited_resource()->get_path() == p_file) {
+			_close_tab(i, false, false);
+			break;
+		}
+	}
+}
+
 void ScriptEditor::_add_callback(Object *p_obj, const String &p_function, const PackedStringArray &p_args) {
 	ERR_FAIL_NULL(p_obj);
 	Ref<Script> scr = p_obj->get_script();
@@ -3764,6 +3777,7 @@ void ScriptEditor::_bind_methods() {
 	ClassDB::bind_method(D_METHOD("update_docs_from_script", "script"), &ScriptEditor::update_docs_from_script);
 	ClassDB::bind_method(D_METHOD("clear_docs_from_script", "script"), &ScriptEditor::clear_docs_from_script);
 
+	ClassDB::bind_method(D_METHOD("close_file", "path"), &ScriptEditor::close_file);
 	ClassDB::bind_method(D_METHOD("save_all_scripts"), &ScriptEditor::save_all_scripts);
 
 	ADD_SIGNAL(MethodInfo("editor_script_changed", PropertyInfo(Variant::OBJECT, "script", PROPERTY_HINT_RESOURCE_TYPE, Script::get_class_static())));
