801cabc94000e7d477c1c2b5131ea3c0414fa674
Nintorch <92302738+Nintorch@users.noreply.github.com>
2026-02-26 21:08:31 +0500

    Project setting to ignore joypads on unfocused app
    Co-Authored-By: Christoph Taucher <4498502+chtau@users.noreply.github.com>

--- scene/debugger/scene_debugger.h
+++ scene/debugger/scene_debugger.h
@@ -106,6 +106,7 @@ private:
 	static Error _msg_runtime_node_select_set_avoid_locked(const Array &p_args);
 	static Error _msg_runtime_node_select_set_prefer_group(const Array &p_args);
 	static Error _msg_rq_screenshot(const Array &p_args);
+	static Error _msg_report_window_focused(const Array &p_args);
 
 	static Error _msg_runtime_node_select_reset_camera_2d(const Array &p_args);
 	static Error _msg_transform_camera_2d(const Array &p_args);
