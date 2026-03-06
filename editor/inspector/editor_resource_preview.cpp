2156d9ec91021834b52ba03fb601c3193bc08077
connortodd21 <connortodd21@gmail.com>
2026-03-05 18:17:04 -0800

    Reload rescources in editor_resource_preview.cpp when import settings changed

--- editor/inspector/editor_resource_preview.cpp
+++ editor/inspector/editor_resource_preview.cpp
@@ -323,10 +323,12 @@ void EditorResourcePreview::_iterate() {
 		// No cache found, generate.
 		_generate_preview(texture, small_texture, item, cache_base, preview_metadata);
 	} else {
-		uint64_t modtime = FileAccess::get_modified_time(item.path);
+		uint64_t source_modtime = FileAccess::get_modified_time(item.path);
 		String import_path = item.path + ".import";
-		if (FileAccess::exists(import_path)) {
-			modtime = MAX(modtime, FileAccess::get_modified_time(import_path));
+		bool has_import_file = FileAccess::exists(import_path);
+		uint64_t import_modtime = 0;
+		if (has_import_file) {
+			import_modtime = FileAccess::get_modified_time(import_path);
 		}
 
 		int tsize;
@@ -344,7 +346,12 @@ void EditorResourcePreview::_iterate() {
 		} else if (outdated) {
 			cache_valid = false;
 			f.unref();
-		} else if (last_modtime != modtime) {
+		} else if (has_import_file && import_modtime > last_modtime) {
+			// Import settings changed; reload cached resource.
+			ResourceLoader::load(item.path, "", ResourceFormatLoader::CACHE_MODE_REPLACE);
+			f.unref();
+			cache_valid = false;
+		} else if (source_modtime > last_modtime) {
 			String last_md5 = f->get_line();
 			String md5 = FileAccess::get_md5(item.path);
 			f.unref();
@@ -360,6 +367,7 @@ void EditorResourcePreview::_iterate() {
 					// some proper cleanup/disabling of resource preview generation.
 					ERR_PRINT("Cannot create file '" + file + "'. Check user write permissions.");
 				} else {
+					uint64_t modtime = MAX(source_modtime, import_modtime);
 					_write_preview_cache(f2, thumbnail_size, has_small_texture, modtime, md5, preview_metadata);
 				}
 			}
