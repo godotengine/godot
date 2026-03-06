xianofantasia/Go

Tweak shader compilation error messages
- Mention the shader type that failed compilation.
- Mention the most common cause for this error (which are syntax errors).
- Mention that the shader code is printed nearby for troubleshooting.

servers/rendering/dummy/storage/material_storage.cpp
@@ -88,7 +88,7 @@ void MaterialStorage::shader_set_code(RID p_shader, const String &p_code) {
 	ShaderCompiler::GeneratedCode gen_code;
 
 	Error err = MaterialStorage::get_singleton()->dummy_compiler.compile(new_mode, p_code, &actions, "", gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, vformat("%s shader compilation failed (most likely due to a syntax error). Shader code has been printed nearby for troubleshooting.", mode_string.capitalize()));
 }
 
 void MaterialStorage::get_shader_parameter_list(RID p_shader, List<PropertyInfo> *p_param_list) const {

servers/rendering/renderer_rd/environment/fog.cpp
@@ -336,7 +336,7 @@ void Fog::FogShaderData::set_code(const String &p_code) {
 	Fog *fog_singleton = Fog::get_singleton();
 
 	Error err = fog_singleton->volumetric_fog.compiler.compile(RS::SHADER_FOG, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Fog shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Fog shader compilation failed (most likely due to a syntax error). Shader code has been printed nearby for troubleshooting.");
 
 	if (version.is_null()) {
 		version = fog_singleton->volumetric_fog.shader.version_create();

servers/rendering/renderer_rd/environment/sky.cpp
@@ -105,7 +105,7 @@ void SkyRD::SkyShaderData::set_code(const String &p_code) {
 	RendererSceneRenderRD *scene_singleton = static_cast<RendererSceneRenderRD *>(RendererSceneRenderRD::singleton);
 
 	Error err = scene_singleton->sky.sky_shader.compiler.compile(RS::SHADER_SKY, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Sky shader compilation failed (most likely due to a syntax error). Shader code has been printed nearby for troubleshooting.");
 
 	if (version.is_null()) {
 		version = scene_singleton->sky.sky_shader.shader.version_create();

servers/rendering/renderer_rd/storage_rd/particles_storage.cpp
@@ -1683,7 +1683,7 @@ void ParticlesStorage::ParticlesShaderData::set_code(const String &p_code) {
 	actions.uniforms = &uniforms;
 
 	Error err = particles_storage->particles_shader.compiler.compile(RS::SHADER_PARTICLES, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Particles shader compilation failed (most likely due to a syntax error). Shader code has been printed nearby for troubleshooting.");
 
 	if (version.is_null()) {
 		version = particles_storage->particles_shader.shader.version_create();

godotengine/godot

servers/rendering/dummy/storage/material_storage.cpp
@@ -189,7 +189,7 @@ void MaterialStorage::shader_set_code(RID p_shader, const String &p_code) {
 	ShaderCompiler::GeneratedCode gen_code;
 
 	Error err = MaterialStorage::get_singleton()->dummy_compiler.compile(new_mode, p_code, &actions, "", gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, vformat("%s shader compilation failed (most likely due to a syntax error). Shader code has been printed above for troubleshooting.", mode_string.capitalize()));
 }
 
 void MaterialStorage::get_shader_parameter_list(RID p_shader, List<PropertyInfo> *p_param_list) const {

servers/rendering/renderer_rd/environment/fog.cpp
@@ -379,7 +379,7 @@ void Fog::FogShaderData::set_code(const String &p_code) {
 	Fog *fog_singleton = Fog::get_singleton();
 
 	Error err = fog_singleton->volumetric_fog.compiler.compile(RSE::SHADER_FOG, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Fog shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Fog shader compilation failed (most likely due to a syntax error). Shader code has been printed above for troubleshooting.");
 
 	if (version.is_null()) {
 		version = fog_singleton->volumetric_fog.shader.version_create();

servers/rendering/renderer_rd/environment/sky.cpp
@@ -107,7 +107,7 @@ void SkyRD::SkyShaderData::set_code(const String &p_code) {
 	RendererSceneRenderRD *scene_singleton = static_cast<RendererSceneRenderRD *>(RendererSceneRenderRD::singleton);
 
 	Error err = scene_singleton->sky.sky_shader.compiler.compile(RSE::SHADER_SKY, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Sky shader compilation failed (most likely due to a syntax error). Shader code has been printed above for troubleshooting.");
 
 	if (version.is_null()) {
 		version = scene_singleton->sky.sky_shader.shader.version_create();

servers/rendering/renderer_rd/storage_rd/particles_storage.cpp
@@ -1716,7 +1716,7 @@ void ParticlesStorage::ParticlesShaderData::set_code(const String &p_code) {
 	actions.uniforms = &uniforms;
 
 	Error err = particles_storage->particles_shader.compiler.compile(RSE::SHADER_PARTICLES, code, &actions, path, gen_code);
-	ERR_FAIL_COND_MSG(err != OK, "Shader compilation failed.");
+	ERR_FAIL_COND_MSG(err != OK, "Particles shader compilation failed (most likely due to a syntax error). Shader code has been printed above for troubleshooting.");
 
 	if (version.is_null()) {
 		version = particles_storage->particles_shader.shader.version_create();