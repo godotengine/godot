331541b25af890a14e19095f4213ca47e0b17122
ryevdokimov <robert.yevdokimov@autstand.com>
2026-03-08 00:34:57 -0500

    Add vertex snapping to the 3D editor

--- editor/scene/3d/node_3d_editor_plugin.h
+++ editor/scene/3d/node_3d_editor_plugin.h
@@ -235,6 +235,16 @@ private:
 	bool collision_reposition = false;
 	real_t gizmo_scale;
 
+	bool vertex_snap_mode = false;
+	Key vertex_snap_keycode = Key::NONE;
+	bool vertex_snap_dragging = false;
+	Vector3 vertex_snap_source;
+	Plane vertex_snap_drag_plane;
+	Vector3 vertex_snap_target;
+	bool vertex_snap_has_target = false;
+	bool vertex_snap_has_source = false;
+	HashMap<ObjectID, Vector3> vertex_snap_original_positions;
+
 	PanelContainer *info_panel = nullptr;
 	Label *info_label = nullptr;
 	Label *cinema_label = nullptr;
@@ -271,6 +281,14 @@ private:
 	ObjectID _select_ray(const Point2 &p_pos) const;
 	void _find_items_at_pos(const Point2 &p_pos, Vector<_RayResult> &r_results, bool p_include_locked);
 
+	float _min_screen_dist_to_aabb(const AABB &p_aabb, const Transform3D &p_transform, const Point2 &p_cursor) const;
+	bool _find_closest_vertex_on_node(const Point2 &p_screen_pos, Node3D *p_node, float &r_closest_screen_dist, Vector3 &r_vertex_world) const;
+	bool _find_closest_vertex_in_scene(const Point2 &p_screen_pos, float p_threshold, Vector3 &r_vertex_world, const HashMap<ObjectID, Vector3> *p_exclude = nullptr);
+	void _vertex_snap_update_source(const Point2 &p_screen_pos);
+	void _vertex_snap_commit();
+	void _vertex_snap_cancel();
+	bool _is_vertex_occluded(const Vector3 &p_world_pos, const Vector2 &p_screen_pos) const;
+
 	Transform3D _get_camera_transform() const;
 	int get_selected_count() const;
 	void cancel_transform();
@@ -279,6 +297,7 @@ private:
 	Vector3 _get_camera_position() const;
 	Vector3 _get_camera_normal() const;
 	Vector3 _get_screen_to_space(const Vector3 &p_vector3);
+	Vector<Plane> _build_screen_frustum(const Point2 &p_min, const Point2 &p_max);
 
 	void _select_region();
 	bool _transform_gizmo_select(const Vector2 &p_screenpos, bool p_highlight_only = false);
