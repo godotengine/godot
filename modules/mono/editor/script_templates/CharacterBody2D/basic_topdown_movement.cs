4c701438db07603f46b6fbc89e20f8735da979e7
KristianNikolov07 <kris.nikolov07@gmail.com>
2026-03-13 08:47:28 +0200

    Added script template for basic topdown Character2D movement
    Update modules/mono/editor/script_templates/CharacterBody2D/basic_topdown_movement.cs
    
    Co-authored-by: Danil Alexeev <dalexeev12@yandex.ru>
    
    Update modules/mono/editor/script_templates/CharacterBody2D/basic_topdown_movement.cs
    
    Co-authored-by: Danil Alexeev <dalexeev12@yandex.ru>

---
+++ modules/mono/editor/script_templates/CharacterBody2D/basic_topdown_movement.cs
@@ -0,0 +1,25 @@
+// meta-description: Classic movement for topdown games
+
+using _BINDINGS_NAMESPACE_;
+using System;
+
+public partial class _CLASS_ : _BASE_
+{
+    public const float Speed = 300.0f;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        // As good practice, you should replace UI actions with custom gameplay actions.
+        Vector2 direction = Input.GetVector("ui_left", "ui_right", "ui_up", "ui_down");
+        if (direction != Vector2.Zero)
+        {
+            Velocity = direction * Speed;
+        }
+        else
+        {
+            Velocity = Velocity.move_toward(Vector2.Zero, Speed);
+        }
+
+        MoveAndSlide();
+    }
+}
