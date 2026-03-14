f5859d4ffabf8ad8f6a468a5f0832ea8cab52fab
LiveTrower <alfonsogrimaldo2001@gmail.com>
2026-03-13 19:09:04 -0600

    Add collision shape gizmo to SpringArm3D

--- editor/scene/3d/node_3d_editor_plugin.cpp
+++ editor/scene/3d/node_3d_editor_plugin.cpp
@@ -8659,6 +8659,7 @@ void Node3DEditor::_notification(int p_what) {
 			}
 			if (EditorSettings::get_singleton()->check_changed_settings_in_group("editors/3d_gizmos/gizmo_settings")) {
 				CollisionShape3DGizmoPlugin::set_show_only_when_selected(EDITOR_GET("editors/3d_gizmos/gizmo_settings/show_collision_shapes_only_when_selected"));
+				SpringArm3DGizmoPlugin::set_show_only_when_selected(EDITOR_GET("editors/3d_gizmos/gizmo_settings/show_collision_shapes_only_when_selected"));
 				update_all_gizmos();
 			}
 		} break;
