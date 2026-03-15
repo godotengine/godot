2669dd9849edb5eb5ed0c4b6a77ddca359dc1773
Lukas Martinek <64120576+LathiccBoy@users.noreply.github.com>
2026-03-15 14:25:02 -0700

    Apply suggestions from code review
    Better test messages
    
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- tests/scene/test_control.cpp
+++ tests/scene/test_control.cpp
@@ -1237,13 +1237,13 @@ TEST_CASE("[SceneTree][Control] Container layout") {
 		container->notification(Container::NOTIFICATION_SORT_CHILDREN);
 		CHECK_MESSAGE(
 			b1->get_position().y == 0,
-			"First child at top of container");
+			"First child should be at the top of the container.");
 		CHECK_MESSAGE(
 			Math::is_equal_approx(b2->get_position().y, b1->get_size().y),
-			"Second child after first child's height");
+			"Second child position should be after first child's height.");
 		CHECK_MESSAGE(
 			Math::is_equal_approx(b3->get_position().y, b1->get_size().y + b2->get_size().y),
-			"Third child after sum of other children's heights");
+			"Third child position should be after the sum of other children's heights.");
 		root->remove_child(container);
 		memdelete(container);
 	}
@@ -1261,13 +1261,13 @@ TEST_CASE("[SceneTree][Control] Container layout") {
 		container->notification(Container::NOTIFICATION_SORT_CHILDREN);
 		CHECK_MESSAGE(
 			b1->get_position().x == 0,
-			"First child left side of container");
+			"First child should be on the left side of the container.");
 		CHECK_MESSAGE(
 			Math::is_equal_approx(b2->get_position().x, b1->get_size().x),
-			"Second child after first child's length");
+			"Second child should be after the first child's width.");
 		CHECK_MESSAGE(
 			Math::is_equal_approx(b3->get_position().x, b1->get_size().x + b2->get_size().x),
-			"Third child after sum of other children's lengths");
+			"Third child should be after the sum of the other children's widths.");
 		root->remove_child(container);
 		memdelete(container);
 	}
