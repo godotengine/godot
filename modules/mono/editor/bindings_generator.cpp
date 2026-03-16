b65d6f70876e25a92d217a921674bef022f70cd9
warappa <spoiltnr1@gmx.at>
2026-03-16 19:00:09 +0100

    Fail if base_name was not found instead of just logging it

--- modules/mono/editor/bindings_generator.cpp
+++ modules/mono/editor/bindings_generator.cpp
@@ -2661,8 +2661,7 @@ Error BindingsGenerator::_generate_cs_type(const TypeInterface &itype, const Str
 				}
 				output << ".MethodRegistry)";
 			} else {
-				ERR_PRINT("Base type '" + itype.base_name.operator String() + "' does not exist, for class '" + itype.name + "'.");
-				return ERR_INVALID_DATA;
+				ERR_FAIL_V_MSG(ERR_INVALID_DATA, "Base type '" + itype.base_name.operator String() + "' does not exist, for class '" + itype.name + "'.");
 			}
 		}
 
