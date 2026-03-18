fb3d0c3a8a9ec17b15f74136e1b39e6214d5f18b
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-03-18 09:25:26 -0700

    Add ScriptEditor::get_unsaved_files

--- editor/script/script_editor_plugin.h
+++ editor/script/script_editor_plugin.h
@@ -424,6 +424,7 @@ public:
 	void get_breakpoints(List<String> *p_breakpoints);
 
 	void reload_open_files();
+	PackedStringArray get_unsaved_files() const;
 	PackedStringArray get_unsaved_scripts() const;
 	void save_current_script();
 	void save_all_scripts();
