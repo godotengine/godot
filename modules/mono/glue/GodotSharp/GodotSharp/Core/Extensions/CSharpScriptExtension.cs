63b9580463fc85ae1f1ce98f965ee2afb363c643
Valentin Arthur Thomas <64769541+warquys@users.noreply.github.com>
2026-03-23 04:44:43 +0100

    Fix comment formatting in CSharpScriptExtension

--- modules/mono/glue/GodotSharp/GodotSharp/Core/Extensions/CSharpScriptExtension.cs
+++ modules/mono/glue/GodotSharp/GodotSharp/Core/Extensions/CSharpScriptExtension.cs
@@ -6,7 +6,7 @@ namespace Godot
     public partial class CSharpScript
     {
         /// <summary>
-        /// Return the <see cref="Type"/> of the C# Script associated to this instance. 
+        /// Return the <see cref="Type"/> of the C# Script associated to this instance.
         /// </summary>
         /// <exception cref="InvalidOperationException">
         /// The C# Script cannot be found (in case of an invalid pointer).
