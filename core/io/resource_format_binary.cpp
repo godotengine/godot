555d2506360779bce95d0e9032991ae2b1ea3fe7
nikitalita <69168929+nikitalita@users.noreply.github.com>
2026-03-26 16:06:25 -0700

    Add `_start_load()`, `_finish_load()` to `Resource`

--- core/io/resource_format_binary.cpp
+++ core/io/resource_format_binary.cpp
@@ -810,6 +810,8 @@ Error ResourceLoaderBinary::load() {
 			internal_index_cache[path] = res;
 		}
 
+		res->_start_load(SNAME("binary"), ver_format);
+
 		int pc = f->get_32();
 
 		//set properties
@@ -885,6 +887,8 @@ Error ResourceLoaderBinary::load() {
 #ifdef TOOLS_ENABLED
 		res->set_edited(false);
 #endif
+
+		res->_finish_load(SNAME("binary"), ver_format);
 
 		if (progress) {
 			*progress = (i + 1) / float(internal_resources.size());
--- scene/resources/resource_format_text.cpp
+++ scene/resources/resource_format_text.cpp
@@ -186,6 +186,7 @@ Ref<PackedScene> ResourceLoaderText::_parse_node_tag(VariantParser::ResourcePars
 	if (packed_scene.is_null()) {
 		packed_scene.instantiate();
 	}
+	packed_scene->_start_load(SNAME("text"), format_version);
 
 	while (true) {
 		if (next_tag.name == "node") {
@@ -294,6 +295,7 @@ Ref<PackedScene> ResourceLoaderText::_parse_node_tag(VariantParser::ResourcePars
 						return Ref<PackedScene>();
 					} else {
 						error = OK;
+						packed_scene->_finish_load(SNAME("text"), format_version);
 						return packed_scene;
 					}
 				}
@@ -385,6 +387,7 @@ Ref<PackedScene> ResourceLoaderText::_parse_node_tag(VariantParser::ResourcePars
 					return Ref<PackedScene>();
 				} else {
 					error = OK;
+					packed_scene->_finish_load("text", format_version);
 					return packed_scene;
 				}
 			}
@@ -408,6 +411,7 @@ Ref<PackedScene> ResourceLoaderText::_parse_node_tag(VariantParser::ResourcePars
 					return Ref<PackedScene>();
 				} else {
 					error = OK;
+					packed_scene->_finish_load("text", format_version);
 					return packed_scene;
 				}
 			}
@@ -629,6 +633,7 @@ Error ResourceLoaderText::load() {
 
 		int_resources[id] = res; // Always assign int resources.
 		if (do_assign) {
+			res->_start_load(SNAME("text"), format_version);
 			if (cache_mode != ResourceFormatLoader::CACHE_MODE_IGNORE) {
 				res->set_path(path, cache_mode == ResourceFormatLoader::CACHE_MODE_REPLACE);
 			} else {
@@ -714,6 +719,10 @@ Error ResourceLoaderText::load() {
 		if (!missing_resource_properties.is_empty()) {
 			res->set_meta(META_MISSING_RESOURCES, missing_resource_properties);
 		}
+
+		if (do_assign) {
+			res->_finish_load(SNAME("text"), format_version);
+		}
 	}
 
 	while (true) {
@@ -765,6 +774,8 @@ Error ResourceLoaderText::load() {
 				resource = Ref<Resource>(r);
 			}
 		}
+
+		resource->_start_load(SNAME("text"), format_version);
 
 		Dictionary missing_resource_properties;
 
@@ -859,6 +870,7 @@ Error ResourceLoaderText::load() {
 		if (!missing_resource_properties.is_empty()) {
 			resource->set_meta(META_MISSING_RESOURCES, missing_resource_properties);
 		}
+		resource->_finish_load(SNAME("text"), format_version);
 
 		error = OK;
 
