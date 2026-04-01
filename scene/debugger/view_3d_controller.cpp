fd5332c8e97701bb42ba577c501661f8102ca040
ryevdokimov <robert.yevdokimov@autstand.com>
2026-04-01 16:53:03 -0400

    Fix 3D zoom indicator not showing in editor

--- scene/debugger/view_3d_controller.cpp
+++ scene/debugger/view_3d_controller.cpp
@@ -619,6 +619,8 @@ void View3DController::scale_cursor_distance(const float p_scale) {
 	} else {
 		zoom_failed_attempts_count = 0;
 	}
+
+	emit_signal(SNAME("cursor_distance_scaled"));
 }
 
 void View3DController::set_shortcut(const ShortcutName p_name, const Ref<Shortcut> &p_shortcut) {
@@ -792,6 +794,7 @@ void View3DController::_bind_methods() {
 
 	ADD_SIGNAL(MethodInfo("cursor_panned"));
 	ADD_SIGNAL(MethodInfo("cursor_interpolated"));
+	ADD_SIGNAL(MethodInfo("cursor_distance_scaled"));
 }
 
 #endif // _3D_DISABLED
