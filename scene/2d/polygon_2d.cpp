57703f8c1cf79af56f6ba3f18398192f3d31b753
Omniologist <sjtciu@gmail.com>
2026-04-01 14:38:22 +1030

    feat: added debug warning for failed triangulation.

--- scene/2d/polygon_2d.cpp
+++ scene/2d/polygon_2d.cpp
@@ -326,6 +326,9 @@ void Polygon2D::_notification(int p_what) {
 
 			if (invert || polygons.is_empty()) {
 				index_array = Geometry2D::triangulate_polygon(points);
+				if (index_array.is_empty()) {
+					WARN_PRINT_ONCE_ED("Failed to triangulate polygon(s). Polygon data may be invalid, degenerate or self-intersecting.");
+				}
 			} else {
 				//draw individual polygons
 				for (int i = 0; i < polygons.size(); i++) {
