ad5796f83d95d71cea86018a934241db2bece1bd
Ryan Vail <ryanvail64@gmail.com>
2026-04-04 18:17:48 -0400

    Try to avoid `queue_redraw` in 2D ray and shape cast `set_enabled` methods
    Adds a fast path if `set_enabled` was called with the existing `enabled`
    value, meaning it has no effect.
    
    Only queue a redraw if collisions are being debugged, so when debugging
    is disabled the cost of the redraw logic can be skipped.

--- scene/2d/physics/ray_cast_2d.cpp
+++ scene/2d/physics/ray_cast_2d.cpp
@@ -102,10 +102,19 @@ Vector2 RayCast2D::get_collision_normal() const {
 }
 
 void RayCast2D::set_enabled(bool p_enabled) {
+	if (enabled == p_enabled) {
+		return;
+	}
 	enabled = p_enabled;
-	queue_redraw();
-	if (is_inside_tree() && !Engine::get_singleton()->is_editor_hint()) {
-		set_physics_process_internal(p_enabled);
+
+	if (is_inside_tree()) {
+		if (get_tree()->is_debugging_collisions_hint()) {
+			queue_redraw();
+		}
+
+		if (!Engine::get_singleton()->is_editor_hint()) {
+			set_physics_process_internal(p_enabled);
+		}
 	}
 	if (!p_enabled) {
 		collided = false;
