11486bd3e58eccb958a17e3de031b25064df1afa
Edward Chan <edwardchanchunho@gmail.com>
2026-04-18 22:51:32 -0400

    Fixed 3D snap precision to use default_float_step.

--- editor/scene/3d/node_3d_editor_plugin.cpp
+++ editor/scene/3d/node_3d_editor_plugin.cpp
@@ -9748,6 +9748,9 @@ void Node3DEditor::_notification(int p_what) {
 				update_all_gizmos();
 			}
 			_update_vertex_snap_tooltips();
+			if (EditorSettings::get_singleton()->check_changed_settings_in_group("interface/inspector")) {
+				snap_translate->set_step(EDITOR_GET("interface/inspector/default_float_step"));
+			}
 		} break;
 
 		case NOTIFICATION_PHYSICS_PROCESS: {
@@ -10822,7 +10825,7 @@ Node3DEditor::Node3DEditor() {
 
 	snap_translate = memnew(EditorSpinSlider);
 	snap_translate->set_min(0.0);
-	snap_translate->set_step(0.001);
+	snap_translate->set_step(EDITOR_GET("interface/inspector/default_float_step"));
 	snap_translate->set_max(10.0);
 	snap_translate->set_suffix("m");
 	snap_translate->set_allow_greater(true);
