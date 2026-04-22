1f7adfbf67fd08cf364f6e61ba5ea58d501b27d2
Chesed <potatolegend200@gmail.com>
2026-04-23 02:12:14 +0530

    add missing include into material editor

--- editor/scene/material_editor_plugin.cpp
+++ editor/scene/material_editor_plugin.cpp
@@ -31,6 +31,7 @@
 #include "material_editor_plugin.h"
 
 #include "core/config/project_settings.h"
+#include "core/io/resource_loader.h"
 #include "core/object/callable_mp.h"
 #include "core/object/class_db.h"
 #include "editor/editor_node.h"
