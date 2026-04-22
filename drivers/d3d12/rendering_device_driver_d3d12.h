f867377ad55baedc1dd07013d6c85a6c4394fc81
Skyth <19259897+blueskythlikesclouds@users.noreply.github.com>
2026-04-22 16:23:51 +0300

    Fix OOB writes in D3D12 when setting vertex arrays.

--- drivers/d3d12/rendering_device_driver_d3d12.h
+++ drivers/d3d12/rendering_device_driver_d3d12.h
@@ -464,7 +464,7 @@ private:
 		LocalVector<AttachmentLayout> attachment_layouts;
 
 		const VertexFormatInfo *vf_info = nullptr;
-		D3D12_VERTEX_BUFFER_VIEW vertex_buffer_views[8] = {};
+		D3D12_VERTEX_BUFFER_VIEW vertex_buffer_views[D3D12_IA_VERTEX_INPUT_RESOURCE_SLOT_COUNT] = {};
 		uint32_t vertex_buffer_count = 0;
 	};
 
