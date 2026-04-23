f7d322a69632b0092af4db3ef5a11b1c7f055683
SlalomSquid <slalomsquid@outlook.com>
2026-04-23 15:10:05 +0800

    undo

--- editor/inspector/editor_property_name_processor.cpp
+++ editor/inspector/editor_property_name_processor.cpp
@@ -155,7 +155,7 @@ EditorPropertyNameProcessor::EditorPropertyNameProcessor() {
 	capitalize_string_remaps["arm32"] = "arm32";
 	capitalize_string_remaps["arm64"] = "arm64";
 	capitalize_string_remaps["arm64-v8a"] = "arm64-v8a";
-	capitalize_string_remaps["armeabi-v7a"] = "armabi-v7a";
+	capitalize_string_remaps["armeabi-v7a"] = "armeabi-v7a";
 	capitalize_string_remaps["arvr"] = "ARVR";
 	capitalize_string_remaps["astc"] = "ASTC";
 	capitalize_string_remaps["bbcode"] = "BBCode";
