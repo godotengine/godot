d96997836b82f7cbc683940d4c88f7aebdc212d8
NoNormalDev <190293561+NoNormalDev@users.noreply.github.com>
2026-04-25 23:40:43 +0200

    Add set_multicast_send_interface method to PacketPeerUDP

--- core/io/net_socket.h
+++ core/io/net_socket.h
@@ -117,6 +117,7 @@ public:
 	virtual void set_reuse_address_enabled(bool p_enabled) = 0;
 	virtual Error join_multicast_group(const IPAddress &p_multi_address, const String &p_if_name) = 0;
 	virtual Error leave_multicast_group(const IPAddress &p_multi_address, const String &p_if_name) = 0;
+	virtual Error set_multicast_send_interface(const String &p_if_name) = 0;
 
 	virtual ~NetSocket() {}
 };
