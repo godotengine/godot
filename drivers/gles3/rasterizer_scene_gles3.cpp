a041297050af9de5c5b3a8ca36db8bd29cb385d1
CookieBadger <emil.dobetsberger@gmail.com>
2026-04-26 20:48:52 +0200

    change LTC lookup table texture format to RGBAF and set p_8bit_dst to true in gaussian_blur

--- drivers/gles3/rasterizer_scene_gles3.cpp
+++ drivers/gles3/rasterizer_scene_gles3.cpp
@@ -3215,7 +3215,7 @@ void RasterizerSceneGLES3::_render_list_template(RenderListParameters *p_params,
 
 				ltc.lut1_texture = RS::get_singleton()->texture_2d_create(lut1_image);
 
-				int lut2_bytes = 3 * dimensions * dimensions;
+				int lut2_bytes = 4 * dimensions * dimensions;
 				size_t lut2_size = lut2_bytes * 4;
 
 				Ref<Image> lut2_image;
@@ -3223,7 +3223,7 @@ void RasterizerSceneGLES3::_render_list_template(RenderListParameters *p_params,
 				lut2_data.resize(lut2_size);
 
 				memcpy(lut2_data.ptrw(), LTC_LUT2, lut2_size);
-				lut2_image = Image::create_from_data(dimensions, dimensions, false, Image::FORMAT_RGBF, lut2_data);
+				lut2_image = Image::create_from_data(dimensions, dimensions, false, Image::FORMAT_RGBAF, lut2_data);
 
 				ltc.lut2_texture = RS::get_singleton()->texture_2d_create(lut2_image);
 			}
