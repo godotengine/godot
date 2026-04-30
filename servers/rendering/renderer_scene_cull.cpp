06a3be3405b20a6c016808019444f8a785970c3a
Max <mhumber@rndtech.co.uk>
2026-04-30 21:55:09 +0100

    Style check fixes.

--- servers/rendering/renderer_scene_cull.cpp
+++ servers/rendering/renderer_scene_cull.cpp
@@ -3397,8 +3397,7 @@ void RendererSceneCull::_render_scene(const RendererSceneRender::CameraData *p_c
 			//check shadow..
 
 			if (light) {
-				if (p_using_shadows && p_shadow_atlas.is_valid() && RSG::light_storage->light_has_shadow(E->base) && !(RSG::light_storage->light_get_type(E->base) == RSE::LIGHT_DIRECTIONAL &&
-					(RSG::light_storage->light_directional_get_sky_mode(E->base) == RSE::LIGHT_DIRECTIONAL_SKY_MODE_SKY_ONLY || RSG::light_storage->light_get_bake_mode(E->base) != RSE::LIGHT_BAKE_FULL))) {
+				if (p_using_shadows && p_shadow_atlas.is_valid() && RSG::light_storage->light_has_shadow(E->base) && !(RSG::light_storage->light_get_type(E->base) == RSE::LIGHT_DIRECTIONAL && (RSG::light_storage->light_directional_get_sky_mode(E->base) == RSE::LIGHT_DIRECTIONAL_SKY_MODE_SKY_ONLY || RSG::light_storage->light_get_bake_mode(E->base) != RSE::LIGHT_BAKE_FULL))) {
 					lights_with_shadow.push_back(E);
 				}
 				//add to list
