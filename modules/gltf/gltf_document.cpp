e9b27fd3bbf9939914d550689d04b1abc899db55
Arseny Kapoulkine <arseny.kapoulkine@gmail.com>
2026-05-01 17:06:17 -0700

    Implement tangent space generation with vertex splitting using meshopt
    meshopt_generateTangents is used to generate per-corner tangent vectors.
    For unindexed inputs, we then simply copy the result to the output. For
    indexed inputs, we copy them into the existing vertices and split
    vertices with divergent tangents on the fly.
    
    Splitting may interfere with blend shapes as it may disturb shared
    indexing. While it would be possible to propagate the split vector into
    the blend shape code and apply it to the blend targets as well, for now
    we simply avoid doing the splitting in the first place if the mesh has
    blend shapes.
    
    Script binding for generate_tangents uses pre-splitting behavior for
    compatibility.

--- modules/gltf/gltf_document.cpp
+++ modules/gltf/gltf_document.cpp
@@ -1834,8 +1834,7 @@ Error GLTFDocument::_parse_meshes(Ref<GLTFState> p_state) {
 			}
 			mesh_surface_tool->index();
 			if (generate_tangents && a.has("TEXCOORD_0")) {
-				//must generate mikktspace tangents.. ergh..
-				mesh_surface_tool->generate_tangents();
+				mesh_surface_tool->generate_tangents(/*split*/ !mesh_prim.has("targets"));
 			}
 			array = mesh_surface_tool->commit_to_arrays();
 
@@ -1974,7 +1973,7 @@ Error GLTFDocument::_parse_meshes(Ref<GLTFState> p_state) {
 					}
 					blend_surface_tool->index();
 					if (generate_tangents) {
-						blend_surface_tool->generate_tangents();
+						blend_surface_tool->generate_tangents(/*split*/ false);
 					}
 					array_copy = blend_surface_tool->commit_to_arrays();
 
