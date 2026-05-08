7b05c2205deef2242ee9d9854c147f58c1ac4f2b
M-Boyo <isaaklm6@gmail.com>
2026-05-08 20:58:37 +0200

    Fix sleeping behaviour in integrate forces

--- scene/3d/physics/physical_bone_3d.cpp
+++ scene/3d/physics/physical_bone_3d.cpp
@@ -807,7 +807,7 @@ void PhysicalBone3D::_body_state_changed(PhysicsDirectBodyState3D *p_state) {
 		return;
 	}
 
-	if (GDVIRTUAL_IS_OVERRIDDEN(_integrate_forces)) {
+	if (!p_state->is_sleeping() && GDVIRTUAL_IS_OVERRIDDEN(_integrate_forces)) {
 		_sync_body_state(p_state);
 
 		Transform3D old_transform = get_global_transform();
