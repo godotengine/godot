2c862b930c52e7633277e04365114c9651b52bc8
kobewi <kobewi4e@gmail.com>
2026-05-12 15:22:51 +0200

    Improve 2D editor dropping code

--- editor/scene/canvas_item_editor_plugin.h
+++ editor/scene/canvas_item_editor_plugin.h
@@ -685,6 +685,7 @@ class CanvasItemEditorViewport : public Control {
 
 	bool _cyclical_dependency_exists(const String &p_target_scene_path, Node *p_desired_node) const;
 	bool _is_any_texture_selected() const;
+	void _add_node_to_scene(Node *p_parent, Node *p_child, const Vector2 &p_target_position);
 	void _create_texture_node(Node *p_parent, Node *p_child, const String &p_path, const Point2 &p_point);
 	void _create_audio_node(Node *p_parent, const String &p_path, const Point2 &p_point);
 	bool _create_instance(Node *p_parent, const String &p_path, const Point2 &p_point);
