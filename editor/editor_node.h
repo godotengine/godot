dc4d08187456a4b0c45f1a2d830672b2fc63455f
kobewi <kobewi4e@gmail.com>
2026-05-24 00:22:57 +0200

    Add extended dock slots

--- editor/editor_node.h
+++ editor/editor_node.h
@@ -308,12 +308,20 @@ private:
 	ConfirmationDialog *video_restart_dialog = nullptr;
 
 	// Split containers.
+	friend class EditorDockManager; // TODO remove
+
+	DockSplitContainer *main_vsplit = nullptr;
+	DockSplitContainer *main_hsplit = nullptr;
+
 	DockSplitContainer *left_l_vsplit = nullptr;
 	DockSplitContainer *left_r_vsplit = nullptr;
-	DockSplitContainer *main_hsplit = nullptr;
+
+	DockSplitContainer *center_vsplit = nullptr;
+
 	DockSplitContainer *right_l_vsplit = nullptr;
 	DockSplitContainer *right_r_vsplit = nullptr;
-	DockSplitContainer *center_split = nullptr;
+
+	DockSplitContainer *bottom_hsplit = nullptr;
 
 	// Main tabs.
 	EditorSceneTabs *scene_tabs = nullptr;
@@ -770,7 +778,7 @@ public:
 
 	static EditorTitleBar *get_title_bar() { return singleton->title_bar; }
 	static VSplitContainer *get_top_split() { return singleton->top_split; }
-	static DockSplitContainer *get_center_split() { return singleton->center_split; }
+	static DockSplitContainer *get_center_split() { return singleton->center_vsplit; }
 	static EditorBottomPanel *get_bottom_panel() { return singleton->bottom_panel; }
 	static EditorMainScreen *get_editor_main_screen() { return singleton->editor_main_screen; }
 
