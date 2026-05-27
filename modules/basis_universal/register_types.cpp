65a0c75bf1dd39029b0194a01b64122f0c4122b7
DeeJayLSP <djlsp@proton.me>
2026-05-27 17:07:02 -0300

    Add missing range to `zstd_supercompression_level` setting

--- modules/basis_universal/register_types.cpp
+++ modules/basis_universal/register_types.cpp
@@ -44,7 +44,7 @@ void initialize_basis_universal_module(ModuleInitializationLevel p_level) {
 #ifdef TOOLS_ENABLED
 	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/textures/basis_universal/rdo_dict_size", PROPERTY_HINT_RANGE, "64,65536,1"), 1024);
 	GLOBAL_DEF(PropertyInfo(Variant::BOOL, "rendering/textures/basis_universal/zstd_supercompression"), true);
-	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/textures/basis_universal/zstd_supercompression_level"), 6);
+	GLOBAL_DEF(PropertyInfo(Variant::INT, "rendering/textures/basis_universal/zstd_supercompression_level", PROPERTY_HINT_RANGE, "1,22,1"), 6);
 
 	Image::basis_universal_packer = basis_universal_packer;
 #endif
