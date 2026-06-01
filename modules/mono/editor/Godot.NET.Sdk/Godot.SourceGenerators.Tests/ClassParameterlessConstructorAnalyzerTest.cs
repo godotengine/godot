1198e8a56ec211a7fb5d7d0dafff44798b00758e
Ryan Boyer <ryanjboyer@gmail.com>
2026-06-01 15:51:05 -0600

    Fix code style

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Tests/ClassParameterlessConstructorAnalyzerTest.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators.Tests/ClassParameterlessConstructorAnalyzerTest.cs
@@ -18,3 +18,4 @@ public class ClassParameterlessConstructorTest
             .Verify("ClassParameterlessConstructor.GD0004.cs", "ClassParameterlessConstructor.GD0004.fixed.cs");
     }
 }
+
--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/ClassParameterlessConstructorAnalyzer.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/ClassParameterlessConstructorAnalyzer.cs
@@ -105,3 +105,4 @@ namespace Godot.SourceGenerators
         }
     }
 }
+
