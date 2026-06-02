28420853be50781cc41a23ce3858020fd825e914
Jorrit Rouwe <jrouwe@gmail.com>
2026-06-02 21:26:16 +0200

    Change ConeTwistJoint3D to use cone shaped limits
    This matches the behaviour in GodotPhysics3D.

--- modules/jolt_physics/joints/jolt_cone_twist_joint_3d.cpp
+++ modules/jolt_physics/joints/jolt_cone_twist_joint_3d.cpp
@@ -78,7 +78,6 @@ JPH::Constraint *JoltConeTwistJoint3D::_build_swing_twist(JPH::Body *p_jolt_body
 	constraint_settings.mPosition2 = to_jolt_r(p_shifted_ref_b.origin);
 	constraint_settings.mTwistAxis2 = to_jolt(p_shifted_ref_b.basis.get_column(Vector3::AXIS_X));
 	constraint_settings.mPlaneAxis2 = to_jolt(p_shifted_ref_b.basis.get_column(Vector3::AXIS_Z));
-	constraint_settings.mSwingType = JPH::ESwingType::Pyramid;
 
 	if (p_jolt_body_a == nullptr) {
 		return constraint_settings.Create(JPH::Body::sFixedToWorld, *p_jolt_body_b);
