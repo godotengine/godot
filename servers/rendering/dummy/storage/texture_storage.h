xianofantasia/Go

Add an import option to correct a heightmap texture's baseline or normalize it
Automatic detection is implemented in the BaseMaterial3D shader.
It can be integrated in custom shaders by adding `hint_height` to the relevant
sampler uniforms or setting the texture mode to Height Map
in VisualShaderNodeTextureParameter.

When a texture is detected to be used as a heightmap, it'll switch to
the Correct Baseline mode automatically, which reduces parallax issues
without requiring manual adjustments to the heightmap scale.

If you want to further improve quality (or reduce the number of deep
parallax steps without impacting quality), you can use the Normalized
mode which requires adjusting the heightmap scale used in the material.
When doing so, the required heightmap scale multiplier is printed
on import so you can get the same effective heightmap scale as before.

servers/rendering/dummy/storage/texture_storage.h
@@ -122,6 +122,7 @@ public:
 	virtual void texture_set_detect_3d_callback(RID p_texture, RS::TextureDetectCallback p_callback, void *p_userdata) override {}
 	virtual void texture_set_detect_normal_callback(RID p_texture, RS::TextureDetectCallback p_callback, void *p_userdata) override {}
 	virtual void texture_set_detect_roughness_callback(RID p_texture, RS::TextureDetectRoughnessCallback p_callback, void *p_userdata) override {}
+	virtual void texture_set_detect_height_callback(RID p_texture, RS::TextureDetectCallback p_callback, void *p_userdata) override {}
 
 	virtual void texture_debug_usage(List<RS::TextureInfo> *r_info) override {}
 

servers/rendering/renderer_rd/storage_rd/texture_storage.cpp
@@ -1614,6 +1614,14 @@ void TextureStorage::texture_set_detect_roughness_callback(RID p_texture, RS::Te
 	tex->detect_roughness_callback = p_callback;
 }
 
+void TextureStorage::texture_set_detect_height_callback(RID p_texture, RS::TextureDetectCallback p_callback, void *p_userdata) {
+	Texture *tex = texture_owner.get_or_null(p_texture);
+	ERR_FAIL_NULL(tex);
+
+	tex->detect_height_callback_ud = p_userdata;
+	tex->detect_height_callback = p_callback;
+}
+
 void TextureStorage::texture_debug_usage(List<RS::TextureInfo> *r_info) {
 	List<RID> textures;
 	texture_owner.get_owned_list(&textures);

godotengine/godot

servers/rendering/dummy/storage/texture_storage.h
@@ -127,6 +127,7 @@ public:
 	virtual void texture_set_detect_3d_callback(RID p_texture, RenderingServerTypes::TextureDetectCallback p_callback, void *p_userdata) override {}
 	virtual void texture_set_detect_normal_callback(RID p_texture, RenderingServerTypes::TextureDetectCallback p_callback, void *p_userdata) override {}
 	virtual void texture_set_detect_roughness_callback(RID p_texture, RenderingServerTypes::TextureDetectRoughnessCallback p_callback, void *p_userdata) override {}
+	virtual void texture_set_detect_height_callback(RID p_texture, RenderingServerTypes::TextureDetectCallback p_callback, void *p_userdata) override {}
 
 	virtual void texture_debug_usage(List<RenderingServerTypes::TextureInfo> *r_info) override {}
 

servers/rendering/renderer_rd/storage_rd/texture_storage.cpp
@@ -2093,6 +2093,14 @@ void TextureStorage::texture_set_detect_roughness_callback(RID p_texture, Render
 	tex->detect_roughness_callback = p_callback;
 }
 
+void TextureStorage::texture_set_detect_height_callback(RID p_texture, RenderingServerTypes::TextureDetectCallback p_callback, void *p_userdata) {
+	Texture *tex = texture_owner.get_or_null(p_texture);
+	ERR_FAIL_NULL(tex);
+
+	tex->detect_height_callback_ud = p_userdata;
+	tex->detect_height_callback = p_callback;
+}
+
 void TextureStorage::texture_debug_usage(List<RenderingServerTypes::TextureInfo> *r_info) {
 	for (const RID &rid : texture_owner.get_owned_list()) {
 		Texture *t = texture_owner.get_or_null(rid);