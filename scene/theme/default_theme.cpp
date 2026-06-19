59e4e00ea383eaa297e009d90fe5710b30aa295f
Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
2026-06-19 09:56:33 +0300

    Add missing file dialog icons to default theme.

--- scene/theme/default_theme.cpp
+++ scene/theme/default_theme.cpp
@@ -729,6 +729,13 @@ void fill_default_theme(Ref<Theme> &theme, const Ref<Font> &default_font, const 
 
 	theme->set_icon("file_thumbnail", "FileDialog", icons["file_thumbnail"]);
 	theme->set_icon("folder_thumbnail", "FileDialog", icons["folder_thumbnail"]);
+	theme->set_icon("menu_copy_path", "FileDialog", icons["action_copy"]);
+	theme->set_icon("menu_delete", "FileDialog", icons["action_remove"]);
+	theme->set_icon("menu_new_folder", "FileDialog", icons["folder_create"]);
+	theme->set_icon("menu_refresh", "FileDialog", icons["reload"]);
+	theme->set_icon("menu_show_in_file_manager", "FileDialog", icons["filesystem"]);
+	theme->set_icon("menu_open_bundle", "FileDialog", icons["folder_browse"]);
+
 	theme->set_color("folder_icon_color", "FileDialog", Color(1, 1, 1));
 	theme->set_color("file_icon_color", "FileDialog", Color(1, 1, 1));
 	theme->set_color("file_disabled_color", "FileDialog", Color(1, 1, 1, 0.25));
