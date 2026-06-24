2448996f8a6b3d21e8a586cbc15a40bcf0bc6183
Jon Tamayo <volsungn@gmail.com>
2026-06-24 13:49:51 +0200

    Just turn 1 to 0

--- thirdparty/vulkan/vk_mem_alloc.cpp
+++ thirdparty/vulkan/vk_mem_alloc.cpp
@@ -5,13 +5,6 @@
 #endif
 #endif
 
-// AMD integrated GPUs report a low VkPhysicalDeviceLimits::maxMemoryAllocationCount
-// (typically 4096). VMA 3.3.0 changed VMA_DEBUG_DONT_EXCEED_MAX_MEMORY_ALLOCATION_COUNT
-// to default to 1, which makes VMA itself fail allocations with VK_ERROR_TOO_MANY_OBJECTS
-// once that count is reached, instead of forwarding the request to the driver. This
-// regressed content-heavy projects that ran fine on 4.6 (VMA 3.1.0, where the default was
-// 0) on those GPUs. Restore the previous behavior and let the driver decide.
-// See https://github.com/godotengine/godot/issues/120534
 #define VMA_DEBUG_DONT_EXCEED_MAX_MEMORY_ALLOCATION_COUNT 0
 
 #include "vk_mem_alloc.h"
