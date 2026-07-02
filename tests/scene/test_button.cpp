892744c91995866cf91143f71ba9f5f9ae4687a8
Kuroda Kayn <kurodakayn@outlook.com>
2026-07-02 22:09:58 +0800

    Add tests for emulated Button input

--- tests/scene/test_button.cpp
+++ tests/scene/test_button.cpp
@@ -32,17 +32,20 @@
 
 TEST_FORCE_LINK(test_button)
 
+#include "core/input/input.h"
+#include "core/input/input_map.h"
 #include "scene/gui/button.h"
 #include "scene/main/scene_tree.h"
 #include "scene/main/window.h"
 #include "tests/display_server_mock.h"
+#include "tests/signal_watcher.h"
 
 namespace TestButton {
 
 TEST_CASE("[SceneTree][Button] is_hovered()") {
 	// Create new button instance.
 	Button *button = memnew(Button);
-	CHECK(button != nullptr);
+	REQUIRE(button != nullptr);
 	Window *root = SceneTree::get_singleton()->get_root();
 	root->add_child(button);
 
@@ -61,6 +64,111 @@ TEST_CASE("[SceneTree][Button] is_hovered()") {
 	SEND_GUI_MOUSE_MOTION_EVENT(Point2i(150, 150), MouseButtonMask::NONE, Key::NONE);
 	CHECK(button->is_hovered() == false);
 
+	memdelete(button);
+}
+
+TEST_CASE("[SceneTree][Button] touch event with emulated mouse triggers pressed once") {
+	Button *button = memnew(Button);
+	REQUIRE(button != nullptr);
+	Window *root = SceneTree::get_singleton()->get_root();
+	root->add_child(button);
+
+	button->set_size(Size2i(50, 50));
+	button->set_position(Size2i(10, 10));
+	button->set_action_mode(BaseButton::ACTION_MODE_BUTTON_PRESS);
+
+	const bool was_emulating_mouse_from_touch = Input::get_singleton()->is_emulating_mouse_from_touch();
+	Input::get_singleton()->set_emulate_mouse_from_touch(true);
+
+	SEND_GUI_MOUSE_MOTION_EVENT(Point2i(25, 25), MouseButtonMask::NONE, Key::NONE);
+	CHECK(button->is_hovered());
+
+	SIGNAL_WATCH(button, SNAME("pressed"));
+
+	SEND_GUI_TOUCH_EVENT(Point2i(25, 25), true, false);
+	SIGNAL_CHECK(SNAME("pressed"), { {} });
+
+	SEND_GUI_TOUCH_EVENT(Point2i(25, 25), false, false);
+	SIGNAL_CHECK_FALSE(SNAME("pressed"));
+
+	SIGNAL_UNWATCH(button, SNAME("pressed"));
+	Input::get_singleton()->set_emulate_mouse_from_touch(was_emulating_mouse_from_touch);
+	memdelete(button);
+}
+
+TEST_CASE("[SceneTree][Button] touch event with emulated mouse ui_accept triggers pressed once") {
+	Button *button = memnew(Button);
+	REQUIRE(button != nullptr);
+	Window *root = SceneTree::get_singleton()->get_root();
+	root->add_child(button);
+
+	button->set_size(Size2i(50, 50));
+	button->set_position(Size2i(10, 10));
+	button->set_action_mode(BaseButton::ACTION_MODE_BUTTON_PRESS);
+
+	Ref<InputEventMouseButton> ui_accept_event;
+	ui_accept_event.instantiate();
+	ui_accept_event->set_device(InputMap::ALL_DEVICES);
+	ui_accept_event->set_button_index(MouseButton::LEFT);
+	ui_accept_event->set_button_mask(MouseButtonMask::LEFT);
+	InputMap::get_singleton()->action_add_event(SNAME("ui_accept"), ui_accept_event);
+
+	const bool was_emulating_mouse_from_touch = Input::get_singleton()->is_emulating_mouse_from_touch();
+	Input::get_singleton()->set_emulate_mouse_from_touch(true);
+
+	SIGNAL_WATCH(button, SNAME("pressed"));
+
+	SEND_GUI_TOUCH_EVENT(Point2i(25, 25), true, false);
+	SIGNAL_CHECK(SNAME("pressed"), { {} });
+
+	SEND_GUI_TOUCH_EVENT(Point2i(25, 25), false, false);
+	SIGNAL_CHECK_FALSE(SNAME("pressed"));
+
+	SIGNAL_UNWATCH(button, SNAME("pressed"));
+	Input::get_singleton()->set_emulate_mouse_from_touch(was_emulating_mouse_from_touch);
+	InputMap::get_singleton()->action_erase_event(SNAME("ui_accept"), ui_accept_event);
+	memdelete(button);
+}
+
+TEST_CASE("[SceneTree][Button] emulated mouse ui_accept event does not trigger pressed") {
+	Button *button = memnew(Button);
+	REQUIRE(button != nullptr);
+	Window *root = SceneTree::get_singleton()->get_root();
+	root->add_child(button);
+
+	button->set_size(Size2i(50, 50));
+	button->set_position(Size2i(10, 10));
+	button->set_action_mode(BaseButton::ACTION_MODE_BUTTON_PRESS);
+
+	Ref<InputEventMouseButton> ui_accept_event;
+	ui_accept_event.instantiate();
+	ui_accept_event->set_device(InputMap::ALL_DEVICES);
+	ui_accept_event->set_button_index(MouseButton::LEFT);
+	ui_accept_event->set_button_mask(MouseButtonMask::LEFT);
+	InputMap::get_singleton()->action_add_event(SNAME("ui_accept"), ui_accept_event);
+
+	SEND_GUI_MOUSE_MOTION_EVENT(Point2i(25, 25), MouseButtonMask::NONE, Key::NONE);
+	CHECK(button->is_hovered());
+
+	SIGNAL_WATCH(button, SNAME("pressed"));
+
+	Ref<InputEventMouseButton> event;
+	event.instantiate();
+	event->set_device(InputEvent::DEVICE_ID_EMULATION);
+	event->set_position(Point2i(25, 25));
+	event->set_button_index(MouseButton::LEFT);
+	event->set_button_mask(MouseButtonMask::LEFT);
+	event->set_factor(1);
+	event->set_pressed(true);
+	CHECK(event->is_action(SNAME("ui_accept"), true));
+
+	_SEND_DISPLAYSERVER_EVENT(event);
+	MessageQueue::get_singleton()->flush();
+
+	SIGNAL_CHECK_FALSE(SNAME("pressed"));
+
+	SIGNAL_UNWATCH(button, SNAME("pressed"));
+	InputMap::get_singleton()->action_erase_event(SNAME("ui_accept"), ui_accept_event);
 	memdelete(button);
 }
 
