98e73a8dac656b5b351cc863a6bb836b2fd5072b
KamalaDaleyKhan <77870698+ThrdPrsnLmtd@users.noreply.github.com>
2026-07-08 11:01:31 -0500

    Tidying includes

--- tests/scene/test_button.cpp
+++ tests/scene/test_button.cpp
@@ -33,9 +33,6 @@
 TEST_FORCE_LINK(test_button)
 
 #include "scene/gui/button.h"
-#include "scene/main/scene_tree.h"
-#include "scene/main/window.h"
-#include "tests/display_server_mock.h"
 
 namespace TestButton {
 
