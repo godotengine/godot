f0a1bc64131f00f19f1d31d8b0e4ffcd83738ef5
Chris -- <41486691+Christakou@users.noreply.github.com>
2026-07-14 18:26:41 +0100

    Update editor/scene/3d/path_3d_editor_plugin.cpp
    Co-authored-by: Tomasz Chabora <kobewi4e@gmail.com>

--- editor/scene/3d/path_3d_editor_plugin.cpp
+++ editor/scene/3d/path_3d_editor_plugin.cpp
@@ -1162,7 +1162,7 @@ Path3DEditorPlugin::Path3DEditorPlugin() {
 	curve_auto_tangent_mode->set_theme_type_variation(SceneStringName(FlatButton));
 	curve_auto_tangent_mode->set_toggle_mode(true);
 	curve_auto_tangent_mode->set_focus_mode(Control::FOCUS_ACCESSIBILITY);
-	curve_auto_tangent_mode->set_tooltip_text(TTR("Auto Tangent Mode"));
+	curve_auto_tangent_mode->set_tooltip_text(TTRC("Auto Tangent Mode"));
 	curve_auto_tangent_mode->connect(SceneStringName(pressed), callable_mp(this, &Path3DEditorPlugin::_mode_changed).bind(MODE_AUTO_TANGENT));
 	toolbar->add_child(curve_auto_tangent_mode);
 
