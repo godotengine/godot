1aac621efef4dcaaf5fbef07013a2b1f1fa33a45
aurpine <12262539+aurpine@users.noreply.github.com>
2026-07-25 05:17:51 -0400

    Undo gdscript.cpp changes

--- modules/gdscript/gdscript.cpp
+++ modules/gdscript/gdscript.cpp
@@ -163,7 +163,6 @@ GDScriptInstance *GDScript::_create_instance(const Variant **p_args, int p_argco
 	instance->owner_id = p_owner->get_instance_id();
 #ifdef DEBUG_ENABLED
 	//needed for hot reloading
-	instance->member_indices_cache.reserve(member_indices.size());
 	for (const KeyValue<StringName, MemberInfo> &E : member_indices) {
 		instance->member_indices_cache[E.key] = E.value.index;
 	}
@@ -1557,8 +1556,8 @@ bool GDScriptInstance::set(const StringName &p_name, const Variant &p_value) {
 		HashMap<StringName, GDScript::MemberInfo>::Iterator E = script->member_indices.find(p_name);
 		if (E) {
 			const GDScript::MemberInfo *member = &E->value;
-			if (!member->data_type.is_type(p_value)) {
-				Variant value = p_value;
+			Variant value = p_value;
+			if (!member->data_type.is_type(value)) {
 				const Variant *args = &p_value;
 				Callable::CallError err;
 				Variant::construct(member->data_type.builtin_type, value, &args, 1, err);
@@ -1567,12 +1566,12 @@ bool GDScriptInstance::set(const StringName &p_name, const Variant &p_value) {
 				}
 			}
 			if (likely(script->valid) && member->setter) {
-				const Variant *args = &p_value;
+				const Variant *args = &value;
 				Callable::CallError err;
 				callp(member->setter, &args, 1, err);
 				return err.error == Callable::CallError::CALL_OK;
 			} else {
-				members[member->index] = p_value;
+				members[member->index] = value;
 				return true;
 			}
 		}
