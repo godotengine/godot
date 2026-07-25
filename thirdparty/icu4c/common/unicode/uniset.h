65571e51f78c748c90e318ffaf5e9afe18262fc9
Aaron Franke <arnfranke@yahoo.com>
2026-07-24 21:34:10 -0700

    [4.0] Remove final from ICU4C UnicodeSet class

--- thirdparty/icu4c/common/unicode/uniset.h
+++ thirdparty/icu4c/common/unicode/uniset.h
@@ -282,7 +282,7 @@ class RuleCharacterIterator;
  * @author Alan Liu
  * @stable ICU 2.0
  */
-class U_COMMON_API UnicodeSet U_FINAL : public UnicodeFilter {
+class U_COMMON_API UnicodeSet : public UnicodeFilter {
 private:
     /**
      * Enough for sets with few ranges.
