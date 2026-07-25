BendyLand/voltaire

Fix incorrect per-instance shader parameters when exporting in headless mode

servers/rendering/dummy/storage/mesh_storage.h
@@ -122,8 +122,21 @@ public:
 	virtual void mesh_surface_update_skin_region(RID p_mesh, int p_surface, int p_offset, const Vector<uint8_t> &p_data) override {}
 	virtual void mesh_surface_update_index_region(RID p_mesh, int p_surface, int p_offset, const Vector<uint8_t> &p_data) override {}
 
-	virtual void mesh_surface_set_material(RID p_mesh, int p_surface, RID p_material) override {}
-	virtual RID mesh_surface_get_material(RID p_mesh, int p_surface) const override { return RID(); }
+	virtual void mesh_surface_set_material(RID p_mesh, int p_surface, RID p_material) override {
+		DummyMesh *m = mesh_owner.get_or_null(p_mesh);
+		ERR_FAIL_NULL(m);
+		ERR_FAIL_UNSIGNED_INDEX((uint32_t)p_surface, m->surfaces.size());
+		RenderingServerTypes::SurfaceData s = m->surfaces.get(p_surface);
+		s.material = p_material;
+		m->surfaces.set(p_surface, s);
+		m->dependency.changed_notify(Dependency::DEPENDENCY_CHANGED_MATERIAL);
+	}
+	virtual RID mesh_surface_get_material(RID p_mesh, int p_surface) const override {
+		DummyMesh *m = mesh_owner.get_or_null(p_mesh);
+		ERR_FAIL_NULL_V(m, RID());
+		ERR_FAIL_UNSIGNED_INDEX_V((uint32_t)p_surface, m->surfaces.size(), RID());
+		return m->surfaces[p_surface].material;
+	}
 
 	virtual RenderingServerTypes::SurfaceData mesh_get_surface(RID p_mesh, int p_surface) const override {
 		DummyMesh *m = mesh_owner.get_or_null(p_mesh);

godotengine/godot

(cherry picked from commit 552cca4e7702b5ef944e74b4c86d4656ac82aa46)
Co-Authored-By: Pāvels Nadtočajevs <7645683+bruvzg@users.noreply.github.com>
Co-Authored-By: Jon Tamayo <volsungn@gmail.com>

servers/rendering/dummy/storage/mesh_storage.h
@@ -122,8 +122,21 @@ public:
 	virtual void mesh_surface_update_skin_region(RID p_mesh, int p_surface, int p_offset, const Vector<uint8_t> &p_data) override {}
 	virtual void mesh_surface_update_index_region(RID p_mesh, int p_surface, int p_offset, const Vector<uint8_t> &p_data) override {}
 
-	virtual void mesh_surface_set_material(RID p_mesh, int p_surface, RID p_material) override {}
-	virtual RID mesh_surface_get_material(RID p_mesh, int p_surface) const override { return RID(); }
+	virtual void mesh_surface_set_material(RID p_mesh, int p_surface, RID p_material) override {
+		DummyMesh *m = mesh_owner.get_or_null(p_mesh);
+		ERR_FAIL_NULL(m);
+		ERR_FAIL_UNSIGNED_INDEX((uint32_t)p_surface, m->surfaces.size());
+		RS::SurfaceData s = m->surfaces.get(p_surface);
+		s.material = p_material;
+		m->surfaces.set(p_surface, s);
+		m->dependency.changed_notify(Dependency::DEPENDENCY_CHANGED_MATERIAL);
+	}
+	virtual RID mesh_surface_get_material(RID p_mesh, int p_surface) const override {
+		DummyMesh *m = mesh_owner.get_or_null(p_mesh);
+		ERR_FAIL_NULL_V(m, RID());
+		ERR_FAIL_UNSIGNED_INDEX_V((uint32_t)p_surface, m->surfaces.size(), RID());
+		return m->surfaces[p_surface].material;
+	}
 
 	virtual RS::SurfaceData mesh_get_surface(RID p_mesh, int p_surface) const override {
 		DummyMesh *m = mesh_owner.get_or_null(p_mesh);