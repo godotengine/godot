1b1d55307bd3d4660263bcc2b5c4e1d54eb9abea
Robin Haudenhuyse <robin.haudenhuyse@gmail.com>
2026-07-27 19:39:21 +0200

    updated test_ray for new physics folder structure

--- servers/physics_3d/queries/physics_testray_query_result_3d.h
+++ servers/physics_3d/queries/physics_testray_query_result_3d.h
@@ -1,5 +1,5 @@
 /**************************************************************************/
-/*  physics_direct_space_state_3d.h                                       */
+/*  physics_testray_query_result_3d.h                                     */
 /**************************************************************************/
 /*                         This file is part of:                          */
 /*                             GODOT ENGINE                               */
@@ -30,39 +30,25 @@
 
 #pragma once
 
-#include "core/variant/type_info.h"
+#include "core/object/ref_counted.h"
 #include "servers/physics_3d/physics_server_3d_types.h"
-#include "servers/physics_3d/queries/physics_point_query_parameters_3d.h"
-#include "servers/physics_3d/queries/physics_ray_query_parameters_3d.h"
-#include "servers/physics_3d/queries/physics_shape_query_parameters_3d.h"
-#include "servers/physics_3d/queries/physics_testray_query_result_3d.h"
 
-class PhysicsDirectSpaceState3D : public Object {
-	GDCLASS(PhysicsDirectSpaceState3D, Object);
+class PhysicsTestRayResult3D : public RefCounted {
+	GDCLASS(PhysicsTestRayResult3D, RefCounted);
 
-private:
-	Dictionary _intersect_ray(RequiredParam<PhysicsRayQueryParameters3D> rp_ray_query);
-	bool _test_ray(RequiredParam<PhysicsRayQueryParameters3D> rp_ray_query, const Ref<PhysicsTestRayResult3D> &p_result);
-	TypedArray<Dictionary> _intersect_point(RequiredParam<PhysicsPointQueryParameters3D> rp_point_query, int p_max_results = 32);
-	TypedArray<Dictionary> _intersect_shape(RequiredParam<PhysicsShapeQueryParameters3D> rp_shape_query, int p_max_results = 32);
-	Vector<real_t> _cast_motion(RequiredParam<PhysicsShapeQueryParameters3D> rp_shape_query);
-	TypedArray<Vector3> _collide_shape(RequiredParam<PhysicsShapeQueryParameters3D> rp_shape_query, int p_max_results = 32);
-	Dictionary _get_rest_info(RequiredParam<PhysicsShapeQueryParameters3D> rp_shape_query);
+	PS3DT::RayResult result;
 
 protected:
 	static void _bind_methods();
 
 public:
-	virtual bool intersect_ray(const PS3DT::RayParameters &p_parameters, PS3DT::RayResult &r_result) = 0;
+	PS3DT::RayResult *get_result_ptr() { return &result; }
 
-	virtual int intersect_point(const PS3DT::PointParameters &p_parameters, PS3DT::ShapeResult *r_results, int p_result_max) = 0;
-
-	virtual int intersect_shape(const PS3DT::ShapeParameters &p_parameters, PS3DT::ShapeResult *r_results, int p_result_max) = 0;
-	virtual bool cast_motion(const PS3DT::ShapeParameters &p_parameters, real_t &p_closest_safe, real_t &p_closest_unsafe, PS3DT::ShapeRestInfo *r_info = nullptr) = 0;
-	virtual bool collide_shape(const PS3DT::ShapeParameters &p_parameters, Vector3 *r_results, int p_result_max, int &r_result_count) = 0;
-	virtual bool rest_info(const PS3DT::ShapeParameters &p_parameters, PS3DT::ShapeRestInfo *r_info) = 0;
-
-	virtual Vector3 get_closest_point_to_object_volume(RID p_object, const Vector3 p_point) const = 0;
-
-	PhysicsDirectSpaceState3D();
+	Vector3 get_position() const { return result.position; }
+	Vector3 get_normal() const { return result.normal; }
+	RID get_rid() const { return result.rid; }
+	ObjectID get_collider_id() const { return result.collider_id; }
+	Object *get_collider() const { return result.collider; }
+	int get_shape() const { return result.shape; }
+	int get_face_index() const { return result.face_index; }
 };
