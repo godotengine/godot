2bf85fd17e15dd0466688fd5877475be58cb378e
low-moon <298359369+low-moon@users.noreply.github.com>
2026-07-29 00:30:34 +0100

    Fixed subviewport container scaling when stretched

--- scene/debugger/runtime_node_select.cpp
+++ scene/debugger/runtime_node_select.cpp
@@ -811,7 +811,9 @@ void RuntimeNodeSelect::_update_selection() {
 			continue;
 		}
 
-		Transform2D xform = ci->get_global_transform_with_canvas();
+		// Convert item coordinates to screen coordinates then back to root canvas, to account for subviewports
+		Window *root = SceneTree::get_singleton()->get_root();
+		Transform2D xform = root->get_screen_transform().affine_inverse() * ci->get_viewport()->get_screen_transform() * ci->get_global_transform_with_canvas();
 
 		// Fallback.
 		Rect2 rect = Rect2(Vector2(), Vector2(10, 10));
