b678ccd6a6d62222889673e55767da58093468fa
Gabriel Zerbib <moddingear@gmail.com>
2026-07-31 14:18:54 +0200

    Allow CapsuleMesh, CaspuleShape and CylinderShape to have tapering, unify gizmo handles across Capsule and Cylinders

--- scene/resources/3d/primitive_meshes.h
+++ scene/resources/3d/primitive_meshes.h
@@ -122,16 +122,19 @@ public:
 };
 
 /**
-	Mesh for a simple capsule
+	A capsule
 */
 class CapsuleMesh : public PrimitiveMesh {
 	GDCLASS(CapsuleMesh, PrimitiveMesh);
 
 private:
-	float radius = 0.5;
-	float height = 2.0;
+	real_t top_radius = 0.5;
+	real_t bottom_radius = 0.5;
+	real_t mid_height = 1.0;
+	// number of points in the X-Z plane
 	int radial_segments = 64;
-	int rings = 8;
+	// number of rings in the Y axis
+	int rings = 16;
 
 protected:
 	static void _bind_methods();
@@ -140,13 +143,32 @@ protected:
 	virtual void _update_lightmap_size() override;
 
 public:
-	static void create_mesh_array(Array &p_arr, float radius, float height, int radial_segments = 64, int rings = 8, bool p_add_uv2 = false, const float p_uv2_padding = 1.0);
+	static bool is_sphere(real_t p_radius_top, real_t p_radius_bottom, real_t p_mid_height);
 
-	void set_radius(const float p_radius);
-	float get_radius() const;
+	// Return the angle of the tangent to the two circles. 0 when both circles have the same radius, positive when the bottom is bigger than the top, -pi/2 to pi/2
+	static real_t get_tangent_angle(real_t p_radius_top, real_t p_radius_bottom, real_t p_mid_height);
 
-	void set_height(const float p_height);
-	float get_height() const;
+	//Non-tapered mesh
+	static void create_mesh_array(Array &p_arr, real_t p_radius, real_t p_height, int p_radial_segments = 64, int p_rings = 16, bool p_add_uv2 = false, const real_t p_uv2_padding = 1.0);
+	//Tapered mesh
+	static void create_mesh_array(Array &p_arr, real_t p_radius_top, real_t p_radius_bottom, real_t p_mid_height, int p_radial_segments = 64, int p_rings = 16, bool p_add_uv2 = false, const real_t p_uv2_padding = 1.0);
+
+	void set_top_radius(const real_t p_top_radius);
+	real_t get_top_radius() const;
+
+	void set_bottom_radius(const real_t p_bottom_radius);
+	real_t get_bottom_radius() const;
+
+	void set_radius(const real_t p_radius);
+	real_t get_radius() const;
+
+	void set_mid_height(const real_t p_mid_height);
+	real_t get_mid_height() const;
+
+	void set_height(const real_t p_height);
+	real_t get_height() const;
+
+	bool is_tapered() const { return top_radius != bottom_radius; }
 
 	void set_radial_segments(const int p_segments);
 	int get_radial_segments() const;
