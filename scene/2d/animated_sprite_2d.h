fe27ded36f765135f2bd1aebcfc97ae8bc182f51
Michael Alexsander <michaelalexsander@protonmail.com>
2026-07-31 03:59:24 -0300

    Make possible to manipulate `CanvasItem`s in the Game view

--- scene/2d/animated_sprite_2d.h
+++ scene/2d/animated_sprite_2d.h
@@ -71,16 +71,14 @@ protected:
 	void _validate_property(PropertyInfo &p_property) const;
 
 public:
-#ifdef TOOLS_ENABLED
+#ifdef DEBUG_ENABLED
 	virtual Dictionary _edit_get_state() const override;
 	virtual void _edit_set_state(const Dictionary &p_state) override;
 
 	virtual void _edit_set_pivot(const Point2 &p_pivot) override;
 	virtual Point2 _edit_get_pivot() const override;
 	virtual bool _edit_use_pivot() const override;
-#endif // TOOLS_ENABLED
 
-#ifdef DEBUG_ENABLED
 	virtual Rect2 _edit_get_rect() const override;
 	virtual bool _edit_use_rect() const override;
 #endif // DEBUG_ENABLED
