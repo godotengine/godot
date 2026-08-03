70d49dec8fdd8683a7f0e68f8604e88c1ba8cd19
kobewi <kobewi4e@gmail.com>
2026-08-03 15:27:45 +0200

    Add object_created_in_editor signal
    Co-authored-by: Malcolm Nixon <Malcolm.nixon@gmail.com>

--- editor/docks/scene_tree_dock.cpp
+++ editor/docks/scene_tree_dock.cpp
@@ -1622,6 +1622,7 @@ void SceneTreeDock::_tool_selected(int p_tool, bool p_confirm_override) {
 				}
 			}
 
+			EditorNode::get_editor_data().instantiate_object_properties(new_node);
 			add_root_node(new_node);
 
 			if (GLOBAL_GET("editor/naming/node_name_casing").operator int() != NAME_CASING_PASCAL_CASE) {
@@ -3744,6 +3745,7 @@ void SceneTreeDock::_script_dropped(const String &p_file, NodePath p_to) {
 		}
 		new_node->set_name(Node::adjust_name_casing(p_file.get_file().get_basename()));
 		new_node->set_script(scr);
+		EditorNode::get_editor_data().instantiate_object_properties(new_node);
 
 		undo_redo->create_action(TTR("Instantiate Script"));
 		undo_redo->add_do_method(n, "add_child", new_node, true);
