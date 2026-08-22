6ede13ba9235386786b1d3104f125dfd6ba989ed
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-22 15:20:59 -0300

    Create rendering_server_3ds.cpp

---
+++ platform/3ds/rendering_server_3ds.cpp
@@ -0,0 +1,13 @@
+#include "rendering_server_3ds.h"
+
+RenderingServer3DS::RenderingServer3DS() {
+}
+
+RenderingServer3DS::~RenderingServer3DS() {
+}
+
+void RenderingServer3DS::initialize() {
+}
+
+void RenderingServer3DS::draw_frame() {
+}
