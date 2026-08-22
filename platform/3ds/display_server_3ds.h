f5f11c1b827f8fdf72c6f9a94c190c14308c7a11
HydroForgeEngine <lupazupaengine@gmail.com>
2026-08-22 15:18:37 -0300

    Create display_server_3ds.h

---
+++ platform/3ds/display_server_3ds.h
@@ -0,0 +1,12 @@
+#ifndef DISPLAY_SERVER_3DS_H
+#define DISPLAY_SERVER_3DS_H
+
+class DisplayServer3DS {
+public:
+    DisplayServer3DS();
+    ~DisplayServer3DS();
+
+    void process_events();
+};
+
+#endif
