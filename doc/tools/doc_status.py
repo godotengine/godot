44415593c63ddcbdfc6fd4ec94a1aa9f8e4f8d3e
anupamme <mediratta@gmail.com>
2026-08-25 01:37:58 +0000

    fix: gitlab.bandit.B313.B314.B315.B316.B318.B319.B320.B405.B406.B407.B408.B409.B410 security vulnerability
    Automated security fix generated by OrbisAI Security

--- doc/tools/doc_status.py
+++ doc/tools/doc_status.py
@@ -7,7 +7,7 @@ import math
 import os
 import re
 import sys
-import xml.etree.ElementTree as ET
+import defusedxml.ElementTree as ET
 
 sys.path.insert(0, os.path.join(os.path.dirname(os.path.abspath(__file__)), "../../"))
 
