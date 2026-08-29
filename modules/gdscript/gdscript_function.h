10d5c09106af72ead3a92a142d2bc5a67ed22630
Shoyguer <shoyguer@gmail.com>
2026-08-28 21:01:52 -0300

    Fix to breakpoints

--- modules/gdscript/gdscript_function.h
+++ modules/gdscript/gdscript_function.h
@@ -35,6 +35,7 @@
 #include "core/object/ref_counted.h"
 #include "core/object/script_language.h"
 #include "core/string/string_name.h"
+#include "core/templates/hash_set.h"
 #include "core/templates/pair.h"
 #include "core/templates/self_list.h"
 #include "core/variant/variant.h"
@@ -453,6 +454,10 @@ private:
 	String _get_callable_call_error(const String &p_where, const Callable &p_callable, const Variant **p_argptrs, int p_argcount, const Variant &p_ret, const Callable::CallError &p_err) const;
 #endif
 
+#ifdef TOOLS_ENABLED
+	HashSet<int> executable_lines;
+#endif
+
 	Variant _get_default_variant_for_data_type(const GDScriptDataType &p_data_type);
 
 public:
@@ -483,6 +488,10 @@ public:
 	_FORCE_INLINE_ int get_argument_count() const { return _argument_count; }
 	_FORCE_INLINE_ Variant get_rpc_config() const { return rpc_config; }
 	_FORCE_INLINE_ int get_max_stack_size() const { return _stack_size; }
+#ifdef TOOLS_ENABLED
+	_FORCE_INLINE_ const HashSet<int> &get_executable_lines() const { return executable_lines; }
+	_FORCE_INLINE_ const Vector<GDScriptFunction *> &get_lambdas() const { return lambdas; }
+#endif
 
 	Variant get_constant(int p_idx) const;
 	StringName get_global_name(int p_idx) const;
