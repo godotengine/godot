3a23a4d31460257460c612a9657873ad8edaace6
Dmytro Shakhun <82524202+feeib@users.noreply.github.com>
2026-08-31 11:25:00 +0200

    Update scene/animation/animation_mixer.cpp
    Co-authored-by: A Thousand Ships <96648715+AThousandShips@users.noreply.github.com>

--- scene/animation/animation_mixer.cpp
+++ scene/animation/animation_mixer.cpp
@@ -36,7 +36,6 @@
 #include "core/object/callable_mp.h"
 #include "core/object/class_db.h"
 #include "core/string/string_name.h"
-#include "core/typedefs.h"
 #include "scene/2d/audio_stream_player_2d.h"
 #include "scene/animation/animation_player.h"
 #include "scene/audio/audio_stream_player.h"
