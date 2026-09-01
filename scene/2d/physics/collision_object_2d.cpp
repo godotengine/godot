espumita00/test

Improve physics body/shape scale warnings, check shear/global, add to 2D

scene/2d/physics/collision_object_2d.cpp
@@ -589,6 +589,10 @@ PackedStringArray CollisionObject2D::get_configuration_warnings() const {
 		warnings.push_back(RTR("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape2D or CollisionPolygon2D as a child to define its shape."));
 	}
 
+	if (!get_transform().is_conformal() || !get_global_transform().is_conformal()) {
+		warnings.push_back(RTR("A non-uniformly scaled CollisionObject2D node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on both axes), and change the size in children collision shapes instead."));
+	}
+
 	return warnings;
 }
 

scene/3d/physics/collision_object_3d.cpp
@@ -735,9 +735,8 @@ PackedStringArray CollisionObject3D::get_configuration_warnings() const {
 		warnings.push_back(RTR("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape3D or CollisionPolygon3D as a child to define its shape."));
 	}
 
-	Vector3 scale = get_transform().get_basis().get_scale();
-	if (!(Math::is_zero_approx(scale.x - scale.y) && Math::is_zero_approx(scale.y - scale.z))) {
-		warnings.push_back(RTR("With a non-uniform scale this node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on all axes), and change the size in children collision shapes instead."));
+	if (!get_transform().get_basis().is_conformal() || !get_global_transform().get_basis().is_conformal()) {
+		warnings.push_back(RTR("A non-uniformly scaled CollisionObject3D node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on all axes), and change the size in children collision shapes instead."));
 	}
 
 	return warnings;

godotengine/godot

scene/2d/physics/collision_object_2d.cpp
@@ -610,6 +610,10 @@ PackedStringArray CollisionObject2D::get_configuration_warnings() const {
 		warnings.push_back(RTR("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape2D or CollisionPolygon2D as a child to define its shape."));
 	}
 
+	if (!get_transform().is_conformal() || !get_global_transform().is_conformal()) {
+		warnings.push_back(RTR("A non-uniformly scaled CollisionObject2D node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on both axes), and change the size in child collision shapes instead."));
+	}
+
 	return warnings;
 }
 

scene/3d/physics/collision_object_3d.cpp
@@ -756,9 +756,8 @@ PackedStringArray CollisionObject3D::get_configuration_warnings() const {
 		warnings.push_back(RTR("This node has no shape, so it can't collide or interact with other objects.\nConsider adding a CollisionShape3D or CollisionPolygon3D as a child to define its shape."));
 	}
 
-	Vector3 scale = get_transform().get_basis().get_scale();
-	if (!(Math::is_zero_approx(scale.x - scale.y) && Math::is_zero_approx(scale.y - scale.z))) {
-		warnings.push_back(RTR("With a non-uniform scale this node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on all axes), and change the size in children collision shapes instead."));
+	if (!get_transform().get_basis().is_conformal() || !get_global_transform().get_basis().is_conformal()) {
+		warnings.push_back(RTR("A non-uniformly scaled CollisionObject3D node will probably not function as expected.\nPlease make its scale uniform (i.e. the same on all axes), and change the size in child collision shapes instead."));
 	}
 
 	return warnings;