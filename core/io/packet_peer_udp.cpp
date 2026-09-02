1fa3b5aa714f4505705527a884f6145fe45e3a2d
Muhammad Mukarram Arif <96686328+mukarramarif@users.noreply.github.com>
2026-09-02 12:55:22 -0400

    Change port parameter type to uint16 in connect_to_host

--- core/io/packet_peer_udp.cpp
+++ core/io/packet_peer_udp.cpp
@@ -223,11 +223,11 @@ void PacketPeerUDP::disconnect_shared_socket() {
 	close();
 }
 
-Error PacketPeerUDP::connect_to_host(const IPAddress &p_host, int p_port) {
+Error PacketPeerUDP::connect_to_host(const IPAddress &p_host, uint16 p_port) {
 	ERR_FAIL_COND_V(udp_server, ERR_LOCKED);
 	ERR_FAIL_COND_V(_sock.is_null(), ERR_UNAVAILABLE);
 	ERR_FAIL_COND_V(!p_host.is_valid(), ERR_INVALID_PARAMETER);
-	ERR_FAIL_COND_V_MSG(p_port < 1 || p_port > 65535, ERR_INVALID_PARAMETER, "The remote port number must be between 1 and 65535 (inclusive).");
+	ERR_FAIL_COND_V_MSG(p_port < 0 || p_port > 65535, ERR_INVALID_PARAMETER, "The remote port number must be between 0 and 65535 (inclusive).");
 
 	Error err;
 
