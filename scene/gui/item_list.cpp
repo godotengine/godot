de169bd434c28b42764c12479db2a3c98c45debc
GalaxyShard <galaxyshard.vcs@byteroach.com>
2026-09-04 00:21:20 +0000

    Fix panning & touchpad scrolling on Linux & MacOS

--- scene/gui/item_list.cpp
+++ scene/gui/item_list.cpp
@@ -1170,8 +1170,9 @@ void ItemList::gui_input(const Ref<InputEvent> &p_event) {
 
 	Ref<InputEventPanGesture> pan_gesture = p_event;
 	if (pan_gesture.is_valid()) {
-		scroll_bar_v->set_value(scroll_bar_v->get_value() + scroll_bar_v->get_page() * pan_gesture->get_delta().y / 8);
-		scroll_bar_h->set_value(scroll_bar_h->get_value() + scroll_bar_h->get_page() * pan_gesture->get_delta().x / 8);
+		Vector2 delta = pan_gesture->get_delta();
+		scroll_bar_v->set_value(scroll_bar_v->get_value() + delta.y);
+		scroll_bar_h->set_value(scroll_bar_h->get_value() + delta.x);
 	}
 
 	if (scroll_value_modified && (scroll_bar_v->get_value() != prev_scroll_v || scroll_bar_h->get_value() != prev_scroll_h)) {
