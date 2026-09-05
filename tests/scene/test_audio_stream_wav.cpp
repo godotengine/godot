49e353f8cf6ea1bee021c9414231091d29c0e67d
Aaron Franke <arnfranke@yahoo.com>
2026-09-05 15:39:31 -0700

    Fix AudioStreamWAV integer sign sanitizer errors

--- tests/scene/test_audio_stream_wav.cpp
+++ tests/scene/test_audio_stream_wav.cpp
@@ -76,8 +76,9 @@ Vector<uint8_t> gen_pcm8_test(float wav_rate, int wav_count, bool stereo) {
 		// Map sin wave to full range of 8-bit values.
 		uint8_t wav_8bit = Math::fast_ftoi(((wav + 1) / 2) * UINT8_MAX);
 		// Unlike the .wav format, AudioStreamWAV expects signed 8-bit wavs.
-		uint8_t wav_8bit_signed = wav_8bit - (INT8_MAX + 1);
-		write_ptr[i] = wav_8bit_signed;
+		int8_t wav_8bit_signed = wav_8bit - (INT8_MAX + 1);
+		// Cast to uint8_t to match Vector<uint8_t> type, but this is really a signed value.
+		write_ptr[i] = (uint8_t)wav_8bit_signed;
 	}
 
 	return buffer;
@@ -104,8 +105,9 @@ Vector<uint8_t> gen_pcm16_test(float wav_rate, int wav_count, bool stereo) {
 		// Map sin wave to full range of 16-bit values.
 		uint16_t wav_16bit = Math::fast_ftoi(((wav + 1) / 2) * UINT16_MAX);
 		// The .wav format expects wavs larger than 8 bits to be signed.
-		uint16_t wav_16bit_signed = wav_16bit - (INT16_MAX + 1);
-		encode_uint16(wav_16bit_signed, write_ptr + (i * 2));
+		int16_t wav_16bit_signed = wav_16bit - (INT16_MAX + 1);
+		// Cast to uint16_t to match encode_uint16 type, but this is really a signed value.
+		encode_uint16((uint16_t)wav_16bit_signed, write_ptr + (i * 2));
 	}
 
 	return buffer;
