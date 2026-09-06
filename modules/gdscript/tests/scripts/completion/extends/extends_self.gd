abe63a168609387b487f1087ebda6e54b3f1af31
Dexter <Dexterf31415@gmail.com>
2026-09-06 09:46:00 +0330

    Code completion is not suggesting the subclass for inherited class

---
+++ modules/gdscript/tests/scripts/completion/extends/extends_self.gd
@@ -0,0 +1,9 @@
+extends Node
+
+class InnerClassA extends Node:
+	pass
+
+class InnerClassB extends Node:
+	pass
+
+class InnerClassC extends ➡
---
+++ modules/gdscript/tests/scripts/completion/extends/test_extends_class.notest.gd
@@ -0,0 +1,5 @@
+class_name TestExtendsClass
+extends Node
+
+class InnerClassA extends Node:
+	pass
