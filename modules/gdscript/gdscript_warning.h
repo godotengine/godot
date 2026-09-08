9c06028fd1995ef926b482beb97e28b5f4a215a9
HolonProduction <holonproduction@gmail.com>
2026-09-09 01:27:03 +0200

    Add GDScript warnings around setters and getters
    - Warn about using coroutines for setters getters
    - Warn about gradual typing between setters/getters and their vars

--- modules/gdscript/gdscript_warning.h
+++ modules/gdscript/gdscript_warning.h
@@ -93,6 +93,8 @@ public:
 		GET_NODE_DEFAULT_WITHOUT_ONREADY, // A class variable uses `get_node()` (or the `$` notation) as its default value, but does not use the @onready annotation.
 		ONREADY_WITH_EXPORT, // The `@onready` annotation will set the value after `@export` which is likely not intended.
 		ONREADY_WITH_CAST, // The cast will silently assign `null` if the node has a wrong type. This is likely not intended.
+		SET_GET_GRADUALLY_TYPED, // Typed setters for untyped vars or untyped getters typed vars might lead to runtime errors.
+		SET_GET_COROUTINE, // Coroutine getters violate the type by returning function states. Coroutine setters can prevent the var from updating correctly.
 #ifndef DISABLE_DEPRECATED
 		PROPERTY_USED_AS_FUNCTION, // Function not found, but there's a property with the same name.
 		CONSTANT_USED_AS_FUNCTION, // Function not found, but there's a constant with the same name.
@@ -153,6 +155,8 @@ public:
 		ERROR, // GET_NODE_DEFAULT_WITHOUT_ONREADY // May not work as expected.
 		ERROR, // ONREADY_WITH_EXPORT // May not work as expected.
 		WARN, // ONREADY_WITH_CAST
+		WARN, // SET_GET_GRADUALLY_TYPED
+		ERROR, // SET_GET_COROUTINE
 #ifndef DISABLE_DEPRECATED
 		WARN, // PROPERTY_USED_AS_FUNCTION
 		WARN, // CONSTANT_USED_AS_FUNCTION
