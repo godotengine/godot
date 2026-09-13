cde2d345c3a1156aa8c524bca53ba3ec443acd77
Julian Todd <julian@goatchurch.org.uk>
2026-09-13 15:58:27 +0100

    use math::abs

--- scene/3d/spring_bone_collision_capsule_3d.cpp
+++ scene/3d/spring_bone_collision_capsule_3d.cpp
@@ -328,7 +328,7 @@ static real_t _closest_capsule_sphere_to_taper(const Vector3 &p_head, const Vect
 	real_t y = x * capsule_vec_slope;
 	//real_t ya = y - x / (capsule_vec_slope * cone_slope * cone_slope);
 
-	real_t emu = (fabs(C_capsule_vec_inplane.y) > 0.5 ? y / (C_capsule_vec_inplane.y * p_length) : x / (C_capsule_vec_inplane.x * p_length)); // Same value, but protects from a division by 0.
+	real_t emu = (Math::abs(C_capsule_vec_inplane.y) > 0.5 ? y / (C_capsule_vec_inplane.y * p_length) : x / (C_capsule_vec_inplane.x * p_length)); // Same value, but protects from a division by 0.
 	real_t mu0 = mu + emu;
 
 	// If the sphere in the bone cone is off and endpoint, then pick a mu in the collision capsule
