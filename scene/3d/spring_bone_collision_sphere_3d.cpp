0a2d35679340e5ecbc758cf0d1019078f089f881
Julian Todd <julian@goatchurch.org.uk>
2026-09-13 16:03:53 +0100

    include order

--- scene/3d/spring_bone_collision_sphere_3d.cpp
+++ scene/3d/spring_bone_collision_sphere_3d.cpp
@@ -31,8 +31,9 @@
 #include "spring_bone_collision_sphere_3d.h"
 #include "spring_bone_collision_sphere_3d.compat.inc"
 
+#include "scene/3d/spring_bone_simulator_3d.h"
+
 #include "core/object/class_db.h"
-#include "spring_bone_simulator_3d.h"
 
 void SpringBoneCollisionSphere3D::set_radius(float p_radius) {
 	radius = p_radius;
