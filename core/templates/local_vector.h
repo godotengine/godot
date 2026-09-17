777e8632ca5fc4f119c55101ad9739b3f453ceba
HolonProduction <holonproduction@gmail.com>
2026-09-17 19:44:02 +0200

    Fix `LocalVector<T>::clear` when `T` has no default constructor

--- core/templates/local_vector.h
+++ core/templates/local_vector.h
@@ -76,6 +76,16 @@ private:
 		}
 	}
 
+	// Can't use `_resize(0)`, since it requires a no-arg-constructor even if it wouldn't be called.
+	void _clear() {
+		if constexpr (!std::is_trivially_destructible_v<T>) {
+			for (U i = 0; i < count; i++) {
+				data[i].~T();
+			}
+		}
+		count = 0;
+	}
+
 public:
 	_FORCE_INLINE_ T *ptr() _LIFETIME_BOUND_ { return data; }
 	_FORCE_INLINE_ const T *ptr() const _LIFETIME_BOUND_ { return data; }
@@ -174,7 +184,7 @@ public:
 	[[deprecated("Use reverse() instead")]] void invert() { reverse(); }
 #endif
 
-	_FORCE_INLINE_ void clear() { resize(0); }
+	_FORCE_INLINE_ void clear() { _clear(); }
 	_FORCE_INLINE_ void reset() {
 		clear();
 		if (data) {
