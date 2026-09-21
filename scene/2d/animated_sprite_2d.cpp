72f88180a10f7e1acded1dcf07b0dc43877ef0ef
Siguning <siguning@gmail.com>
2026-09-21 16:45:32 +0900

    fix: both 2d and 3d sprite's frame skipping bug on very short frame

--- scene/2d/animated_sprite_2d.cpp
+++ scene/2d/animated_sprite_2d.cpp
@@ -242,6 +242,13 @@ void AnimatedSprite2D::_notification(int p_what) {
 						frame_progress = 0.0;
 						queue_redraw();
 						emit_signal(SceneStringName(frame_changed));
+
+						speed = frames->get_animation_speed(animation) * speed_scale * custom_speed_scale * frame_speed_scale;
+						abs_speed = Math::abs(speed);
+
+						if (speed == 0) {
+							return; // Do nothing.
+						}
 					}
 					double to_process = MIN((1.0 - frame_progress) / abs_speed, remaining);
 					frame_progress += to_process * abs_speed;
@@ -273,6 +280,13 @@ void AnimatedSprite2D::_notification(int p_what) {
 						frame_progress = 1.0;
 						queue_redraw();
 						emit_signal(SceneStringName(frame_changed));
+
+						speed = frames->get_animation_speed(animation) * speed_scale * custom_speed_scale * frame_speed_scale;
+						abs_speed = Math::abs(speed);
+
+						if (speed == 0) {
+							return; // Do nothing.
+						}
 					}
 					double to_process = MIN(frame_progress / abs_speed, remaining);
 					frame_progress -= to_process * abs_speed;
